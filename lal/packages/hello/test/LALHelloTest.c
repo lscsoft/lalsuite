9080d8fda4bc227ad71a13885547e20ec6a0e191
jolien <jolien>
2000-03-12 23:14:46 +0000

    Added ClearStatus() routine. Removed file protected. Original: 26798eb69b1a555634104173e490d705e3cd9b53

--- lal/packages/hello/test/LALHelloTest.c
+++ lal/packages/hello/test/LALHelloTest.c
@@ -7,6 +7,26 @@
  *-----------------------------------------------------------------------
  */
 
+#ifndef _STDIO_H
+#include <stdio.h>
+#ifndef _STDIO_H
+#define _STDIO_H
+#endif
+#endif
+
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#ifndef _STDLIB_H
+#define _STDLIB_H
+#endif
+#endif
+
+#ifndef _STRING_H
+#include <string.h>
+#ifndef _STRING_H
+#define _STRING_H
+#endif
+#endif
 
 #ifndef _LALSTDLIB_H
 #include "LALStdlib.h"
@@ -22,17 +42,185 @@
 #endif
 #endif
 
+#define CODES_(x) #x
+#define CODES(x) CODES_(x)
+
 NRCSID (MAIN, "$Id$");
 
-int debuglevel = 2;
+extern char *optarg;
+extern int   optind;
+
+int debuglevel = 0;
+int verbose    = 0;
+
+static void
+Usage (const char *program, int exitflag);
+
+static void
+ParseOptions (int argc, char *argv[]);
+
+static void
+TestStatus (Status *status, const char *expectedCodes, int exitCode);
+
+static void
+ClearStatus (Status *status);
 
 int
-main ()
+main (int argc, char *argv[])
 {
   static Status status;
+
+  ParseOptions (argc, argv);
+
   LALHello (&status, NULL);
-  REPORTSTATUS (&status);
-  LALHello (&status, "protected");
-  REPORTSTATUS (&status);
+  TestStatus (&status, CODES(0), 1);
+
+  LALHello (&status, "\0");
+  TestStatus (&status, CODES(-1), 1);
+  /* forget to clear status */
+
+  LALHello (&status, NULL);
+  TestStatus (&status, CODES(-2), 1);
+
+  /* now clear status */
+  ClearStatus (&status);
+
+  LALCheckMemoryLeaks ();
   return 0;
+}
+
+
+/*
+ * TestStatus ()
+ *
+ * Routine to check that the status code status->statusCode agrees with one of
+ * the codes specified in the space-delimited string ignored; if not,
+ * exit to the system with code exitcode.
+ *
+ */
+static void
+TestStatus (Status *status, const char *ignored, int exitcode)
+{
+  char  str[64];
+  char *tok;
+
+  if (verbose)
+  {
+    REPORTSTATUS (status);
+  }
+
+  if (strncpy (str, ignored, sizeof (str)))
+  {
+    if ((tok = strtok (str, " ")))
+    {
+      do
+      {
+        if (status->statusCode == atoi (tok))
+        {
+          return;
+        }
+      }
+      while ((tok = strtok (NULL, " ")));
+    }
+    else
+    {
+      if (status->statusCode == atoi (tok))
+      {
+        return;
+      }
+    }
+  }
+
+  fprintf (stderr, "\nExiting to system with code %d\n", exitcode);
+  exit (exitcode);
+}
+
+
+/*
+ *
+ * ClearStatus ()
+ *
+ * Recursively applies DETATCHSTATUSPTR() to status structure to destroy
+ * linked list of statuses.
+ *
+ */
+void
+ClearStatus (Status *status)
+{
+  if (status->statusPtr)
+  {
+    ClearStatus      (status->statusPtr);
+    DETATCHSTATUSPTR (status);
+  }
+}
+
+
+/*
+ * Usage ()
+ *
+ * Prints a usage message for program program and exits with code exitcode.
+ *
+ */
+static void
+Usage (const char *program, int exitcode)
+{
+  fprintf (stderr, "Options:\n");
+  fprintf (stderr, "  -h         print this message\n");
+  fprintf (stderr, "  -q         quiet: run silently\n");
+  fprintf (stderr, "  -v         verbose: print extra information\n");
+  fprintf (stderr, "  -d level   set debuglevel to level\n");
+  exit (exitcode);
+}
+
+
+/*
+ * ParseOptions ()
+ *
+ * Parses the argc - 1 option strings in argv[].
+ *
+ */
+static void
+ParseOptions (int argc, char *argv[])
+{
+  while (1)
+  {
+    int c = -1;
+
+    c = getopt (argc, argv, "hqvd:");
+    if (c == -1)
+    {
+      break;
+    }
+
+    switch (c)
+    {
+      case 'd': /* set debug level */
+        debuglevel = atoi (optarg);
+        break;
+
+      case 'v': /* verbose */
+        ++verbose;
+        break;
+
+      case 'q': /* quiet: run silently (ignore error messages) */
+        freopen ("/dev/null", "w", stderr);
+        freopen ("/dev/null", "w", stdout);
+        break;
+
+      case 'h':
+        Usage (argv[0], 0);
+        break;
+
+      default:
+        Usage (argv[0], 1);
+    }
+
+  }
+
+  if (optind < argc)
+  {
+    Usage (argv[0], 1);
+  }
+
+  return;
 }
