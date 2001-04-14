b2d0f35fad0b5c03d49f1cf522c9fb511e0c8808
duncan <duncan>
2001-04-14 00:20:17 +0000

    removed the (currently incorrect) test programs Original: 5c91693b7454ac761e592897212b6bbeb5a54fe5

--- lal/packages/findchirp/test/FindChirpMain.c
+++ lal/packages/findchirp/test/FindChirpMain.c
@@ -16,59 +16,20 @@
 
 NRCSID (MAIN, "$Id$");
 
-/* global variables */
-#define FINDCHIRPGLOBAL_INIT
-#include "FindChirpGlobal.h"
-#undef FINDCHIRPGLOBAL_INIT
+/*
+ *
+ * removed test programe until new engine is complete
+ *
+ */
 
-int lalDebugLevel = 1;
+int lalDebugLevel = 0;
 
 int
 main (int argc, char *argv[])
 {
-  static LALStatus  stat;
-  MPIDebugParams debug;
-  MPIId          id;
+  MPI_Init( &argc, &argv );
+  MPI_Finalize();
 
-  MPI_Init (&argc, &argv);
-  MPI_Comm_size (MPI_COMM_WORLD, &id.numProcs);
-  MPI_Comm_rank (MPI_COMM_WORLD, &id.myId);
-  MPI_Get_processor_name (id.procName, &id.nameLen);
-
-  /* write kill script */
-  LALMPIKillScript (&stat, &id, MPI_COMM_WORLD);
-
-  /* debugging example */
-  if (0)
-  {
-    LALMPIExportEnvironment (&stat, "DISPLAY", id.myId, MPI_COMM_WORLD);
-    debug.debugger = id.myId == 0 ? "ddd" : NULL ; /* attatch ddd to master */
-    debug.progName = argv[0];
-    debug.delay    = 15;
-    debug.myId     = id.myId;
-    debug.mpiComm  = MPI_COMM_WORLD;
-    LALMPIDebug (&stat, &debug);
-  }
-
-  if (id.myId == 0)
-  {
-    Master (&stat, id);
-    REPORTSTATUS (&stat);
-  }
-  else
-  {
-    char fname[64];
-    sprintf (fname, "Slave%03d.err", id.myId);
-    freopen (fname, "w", stderr);
-    sprintf (fname, "Slave%03d.out", id.myId);
-    freopen (fname, "w", stdout);
-    Slave (&stat, id);
-    REPORTSTATUS (&stat);
-  }
-
-  MPI_Finalize ();
-
-  LALCheckMemoryLeaks ();
   return 0;
 }
 
