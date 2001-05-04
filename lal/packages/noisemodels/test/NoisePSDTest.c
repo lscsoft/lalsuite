25113ac207149f7fbb914eb3db83b6b2ff6b6e7c
duncan <duncan>
2001-05-04 22:26:09 +0000

    added new code from Sathya: inspiral010504.tar.gz Original: eb24df79af320f9159217f74fde305c40447a5c7

---
+++ lal/packages/noisemodels/test/NoisePSDTest.c
@@ -0,0 +1,63 @@
+/*  <lalVerbatim file="NoisePSDTestCV">
+Author: Sathyaprakash, B. S.
+$Id$
+</lalVerbatim>  */
+/* <lalLaTeX>
+\subsection{Module \texttt{NoisePSDTest.c}}
+Module to create NoiseSpectralDensity.
+
+\subsubsection*{Prototypes}
+\vspace{0.1in}
+\input{NoisePSDTestCP}
+\index{\verb&NoisePSDTest()&}
+
+\subsubsection*{Description}
+\subsubsection*{Algorithm}
+\subsubsection*{Uses}
+\begin{verbatim}
+\end{verbatim}
+
+\subsubsection*{Notes}
+
+\vfill{\footnotesize\input{NoisePSDTestCV}}
+</lalLaTeX>  */
+#include <lal/LALNoiseModels.h>
+
+INT4 lalDebugLevel=1;
+/*  <lalVerbatim file="NoisePSDTestCP"> */
+int main () 
+{  /*  </lalVerbatim>  */
+
+   static LALStatus status;
+   REAL8Vector psd;
+   REAL8       df;
+   INT4 i;
+
+   df = 1.0;
+   psd.length = 8193;
+
+   psd.data = LALMalloc(sizeof(REAL8) * psd.length);
+
+   LALNoiseSpectralDensity(&status, &psd, &LALGEOPsd, df); 
+
+   for (i=2; i<(INT4)psd.length; i++) {
+        if (psd.data[i]) printf ("%d %e\n", i, psd.data[i]);
+   }
+   printf("&\n");
+   LALNoiseSpectralDensity(&status, &psd, &LALLIGOIPsd, df); 
+   for (i=2; i<(INT4)psd.length; i++) {
+        if (psd.data[i]) printf ("%d %e\n", i, psd.data[i]);
+   }
+   printf("&\n");
+   LALNoiseSpectralDensity(&status, &psd, &LALVIRGOPsd, df); 
+   for (i=2; i<(INT4)psd.length; i++) {
+        if (psd.data[i]) printf ("%d %e\n", i, psd.data[i]);
+   }
+   printf("&\n");
+   LALNoiseSpectralDensity(&status, &psd, &LALTAMAPsd, df); 
+   for (i=2; i<(INT4)psd.length; i++) {
+        if (psd.data[i]) printf ("%d %e\n", i, psd.data[i]);
+   }
+  return 0;
+}
+
