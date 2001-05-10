4456edad50c1b2811ac2bd484634dfe2ba60d148
duncan <duncan>
2001-05-10 02:08:57 +0000

    added module to manipulate linked lists of templates and template nodes Original: d8257443e9daef423132d5551693959170eacef1

---
+++ lal/packages/findchirp/src/FindChirpLinkedList.c
@@ -0,0 +1,110 @@
+/*----------------------------------------------------------------------- 
+ * 
+ * File Name: FindChirpLinkedList.c
+ *
+ * Author: Brown, D. A.
+ * 
+ * Revision: $Id$
+ * 
+ *-----------------------------------------------------------------------
+ */
+
+#include <lal/FindChirpEngine.h>
+
+NRCSID (FINDCHIRPLINKEDLISTC, "$Id$");
+
+void
+LALFindChirpCreateTmpltNode (
+    LALStatus                  *status,
+    InspiralTemplate           *tmplt,
+    InspiralTemplateNode      **tmpltNode
+    )
+{
+  InspiralTemplateNode         *current = NULL;
+
+  INITSTATUS( status, "LALFindChirpCreateTmpltNode", FINDCHIRPLINKEDLISTC );
+
+  ASSERT( tmplt, status, FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+
+
+  /*
+   *
+   * create a new template node after the current one
+   *
+   */
+
+
+  /* store the address of the current node */
+  current = *tmpltNode;
+
+  /* create memory for the template node */
+  *tmpltNode = (InspiralTemplateNode *) LALMalloc( sizeof(InspiralTemplateNode) );
+  if ( !tmpltNode )
+  {
+    ABORT( status, FINDCHIRPENGINEH_EALOC, FINDCHIRPENGINEH_MSGEALOC );
+  }
+
+  (*tmpltNode)->prev     = NULL;
+  (*tmpltNode)->next     = NULL;
+  (*tmpltNode)->tmpltPtr = tmplt;
+
+  /* link the list */
+  if ( current ) 
+  {
+    if ( current->next )
+    {
+      (*tmpltNode)->next = current->next;
+      current->next->prev = *tmpltNode;
+    }
+    (*tmpltNode)->prev = current;
+    current->next = *tmpltNode;
+  }
+
+  RETURN( status );
+}  
+
+
+void
+FindChirpDestroyTmpltNode ( 
+    LALStatus                  *status,
+    InspiralTemplateNode      **tmpltNode
+    )
+{
+  InspiralTemplateNode  *prev = NULL;
+  InspiralTemplateNode  *next = NULL;
+
+  INITSTATUS( status, "FindChirpDestroyTmpltNode", FINDCHIRPLINKEDLISTC );
+
+  ASSERT( tmpltNode, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+
+
+  /*
+   *
+   * destroy the node pointed at and return a pointer to the node before
+   *
+   */
+
+  
+  /* store the previous and next nodes */
+  prev = (*tmpltNode)->prev;
+  next = (*tmpltNode)->next;
+
+  /* destroy the node */
+  LALFree( *tmpltNode );
+  *tmpltNode = NULL;
+
+  /* relink the list */
+  if ( next ) next->prev = prev;
+  if ( prev ) 
+  {
+    prev->next = next;
+    *tmpltNode = prev;
+  }
+  else
+  {
+    *tmpltNode = next;
+  }
+  
+  RETURN( status );
+}  
