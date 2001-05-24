16cab6057daf11f73078a87f1d2cbcc1f44676a9
whelan <whelan>
2001-05-24 23:30:23 +0000

    added VIRGO, GEO600, TAMA300, and CIT40 and changed some names Original: 791aee49193093a9cdd5a20c33185886eb1c3c31

--- lal/packages/tools/include/DetectorSite.h
+++ lal/packages/tools/include/DetectorSite.h
@@ -6,7 +6,7 @@ $Id$
 /********************************* <lalLaTeX>
 
 \section{Header \texttt{DetectorSite.h}}
-\label{s:DetectorSite.h}
+\label{tools:s:DetectorSite.h}
 
 This header defines structures to hold the basic data describing
 a gravitational wave detector.  
@@ -17,12 +17,12 @@ a gravitational wave detector.
 \end{verbatim}
 
 According to the common frame format specification
-\cite{LIGOVIRGO:2000} the geometry of an interferometric detector will
-be stored in a \texttt{FrDetector} structure, specifying the location
-of the detector vertex and the orientation of its arms in geodetic
-co\"{o}rdinates suited to geographical surveying.  Resonant bars and
-other sorts of detectors, if they write their data to frames, are
-expected to fill this structure with their location and
+\cite{tools:LIGOVIRGO:2000} the geometry of an interferometric
+detector will be stored in a \texttt{FrDetector} structure, specifying
+the location of the detector vertex and the orientation of its arms in
+geodetic co\"{o}rdinates suited to geographical surveying.  Resonant
+bars and other sorts of detectors, if they write their data to frames,
+are expected to fill this structure with their location and
 orientation in some way suited to the detector type.
 
 For most data analysis tasks, however, any gravitational wave detector
@@ -60,7 +60,7 @@ in degrees, increases to the North, and ranges
 from $-90^\circ$ to $90^\circ$.  The \textit{elevation} $h$ is the
 signed distance along this ray from the reference ellipsoid to the
 point in question.  This co\"{o}rdinate system is described in more
-detail in \cite{Althouse:1999}.
+detail in \cite{tools:Althouse:1999}.
 
 \subsubsection*{Altitude and Azimuth Angles}
 
@@ -85,22 +85,22 @@ $x^2$ axis which pierces the earth's surface at 90 degrees East
 longitude on the equator, and an $x^3$ axis which pierces the Earth's
 surface at the North Pole.  The co\"{o}rdinates $x^1$, $x^2$, $x^3$
 correspond to the Earth-fixed co\"{o}rdinates $X_E$, $Y_E$, $Z_E$
-defined in \cite{Althouse:1999}, respectively.
+defined in \cite{tools:Althouse:1999}, respectively.
 
 The relationship between geodetic and Cartesian co\"{o}rdinates is
 given by
 \begin{eqnarray}
-\label{e:cart1}
+\label{tools:e:cart1}
 x^1&=&\left(
           \frac{a^2}{\sqrt{a^2\cos^2\beta+b^2\sin^2\beta}}
           + h
        \right) \cos\beta\cos\lambda             \\
-\label{e:cart2}
+\label{tools:e:cart2}
 x^2&=&\left(
           \frac{a^2}{\sqrt{a^2\cos^2\beta+b^2\sin^2\beta}}
           + h
        \right) \cos\beta\sin\lambda             \\
-\label{e:cart3}
+\label{tools:e:cart3}
 x^3&=&\left(
           \frac{b^2}{\sqrt{a^2\cos^2\beta+b^2\sin^2\beta}}
           + h
@@ -114,19 +114,19 @@ x^3&=&\left(
 
 /**************************************** <lalLaTeX file="DetectorSiteHB">
 
-\bibitem{LIGOVIRGO:2000}
+\bibitem{tools:LIGOVIRGO:2000}
 LIGO Data Group and VIRGO Data Acquisition Group, ``Specification of a
 Common Data Frame Format for Interferometric Gravitational Wave
 Detectors (IGWD)'', LIGO Technical Note
 \href{http://www.ligo.caltech.edu/docs/T/T970130-D.pdf}{LIGO-T970130}
 
-\bibitem{Althouse:1999}
+\bibitem{tools:Althouse:1999}
 William Althouse, Larry Jones, and Albert Lazzarini, ``Determination
 of Global and Local Coordinate Axes for the LIGO Sites'', LIGO
 Technical Note
 \href{http://www.ligo.caltech.edu/docs/T/T980044-10.pdf}{LIGO-T980044}
 
-% \bibitem{Lazzarini:1995}
+% \bibitem{tools:Lazzarini:1995}
 % Albert Lazzarini, ``Derivation
 % of Global and Local Coordinate Axes for the LIGO Sites'', LIGO
 % Technical Note LIGO-T950004
@@ -144,7 +144,6 @@ extern "C" {
 
 NRCSID (DETECTORSITEH, "$Id$");
 
-
 /******************************** <lalErrTable file="DetectorSiteHE"> */
 
 #define DETECTORSITEH_ENULLP        1
@@ -161,12 +160,12 @@ NRCSID (DETECTORSITEH, "$Id$");
 
 \subsubsection*{The \texttt{LALDetectorType} enumeration}
 \index{\texttt{LALDetectorType}}
-\index{\texttt{LALABSENTDETECTOR}}
-\index{\texttt{LALIFODIFFDETECTOR}}
-\index{\texttt{LALIFOXARMDETECTOR}}
-\index{\texttt{LALIFOYARMDETECTOR}}
-\index{\texttt{LALIFOCOMMDETECTOR}}
-\index{\texttt{LALCYLBARDETECTOR}}
+\index{\texttt{LALDETECTORTYPE\_ABSENT}}
+\index{\texttt{LALDETECTORTYPE\_IFODIFF}}
+\index{\texttt{LALDETECTORTYPE\_IFOXARM}}
+\index{\texttt{LALDETECTORTYPE\_IFOYARM}}
+\index{\texttt{LALDETECTORTYPE\_IFOCOMM}}
+\index{\texttt{LALDETECTORTYPE\_CYLBAR}}
 
 Since data from bars as well as interferometers can be written to
    frames, we need an additional piece of information to interpret the
@@ -175,35 +174,43 @@ Since data from bars as well as interferometers can be written to
    long axis of a bar?  The \texttt{LALDetectorType} enumeration
    provides a way to keep track of that.
 
-The possible values are:
+The possible values are (each value is prefaced by
+\texttt{LALDETECTORTYPE\_}):
 \begin{description}
-  \item[\texttt{LALABSENTDETECTOR}] No \texttt{FrDetector} associated with the structure
-  \item[\texttt{LALIFODIFFDETECTOR}] Interferometer in differential mode
-  \item[\texttt{LALIFOXARMDETECTOR}] Interferometer in one-armed mode (X arm)
-  \item[\texttt{LALIFOYARMDETECTOR}] Interferometer in one-armed mode (Y arm)
-  \item[\texttt{LALIFOCOMMDETECTOR}] Interferometer in common mode
-  \item[\texttt{LALCYLBARDETECTOR}]  Cylindrical bar
-%  \item[\texttt{SPHEREMONO}] Resonant sphere in $\ell=0$ mode
-%  \item[\texttt{SPHEREQUAD0}] Resonant sphere in $\ell=2$, $m=0$ mode
-%  \item[\texttt{SPHEREQUAD1S}] Resonant sphere in $\ell=2$, 
+  \item[\texttt{LALDETECTORTYPE\_ABSENT}] No \texttt{FrDetector} 
+	associated with the structure
+  \item[\texttt{LALDETECTORTYPE\_IFODIFF}] Interferometer 
+	in differential mode
+  \item[\texttt{LALDETECTORTYPE\_IFOXARM}] Interferometer 
+	in one-armed mode (X arm)
+  \item[\texttt{LALDETECTORTYPE\_IFOYARM}] Interferometer 
+	in one-armed mode (Y arm)
+  \item[\texttt{LALDETECTORTYPE\_IFOCOMM}] Interferometer in common mode
+  \item[\texttt{LALDETECTORTYPE\_CYLBAR}]  Cylindrical bar
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREMONO}] Resonant sphere in $\ell=0$ mode
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREQUAD0}] Resonant sphere in
+%	$\ell=2$, $m=0$ mode
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREQUAD1S}] Resonant sphere in $\ell=2$, 
 %     $m=\pm 1$ (sine) mode
-%  \item[\texttt{SPHEREQUAD1C}] Resonant sphere in $\ell=2$, 
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREQUAD1C}] Resonant sphere in $\ell=2$, 
 %     $m=\pm 1$ (cosine) mode
-%  \item[\texttt{SPHEREQUAD2S}] Resonant sphere in $\ell=2$, 
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREQUAD2S}] Resonant sphere in $\ell=2$, 
 %     $m=\pm 2$ (sine) mode
-%  \item[\texttt{SPHEREQUAD2C}] Resonant sphere in $\ell=2$, 
+%  \item[\texttt{LALDETECTORTYPE\_SPHEREQUAD2C}] Resonant sphere in $\ell=2$, 
 %     $m=\pm 2$ (cosine) mode
 \end{description}
 
 ********************************** </lalLaTeX> */
 
+#define LALIFODIFFDETECTOR 1
+
 typedef enum {
-  LALABSENTDETECTOR,       /* No FrDetector associated with the structure */
-  LALIFODIFFDETECTOR,      /* IFO in differential mode */
-  LALIFOXARMDETECTOR,      /* IFO in one-armed mode (X arm) */
-  LALIFOYARMDETECTOR,      /* IFO in one-armed mode (Y arm) */
-  LALIFOCOMMDETECTOR,      /* IFO in common mode */
-  LALCYLBARDETECTOR,       /* Cylindrical Bar */
+  LALDETECTORTYPE_ABSENT,       /* No FrDetector associated with the structure */
+  LALDETECTORTYPE_IFODIFF,      /* IFO in differential mode */
+  LALDETECTORTYPE_IFOXARM,      /* IFO in one-armed mode (X arm) */
+  LALDETECTORTYPE_IFOYARM,      /* IFO in one-armed mode (Y arm) */
+  LALDETECTORTYPE_IFOCOMM,      /* IFO in common mode */
+  LALDETECTORTYPE_CYLBAR,       /* Cylindrical Bar */
   /* SPHEREMONO,    Resonant Sphere in l=0 mode */
   /* SPHEREQUAD0,   Resonant sphere in l=2, m=0 mode */
   /* SPHEREQUAD1S,  Resonant sphere in l=2, m=+/-1 (sine) mode */
@@ -217,47 +224,54 @@ typedef enum {
 
 \subsubsection*{Cached Detectors}
 
-\index{\texttt{lalCachedDetectors[]}}
-In practice, we will often be working with fixed unchanging site
-   geometry, e.g., for the LIGO interferometers; to avoid constantly
-   reconstructing the corresponding \texttt{LALDetector}s, we should
-   define some constant \texttt{LALDetector}s describing them.  Those
-   are stored in a constant array of \texttt{LALDetector} structures
-   known as \texttt{lalCachedDetectors}, which is declared
-   \texttt{extern} in this header and defined in
-   \texttt{CreateDetector.c} (see Sec.~\ref{ss:CreateDetector.c}).
+\index{\texttt{lalCachedDetectors[]}} In practice, we will often be
+working with fixed unchanging site geometry, e.g., for the LIGO
+interferometers; to avoid constantly reconstructing the corresponding
+\texttt{LALDetector}s, we should define some constant
+\texttt{LALDetector}s describing them.  Those are stored in a constant
+array of \texttt{LALDetector} structures known as
+\texttt{lalCachedDetectors}, which is declared \texttt{extern} in this
+header and defined in \texttt{CreateDetector.c} (see
+Sec.~\ref{tools:ss:CreateDetector.c}).
 
 The \texttt{LALCreateDetector()} routine will first look through the
-   \texttt{lalCachedDetectors} array for a \texttt{LALDetector}
-   structure with matching \texttt{type} and \texttt{frDetector.name}
-   fields; if it finds one, it returns a copy of that; if not, it
-   creates one.
+\texttt{lalCachedDetectors} array for a \texttt{LALDetector} structure
+with matching \texttt{type} and \texttt{frDetector.name} fields; if it
+finds one, it returns a copy of that; if not, it creates one.
 
-   The header \texttt{DetectorSite.h} also defines an enumeration of
-   the indices of the known detectors:
-   \index{\texttt{LALDetectorIndexLHODIFF}}
-   \index{\texttt{LALDetectorIndexLLODIFF}}
-   \index{\texttt{LALNumCachedDetectors}}
+The header \texttt{DetectorSite.h} also defines an enumeration of the
+indices of the known detectors:
+\index{\texttt{LALDetectorIndexLHODIFF}}
+\index{\texttt{LALDetectorIndexLLODIFF}}
+\index{\texttt{LALDetectorIndexVIRGODIFF}}
+\index{\texttt{LALDetectorIndexGEO600DIFF}}
+\index{\texttt{LALDetectorIndexTAMA300DIFF}}
+\index{\texttt{LALDetectorIndexCIT40DIFF}}
+\index{\texttt{LALNumCachedDetectors}}
 ********************************** </lalLaTeX> */
 
-/********************************* <lalVerbatim> */
+/********************************** <lalVerbatim> */
 enum
 {
   LALDetectorIndexLHODIFF,
   LALDetectorIndexLLODIFF,
+  LALDetectorIndexVIRGODIFF,
+  LALDetectorIndexGEO600DIFF,
+  LALDetectorIndexTAMA300DIFF,
+  LALDetectorIndexCIT40DIFF,
   LALNumCachedDetectors
 };
 /********************************** </lalVerbatim> */
 
 
-/********************************* <lalLaTeX> 
+  /********************************* <lalLaTeX> 
 For example, the \texttt{LALDetector} representing LIGO Hanford in
 differential mode is
 \texttt{lalCachedDetectors[LALDetectorIndexLHODIFF]}.
 
 \subsection*{Structures}
 
-/********************************** </lalLaTeX> */
+********************************** </lalLaTeX> */
 
 /********************************* <lalLaTeX> 
 
@@ -266,7 +280,7 @@ differential mode is
 
 The \texttt{LALFrDetector} structure holds site geometry information
 in the same format as the \texttt{FrDetector} structure defined in the
-frames spec. \cite{LIGOVIRGO:2000}  The fields are:
+frames spec. \cite{tools:LIGOVIRGO:2000}  The fields are:
 \begin{description}
   \item[\texttt{CHAR name[LALNameLength]}] A unique identifying string.
   \item[\texttt{REAL8 vertexLongitudeDegrees}] The geodetic longitude 
@@ -334,10 +348,10 @@ typedef struct tagLALDetector
   LALFrDetector    frDetector;
 } LALDetector;
 
-void LALCreateDetector( LALStatus       *status,
-			LALDetector     *output,
-			LALFrDetector   *input,
-			LALDetectorType  type );
+void LALCreateDetector( LALStatus             *status,
+			LALDetector           *output,
+			const LALFrDetector   *input,
+			const LALDetectorType  type );
 
 extern const LALDetector lalCachedDetectors[LALNumCachedDetectors];
 
