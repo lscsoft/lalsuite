33495486791bcc121e727a8ef61623c84eaf2ca2
dwchin <dwchin>
2001-08-09 21:39:58 +0000

    No commandline arguments are now necessary, so a make check will run the test. Original: b3260bf050a57ef0887d92a2034183aa2da515ac

--- lal/packages/date/test/TestUtoGPS.c
+++ lal/packages/date/test/TestUtoGPS.c
@@ -8,7 +8,7 @@
 extern char *tzname[2];
 /* struct tm *gmtime_r( const time_t *, struct tm * ); */
 
-INT4 lalDebugLevel = 2;
+INT4 lalDebugLevel = LALALLDBG;
 
 NRCSID (TESTUTOGPSC, "$Id$");
 
@@ -21,7 +21,8 @@ NRCSID (TESTUTOGPSC, "$Id$");
 const time_t UTCGPS = 315964811;
 
 /* Taken from GRASP */
-static void printone(LALStatus *status, LIGOTimeUnix *time1)
+static void printone(LALStatus *status, LIGOTimeUnix *time1, int has_leap_p,
+                     int *retval)
 {
     LIGOTimeGPS  gpstime;
     LALDate      laldate;
@@ -37,31 +38,61 @@ static void printone(LALStatus *status, LIGOTimeUnix *time1)
      * Allocate space for CHARVectors
      */
     LALCHARCreateVector(status, &utc, (UINT4)64);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
     LALCHARCreateVector(status, &gmt, (UINT4)64);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
     LALCHARCreateVector(status, &gps, (UINT4)64);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
+
 
     /* compute GPS time */
     LALUtoGPS(status, &gpstime, time1);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
 
     /*
      * construct strings with appropriate time stamps
      */
     /* gmtime() */
     gmtime_r((time_t *)&(time1->unixSeconds), &utimestruct);
-    strftime(gmt->data, gmt->length,
-             "%Y-%m-%d %H:%M:%S UTC %a", &(utimestruct));
+    if (strftime(gmt->data, gmt->length,
+                 "%Y-%m-%d %H:%M:%S UTC %a", &(utimestruct)) == 0)
+      {
+        exit(1);
+      }
 
     /* LALUtime() */
     LALUtime(status, &laldate, time1);
-    /* strcpy(utc, asctime(&(laldate.unixDate))); */
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
+
+    if (has_leap_p > 0)
+      if (laldate.unixDate.tm_sec == 60) 
+        *retval = 0;
+      else
+        *retval = 1;
+    else
+      *retval = 0;
+
     LALDateString(status, utc, &laldate);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
 
     /* GPS */
     /* tmp.unixSeconds += UTCGPS; */ /* shift back to Unix epoch */
     LALGPStoU(status, &tmp, &gpstime);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
     LALUtime(status, &laldate, &tmp);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
     LALDateString(status, gps, &laldate);
-    /* strcpy(gps, asctime(&(laldate.unixDate))); */
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
+
 
     /* printf("C-time: %10d GPS Time %10d  UTC: %.24s GPS: %.24s gmtime: %s",
        (int)(time1->unixSeconds),(int)(gpstime.gpsSeconds),utc,gps,gmt); */
@@ -73,28 +104,36 @@ static void printone(LALStatus *status, LIGOTimeUnix *time1)
      * House cleaning
      */
     LALCHARDestroyVector(status, &utc);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
+
     LALCHARDestroyVector(status, &gmt);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
+
     LALCHARDestroyVector(status, &gps);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(status);
     
     RETURN (status);
 }
 
 static void
-testunixtime(LALStatus *status, const LIGOTimeUnix *time1)
+testunixtime(LALStatus *status, const LIGOTimeUnix *time1, int has_leap_p,
+             int *retval)
 {
     LIGOTimeUnix tmp;
 
-    INITSTATUS (status, "testunixtime", TESTUTOGPSC);
 
     tmp.unixSeconds     = time1->unixSeconds;
     tmp.unixNanoSeconds = time1->unixNanoSeconds;
-    
+
     tmp.unixSeconds--;
-    printone(status, &tmp);
+    printone(status, &tmp, has_leap_p, retval);
     tmp.unixSeconds++;
-    printone(status, &tmp);
+    printone(status, &tmp, has_leap_p, retval);
     tmp.unixSeconds++;
-    printone(status, &tmp);
+    printone(status, &tmp, has_leap_p, retval);
     printf("           |            |                             |                             |         \n");
 
     
@@ -110,8 +149,9 @@ main(int argc, char *argv[])
     LIGOTimeUnix unixtime;
     LALDate      date;
     time_t       now;
-    CHARVector  *timestamp;
+    CHARVector  *timestamp = NULL;
     time_t       tmptime;
+    int          retval = 0;
     static LALStatus status;
 
 
@@ -121,14 +161,15 @@ main(int argc, char *argv[])
          * Print help message and exit
          */
         printf("Usage: TestUtoGPS debug_level -- debug_level = [0,1,2]\n");
-        return 0;
+        lalDebugLevel = 0;
     }
 
     if (argc == 2)
         lalDebugLevel = atoi(argv[1]);
 
-
     LALCHARCreateVector(&status, &timestamp, (UINT4)128);
+    if (lalDebugLevel > 2) 
+      REPORTSTATUS(&status);
 
     printf("TEST Unix time and Unix to GPS conversion\n");
     printf("   and LALDateString\n");
@@ -144,46 +185,43 @@ main(int argc, char *argv[])
     printf("unixtime = %13d;\t\t", unixtime.unixSeconds);
 
     LALUtoGPS(&status, &gpstime, &unixtime);
+    if (lalDebugLevel > 2)
+      REPORTSTATUS(&status);
 
     printf("gpstime = %13d\n", gpstime.gpsSeconds);
     printf("\n");
 
     tmptime = time(NULL);
-    fprintf(stderr, "FOO 1\n");
     unixtime.unixSeconds = tmptime;
-    fprintf(stderr, "FOO 2\n");
     unixtime.unixNanoSeconds = 0;
-    fprintf(stderr, "FOO 3\n");
     LALUtime(&status, &date, &unixtime);
-    fprintf(stderr, "FOO 4\n");
     LALDateString(&status, timestamp, &date);
-    fprintf(stderr, "FOO 5\n");
 
     printf("Various times, some which include leap second - look for '60' in the seconds field of the time:\n\n");
 
-    printf(" Unix time | GPS Time   |          UTC                |          GPS                | gmtime_r()\n");
+    printf(" Unix time | GPS Time   |        UTC (LALUtime)       |          GPS                | gmtime_r()\n");
     printf("================================================================================================================\n");
 
     unixtime.unixSeconds = 1;
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 0, &retval);
     unixtime.unixSeconds = 33350400;
-    testunixtime(&status, &unixtime);
-    unixtime.unixSeconds = 63072000;
-    testunixtime(&status, &unixtime);
-    unixtime.unixSeconds = 78796801;
-    testunixtime(&status, &unixtime);
-    unixtime.unixSeconds = 94694402;
-    testunixtime(&status, &unixtime);
-    unixtime.unixSeconds = 126230403;
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 1, &retval);
+    unixtime.unixSeconds = 63072001;
+    testunixtime(&status, &unixtime, 1, &retval);
+    unixtime.unixSeconds = 78796802;
+    testunixtime(&status, &unixtime, 1, &retval);
+    unixtime.unixSeconds = 94694403;
+    testunixtime(&status, &unixtime, 1, &retval);
+    unixtime.unixSeconds = 126230404;
+    testunixtime(&status, &unixtime, 1, &retval);
     unixtime.unixSeconds = 315964811;
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 0, &retval);
     unixtime.unixSeconds = 784880277;
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 0, &retval);
     unixtime.unixSeconds = 911110677;
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 0, &retval);
     unixtime.unixSeconds = time(NULL);
-    testunixtime(&status, &unixtime);
+    testunixtime(&status, &unixtime, 0, &retval);
 
-    return 0;
+    return retval;
 }
