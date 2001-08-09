e3df7638b828d29c48223d712389191a1407d999
dwchin <dwchin>
2001-08-09 21:39:22 +0000

    Fixed up test -- no more core dumps. Original: 43b9910ee0a56e0f514e429d0e34ec2b7d2ce588

--- lal/packages/date/test/TestDateString.c
+++ lal/packages/date/test/TestDateString.c
@@ -7,7 +7,7 @@
 #include <lal/Date.h>
 #include <lal/AVFactories.h>
 
-INT4 lalDebugLevel = 2;
+INT4 lalDebugLevel = LALALLDBG;
 
 NRCSID (TESTDATESTRINGC, "$Id$");
 
@@ -15,47 +15,41 @@ int main(void)
 {
     static LALStatus stat;
     LIGOTimeUnix     unixtime;
+    time_t           tmpsecs;
     LALDate          date;
     CHAR             refstamp[128];
+    CHAR             tmpstamp[128];
     CHARVector      *timestamp = NULL;
     struct tm        tmdate;
 
     LALCHARCreateVector(&stat, &timestamp, (UINT4)64);
+    REPORTSTATUS (&stat);
 
-    /* unixtime.unixSeconds = 60858; */
-    /* 24 leap seconds introduced since 1970 */
     unixtime.unixSeconds = (24*365 + 8*366 + 2*31 + 28)*86400 - 1 + 24;
     unixtime.unixNanoSeconds = 0;
 
     LALUtime(&stat, &date, &unixtime);
+    REPORTSTATUS(&stat);
 
     LALDateString(&stat, timestamp, &date);
+    REPORTSTATUS(&stat);
 
     sprintf(refstamp, "2002-03-31 23:59:59 UTC Sun");
-
-    if (lalDebugLevel > 0)
-    {
-      fprintf(stderr, "asctime(gmtime(&(unixtime.unixSeconds))) = %s",
-              asctime(gmtime(&(unixtime.unixSeconds))));
-      fprintf(stderr, "                                    expect Mon Apr  1 00:00:23 2002\n");
-      unixtime.unixSeconds = 0;
-      fprintf(stderr, "asctime(gmtime(&(unixtime.unixSeconds))) = %s",
-              asctime(gmtime(&(unixtime.unixSeconds))));
-      fprintf(stderr, "                                    expect Thu Jan  1 00:00:00 1970\n");
-      fprintf(stderr, "refstamp  = %s\n", refstamp);
-      fprintf(stderr, "timestamp = %s\n", timestamp->data);
-    }
+    fprintf(stderr, "refstamp  = %s\n", refstamp);
+    fprintf(stderr, "timestamp = %s\n", timestamp->data);
 
     if (strcmp(refstamp, timestamp->data) == 0)
       {
+        LALCHARDestroyVector(&stat, &timestamp);
+        REPORTSTATUS(&stat);
+        LALCheckMemoryLeaks();
         return 0;
       }
     else
       {
+        LALCHARDestroyVector(&stat, &timestamp);
+        REPORTSTATUS(&stat);
+        LALCheckMemoryLeaks();
         return 1;
       }
-
-    LALCheckMemoryLeaks();
-    
-    return 0;
 }
