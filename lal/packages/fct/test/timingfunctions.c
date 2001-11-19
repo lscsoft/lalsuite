bc3126df19a3fb9e079c33b6a42564e4a0d56fe1
charlton <charlton>
2001-11-19 21:33:18 +0000

    Jeff's fixes Original: 6eba9dacd53c9d69c9099ae8aa16d8fbd762095d

--- lal/packages/fct/test/timingfunctions.c
+++ lal/packages/fct/test/timingfunctions.c
@@ -57,28 +57,48 @@ double UserTimeVal(void) {
 }
 
 double WallTimeVal(void) {
-   /*returns the time difference in seconds between calls. 
+  /*returns the user time difference in seconds between calls. 
     It should be used like this:
       WallTimeVal();
       FunctionToBeTimed(ARGS);
       TempTime = WallTime();
-      printf("FunctionToBeTimed took %g seconds\n");
-   */
-  static struct timeval last;
-  struct timeval now;
+      printf("FunctionToBeTimed took %g user seconds\n");
+    Please note that this function can only deal with one wrap around.
+    Wrap arounds occur about every 35 user minutes.*/
+
+  static clock_t last_walltime = 0;
+  static clock_t now_walltime = 0;
+  struct tms now;
   double val;
-  int Check;
-
-  Check = gettimeofday(&now, 0);
-
+  clock_t Check;
+  
+  
+  Check = times(&now);
+ 
   if (Check < 0) {
-    printf("There was a problem. Check = %d\n", Check);
-  } 
+    printf("There was a problem. Check = %ld\n", Check);
+  }
+  now_walltime = now.tms_utime + now.tms_stime;
+ 
+  if (now_walltime >= last_walltime) {
 
-  val = (double) (now.tv_sec - last.tv_sec) + (double)(now.tv_usec - last.tv_usec)/1000000.0;
-
-  last.tv_sec = now.tv_sec;
-  last.tv_usec = now.tv_usec;
-
+    val = (now_walltime - last_walltime) / (double)CLOCKS_PER_SEC;
+    
+  } else if (now_walltime < last_walltime) {
+    
+    printf("Wrapped around. last_walltime = %ld, now_walltime = %ld, diff = %ld\n", \
+           last_walltime, now_walltime, now_walltime + (LONG_MAX - last_walltime));
+    
+    val = (now_walltime + (LONG_MAX - last_walltime))/(double)CLOCKS_PER_SEC;
+    
+  }
+  if (val < 0){
+    printf("now_walltime = %ld, last_walltime = %ld, val = %g\n", \
+           last_walltime, now_walltime, val);
+  }
+  
+  last_walltime = now_walltime;
+  
   return(val);
 }
+
