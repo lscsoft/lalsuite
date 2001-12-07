9c7c5d7c9a7af217ab37d4f095fc8e2bb87815f6
cutler <cutler>
2001-12-07 14:00:07 +0000

    1)Changed part that reads first line of earth and sun ephem. files, since a)leap sec is no longer given in that first line (instead supplied by driver package), and now first line does include the time-spacing between consecutive entries in ephemeris tables. 2) Storage for edat->earth and edat->sun now allocated with LALMalloc    (previously was hard-wired in LALBarycenter.h). Original: 2f23ea013c542eb6b317a5861cfcdfb7612adfde

--- lal/packages/pulsar/test/LALInitBarycenter.c
+++ lal/packages/pulsar/test/LALInitBarycenter.c
@@ -40,6 +40,7 @@ LALFclose()
 </lalLaTeX> */
 
 #include "LALInitBarycenter.h"
+#include <lal/LALBarycenter.h>
 #include <lal/FileIO.h>
 		 
 NRCSID(LALINITBARYCENTERC,"$Id$");
@@ -56,7 +57,6 @@ LALInitBarycenter(LALStatus *stat, EphemerisData *edat)
                           at first instant of new year, UTC; equivalently
                           leap is # of leap secs added between Jan.6, 1980 and
                           Jan. 2 of given year */
-    INT4 nentriesE, nentriesS; /*number of rows in the earth, sun ephem */
     INT4 t2000 = 630720013;
 
     INITSTATUS(stat,"LALInitBarycenter",LALINITBARYCENTERC);
@@ -65,6 +65,11 @@ LALInitBarycenter(LALStatus *stat, EphemerisData *edat)
     fp1 = LALOpenDataFile(edat->ephiles.earthEphemeris);  
     fp2 = LALOpenDataFile(edat->ephiles.sunEphemeris);  
 
+    /*
+    fp1 = fopen(edat->ephiles.earthEphemeris,"r");  
+    fp2 = fopen(edat->ephiles.sunEphemeris,"r");  
+    */
+
     /* CHECK THAT fp1 and fp2 are not NULL: */
     if ( ( fp1 == NULL ) || ( fp2 == NULL ) ) {
       ABORT (stat, LALINITBARYCENTERH_EOPEN, LALINITBARYCENTERH_MSGEOPEN);
@@ -72,75 +77,52 @@ LALInitBarycenter(LALStatus *stat, EphemerisData *edat)
     
 /*reading first line of each file */
 
-	 fscanf(fp1,"%d %d %d\n", &gpsYr, &leap, &nentriesE);
-         edat->leap = leap; 
-	 fscanf(fp2,"%d %d %d\n", &gpsYr, &leap, &nentriesS);
+	 fscanf(fp1,"%d %le %d\n", &gpsYr, &edat->dtEtable, &edat->nentriesE);
+	 fscanf(fp2,"%d %le %d\n", &gpsYr, &edat->dtStable, &edat->nentriesS);
     
+     edat->earth  = (PosVelAcc *)LALMalloc(edat->nentriesE*sizeof(PosVelAcc)); 
+     edat->sun  = (PosVelAcc *)LALMalloc(edat->nentriesS*sizeof(PosVelAcc)); 
 
 /*first column in earth.dat or sun.dat is gps time--one long integer
   giving the number of secs that have ticked since start of GPS epoch
   +  on 1980 Jan. 6 00:00:00 UTC 
-  right now it's hard-wired in that entries in earth.dat and sun.dat
-  are exactly one hour apart
 */
-       for (j=0; j < nentriesE; ++j){
+       for (j=0; j < edat->nentriesE; ++j){
 
 	 /* check return value of fscanf */
 	 fscanf(fp1,"%le %le %le %le %le %le %le %le %le %le\n",
-		&edat->gpsE[j],&edat->position[j][0],
-		&edat->position[j][1],&edat->position[j][2],
-		&edat->velocity[j][0],&edat->velocity[j][1],
-		&edat->velocity[j][2],&edat->acceleration[j][0],
-		&edat->acceleration[j][1],
-		&edat->acceleration[j][2] );
+		&edat->earth[j].gps,&edat->earth[j].pos[0],
+		&edat->earth[j].pos[1],&edat->earth[j].pos[2],
+		&edat->earth[j].vel[0],&edat->earth[j].vel[1],
+		&edat->earth[j].vel[2],&edat->earth[j].acc[0],
+		&edat->earth[j].acc[1],
+		&edat->earth[j].acc[2] );
        }
 
-       for (j=0; j < nentriesS; ++j){
+       for (j=0; j < edat->nentriesS; ++j){
 	 fscanf(fp2,"%le %le %le %le %le %le %le %le %le %le\n",
-		&edat->gpsS[j],&edat->sunPos[j][0],
-		&edat->sunPos[j][1],&edat->sunPos[j][2],
-		&edat->sunVel[j][0],&edat->sunVel[j][1],
-		&edat->sunVel[j][2],
-		&edat->sunAccel[j][0],&edat->sunAccel[j][1],
-		&edat->sunAccel[j][2] );
+		&edat->sun[j].gps,&edat->sun[j].pos[0],
+		&edat->sun[j].pos[1],&edat->sun[j].pos[2],
+		&edat->sun[j].vel[0],&edat->sun[j].vel[1],
+		&edat->sun[j].vel[2],&edat->sun[j].acc[0],
+		&edat->sun[j].acc[1],
+		&edat->sun[j].acc[2] );
        }
        
-       /* below just for testing
-	  printf("gpsE[nentriesE -1], gpsS[nentriesS -1] = %25.17e 
-          %25.17e \n", edat->gpsE[nentriesE -1], 
-          edat->gpsS[nentriesS -1]);
-	  
-	  printf("gpsE[nentriesE -1], gpsS[nentriesS -1] = %25.17e 
-          %25.17e \n", (*edat).gpsE[nentriesE -1], 
-          (*edat).gpsS[nentriesS -1]);
-	  
-	  for (j= 0; j<nentriesS;++j){
-	  printf("j, gpsS[j] =%d %25.17e \n", j, edat->gpsS[j]); }
-       */
        
-       /*curt: as test, make sure last entries for gpsE,s are 
-  reasonable numbers;
-  specifically, checking that they are of same order as t2000 
-  --within factor e
+/*Test to make sure last entries for gpsE,S are 
+  reasonable numbers; specifically, checking that they are 
+  of same order as t2000 
+  --within factor e  
+Machine dependent: to be fixed!
 
-
-        printf("edat->gpsE[nentriesE -1]/t2000,
-         1.e0*(edat->gpsE[nentriesE -1])/t2000,
-           log(1.e0*(edat->gpsE[nentriesE -1])/t2000),
-           fabs( log(1.e0*(edat->gpsE[nentriesE -1])/t2000)) = %25.17e 
-          %25.17e %25.17e %25.17e \n", 
-         edat->gpsE[nentriesE -1]/t2000,
-         1.e0*(edat->gpsE[nentriesE -1])/t2000,
-           log(1.e0*(edat->gpsE[nentriesE -1])/t2000),
-           fabs( log(1.e0*(edat->gpsE[nentriesE -1])/t2000))
-          );
+    if ( (fabs(log(1.e0*(edat->earth[edat->nentriesE -1].gps)/t2000)) > 1.e0) 
+        ||(fabs(log(1.e0*(edat->sun[edat->nentriesS -1].gps)/t2000)) > 1.e0) ){
+      LALFree(edat->earth);
+      LALFree(edat->sun);
+      ABORT(stat,LALINITBARYCENTERH_EEPHFILE, LALINITBARYCENTERH_MSGEEPHFILE); 
+    }
 */
-        ASSERT (fabs(log(1.e0*(edat->gpsE[nentriesE -1])/t2000)) < 1.e0, 
-         stat, LALINITBARYCENTERH_EEPHFILE, LALINITBARYCENTERH_MSGEEPHFILE);
-
-        ASSERT (fabs(log(1.e0*(edat->gpsS[nentriesS -1])/t2000)) < 1.e0, 
-         stat, LALINITBARYCENTERH_EEPHFILE, LALINITBARYCENTERH_MSGEEPHFILE);
-	
 	fclose(fp1); 
         fclose(fp2);       
 	
@@ -148,3 +130,7 @@ LALInitBarycenter(LALStatus *stat, EphemerisData *edat)
 	DETATCHSTATUSPTR(stat);
 	RETURN(stat);
 }
+
+
+
+
