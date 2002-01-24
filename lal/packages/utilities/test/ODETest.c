6773cbb7d7eee9432ac78b99be0b320eb0877adf
jolien <jolien>
2002-01-24 22:41:44 +0000

    Made inline bisection safer. Original: 39cd157164e613e082e3d5862999f559b2369333

--- lal/packages/utilities/test/ODETest.c
+++ lal/packages/utilities/test/ODETest.c
@@ -179,7 +179,7 @@ static int EccentricOrbit( void )
 
     psi = M = 2 * LAL_PI * indep.t / P;
     del = psi - e * sin( psi ) - M;
-    while ( fabs( del ) > LAL_REAL4_EPS )
+    while ( fabs( del ) > 10 * LAL_REAL4_EPS )
     {
       psi += ( del < 0 ? 1 : -1 ) * ( fac *= 0.5 ) * e;
       del  = psi - e * sin( psi ) - M;
