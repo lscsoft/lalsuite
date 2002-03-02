455e6f172cb83b0b41bc2699d4393fdcb0ca5e9c
duncan <duncan>
2002-03-02 00:43:44 +0000

    initial entry Original: 1fcb814460d139817643a91711497b96ab4dfddd

---
+++ lal/packages/findchirp/src/FindChirpSimulation.c
@@ -0,0 +1,314 @@
+/*----------------------------------------------------------------------- 
+ * 
+ * File Name: FindChirpSimulation.c
+ *
+ * Author: Brown, D. A., and Creighton, T. D.
+ * 
+ * Revision: $Id$
+ * 
+ *-----------------------------------------------------------------------
+ */
+
+#include <lal/Units.h>
+#include <lal/AVFactories.h>
+#include <lal/SeqFactories.h>
+#include <lal/LALNoiseModels.h>
+#include <lal/FindChirpEngine.h>
+
+NRCSID( FINDCHIRPSIMULATIONC, "$Id$" );
+
+
+#if 0
+/*-----------------------------------------------------------------------*/
+void
+LALFindChirpCreateLoudestEvent (
+    LALStatus                          *status,
+    LoudestInspiralEvent              **loudest,
+    UINT4                               length
+    )
+{
+  INITSTATUS( status, "LALFindChirpCreateLoudestEvent", FINDCHIRPSIMULATIONC );
+  ATTATCHSTATUSPTR( status );
+
+  ASSERT( loudest, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+  ASSERT( !*loudest, status, 
+      FINDCHIRPENGINEH_ENNUL, FINDCHIRPENGINEH_MSGENNUL );
+
+  /* allocate memory for the structure */
+  *loudest = (LoudestInspiralEvent *)
+    LALCalloc( 1, sizeof(LoudestInspiralEvent) );
+  if ( ! *loudest )
+  {
+    ABORT( status, FINDCHIRPENGINEH_EALOC, FINDCHIRPENGINEH_MSGEALOC );
+  }
+
+  *loudest->length = length;
+
+  /* create storeage for loudest event and (s|s) for each segment */
+  *loudest->event =  (InspiralEvent *)
+    LALCalloc( numSegments, sizeof(InspiralEvent) );
+  *loudest->signalNorm = (REAL4 *)
+    LALCalloc( dataSegVec->length, sizeof(REAL4) );
+  if ( ! params->simParams->loudestEvent || 
+      ! params->simParams->signalNorm )
+  {
+    ABORT( status, FINDCHIRPENGINEH_EALOC, FINDCHIRPENGINEH_MSGEALOC );
+  }
+
+
+
+  DETATCHSTATUSPTR( status );
+  RETURN( status );
+}
+
+/*-----------------------------------------------------------------------*/
+void
+LALFindChirpDestroyLoudestEvent (
+    LALStatus                          *status,
+    InspiralEvent                      *loudestEvent,
+    UINT4                               numSegments
+    )
+{
+  
+
+  INITSTATUS( status, "LALFindChirpCreateLoudestEvent", FINDCHIRPSIMULATIONC );
+  ATTATCHSTATUSPTR( status );
+
+
+
+  DETATCHSTATUSPTR( status );
+  RETURN( status );
+}
+#endif
+
+
+/*-----------------------------------------------------------------------*/
+void
+LALFindChirpInjectSignals (
+    LALStatus                  *status,
+    REAL4TimeSeries            *chan,
+    COMPLEX8FrequencySeries    *resp,
+    InspiralInjection          *events
+    )
+{
+  UINT4                 k;
+  DetectorResponse      detector;
+  InspiralInjection    *thisEvent = NULL;
+  PPNParamStruc         ppnParams;
+  CoherentGW            waveform;
+  INT8                  waveformStartTime;
+  INT8                  chanStartTime;
+  REAL4TimeSeries       signal;
+  COMPLEX8Vector       *unity = NULL;
+
+
+  INITSTATUS( status, "LALFindChirpInjectSignals", FINDCHIRPSIMULATIONC );
+  ATTATCHSTATUSPTR( status );
+
+  ASSERT( chan, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+  ASSERT( chan->data, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+  ASSERT( chan->data->data, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+
+  ASSERT( events, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+
+  ASSERT( resp, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+  ASSERT( resp->data, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+  ASSERT( resp->data->data, status, 
+      FINDCHIRPENGINEH_ENULL, FINDCHIRPENGINEH_MSGENULL );
+
+
+  /*
+   *
+   * set up structures and parameters needed
+   *
+   */
+
+
+  LALGPStoINT8( status->statusPtr, &chanStartTime, &(chan->epoch) );
+  CHECKSTATUSPTR( status );
+ 
+  ppnParams.deltaT   = chan->deltaT;
+  ppnParams.fStartIn = 40.0;
+  ppnParams.fStopIn  = -1;
+  ppnParams.lengthIn = 0;
+  ppnParams.ppn      = NULL;
+
+
+  /*
+   *
+   * compute the transfer function from the given response function
+   *
+   */
+
+
+  /* allocate memory and copy the parameters describing the freq series */
+  detector.transfer = (COMPLEX8FrequencySeries *)
+    LALCalloc( 1, sizeof(COMPLEX8FrequencySeries) );
+  if ( ! detector.transfer ) 
+  {
+    ABORT( status, FINDCHIRPENGINEH_EALOC, FINDCHIRPENGINEH_MSGEALOC );
+  }
+  memcpy( &(detector.transfer->epoch), &(resp->epoch),
+      sizeof(LIGOTimeGPS) );
+  detector.transfer->f0 = resp->f0;
+  detector.transfer->deltaF = resp->deltaF;
+
+  /* set the detector site */
+  /* XXX not sure about this XXX */
+  detector.site = NULL;
+
+  /* set up units for the transfer function */
+  {
+    RAT4 negOne = { -1, 0 };
+    LALUnit unit;
+    LALUnitPair pair;
+    pair.unitOne = &lalADCCountUnit;
+    pair.unitTwo = &lalStrainUnit;
+    LALUnitRaise( status->statusPtr, &unit, pair.unitTwo, &negOne );
+    CHECKSTATUSPTR( status );
+    pair.unitTwo = &unit;
+    LALUnitMultiply( status->statusPtr, &(detector.transfer->sampleUnits),
+			  &pair );
+    CHECKSTATUSPTR( status );
+  }
+
+  /* invert the response function to get the transfer function */
+  LALCCreateVector( status->statusPtr, &( detector.transfer->data ),
+      resp->data->length );
+  CHECKSTATUSPTR( status );
+
+  LALCCreateVector( status->statusPtr, &unity, resp->data->length );
+  CHECKSTATUSPTR( status );
+  for ( k = 0; k < resp->data->length; ++k ) 
+  {
+    unity->data[k].re = 1.0;
+    unity->data[k].im = 0.0;
+  }
+
+  LALCCVectorDivide( status->statusPtr, detector.transfer->data, unity,
+      resp->data );
+  CHECKSTATUSPTR( status );
+
+  LALCDestroyVector( status->statusPtr, &unity );
+  CHECKSTATUSPTR( status );
+
+
+  /*
+   *
+   * loop over the signals and inject them into the time series
+   *
+   */
+
+
+  for ( thisEvent = events; thisEvent; thisEvent = thisEvent->next )
+  {
+
+
+   /* 
+    *
+    * populate ppn parameter structure from injection event 
+    *
+    */
+
+
+    /* input fields */
+    ppnParams.mTot = thisEvent->totalMass;
+    ppnParams.eta  = thisEvent->eta;
+    ppnParams.d    = thisEvent->dist * LAL_PC_SI * 1000.0;
+    ppnParams.inc  = thisEvent->inclination * LAL_PI / 180.0;
+    ppnParams.phi  = thisEvent->coaPhase * LAL_PI / 180.0;
+
+    /* passed fields */
+    ppnParams.position.longitude   = thisEvent->longitude;
+    ppnParams.position.latitude    = thisEvent->latitude;
+    ppnParams.position.system      = COORDINATESYSTEM_EQUATORIAL;
+    ppnParams.psi                  = thisEvent->polarization;
+    ppnParams.epoch.gpsSeconds     = 0;
+    ppnParams.epoch.gpsNanoSeconds = 0;
+
+
+    /* 
+     *
+     * generate waveform and inject it into the data
+     *
+     */
+
+
+    /* clear the waveform structure */
+    memset( &waveform, 0, sizeof(CoherentGW) );
+
+    LALGeneratePPNInspiral( status->statusPtr, &waveform, &ppnParams );
+    CHECKSTATUSPTR( status );
+
+    if ( ppnParams.dfdt > 2.0 ) 
+    {
+      fprintf( stderr, "Waveform sampling interval is too large:\n"
+          "\tmaximum df*dt = %f", ppnParams.dfdt );
+      fflush( stderr );
+    }
+
+    /* get the gps start time of the signal to inject */
+    waveformStartTime = thisEvent->coaTime - 
+      (INT8) ( 1000000000.0 * ppnParams.tc );
+
+    /* if the waveform lies within the time series, inject it */
+    if ( waveformStartTime >= chanStartTime )
+    {
+      /* set the start times for injection */
+      LALINT8toGPS( status->statusPtr, 
+          &(signal.epoch), waveformStartTime );
+      CHECKSTATUSPTR( status );
+      memcpy( &(waveform.a->epoch), &(signal.epoch), 
+          sizeof(LIGOTimeGPS) );
+      memcpy( &(waveform.f->epoch), &(signal.epoch), 
+          sizeof(LIGOTimeGPS) );
+      memcpy( &(waveform.phi->epoch), &(signal.epoch), 
+          sizeof(LIGOTimeGPS) );
+
+      /* simulate the detectors response to the inspiral */
+      LALSCreateVector( status->statusPtr, &(signal.data), 
+          (UINT4) ppnParams.length );
+      CHECKSTATUSPTR( status );
+      
+      LALSimulateCoherentGW( status->statusPtr, 
+          &signal, &waveform, &detector );
+      CHECKSTATUSPTR( status );
+
+      /* inject the signal into the data channel */
+      LALSSInjectTimeSeries( status->statusPtr, chan, &signal );
+      CHECKSTATUSPTR( status );
+
+      /* destroy the signal */
+      LALSDestroyVector( status->statusPtr, &(signal.data) );
+      CHECKSTATUSPTR( status );
+    }
+
+    LALSDestroyVectorSequence( status->statusPtr, &(waveform.a->data) );
+    CHECKSTATUSPTR( status );
+    
+    LALSDestroyVector( status->statusPtr, &(waveform.f->data) );
+    CHECKSTATUSPTR( status );
+    
+    LALDDestroyVector( status->statusPtr, &(waveform.phi->data) );
+    CHECKSTATUSPTR( status );
+    
+    LALFree( waveform.a );
+    LALFree( waveform.f );
+    LALFree( waveform.phi );
+  }
+
+  LALCDestroyVector( status->statusPtr, &( detector.transfer->data ) );
+  CHECKSTATUSPTR( status );
+
+  LALFree( detector.transfer );
+
+  DETATCHSTATUSPTR( status );
+  RETURN( status );
+}
