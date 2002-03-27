92b8d5d32b6eedabbc7573f15d57b49c6f1bba21
duncan <duncan>
2002-03-27 20:28:57 +0000

    modified to pass REAL8FrequencySeries to integrand Original: f83e545db4a83af1cea1fb14afa7c4044cb3931b

--- lal/packages/bank/src/LALInspiralMoments.c
+++ lal/packages/bank/src/LALInspiralMoments.c
@@ -1,9 +1,10 @@
 /*  <lalVerbatim file="LALInspiralMomentsCV">
-Author: Sathyaprakash, B. S.
+Authors: Brown, D. A., and Sathyaprakash, B. S.
 $Id$
 </lalVerbatim>  */
 
-/*  <lalLaTeX>
+#if 0
+<lalLaTeX>
 
 \subsection{Module \texttt{LALInspiralMoments.c}}
 
@@ -18,24 +19,32 @@ Module to calculate the moment of the noise power spectral density.
 
 The moments of the noise curve are defined as
 \begin{equation}
-I(q)  \equiv S_{h}(f_{0}) \int^{f_{c}/f_{0}}_{f_{s}/f_{0}} \frac{x^{-q/3}}{s_{h}(x f_{0})} \, dx \,.
+I(q)  \equiv S_{h}(f_{0}) \int^{f_{c}/f_{0}}_{f_{s}/f_{0}}
+\frac{x^{-q/3}}{s_{h}(x f_{0})} \, dx \,.  
 \end{equation}
-Because in practice we will always divide one of these moments by another, we do not need to include
-the $S_{h}(f_{0})$ term, which always cancels.
+Because in practice we will always divide one of these moments by another, we
+do not need to include the $S_{h}(f_{0})$ term, which always cancels.
 This function calculates the integral
 \begin{equation}
 I = \int^{f_{c}/f_{0}}_{f_{s}/f_{0}} \frac{x^{-q/3}}{s_{h}(x f_{0})} \, dx \,.
-\end{equation}
-It then divides this quantity by a normalisation constant which has been passed to the function. In
-the case of calculating the components of the metric for the signal manifold for the purpose of
-generating a template bank, this constant is given by $I(7)$, because of the definition of the quantity
+\end{equation} 
+It then divides this quantity by a normalisation constant which has been
+passed to the function. In the case of calculating the components of the
+metric for the signal manifold for the purpose of generating a template bank,
+this constant is given by $I(7)$, because of the definition of the quantity
 \begin{equation}
 J(q) \equiv \frac{I(q)}{I(7)} \,.
 \end{equation}
 
-
 \subsubsection*{Algorithm}
-
+Given the exponent \texttt{pars.ndx} and limits of integration
+\texttt{pars.xmin} and \texttt{pars.xmax} this function returns the moment of
+the power spectral density specified by by the frequency series
+\texttt{pars.shf} according to
+\begin{equation}
+\mathtt{moment} = \int_{\mathtt{xmin}}^{\mathtt{xmax}} 
+\frac{x^{-\mathtt{ndx}}}{S_h(x)}\, dx \, .
+\end{equation}
 
 \subsubsection*{Uses}
 \begin{verbatim}
@@ -47,21 +56,8 @@ LALDRombergIntegrate
 
 \vfill{\footnotesize\input{LALInspiralMomentsCV}}
 
-</lalLaTeX>  */
-
-
-
-
-/* ---------------------------------------------------------------------
-  Author: B.S.Sathyaprakash, U.W.C., Cardiff.
-  Revision History: Last Update 15.7.97.
-  Purpose: Given the exponent {\sf ndx} and limits of integration 
-     {\sf xs} and {\sf xc} this function returns the moment of the 
-     power spectral density specified by {\sf choice}.
-  Inputs: 
-  Outputs:
-     ${\sf moments} = \int_{xs}^{xc} x^{\sf -ndx}~dx/S_h(x).$
-*----------------------------------------------------------------------- */
+</lalLaTeX>
+#endif
 
 #include <lal/LALInspiralBank.h>
 #include <lal/Integrate.h>
@@ -75,18 +71,26 @@ void LALInspiralMoments(LALStatus         *status,
                         InspiralMomentsIn pars)
 { /* </lalVerbatim> */
 
+   REAL8 fMax;
    DIntegrateIn In;
-   void *funcparams;
-   InspiralMomentsIntegrandIn intIn;
+   void *funcparams = (void *) &pars;
 
    INITSTATUS (status, "LALInspiralMoments", LALINSPIRALMOMENTSC);
    ATTATCHSTATUSPTR(status);
-   intIn.ndx = pars.ndx;
 
-   ASSERT (pars.NoisePsd, status, LALINSPIRALBANKH_ENULL, LALINSPIRALBANKH_MSGENULL);
+   ASSERT (pars.shf, status, LALINSPIRALBANKH_ENULL, LALINSPIRALBANKH_MSGENULL);
+   ASSERT (pars.shf->data, status, LALINSPIRALBANKH_ENULL, LALINSPIRALBANKH_MSGENULL);
+   ASSERT (pars.shf->data->data, status, LALINSPIRALBANKH_ENULL, LALINSPIRALBANKH_MSGENULL);
 
-   intIn.NoisePsd = pars.NoisePsd;
-   funcparams = (void *) &(intIn);
+   /* make sure that the minimum and maximum of the integral are within */
+   /* the frequency series                                              */
+   fMax = pars.shf->f0 + (REAL8) pars.shf->data->length * 
+     pars.shf->deltaF;
+   if ( pars.xmin < pars.shf->f0 || pars.xmax > fMax )
+   {
+     ABORT( status, 999, "limits outside range of frequency series" );
+   }
+
    In.function = LALInspiralMomentsIntegrand;
    In.xmin = pars.xmin;
    In.xmax = pars.xmax;
