1a8b317be893283f4f566026be5955789c621e7f
owen <owen>
2002-05-04 18:14:54 +0000

    Set number of spindown parameters to zero, changed xmgrace to use pipe, added missing projection of metric (last result), tweaked documentation, and added basic error checking. Original: 2cf5eea0dcbd8851535ddfb498efea4121120cf8

--- lal/packages/pulsar/test/PtoleMetricTest.c
+++ lal/packages/pulsar/test/PtoleMetricTest.c
@@ -22,21 +22,29 @@ ordering of the components is $(f_0, \alpha, \delta, f_1, \ldots)$ for the
 unprojected metric, and $(\alpha, \delta, f_1, \ldots)$ for the metric with
 $f_0$ projected out.
 
-With no options, this program displays components for a single point in
-parameter space for two different integration times.
+With no options, this program displays metric components for a single point
+in parameter space for two different integration times.
 
 The \texttt{-t} option causes the program to showcase error messages when
 given bad parameter values, etc.
 
-The \texttt{-x} option produces a graph of the 2% power mismatch contours on
-the sky. Dimensions of the ellipses have been exaggerated by 10 for
+The \texttt{-x} option produces a graph of the 2\% power mismatch contours
+on the sky. Dimensions of the ellipses have been exaggerated by 10 for
 legibility. The purpose of the graph is to get a feel for how ellipses are
 varying across parameter space. Note that this option makes a system call to
-the xmgrace program, and will not work if that program is not installed on
-your system.
+the \texttt{xmgrace} program, and will not work if that program is not
+installed on your system.
 
 \subsubsection*{Exit Codes}
-
+************************************************ </lalLaTeX><lalErrTable> */
+#define PTOLEMETRICTESTC_EMEM 1
+#define PTOLEMETRICTESTC_ESUB 2
+#define PTOLEMETRICTESTC_ESYS 3
+ 
+#define PTOLEMETRICTESTC_MSGEMEM "memory (de)allocation error"
+#define PTOLEMETRICTESTC_MSGESUB "subroutine failed"
+#define PTOLEMETRICTESTC_MSGESYS "system call failed"
+/************************************************** </lalErrTable><lalLaTeX>
 \subsubsection*{Algorithm}
 
 \subsubsection*{Uses}
@@ -50,6 +58,7 @@ LALDCreateVector()
 LALDDestroyVector()
 LALProjectMetric()
 LALPtoleMetric()
+xmgrace
 \end{verbatim}
 
 \subsubsection*{Notes}
@@ -77,7 +86,7 @@ complication to the tiling.
 
 NRCSID( PTOLEMETRICTESTC, "$Id" );
 
-#define NUM_SPINDOWN 1 /* Number of spindown parameters */
+#define NUM_SPINDOWN 0 /* Number of spindown parameters */
 #define SPOKES 30
 
 int lalDebugLevel = 0;
@@ -123,11 +132,22 @@ int main( int argc, char *argv[] ) {
     printf("\nTesting bad spindown parameters...\n");
     LALPtoleMetric( &status, metric, &in );
   }
-  /* Good spindown parameters: all zero */
-  LALCreateVector( &status, &spindown, NUM_SPINDOWN );
-  for (j=0; (UINT4)j<spindown->length; j++)
-    spindown->data[j] = 0;
-  in.spindown = spindown;
+  /* Good spindown parameters: all zero (if we have any) */
+  if( NUM_SPINDOWN > 0 )
+  {
+    LALCreateVector( &status, &spindown, NUM_SPINDOWN );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGEMEM );
+      return PTOLEMETRICTESTC_EMEM;
+    }
+    for (j=0; (UINT4)j<spindown->length; j++)
+      spindown->data[j] = 0;
+    in.spindown = spindown;
+  }
+  else
+    in.spindown = NULL;
 
   /* Good epoch: simple as possible, need to find those constants */
   in.epoch.gpsSeconds = 0;
@@ -163,6 +183,11 @@ int main( int argc, char *argv[] ) {
   }
   /* Allocate storage for output metric. */
   LALDCreateVector( &status, &metric, (4+NUM_SPINDOWN)*(5+NUM_SPINDOWN)/2 );
+  if( status.statusCode )
+  {
+    printf( "%s line %d: %s\n", __FILE__, __LINE__, PTOLEMETRICTESTC_MSGEMEM );
+    return PTOLEMETRICTESTC_EMEM;
+  }
 
   /* Print results if no options. */
   if (argc == 1) {
@@ -170,12 +195,24 @@ int main( int argc, char *argv[] ) {
     printf( "\nValid results for duration 1e5 seconds:\n" );
     in.duration = 1e5;
     LALPtoleMetric( &status, metric, &in );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGESUB );
+      return PTOLEMETRICTESTC_ESUB;
+    }
     for (j=0; j<=2+NUM_SPINDOWN; j++) {
       for (k=0; k<=j; k++)
         printf( "  %+.3e", metric->data[k+j*(j+1)/2] );
       printf("\n");
     }
     LALProjectMetric( &status, metric, 0 );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGESUB );
+      return PTOLEMETRICTESTC_ESUB;
+    }
     printf( "With f0 projected out:\n" );
     for (j=1; j<=2+NUM_SPINDOWN; j++) {
       for (k=1; k<=j; k++)
@@ -186,10 +223,23 @@ int main( int argc, char *argv[] ) {
     printf( "\nValid results for duration 1e7 seconds:\n" );
     in.duration = 1e7;
     LALPtoleMetric( &status, metric, &in );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGESUB );
+      return PTOLEMETRICTESTC_ESUB;
+    }
     for (j=0; j<=2+NUM_SPINDOWN; j++) {
       for (k=0; k<=j; k++)
         printf( "  %+.3e", metric->data[k+j*(j+1)/2] );
       printf("\n");
+    }
+    LALProjectMetric( &status, metric, 0 );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGESUB );
+      return PTOLEMETRICTESTC_ESUB;
     }
     printf( "With f0 projected out:\n" );
     for (j=1; j<=2+NUM_SPINDOWN; j++) {
@@ -204,7 +254,13 @@ int main( int argc, char *argv[] ) {
 
     /* Take care of preliminaries. */
     in.duration = 1e5;
-    pvc = fopen( "pvc", "w" );
+    pvc = popen( "xmgrace -pipe", "w" );
+    if( !pvc )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGESYS );
+      return PTOLEMETRICTESTC_ESYS;
+    }
     fprintf( pvc, "@xaxis label \"Right ascension (degrees)\"\n" );
     fprintf( pvc, "@yaxis label \"Declination (degrees)\"\n" );
 
@@ -218,6 +274,12 @@ int main( int argc, char *argv[] ) {
         in.position.longitude = ra*LAL_PI_180;
         in.position.latitude = dec*LAL_PI_180;
         LALPtoleMetric( &status, metric, &in );
+        if( status.statusCode )
+        {
+          printf( "%s line %d: %s\n", __FILE__, __LINE__,
+                  PTOLEMETRICTESTC_MSGESUB );
+          return PTOLEMETRICTESTC_ESUB;
+        }
         /* Rename \gamma_{\alpha\alpha}. */
         gaa = metric->data[2];
         /* Rename \gamma_{\alpha\delta}. */
@@ -252,16 +314,26 @@ int main( int argc, char *argv[] ) {
       } /* for (ra...) */
     } /* for (dec...) */
     fclose( pvc );
-    /* Yes, system() calls are dirty, but we're only doing two. */
-    system( "xmgrace pvc &" );
-    system( "sleep 2; rm -f pvc" );
   } /* if (grace) */
 
   printf("\nCleaning up and leaving...\n");
-  LALDestroyVector( &status, &spindown );
+  if( spindown )
+  {
+    LALDestroyVector( &status, &spindown );
+    if( status.statusCode )
+    {
+      printf( "%s line %d: %s\n", __FILE__, __LINE__,
+              PTOLEMETRICTESTC_MSGEMEM );
+      return PTOLEMETRICTESTC_EMEM;
+    }
+  }
   LALDDestroyVector( &status, &metric );
+  if( status.statusCode )
+  {
+    printf( "%s line %d: %s\n", __FILE__, __LINE__,
+            PTOLEMETRICTESTC_MSGEMEM );
+    return PTOLEMETRICTESTC_EMEM;
+  }
   LALCheckMemoryLeaks();
-
-  /* Get while the getting's good. */
   return 0;
 } /* main() */
