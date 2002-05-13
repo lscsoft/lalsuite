e59fc8c9389f3b1f59287a5e2353948c7fe6c86a
sberukoff <sberukoff>
2002-05-13 16:02:28 +0000

    Added amplitude demodulation routine. Original: 96eb6f40ba6ae4c305f11ee19fe3dfcdeb7bd51e

---
+++ lal/packages/pulsar/include/LALComputeAM.h
@@ -0,0 +1,131 @@
+/*** <lalVerbatim file="LALComputeAMHV">
+Author: Berukoff, S.J.
+$Id$
+*** </lalVerbatim> */
+
+/* <lalLaTeX>
+\section{Header \texttt{LALComputeAM.h}}
+\label{s:LALComputeAM.h}
+Computes filter components for amplitude demodulation.
+
+\subsection*{Synposis}
+\begin{verbatim}
+#include <lal/LALComputeAM.h>
+\end{verbatim}
+
+\noindent  In order to compute the optimal statistic for pulsar searches, one must take account of the various modulations that change the emitted, (fairly) simple sinusoid into a non-trivial function of parameters.  The frequency evolution of the signal (spindown effects, Doppler modulation, etc.) have already been accounted for; this routine filters the amplitude modulation effects.
+
+</lalLaTeX> */
+
+
+#ifndef _LALCOMPUTEAM_H
+#define _LALCOMPUTEAM_H
+
+#include <math.h>
+#include <lal/DetResponse.h>
+#include <lal/DetectorSite.h>
+#include "LALBarycenter.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+  NRCSID (LALCOMPUTEAMH, "$Id: LALComputeAM.h");
+  
+  /* Author-defined error codes */
+  /* <lalLaTeX>
+     \subsection*{Error conditions}
+     \vspace{0.1in}
+     \input{LALComputeAMHErrorTable}
+     </lalLaTeX> */
+  
+  /* <lalErrTable file="LALComputeAMHErrorTable"> */
+#define LALCOMPUTEAMH_ENOTS        1
+#define LALCOMPUTEAMH_EBCERR       2
+#define LALCOMPUTEAMH_EESERR       3
+#define LALCOMPUTEAMH_EEPH         4
+#define LALCOMPUTEAMH_EDAS         5
+#define LALCOMPUTEAMH_EFRD         6
+
+#define LALCOMPUTEAMH_MSGENOTS    "Input LIGOTimeGPS Vector is wrong size or NULL"
+#define LALCOMPUTEAMH_MSGEBCERR   "Baryinput pointer is invalid"
+#define LALCOMPUTEAMH_MSGEESERR   "EarthState structure invalid, or pointer NULL"
+#define LALCOMPUTEAMH_MSGEEPH     "Ephemeris Table invalid, or pointer NULL"
+#define LALCOMPUTEAMH_MSGEDAS     "Detector and source information invalid, or pointer NULL"
+#define LALCOMPUTEAMH_MSGEFRD     "Detector geometry information invalid, or pointer NULL"
+  /* </lalErrTable> */
+
+  /* <lalLaTeX>
+\subsection*{Structures}
+
+\begin{verbatim}
+struct AMCoeffs
+\end{verbatim}
+\index{\texttt{AMCoeffs}}
+
+\noindent This structure contains the output of the routine: $a(t)$, $b(t)$, and the scalar products therein.  That is:
+
+\begin{description}
+\item[\texttt{REAL4Vector *a}]  The function $a(t)$
+\item[\texttt{REAL4Vector *b}]  The function $b(t)$
+\item[\texttt{REAL4 A}]  The scalar product $(a||a)$
+\item[\texttt{REAL4 B}]  The scalar product $(b||b)$
+\item[\texttt{REAL4 C}]  The scalar product $(a||b)$
+\item[\texttt{REAL4 D}]  The quantity $AB-C^{2}$
+\end{description}
+
+\begin{verbatim}
+struct AMCoeffsParams
+\end{verbatim}
+\index{\texttt{AMCoeffsParams}}
+
+\noindent This structure contains the parameters for the routine.  They include:
+
+\begin{description}
+\item[\texttt{BarycenterInput *baryinput}]  Parameters from $LALBarycenter()$
+\item[\texttt{EarthState *earth}]  The state of the earth at time t
+\item[\texttt{EphemerisDate *edat}]  Pointer to the ephemerides
+\item[\texttt{LALDetAndSource *das}]  Detector and source information
+\item[\texttt{LALFrDetector}]  Detector geometry information
+\item[\texttt{REAL4 polAngle}]  Polarization angle
+\item[\texttt{REAL4 tObs}]  Observation time
+\end{description}
+</lalLaTeX> */
+
+typedef struct AMCoeffsTag
+{
+  REAL4Vector     *a;          /* the function a(t)         */
+  REAL4Vector     *b;          /* the function b(t)         */
+  REAL4           A;           /* the scalar product (a||a) */
+  REAL4           B;           /* the scalar product (b||b) */
+  REAL4           C;           /* the scalar product (a||b) */
+  REAL4           D;           /* the quantity AB-C^2       */
+} AMCoeffs;
+
+typedef struct AMCoeffsParamsTag
+{
+  BarycenterInput      *baryinput;  /* data from Barycentring routine */
+  EarthState           *earth;      /* from LALBarycenter()           */
+  EphemerisData        *edat;       /* the ephemerides                */
+  LALDetAndSource      *das;        /* det and source information     */
+  LALFrDetector        *det;        /* detector geometry              */
+  REAL4                polAngle;   /* polarization angle             */
+  REAL4                tObs;       /* observation time               */
+} AMCoeffsParams;
+
+
+  /* <lalLaTeX>
+\newpage\input{LALComputeAMHV}
+\newpage\input{LALComputeAMC}
+</lalLaTeX> */
+
+void LALComputeAM (LALStatus          *stat,
+		   AMCoeffs           *coe,
+		   LIGOTimeGPS        *ts,
+		   AMCoeffsParams     *params);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LALCOMPUTEAM_H */
---
+++ lal/packages/pulsar/src/LALComputeAM.c
@@ -0,0 +1,139 @@
+/********************************* <lalVerbatim file="LALComputeAMCV">
+Author:  Berukoff, S.J.  $Id$
+********************************** </lalVerbatim> */
+
+/* <lalLaTeX>
+\subsection{Module \texttt{LALComputeAM.c}}\label{ss:LALComputeAM.c}
+Computes quantities for amplitude demodulation.
+
+\subsection*{Prototypes}
+\vspace{0.1in}
+\input{LALComputeAMD}
+\index{\texttt{LALComputeAM()}}
+
+\subsubsection*{Description}
+This routine computes the quantities $a(t)$ and $b(t)$ as defined in Jaranowski, Krolak, and Schutz (gr-qc/9804014), hereafter JKS.  These functions quantify the dependence of the detector output on the beam-pattern functions $F_{+}$ and $F_{\times}$; in fact, $a(t)$ and $b(t)$ {\it{are}} the beam-pattern functions, without the dependence on polarization angle and detector arm angle.  Since the \verb@LALDemod()@ suite is an attempt to compute an optimal statistic, it is necessary to include these quantities in the computation.  Otherwise, the motion of the Earth as it revolves about its axis will smear the signal into several neighboring bins centered about the search frequency, consequently losing valuable SNR.
+
+\subsubsection*{Algorithm}
+The routine is really simple.  From JKS, 
+\begin{eqnarray}
+F_{+} = \sin \zeta [ a(t) \cos 2 \psi + b(t) \sin 2 \psi ] \\
+F_{\times} = \sin \zeta [ b(t) \cos 2 \psi - a(t) \sin 2 \psi ]
+\end{eqnarray}
+We use the routine \verb@LALComputeDetAMResponse()@ to calculate $F_{+}$ and $F_{\times}$ for a given polarization angle, and then extract $a(t)$ and $b(t)$, once for each timestamp $t$.  Additionally, computation of the optimal statistic requires that we compute inner products of these two quantities for later use.  See \ref{Deft algo} for more on the optimal statistic.
+
+\subsubsection*{Uses}
+\begin{verbatim}
+LALBarycenter()
+LALBarycenterEarth()
+LALComputeDetAMResponse()
+\end{verbatim}
+
+\subsubsection*{Notes}
+
+\vfill{\footnotesize\input{LALComputeAMCV}}
+
+</lalLaTeX> */
+
+/* loop protection */
+#ifndef LALCOMPUTEAM_C
+#define LALCOMPUTEAM_C
+#endif
+
+#include "LALComputeAM.h"
+
+#define SQ(x) (x) * (x)
+
+NRCSID (LALCOMPUTEAMC, "$Id LALComputeAM.c $");
+
+/* <lalVerbatim file="LALComputeAMD"> */
+void LALComputeAM (LALStatus          *status,
+		   AMCoeffs           *coe,
+		   LIGOTimeGPS        *ts,
+		   AMCoeffsParams     *params)
+{  /* </lalVerbatim> */
+
+  REAL4 zeta;                  /* sine of angle between detector arms        */
+  INT4 i;                      /* temporary loop index                       */ 
+  EarthState earth;            /* earth information needed for LALBarycenter */
+  EmissionTime emit;           /* output of LALBarycenter                    */
+  LALDetAMResponse response;   /* output of LALComputeDetAMResponse          */
+  
+  REAL4 sumA2=0.0;
+  REAL4 sumB2=0.0;
+  REAL4 sumAB=0.0;             /* variables to store scalar products         */
+  INT4 length=coe->a->length;  /* length of input time series                */
+  
+  REAL4 cos2psi;
+  REAL4 sin2psi;               /* temp variables                             */
+ 
+  INITSTATUS(status, "LALComputeAM", LALCOMPUTEAMC);
+  ATTATCHSTATUSPTR(status);
+
+  /* Must put an ASSERT checking that ts vec and coe vec are same length */
+
+  /* Compute the angle between detector arms, then the reciprocal */
+  {
+    LALFrDetector det = params->das->pDetector->frDetector;
+    zeta = 1.0/(sin(det.xArmAzimuthRadians - det.yArmAzimuthRadians));
+  }  
+
+  cos2psi = cos(2.0 * params->polAngle); 
+  sin2psi = sin(2.0 * params->polAngle);  
+
+  /* Note the length is the same for the b vector */
+  for(i=0; i<length; ++i)
+    {
+      REAL4 *a = coe->a->data;
+      REAL4 *b = coe->b->data;
+      
+      /* Compute barycenter correction to GPS */
+      LALBarycenterEarth(status->statusPtr, &earth, &(ts[i]), params->edat);
+      params->baryinput->tgps = ts[i];
+      LALBarycenter(status->statusPtr, &emit, params->baryinput, &earth);
+      
+      /* Compute F_plus, F_cross */
+      LALComputeDetAMResponse(status->statusPtr, &response, params->das, &(emit.te));
+      
+      /*  Compute a, b from JKS eq 10,11
+       *  a = zeta * (F_plus*cos(2\psi)+F_cross*sin(2\psi))
+       *  b = zeta * (F_cross*cos(2\psi)-Fplus*sin(2\psi))
+       */
+      a[i] = zeta * (response.plus*cos2psi+response.cross*sin2psi); 
+      b[i] = zeta * (response.cross*cos2psi-response.plus*sin2psi); 
+
+      /* Compute scalar products */
+      sumA2 += SQ(a[i]);                       /*  A  */
+      sumB2 += SQ(b[i]);                       /*  B  */
+      sumAB += (a[i]) * (b[i]);                /*  C  */
+    }
+
+  {
+    /* Normalization factor */
+    REAL8 L = SQ(2.0/(REAL8)length);
+    
+    /* Assign output values and normalise */
+    coe->A = L*sumA2;
+    coe->B = L*sumB2;
+    coe->C = L*sumAB;
+    coe->D = ( coe->A * coe->B - SQ(coe->C) );
+    /* protection against case when AB=C^2 */
+    if(coe->D == 0) coe->D=1.0e-9;
+  }
+
+  /* Normal exit */
+
+  DETATCHSTATUSPTR(status);
+  RETURN(status);
+}
+
+
+      
+
+			      
+			      
+
+
+
+
+
