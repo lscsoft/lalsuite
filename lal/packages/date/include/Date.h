f427b58cf4059e271c4fabf6e0bda80148d1c5bd
dwchin <dwchin>
2002-05-13 21:06:05 +0000

    Fixed error message for Max Tested time -- no leap sec until, at earliest, 30-Sep-2002 23:59:00 UTC. Original: 585176ba4d494de4b6bbffc90037e0e0dbdbd38c

--- lal/packages/date/include/Date.h
+++ lal/packages/date/include/Date.h
@@ -86,7 +86,7 @@ NRCSID (DATEH, "$Id$");
 #define DATEH_MSGENULLINPUT "Input is NULL"
 #define DATEH_MSGENULLOUTPUT "Output is NULL"
 #define DATEH_MSGEDATETOOEARLY "Date too early: Julian Day can only be computed for dates >= 1900-03-01"
-#define DATEH_MSGERANGEGPSABS "Input time out of range: only able to accurately convert times between 1980-Jan-06 00:00:00 UTC (GPS 0) and 2002-Mar-31 23:59:00 UTC (GPS 701654353)"
+#define DATEH_MSGERANGEGPSABS "Input time out of range: only able to accurately convert times between 1980-Jan-06 00:00:00 UTC (GPS 0) and 2002-Sep-30 23:59:00 UTC (GPS 717465553)"
 #define DATEH_MSGEBUFFTOOSMALL "Output timestamp string too small: min. size = 26"
 #define DATEH_MSGEASCTIMEFAIL "asctimeUNDERSCOREr() failed"
 #define DATEH_MSGEGPSDATETOOEARLY "Date too early: GPS time only defined for times on or after 1980-Jan-06 00:00:00 UTC"
