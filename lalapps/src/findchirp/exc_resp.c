8f3f86dd43b3de94b1bbf4816df2fe9abdd60104
patrick <patrick>
2003-02-18 23:41:46 +0000

    Updated for S2 run Original: 3fc07c323ed09f9042f0617e2c872713e2ba49db

--- lalapps/src/findchirp/exc_resp.c
+++ lalapps/src/findchirp/exc_resp.c
@@ -10,15 +10,25 @@ RCSID( "$Id$" );
 
 /* Calibration Constants for L1:LSC-ETMX        */
 /* Based on mail from Gaby Mon, 24 Jun 2002     */
-#define L1_DC_RESP 2.63         /* nm/count */
+/* #define L1_DC_RESP 2.63  */      /* nm/count */
+/* Based on e-log entry http://www.ligo-la.caltech.edu/ilog/pub/ilog.cgi?group=detector&date_to_view=02/01/2003&anchor_to_scroll_to=2003:02:02:00:28:03-brian */
+#define L1_DC_RESP 0.39         /* +/- 0.02 (nm/ct) */
 #define L1_PEND_F0 0.76         /* Hz */
 #define L1_LENGTH  4000         /* m */
 
 /* Calibration constants for H2:LSC-ETMX        */
 /* Based on E7 calibration web page             */
-#define H2_DC_RESP 1.33         /* nm/count */
+/* #define H2_DC_RESP 1.33 */        /* nm/count */
+/* Based on e-mail from Mike Landry Wed, 12 Feb 2003 11:21:12 -0800 */
+#define H2_DC_RESP 1.05         /* nm/count */
 #define H2_PEND_F0 0.74         /* Hz */
 #define H2_LENGTH  2000         /* m */
+
+/* Calibration constants for H1:LSC-ETMX        */
+/* Based on e-mail from Mike Landry Wed, 12 Feb 2003 11:21:12 -0800 */
+#define H1_DC_RESP 0.93         /* nm/count */
+#define H1_PEND_F0 0.74         /* Hz */
+#define H1_LENGTH  4000         /* m */
 
 #define usgfmt \
 "Usage: %s [options]\n"\
@@ -47,7 +57,7 @@ int main ( int argc, char *argv[] )
   REAL4 f_min = 25.0;
   REAL4 f_max = 3000.0;
   REAL4 df;
-  FILE  *l1_fp = NULL, *h2_fp = NULL;
+  FILE  *l1_fp = NULL, *h2_fp = NULL, *h1_fp = NULL;
   
   /* parse options */
   while ( 0 < ( opt = getopt( argc, argv, "hVvd:L:H:n:" ) ) )
@@ -90,7 +100,8 @@ int main ( int argc, char *argv[] )
 
   l1_fp = fopen( "L1:LSC-ETMX_response", "w" );
   h2_fp = fopen( "H2:LSC-ETMX_response", "w" );
-  if ( !l1_fp || !h2_fp )
+  h1_fp = fopen( "H1:LSC-ETMX_response", "w" );
+  if ( !l1_fp || !h2_fp || !h1_fp )
   {
     fprintf( stderr, "error opening response function files for writing\n" );
     return 1;
@@ -102,6 +113,9 @@ int main ( int argc, char *argv[] )
   fprintf( h2_fp, "# epoch = %lli\n", 0LL ); 
   fprintf( h2_fp, "# f0 = %e\n", (REAL8) f_min );
   fprintf( h2_fp, "# deltaF = %e\n", (REAL8) df );
+  fprintf( h1_fp, "# epoch = %lli\n", 0LL ); 
+  fprintf( h1_fp, "# f0 = %e\n", (REAL8) f_min );
+  fprintf( h1_fp, "# deltaF = %e\n", (REAL8) df );
 
   for ( k = 0; k < numpts; ++k )
   {
@@ -110,12 +124,16 @@ int main ( int argc, char *argv[] )
       ( L1_LENGTH * f * f );
     REAL4 h2_r = ( H2_PEND_F0 * H2_PEND_F0 * H2_DC_RESP * 1e-9 ) / 
       ( H2_LENGTH * f * f );
+    REAL4 h1_r = ( H1_PEND_F0 * H1_PEND_F0 * H1_DC_RESP * 1e-9 ) / 
+      ( H1_LENGTH * f * f );
     fprintf( l1_fp, "%e %e\n", l1_r, 0.0 );
     fprintf( h2_fp, "%e %e\n", h2_r, 0.0 );
+    fprintf( h1_fp, "%e %e\n", h1_r, 0.0 );
   }
 
   fclose( l1_fp );
   fclose( h2_fp );
+  fclose( h1_fp );
 
   return 0;
 }
