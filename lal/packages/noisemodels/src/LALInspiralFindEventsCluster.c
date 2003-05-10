eb46acf5c219b789f3dd400e11200bbb447f54ca
sathya <sathya>
2003-05-10 14:57:47 +0000

    Adding event clustering and new normalisation modules Original: fc16bb1b4d1ada2fd3fc468bc4ed51c3a1815740

---
+++ lal/packages/noisemodels/src/LALInspiralFindEventsCluster.c
@@ -0,0 +1,357 @@
+/*  <lalVerbatim file="LALInspiralFindEventsClusterCV">
+Author: Sathyaprakash, B. S.
+$Id$
+</lalVerbatim>  */
+/* <lalLaTeX>
+\subsection{Module \texttt{LALInspiralFindEventsCluster.c}}
+Module to find all `distinct' events in a given data set with an SNR 
+larger than a pre-specified threshold. To select `distinct'
+events the code uses two thresholds; all events crossing a first
+{\it lower} threshold are clustered. If at least one of the points
+in the clustered event crosses a second {\it higher} threshold then the clustered
+event is a trigger and what is recorded is the loudest point in the
+clustered event and the number of points above the first threshold in
+the cluster. 
+
+The module uses two orthogonal inspiral signals of specified parameters 
+with a weight specified in a psd array. The code returns
+the number of events found, and for each event the snr, 
+the bin number and the phase of the template at that bin.
+
+\subsubsection*{Prototypes}
+\vspace{0.1in}
+\input{LALInspiralFindEventsClusterCP}
+\index{\verb&LALInspiralFindEventsCluster()&}
+
+\subsubsection*{Description}
+\subsubsection*{Algorithm}
+\subsubsection*{Uses}
+\begin{verbatim}
+LALInspiralWave
+LALREAL4VectorFFT
+LALInspiralWaveNormaliseLSO
+LALInspiralWaveCorrelate
+\end{verbatim}
+
+\subsubsection*{Notes}
+
+\vfill{\footnotesize\input{LALInspiralFindEventsClusterCV}}
+</lalLaTeX>  */
+#include <lal/LALNoiseModels.h>
+
+NRCSID (LALINSPIRALFINDEVENTSCLUSTERC, "$Id$");
+
+/*  <lalVerbatim file="LALInspiralFindEventsClusterCP"> */
+void
+LALInspiralFindEventsCluster 
+   (
+   LALStatus            *status,
+   INT4                 *nEvents,
+   InspiralEventsList   **eventlist,
+   InspiralFindEventsIn *findeventsin
+   )
+{  /*  </lalVerbatim>  */
+
+   /* 
+    * We shall assume that the number of events to be found in 
+    * each template is no more than 100000.
+   */
+   INT4 i;
+   INT4 nBegin;
+   INT4 nEnd;
+   INT4 numPoints;
+   INT4 previous;
+   INT4 bin;
+
+   REAL8 eSec;
+   REAL8 dt;
+   REAL8 df;
+   REAL8 f;
+   REAL8 norm;
+   REAL8 cmax;
+   REAL8 phase;
+   REAL8 distanceNorm;
+   REAL8 flso;
+   REAL8 msevenby3;
+   REAL8 x;
+   REAL8 y;
+   REAL8 z;
+
+   REAL8 totalMass;
+   REAL8 eta;
+   REAL8 dist;
+   REAL4 chisq;
+
+   REAL4Vector filter1;
+   REAL4Vector filter2;
+   REAL4Vector output1;
+   REAL4Vector output2;
+   REAL4Vector buffer;
+   REAL4Vector SNRZeroIntegrand;
+
+   StatsREAL4VectorOut statsout1;
+   StatsREAL4VectorOut statsout2;
+
+   InspiralWaveCorrelateIn corrin;
+   InspiralSNRIntegrandParams params;
+   InspiralChisqDataVec chisqDataVec;
+   InspiralChisqParams chisqParams;
+   InspiralWaveNormaliseIn normin;
+
+   INITSTATUS (status, "LALInspiralFindEventsCluster", LALINSPIRALFINDEVENTSCLUSTERC);
+   ATTATCHSTATUSPTR(status);
+
+   ASSERT (findeventsin->psd.data,  status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+   ASSERT (findeventsin->signal.data,  status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+
+   output1.length = output2.length = findeventsin->signal.length;
+   filter1.length = filter2.length = findeventsin->signal.length;
+   SNRZeroIntegrand.length = findeventsin->signal.length;
+
+
+   ASSERT (findeventsin->nEnd >= 0,  status, LALNOISEMODELSH_ECHOICE, LALNOISEMODELSH_MSGECHOICE);
+   ASSERT (findeventsin->nEnd <= (INT4)output1.length,  status, LALNOISEMODELSH_ECHOICE, LALNOISEMODELSH_MSGECHOICE);
+   ASSERT (findeventsin->nBegin >= 0,  status, LALNOISEMODELSH_ECHOICE, LALNOISEMODELSH_MSGECHOICE);
+   ASSERT (findeventsin->nBegin <= (INT4)output1.length,  status, LALNOISEMODELSH_ECHOICE, LALNOISEMODELSH_MSGECHOICE);
+
+
+   dt = 1.L/findeventsin->param.tSampling;
+   df = 1.L/(output1.length * dt);
+   nBegin = findeventsin->nBegin;
+   nEnd = findeventsin->signal.length - findeventsin->nEnd;
+
+   buffer.length = nEnd - nBegin;
+   if (!(output1.data = (REAL4*) LALMalloc(sizeof(REAL4)*output1.length))) {
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+   if (!(output2.data = (REAL4*) LALMalloc(sizeof(REAL4)*output2.length))) {
+      LALFree(output1.data);
+      output1.data = NULL;
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+   if (!(filter1.data = (REAL4*) LALMalloc(sizeof(REAL4)*filter1.length))) {
+      LALFree(output1.data);
+      LALFree(output2.data);
+      output1.data = NULL;
+      output2.data = NULL;
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+   if (!(filter2.data = (REAL4*) LALMalloc(sizeof(REAL4)*filter2.length))) {
+      LALFree(output1.data);
+      LALFree(output2.data);
+      LALFree(filter1.data);
+      output1.data = NULL;
+      output2.data = NULL;
+      filter1.data = NULL;
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+   if (!(buffer.data = (REAL4*) LALMalloc(sizeof(REAL4)*buffer.length))) {
+      LALFree(output1.data);
+      LALFree(output2.data);
+      LALFree(filter1.data);
+      LALFree(filter2.data);
+      output1.data = NULL;
+      output2.data = NULL;
+      filter1.data = NULL;
+      filter2.data = NULL;
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+   if (!(SNRZeroIntegrand.data = (REAL4*) LALMalloc(sizeof(REAL4)*SNRZeroIntegrand.length))) {
+      LALFree(output1.data);
+      LALFree(output2.data);
+      LALFree(filter1.data);
+      LALFree(filter2.data);
+      LALFree(buffer.data);
+      output1.data = NULL;
+      output2.data = NULL;
+      filter1.data = NULL;
+      filter2.data = NULL;
+      buffer.data = NULL;
+      ABORT (status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+   }
+
+   findeventsin->param.nStartPad = 0;
+   findeventsin->param.startPhase = LAL_PI_2;
+   LALInspiralWave(status->statusPtr, &output2, &findeventsin->param);
+   CHECKSTATUSPTR(status);
+   findeventsin->param.startPhase = 0.;
+   LALInspiralWave(status->statusPtr, &output1, &findeventsin->param);
+   CHECKSTATUSPTR(status);
+   if (findeventsin->displayTemplates)
+   {
+      for (i=0;i<(INT4)output1.length;i++) 
+         printf("%e %e\n", i*dt, output1.data[i]);printf("&\n");
+      for (i=0;i<(INT4)output1.length;i++) 
+         printf("%e %e\n", i*dt, output2.data[i]);printf("&\n");
+   }
+   LALREAL4VectorFFT(status->statusPtr, &filter1, &output1, findeventsin->fwdp);
+   CHECKSTATUSPTR(status);
+   LALREAL4VectorFFT(status->statusPtr, &filter2, &output2, findeventsin->fwdp);
+   CHECKSTATUSPTR(status);
+   normin.psd = &(findeventsin->psd);
+   normin.df = df;
+   normin.fCutoff = findeventsin->param.fCutoff;
+   normin.samplingRate = findeventsin->param.tSampling;
+   LALInspiralWaveNormaliseLSO(status->statusPtr, &filter1, &norm, &normin);
+   CHECKSTATUSPTR(status);
+   LALInspiralWaveNormaliseLSO(status->statusPtr, &filter2, &norm, &normin);
+   CHECKSTATUSPTR(status);
+   corrin.df = df;
+   corrin.psd = findeventsin->psd;
+   corrin.revp = findeventsin->revp;
+   corrin.signal1 = findeventsin->signal;
+   corrin.fCutoff = findeventsin->param.fCutoff;
+   corrin.samplingRate = findeventsin->param.tSampling;
+
+   corrin.signal2 = filter1;
+   LALInspiralWaveCorrelate(status->statusPtr, &output1, corrin);
+   CHECKSTATUSPTR(status);
+   corrin.signal2 = filter2;
+   LALInspiralWaveCorrelate(status->statusPtr, &output2, corrin);
+   CHECKSTATUSPTR(status);
+
+
+   for (i=nBegin;i<nEnd;i++) 
+	   buffer.data[i-nBegin] = output1.data[i];
+   LALStatsREAL4Vector(status->statusPtr, &statsout1, &buffer);
+   CHECKSTATUSPTR(status);
+   for (i=nBegin;i<nEnd;i++) 
+	   buffer.data[i-nBegin] = output2.data[i];
+   LALStatsREAL4Vector(status->statusPtr, &statsout2, &buffer);
+   CHECKSTATUSPTR(status);
+
+   if (findeventsin->displayCorrelationStats)
+   {
+	   fprintf(stderr, "mean=%e std=%e min=%e max=%e\n", statsout1.mean, statsout1.stddev, statsout1.min, statsout1.max);   
+   
+	   fprintf(stderr, "mean=%e std=%e min=%e max=%e\n", statsout2.mean, statsout2.stddev, statsout2.min, statsout2.max);   
+   }
+   
+   if (findeventsin->displayCorrelation)
+   {
+      for (i=nBegin;i<nEnd;i++) 
+         {
+            x = pow ( pow( output1.data[i], 2.) + pow( output2.data[i], 2.), 0.5); 
+            printf("%16.12e %e\n", (findeventsin->currentGPSTime-715434100.) + i*dt+findeventsin->param.tC, x);
+         }
+         printf("&\n");   
+   }
+   msevenby3 = -7.L/3.L;
+   distanceNorm = 0.;
+   totalMass = findeventsin->param.totalMass*LAL_MTSUN_SI;
+   eta = findeventsin->param.eta;
+   flso = 1.L/(pow(6.L,1.5L)*totalMass*LAL_PI);
+   for (i=1; i<findeventsin->psd.length; i++)
+   {
+	   f = i*df;
+	   if (f > flso) break;
+	   if (findeventsin->psd.data[i]) distanceNorm += pow(f,msevenby3)/findeventsin->psd.data[i];
+   }
+   distanceNorm = sqrt(distanceNorm);
+   distanceNorm *= df * pow(totalMass, 5.L/6.L) * sqrt(5.L*eta/12.L)/pow(LAL_PI,2.L/3.L) ;
+
+   *nEvents = 0;
+   previous = 0;
+   cmax = 0.;
+   numPoints = 0;
+
+   for (i=nBegin; i<nEnd; i++) 
+   {
+       x = output1.data[i];
+       y = output2.data[i];
+       z = sqrt(x*x + y*y);
+       if (z>findeventsin->ClusterThreshold)
+       {
+	  numPoints++;
+	  if (previous)
+	  {
+	     if (z>cmax) 
+	     {
+                cmax = z ;
+		bin = i;
+		phase = atan2(y,x);
+	     }
+	  }
+	  else
+	  {
+	     previous = 1;
+	     cmax = z;
+	     bin = i;
+	     phase = atan2(y,x);
+	  }
+       }
+       else
+       {
+	  if (previous)
+	  {
+	     if (cmax > findeventsin->Threshold) 
+	     {
+          
+		if (!(*eventlist = (InspiralEventsList*)
+					     LALRealloc(*eventlist, sizeof(InspiralEventsList)*((*nEvents)+1))))
+		{
+			ABORT(status, LALNOISEMODELSH_EMEM, LALNOISEMODELSH_MSGEMEM);
+		}
+		fprintf(stderr, "Triggering event %d with %d points and max at bin %d\n", (*nEvents)+1, numPoints, bin);
+   
+		/*
+		scanf("%d\n", &numPoints);
+   
+		if (numPoints)
+		{
+			for (i=nBegin;i<nEnd;i++) 
+			{
+				x = pow ( pow( output1.data[i], 2.) + pow( output2.data[i], 2.), 0.5); 
+				printf("%e %e\n",i*dt, x);
+			}
+			printf("&\n");   
+		}
+		*/
+
+		(*eventlist)[*nEvents].snr = cmax;
+		(*eventlist)[*nEvents].param = findeventsin->param;
+		(*eventlist)[*nEvents].phase = phase;
+		dist = distanceNorm / cmax;
+		(*eventlist)[*nEvents].effDistance = LAL_C_SI * dist / LAL_PC_SI /1.e6;
+		(*eventlist)[*nEvents].amplitude = 4.*eta*(totalMass/dist)*pow(LAL_PI*totalMass*100.,2.L/3.L);
+		(*eventlist)[*nEvents].bin = bin;
+		eSec = (double) bin / findeventsin->param.tSampling;
+		(*eventlist)[*nEvents].impulseTime = findeventsin->currentGPSTime + (int) eSec;
+		(*eventlist)[*nEvents].impulseTimeNS = (int) (1.e9 * (eSec - (int) eSec));
+		eSec += findeventsin->param.tC;
+		(*eventlist)[*nEvents].endTime = findeventsin->currentGPSTime + (int) eSec;
+		(*eventlist)[*nEvents].endTimeNS = (int) (1.e9 * (eSec - (int) eSec));
+		(*eventlist)[*nEvents].sigmasq = (statsout1.var+statsout2.var)/2.;
+   
+		/* Now call the chi-squared code */
+		params.lag = (*eventlist)[*nEvents].bin;
+		params.phase = (*eventlist)[*nEvents].phase;
+		params.deltaT = 1.0/findeventsin->param.tSampling;
+		corrin.signal2 = filter1;
+		LALInspiralComputeSNRIntegrand(status->statusPtr, &SNRZeroIntegrand, corrin, &params);
+		chisqDataVec.SNRIntegrand = &SNRZeroIntegrand;
+		chisqDataVec.psd = &(findeventsin->psd);
+		chisqParams.totalMass = totalMass;
+		chisqParams.fLower = findeventsin->param.fLower;
+		chisqParams.deltaT = params.deltaT;
+		chisqParams.nBins = 8;
+		LALInspiralComputeChisq(status->statusPtr, &chisq, &chisqDataVec, &chisqParams); 
+		(*eventlist)[*nEvents].chisq = chisq;
+		(*eventlist)[*nEvents].chisqDOF = 2*(chisqParams.nBins-1);
+		(*nEvents)++;
+	     }
+	     previous = 0;
+	     numPoints = 0;
+          }
+       }
+   }
+   LALFree(filter1.data);
+   LALFree(filter2.data);
+   LALFree(output1.data);
+   LALFree(output2.data);
+   LALFree(buffer.data);
+   LALFree(SNRZeroIntegrand.data);
+   DETATCHSTATUSPTR(status);
+   RETURN(status);
+}
+
---
+++ lal/packages/noisemodels/src/LALInspiralWaveNormaliseLSO.c
@@ -0,0 +1,110 @@
+/*  <lalVerbatim file="LALInspiralWaveNormaliseLSOCV">
+Author: Sathyaprakash, B. S.
+$Id$
+</lalVerbatim>  */
+/* <lalLaTeX>
+\subsection{Module \texttt{LALInspiralWaveNormaliseLSO.c}}
+Module to find the norm of a signal and to return a normaliseLSOd 
+array. The original signal is left untouched.
+
+\subsubsection*{Prototypes}
+\vspace{0.1in}
+\input{LALInspiralWaveNormaliseLSOCP}
+\index{\verb&LALInspiralWaveNormaliseLSO()&}
+
+\subsubsection*{Description}
+Given the positive frequency Fourier components 
+$H_k,$ $k=0,\ldots,n-1,$ of a vector 
+and the noise PSD $S_m,$ $m=0,\ldots,n/2,$ 
+this module first computes the norm $H$ of the vector treating
+$S_m$ as the measure: 
+(note that in {\em fftw} notation, the zeroth frequency 
+component is $H_0,$ Nyquist 
+is $H_{n/2},$ $H_k,$ $k \ne 0,n/2,$ ($H_{n-k})$ is the real (imaginary)
+part of the $k$th harmonic) 
+\begin{equation}
+H = \sum_{k=1}^{n/2-1} \frac{H_k^2 + H^2_{n-k}}{S_k}.
+\label{eq:inspiralnorm}
+\end{equation}
+(Note that the zeroth and Nyquist components are ignored in the
+computation of the norm.)
+It then replaces the original vector $H_k$ with normalized
+vector using: 
+\begin{equation}
+\widehat H_k = \frac {H_k}{\sqrt H},\ \ k=0,\ldots n-1.
+\end{equation}
+Note that the norm of $\widehat H_k,$ as defined in Eq.~(\ref{eq:inspiralnorm}), 
+is unity.
+\subsubsection*{Algorithm}
+\subsubsection*{Uses}
+\begin{verbatim}
+none.
+\end{verbatim}
+
+\subsubsection*{Notes}
+
+\vfill{\footnotesize\input{LALInspiralWaveNormaliseLSOCV}}
+</lalLaTeX>  */
+#include <lal/LALNoiseModels.h>
+
+NRCSID (LALINSPIRALWAVENORMALISEC, "$Id$");
+
+/*  <lalVerbatim file="LALInspiralWaveNormaliseLSOCP"> */
+void
+LALInspiralWaveNormaliseLSO 
+   (
+   LALStatus   *status, 
+   REAL4Vector *filter,
+   REAL8       *norm,
+   InspiralWaveNormaliseIn *in 
+   ) 
+{  /*  </lalVerbatim>  */
+
+  INT4 i, n, nby2, k;
+  REAL8 psdvalue, f;
+
+  INITSTATUS (status, "LALInspiralWaveNormaliseLSO", LALINSPIRALWAVENORMALISEC);
+  ATTATCHSTATUSPTR(status);
+
+  ASSERT (filter->data,  status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+  ASSERT (in->psd->data,  status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+  ASSERT (in->psd->length == filter->length/2+1, status, LALNOISEMODELSH_ESIZE, LALNOISEMODELSH_MSGESIZE);
+
+  n = filter->length;
+  *norm = 0;
+  nby2 = n/2;
+
+  for (i=1; i<nby2; i++) 
+  {
+     k = n-i;
+     psdvalue = in->psd->data[i];
+     f = i*in->df;
+     if (f>in->fCutoff) break;
+     if (psdvalue) 
+     {
+	*norm += (pow(filter->data[i], 2.) + pow(filter->data[k], 2.))/psdvalue;
+     }
+  }
+
+  /* If we want to add the negative frequency components as well now is
+   * the time to do that before 0th and Nyquist contributions are added
+   */
+
+  (*norm) *= 2.;
+
+  if (in->psd->data[0]) *norm += pow(filter->data[0], 2.)/in->psd->data[0];
+  f = nby2*in->df;
+  if (f<in->fCutoff) if (in->psd->data[nby2]) *norm += pow(filter->data[nby2], 2.)/in->psd->data[nby2];
+
+  *norm /= ((double) n * in->samplingRate);
+  *norm = sqrt(*norm);
+  
+  for (i=0; i<n; i++)
+  { 
+	  *(filter->data+i) /= *norm;
+  }
+
+  DETATCHSTATUSPTR(status);
+  RETURN(status);
+}
+
