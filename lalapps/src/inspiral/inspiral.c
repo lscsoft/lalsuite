a3ace01678d5f71adb23b160bfb846cef6bc10ce
duncan <duncan>
2003-06-17 04:47:02 +0000

    added check on caches and moved temp fr utils to separate file Original: 8963f148f1f1701645bd3802c4a3a22d49a02cf6

--- lalapps/src/inspiral/inspiral.c
+++ lalapps/src/inspiral/inspiral.c
@@ -11,6 +11,7 @@
 
 #include <config.h>
 #include "inspiral.h"
+#include "inspiralfrutils.h"
 #include "ligolwbank.h"
 
 RCSID( "$Id$" );
@@ -1313,90 +1314,19 @@ int arg_parse_check( int argc, char *argv[], MetadataTable procparams )
     exit( 1 );
   }
 
+  /* check that the frame caches have been specified */
+  if ( ! frInCacheName )
+  {
+    fprintf( stderr, "--frame-cache must be specified\n" );
+    exit( 1 );
+  }
+  if ( ! calCacheName )
+  {
+    fprintf( stderr, "--calibration-cache must be specified\n" );
+    exit( 1 );
+  }
+
   return 0;
-}
-
-
-/*
- *
- * functions to write data to frame files, since lal doesn't have this yet
- *
- */
-
-
-FrameH *fr_add_proc_REAL4TimeSeries ( 
-    FrameH                     *frame, 
-    REAL4TimeSeries            *chan,
-    const char                 *unit,
-    const char                 *suffix
-    )
-{
-  char          chname[256];
-  struct        series fdata;
-
-  snprintf( chname, sizeof(chname), "%s_%s", chan->name, suffix );
-    fdata.name = chname;
-    fdata.tbeg = chan->epoch;
-    memset( &fdata.tend, 0, sizeof(LIGOTimeGPS) );
-    epoch_add( &fdata.tend, &(chan->epoch), 
-        chan->deltaT * (REAL8) chan->data->length );
-    fdata.dom = Time;
-    fdata.type = FR_VECT_4R;
-    fdata.step = (float) chan->deltaT;
-    fdata.unit = unit;
-    fdata.size = (size_t) chan->data->length;
-    fdata.data = (float *) chan->data->data;
-    return fr_add_proc_data( frame, &fdata );
-}
-
-FrameH *fr_add_proc_REAL4FrequencySeries ( 
-    FrameH                     *frame, 
-    REAL4FrequencySeries       *chan,
-    const char                 *unit,
-    const char                 *suffix
-    )
-{
-  char          chname[256];
-  struct        series fdata;
-
-  snprintf( chname, sizeof(chname), "%s_%s", chan->name, suffix );
-    fdata.name = chname;
-    fdata.tbeg = chan->epoch;
-    memset( &fdata.tend, 0, sizeof(LIGOTimeGPS) );
-    epoch_add( &fdata.tend, &chan->epoch, 
-        (chan->data->length - 1) / (chan->deltaF * chan->data->length) );
-    fdata.dom = Freq;
-    fdata.type = FR_VECT_4R;
-    fdata.step = (float) chan->deltaF;
-    fdata.unit = unit;
-    fdata.size = (size_t) chan->data->length;
-    fdata.data = (float *) chan->data->data;
-    return fr_add_proc_data( frame, &fdata );
-}
-
-FrameH *fr_add_proc_COMPLEX8FrequencySeries ( 
-    FrameH                        *frame, 
-    COMPLEX8FrequencySeries       *chan,
-    const char                    *unit,
-    const char                    *suffix
-    )
-{
-  char          chname[256];
-  struct        series fdata;
-
-  snprintf( chname, sizeof(chname), "%s_%s", chan->name, suffix );
-    fdata.name = chname;
-    fdata.tbeg = chan->epoch;
-    memset( &fdata.tend, 0, sizeof(LIGOTimeGPS) );
-    epoch_add( &fdata.tend, &chan->epoch, 
-        (chan->data->length - 1) / (chan->deltaF * chan->data->length) );
-    fdata.dom = Freq;
-    fdata.type = FR_VECT_8C;
-    fdata.step = (float) chan->deltaF;
-    fdata.unit = unit;
-    fdata.size = (size_t) chan->data->length;
-    fdata.data = (float *) chan->data->data;
-    return fr_add_proc_data( frame, &fdata );
 }
 
 #undef ADD_PROCESS_PARAM
