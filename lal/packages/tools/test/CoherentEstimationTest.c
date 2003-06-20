61269e5a54dd2165b5a0b484622415a33376dbe8
jolien <jolien>
2003-06-20 19:23:47 +0000

    Fixed wrong datatypes. Original: 881f820c4708836226d2d96a700082dc16f3c992

--- lal/packages/tools/test/CoherentEstimationTest.c
+++ lal/packages/tools/test/CoherentEstimationTest.c
@@ -10,21 +10,21 @@ NRCSID( COHERENTESTIMATIONTESTC, "$Id$" );
 int lalDebugLevel = 3;
 
 int
-main(int argc, char **argv)
+main(void)
 {
 
   static LALStatus stat;
-  UINT4 i, j;
+  UINT4 i/*, j*/;
   UINT4 Ndetectors = 2; /* number of detectors */
-  COMPLEX8TimeSeries output;
-  COMPLEX8Vector *outputData;
+  REAL4TimeSeries output;
+  REAL4Vector *outputData;
   CoherentEstimation params;
   DetectorsData in;
 
-  COMPLEX8ZPGFilter filterInput;
+  COMPLEX16ZPGFilter filterInput;
   UINT4 filLength = 3;
-  COMPLEX8 buffer[3];
-  COMPLEX8Vector *Z, *P;
+  COMPLEX16 buffer[3];
+  COMPLEX16Vector *Z, *P;
 
   SkyPosition Pos;
 
@@ -32,7 +32,7 @@ main(int argc, char **argv)
   INT4 seed = 0;
   UINT4 length = 360 * 2048;
 
-  REAL4IIRFilter *filt = NULL;
+  REAL8IIRFilter *filt = NULL;
 
   /* initialize CoherentEstimation structure */
   params.preProcessed = 0; /* for pre-processing to happen */
@@ -46,8 +46,8 @@ main(int argc, char **argv)
   /* pre-processing filters */
   filterInput.deltaT = 4.8828125E-4; /* 2048 Hz sampling */
   Z = P = NULL;
-  LALCCreateVector(&stat, &Z, filLength);
-  LALCCreateVector(&stat, &P, filLength);
+  LALZCreateVector(&stat, &Z, filLength);
+  LALZCreateVector(&stat, &P, filLength);
   filterInput.zeros = Z;
   filterInput.poles = P;
   filterInput.gain.re = 31.55634918610406103312016;
@@ -63,10 +63,10 @@ main(int argc, char **argv)
   Z->data[2].re = 0.4142135623730950899634706;
   Z->data[2].im = 0.0;
 
-  params.filters = (REAL4IIRFilter **)LALMalloc(Ndetectors * sizeof(REAL4IIRFilter *));
+  params.filters = (REAL8IIRFilter **)LALMalloc(Ndetectors * sizeof(REAL8IIRFilter *));
   for(i=0; i<Ndetectors; i++) {
     params.filters[i] = NULL;
-    LALCreateREAL4IIRFilter(&stat, params.filters + i, &filterInput);
+    LALCreateREAL8IIRFilter(&stat, params.filters + i, &filterInput);
   }
 
   /* sky position */
@@ -81,10 +81,10 @@ main(int argc, char **argv)
   /* create direct filter */
   filterInput.gain.re = 0.031689343849711;
   filterInput.gain.im = 0.0;
-  memcpy(buffer, P, filLength * sizeof(COMPLEX8));
-  memcpy(P, Z, filLength * sizeof(COMPLEX8));
-  memcpy(Z, buffer, filLength * sizeof(COMPLEX8));
-  LALCreateREAL4IIRFilter(&stat, &filt, &filterInput);
+  memcpy(buffer, P, filLength * sizeof(COMPLEX16));
+  memcpy(P, Z, filLength * sizeof(COMPLEX16));
+  memcpy(Z, buffer, filLength * sizeof(COMPLEX16));
+  LALCreateREAL8IIRFilter(&stat, &filt, &filterInput);
   
 
   /* Initialize input */
@@ -112,12 +112,12 @@ main(int argc, char **argv)
     return 0;
     */
 
-    LALIIRFilterREAL4Vector(&stat, in.data[i].data, filt);
+    LALDIIRFilterREAL4Vector(&stat, in.data[i].data, filt);
   }
   
   /* allocate output */
   outputData = NULL;
-  LALCCreateVector(&stat, &outputData, length);
+  LALSCreateVector(&stat, &outputData, length);
   output.data = outputData;
 
   /*
@@ -130,7 +130,7 @@ main(int argc, char **argv)
   LALCoherentEstimation(&stat, &output, &params, &in);
 
   for(i=0;i<length;i++)
-    printf("%g\t%g\n",output.data->data[i].re, output.data->data[i].im);
+    printf("%g\n",output.data->data[i]);
 
 
   return 0;
