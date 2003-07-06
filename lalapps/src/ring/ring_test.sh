05abc5e83edd52213f17a12bce586a2f8d4e151b
jolien <jolien>
2003-07-06 19:21:09 +0000

    Removed uniq bit that is not standard. Original: c112cca8011337c9fa20fae7b86692b9d3ce4282

--- lalapps/src/ring/ring_test.sh
+++ lalapps/src/ring/ring_test.sh
@@ -3,12 +3,12 @@ d=${srcdir:-.}
 cmd="./lalapps_ring -v -d 1 -f $d/\*.F -r $d/response.asc -i $d/filterpar.in -o events.out"
 echo $cmd
 eval $cmd || exit $?
-sed -n '/#/p' events.out \
-    > maxevents.out \
-  || exit $?
-sed '/#/d' events.out \
-    | sort -r -k2 \
-    | sort -k1,1 -t. -s \
-    | uniq -w9 \
-    >> maxevents.out \
-  || exit $?
+#sed -n '/#/p' events.out \
+#    > maxevents.out \
+#  || exit $?
+#sed '/#/d' events.out \
+#    | sort -r -k2 \
+#    | sort -k1,1 -t. -s \
+#    | uniq -w9 \
+#    >> maxevents.out \
+#  || exit $?
