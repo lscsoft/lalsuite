cbc9c42d03ebe2a112b3b01be9240adf03559d8e
duncan <duncan>
2003-08-06 22:10:09 +0000

    added documentation Original: 62d5feadb7499f1317146e17c7c8a2ad82e6ce13

--- lal/packages/tools/include/ResampleTimeSeries.h
+++ lal/packages/tools/include/ResampleTimeSeries.h
@@ -7,14 +7,17 @@
  *
  * \section{Header \texttt{ResampleTimeSeries.h}}
  *
- * High-level routines for resampleing time series data.
+ * Provides routines to resample a time series. At present only integer
+ * downsampling or \verb|REAL4TimeSeries| by a power of two is supported.
  *
  * \subsection*{Synopsis}
  * \begin{verbatim}
  * #include <lal/ResampleTimeSeries.h>
  * \end{verbatim}
  *
- * Provides a high level interface for resampling time series data.
+ * \noindent This header covers routines that resample time series by applying
+ * a low pass filter and decimating the resulting time series. Further
+ * documentation is given in the individual routines' modules.
  *
  **** </lalLaTeX> */
 
@@ -60,11 +63,58 @@ NRCSID( RESAMPLETIMESERIESH, "$Id$" );
 #define RESAMPLETIMESERIESH_MSGELDAS "Input resample factor with LDAS FIR"
 /**** </lalErrTable> */
 
-/**** <lalLaTeX>
- *
- * \subsection*{Structures}
- *
- **** </lalLaTeX> */
+/************************************************************* <lalLaTeX>
+\subsection*{Types}
+\idx[Type]{ResampleTSFilter}
+\idx[Type]{ResampleTSFilterParams}
+\idx[Type]{ResampleTSParams}
+
+\subsubsection*{Enum \texttt{ResampleTSFilter}}
+This enum type contains the different low pass filters available to
+prevent power above the new Nyquist frequency entering the resampled 
+time series due to aliasing. The options are:
+
+\begin{description}
+\item[\texttt{defaultButterworth}] An IIR butterwoth filter of order 20 
+with attenuation 0.1 at the new Nyquist frequency. See the package tdfilters
+for documentation of butterworth filters in LAL.
+
+\item[\texttt{LDASfirLP}] For downsampling by a factor of 2, 4 or 8 an
+implementation of the FIR filter used by the LDAS datacondAPI resample().
+This is provided for testing the result of standalone codes and codes 
+running under LDAS. The LDAS filter provided here has filter order parameter
+10, so the order of the filter is $2 \times 10 \times q$ where $q$ is the
+resampling ratio.
+\end{description}
+
+\subsubsection*{Union \texttt{ResampleTSFilterParams}}
+This union is provided so that the code can store the parameters of the 
+filter in a place accessible by the user for user designed low pass filters.
+This is not presently implemented and this structure may be ignored.
+\begin{description}
+\item[\texttt{butterworth}] A structure of type \verb|PassBandParamStruc| used
+to store the parameters of the butterworth filter used to perform low pass
+filtering.
+
+\item[\texttt{iirfilter}] A structure of type \verb|REAL8IIRFilter| used
+to store the parameters of the IIR or FIR filter used to perform low pass
+filtering.
+\end{description}
+
+\subsubsection*{Structure \texttt{tagResampleTimeSeriesParams}}
+This structure controls the behaviour of the resampling function.
+\begin{description}
+\item[\texttt{deltaT}] The sample interval desired in the down sampled time
+series.
+
+\item[\texttt{filterType}] The type of filter with which to perform the low
+pass filtering.
+
+\item[\texttt{filterParams}] Filter parameters for the low pass filter. 
+Presently ignored.
+\end{description}
+
+************************************************************* </lalLaTeX> */
 
 typedef enum
 {
