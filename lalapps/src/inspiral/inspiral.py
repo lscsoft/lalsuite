c61d588eb48620040e83edeec1915bee2e35056d
duncan <duncan>
2003-09-23 17:28:52 +0000

    documentation and some minro re-arangement of functions Original: 282c8e2f11dbeb07afe89fa994d346ee42f69ba5

--- lalapps/src/inspiral/inspiral.py
+++ lalapps/src/inspiral/inspiral.py
@@ -16,30 +16,51 @@ import exceptions
 import pipeline
 
 
-
 class InspiralError(exceptions.Exception):
   def __init__(self, args=None):
     self.args = args
 
 
-
 class DataFindJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A LALdataFind job used by the inspiral pipeline. The static arguments are
+  read from the section [datafind] in the ini file. The stdout from
+  LALdataFind contains the paths to the frame files and is directed to a file
+  in the cache directory named by site and GPS start and end times. The stderr
+  is directed to the logs directory. The job always runs in the scheduler
+  universe. The path to the executable is determined from the ini file.
+  """
   def __init__(self,cp):
-    pipeline.CondorDAGJob.__init__(self,'LALDataFind','scheduler')
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__executable = cp.get('condor','datafind')
+    self.__universe = 'scheduler'
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
     pipeline.AnalysisJob.__init__(self,cp)
 
     for sec in ['datafind']:
       self.add_ini_args(cp,sec)
 
-    self.set_stdout_file('cache/$(frame-cache)')
     self.set_stderr_file('logs/datafind-$(instrument)-$(start)-$(end).err')
     self.set_sub_file('datafind.sub')
 
 
-
 class TmpltBankJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_tmpltbank job used by the inspiral pipeline. The static arguments
+  are read from the sections [data] and [tmpltbank] in the ini file. The
+  stdout and stderr from the job are directed to the logs directory. The job
+  runs in the universe specfied in the ini file. The path to the executable
+  is determined from the ini file.
+  """
   def __init__(self,cp):
-    pipeline.CondorDAGJob.__init__(self,'lalapps_tmpltbank','vanilla')
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__executable = cp.get('condor','tmpltbank')
+    self.__universe = cp.get('condor','univese')
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
     pipeline.AnalysisJob.__init__(self,cp)
 
     for sec in ['data','tmpltbank']:
@@ -52,10 +73,21 @@ class TmpltBankJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('tmpltbank.sub')
     
 
-
 class InspiralJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_inspiral job used by the inspiral pipeline. The static arguments
+  are read from the sections [data] and [inspiral] in the ini file. The
+  stdout and stderr from the job are directed to the logs directory. The job
+  runs in the universe specfied in the ini file. The path to the executable
+  is determined from the ini file.
+  """
   def __init__(self,cp):
-    pipeline.CondorDAGJob.__init__(self,'lalapps_inspiral','vanilla')
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__executable = cp.get('condor','inspiral')
+    self.__universe = cp.get('condor','univese')
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
     pipeline.AnalysisJob.__init__(self,cp)
 
     for sec in ['data','inspiral']:
@@ -68,10 +100,21 @@ class InspiralJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('inspiral.sub')
     
 
-
 class TrigToTmpltJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_trigtotmplt job used by the inspiral pipeline. The static
+  arguments are read from the section [trigtotmplt] in the ini file.  The
+  stdout and stderr from the job are directed to the logs directory. The job
+  always runs in the scheduler universe. The path to the executable is
+  determined from the ini file.
+  """
   def __init__(self,cp):
-    pipeline.CondorDAGJob.__init__(self,'lalapps_trigtotmplt','scheduler')
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__executable = cp.get('condor','trigtotmplt')
+    self.__universe = 'scheduler'
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
     pipeline.AnalysisJob.__init__(self,cp)
     
     for sec in ['trigtotmplt']:
@@ -84,9 +127,20 @@ class TrigToTmpltJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('trigtotmplt.sub')
 
 
-
 class IncaJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_inca job used by the inspiral pipeline. The static arguments are
+  read from the section [inca] in the ini file.  The stdout and stderr from
+  the job are directed to the logs directory. The job always runs in the
+  scheduler universe. The path to the executable is determined from the ini
+  file.
+  """
   def __init__(self,cp):
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__universe = 'scheduler'
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
     pipeline.CondorDAGJob.__init__(self,'lalapps_inca','scheduler')
     pipeline.AnalysisJob.__init__(self,cp)
     
@@ -98,48 +152,85 @@ class IncaJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('inca.sub')
 
 
-
 class DataFindNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
+  """
+  A DataFindNode runs an instance of datafind in a Condor DAG.
+  """
   def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of LALdataFind.
+    """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
     self.__start = 0
     self.__end = 0
     self.__instrument = None
-    self.__cache = None
+    self.__output = None
    
+  def __set_output(self):
+    """
+    Private method to set the file to write the cache to. Automaticaly set
+    once the ifo, start and end times have been set.
+    """
+    if self.__start and self.__end and self.__instrument:
+      self.__output = 'cache/' + self.__instrument + '-' + str(self.__start) 
+      self.__output = self.__output + '-' + str(self.__end) + '.cache'
+      self.job().set_stdout_file(self.__output)
+
   def set_start(self,time):
+    """
+    Set the start time of the datafind query.
+    time = GPS start time of query.
+    """
     self.add_var('start', time)
     self.__start = time
-    self.set_cache()
+    self.__set_output()
 
   def set_end(self,time):
+    """
+    Set the end time of the datafind query.
+    time = GPS end time of query.
+    """
     self.add_var('end', time)
     self.__end = time
-    self.set_cache()
+    self.__set_output()
 
   def set_ifo(self,ifo):
+    """
+    Set the IFO to retrieve data for. Since the data from both Hanford 
+    interferometers is stored in the same frame file, this takes the first 
+    letter of the IFO (e.g. L or H) and passes it to the --instrument option
+    of LALdataFind.
+    ifo = IFO to obtain data for.
+    """
     self.add_var('instrument',ifo[0])
     self.__instrument = ifo[0]
-    self.set_cache()
-
-  def set_cache(self):
-    if self.__start and self.__end and self.__instrument:
-      self.__cache = self.__instrument + '-' + str(self.__start) 
-      self.__cache = self.__cache + '-' + str(self.__end) + '.cache'
-      self._CondorDAGNode__vars['frame-cache'] = self.__cache
+    self.__set_output()
 
   def get_output(self):
-    return self.__cache
-
+    """
+    Return the output file, i.e. the file containing the frame cache data.
+    """
+    return self.__output
 
 
 class TmpltBankNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
+  """
+  A TmpltBankNode runs an instance of the template bank generation job in a
+  Condor DAG.
+  """
   def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_tmpltbank.
+    """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
 
   def get_output(self):
+    """
+    Returns the file name of output from the template bank code. This must
+    be kept synchronized with the name of the output file in tmpltbank.c.
+    """
     if not self.get_start() or not self.get_end() or not self.get_ifo():
       raise InspiralError, "Start time, end time or ifo has not been set"
     bank = self.get_ifo() + '-TMPLTBANK-' + str(self.get_start())
@@ -147,54 +238,80 @@ class TmpltBankNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     return bank
 
 
-
 class InspiralNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
+  """
+  An InspiralNode runs an instance of the inspiral code in a Condor DAG.
+  """
   def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_inspiral.
+    """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
     self.__output = None
 
-  def set_ifo(self,ifo):
-    self._AnalysisNode__ifo = ifo
-    self.add_var('channel-name', ifo + ':' + self.job().channel())
-    self.add_var('calibration-cache', self.job().calibration(ifo))
-    
   def set_bank(self,bank):
     self.add_var('bank-file', bank)
-    out = self.get_ifo() + '-INSPIRAL-' + str(self.get_start()) + '-'
-    self.__output = out + str(self.get_end() - self.get_start()) + '.xml'
 
   def get_output(self):
-     return self.__output
-
+    """
+    Returns the file name of output from the inspiral code. This must be kept
+    synchronized with the name of the output file in inspiral.c.
+    """
+    if not self.get_start() or not self.get_end() or not self.get_ifo():
+      raise InspiralError, "Start time, end time or ifo has not been set"
+    out = self.get_ifo() + '-INSPIRAL-' + str(self.get_start()) + '-'
+    return out + str(self.get_end() - self.get_start()) + '.xml'
 
 
 class TrigToTmpltNode(pipeline.CondorDAGNode,pipeline.AnalysisNode):
+  """
+  A TrigToTmpltNode runs an instance of the triggered bank generator in a
+  Condor DAG.
+  """
   def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_trigtotmplt.
+    """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
 
 
-
 class IncaNode(pipeline.CondorDAGNode,pipeline.AnalysisNode):
+  """
+  An IncaNode runs an instance of the inspiral coincidence code in a Condor
+  DAG.
+  """
   def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_inca.
+    """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
     self.__input_a = []
     self.__input_b = []
     
   def add_input_a(self, file):
+    """
+    Add a file as input for IFO A.
+    file = path to XML file for IFO A.
+    """
     self.__input_a.append(file)
     self.add_var('input-a', ' '.join(self.__input_a))
 
   def add_input_b(self, file):
+    """
+    Add a file as input for IFO B.
+    file = path to XML file for IFO B.
+    """
     self.__input_b.append(file)
     self.add_var('input-b', ' '.join(self.__input_b))
 
   def set_output(self,string):
-    self.__output = string + '-INCA-' + str(self.get_start()) + '-'
-    self.__output = self.__output + str(self.get_end() - self.get_start())
-    self.__output = self.__output + '.xml'
-
-  def get_output(self):
-    return self.__output
+    """
+    Name the inca output file based on the standard naming convention with
+    string at the start for the ifos.
+    """
+    out = string + '-INCA-' + str(self.get_start()) + '-'
+    out = out + str(self.get_end() - self.get_start()) + '.xml'
+    pipeline.AnalysisNode.set_output(self,out)
