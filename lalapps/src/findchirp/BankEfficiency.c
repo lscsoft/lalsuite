40ce27a110f1ca3272601431efe044eecd4888ce
sathya <sathya>
2003-11-07 16:55:59 +0000

    New BankEfficiency code that makes full use of the new interface to bank codes. Original: 30de3b43e3d88d3f8f30b7f2274018669ecfaf03

--- lalapps/src/findchirp/BankEfficiency.c
+++ lalapps/src/findchirp/BankEfficiency.c
@@ -89,7 +89,6 @@ LALInspiralParameterCalc
 #include <lal/AVFactories.h>
 #include <lal/SeqFactories.h>
  
-void  LALInspiralCreateFlatBank(LALStatus *status, REAL4VectorSequence *list, InspiralBankParams *bankParams);
 void printf_timeseries (INT4 n, REAL4 *signal, REAL8 delta, REAL8 t0);
 
 INT4 lalDebugLevel=0;
@@ -97,82 +96,120 @@ INT4 lalDebugLevel=0;
 int 
 main (  int argc, char **argv ) 
 {
-   REAL4VectorSequence *list=NULL;
    static LALStatus status;
-   static InspiralBankParams bankParams;
 	   
-   INT4 ntrials, i, j, nlist, jmax, approx;
+   INT4 ntrials, i, j, nlist, jmax, approx=0, template=8;
    REAL8 df, omax;
 /*
    REAL8 dt0, dt1, g00, g11, h00, h11, h01, ang, match;
 */
    REAL4Vector signal, correlation;
-   UINT4 numFcutTemplates = 5, quietFlag = 0;
+   UINT4 quietFlag = 0;
    void *noisemodel = LALLIGOIPsd;
    static RandomInspiralSignalIn randIn;
    static InspiralWaveOverlapIn overlapin;
    static InspiralWaveOverlapOut overlapout, overlapoutmax;
-   static CreateVectorSequenceIn in; 
-   REAL8FrequencySeries shf;
-   static InspiralMetric metric;
    RealFFTPlan *fwdp=NULL,*revp=NULL;
-   static InspiralTemplateList *tmpltList=NULL;
-   
+   InspiralTemplateList *list=NULL;
+   static InspiralCoarseBankIn  coarseIn; 
    
    quietFlag = 0;	
-   numFcutTemplates  = 5;
-   randIn.useed = 128092;
    ntrials=2;
-   randIn.mMin = 5.0;
-   randIn.mMax = 20.0;
-   randIn.param.fLower = 60;
-   randIn.param.alpha = 0;  
-   randIn.param.fendBCV = 300;
+   approx = 0;
+  
+   coarseIn.fLower = 40.L;
+   coarseIn.fUpper = 1000.L;
+   coarseIn.tSampling = 4096.L;
+   coarseIn.order = twoPN;
+   coarseIn.space = Tau0Tau3;
+   coarseIn.mmCoarse = 0.95;
+   coarseIn.mmFine = 0.97;
+   coarseIn.iflso = 0.0L;
+   coarseIn.mMin = 5.0;
+   coarseIn.mMax = 20.0;
+   coarseIn.MMax = coarseIn.mMax * 2.;
+   coarseIn.massRange = MinMaxComponentMass; 
+   /* coarseIn.massRange = MinComponentMassMaxTotalMass;*/
+   /* minimum value of eta */
+   coarseIn.etamin = coarseIn.mMin * ( coarseIn.MMax - coarseIn.mMin) / pow(coarseIn.MMax,2.);
+   coarseIn.psi0Min = 1.e0;
+   coarseIn.psi0Max = 2.5e5;
+   coarseIn.psi3Min = -2.2e3;
+   coarseIn.psi3Max = 1.e1;
+   coarseIn.alpha = 0.L;
+   coarseIn.numFcutTemplates = 5;
+   coarseIn.approximant = TaylorT1;
+
+   randIn.useed = 128092;
    randIn.type = 0;
    randIn.SignalAmp = 10.0;
    randIn.param.approximant = PadeT1;
-   bankParams.minimalMatch = 0.80;
-   approx = 0;
-   randIn.param.order = 4;
-   
-   bankParams.x0Min = 0.0;
-   bankParams.x0Max = 2.5e5;
-   bankParams.x1Min = -2.2e3;
-   bankParams.x1Max = 0.0;
-
+   randIn.param.order = twoPN;
+   /*
+   randIn.param.fendBCV = 300;
+   */
+   randIn.param.startTime=0.0; 
+   randIn.param.startPhase=0.88189; 
+   randIn.param.nStartPad=2000;
+   randIn.param.signalAmplitude = 1.0;
+   randIn.param.nEndPad = 2000;
 
    i=1;
    while(i <argc)
    {
 	   if (strcmp(argv[i],"-fl")==0)
-		   randIn.param.fLower = atof(argv[++i]);
+		   coarseIn.fLower = atof(argv[++i]);
 	   else if (strcmp(argv[i],"-mMin")==0)
-		   randIn.mMin = atof(argv[++i]); 
+		   coarseIn.mMin = atof(argv[++i]); 
+	   else if (strcmp(argv[i],"-mMax")==0)
+		   coarseIn.mMax = atoi(argv[++i]);
+	   else if (strcmp(argv[i],"-x0Max")==0)
+		   coarseIn.psi0Max =atof(argv[++i]);
+	   else if (strcmp(argv[i],"-x1Min")==0)
+		   coarseIn.psi3Min =atof(argv[++i]);
+	   else if (strcmp(argv[i],"-mm")==0)
+		   coarseIn.mmCoarse =atof(argv[++i]);
 	   else if (strcmp(argv[i],"-numFcut")==0)
-		   numFcutTemplates = atof(argv[++i]); 
+		   coarseIn.numFcutTemplates = atof(argv[++i]); 
 	   else if (strcmp(argv[i],"-simType")==0)
 		   randIn.type = atoi(argv[++i]);
-	   else if (strcmp(argv[i],"-quiet")==0)
-		   quietFlag = 1;
-	   else if (strcmp(argv[i],"-mMax")==0)
-		   randIn.mMax = atoi(argv[++i]);
 	   else if (strcmp(argv[i],"-sigAmp")==0)
 		   randIn.SignalAmp = atof(argv[++i]);
 	   else if (strcmp(argv[i],"-alpha")==0)
-		   randIn.param.alpha = atof(argv[++i]); 
+		   coarseIn.alpha = atof(argv[++i]); 
 	   else if (strcmp(argv[i],"-order")==0)
 		   randIn.param.order = atoi(argv[++i]); 
 	   else if (strcmp(argv[i],"-n")==0)
 		   ntrials = atoi(argv[++i]);       
 	   else if (strcmp(argv[i],"-seed")==0)
 		   randIn.useed = atoi(argv[++i])+1;
-	   else if (strcmp(argv[i],"-x0Max")==0)
-		   bankParams.x0Max =atof(argv[++i]);
-	   else if (strcmp(argv[i],"-x1Min")==0)
-		   bankParams.x1Min =atof(argv[++i]);
-	   else if (strcmp(argv[i],"-mm")==0)
-		   bankParams.minimalMatch =atof(argv[++i]);
-	   else if (strcmp(argv[i], "-approximant")==0)
+	   else if (strcmp(argv[i],"-quiet")==0)
+		   quietFlag = 1;
+	   else if (strcmp(argv[i], "-template")==0)
+	   {
+		   if (strcmp(argv[++i],"TaylorT1")==0)
+			   template = 0;
+		   else if (strcmp(argv[i],"TaylorT2")==0)
+			   template = 1;
+		   else if (strcmp(argv[i],"TaylorT3")==0)
+			   template = 2;
+		   else if (strcmp(argv[i],"TaylorF1")==0)
+			   template = 3;
+		   else if (strcmp(argv[i],"TaylorF2")==0)
+			   template = 4;
+		   else if (strcmp(argv[i],"PadeT1")==0)
+			   template = 5;
+		   else if (strcmp(argv[i],"PadeF1")==0)
+			   template = 6;
+		   else if (strcmp(argv[i],"EOB")==0)
+			   template = 7;
+		   else if (strcmp(argv[i],"BCV")==0)
+			   template = 8;
+		   else if (strcmp(argv[i],"SpinTaylorT3")==0)
+			   template = 9;
+		   coarseIn.approximant = template;	
+	   }	
+	   else if (strcmp(argv[i], "-approximant")==0 || (strcmp(argv[i], "-signal")==0))
 	   {
 		   if (strcmp(argv[++i],"TaylorT1")==0)
 			   approx = 0;
@@ -200,21 +237,23 @@ main (  int argc, char **argv )
 	   {
 		   fprintf(stderr,"\nUSAGE: %s [options]\n", argv[0]);
 		   fprintf(stderr,"The options are (with default values in brackets)\n");
-		   fprintf(stderr,"      -alpha : BCV amplitude correction parameter (%7.2f)\n",randIn.param.alpha);
-		   fprintf(stderr,"-approximant : Post-Newtonian model such as TaylorT1, PadeT1, EOB, BCV ...  (PadeT1)\n");
-		   fprintf(stderr,"         -fl : lower frequency cutoff             (%7.2f) Hz\n", randIn.param.fLower);
-		   fprintf(stderr,"       -mMin : minimal mass of component stars    (%7.2f) SolarMass\n", randIn.mMin);
-		   fprintf(stderr,"       -mMax : maximal mass of component stars    (%7.2f) SolarMass\n", randIn.mMax);
-		   fprintf(stderr,"         -mm : minimal match for template bank    (%7.3f)\n", bankParams.minimalMatch);
-		   fprintf(stderr,"          -n : number of trials                   (%d)\n", ntrials);
-		   fprintf(stderr,"    -numFcut : number of layers in Fcut dimension (%7.2d)\n", numFcutTemplates);
-		   fprintf(stderr,"      -order : order of PN model                  (%7.2d)\n", randIn.param.order);
 		   fprintf(stderr,"      -quiet : if this flag is present, the output is restricted to the minimum\n");
-		   fprintf(stderr,"       -seed : seed for random generation         (%d)\n", randIn.useed);
-		   fprintf(stderr,"     -sigAmp : amplitude of the signal            (%7.2f)\n", randIn.SignalAmp);
-		   fprintf(stderr,"    -simType : type of simulation, 0, 1 or 2      (%d)\n\n", randIn.type);
-		   fprintf(stderr,"      -x0Max : Max value of psi0 (%7.2f)\n\n", bankParams.x0Max);
-		   fprintf(stderr,"      -x1Min : Min value of psi  (%7.2f)\n\n", bankParams.x1Min);
+		   fprintf(stderr,"          -n : number of trials                   (%d)\n",       ntrials);
+		   fprintf(stderr,"         -fl : lower frequency cutoff             (%7.2f) Hz\n", coarseIn.fLower);
+		   fprintf(stderr,"       -mMin : minimal mass of component stars    (%7.2f) Mo\n", coarseIn.mMin);
+		   fprintf(stderr,"       -mMax : maximal mass of component stars    (%7.2f) Mo\n", coarseIn.mMax);
+		   fprintf(stderr,"      -x0Max : Max value of psi0 (%7.2f)\n\n",                   coarseIn.psi0Max);
+		   fprintf(stderr,"      -x1Min : Min value of psi3 (%7.2f)\n\n",                   coarseIn.psi3Min);
+		   fprintf(stderr,"      -alpha : BCV amplitude correction parameter (%7.2f)\n",    coarseIn.alpha);
+		   fprintf(stderr,"    -numFcut : number of layers in Fcut dimension (%7.2d)\n",    coarseIn.numFcutTemplates);
+		   fprintf(stderr,"         -mm : minimal match for template bank    (%7.3f)\n",    coarseIn.mmCoarse);
+		   fprintf(stderr,"    -simType : type of simulation, 0, 1 or 2      (%d)\n\n",     randIn.type);
+		   fprintf(stderr,"-approximant : PN model for Monte-Carlo (TaylorT1, ...) (%d)\n", randIn.param.approximant);
+		   fprintf(stderr,"     -signal : same as -approximant\n");
+		   fprintf(stderr,"   -template : PN model for template bank (TaylorT1, ...) (%d)\n", coarseIn.approximant);
+		   fprintf(stderr,"      -order : order of PN model                  (%7.2d)\n",    randIn.param.order);
+		   fprintf(stderr,"       -seed : seed for random generation         (%d)\n",       randIn.useed);
+		   fprintf(stderr,"     -sigAmp : amplitude of the signal            (%7.2f)\n",    randIn.SignalAmp);
 		   return 1;	
 
 	   }
@@ -240,20 +279,18 @@ main (  int argc, char **argv )
 /*---------------------------------------------------------------------------*/
    randIn.NoiseAmp = 1.0;
 
-   randIn.param.startTime=0.0; 
-   randIn.param.startPhase=0.88189; 
-   randIn.param.nStartPad=2000;
-   randIn.psi0Min = bankParams.x0Min;
-   randIn.psi0Max = bankParams.x0Max;
-   randIn.psi3Max = bankParams.x1Max;
-   randIn.psi3Min = bankParams.x1Min;
-
-   randIn.param.fCutoff = 1000.;
-   randIn.param.tSampling = 2048;
-   randIn.param.signalAmplitude = 1.0;
-   randIn.param.nEndPad = 2000;
-   randIn.param.massChoice = m1Andm2;
+   randIn.mMin = coarseIn.mMin;
+   randIn.mMax = coarseIn.mMax;
    randIn.MMax = 2.*randIn.mMax;
+   randIn.param.fLower = coarseIn.fLower;
+   randIn.param.alpha = coarseIn.alpha;
+   randIn.psi0Min = coarseIn.psi0Min;
+   randIn.psi0Max = coarseIn.psi0Max;
+   randIn.psi3Min = coarseIn.psi3Min;
+   randIn.psi3Max = coarseIn.psi3Max;
+   randIn.param.fCutoff = coarseIn.fUpper;
+   randIn.param.tSampling = coarseIn.tSampling;
+   randIn.param.massChoice = m1Andm2;
    randIn.param.mass1 = randIn.mMin;
    randIn.param.mass2 = randIn.mMin;
 
@@ -268,60 +305,34 @@ main (  int argc, char **argv )
    randIn.psd.length = signal.length/2 + 1;
    signal.data = (REAL4*) LALMalloc(sizeof(REAL4)*signal.length);
    correlation.data = (REAL4*) LALMalloc(sizeof(REAL4)*correlation.length);
-   memset( &(shf), 0, sizeof(REAL8FrequencySeries) );
-   shf.f0 = 0;
-   LALDCreateVector( &status, &(shf.data), randIn.psd.length );
-   shf.deltaF = randIn.param.tSampling / signal.length;
+   memset( &(coarseIn.shf), 0, sizeof(REAL8FrequencySeries) );
+   coarseIn.shf.f0 = 0;
+   LALDCreateVector( &status, &(coarseIn.shf.data), randIn.psd.length );
+   coarseIn.shf.deltaF = randIn.param.tSampling / signal.length;
    randIn.psd.data = (REAL8*) LALMalloc(sizeof(REAL8)*randIn.psd.length);
    df = randIn.param.tSampling/(float) signal.length;
-   LALNoiseSpectralDensity (&status, shf.data, noisemodel, df);
-	   
-   /*---------------------------------------------------------------------------*/
-   /* Prepare parameters needed to create a verctor sequence to store BCV templates */
-   /*---------------------------------------------------------------------------*/
-   in.length = 1;
-   in.vectorLength = 2;
-   LALSCreateVectorSequence(&status, &list, &in);
-   list->vectorLength = 2;
-   LALInspiralComputeMetricBCV(&status, &metric, &shf, &randIn.param);
-   bankParams.metric = &metric;
-   LALInspiralCreateFlatBank(&status, list, &bankParams);
-   nlist = list->length;
-
-   tmpltList = (InspiralTemplateList *) LALMalloc (sizeof (InspiralTemplateList) * nlist);
-
-   /* Print out the template parameters */
-   for (i=0; i<nlist; i++)
-   {
-   	/*Retain only those templates that have meaningful chirptimes*/
-   	tmpltList[i].params.psi0 = (REAL8) list->data[2*i];
-   	tmpltList[i].params.psi3 = (REAL8) list->data[2*i+1];
-   	tmpltList[i].params.fLower = randIn.param.fLower;
-   	tmpltList[i].params.nStartPad = randIn.param.nStartPad;
-   	tmpltList[i].params.startPhase = randIn.param.startPhase;
-	tmpltList[i].params.nEndPad = randIn.param.nEndPad;
-	tmpltList[i].params.tSampling = randIn.param.tSampling;
-	tmpltList[i].params.fendBCV = randIn.param.fendBCV;
-	tmpltList[i].params.massChoice = psi0Andpsi3;
-	tmpltList[i].params.approximant = BCV;
-   }
-
-	LALInspiralBCVFcutBank( &status, &tmpltList, &nlist, numFcutTemplates) ;
-
+   LALNoiseSpectralDensity (&status, coarseIn.shf.data, noisemodel, df);
+   LALInspiralCreateCoarseBank(&status,&list, &nlist, coarseIn);
    if (nlist==0) exit(0);
-   if (!quietFlag){
+	   
+   if (!quietFlag)
+   {
    for (i=0; i<nlist; i++)
    {
-	   fprintf(stdout, "%e %e %e %e\n", 
-			   tmpltList[i].params.psi0, 
-			   tmpltList[i].params.psi3, 
-			   tmpltList[i].params.totalMass, 
-			   tmpltList[i].params.fendBCV);
+	   if (coarseIn.approximant == BCV)
+	   {
+	   
+		   fprintf(stdout, "%e %e %e %e\n", list[i].params.psi0, list[i].params.psi3, list[i].params.totalMass, list[i].params.fendBCV);
+	   }
+	   else
+	   {
+		   fprintf(stdout, "%e %e %e %e\n", list[i].params.mass1, list[i].params.mass2, list[i].params.t0, list[i].params.t3);
+	   }
    }
    }
 /* REPORTSTATUS(&status); */
 
-   randIn.psd = *shf.data;
+   randIn.psd = *coarseIn.shf.data;
    overlapin.psd = randIn.psd;
 /*--------------------------
    Estimate the plans 
@@ -333,16 +344,24 @@ main (  int argc, char **argv )
    overlapin.fwdp = randIn.fwdp = fwdp;
    overlapin.revp = revp;
    
-   if (!quietFlag){
+   /* if (!quietFlag) */
+   {
    	fprintf(stdout, "#---------------------------------------------------------------\n");
 	fprintf(stdout, "#Number of Coarse Bank Templates=%d\n",nlist);
    	fprintf(stdout, "#Signal Length=%d, Number of sims=%d\n", signal.length, ntrials);
-   	fprintf(stdout, "#psi0Min=%e, psi0Max=%e, psi3Min=%e, psi3Max=%e\n",
-		   randIn.psi0Min,randIn.psi0Max,randIn.psi3Min,randIn.psi3Max);
 	fprintf(stdout,"---------------------------------------------------------------\n");
-   	fprintf(stdout, "#psi0Tmplt   psi0Sig    psi3Tmplt    psi3Sig    fFinalTmplt   fFinalSig  m1 m2 Overlap/SNR\n");
-   	fprintf(stdout,"---------------------------------------------------------------\n");
+   	if (coarseIn.approximant==BCV)
+	{
+		fprintf(stdout, "#psi0Min=%e, psi0Max=%e, psi3Min=%e, psi3Max=%e, signal=%d, template=%d\n", coarseIn.psi0Min,coarseIn.psi0Max,coarseIn.psi3Min,coarseIn.psi3Max,approx,template);
+		fprintf(stdout, "#psi0Tmplt   psi0Sig    psi3Tmplt    psi3Sig    fFinalTmplt   fFinalSig  m1 m2 Overlap/SNR\n");
 	}
+	else
+	{
+		fprintf(stdout, "#mMin=%e, mMax=%e, signal=%d, template=%d\n", coarseIn.mMin,coarseIn.mMax,approx,template);
+		fprintf(stdout, "#m1Tmplt   m1Sig    m2Tmplt    m2Sig    fFinalTmplt   fFinalSig  Overlap/SNR\n");
+	}
+   	fprintf(stdout,"---------------------------------------------------------------\n");
+   }
    
    while (ntrials--) 
    {
@@ -360,14 +379,14 @@ main (  int argc, char **argv )
 	      
       for (j=0; j<nlist; j++) 
       {
-     	      overlapin.param = tmpltList[j].params;
+     	      overlapin.param = list[j].params;
 	      if (overlapin.param.approximant==BCV) 
-		      overlapin.param.fCutoff = tmpltList[j].params.fendBCV;
+		      overlapin.param.fCutoff = list[j].params.fendBCV;
 	      else
 		      overlapin.param.fCutoff = randIn.param.fCutoff;
 
 	      LALInspiralWaveOverlap(&status,&correlation,&overlapout,&overlapin);
-              tmpltList[j].params.fFinal = overlapin.param.fFinal;
+              list[j].params.fFinal = overlapin.param.fFinal;
 	      
 	      if (omax < overlapout.max) 
 	      {
@@ -377,34 +396,47 @@ main (  int argc, char **argv )
 	      }
       }
 
-      LALInspiralParameterCalc(&status, &tmpltList[jmax].params);
-      fprintf(stdout, "%e %e %e %e %e %e %e %e %e %e %e\n", 
-		      tmpltList[jmax].params.psi0, 
+      LALInspiralParameterCalc(&status, &list[jmax].params);
+      if (coarseIn.approximant==BCV)
+	      fprintf(stdout, "%e %e %e %e %e %e %e %e %e %e %e\n", 
+		      list[jmax].params.psi0, 
 		      randIn.param.psi0, 
-                      tmpltList[jmax].params.psi3,
+                      list[jmax].params.psi3,
 		      randIn.param.psi3, 
-		      tmpltList[jmax].params.fFinal, 
+		      list[jmax].params.fFinal, 
 		      randIn.param.fFinal,
-		      tmpltList[jmax].params.totalMass,
+		      list[jmax].params.totalMass,
 		      randIn.param.totalMass,
 		      randIn.param.mass1,
 		      randIn.param.mass2,
+		      omax);
+      else
+	      fprintf(stdout, "%e %e %e %e %e %e %e %e %e %e %e\n", 
+		      list[jmax].params.mass1, 
+		      randIn.param.mass1, 
+                      list[jmax].params.mass2,
+		      randIn.param.mass2, 
+                      list[jmax].params.totalMass,
+		      randIn.param.totalMass, 
+                      list[jmax].params.eta,
+		      randIn.param.eta, 
+		      list[jmax].params.fFinal, 
+		      randIn.param.fFinal,
 		      omax);
       fflush(stdout);
    }
    
    /* destroy the plans, correlation and signal */
 
-   /*
-      LALDDestroyVector( &status, &(shf.data) );
+   
+      LALDDestroyVector( &status, &(coarseIn.shf.data) );
       if (signal.data != NULL) LALFree(signal.data);
       if (correlation.data != NULL) LALFree(correlation.data);
       if (randIn.psd.data != NULL) LALFree(randIn.psd.data);
-      if (list!= NULL) LALFree(list);
       LALDestroyRealFFTPlan(&status,&fwdp);   
       LALDestroyRealFFTPlan(&status,&revp);
       LALCheckMemoryLeaks();
-    */
+    
    
    return(0);
 
