2d9b15673d71f40432c83334fe45dd861ec157f3
thomas <thomas>
2003-12-02 16:36:33 +0000

    In order to integrate waveforms from inspiral pacakge, I rewrote completely the GenerateInspiral.c and .h which are now the interface to the duncan's code in lalapps in the directory ./insprial and ./findchirp Original: a1150b5fa3a1b6a431a9746920aa1054be0dc495

--- lal/packages/inject/include/GenerateInspiral.h
+++ lal/packages/inject/include/GenerateInspiral.h
@@ -6,6 +6,7 @@
 #include <lal/GeneratePPNInspiral.h>
 #include <lal/GenerateSpinOrbitCW.h>
 #include <lal/GenerateTaylorCW.h>
+#include <lal/LIGOMetadataTables.h>
 
 
 #ifdef  __cplusplus
@@ -18,37 +19,32 @@ NRCSID( GENERATEINSPIRALH, "$Id$" );
 
 /* A reference number for the method already implemented in the injection package*/
 typedef enum {  
-   PPN       = 101,
-   SpinOrbitCW      = 102,
-   TaylorCW  = 103
+   PPN       	= 101,
+   SpinOrbitCW	= 102,
+   TaylorCW  	= 103
  } Method;
 
 
 
-/* Structure which uses the structures already defined in the inspiral package
- and injection package. */
-typedef struct 
-tagGeneralInspiralStruc
-{
-  PPNParamStruc          ppn;
-  SpinOrbitCWParamStruc  socw;
-  TaylorCWParamStruc     taylorcw;
-  InspiralTemplate       inspiral;
-  Method method;
-} GeneralInspiralStruc;
+/**/
+void
+LALInjectInspiral(
+		    LALStatus        *status,
+		    CoherentGW       *waveform,
+		    SimInspiralTable  *params, 
+		    REAL4 *buf1, 
+		    REAL8 *buf2,
+		    UINT4 *buf3
+		    );
 
 
 /**/
 void
-LALGenerateInspiral(
-		    LALStatus        *status,
-		    CoherentGW       *waveform,
-		    GeneralInspiralStruc  *params
-		    );
-
-
-
-
+LALGetApproximantAndOrder(
+			  LALStatus *status,
+			  CHAR *waveform,
+			  UINT4 *order,
+			  UINT4 *approximant);
 #ifdef  __cplusplus
 #pragma {
 }
--- lal/packages/inject/src/GenerateInspiral.c
+++ lal/packages/inject/src/GenerateInspiral.c
@@ -7,115 +7,253 @@
 
 
 
-NRCSID( GENERATEINSPIRALC, "$Id$" );
+NRCSID( INJECTINSPIRALC, "$Id$" );
 
-#define GENERATEINSPIRALC_ENORM 0
-#define GENERATEINSPIRALC_ESUB  1
-#define GENERATEINSPIRALC_EARG  2
-#define GENERATEINSPIRALC_EVAL  3
-#define GENERATEINSPIRALC_EFILE 4
-#define GENERATEINSPIRALC_EMEM  5
+#define INJECTINSPIRALC_ENORM 0
+#define INJECTINSPIRALC_ESUB  1
+#define INJECTINSPIRALC_EARG  2
+#define INJECTINSPIRALC_EVAL  3
+#define INJECTINSPIRALC_EFILE 4
+#define INJECTINSPIRALC_EMEM  5
+#define INJECTINSPIRALC_EAPPROXIMANT  6
+#define INJECTINSPIRALC_EORDER  7
 
-#define GENERATEINSPIRALC_MSGENORM "Normal exit"
-#define GENERATEINSPIRALC_MSGESUB  "Subroutine failed"
-#define GENERATEINSPIRALC_MSGEARG  "Error parsing arguments"
-#define GENERATEINSPIRALC_MSGEVAL  "Input argument out of valid range"
-#define GENERATEINSPIRALC_MSGEFILE "Could not open file"
-#define GENERATEINSPIRALC_MSGEMEM  "Out of memory"
+#define INJECTINSPIRALC_MSGENORM "Normal exit"
+#define INJECTINSPIRALC_MSGESUB  "Subroutine failed"
+#define INJECTINSPIRALC_MSGEARG  "Error parsing arguments"
+#define INJECTINSPIRALC_MSGEVAL  "Input argument out of valid range"
+#define INJECTINSPIRALC_MSGEFILE "Could not open file"
+#define INJECTINSPIRALC_MSGEMEM  "Out of memory"
+#define INJECTINSPIRALC_MSGEAPPROXIMANT  "Error parsing order"
+#define INJECTINSPIRALC_MSGEORDER  "Error parsing approximant"
 
-static void
-ClearStatus (LALStatus *status);
+
+
+#define INJECTINSPIRAL_APPROXIMANT PPN
+#define INJECTINSPIRAL_M1          1.4
+#define INJECTINSPIRAL_M2         1.4
+#define INJECTINSPIRAL_DISTANCE   10  /*mpc*/
+#define INJECTINSPIRAL_TC          0.
+#define INJECTINSPIRAL_INCLINATION 0.
+#define INJECTINSPIRAL_DEC         0.
+#define INJECTINSPIRAL_RA          0.
+#define INJECTINSPIRAL_PSI         0.
+#define INJECTINSPIRAL_PHIC        0.
+#define INJECTINSPIRAL_FLOWER     40.
+#define INJECTINSPIRAL_FUPPER   ((INJECTINSPIRAL_SAMPLING-1)/2.)   /*must be < sampling/2*/
+#define INJECTINSPIRAL_ZETA2       0.
+#define INJECTINSPIRAL_OMEGAS      0.
+#define INJECTINSPIRAL_THETA       0.
+#define INJECTINSPIRAL_T0          0.
+#define INJECTINSPIRAL_T1          1.
+#define INJECTINSPIRAL_T2          2.
+#define INJECTINSPIRAL_SAMPLING 2048.
+#define INJECTINSPIRAL_A1          1000.
+#define INJECTINSPIRAL_A2          1000.
+#define INJECTINSPIRAL_PHI0        0.
+#define INJECTINSPIRAL_F0         100.
+#define INJECTINSPIRAL_ARG         0
+#define INJECTINSPIRAL_UDOT        0.5
+#define INJECTINSPIRAL_RP          0.5
+#define INJECTINSPIRAL_E           0.   
+#define INJECTINSPIRAL_ALPHA       0.
+#define INJECTINSPIRAL_FBCV        INJECTINSPIRAL_FUPPER
+#define INJECTINSPIRAL_DELTAT      0  /*default use deltaT = 1/sampling*/
+#define INJECTINSPIRAL_LENGTH   1024  
+#define INJECTINSPIRAL_LENGTHIN  1e9
 
 
 
 void
-LALGenerateInspiral(
+LALInjectInspiral(
 		    LALStatus             *status,
 		    CoherentGW            *waveform,
-		    GeneralInspiralStruc  *params
+		    SimInspiralTable      *thisEvent,
+		    REAL4 *buffer1,
+		    REAL8 *buffer2,
+		    UINT4 *buffer3
 		    )
 {
 
-  /* System-derived constants. */
-  REAL4 mTot, mu;      /* total mass and reduced mass */
-  REAL4 eta, etaInv;   /* mass ratio and its inverse */
-  REAL4 phiC;          /* phase at coalescence */
-  REAL4 cosI;          /* cosine of system inclination */
-  REAL4 fFac;          /* SI normalization for f and t */
-  REAL4 f2aFac;        /* factor multiplying f in amplitude function */
-  REAL4 yOld,  apFac, acFac;  /* extra factor in plus and cross amplitudes */
-  INT4 padding = -1;
-  /* Integration parameters. */
+  /*
+  REAL8 
+    tc       = INJECTINSPIRAL_TC,
+    m1       = INJECTINSPIRAL_M1,
+    m2       = INJECTINSPIRAL_M2,
+    d        = INJECTINSPIRAL_DISTANCE,
+    inc      = INJECTINSPIRAL_INCLINATION,
+    ra       = INJECTINSPIRAL_RA,
+    dec      = INJECTINSPIRAL_DEC,
+    psi      = INJECTINSPIRAL_PSI,
+    phic     = INJECTINSPIRAL_PHIC,
+    fi       = INJECTINSPIRAL_FLOWER,
+    ff       = INJECTINSPIRAL_FUPPER,*/
+REAL8
+    zeta2    = INJECTINSPIRAL_ZETA2,
+    omegas   = INJECTINSPIRAL_OMEGAS,
+  theta    = INJECTINSPIRAL_THETA;
 
-  INT4  nMax;   /* index over timesteps, and its maximum + 1 */
-  CreateVectorSequenceIn in;
-
-
-  static REAL4Vector 
-    *inject_hp, 
-    *inject_hc, 
-    *inject_freq, 
-    *inject_phase;
+/*
+    t0       = INJECTINSPIRAL_T0,
+    t1       = INJECTINSPIRAL_T1,
+    t2       = INJECTINSPIRAL_T2,
+    a1       = INJECTINSPIRAL_A1,
+    a2       = INJECTINSPIRAL_A2,
+    phi0     = INJECTINSPIRAL_PHI0,
+    f0       = INJECTINSPIRAL_F0,
+    arg      = INJECTINSPIRAL_ARG,
+    udot     = INJECTINSPIRAL_UDOT,
+    rp       = INJECTINSPIRAL_RP,
+    e        = INJECTINSPIRAL_E,
+    alpha    = INJECTINSPIRAL_ALPHA,
+    fbcv     = INJECTINSPIRAL_FBCV,
+    sampling = INJECTINSPIRAL_SAMPLING,
+    deltaT   = INJECTINSPIRAL_DELTAT, 
+    length   = INJECTINSPIRAL_LENGTH;
   
-  REAL8 dt;
+*/
+
+
+  UINT4 
+    order  = -1,
+    approximant = -1;
+    /*    start=0,
+	  tail=0;*/
+  
+  /*
+  UINT4 i,
+    nspin=0;
+    
+
+
+  REAL8
+    fdata[128];
+  */
+
+
+  PPNParamStruc         ppnParams;
+  InspiralTemplate      inspiralParams;
+
+  /* System-derived constants. */
+
+
+  
+
   UINT4 n;
 
   /*  LALAssert(approximant);*/
 
-  INITSTATUS(status, "LALGenerateInspiral",GENERATEINSPIRALC);
+  INITSTATUS(status, "LALGenerateInspiral",INJECTINSPIRALC);
   ATTATCHSTATUSPTR(status);
+
+  LALGetApproximantAndOrder(status->statusPtr, 
+			    thisEvent->waveform, 
+			    &order ,
+			    &approximant);
   
+    CHECKSTATUSPTR(status);
 
-  dt = 1./params->inspiral.tSampling;
-  
-
-
-
-
-    
-  if (params->method == 0){
-    LALInspiralWaveLength(status->statusPtr, &n, params->inspiral);
-    ClearStatus(status);      
-    printf(" #     here length = %d et approx=%d\n", n, params->inspiral.approximant);
-    
-    
-    LALInspiralParameterCalc(status->statusPtr, &params->inspiral);
-    ClearStatus(status);      
-
-    
-    
-    LALCreateVector(status->statusPtr, &inject_hp, n);
-    ClearStatus(status);      
-    LALCreateVector(status->statusPtr, &inject_hc, n);
-    ClearStatus(status);      
-    LALCreateVector(status->statusPtr, &inject_freq, n);
-    ClearStatus(status);      
-    LALCreateVector(status->statusPtr, &inject_phase, n);
-    ClearStatus(status);      
+  if (order == -1)
+    {
+      ABORT(status, INJECTINSPIRALC_EORDER, INJECTINSPIRALC_MSGEORDER);
+    }
+  if (approximant == -1)
+    {
+      ABORT(status, INJECTINSPIRALC_EAPPROXIMANT, INJECTINSPIRALC_MSGEAPPROXIMANT);
+    }
 
 
+  /*  dt = 1./params->inspiral.tSampling;*/
   
 
 
-    switch(params->inspiral.approximant)
+  /* fill structures ppn ou inspiral */ 
+  switch (approximant)
+    {
+    case PPN:
+      ppnParams.mTot = thisEvent->mass1 + thisEvent->mass2;
+      ppnParams.eta  = thisEvent->eta;
+      ppnParams.d    = thisEvent->distance * 1.0e6 * LAL_PC_SI;
+      ppnParams.inc  = thisEvent->inclination; 
+      ppnParams.phi  = thisEvent->coa_phase;   
+      ppnParams.fStartIn = INJECTINSPIRAL_FLOWER;
+      ppnParams.fStopIn  = -1.0 / 
+	(6.0 * sqrt(6.0) * LAL_PI * ppnParams.mTot * LAL_MTSUN_SI); 
+
+
+      ppnParams.position.longitude   = thisEvent->longitude;
+      ppnParams.position.latitude    = thisEvent->latitude;  
+      ppnParams.position.system      = COORDINATESYSTEM_EQUATORIAL;
+      ppnParams.psi                  = thisEvent->polarization; 
+      ppnParams.epoch.gpsSeconds     = 0;
+      ppnParams.epoch.gpsNanoSeconds = 0;
+      ppnParams.ppn = NULL;
+      ppnParams.lengthIn = 0;
+      ppnParams.deltaT = *buffer1;
+
+      break;
+
+    case EOB:
+    case TaylorT1:
+    case TaylorT2:
+    case TaylorT3:
+      inspiralParams.ieta        = 1;
+      inspiralParams.mass1       = thisEvent->mass1;
+      inspiralParams.mass2       = thisEvent->mass2;
+      inspiralParams.startTime   = 0.0;
+      inspiralParams.startPhase  = 0.0;
+      inspiralParams.nStartPad   = 0;
+      inspiralParams.nEndPad     = 0;
+      inspiralParams.signalAmplitude = 1.; 
+      inspiralParams.distance    = thisEvent->distance * LAL_PC_SI * 1e6; 
+      inspiralParams.fLower      = INJECTINSPIRAL_FLOWER;
+      inspiralParams.fCutoff     = INJECTINSPIRAL_FUPPER;
+      inspiralParams.order       = order;
+      inspiralParams.inclination = thisEvent->inclination ;
+      inspiralParams.massChoice=m1Andm2;
+      inspiralParams.tSampling = 1./ (*buffer1);
+      inspiralParams.OmegaS = omegas;
+      inspiralParams.Theta  = theta;
+      inspiralParams.Zeta2  = zeta2;
+      inspiralParams.approximant  = approximant;
+      break;
+    }
+
+
+  /* si inspiral on affecte memoire */
+  switch  (approximant){
+  case EOB:
+  case TaylorT1:
+  case TaylorT2:
+  case TaylorT3:
+    LALInspiralWaveLength(status->statusPtr, &n, inspiralParams);
+    CHECKSTATUSPTR(status);      
+    LALInspiralParameterCalc(status->statusPtr, &inspiralParams);
+    CHECKSTATUSPTR(status);                  
+    break;
+  }
+
+  /*veritable computation c ici*/
+  switch(approximant)
       {
       case TaylorT1: 
 	LALInspiralWave1ForInjection(status->statusPtr, 
-				     inject_hc, 
-				     inject_hp,
-				     inject_phase, 
-				     inject_freq,
-				     &params->inspiral);
-	ClearStatus(status);      
+				     waveform,
+				     &inspiralParams);
+	*buffer1 = inspiralParams.tSampling;
+	*buffer2 = inspiralParams.tC;
+	*buffer3 = inspiralParams.nStartPad;
+	CHECKSTATUSPTR(status);      
 	break;
+	/*
       case TaylorT2: 
 	LALInspiralWave2ForInjection(status->statusPtr, 
 				     inject_hc, 
 				     inject_hp,
 				     inject_phase, 
 				     inject_freq,
-				     &params->inspiral);
-	ClearStatus(status);      
+				     &inspiralParams);
+	CHECKSTATUSPTR(status);      
 	break;
       case TaylorT3: 
 	LALInspiralWave3ForInjection(status->statusPtr, 
@@ -123,9 +261,9 @@ LALGenerateInspiral(
 				     inject_hp,
 				     inject_phase, 
 				     inject_freq,
-				     &params->inspiral);
-	ClearStatus(status);      
-	break;
+				     &inspiralParams);
+	CHECKSTATUSPTR(status);      
+	break;*/
       case TaylorF1:
       case TaylorF2:      
       case PadeT1:
@@ -135,190 +273,112 @@ LALGenerateInspiral(
       case SpinTaylorT3:
       case EOB:       
 	LALEOBWaveformForInjection(status->statusPtr, 
-				   inject_hc, 
-				   inject_hp,
-				   inject_phase, 
-				   inject_freq,
-				   &params->inspiral);
-	ClearStatus(status);      
+				   waveform,
+				   &inspiralParams);
+	*buffer1 = inspiralParams.tSampling;
+
+
+	*buffer2 = inspiralParams.tC;
+	*buffer3 = inspiralParams.nStartPad;
+	CHECKSTATUSPTR(status);      
 	break;
+      case PPN:
+	LALGeneratePPNInspiral(status->statusPtr, waveform, &ppnParams);	    	
+	CHECKSTATUSPTR(status);   
+	*buffer1 = ppnParams.dfdt;
+	*buffer2 = ppnParams.tc;
+	*buffer3 = ppnParams.length;
+
+	break;
+	/*      case SpinOrbitCW:	   
+	LALGenerateSpinOrbitCW(status->statusPtr, waveform, &params->socw);
+	CHECKSTATUSPTR(status);      
+	break;
+      case TaylorCW:
+	LALGenerateTaylorCW(status->statusPtr, waveform, &params->taylorcw);
+	CHECKSTATUSPTR(status);      
+	break;*/
       default: 
 	fprintf(stderr,"nothing to do (bad approximant?)");
 	break;
       }
-    
-    
-    
-   nMax = 0;
-   padding=0;    
-   /* on avance pour eviter le padding avant le signal*/
-   while (inject_freq->data[padding]==0) 
-     padding++;
-   /*recherche dichotomique du max peut etre*/
-   while (inject_freq->data[padding + nMax]!=0  &&  (padding+nMax) < inject_freq->length)
-     {
-       nMax++;
-     }
-
-
-  /*******************************************************************
-   * COMPUTE SYSTEM PARAMETERS                                       *
-   *******************************************************************/
-
-
-   etaInv = 2.0 / eta;
-   mTot = params->inspiral.mass1 +params->inspiral.mass2;
-   eta = params->inspiral.mass1 *params->inspiral.mass2;
-   eta/=mTot;
-   eta/=mTot;
-
-   mu = eta * mTot;
-   cosI = cos( params->inspiral.inclination );
-      
-   /* Compute frequency, phase, and amplitude factors. */
-   fFac = 1.0 / ( 4.0*LAL_TWOPI*LAL_MTSUN_SI*mTot );
-   dt = -1. * eta / ( params->inspiral.tSampling * 5.0*LAL_MTSUN_SI*mTot );      
-   f2aFac = LAL_PI*LAL_MTSUN_SI*mTot*fFac;   
-   apFac = acFac = -2.0*mu*LAL_MRSUN_SI/params->inspiral.distance;
-   apFac *= 1.0 + cosI*cosI;
-   acFac *= 2.0*cosI;
-   
-   
-
-  /*******************************************************************
-   * GENERATE WAVEFORM                                               *
-   *******************************************************************/
-
-   if ( ( waveform->a = (REAL4TimeVectorSeries *)
-	  LALCalloc(1, sizeof(REAL4TimeVectorSeries) ) ) == NULL ) {
-     ABORT( status->statusPtr, GENERATEPPNINSPIRALH_EMEM,
-	    GENERATEPPNINSPIRALH_MSGEMEM );
-   }
-   memset( waveform->a, 0, sizeof(REAL4TimeVectorSeries) );
-   if ( ( waveform->f = (REAL4TimeSeries *)
-	  LALCalloc(1, sizeof(REAL4TimeSeries) ) ) == NULL ) {
-
-     LALFree( waveform->a ); waveform->a = NULL;
-     ABORT( status->statusPtr, GENERATEPPNINSPIRALH_EMEM,
-	    GENERATEPPNINSPIRALH_MSGEMEM );
-   }
-   memset( waveform->f, 0, sizeof(REAL4TimeSeries) );
-   if ( ( waveform->phi = (REAL8TimeSeries *)
-	  LALCalloc(1, sizeof(REAL8TimeSeries) ) ) == NULL ) {
-     LALFree( waveform->a ); waveform->a = NULL;
-     LALFree( waveform->f ); waveform->f = NULL;
-     ABORT( status->statusPtr, GENERATEPPNINSPIRALH_EMEM,
-	    GENERATEPPNINSPIRALH_MSGEMEM );
-   }
-   ClearStatus(status);      
-   memset( waveform->phi, 0, sizeof(REAL8TimeSeries) );
-   
-   
-   
-  in.length = nMax;
-  in.vectorLength = 2;
-  LALSCreateVectorSequence( status->statusPtr, &( waveform->a->data ), &in );
-  ClearStatus(status);      
-  LALSCreateVector( status->statusPtr, &( waveform->f->data ), nMax );
-  ClearStatus(status);      
-  LALDCreateVector( status->statusPtr, &( waveform->phi->data ), nMax );
-  ClearStatus(status);        
   
 
-  n = 0;
-  phiC = inject_phase->data[nMax+ padding -1];
   
-  
-  while ( n < nMax ) {
-    waveform->f->data->data[n]     =  inject_freq->data[n+padding];    
-    waveform->a->data->data[2*n]   =  apFac * inject_hc->data[n+padding];
-    waveform->a->data->data[2*n+1] =  acFac * inject_hp->data[n+padding];
-    waveform->phi->data->data[n]   =  inject_phase->data[n +padding] - phiC;    
-    n++;           
-  }
-  
-  
-  yOld = params->inspiral.fFinal  ;
-  params->ppn.tc = n/params->inspiral.tSampling * ( 5.0*LAL_MTSUN_SI*mTot ) / eta;
-
-  /*******************************************************************
-   * CLEANUP                                                         *
-   *******************************************************************/
-
-  waveform->a->epoch = waveform->f->epoch = waveform->phi->epoch
-    = params->ppn.epoch;
-  waveform->a->deltaT = waveform->f->deltaT = waveform->phi->deltaT
-    = params->ppn.deltaT;
-  waveform->a->sampleUnits = lalStrainUnit;
-  waveform->f->sampleUnits = lalHertzUnit;
-  waveform->phi->sampleUnits = lalDimensionlessUnit;
-
-
-  /*  params->ppn->dfdt = dyMax*fFac*params->deltaT;*/
-   params->ppn.dfdt = 1.*fFac*params->ppn.deltaT;
-  params->ppn.fStop = yOld*fFac;
-  params->ppn.length = n;
-
-
-  LALDestroyVector(status->statusPtr, &inject_hp);
-  ClearStatus(status);      
-  LALDestroyVector(status->statusPtr, &inject_hc);
-  ClearStatus(status);      
-  LALDestroyVector(status->statusPtr, &inject_freq);
-  ClearStatus(status);      
-  LALDestroyVector(status->statusPtr, &inject_phase);  
-  ClearStatus(status);      
-  
-  
-  
-
-
-  }
-  else
-    {
-      switch(params->method)
-	{
-	case PPN:
-	  printf("# here ppn\n");
-	  LALGeneratePPNInspiral(status->statusPtr, waveform, &params->ppn);	    
-	  ClearStatus(status);      
-	  printf("# here ppn gfin \n");
-
-	  break;
-	case SpinOrbitCW:
-
-	  
-	  LALGenerateSpinOrbitCW(status->statusPtr, waveform, &params->socw);
-	  ClearStatus(status);      
-	  break;
-	case TaylorCW:
-	  LALGenerateTaylorCW(status->statusPtr, waveform, &params->taylorcw);
-	  ClearStatus(status);      
-	  break;
-	}
-    }
-  
-  
-  /*if from inspiral pacakge additional computation are needed*/
-  
-  
-  /*  DETATCHSTATUSPTR( status );*/
+  DETATCHSTATUSPTR( status );
   RETURN(status);
 }
 
 
 
 
-
-
+/* <lalVerbatim file="GetApproximantAndOrder"> */
 void
-ClearStatus (
-    LALStatus   *status
-            )
+LALGetApproximantAndOrder(
+			  LALStatus *status,
+			  CHAR *waveform,
+			  UINT4 *order,
+			  UINT4 *approximant)
 {
-  if (status->statusPtr)
-  {
-    ClearStatus      (status->statusPtr);
-    DETATCHSTATUSPTR (status);
-  }
+  char *orderptr = NULL;
+  char *approxptr = NULL; 
+  
+  INITSTATUS( status, "LALGetApproximantAndOrder", INJECTINSPIRALC );
+  ATTATCHSTATUSPTR( status );
+  
+  ASSERT( waveform, status, 1,"Null pointer");
+  
+  if (  (orderptr = strstr(waveform, "newtonian") ) )
+    *order = newtonian;
+  else if (  (orderptr = strstr(waveform, "oneHalfPN") ) )
+    *order = oneHalfPN;
+  else if (  (orderptr = strstr(waveform,  "onePN") ) )
+    *order = onePN;
+  else if (  (orderptr =  strstr(waveform,  "onePointFivePN") ) )
+    *order = onePointFivePN;
+  else if (  (orderptr = strstr(waveform, "twoPN") ) )
+    *order = twoPN;
+  else if (  (orderptr = strstr(waveform,  "twoPointFive") ) )
+    *order = twoPointFivePN;
+  else if (  (orderptr = strstr(waveform, "threePN") ) )
+    *order = threePN;
+  else if (  (orderptr = strstr(waveform, "threePointFivePN") ) )
+    *order = threePointFivePN;
+
+
+  if (  (approxptr = strstr(waveform, "TaylorT1" ) ) )
+    *approximant = TaylorT1;
+  else if (  (approxptr = strstr(waveform, "TaylorT2" ) ) )
+    *approximant = TaylorT2;
+  else if (  (approxptr = strstr(waveform, "TaylorT3" ) ) )
+    *approximant = TaylorT3;
+  else if (  (approxptr = strstr(waveform, "TaylorF1" ) ) )
+    *approximant = TaylorF1;
+  else if (  (approxptr = strstr(waveform, "TaylorF2" ) ) )
+    *approximant = TaylorF2;
+  else if (  (approxptr = strstr(waveform, "PadeT1" ) ) )
+    *approximant = PadeT1;
+  else if (  (approxptr = strstr(waveform, "PadeF1" ) ) )
+    *approximant = PadeF1;
+  else if (  (approxptr = strstr(waveform, "EOB" ) ) )
+    *approximant = EOB;
+  else if (  (approxptr = strstr(waveform, "BCV" ) ) )
+    *approximant = BCV;
+  else if (  (approxptr = strstr(waveform, "SpinTaylorT3" ) ) )
+    *approximant = SpinTaylorT3;
+  else if (  (approxptr = strstr(waveform, "GeneratePPN" ) ) )
+    *approximant = PPN;
+  else if (  (approxptr = strstr(waveform, "TaylorCW" ) ) )
+    *approximant = TaylorCW;
+  else if (  (approxptr = strstr(waveform, "SpinOrbitCW" ) ) )
+    *approximant = SpinOrbitCW;
+  
+
+  DETATCHSTATUSPTR( status );
+  RETURN( status );
 }
+
+
+
+
+
