9bcdc57dfe08bcad8d4e9a8910670815e2f7c4ab
emessarit <emessarit>
2004-02-18 17:46:33 +0000

    corrected previous statement Original: 1008c6b65a1a4648de4e72259b5bf1bfcaa85cbe

--- lalapps/src/inspiral/inca.c
+++ lalapps/src/inspiral/inca.c
@@ -1268,8 +1268,8 @@ int main( int argc, char *argv[] )
 	  if (tb > ta + errorParams.dt )
 	  {
 	    /* we are outside the time coincidence so move to the next event */
-            if ( vrbflg ) fprintf(stdout, " outside the time coincidence
-		window " );
+            if ( vrbflg ) fprintf(stdout, " outside the time coincidence window"
+		);
 	    break;
 	  }
 	  else
