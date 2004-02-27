372aa97e1b278d883fdb103936483c2eb256518e
sintes <sintes>
2004-02-27 15:59:51 +0000

    new entry Original: 1f91b50600278b2345476d638d8f78d647a281a2

---
+++ lal/packages/houghpulsar/include/Velocity.h
@@ -0,0 +1,166 @@
+/* ***************************************************
+ *  File Name: Velocity.h
+ *
+ *  Author: Krishnan, B.
+ *
+ *  $Id$ 
+ *
+ *  Created by Badri Krishnan on May 23, 2003
+***************************************************** */
+
+
+/************************************<lalVerbatim file="VelocityHV">
+Author: Krishnan, B., Sintes, A.M.
+$Id$
+*************************************</lalVerbatim> */
+
+/* <lalLaTeX> *************************************************
+
+\section{Header \texttt{Velocity.h}}
+\label{s:Velocity.h}
+Computation of instant and averaged velocities for a given detector and the
+like.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsection*{Synopsis}
+
+\begin{verbatim}
+#include <lal/Velocity.h>
+\end{verbatim}
+
+To find the velocity of a given detetector at a given time, or the averaged
+velocity  of a detector in a certain time interval.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsection*{Error conditions}
+\vspace{0.1in}
+\input{VelocityHErrorTable}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsection*{Structures}
+\begin{verbatim}
+struct VelocityPar
+\end{verbatim}
+\index{\texttt{VelocityPar}}
+\noindent This structure stores the parameters required by LALBarycenter to calculate 
+Earth velocity at a given detector location.
+\begin{description}
+\item[\texttt{LALDetector    detector}]  
+\item[\texttt{EphemerisData  *edat}]  ephemeris data pointer from LALInitBarycenter
+\item[\texttt{LIGOTimeGPS  startTime}]  start of time interval
+\item[\texttt{REAL8          tBase}]  duration of interval
+\item[\texttt{REAL8          vTol}]   fractional accuracy required for velocity
+\end{description}
+ 
+  
+\begin{verbatim}
+struct AvgVelPar
+\end{verbatim}
+\index{\texttt{AvgVelPar}}
+\begin{description}
+\item[\texttt{LALDetector    detector}] 
+\item[\texttt{EphemerisData  *edat}] 
+\end{description}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\vfill{\footnotesize\input{VelocityHV}}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\newpage\input{VelocityC}
+%%%%%%%%%%Test program. %%
+\newpage\input{TestVelocityC}
+
+*************************************************** </lalLaTeX> */
+
+
+/* double inclusion protection */
+#ifndef _VELOCITY_H
+#define _VELOCITY_H
+
+/* *************
+ *    Includes. This header may include others; if so, they go immediately 
+ *    after include-loop protection. Includes should appear in the following 
+ *    order: 
+ *    a. Standard library includes
+ *    b. LDAS includes
+ *    c. LAL includes
+ */
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<errno.h>
+
+
+#include<lal/Date.h>
+#include<lal/LALDatatypes.h>
+#include<lal/ComputeSky.h>
+#include<lal/LALBarycenter.h>
+#include<lal/LALInitBarycenter.h>
+#include<lal/LALStdlib.h>
+#include<lal/LALConstants.h>
+
+/*  ****************************************************
+ *   Protection against C++ name mangling
+ */
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+/* ***************************************
+ *   Assignment of Id string using NRCSID()  
+ */
+ 
+NRCSID( VELOCITYH, "$Id$");
+
+/* ***************************************
+ *   Error codes and messages. This must be auto-extracted for 
+ *    inclusion in the documentation.
+ */
+/* <lalErrTable file="VelocityHErrorTable"> */
+#define VELOCITYH_ENULL 1
+#define VELOCITYH_EVAL 2
+#define VELOCITYH_MSGENULL "Null Pointer"
+#define VELOCITYH_MSGEVAL "Invalid Value"
+/* </lalErrTable>  */
+
+/* *****************************************************
+ *   Structure, enum, union, etc., typdefs.
+ */
+
+/* parameters required by LALBarycenter to calculate Earth velocity at 
+   a given detector location */
+typedef struct tagVelocityPar {
+  LALDetector    detector;
+  EphemerisData  *edat;  /* ephemeris data pointer from LALInitBarycenter */
+  LIGOTimeGPS    startTime; /* start of time interval */
+  REAL8          tBase; /* duration of interval */
+  REAL8          vTol;  /* fractional accuracy required for velocity */
+} VelocityPar;
+
+
+typedef struct tagAvgVelPar {
+  LALDetector    detector;
+  EphemerisData  *edat;
+} AvgVelPar;
+
+/* ***************************************************
+ *  Functions Declarations (i.e., prototypes).
+ */
+void LALAvgDetectorVel(LALStatus    *status,
+		    REAL8        v[3], /* output vector representing average velocity */ 
+		    VelocityPar  *in); /* parameters required to calculate V */
+
+void LALDetectorVel(LALStatus   *status, 
+		 REAL8       v[3],  /* output velocity vector */ 
+		 LIGOTimeGPS *time, /* time at which velocity is calculated */
+		 AvgVelPar *in); /* detector for which velocity is calculated */
+
+/* ****************************************************** */
+
+#ifdef  __cplusplus
+}                /* Close C++ protection */
+#endif
+
+#endif  /* end of double inclusion protection */
---
+++ lal/packages/houghpulsar/test/TestVelocity.c
@@ -0,0 +1,246 @@
+/*-----------------------------------------------------------------------
+ *
+ * File Name: TestVelocity.c
+ *
+ * Authors: Krishnan, B., Sintes, A.M., 
+ *
+ * Revision: $Id$
+ *
+ * History:   Created by Badri Krishnan May 24, 2003
+ *        
+ *
+ *-----------------------------------------------------------------------
+ */
+
+/*
+ * 1.  An author and Id block
+ */
+
+/************************************ <lalVerbatim file="TestVelocityCV">
+Author: Krishnan, B., Sintes, A.M. 
+$Id$
+************************************* </lalVerbatim> */
+
+/*
+ * 2. Commented block with the documetation of this module
+ */
+/* ************************************************<lalLaTeX>
+\subsection{Program \ \texttt{TestVelocity.c}}
+\label{s:TestVelocity.c}
+Tests the calculation of the averaged velocity of a given detector.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsubsection*{Usage}
+\begin{verbatim}
+TestVelocity  [-d debuglevel] [-a accuracy] 
+\end{verbatim}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsubsection*{Description}
+This program computes the averaged velocity  of the GEO600 detector 
+between the times 730000044 and 730003644 with a default accuracy of 0.01.
+The two ephemeris files (e.g., for data taken in 2003, \verb@sun03.dat@ and
+\verb@earth03.dat@) are assumed to be in the  directory
+\verb@lal/packages/pulsar/test/@.
+
+The \verb@-d@ option sets the debug level to the specified value
+\verb@debuglevel@.  The \verb@-a@ flag tells the program which accuracy to use.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsubsection*{Exit codes}
+\vspace{0.1in}
+\input{TESTVELOCITYCErrorTable}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsubsection*{Uses}
+\begin{verbatim}
+LALAvgDetectorVel()
+LALPrintError()
+LALMalloc()
+LALFree()
+LALCheckMemoryLeaks()
+\end{verbatim}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\subsubsection*{Notes}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\vfill{\footnotesize\input{TestVelocityCV}}
+
+********************************************</lalLaTeX> */
+
+/* #include "./Velocity.h" */
+#include <lal/Velocity.h> 
+
+NRCSID (TESTVELOCITYC, "$Id$");
+
+
+/* Error codes and messages */
+
+/************** <lalErrTable file="TESTVELOCITYCErrorTable"> */
+#define TESTVELOCITYC_ENORM 0
+#define TESTVELOCITYC_ESUB  1
+#define TESTVELOCITYC_EARG  2
+#define TESTVELOCITYC_EBAD  3
+#define TESTVELOCITYC_EFILE 4
+
+#define TESTVELOCITYC_MSGENORM "Normal exit"
+#define TESTVELOCITYC_MSGESUB  "Subroutine failed"
+#define TESTVELOCITYC_MSGEARG  "Error parsing arguments"
+#define TESTVELOCITYC_MSGEBAD  "Bad argument values"
+#define TESTVELOCITYC_MSGEFILE "Could not create output file"
+/******************************************** </lalErrTable> */
+
+
+/* Default parameters. */
+
+INT4 lalDebugLevel=7;
+
+/* #define T0SEC 714153733 */
+#define T0SEC 730000044 
+#define T0NSEC 0
+#define TBASE 3600.0
+#define ACCURACY 0.01
+
+/* Locations of the earth and sun ephemeris data */
+
+#define EARTHDATA "../../pulsar/test/earth03.dat" 
+#define SUNDATA "../../pulsar/test/sun03.dat" 
+
+/* Usage format string.  */
+#define USAGE "Usage: %s [-d debuglevel] [-a accuracy]\n"
+
+/*********************************************************************/
+/* Macros for printing errors & testing subroutines (from Creighton) */
+/*********************************************************************/
+
+#define ERROR( code, msg, statement )                                \
+do {                                                                 \
+  if ( lalDebugLevel & LALERROR )                                    \
+    LALPrintError( "Error[0] %d: program %s, file %s, line %d, %s\n" \
+                   "        %s %s\n", (code), *argv, __FILE__,       \
+              __LINE__, TESTVELOCITYC, statement ? statement :  \
+                   "", (msg) );                                      \
+} while (0)
+
+#define INFO( statement )                                            \
+do {                                                                 \
+  if ( lalDebugLevel & LALINFO )                                     \
+    LALPrintError( "Info[0]: program %s, file %s, line %d, %s\n"     \
+                   "        %s\n", *argv, __FILE__, __LINE__,        \
+              TESTVELOCITYC, (statement) );                     \
+} while (0)
+
+#define SUB( func, statusptr )                                       \
+do {                                                                 \
+  if ( (func), (statusptr)->statusCode ) {                           \
+    ERROR( TESTVELOCITYC_ESUB, TESTVELOCITYC_MSGESUB,      \
+           "Function call \"" #func "\" failed:" );                  \
+    return TESTVELOCITYC_ESUB;                                  \
+  }                                                                  \
+} while (0)
+/******************************************************************/
+
+/* A global pointer for debugging. */
+#ifndef NDEBUG
+char *lalWatch;
+#endif
+
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+/* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv------------------------------------ */
+int main(int argc, char *argv[]){ 
+
+  static LALStatus       status; 
+  static VelocityPar     velPar;
+  static REAL8           vel[3];
+  static  EphemerisData  *edat = NULL;
+  INT4   arg;                         /* Argument counter */
+  REAL8  vTol=0.01 ;
+  /* INT4   c, errflg=0;*/
+  /*  optarg = NULL; */
+  /* ------------------------------------------------------- */
+
+ 
+  /* default values */
+  velPar.detector = lalCachedDetectors[LALDetectorIndexGEO600DIFF];
+  velPar.startTime.gpsSeconds = T0SEC;
+  velPar.startTime.gpsNanoSeconds = T0NSEC;
+  velPar.tBase = TBASE;
+  velPar.vTol = ACCURACY;  
+
+ /********************************************************/
+  /* Parse argument list.  i stores the current position. */
+  /********************************************************/
+  arg = 1;
+  while ( arg < argc ) {
+    /* Parse debuglevel option. */
+    if ( !strcmp( argv[arg], "-d" ) ) {
+      if ( argc > arg + 1 ) {
+        arg++;
+        lalDebugLevel = atoi( argv[arg++] );
+      } else {
+        ERROR( TESTVELOCITYC_EARG, TESTVELOCITYC_MSGEARG, 0 );
+        LALPrintError( USAGE, *argv );
+        return TESTVELOCITYC_EARG;
+      }
+    }
+    /* Parse accuracy option. */
+    else if ( !strcmp( argv[arg], "-a" ) ) {
+      if ( argc > arg + 1 ) {
+        arg++;
+	vTol= atof( argv[arg++]);
+        velPar.vTol= vTol;
+      } else {
+        ERROR( TESTVELOCITYC_EARG, TESTVELOCITYC_MSGEARG, 0 );
+        LALPrintError( USAGE, *argv );
+        return TESTVELOCITYC_EARG;
+      }
+    }
+    /* Unrecognized option. */
+    else {
+      ERROR( TESTVELOCITYC_EARG, TESTVELOCITYC_MSGEARG, 0 );
+      LALPrintError( USAGE, *argv );
+      return TESTVELOCITYC_EARG;
+    }
+  } /* End of argument parsing loop. */
+  /******************************************************************/
+
+  /* read ephemeris data */
+  /* ephemeris info */
+  edat = (EphemerisData *)LALMalloc(sizeof(EphemerisData));
+  (*edat).ephiles.earthEphemeris = EARTHDATA;
+  (*edat).ephiles.sunEphemeris = SUNDATA;
+  /* is this the right number of leap seconds? */
+  (*edat).leap = 13;
+
+  /* read in ephemeris data */
+  SUB( LALInitBarycenter( &status, edat), &status);
+
+  /* fill in ephemeris data in velPar */
+  velPar.edat = edat;
+
+  /*  for (i=0; i<1400; i++) */
+  SUB( LALAvgDetectorVel ( &status, vel, &velPar), &status );
+
+
+  printf("AvgDetectorVel= %g, %g, %g \n", vel[0],vel[1],vel[2]);
+ 
+
+  LALFree(edat->ephemE);
+  LALFree(edat->ephemS);
+  LALFree(edat);
+  LALCheckMemoryLeaks(); 
+
+  INFO(TESTVELOCITYC_MSGENORM);
+  return TESTVELOCITYC_ENORM;
+}
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+
+
+
+
+
+
+
