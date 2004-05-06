deea7085e78bc3edd1dd5f862d7e07fe35fc92c0
thomas <thomas>
2004-05-06 17:15:27 +0000

    additional documentation for Injection of inspiral models. Original: c57374f2d3ac78ddd2610cb6db6f708c151ec945

--- lal/packages/inject/include/GenerateInspiral.h
+++ lal/packages/inject/include/GenerateInspiral.h
@@ -1,3 +1,21 @@
+/* <lalVerbatim file="GenerateInspiralHV">
+ * Author: Cokelaer, T.
+ * $Id$
+ * </lalVerbatim>  */
+
+
+/* <lalLaTeX>
+ * 	\section{Header \texttt{GenerateInspiral.h}}
+ * 	\label{s:GenerateInspiral.h}
+ * 	
+ * 	Header file for the inspiral injection interface code.
+ * 	
+ * 	\subsection*{Synopsis}
+ * 	\begin{verbatim}
+ * 	#include <lal/GenerateInspiral.h>
+ * 	\end{verbatim}
+ * </lalLaTeX> */
+
 #ifndef _GENERATEINSPIRAL_H
 #define _GENERATEINSPIRAL_H
 
@@ -8,42 +26,59 @@
 #include <lal/GenerateTaylorCW.h>
 #include <lal/LIGOMetadataTables.h>
 
-
 #ifdef  __cplusplus
 extern "C" {
 #pragma }
 #endif
 
-
 NRCSID( GENERATEINSPIRALH, "$Id$" );
 
-/* A reference number for the method already implemented in the injection package*/
+/*  <lalLaTeX>
+ *  	\subsection*{Error codes}
+ *  </lalLaTeX>  */
+
+/* <lalErrTable> */
+#define GENERATEINSPIRALH_ENORM 	0
+#define GENERATEINSPIRALH_ENULL  	1
+#define GENERATEINSPIRALH_EAPPROXIMANT  6
+#define GENERATEINSPIRALH_EORDER  	7
+
+#define GENERATEINSPIRALH_MSGENORM 		"Normal exit"
+#define GENERATEINSPIRALH_MSGENULL  		"Null pointer"
+#define GENERATEINSPIRALH_MSGEAPPROXIMANT  	"Non valid approximant"
+#define GENERATEINSPIRALH_MSGEORDER  		"Non valid order"
+/* </lalErrTable> */
+
+
+/* EOB a 3PN */
+#define GENERATEINSPIRAL_ZETA2       0.
+#define GENERATEINSPIRAL_OMEGAS      0.
+#define GENERATEINSPIRAL_THETA       0.
+
+/* A reference number for the method already 
+ * implemented in the injection package. Should add PPN to 
+ * the inspiral strucutre. the two others are useless here
+ * */
 typedef enum {  
    PPN       	= 101,
    SpinOrbitCW	= 102,
    TaylorCW  	= 103
  } Method;
 
+/*  <lalLaTeX>
+ *     \newpage\input{GenerateInspiralC}
+ *  </lalLaTeX>  */
+void LALGenerateInspiral(LALStatus        *status,
+			 CoherentGW       *waveform,
+			 SimInspiralTable *params, 
+			 PPNParamStruc    *ppnParamsInputOutput );
 
+void ComputeSpin(InspiralTemplate *params);
 
-/**/
-void
-LALGenerateInspiral(
-		    LALStatus        *status,
-		    CoherentGW       *waveform,
-		    SimInspiralTable  *params, 
-		    PPNParamStruc         *ppnParamsInputOutput
-		    );
-
-
-/**/void ComputeSpin(InspiralTemplate *params);
-
-void
-LALGetApproximantAndOrder(
-			  LALStatus *status,
-			  CHAR *waveform,
-			  UINT4 *order,
-			  UINT4 *approximant);
+void LALGetApproximantAndOrderFromString(LALStatus *status,
+					 CHAR *waveform,
+					 UINT4 *order,
+					 UINT4 *approximant);
 #ifdef  __cplusplus
 #pragma {
 }
