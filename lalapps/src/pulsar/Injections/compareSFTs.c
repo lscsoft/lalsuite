b0b9f12c167b62b53a41c5ce4af37668dd39b95c
reinhard <reinhard>
2004-05-27 13:03:42 +0000

    new code: given two directories and SFT-patterns, compares them on relative 	difference in power and phase. Original: 3e9459abe6b29cf1b719af25fa49060d75b1399b

---
+++ lalapps/src/pulsar/Injections/compareSFTs.c
@@ -0,0 +1,202 @@
+ /*-----------------------------------------------------------------------
+ *
+ * File Name: compareSFTs.c
+ *
+ * Authors: Prix, R.
+ *
+ * Revision: $Id$
+ *           
+ *-----------------------------------------------------------------------
+ */
+
+/* ---------- includes ---------- */
+#include <lalapps.h>
+
+#include <lal/UserInput.h>
+#include <lal/SFTfileIO.h>
+
+RCSID ("$Id");
+
+/* Error codes and messages */
+/* <lalErrTable file="MAKEFAKEDATACErrorTable"> */
+#define MAKEFAKEDATAC_ENORM 	0
+#define MAKEFAKEDATAC_ESUB  	1
+#define MAKEFAKEDATAC_EARG  	2
+#define MAKEFAKEDATAC_EBAD  	3
+#define MAKEFAKEDATAC_EFILE 	4
+#define MAKEFAKEDATAC_ENOARG 	5
+
+#define MAKEFAKEDATAC_MSGENORM "Normal exit"
+#define MAKEFAKEDATAC_MSGESUB  "Subroutine failed"
+#define MAKEFAKEDATAC_MSGEARG  "Error parsing arguments"
+#define MAKEFAKEDATAC_MSGEBAD  "Bad argument values"
+#define MAKEFAKEDATAC_MSGEFILE "File IO error"
+#define MAKEFAKEDATAC_MSGENOARG "Missing argument"
+
+/* </lalErrTable> */
+
+/***************************************************/
+#define TRUE (1==1)
+#define FALSE (1==0)
+
+/* local prototypes */
+/* Prototypes for the functions defined in this file */
+void initUserVars (LALStatus *stat);
+REAL4 mymax (REAL4 x, REAL4 y);
+void compare_SFTs (const SFTtype *sft1, const SFTtype *sft2);
+
+
+/*----------------------------------------------------------------------*/
+static const LALStatus empty_status;
+/*----------------------------------------------------------------------*/
+/* User variables */
+CHAR *uvar_sftBname1;
+CHAR *uvar_sftBname2;
+INT4 uvar_debug;
+BOOLEAN uvar_help;
+
+/*----------------------------------------------------------------------
+ * main function 
+ *----------------------------------------------------------------------*/
+int
+main(int argc, char *argv[]) 
+{
+  LALStatus status = empty_status;	/* initialize status */
+  REAL8 fmin, fmax, deltaF;
+  SFTHeader header;
+  CHAR *fname;
+  SFTVector *SFTs1 = NULL, *SFTs2 = NULL;
+  UINT4 i;
+
+  lalDebugLevel = 3;
+
+  /* set LAL error-handler */
+  lal_errhandler = LAL_ERR_EXIT;	/* exit with returned status-code on error */
+
+  /* register all user-variables */
+  LAL_CALL (initUserVars (&status), &status);	  
+
+  /* read cmdline & cfgfile  */	
+  LAL_CALL (LALUserVarReadAllInput (&status, argc,argv), &status);  
+
+  if (uvar_help) 	/* help requested: we're done */
+    exit (0);
+
+  lalDebugLevel = uvar_debug;
+
+  /* check that all required input-variables have been specified */
+  LAL_CALL (LALUserVarCheckRequired (&status), &status);
+
+  /* figure out what frequency-band these SFTs contain */
+  fname = LALCalloc(1, strlen(uvar_sftBname1) + 10);
+  sprintf (fname, "%s.00000", uvar_sftBname1);
+  LAL_CALL (LALReadSFTheader (&status, &header, fname), &status);
+  LALFree (fname);
+
+  deltaF = 1.0 / header.timeBase;
+  fmin = header.fminBinIndex * deltaF;
+  fmax = fmin + (header.length - 1) * deltaF;
+
+  /* now read in the two sft-vectors */
+  LAL_CALL (LALReadSFTfiles (&status, &SFTs1, fmin, fmax, uvar_sftBname1), &status);
+  LAL_CALL (LALReadSFTfiles (&status, &SFTs2, fmin, fmax, uvar_sftBname2), &status);
+
+  if (SFTs1->length != SFTs2->length) {
+    LALPrintError ("Warning: number of SFTs differ for SFTbname1 and SFTbname2!\n");
+  }
+
+  for (i=0; i < mymax(SFTs1->length, SFTs2->length); i++)
+    compare_SFTs ( &(SFTs1->data[i]), &(SFTs2->data[i]) );
+
+
+  /* free memory */
+  LAL_CALL (LALDestroySFTVector(&status, &SFTs1), &status);
+  LAL_CALL (LALDestroySFTVector(&status, &SFTs2), &status);
+  LAL_CALL (LALDestroyUserVars (&status), &status);
+
+
+  LALCheckMemoryLeaks(); 
+
+  return 0;
+} /* main */
+
+
+/*----------------------------------------------------------------------*/
+/* register all our "user-variables" */
+void
+initUserVars (LALStatus *stat)
+{
+  INITSTATUS( stat, "initUserVars", rcsid );
+  ATTATCHSTATUSPTR (stat);
+
+  /* set some defaults */
+  uvar_debug = lalDebugLevel;
+
+  /* now register all our user-variable */
+
+  regSTRINGUserVar(stat, sftBname1,	'1', UVAR_REQUIRED, "Path and basefilename for SFTs1");
+  regSTRINGUserVar(stat, sftBname2,	'2', UVAR_REQUIRED, "Path and basefilename for SFTs2");
+  regINTUserVar(stat,    debug,		'v', UVAR_OPTIONAL, "set debug-level");
+  regBOOLUserVar(stat,   help,		'h', UVAR_HELP, "Print this help/usage message");
+
+  DETATCHSTATUSPTR (stat);
+  RETURN (stat);
+
+} /* initUserVars() */
+
+
+REAL4 mymax (REAL4 x, REAL4 y)
+{
+  return (x > y ? x : y);
+}
+/* little debug-function: compare two sft's and print relative errors */
+void
+compare_SFTs (const SFTtype *sft1, const SFTtype *sft2)
+{
+  static LALStatus status;
+  UINT4 i;
+  REAL4 errpow= 0, errph = 0;
+  REAL4 re1, re2, im1, im2, pow1, pow2, phase1, phase2;
+  REAL8 Tdiff;
+
+  if (sft1->data->length != sft2->data->length) 
+    {
+      printf ("\ncompare_SFTs(): lengths differ! %d != %d\n", sft1->data->length, sft2->data->length);
+      return;
+    }
+  LALDeltaFloatGPS (&status, &Tdiff, &(sft1->epoch), &(sft2->epoch));
+  if ( Tdiff != 0.0 ) 
+    printf ("epochs differ: (%d s, %d ns)  vs (%d s, %d ns)\n", 
+	    sft1->epoch.gpsSeconds, sft1->epoch.gpsNanoSeconds, sft2->epoch.gpsSeconds, sft2->epoch.gpsNanoSeconds);
+
+  if ( sft1->f0 != sft2->f0)
+    printf ("fmin differ: %fHz vs %fHz\n", sft1->f0, sft2->f0);
+
+  if ( sft1->deltaF != sft2->deltaF )
+    printf ("deltaF differs: %fHz vs %fHz\n", sft1->deltaF, sft2->deltaF);
+
+  for (i=0; i < sft1->data->length; i++)
+    {
+      re1 = sft1->data->data[i].re;
+      im1 = sft1->data->data[i].im;
+      re2 = sft2->data->data[i].re;
+      im2 = sft2->data->data[i].im;
+
+      pow1 = sqrt(re1*re1 + im1*im1);
+      pow2 = sqrt(re2*re2 + im2*im2);
+
+      phase1 = atan2 (im1, re1);
+      phase2 = atan2 (im2, re2);
+
+      errpow = mymax (errpow, fabs(pow1-pow2)/mymax(pow1,pow2) );
+      errph = mymax (errph, fabs(phase1 - phase2) );
+
+    } /* for i */
+
+
+  printf ("maximal relative error in power: %e, max phaseerror: %e radians\n", errpow, errph);
+
+  return;
+
+} /* compare_SFTs() */
+
