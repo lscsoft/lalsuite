f927a4949d9bfd8533a01ae3f2a5371d9134595f
dwchin <dwchin>
2004-06-10 01:59:31 +0000

    Added make_gridding to create different types of gridding. Original: 4b8b88664766ddccd159db4242ee68b36fffc622

---
+++ lalapps/src/detresponse/make_gridding.c
@@ -0,0 +1,167 @@
+/*
+ * Author: David Chin <dwchin@umich.edu> +1-734-709-9119
+ * $Id$
+ */
+
+/* 
+ * Makes a grid given appropriate params
+ */
+ 
+#include "make_gridding.h"
+#include "skygrid.h"
+#include "util.h"
+
+static
+UINT4
+modulo_sum(UINT4 a, UINT4 b, UINT4 mod)
+{
+  return (UINT4)fmod((double)a+(double)b, (double)mod);
+}
+
+/* returns azimuthal angle of Earth's location in ICRS */
+static
+REAL8 
+earth_location(LALStatus *s, LIGOTimeGPS *p_gps, EphemerisData *p_eph)
+{
+  EarthState earth;
+
+  LALBarycenterEarth(s, &earth, p_gps, p_eph);
+  
+  return atan2(earth.posNow[1], earth.posNow[0]);
+}
+
+
+/* fills in an irregular grid */
+static
+UINT4 
+irr_grid_ra(LALStatus *s, REAL8Vector *ra_grid, REAL8 earth_phi)
+{
+  REAL8Vector *g;
+  UINT4 num_grid = ra_grid->length;
+  UINT4 num_grid_4 = num_grid / 4;
+  UINT4 num_grid_2 = num_grid_4 * 2;
+  UINT4 num_grid_3_4 = num_grid_4 * 3;
+  UINT4 i, offset;
+  REAL8 a, b;
+  REAL8 twopi_num_grid = (REAL8)LAL_TWOPI/(REAL8)num_grid;
+
+  LALDCreateVector(s, &g, num_grid);
+  
+  if (num_grid != 0)
+  {
+    if (lalDebugLevel > 1)
+    {
+      fprintf(stderr, "num_grid_4   = %d\n", num_grid_4);
+      fprintf(stderr, "num_grid_2   = %d\n", num_grid_2);
+      fprintf(stderr, "num_grid_3_4 = %d\n", num_grid_3_4);
+      fprintf(stderr, "num_grid     = %d\n", num_grid);
+    }
+    
+    a = 2. / (REAL8)num_grid;
+    b = 2. * a;
+  
+    g->data[num_grid_4] = asin(-1. + a + b*(REAL8)num_grid_4);
+    g->data[num_grid_2] = asin(-1. + a + b*(REAL8)num_grid_2);
+    for (i = 0; i < num_grid_4; ++i)
+    {
+      g->data[i] = asin(-1. + a + b*(REAL8)i);
+      g->data[num_grid_2 - i - 1] = -g->data[i];
+      g->data[num_grid_2 + i] = g->data[i] + (REAL8)LAL_PI;
+      g->data[num_grid - i - 1] = -g->data[i] + (REAL8)LAL_PI;
+    }
+  }
+  
+  /* shift the computed grid above, and fill in the RA grid */
+  offset = (UINT4)rint(earth_phi / twopi_num_grid); /* offset */
+  for (i = 0; i < num_grid; ++i)
+    ra_grid->data[modulo_sum(i, offset, num_grid)] = g->data[i];
+  
+  LALDDestroyVector(s, &g);
+  
+  return num_grid;
+}
+
+void 
+zero_gridding(LALStatus *s, gridding_t *g)
+{
+  UINT4 i;
+  
+  if (g->ra == NULL || g->dec == NULL)
+  {
+    exit(11);
+  }
+  
+  for (i = 0; i < g->ra->length; ++i)
+    g->ra->data[i] = 0.;
+    
+  for (i = 0; i < g->dec->length; ++i)
+    g->dec->data[i] = 0.;
+}
+
+void 
+init_gridding(LALStatus *s, gridding_t *g, 
+              UINT4 num_ra, gridding_geom_t ra_geom, 
+              UINT4 num_dec, gridding_geom_t dec_geom,
+              LIGOTimeGPS *gps)
+{
+  UINT4 i, j;
+  REAL8 pi_num_ra = (REAL8)LAL_PI/(REAL8)num_ra;
+  EphemerisData ephemerides;
+  REAL8         earth_phi; /* azi. position of Earth in solar
+                              system barycenter */
+  
+  if (dec_geom == DETRESP_VARGRID)
+  {
+    fprintf(stderr, "gridding_t does not support variable Dec gridding\n");
+    exit(11);
+  }
+  
+  g->ra_geom = ra_geom;
+  g->dec_geom = dec_geom;
+  
+
+  LALDCreateVector(s, &(g->dec), num_dec);
+  
+  if (g->dec_geom == DETRESP_REGGRID)
+  {
+    for (i = 0; i < g->dec->length; ++i)
+      g->dec->data[i] = (REAL8)LAL_PI * (-0.5 + (0.5 + i)/(REAL8)g->dec->length);
+  }
+  else if (g->dec_geom == DETRESP_IRRGRID)
+  {
+    for (i = 0; i < g->dec->length; ++i)
+      g->dec->data[i] = asin(-1. + (REAL8)(1 + 2*i)/(REAL8)g->dec->length);
+  }
+ 
+  
+  if (g->ra_geom != DETRESP_VARGRID)
+  {
+    LALDCreateVector(s, &(g->ra), num_ra);
+    
+    if (g->ra_geom == DETRESP_REGGRID)
+    {
+      for (i = 0; i < g->ra->length; ++i)
+        g->ra->data[i] = pi_num_ra * (1. + 2.*(REAL8)i);
+    }
+    else if (g->ra_geom == DETRESP_IRRGRID)
+    {
+      /* gridding depends on Earth's position in orbit */ 
+      init_ephemeris(s, &ephemerides);
+      
+      earth_phi = earth_location(s, gps, &ephemerides);
+      
+      cleanup_ephemeris(s, &ephemerides);
+    }
+  }
+  else
+  {
+    xcalloc(sizeof(REAL8Vector**), g->dec->length);
+    
+    for (j = 0; j < g->dec->length; ++j)
+    {
+      i = (UINT4)rint(sin(g->dec->data[j]));
+      LALDCreateVector(s, &(g->ra_irr[j]), i);
+    }
+  }
+  
+}
---
+++ lalapps/src/detresponse/make_gridding.h
@@ -0,0 +1,79 @@
+/*
+ * Author: David Chin <dwchin@umich.edu> +1-734-709-9119
+ * $Id$
+ */
+#ifndef _DETRESPONSE_MAKE_GRIDDING_H
+#define _DETRESPONSE_MAKE_GRIDDING_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include <lal/LALConfig.h>
+#include <lal/LALStdio.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALConstants.h>
+#include <lal/LALDatatypes.h>
+#include <lal/Units.h>
+
+#include <lal/AVFactories.h>
+#include <lal/DetectorSite.h>
+#include <lal/TimeDelay.h>
+#include <lal/DetResponse.h>
+#include <lal/Velocity.h>
+#include <lal/Units.h>
+#include <lal/VectorOps.h>
+#include <lal/SkyCoordinates.h>
+
+#include <lal/PrintFTSeries.h>
+#include <lal/StreamOutput.h>
+
+#include "util.h"
+
+/*
+ * Per axis/coord. grid geometry
+ *
+ * Regular grid:   regular division of the coordinate; total
+ *                 grid cells == num_ra or num_dec
+ * Irregular grid: irregular division of the coordinate;
+ *                 total grid points == num_ra or num_dec
+ * Variable grid:  variable grid; number of grid points varies
+ *                 as a function of the other coordinate;
+ *                 we only support RA grid vary as cos(dec)
+ *
+ * NB: only the RA grid supports variable gridding
+ */
+typedef enum 
+{
+  DETRESP_REGGRID, 
+  DETRESP_IRRGRID, 
+  DETRESP_VARGRID,
+} 
+gridding_geom_t;
+
+/*
+ * Struct that will contain the coordinates of every 
+ * grid cell center
+ */
+typedef 
+struct gridding_tag 
+{
+  gridding_geom_t ra_geom;
+  gridding_geom_t dec_geom;
+  REAL8Vector *ra;
+  REAL8Vector **ra_irr;  /* irregular grid, Dec dependent */
+  REAL8Vector *dec;   
+}
+gridding_t;
+
+
+
+void init_gridding(LALStatus *status, gridding_t *gridding, 
+                   UINT4 num_ra, gridding_geom_t ra_geom, 
+                   UINT4 num_dec, gridding_geom_t dec_geom,
+                   LIGOTimeGPS *gps);
+
+                   
+void zero_gridding(LALStatus *status, gridding_t *gridding);
+
+#endif
