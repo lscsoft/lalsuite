171d0735ebdb48f6c3ec5782980ab45a1c0b5f04
sintes <sintes>
2004-08-11 14:57:24 +0000

    added new file to refine confide level using the finer MC output Original: 939cf4acb7cb601cd6a7515450a8932b4d066414

---
+++ lalapps/src/pulsar/hough/m_files/L1getCofH_fine_v2.m
@@ -0,0 +1,73 @@
+% $Id$
+%
+%  Matlab script to get C(h_0)
+%
+% Remember to add the path where this file is located:
+% addpath /scratch/sintes/CVSDIR/waves/people/sintes/PULSAR/CODES/m_files/
+%   To run simply type  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+Detector = 'L1';
+
+fileoutput = strcat(Detector,'outCH2');
+fid = fopen(fileoutput, 'w');
+
+fileinput = strcat(Detector,'h0newband');
+
+Directory1 = '/scratch/sintes/S2_clean/Driver_allsky/';
+Directory2 = '/scratch/sintes/S2_clean/MC_allsky/';
+
+subdir = 'L1_fine/';
+sufix2='.m.dat';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% getting the different bands, loudest event per band
+% and new h0 interval to refine
+
+nh0=10;
+h0inFile = load(fileinput);
+
+fmin=h0inFile(:,2);
+fmax=h0inFile(:,3);
+Nmax=h0inFile(:,4);
+h0min=h0inFile(:,5);
+h0max=h0inFile(:,6);
+
+Nbands =length(fmin);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+filepath = strcat(Directory2,subdir);
+  addpath(filepath);
+
+filepre = strcat(filepath,'/MC_');
+filepre = strcat(filepre,Detector);
+filepre = strcat(filepre,'_');
+
+for bandnumber=0:Nbands-1
+
+  bn=bandnumber+1;  
+  steph0= (h0max(bn)-h0min(bn))/(nh0-1);
+  h0vect = h0min(bn):steph0:h0max(bn);
+ 
+  mystring = strcat(filepre, int2str( bandnumber ) );
+  mystring = strcat(mystring, '_nc');
+  Ncount0 = load(mystring);
+  nMonteCarlos=length(Ncount0);
+
+ 
+  fprintf(fid,'%d %d %d %d ', bn-1, fmin(bn), fmax(bn), Nmax(bn) );
+  
+   for h0num=1:nh0
+     x=Ncount0(:, h0num+1);   
+     kkcount = find(x>Nmax(bn));
+     CH(h0num) = length(kkcount)/nMonteCarlos;
+     fprintf(fid,' %d %d ', h0vect(h0num), CH(h0num) );
+   end
+     fprintf(fid,' \n');
+   CH
+   
+end
+
+ fclose(fid);
