e9d3f9f101d604d310f6aef8a6f0533a56cd46a2
kipp <kipp>
2004-08-17 15:33:19 +0000

    Add some frequency series convenience functions.  Maybe move them to a lal-wide place later. Original: 110c294366541c853b92b48a5e4d39f3e133d50b

---
+++ lal/packages/burstsearch/include/FrequencySeries.h
@@ -0,0 +1,37 @@
+/* <lalVerbatim file="FrequencySeriesHV">
+Author: Cannon, K. C.
+$Id$
+</lalVerbatim>
+ */
+
+#ifndef _FREQUENCYSERIES_H
+#define _FREQUENCYSERIES_H
+
+#include <lal/LALDatatypes.h>
+
+#define LAL_FAIL_ERR	1
+#define LAL_FAIL_MSG	"operation failed"
+#define LAL_NULL_ERR	2
+#define LAL_NULL_MSG	"unexpected NULL pointer"
+#define LAL_NOMEM_ERR	3
+#define LAL_NOMEM_MSG	"out of memory"
+#define LAL_RANGE_ERR	4
+#define LAL_RANGE_MSG	"parameter out of range"
+
+void LALDestroyREAL4FrequencySeries(
+	LALStatus *status,
+	REAL4FrequencySeries *series
+);
+
+void LALNewREAL4FrequencySeries(
+	LALStatus *status,
+	REAL4FrequencySeries **output,
+	CHAR *name,
+	LIGOTimeGPS epoch,
+	REAL8 f0,
+	REAL8 deltaF,
+	LALUnit sampleUnits,
+	size_t length
+);
+
+#endif  /* _FREQUENCYSERIES_H */
---
+++ lal/packages/burstsearch/src/FrequencySeries.c
@@ -0,0 +1,93 @@
+#include <string.h>
+
+#include <lal/LALDatatypes.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALStatusMacros.h>
+#include <lal/LALError.h>
+#include <lal/FrequencySeries.h>
+
+NRCSID(FREQUENCYSERIESC, "$Id$");
+
+
+static void DestroyREAL4FrequencySeries(
+	REAL4FrequencySeries *series
+)
+{
+	if(series) {
+		if(series->data)
+			LALFree(series->data->data);
+		LALFree(series->data);
+	}
+	LALFree(series);
+}
+
+
+void LALDestroyREAL4FrequencySeries(
+	LALStatus *status,
+	REAL4FrequencySeries *series
+)
+{
+	INITSTATUS(status, "LALDestroyREAL4FrequencySeries", FREQUENCYSERIESC);
+
+	DestroyREAL4FrequencySeries(series);
+
+	RETURN(status);
+}
+
+
+static REAL4FrequencySeries *NewREAL4FrequencySeries(
+	CHAR *name,
+	LIGOTimeGPS epoch,
+	REAL8 f0,
+	REAL8 deltaF,
+	LALUnit sampleUnits,
+	size_t length
+)
+{
+	REAL4FrequencySeries *new;
+	REAL4Sequence *sequence;
+	REAL4 *data;
+
+	new = LALMalloc(sizeof(*new));
+	sequence = LALMalloc(sizeof(*sequence));
+	data = LALMalloc(length * sizeof(*data));
+	if(!new || !sequence || !data) {
+		LALFree(new);
+		LALFree(sequence);
+		LALFree(data);
+		return(NULL);
+	}
+
+	strncpy(new->name, name, LALNameLength);
+	new->epoch = epoch;
+	new->f0 = f0;
+	new->deltaF = deltaF;
+	new->sampleUnits = sampleUnits;
+	new->data = sequence;
+	sequence->data = data;
+	sequence->length = length;
+
+	return(new);
+}
+
+
+void LALNewREAL4FrequencySeries(
+	LALStatus *status,
+	REAL4FrequencySeries **output,
+	CHAR *name,
+	LIGOTimeGPS epoch,
+	REAL8 f0,
+	REAL8 deltaF,
+	LALUnit sampleUnits,
+	size_t length
+)
+{
+	INITSTATUS(status, "LALNewREAL4FrequencySeries", FREQUENCYSERIESC);
+
+	*output = NewREAL4FrequencySeries(name, epoch, f0, deltaF, sampleUnits, length);
+
+	if(!*output)
+		ABORT(status, LAL_FAIL_ERR, LAL_FAIL_MSG);
+
+	RETURN(status);
+}
