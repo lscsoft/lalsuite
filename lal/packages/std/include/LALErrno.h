6f574bcf41fb587dbf42275750a834e91ce749da
kipp <kipp>
2004-08-19 21:11:39 +0000

    Create some error codes and messages for standard LAL-wide use (can't wait for Jolien to come back from vacation :-)). Original: 53c57f8cdd3660cdf27fca833c1a6d3ee34f43ed

---
+++ lal/packages/std/include/LALErrno.h
@@ -0,0 +1,23 @@
+/* <lalVerbatim file="LALErrnoHV">
+Author: Cannon, K. C.
+$Id$
+</lalVerbatim>
+ */
+
+#ifndef _LALERRNO_H
+#define _LALERRNO_H
+
+NRCSID(LALERRNOH, "$Id$");
+
+/* Error codes and corresponding error messages. */
+
+#define LAL_FAIL_ERR	1
+#define LAL_FAIL_MSG	"operation failed"
+#define LAL_NULL_ERR	2
+#define LAL_NULL_MSG	"unexpected NULL pointer"
+#define LAL_NOMEM_ERR	3
+#define LAL_NOMEM_MSG	"out of memory"
+#define LAL_RANGE_ERR	4
+#define LAL_RANGE_MSG	"parameter out of range"
+
+#endif /* _LALERRNO_H */
