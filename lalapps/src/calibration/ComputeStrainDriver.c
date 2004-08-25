8e36b808fe80761a84b37f1084a99081acede620
siemens <siemens>
2004-08-25 18:18:29 +0000

    driver for new lal ComputeStrain function Original: 2329cb615d9e9f84b1c58f4568cc0fbfb7505017

---
+++ lalapps/src/calibration/ComputeStrainDriver.c
@@ -0,0 +1,420 @@
+/*********************************************************************************/
+/*                         Driver for calibration function                       */
+/*                                                                               */
+/*                                  X. Siemens                                   */
+/*                                                                               */
+/*                               UWM - August 2004                               */
+/*********************************************************************************/
+
+#include <config.h>
+#if !defined HAVE_LIBGSL || !defined HAVE_LIBLALFRAME
+#include <stdio.h>
+int main(void) {fputs("disabled, no gsl or no lal frame library support.\n", stderr);return 1;}
+#else
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <glob.h>
+#include <errno.h>
+
+#include <lal/LALDatatypes.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALStdio.h>
+#include <lal/FileIO.h>
+#include <lal/AVFactories.h>
+#include <lal/FrameCache.h>
+#include <lal/FrameStream.h>
+#include <lal/Window.h>
+#include <lal/Calibration.h>
+#include <lal/LALConstants.h>
+#include <lal/BandPassTimeSeries.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALConstants.h>
+#include <lal/AVFactories.h>
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+#define SR 16384                          /* Sampling rate of data (Hz) */
+#define USR 16                            /* Upsampling factor */
+#define MAXALPHAS 100000                    /* Maximum number of calibration factors in a science segment */
+
+#define TESTSTATUS( pstat ) \
+  if ( (pstat)->statusCode ) { REPORTSTATUS(pstat); return 100; } else ((void)0)
+
+/***************************************************************************/
+
+/* STRUCTURES */
+struct CommandLineArgsTag {
+  REAL8 f;                 /* Frequency of the calibration line */
+  REAL8 To;                /* factors integration time */
+  REAL8 G0Re;              /* Real part of open loop gain at cal line freq.*/
+  REAL8 G0Im;              /* Imaginary part of open loop gain at cal line freq. */
+  REAL8 D0Re;              /* Real part of digital filter at cal line freq.*/
+  REAL8 D0Im;              /* Imaginary part of digital filter at cal line freq. */
+  INT4 GPSStart;           /* Start GPS time for the segment to be calibrated */
+  INT4 GPSEnd;             /* End GPS time for the segment to be calibrated */
+  char *FrCacheFile;       /* Frame cache file for corresponding time */
+  char *exc_chan;          /* excitation channel name */    
+  char *darm_chan;         /* darm channel name */ 
+  char *asq_chan;          /* asq channel name */
+} CommandLineArgs;
+
+
+/***************************************************************************/
+
+/* GLOBAL VARIABLES */
+
+StrainIn InputData;
+StrainOut OutputData;
+INT4 duration;
+LIGOTimeGPS gpsStartepoch;
+
+static LALStatus status;
+INT4 lalDebugLevel=3;
+FrCache *framecache;                                           /* frame reading variables */
+FrStream *framestream=NULL;
+
+/***************************************************************************/
+
+/* FUNCTION PROTOTYPES */
+/* Reads the command line */
+int ReadCommandLine(int argc,char *argv[],struct CommandLineArgsTag *CLA);
+
+/* Reads T seconds of AS_Q and DARM_CTRL data */
+int ReadData(struct CommandLineArgsTag CLA);
+
+/* Frees the memory */
+int FreeMem(void);                                        
+
+
+/************************************* MAIN PROGRAM *************************************/
+
+int main(int argc,char *argv[])
+{
+
+  if (ReadCommandLine(argc,argv,&CommandLineArgs)) return 1;
+  if (ReadData(CommandLineArgs)) return 4;
+  
+  LALComputeStrain(&status, &OutputData, &InputData);
+  TESTSTATUS( &status );
+  
+
+  if(FreeMem()) return 8;
+
+  return 0;
+}
+
+/************************************* MAIN PROGRAM ENDS *************************************/
+
+
+/*  FUNCTIONS */
+
+/*******************************************************************************/
+
+
+int ReadData(struct CommandLineArgsTag CLA)
+{
+
+FrPos pos1;
+
+static FrChanIn chanin_darm;
+static FrChanIn chanin_asq;
+static FrChanIn chanin_exc;
+
+
+  chanin_asq.type  = ADCDataChannel;
+  chanin_darm.type = ADCDataChannel;
+  chanin_exc.type  = ADCDataChannel;
+
+  chanin_asq.name  = CLA.asq_chan;
+  chanin_darm.name = CLA.darm_chan;
+  chanin_exc.name  = CLA.exc_chan; 
+
+  /* create Frame cache, open frame stream and delete frame cache */
+  LALFrCacheImport(&status,&framecache,CommandLineArgs.FrCacheFile);
+  TESTSTATUS( &status );
+  LALFrCacheOpen(&status,&framestream,framecache);
+  TESTSTATUS( &status );
+  LALDestroyFrCache(&status,&framecache);
+  TESTSTATUS( &status );
+
+  /* Get channel time step size by calling LALFrGetREAL4TimeSeries */
+  LALFrSeek(&status,&gpsStartepoch,framestream);
+  TESTSTATUS( &status );
+  LALFrGetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.AS_Q,&chanin_asq,framestream);
+  TESTSTATUS( &status );
+  LALFrSetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.DARM,&chanin_darm,framestream);
+  TESTSTATUS( &status );
+  LALFrSetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.EXC,&chanin_exc,framestream);
+  TESTSTATUS( &status );
+
+  /* Allocate space for data vectors */
+  LALSCreateVector(&status,&InputData.AS_Q.data,(UINT4)(duration/InputData.AS_Q.deltaT +0.5));
+  TESTSTATUS( &status );
+  LALSCreateVector(&status,&InputData.DARM.data,(UINT4)(duration/InputData.DARM.deltaT +0.5));
+  TESTSTATUS( &status );
+  LALSCreateVector(&status,&InputData.EXC.data,(UINT4)(duration/InputData.EXC.deltaT +0.5));
+  TESTSTATUS( &status );
+
+  /* Read in the data */
+  LALFrSetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.AS_Q,&chanin_asq,framestream);
+  TESTSTATUS( &status );
+  
+  LALFrSetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.DARM,&chanin_darm,framestream);
+  TESTSTATUS( &status );
+
+  LALFrSetPos(&status,&pos1,framestream);
+  TESTSTATUS( &status );
+  LALFrGetREAL4TimeSeries(&status,&InputData.EXC,&chanin_exc,framestream);
+  TESTSTATUS( &status );
+
+  LALFrClose(&status,&framestream);
+  TESTSTATUS( &status );
+
+  /* Set the rest of the input variables */
+  InputData.Go.re=CLA.G0Re;
+  InputData.Go.im=CLA.G0Im;
+  InputData.Do.re=CLA.D0Re;
+  InputData.Do.im=CLA.D0Im;
+  InputData.f=CLA.f;
+  InputData.To=CLA.To;
+
+  /* Allocate output data */
+  OutputData.ht.deltaT=InputData.AS_Q.deltaT;
+  LALDCreateVector(&status,&OutputData.ht.data,(UINT4)(duration/OutputData.ht.deltaT +0.5));
+  TESTSTATUS( &status );
+
+  OutputData.alpha.deltaT=CLA.To;
+  LALZCreateVector(&status,&OutputData.alpha.data,(UINT4)(duration/OutputData.alpha.deltaT +0.5));
+  TESTSTATUS( &status );
+
+  OutputData.beta.deltaT=CLA.To;
+  LALZCreateVector(&status,&OutputData.beta.data,(UINT4)(duration/OutputData.beta.deltaT +0.5));
+  TESTSTATUS( &status );
+
+  return 0;
+}
+
+/*******************************************************************************/
+
+int ReadCommandLine(int argc,char *argv[],struct CommandLineArgsTag *CLA) 
+{
+  INT4 c, errflg = 0;
+  optarg = NULL;
+  
+  /* Initialize default values */
+  CLA->f=0.0;
+  CLA->To=0.0;
+  CLA->FrCacheFile=NULL;
+  CLA->exc_chan=NULL;
+  CLA->darm_chan=NULL;
+  CLA->asq_chan=NULL;
+  CLA->GPSStart=0;
+  CLA->GPSEnd=0;
+  CLA->G0Re=0.0;
+  CLA->G0Im=0.0;
+  CLA->D0Re=0.0;
+  CLA->D0Im=0.0;
+
+  /* Scan through list of command line arguments */
+  while (!errflg && ((c = getopt(argc, argv,"hf:F:A:E:D:s:e:i:j:k:l:t:"))!=-1))
+    switch (c) {
+    case 'f':
+      /* calibration line frequency */
+      CLA->f=atof(optarg);
+      break;
+    case 't':
+      /* calibration line frequency */
+      CLA->To=atof(optarg);
+      break;
+    case 'F':
+      /* name of frame cache file */
+      CLA->FrCacheFile=optarg;
+      break;
+    case 'E':
+      /* name of excitation channel */
+      CLA->exc_chan=optarg;
+      break;    
+    case 'A':
+      /* name of as_q channel */
+      CLA->asq_chan=optarg;
+      break;    
+    case 'D':
+      /* name of darm channel */
+      CLA->darm_chan=optarg;
+      break;    
+    case 's':
+      /* calibration line frequency */
+      CLA->GPSStart=atof(optarg);
+      break;
+    case 'e':
+      /* calibration line frequency */
+      CLA->GPSEnd=atof(optarg);
+      break;
+    case 'i':
+      /* calibration line frequency */
+      CLA->G0Re=atof(optarg);
+      break;
+    case 'j':
+      /* calibration line frequency */
+      CLA->G0Im=atof(optarg);
+      break;
+    case 'k':
+      /* calibration line frequency */
+      CLA->D0Re=atof(optarg);
+      break;
+    case 'l':
+      /* calibration line frequency */
+      CLA->D0Im=atof(optarg);
+      break;
+   case 'h':
+      /* print usage/help message */
+      fprintf(stdout,"All arguments are required. They are:\n");
+      fprintf(stdout,"\t-f\tFLOAT\t Calibration line frequency in Hz.\n");
+      fprintf(stdout,"\t-t\tFLOAT\t Factors integration time in seconds.\n");
+      fprintf(stdout,"\t-i\tFLOAT\t Real part of the open loop gain at the calibration line frequency.\n");
+      fprintf(stdout,"\t-j\tFLOAT\t Imaginary part of the open loop gain at the calibration line frequency.\n");
+      fprintf(stdout,"\t-k\tFLOAT\t Real part of the digital filter at the calibration line frequency.\n");
+      fprintf(stdout,"\t-l\tFLOAT\t Imaginary part of digital filter at the calibration line frequency.\n");
+      fprintf(stdout,"\t-s\tINT\t GPS start time.\n");
+      fprintf(stdout,"\t-e\tINT\t GPS end time.\n");
+      fprintf(stdout,"\t-F\tSTRING\t Name of frame cache file.\n");
+      fprintf(stdout,"\t-A\tSTRING\t AS_Q channel name (eg, L1:LSC-AS_Q).\n");
+      fprintf(stdout,"\t-E\tSTRING\t Excitation channel name (eg, L1:LSC-ETMX_EXC_DAQ)\n");
+      fprintf(stdout,"\t-D\tSTRING\t Darm channel name (eg, L1:LSC-DARM_CTRL)\n");
+      exit(0);
+      break;
+    default:
+      /* unrecognized option */
+      errflg++;
+      fprintf(stderr,"Unrecognized option argument %c\n",c);
+      exit(1);
+      break;
+    }
+  if(CLA->f == 0.0)
+    {
+      fprintf(stderr,"No calibration line frequency specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+  if(CLA->To == 0.0)
+    {
+      fprintf(stderr,"No integration time for the factors specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+  if(CLA->G0Re == 0.0 )
+    {
+      fprintf(stderr,"No real part of open loop gain specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->G0Im == 0.0 )
+    {
+      fprintf(stderr,"No imaginary part of open loop gain specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->D0Re == 0.0 )
+    {
+      fprintf(stderr,"No real part of digital filter specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->D0Im == 0.0 )
+    {
+      fprintf(stderr,"No imaginary part of digital filter specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->GPSStart == 0)
+    {
+      fprintf(stderr,"No GPS start time specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->GPSEnd == 0)
+    {
+      fprintf(stderr,"No GPS end time specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }
+  if(CLA->FrCacheFile == NULL)
+    {
+      fprintf(stderr,"No frame cache file specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+   if(CLA->exc_chan == NULL)
+    {
+      fprintf(stderr,"No excitation channel specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+   if(CLA->darm_chan == NULL)
+    {
+      fprintf(stderr,"No darm channel specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+   if(CLA->asq_chan == NULL)
+    {
+      fprintf(stderr,"No asq channel specified.\n");
+      fprintf(stderr,"Try ./ComputeStrainDriver -h \n");
+      return 1;
+    }      
+
+   /* Set some global variables */
+   duration = CLA->GPSEnd - CLA->GPSStart;
+   gpsStartepoch.gpsSeconds = CLA->GPSStart;
+   gpsStartepoch.gpsNanoSeconds = 0;
+
+  return errflg;
+}
+
+/*******************************************************************************/
+
+int FreeMem(void)
+{
+
+  LALSDestroyVector(&status,&InputData.AS_Q.data);
+  TESTSTATUS( &status );
+  LALSDestroyVector(&status,&InputData.DARM.data);
+  TESTSTATUS( &status );
+  LALSDestroyVector(&status,&InputData.EXC.data);
+  TESTSTATUS( &status );
+
+  LALDDestroyVector(&status,&OutputData.ht.data);
+  TESTSTATUS( &status );
+
+  LALZDestroyVector(&status,&OutputData.alpha.data);
+  TESTSTATUS( &status );
+
+  LALZDestroyVector(&status,&OutputData.beta.data);
+  TESTSTATUS( &status );
+
+  LALCheckMemoryLeaks();
+ 
+  return 0;
+}
+
+/*******************************************************************************/
+#endif
