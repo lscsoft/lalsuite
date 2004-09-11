8076d70f8e77b5c455e6d6584e6f3bc5165c8b85
kipp <kipp>
2004-09-11 17:45:47 +0000

    Add "non-NULL pointer" error code and message Original: 550c88d9c800c0d6892358e448760e34d65092c7

--- lal/packages/std/include/LALErrno.h
+++ lal/packages/std/include/LALErrno.h
@@ -1,23 +1,27 @@
 /* <lalVerbatim file="LALErrnoHV">
 Author: Cannon, K. C.
-$Id$
+Revision: $Id$
 </lalVerbatim>
  */
 
 #ifndef _LALERRNO_H
 #define _LALERRNO_H
 
-NRCSID(LALERRNOH, "$Id$");
-
-/* Error codes and corresponding error messages. */
+/*
+ * Error codes and corresponding error messages.
+ */
 
 #define LAL_FAIL_ERR	1
 #define LAL_FAIL_MSG	"operation failed"
 #define LAL_NULL_ERR	2
 #define LAL_NULL_MSG	"unexpected NULL pointer"
-#define LAL_NOMEM_ERR	3
+#define LAL_NNULL_ERR	3
+#define LAL_NNULL_MSG	"unexpected non-NULL pointer"
+#define LAL_NOMEM_ERR	4
 #define LAL_NOMEM_MSG	"out of memory"
-#define LAL_RANGE_ERR	4
+#define LAL_RANGE_ERR	5
 #define LAL_RANGE_MSG	"parameter out of range"
+#define LAL_BADPARM_ERR 6
+#define LAL_BADPARM_MSG "invalid parameter value"
 
 #endif /* _LALERRNO_H */
