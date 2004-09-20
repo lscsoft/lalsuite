cf5ed5db36ec37bf9df3bf78b9183f15259af2e3
reinhard <reinhard>
2004-09-20 12:53:17 +0000

    added simple tests for 'make check' Original: e81f911049c85d28715d3a61a88b5bae0717665f

---
+++ sftlib/run_validate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+prog=./SFTvalidate
+
+./SFTwrite >& /dev/null
+
+goodSFTs="SFT-good SFT-test*"
+badSFTs="SFT-bad*"
+
+for i in $goodSFTs; do
+    $prog $i
+done
+
+## this SFTs have to yield error-results
+for i in $badSFTs; do
+    res=`$prog $i`
+    if [ ! -n $res ]; then
+	exit -1;
+    fi
+done
