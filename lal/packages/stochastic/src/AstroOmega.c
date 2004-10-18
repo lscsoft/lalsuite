0c63854bd5bb902b96b316918682dd3633cebd01
tania <tania>
2004-10-18 15:01:21 +0000

    *** empty log message *** Original: 7cb91397d237d62386b95d1052fd437007b6a2bb

--- lal/packages/stochastic/src/AstroOmega.c
+++ lal/packages/stochastic/src/AstroOmega.c
@@ -61,18 +61,15 @@ dV = 4\pi r^2{{c}\over{H_o}}{{dz}\over{E(\Omega_i,z)}}
 where the function E$(\Omega_i,z)$ is
 defined by the equation:
 \begin{equation}
-E(\Omega_i,z) =[(1+z)^2(1+z\Omega_m)-z(2+z)\Omega_v]^{1/2}
+E(\Omega_i,z) =[\Omega_m(1+z)^3 + \Omega_v]^{1/2}
 \end{equation}
 where $\Omega_m$ and $\Omega_v$ are respectively the density parameters
-due to matter (baryonic and non-baryonic) and the vacuum, corresponding
-to a non-zero cosmological constant. The equivalent
-density parameter due to the spatial curvature satisfies $\Omega_k =
-1-\Omega_m -\Omega_v$. 
+due to matter (baryonic and non-baryonic) and the vacuum. 
 The cosmic star formation rate is computed as:
 \begin{equation}
 R_{c}(z) = 
 R_{SF2}(z)
-\frac{E(\Omega_i,z)}{(1+z)^{3/2}}\; 
+\frac{h_0}{0.65}\frac{E(\Omega_i,z)}{(1+z)^{3/2}}\; 
 \mathrm{M}_{\odot}\,\mathrm{yr}^{-1}\,\mathrm{Mpc}^{-3}
 \end{equation}
 where 
@@ -80,15 +77,7 @@ where
 R_{SF2}(z) = \frac{0.15\,e^{3.4z}}{(22 + e^{3.4z})} \;
 \mathrm{M}_{\odot}\,\mathrm{yr}^{-1}\,\mathrm{Mpc}^{-3}
 \end{equation}
-is the cosmic star formation rate (Madau \&Porciani, 2001) in a matter-dominateduniverse ($\Omega_{m}=1$).
-We consider an Einstein - de Sitter universe,
-with non-zero cosmological constant and with $\Omega_m$ = 0.30
- and $\Omega_v$ = 0.70.
-These values are suggested by
-recent BOOMERANG and MAXIMA results (de Bernardis et al. 2000; Hanany et al. 2000) on
-the power spectra of the cosmic microwave background and by observations of distant type 
-Ia supernovas (Perlmutter et al. 1999; Schmidt et al. 1998).
-The Hubble parameter H$_o$ is taken to be $73\,\mathrm{km\,s}^{-1}\mathrm{Mpc}^{-1}$ (Freedman et al., 2001).
+is the cosmic star formation rate (Madau \&Porciani, 2001) in a matter-dominateduniverse ($\Omega_{m}=1$) with H$_o = 65\,\mathrm{km\,s}^{-1}\mathrm{Mpc}^{-1}$.
 Combining the previous equations one obtains:
 \begin{equation}
 \Omega _{gw}(\nu _{o})=\frac{8\pi G}{3c^{2}H_{o}^{3}}\lambda_{p}\, \nu_{o}\int_{0}^{z_{\sup }}\frac{dE_{gw}}{d\nu }\frac{R_{c}(z)}{E(z)(1+z)^2}dz
@@ -132,28 +121,28 @@ int main ()
   static LALStatus status;
   DIntegrateIn  zint;
   AstroOmegaGeneralParams params;
-  AstroOmegaGeneralSourceParams gsourcep;
+  AstroOmegaSourceParams sourcep;
   AstroOmegaCosmoParams cosmop;
-  REAL8 omegaz, zmax, nmaxSource, factSource, nu, test;
+  REAL8 omegaz, nu, test;
 
   //define here the model parameters
   //cosmological parameters
-  cosmop.ho=0.73;
+  cosmop.ho=0.65;
   cosmop.density_matter=0.3;
   cosmop.density_vacuum=0.7;
   cosmop.density_k=0.;
 
   //source parameters for rotating pulsars
-  gsourcep.fact=2.88E-22;
-  gsourcep.numax=4000.;
-  gsourcep.SDensitySource=SDensity;
-  params.cosmoparams=cosmop;
-  params.gsourceparams=gsourcep;
-  for (nu=0.; nu < numax ; nu=nu+10.)
-   {params.extraparams=&nu;
-
+  sourcep.fact = 2.88E-22;
+  sourcep.numax = 4000.;
+  sourcep.SDensitySource = SDensity;
+  params.cosmoparams = cosmop;
+  params.sourceparams = sourcep;
+  for (nu = 0.; nu < sourcep.numax ; nu = nu + 10.)
+   {
+    params.extraparams = &nu;
     LALAstroOmegaSource (&status, &test, nu, &params); 
-    printf("omega(%f)= %.2e\n", nu,test);}
+    printf("omega(%f)= %.2e\n", nu, test);}
    return 0;
   }
 
@@ -161,7 +150,7 @@ int main ()
 \end{verbatim}
 
 \subsubsection*{Notes}
-\vfill{\footnotesize\input{AstroOmegaGeneralCV}}
+\vfill{\footnotesize\input{AstroOmegaCV}}
 
 </lalLaTeX> */ 
 
@@ -184,21 +173,21 @@ void LALAstroOmega (LALStatus *s, REAL8 *omeganu, REAL8 nu, void *p)
    AstroOmegaParams params;
    AstroOmegaSourceParams sourcep;
    AstroOmegaCosmoParams cosmop;
-   REAL8 omegaz, zmax, numaxSource, factSource;   
+   REAL8 omegaz, zmax, numax, fact;   
    INITSTATUS (s, "LALAstroOmega", ASTROOMEGAC);
    ATTATCHSTATUSPTR (s);
   
    params = *((AstroOmegaParams *)p);
    cosmop=params.cosmoparams;
    sourcep = params.sourceparams;
-   numaxSource= sourcep.numax;
-   factSource = sourcep.fact;
+   numax= sourcep.numax;
+   fact = sourcep.fact;
 
-   if((nu>=numaxSource)||(nu<=0.)){*omeganu=0.;}
+   if((nu >= numax)||(nu <= 0.)){*omeganu = 0.;}
    else
     {
-     if (nu<(numaxSource/6.)) {zmax=5.;}
-     else {zmax=(numaxSource/nu)-1.;}
+     if (nu < (numax / 6.)) {zmax = 5.;}
+     else {zmax = (numax / nu)-1.;}
  
      zint.function = dAstroOmega;
      zint.xmin     = 0;
@@ -206,7 +195,7 @@ void LALAstroOmega (LALStatus *s, REAL8 *omeganu, REAL8 nu, void *p)
      zint.type     = ClosedInterval;
 
      LALDRombergIntegrate (s->statusPtr, &omegaz, &zint, &params); 
-     *omeganu = factSource*nu*omegaz;
+     *omeganu = fact * nu * omegaz;
      }
   CHECKSTATUSPTR (s);
   DETATCHSTATUSPTR (s);
@@ -229,12 +218,11 @@ static void SFR (REAL8 *result, REAL8 z)
 
 static void dAstroOmega (LALStatus *s, REAL8 *domegaz, REAL8 z, void *p)
  {
-  
   AstroOmegaParams params;
   AstroOmegaSourceParams sourcep; 
   AstroOmegaCosmoParams cosmop;
   REAL8LALSDensity *SDensitySource;
-  REAL8 vRz, dEgw, nu, nuz, numaxSource, factSource;
+  REAL8 Rc, dEgw, nu, nuz, numax, fact;
   
   INITSTATUS (s, "dAstroOmega", ASTROOMEGAC);
   ATTATCHSTATUSPTR (s); 
@@ -242,17 +230,17 @@ static void dAstroOmega (LALStatus *s, REAL8 *domegaz, REAL8 z, void *p)
   cosmop = params.cosmoparams;
   sourcep = params.sourceparams;
   SDensitySource = sourcep.SDensitySource;
-  numaxSource = sourcep.numax;
-  factSource = sourcep.fact;
+  numax = sourcep.numax;
+  fact = sourcep.fact;
   nu = *((REAL8 *)params.extraparams);
   
   /*frequency in the source frame*/
-  nuz = (1.+z) * nu;
+  nuz = (1. + z) * nu;
   /*single spectral energy density in the source frame*/
   SDensitySource(&dEgw, nuz);
   /*cosmic formation rate*/
-  SFR(&vRz, z); 
-  *domegaz = dEgw * vRz / pow((1.+z),3.5);
+  SFR(&Rc, z); 
+  *domegaz = dEgw * (cosmop.ho / 0.65) * Rc / pow((1.+z),3.5);
   CHECKSTATUSPTR (s);
   DETATCHSTATUSPTR (s);
   RETURN (s);
