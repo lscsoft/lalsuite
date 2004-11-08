3d230cb367a3c3491bb67ec734cbbb6994590301
reinhard <reinhard>
2004-11-08 12:43:01 +0000

    - allow specifying test-code as argument $1 - add timing output Original: e7d6b8adfd6ec1826e4d192755870610d6d9c5a5

--- lalapps/src/pulsar/Injections/testMakefakedata.sh
+++ lalapps/src/pulsar/Injections/testMakefakedata.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
 testDIR=mfd_TEST
+  
+oldcode=./makefakedata_test
+newcodeDEFAULT=./lalapps_Makefakedata
+compCode=./compareSFTs
+
+if [ -z "$1" ]; then
+    newcode=${newcodeDEFAULT}
+else
+    newcode="$1"
+    echo "'$newcode'"
+fi
 
 #prepare test subdirectory
-if [ ! -d $testDIR ]; then
-    mkdir $testDIR && cd $testDIR;
+if [ ! -d "$testDIR" ]; then
+    mkdir $testDIR
 else
 ## cleanup: remove previous output-SFTs
     cd $testDIR;
     rm SFTtest_v2.00* || true
     rm SFTtest_v4.00* || true
+    cd ..
 fi
-  
-oldcode=../makefakedata_test
-newcode=../lalapps_Makefakedata
 
-if [ ! -x $oldcode ]; then
+if [ ! -x "$oldcode" ]; then
     echo "Cannot run reference-code $oldcode!"
     exit -1;
-elif [ ! -x $newcode ]; then
+elif [ ! -x "$newcode" ]; then
     echo "Cannot run test-code $newcode!"
     exit -1;
 fi
 
 # signal parameters
 IFO=LLO
-ephemdir=../ephems
+ephemdir=./ephems
 startTime=731210229
+timestamps=./testT8_1800
 refTime=$startTime
 Tsft=1800
-nTsft=10
+nTsft=20
 fmin=300.0
-Band=0.4
+Band=10.0
 aPlus=1.5
 aCross=0.7
 psi=0.5
@@ -39,19 +49,19 @@ phi0=0.9
 f0=300.2
 alpha=1.7
 delta=0.9
-noiseDir="../../"
+noiseDir="../"
 noiseSFTs="$noiseDir/SFT.0000[0-9]"
 
 dataTMP=In.data-test
-oldCL="-i $dataTMP  -I $IFO -E $ephemdir -G $startTime -S $refTime -n SFTtest_v2 -D $noiseDir"
-newCL="--Tsft=$Tsft --nTsft=$nTsft --fmin=$fmin --Band=$Band --aPlus=$aPlus --aCross=$aCross --psi=$psi --phi0=$phi0 --f0=$f0 --latitude=$delta  --longitude=$alpha --detector=$IFO --ephemDir=$ephemdir --outSFTbname=SFTtest_v4 --startTime=$startTime --refTime=$refTime -D$noiseSFTs"
+oldCL="-i $dataTMP  -I $IFO -E $ephemdir -S $refTime -n ${testDIR}/SFTtest_v2" ## -D $noiseDir"
+newCL="--Tsft=$Tsft --nTsft=$nTsft --fmin=$fmin --Band=$Band --aPlus=$aPlus --aCross=$aCross --psi=$psi --phi0=$phi0 --f0=$f0 --latitude=$delta  --longitude=$alpha --detector=$IFO --ephemDir=$ephemdir --outSFTbname=${testDIR}/SFTtest_v4 --timestampsFile=$timestamps --refTime=$refTime" ## -D$noiseSFTs -v1"
 
 ## produce In.data file for makefakedata_v2
 echo "$Tsft	%Tsft_in_sec
 $nTsft	%nTsft
 $fmin   %first_SFT_frequency_in_Hz	
 $Band	%SFT_freq_band_in_Hz
--1.0	%sigma_(std_of_noise.When=0_only_signal_present)
+0.0	%sigma_(std_of_noise.When=0_only_signal_present)
 $aPlus	%Aplus
 $aCross	%Across
 $psi	%psi
@@ -60,28 +70,27 @@ $f0	%f0
 $delta	%latitude_in_radians
 $alpha	%longitude_in_radians
 0	%max_spin-down_param_order
-T8	%name_of_time-stamps_file
+$timestamps 	%name_of_time-stamps_file
 " > In.data-test
 
 echo "1) Testing isolated pulsar-signal without noise"
-echo "Running 'reference-code' $oldcode":
-if ! $oldcode $oldCL; then
-    echo "...failed. Exiting."
-    exit
-fi
+echo
+echo "Running 'reference-code':"
+echo "$oldcode $oldCL"
+
+time $oldcode $oldCL
 
 # remove temporary In.data - file
 #rm $dataTMP
+echo
+echo "Running new Makefakedata code:"
+echo "$newcode $newCL"
+time $newcode $newCL
 
-echo "Running new code $newcode:"
-echo "Cmd-line args: $newCL"
-if ! $newcode $newCL; then
-    echo "...failed. Exiting."
-    exit
-fi
+echo
+echo "comparison of resulting SFTs:"
 
-echo "comparison:"
-../compareSFTs -1 "./SFTtest_v2*" -2 "./SFTtest_v4*"
+$compCode -1 "${testDIR}/SFTtest_v2*" -2 "${testDIR}/SFTtest_v4*"
 
 
 cd ..
