5530f150e921a09b647f898042380be1f0ccad89
ram <ram>
2004-11-20 18:30:00 +0000

    header file changes for stochastic.c 1.192 Original: 99e09e67f71dde3142263a9cdba7da9a791e9ae2

--- lalapps/src/stochastic/stochastic.h
+++ lalapps/src/stochastic/stochastic.h
@@ -35,16 +35,18 @@ static REAL4TimeSeries *get_ligo_data(LALStatus *status, FrStream *stream,
 		CHAR *channel, LIGOTimeGPS start, LIGOTimeGPS end);
 static REAL4TimeSeries *get_geo_data(LALStatus *status, FrStream *stream,
 		CHAR *channel, LIGOTimeGPS start, LIGOTimeGPS end);
-static REAL8 delta_gps_to_float(LALStatus *status, LIGOTimeGPS *end,
-		LIGOTimeGPS *start);
+static REAL8 delta_gps_to_float(LALStatus *status, LIGOTimeGPS end,
+		LIGOTimeGPS start);
 static REAL4FrequencySeries *omega_gw(LALStatus *status, REAL4 alpha,
 		REAL8 fRef, REAL4 omegaRef, UINT4 length, REAL8 f0, REAL8 deltaF,
     LIGOTimeGPS time);
 static REAL4FrequencySeries *overlap_reduction_function(LALStatus *status,
     UINT4 length, REAL8 f0, REAL8 deltaF, INT4 siteOne, INT4 siteTwo,
     LIGOTimeGPS time);
-static LIGOTimeGPS increment_gps(LALStatus *status, LIGOTimeGPS *time,
+static LIGOTimeGPS increment_gps(LALStatus *status, LIGOTimeGPS time,
 		INT4 increment);
+static REAL4TimeSeries *cut_time_series(LALStatus *status,
+		REAL4TimeSeries *input, LIGOTimeGPS start, LIGOTimeGPS end);
 
 #ifdef  __cplusplus
 }
