8030cb19e6fc73a8c558f5243e9dff5f22eca7a2
hammerd <hammerd>
2004-12-10 19:27:28 +0000

    *** empty log message *** Original: bc9ed6651799db4121f42f8211ce53dfeb880dea

--- lalapps/src/pulsar/make_job_input_scripts.sh
+++ lalapps/src/pulsar/make_job_input_scripts.sh
@@ -24,7 +24,7 @@ do
   cat $a1 \
    | awk '{ N+=$1 ; print $1, $2, $3, N}' \
    | awk '{ if ($4 > '$jobtotal'*'$jobn'/'$njobs' ) if ($4 <= '$jobtotal'*('$jobn'+1)/'$njobs') print $1, $2, $3, $4, '$jobn'}' \
-   | awk '{printf "%s" "%05d" "%s\n", "LSCdataFind --server=dataserver.phys.uwm.edu --observatory '$a2' --type '$a3'_RDS_C01_LX --gps-start-time "$2" --gps-end-time "$2+$1*$3" --url-type file --match localhost | sed s,file://localhost,, >> '$a4'/jobdata.", ""$5"", ".ffl" ; for (sft=0; sft < $1; ++sft) printf "%s" "%05d""%s\n",  "echo "$2+sft*$3" "$3" >> '$a4'/jobtimes.", ""$5"", "" }'
+   | awk '{printf "%s" "%05d" "%s\n", "LSCdataFind --server=dataserver.phys.uwm.edu --observatory '$a2' --type '$a3'_RDS_C02_LX --gps-start-time "$2" --gps-end-time "$2+$1*$3" --url-type file --match localhost | sed s,file://localhost,, >> '$a4'/jobdata.", ""$5"", ".ffl" ; for (sft=0; sft < $1; ++sft) printf "%s" "%05d""%s\n",  "echo "$2+sft*$3" "$3" >> '$a4'/jobtimes.", ""$5"", "" }'
   let "jobn+=1" 
 done
 echo "exit 0"
