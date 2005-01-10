8e0471427608f23f3c15f75f24113d1f5ef3562f
siemens <siemens>
2005-01-10 20:53:49 +0000

    added coincidence Original: 17f76bb4084e922b6698c649d1967e2d77c11bd1

--- lalapps/src/string/cosmicstring.py
+++ lalapps/src/string/cosmicstring.py
@@ -72,6 +72,31 @@ class StringJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('string.sub')
     
 
+class BurcaJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_burca job used by the power pipeline. The static options are
+  read from the section [burca] in the ini file.  The stdout and stderr from
+  the job are directed to the logs directory.  The path to the executable is 
+  determined from the ini file.
+  """
+  def __init__(self,cp):
+    """
+    cp = ConfigParser object from which options are read.
+    """
+    self.__executable = cp.get('condor','burca')
+    self.__universe = cp.get('condor','universe')
+    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
+    pipeline.AnalysisJob.__init__(self,cp)
+    
+    for sec in ['burca']:
+      self.add_ini_opts(cp,sec)
+
+    self.set_stdout_file('logs/burca-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).out')
+    self.set_stderr_file('logs/burca-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).err')
+    self.set_sub_file('burca.sub')
+
+
+
 class DataFindNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
   """
   A DataFindNode runs an instance of datafind in a Condor DAG.
@@ -152,17 +177,76 @@ class StringNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     synchronized with the name of the output file in ring.c.
     """
     if not self.get_start() or not self.get_end() or not self.get_ifo():
-      raise RingError, "Start time, end time or ifo has not been set"
+      raise StringError, "Start time, end time or ifo has not been set"
 
-    basename = self.get_ifo() + '-STRING'
-
-    if self.get_ifo_tag():
-      basename += '_' + self.get_ifo_tag()
-    if self.__usertag:
-      basename += '_' + self.__usertag
+    basename = self.get_ifo() + '-STRINGSEARCH'
 
     return basename + '-' + str(self.get_start()) + '-' + \
       str(self.get_end() - self.get_start()) + '.xml'
 
+
+class BurcaNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
+  """
+  A BurcaNode runs an instance of the burca code in a Condor DAG.
+  """
+  def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_burca.
+    """
+    pipeline.CondorDAGNode.__init__(self,job)
+    pipeline.AnalysisNode.__init__(self)
+    self.__usertag = job.get_config('pipeline','user-tag')
+    self.__start = 0
+    self.__end = 0
+    self.__output = None
+
+  def __set_output(self):
+    """
+    Private method to set the file to write the cache to. Automaticaly set
+    once the ifo, start and end times have been set.
+    """
+    if self.__start and self.__end :
+      self.__output = 'coincidences/' + 'HL' + '-' + str(self.__start) 
+      self.__output = self.__output + '-' + str(self.__end) + '.xml'
+                     
+  def set_ifoa(self,file):
+    """
+    Set the LAL frame cache to to use. The frame cache is passed to the job
+    with the --frame-cache argument.
+    @param file: calibration file to use.
+    """
+    self.add_var_opt('ifo-a', file)
+    self.add_input_file(file)
+
+  def set_ifob(self,file):
+    """
+    Set the LAL frame cache to to use. The frame cache is passed to the job
+    with the --frame-cache argument.
+    @param file: calibration file to use.
+    """
+    self.add_var_opt('ifo-b', file)
+    self.add_input_file(file)   
+
+  def set_start(self,time):
+    """
+    Set the start time of the datafind query.
+    time = GPS start time of query.
+    """
+    self.__start = time
+    self.__set_output()
+
+  def set_end(self,time):
+    """
+    Set the end time of the datafind query.
+    time = GPS end time of query.
+    """
+    self.__end = time
+    self.__set_output()
+
+  def get_output(self):
+    """
+    Return the output file, i.e. the file containing the frame cache data.
+    """
+    return self.__output
 
 
