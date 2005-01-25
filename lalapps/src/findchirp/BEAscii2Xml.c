0a97ab8ebb69b7272f616542b7abccf0039393ac
thomas <thomas>
2005-01-25 16:54:06 +0000

    take into account modification of the output Original: d58fd5ce3ea585372591af36bb2f3c65843326f4

--- lalapps/src/findchirp/BEAscii2Xml.c
+++ lalapps/src/findchirp/BEAscii2Xml.c
@@ -21,6 +21,7 @@
  	Thomas Cokelaer April 2004
 
 [Compilation]
+        gcc BEAscii2Xml.c -o BEAscii2Xml  -I. -I/home/cokelaer/share/include -I../lalapps -I/software/geopptools/include -I../
 	gcc BEAscii2Xml.c -o BEAscii2Xml -I/<include file>
 */
 
@@ -28,65 +29,24 @@
 #include <stdlib.h>
 
 #include <lal/LIGOLwXMLHeaders.h>
-
+#include "BankEfficiency.h"
 
 #define BEASCII2XML_INPUT1 "Trigger.dat"
 #define BEASCII2XML_INPUT2 "BE_Proto.xml"
-
 #define BEASCII2XML_OUTPUT "Trigger.xml"
 
 
-#define BANKEFFICIENCY_PARAMS_OUTPUT \
-"         %f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,"
 
 #define BANKEFFICIENCY_PARAMS_INPUT \
-"         %f %f %f %f %f %f %f %f %f %f %f %f %f %f %d %d "
+"         %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %d %d %f %f %f %f %d %d %d"
 
 
-#define LIGOLW_XML_BANKEFFICIENCY \
-"   <Table Name=\"bankefficiencygroup:bankefficiency:table\">\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:psi0T\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:psi3T\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:psi0I\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:psi3I\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:fT\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:fI\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:totalMassT\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:etaT\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:mass1I\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:mass2I\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:overlap\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:phase\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:alpha\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:alpha_f\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:layerT\" Type=\"real_4\"/>\n" \
-"      <Column Name=\"bankefficiencygroup:bankefficiency:bin\" Type=\"real_4\"/>\n" \
-"      <Stream Name=\"bankefficiencygroup:bankefficiency:table\" Type=\"Local\" Delimiter=\",\">\n"
-
-typedef struct{
-  REAL4 psi0_trigger;
-  REAL4 psi3_trigger;
-  REAL4 psi0_inject;
-  REAL4 psi3_inject;
-  REAL4 fend_trigger;
-  REAL4 fend_inject;
-  REAL4 mass1_inject;
-  REAL4 mass2_inject;
-  REAL4 totalMass_trigger;
-  REAL4 eta_trigger;
-  INT4 layer;
-  REAL4 rho_final;
-  REAL4 alpha;
-  REAL4 alpha_f;
-  INT4 bin;
-  REAL4 phase;
-  UINT4 ntrial;
-} ResultIn;
 
 
 int
 main (int argc, char **argv ) 
 {
+  UINT4 start = 0;
   ResultIn trigger;
   FILE *input1, *input2;
   FILE *output;
@@ -106,7 +66,7 @@ main (int argc, char **argv )
   }
   else 
     {
-      /*lire le proto et sauver dans ouput*/
+      /* read prototype and save in outputfile */
       while(fgets(sbuf,512,input2) !=NULL)
 	fputs(sbuf, output);
       
@@ -114,44 +74,73 @@ main (int argc, char **argv )
 
     fprintf(output,"%s", LIGOLW_XML_BANKEFFICIENCY);
 
-  /* lit ascii input and save in xml format */
-  while(!feof(input1))
+  /* read ascii input and save in xml format */
+    do  
     {
       fscanf(input1,BANKEFFICIENCY_PARAMS_INPUT,
-	     &trigger.psi0_trigger,
-	      &trigger.psi3_trigger,
-	      &trigger.psi0_inject, 
-	      &trigger.psi3_inject,
-	      &trigger.fend_trigger, 
-	      &trigger.fend_inject,
-	      &trigger.totalMass_trigger,
-	      &trigger.eta_trigger,
-	      &trigger.mass1_inject,
-	      &trigger.mass2_inject,
-	      &trigger.rho_final,
-	      &trigger.phase,
-	      &trigger.alpha,
-	      &trigger.alpha_f, 
-	      &trigger.layer,
-	      &trigger.bin);
-      fprintf(output, BANKEFFICIENCY_PARAMS_OUTPUT"\n",
-	      trigger.psi0_trigger,
-	      trigger.psi3_trigger,
-	      trigger.psi0_inject, 
-	      trigger.psi3_inject,
-	      trigger.fend_trigger, 
-	      trigger.fend_inject,
-	      trigger.totalMass_trigger,
-	      trigger.eta_trigger,
-	      trigger.mass1_inject,
-	      trigger.mass2_inject,
-	      trigger.rho_final,
-	      trigger.phase,
-	      trigger.alpha,
-	      trigger.alpha_f, 
-	      trigger.layer,
-	      trigger.bin);
+	      &trigger.psi0_trigger,
+	    &trigger.psi3_trigger,
+	    &trigger.psi0_triggerC,
+	    &trigger.psi3_triggerC,
+	    &trigger.psi0_inject, 
+	    &trigger.psi3_inject,
+	    &trigger.fend_trigger, 
+	    &trigger.fend_triggerC, 
+	    &trigger.fend_inject,
+	    &trigger.totalMass_trigger,
+	    &trigger.eta_trigger,
+	    &trigger.mass1_inject,
+	    &trigger.mass2_inject,
+	    &trigger.rho_final,
+	    &trigger.phase,
+	    &trigger.alpha,
+	    &trigger.alpha_f, 
+	    &trigger.layer,
+	    &trigger.bin,
+	    &trigger.rho_finalC,
+	    &trigger.phaseC,
+	    &trigger.alphaC,
+	    &trigger.alpha_fC, 
+	    &trigger.layerC,
+	     &trigger.binC, &trigger.coaTime
+
+
+	     );
+      if (start==0){
+	      start+=1;
+      }
+      else 
+      {
+	      fprintf(output,"\n");
+      }
+      fprintf(output, BANKEFFICIENCY_PARAMS_ROW,
+	     trigger.psi0_trigger,
+	    trigger.psi3_trigger,
+	    trigger.psi0_triggerC,
+	    trigger.psi3_triggerC,
+	    trigger.psi0_inject, 
+	    trigger.psi3_inject,
+	    trigger.fend_trigger, 
+	    trigger.fend_triggerC, 
+	    trigger.fend_inject,
+	    trigger.totalMass_trigger,
+	    trigger.eta_trigger,
+	    trigger.mass1_inject,
+	    trigger.mass2_inject,
+	    trigger.rho_final,
+	    trigger.phase,
+	    trigger.alpha,
+	    trigger.alpha_f, 
+	    trigger.layer,
+	    trigger.bin,
+	    trigger.rho_finalC,
+	    trigger.phaseC,
+	    trigger.alphaC,
+	    trigger.alpha_fC, 
+	    trigger.layerC,
+	      trigger.binC, trigger.coaTime);
     }
+   while(!feof(input1));
   fprintf(output,"%s", LIGOLW_XML_TABLE_FOOTER);
   fprintf(output,"%s", LIGOLW_XML_FOOTER);
 
