378b30f674b958477f62546ff8545e15bc86b9d6
bema <bema>
2005-02-01 10:54:28 +0000

    reserve the last percent of progress for polka if running it Original: f9d82071d1ae2bc1f727b6683b428d1fd489a4e7

--- lalapps/src/pulsar/FDS_isolated/ComputeFStatistic.c
+++ lalapps/src/pulsar/FDS_isolated/ComputeFStatistic.c
@@ -669,11 +669,12 @@ int main(int argc,char *argv[])
       /* Show some progress */
 #if USE_BOINC
       {
+        /* update progress, the last % is reserved for polka in polka commandline runs */
         double local_fraction_done;
         if (cfsRunNo == 1)
-          local_fraction_done=(((double)loopcounter)/((double)thisScan.numGridPoints))/2;
+          local_fraction_done=(((double)loopcounter)/((double)thisScan.numGridPoints))*0.9/2;
         else if (cfsRunNo == 2)
-          local_fraction_done=(((double)loopcounter)/((double)thisScan.numGridPoints))/2+0.5;
+          local_fraction_done=(((double)loopcounter)/((double)thisScan.numGridPoints))*0.9/2+0.45;
         else
           local_fraction_done=((double)loopcounter)/((double)thisScan.numGridPoints);
         if (local_fraction_done<0.0)
