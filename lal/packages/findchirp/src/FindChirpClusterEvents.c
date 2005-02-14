ee14e3236a9d989d30100a06b73b3808f45011db
patrick <patrick>
2005-02-14 00:51:34 +0000

    fixed the indenting so that Duncan does not throw a wobbler Original: ea1cf8c941ebf13e1b609a4073eeb612d461773c

--- lal/packages/findchirp/src/FindChirpClusterEvents.c
+++ lal/packages/findchirp/src/FindChirpClusterEvents.c
@@ -28,123 +28,124 @@ NRCSID (FINDCHIRPCLUSTEREVENTSC, "$Id$");
 /* <lalVerbatim file="FindChirpClusterEventsCP"> */
 void
 LALFindChirpClusterEvents (
-		LALStatus                  *status,
-		SnglInspiralTable         **eventList,
-		FindChirpFilterInput       *input,
-		FindChirpFilterParams      *params,
-		COMPLEX8                   *q,
-		UINT4                       kmax,
-                UINT4                 numPoints,
-                UINT4                 deltaEventIndex,
-                UINT4                       ignoreIndex,
-		REAL4                       norm,
-                REAL4                 modqsqThresh,
-                REAL4                 chisqThreshFac,
-		UINT4                       numChisqBins,
-		CHAR                        searchName[LIGOMETA_SEARCH_MAX] 
-		)
+    LALStatus                  *status,
+    SnglInspiralTable         **eventList,
+    FindChirpFilterInput       *input,
+    FindChirpFilterParams      *params,
+    COMPLEX8                   *q,
+    UINT4                       kmax,
+    UINT4                       numPoints,
+    UINT4                       deltaEventIndex,
+    UINT4                       ignoreIndex,
+    REAL4                       norm,
+    REAL4                       modqsqThresh,
+    REAL4                       chisqThreshFac,
+    UINT4                       numChisqBins,
+    CHAR                        searchName[LIGOMETA_SEARCH_MAX] 
+    )
 /* </lalVerbatim> */
-
 {
-	UINT4                 eventStartIdx = 0;
-        UINT4                 j;
-        SnglInspiralTable    *thisEvent;
+  UINT4                 eventStartIdx = 0;
+  UINT4                 j;
+  SnglInspiralTable    *thisEvent = NULL;
 
-	INITSTATUS( status, "LALFindChirpClusterEvents", FINDCHIRPCLUSTEREVENTSC );
-	ATTATCHSTATUSPTR( status );
-
-
-	/*
-	 *
-	 * check that the arguments are reasonable
-	 *
-	 */
+  INITSTATUS( status, "LALFindChirpClusterEvents", FINDCHIRPCLUSTEREVENTSC );
+  ATTATCHSTATUSPTR( status );
 
 
-	/* make sure the output handle exists, but points to a null pointer */
-	/*ASSERT( eventList, status, FINDCHIRPH_ENULL, FINDCHIRPH_MSGENULL );
-	  ASSERT( !*eventList, status, FINDCHIRPH_ENNUL, FINDCHIRPH_MSGENNUL );*/
-
-	/* make sure the event handle exists, but points to a null pointer */
-	/*ASSERT( thisEvent, status, FINDCHIRPH_ENULL, FINDCHIRPH_MSGENULL );
-	  ASSERT( !*thisEvent, status, FINDCHIRPH_ENNUL, FINDCHIRPH_MSGENNUL );*/
-
-	/* look for an events in the filter output */
-	for ( j = ignoreIndex; j < numPoints - ignoreIndex; ++j )
-	{
-		REAL4 modqsq = q[j].re * q[j].re + q[j].im * q[j].im;
-
-		/* if snrsq exceeds threshold at any point */
-		if ( modqsq > modqsqThresh )
-		{
-
-			/* if we have don't have a chisq or the chisq drops below the       */
-			/* modified chisq threshold, start processing events                */
-			if ( ! input->segment->chisqBinVec->length ||
-					params->chisqVec->data[j] < 
-					(params->chisqThresh * ( 1.0 + modqsq * chisqThreshFac )) )
-			{
-
-				/*
-				 *
-				 * find the local maximum of the event
-				 *
-				 */
+  /*
+   *
+   * check that the arguments are reasonable
+   *
+   */
 
 
-				if ( ! *eventList )
-				{
-					/* store the start of the crossing */
-					eventStartIdx = j;
+  /* make sure the output handle exists, but points to a null pointer */
+  ASSERT( eventList, status, FINDCHIRPH_ENULL, FINDCHIRPH_MSGENULL );
+  ASSERT( !*eventList, status, FINDCHIRPH_ENNUL, FINDCHIRPH_MSGENNUL );
 
-					/* if this is the first event, start the list */
-					thisEvent = *eventList = (SnglInspiralTable *) 
-						LALCalloc( 1, sizeof(SnglInspiralTable) );
-					if ( ! thisEvent )
-					{
-						ABORT( status, FINDCHIRPH_EALOC, FINDCHIRPH_MSGEALOC );
-					}
+  /*
+   *
+   * Apply one of the clustering algorithms
+   *
+   */
 
-					/* record the data that we need for the clustering algorithm */
-					thisEvent->end_time.gpsSeconds = j;
-					thisEvent->snr = modqsq;
-				}
-				else if ( params->maximiseOverChirp &&
-						j <= thisEvent->end_time.gpsSeconds + deltaEventIndex &&
-						modqsq > thisEvent->snr )
-				{
-					/* if this is the same event, update the maximum */
-					thisEvent->end_time.gpsSeconds = j;
-					thisEvent->snr = modqsq;
-				}
-				else if ( j > thisEvent->end_time.gpsSeconds + deltaEventIndex ||
-						! params->maximiseOverChirp )
-				{
-					/* clean up this event */
-					SnglInspiralTable *lastEvent;
-					/*XXX call FindChirpStoreEvent XXX*/
-					LALFindChirpStoreEvent(status->statusPtr, eventList, params,
-							thisEvent, q, kmax, norm, eventStartIdx, numChisqBins, searchName );
+  /* look for an events in the filter output */
+  for ( j = ignoreIndex; j < numPoints - ignoreIndex; ++j )
+  {
+    REAL4 modqsq = q[j].re * q[j].re + q[j].im * q[j].im;
 
-					/* store the start of the crossing */
-					eventStartIdx = j;
+    /* if snrsq exceeds threshold at any point */
+    if ( modqsq > modqsqThresh )
+    {
 
-					/* allocate memory for the newEvent */
-					lastEvent = thisEvent;
+      /* if we have don't have a chisq or the chisq drops below the       */
+      /* modified chisq threshold, start processing events                */
+      if ( ! input->segment->chisqBinVec->length ||
+          params->chisqVec->data[j] < 
+          (params->chisqThresh * ( 1.0 + modqsq * chisqThreshFac )) )
+      {
 
-					lastEvent->next = thisEvent = (SnglInspiralTable *) 
-						LALCalloc( 1, sizeof(SnglInspiralTable) );
-					if ( ! lastEvent->next )
-					{
-						ABORT( status, FINDCHIRPH_EALOC, FINDCHIRPH_MSGEALOC );
-					}
 
-					/* stick minimal data into the event */
-					thisEvent->end_time.gpsSeconds = j;
-					thisEvent->snr = modqsq;
+        /*
+         *
+         * find the local maximum of the event
+         *
+         */
 
-				}
-			}
-		}
-	}
+
+        if ( ! *eventList )
+        {
+          /* store the start of the crossing */
+          eventStartIdx = j;
+
+          /* if this is the first event, start the list */
+          thisEvent = *eventList = (SnglInspiralTable *) 
+            LALCalloc( 1, sizeof(SnglInspiralTable) );
+          if ( ! thisEvent )
+          {
+            ABORT( status, FINDCHIRPH_EALOC, FINDCHIRPH_MSGEALOC );
+          }
+
+          /* record the data that we need for the clustering algorithm */
+          thisEvent->end_time.gpsSeconds = j;
+          thisEvent->snr = modqsq;
+        }
+        else if ( params->maximiseOverChirp &&
+            j <= thisEvent->end_time.gpsSeconds + deltaEventIndex &&
+            modqsq > thisEvent->snr )
+        {
+          /* if this is the same event, update the maximum */
+          thisEvent->end_time.gpsSeconds = j;
+          thisEvent->snr = modqsq;
+        }
+        else if ( j > thisEvent->end_time.gpsSeconds + deltaEventIndex ||
+            ! params->maximiseOverChirp )
+        {
+          /* clean up this event */
+          SnglInspiralTable *lastEvent;
+          LALFindChirpStoreEvent(status->statusPtr, input, params,
+              thisEvent, q, kmax, norm, eventStartIdx, numChisqBins, 
+              searchName );
+
+          /* store the start of the crossing */
+          eventStartIdx = j;
+
+          /* allocate memory for the newEvent */
+          lastEvent = thisEvent;
+
+          lastEvent->next = thisEvent = (SnglInspiralTable *) 
+            LALCalloc( 1, sizeof(SnglInspiralTable) );
+          if ( ! lastEvent->next )
+          {
+            ABORT( status, FINDCHIRPH_EALOC, FINDCHIRPH_MSGEALOC );
+          }
+
+          /* stick minimal data into the event */
+          thisEvent->end_time.gpsSeconds = j;
+          thisEvent->snr = modqsq;
+        }
+      }
+    }
+  }
 }
