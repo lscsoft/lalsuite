ba02b31c02c9e9aecb30c99840b310ac379f05c5
lgoggin <lgoggin>
2005-03-03 00:42:13 +0000

    brought pipeline up to date for new code Original: 02df602dc3e52a4717af0682a2e2ef505cc40d02

--- lalapps/src/ring/ring.py
+++ lalapps/src/ring/ring.py
@@ -16,35 +16,6 @@ class RingError(exceptions.Exception):
     self.args = args
 
 
-class DataFindJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
-  """
-  A LSCdataFind job used by the ring pipeline. The static options are
-  read from the section [datafind] in the ini file. The stdout from
-  LSCdataFind contains the paths to the frame files and is directed to a file
-  in the cache directory named by site and GPS start and end times. The stderr
-  is directed to the logs directory. The job always runs in the scheduler
-  universe. The path to the executable is determined from the ini file.
-  """
-  def __init__(self,cp):
-    """
-    cp = ConfigParser object from which options are read.
-    """
-    self.__executable = cp.get('condor','datafind')
-    self.__universe = 'scheduler'
-    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
-    pipeline.AnalysisJob.__init__(self,cp)
-
-    for sec in ['datafind']:
-      self.add_ini_opts(cp,sec)
-
-    self.add_condor_cmd('environment',
-      """LD_LIBRARY_PATH=$ENV(LD_LIBRARY_PATH);PYTHONPATH=$ENV(PYTHONPATH)""" )
-
-    self.set_stderr_file('logs/datafind-$(macroinstrument)-$(macrostart)-$(macroend)-$(cluster)-$(process).err')
-    self.set_stdout_file('cache/$(macroinstrument)-$(macrostart)-$(macroend).cache')
-    self.set_sub_file('datafind.sub')
-
-
 class RingJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
   """
   A lalapps_ring job used by the ring pipeline. The static options
@@ -72,93 +43,6 @@ class RingJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
     self.set_sub_file('ring.sub')
     
 
-class BurcaJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
-  """
-  A lalapps_burca job used by the ring pipeline. The static options are
-  read from the section [burca] in the ini file.  The stdout and stderr from
-  the job are directed to the logs directory.  The path to the executable is 
-  determined from the ini file.
-  """
-  def __init__(self,cp):
-    """
-    cp = ConfigParser object from which options are read.
-    """
-    self.__executable = cp.get('condor','burca')
-    self.__universe = cp.get('condor','universe')
-    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
-    pipeline.AnalysisJob.__init__(self,cp)
-    
-    for sec in ['burca']:
-      self.add_ini_opts(cp,sec)
-
-    self.add_condor_cmd('environment',"KMP_LIBRARY=serial;MKL_SERIAL=yes")
-
-    self.set_stdout_file('logs/burca-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).out')
-    self.set_stderr_file('logs/burca-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).err')
-    self.set_sub_file('burca.sub')
-
-class DataFindNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
-  """
-  A DataFindNode runs an instance of datafind in a Condor DAG.
-  """
-  def __init__(self,job):
-    """
-    job = A CondorDAGJob that can run an instance of LSCdataFind.
-    """
-    pipeline.CondorDAGNode.__init__(self,job)
-    pipeline.AnalysisNode.__init__(self)
-    self.__start = 0
-    self.__end = 0
-    self.__instrument = None
-    self.__output = None
-   
-  def __set_output(self):
-    """
-    Private method to set the file to write the cache to. Automaticaly set
-    once the ifo, start and end times have been set.
-    """
-    if self.__start and self.__end and self.__instrument:
-      self.__output = 'cache/' + self.__instrument + '-' + str(self.__start) 
-      self.__output = self.__output + '-' + str(self.__end) + '.cache'
-
-  def set_start(self,time):
-    """
-    Set the start time of the datafind query.
-    time = GPS start time of query.
-    """
-    self.add_var_opt('start', time)
-    self.__start = time
-    self.__set_output()
-
-  def set_end(self,time):
-    """
-    Set the end time of the datafind query.
-    time = GPS end time of query.
-    """
-    self.add_var_opt('end', time)
-    self.__end = time
-    self.__set_output()
-
-  def set_ifo(self,ifo):
-    """
-    Set the IFO to retrieve data for. Since the data from both Hanford 
-    interferometers is stored in the same frame file, this takes the first 
-    letter of the IFO (e.g. L or H) and passes it to the --instrument option
-    of LSCdataFind.
-    ifo = IFO to obtain data for.
-    """
-    self.add_var_opt('instrument',ifo[0])
-    self.__instrument = ifo[0]
-    self.__set_output()
-
-  def get_output(self):
-    """
-    Return the output file, i.e. the file containing the frame cache data.
-    """
-    return self.__output
-
-
-
 class RingNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
   """
   A RingNode runs an instance of the ring code in a Condor DAG.
@@ -179,7 +63,7 @@ class RingNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     if not self.get_start() or not self.get_end() or not self.get_ifo():
       raise RingError, "Start time, end time or ifo has not been set"
 
-    basename = self.get_ifo() + '-POWER'
+    basename = self.get_ifo() + '-RING'
 
     if self.get_ifo_tag():
       basename += '_' + self.get_ifo_tag()
@@ -188,6 +72,3 @@ class RingNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
 
     return basename + '-' + str(self.get_start()) + '-' + \
       str(self.get_end() - self.get_start()) + '.xml'
-
-
-
