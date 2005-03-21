8dcac27c0772ced54b38923ab8e84ee3516cf28a
andy <andy>
2005-03-21 19:45:29 +0000

    Removed changes since first implementation not completed. Original: f227d933c685503d00ba742b73e6fd715005e9b3

--- lal/packages/findchirp/src/FindChirpFilterOutputVeto.c
+++ lal/packages/findchirp/src/FindChirpFilterOutputVeto.c
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------- 
+*----------------------------------------------------------------------- 
  * 
  * File Name: FindChirpFilterOutputVeto.c
  *
@@ -67,85 +67,13 @@ void LALFindChirpFilterOutputVeto(
       FINDCHIRPFILTEROUTPUTVETOC );
   ATTATCHSTATUSPTR( status );
 
-int FindChirpFilterOutputVetoEngine(
-    SnglInspiralTable                  *event,
-    REAL4TimeSeries                    *input,
-    REAL4                               threshold,
-    REAL4                               window
-    )
-{
-  /* store the input values in sample points */
-	
-  UINT4 eventIndex;
-  UINT4 windowIndex;
-
-  /* store the output */
-  UINT4 duration = 0;
-  UINT4 crossings = 0;
-
-  /* convert the time of the event into an index */
-	
-#if 0
-
-	eventIndex = (UINT4) (event->end_time.gpsSeconds - 
-	                     input->segment->data->epoch.gpsSeconds) / input->deltaT;
-		
-#endif
-
-  eventIndex = 262152;
-
-  /* convert the window duration into an index */
-  
-	windowIndex = (UINT4) (window / input->deltaT);
-
-  /* compute the duration and the number of threshold crossings */
-  
-#if 0
-
-  /* count the number of time samples above the given threshold  */ 
-		  
-			for(x = eventIndex - windowIndex; x <= eventIndex ; ++x)
-				{if(input[x] >= threshold)
-				 ++duration;
-				}
-				
-	/* count the number of upward-going crossing at threshold */ 
-	  
-			for(x = eventIndex - windowIndex + 1; x <= eventIndex ; ++x)
-				 {if(input[x-1] <= threshold 
-					&&  input[x] >= threshold)
-					++crossings;
-				}
-				
-			
-#endif
-
-	/* Set temporary value for testing, convert duration units to milliseconds */
-
-  duration = 103 * input->deltaT * 1000;
-	
-	/* Set temporary value for testing */
-	
-  crossings = 99;
-
-  /* record the computed values in the sngl_inspiral event */
-	
-#if 0
-  event->event_duration = (REAL4) duration ; 
-  event->threshold_crossings = (REAL4) crossings; 
-#endif
-
-  /* normal exit */
-	
-  return 0;
-
 
   /* JC: DOESN'T DO MUCH... MAKE IT DO SOMETHING */
   eventList = NULL;
   qVec = NULL;
   qNorm = 0;
   params = NULL;
-	
+        
 
 }
 
