b546fb189752719be2913324caaa66cdf9437ffe
sengupta <sengupta>
2005-05-23 11:19:06 +0000

    Given a psd vector, it performs the Inverse Spectrum truncation in time domain as per the input parameters and returns the new PSD vector. Original: df757b62527b816ddfcc328196154e188248a5e1

---
+++ lal/packages/noisemodels/src/LALTruncateInvSpectrum.c
@@ -0,0 +1,154 @@
+#include <lal/LALStdlib.h>
+#include <lal/RealFFT.h>
+#include <lal/LALNoiseModels.h>
+
+NRCSID (LALTRUNCATEINVSPECTRUMC, "$Id$");
+
+void LALTruncateInvSpectrum( 
+        LALStatus                *status,
+        REAL8Vector              *inputVec,
+        InvSpecTruncationParams    *params 
+        )
+{
+    INT4              i, n, trunc_n;
+    REAL8             df;
+    COMPLEX8Vector    *Hvec = NULL;
+    REAL4Vector       *hvec = NULL;
+    REAL4             *w, norm;
+
+    INITSTATUS( status, "LALTruncateInvSpectrum", LALTRUNCATEINVSPECTRUMC);
+    ATTATCHSTATUSPTR( status );
+
+    ASSERT (inputVec,         status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+    ASSERT (params,           status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+    ASSERT (params->fwdp,     status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+    ASSERT (params->revp,     status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+    ASSERT (params->df > 0.0, status, LALNOISEMODELSH_ENULL, LALNOISEMODELSH_MSGENULL);
+
+    n       = 2 * (params->n - 1);
+    df      = params->df;
+    trunc_n = (INT4) params->psdTruncTime * (n*df);
+
+    // Hvec will contain the FFT i.e 1./sqrt(shf)
+    LALCCreateVector ( status->statusPtr, &Hvec, n/2 + 1 );
+    CHECKSTATUSPTR( status );
+
+    // hvec will contain time domain IFFT
+    LALSCreateVector ( status->statusPtr, &hvec, n );
+    CHECKSTATUSPTR( status );
+
+    // w should point at hvec->data
+    w = hvec->data;
+
+    // Store sqrt (inv spectrum)  (f-domain) in Hvec
+    for (i=0; i<Hvec->length; i++){
+        if (inputVec->data[i] > 0.0) {
+            Hvec->data[i].re = (REAL4) (1./sqrt(inputVec->data[i]));
+            Hvec->data[i].im = 0.0;
+        } else{
+            ABORT( status, LALNOISEMODELSH_ECHOICE, LALNOISEMODELSH_MSGECHOICE );
+        }
+    }
+
+    /* ---- If we are debugging then print the inv sqrt spectrum
+       as it was before (input) --- */
+    if (params->ifDebug) {
+        FILE  *out=NULL;
+        REAL8 dt;
+        /* We should print the frequency domain sqrt (inv spectrum) */
+        out = fopen("FB_sqrt_invSpectrum_before.f","w");
+        for (i=0; i<Hvec->length; i++){
+            fprintf (out, "%e %e\n",
+                    params->df*i, 
+                    Hvec->data[i].re);
+        }
+        fclose(out);
+    }
+
+    // Set Nyquist and zero frequency components of the input spectrum to zero
+    Hvec->data[Hvec->length - 1].re = 0.0;
+    Hvec->data[0].re                = 0.0;
+
+    // Inverse Fourier Transform to time domain
+    LALReverseRealFFT (status->statusPtr, hvec, Hvec, params->revp);
+    CHECKSTATUSPTR(status);
+
+    /* ---- If debugging then print the spectrum in time domain
+       after its inverse Fourier transform --- */
+    if (params->ifDebug) {
+        FILE  *out=NULL;
+        REAL8 dt;
+        dt = 1.0 / (params->df * n);
+        /* We should print the time domain sqrt(inv spectrum) */
+        out = fopen("FB_sqrt_invSpectrum.t","w");
+        for (i=0; i<hvec->length; i++){
+            fprintf (out, "%e %e\n",
+                    dt*i, 
+                    hvec->data[i]);
+        }
+        fclose(out);
+    }
+
+    // truncate in time domain 
+    memset( w + trunc_n/2, 0, (hvec->length - trunc_n) * sizeof(REAL4) );
+
+    /* ---- If debugging print the time domain spectrum after truncation
+       in the time domain --- */
+    if (params->ifDebug) {
+        FILE  *out=NULL;
+        REAL8 dt;
+        dt = 1. / (params->df * n);
+        /* We should print the time domain sqrt (inv spectrum) after padding */
+        out = fopen("FB_sqrt_invSpectrum_pad.t","w");
+        for (i=0; i<hvec->length; i++){
+            fprintf (out, "%e %e\n",
+                    dt*i, 
+                    hvec->data[i]);
+        }
+        fclose(out);
+    }
+
+    // transform to frequency domain
+    LALForwardRealFFT (status->statusPtr, Hvec, hvec, params->fwdp);
+    CHECKSTATUSPTR(status);
+
+    // normalise fourier transform and square
+    norm = 1.0 / (REAL4) (n);
+    for ( i = 0; i < Hvec->length; i++ )
+    {
+        Hvec->data[i].re *= norm;
+        Hvec->data[i].re *= Hvec->data[i].re;
+        Hvec->data[i].im  = 0.0;
+    }
+
+    /* ---- If debugging print the inv sqrt spectrum in Fourier domain
+       after time domain truncation ---- */
+    if (params->ifDebug) {
+        FILE  *out=NULL;
+        /* We should print the frequency domain sqrt (inv spectrum) after inv truncation */
+        out = fopen("FB_sqrt_invSpectrum_after.f","w");
+        for (i=0; i<Hvec->length; i++){
+            fprintf (out, "%e %e\n",
+                    params->df*i, 
+                    sqrt(Hvec->data[i].re));
+        }
+        fclose(out);
+    }
+
+    // populate the input vector structure back with truncated psd
+    for (i=0; i<Hvec->length; i++){
+        if (Hvec->data[i].re > 0.0) 
+              inputVec->data[i] = (REAL8) (1./Hvec->data[i].re);
+    }
+
+    // Clear work space
+    LALCDestroyVector (status->statusPtr, &Hvec);
+    CHECKSTATUSPTR(status);
+
+    LALSDestroyVector (status->statusPtr, &hvec);
+    CHECKSTATUSPTR( status );
+
+    // normal exit 
+    DETATCHSTATUSPTR( status );
+    RETURN( status );
+}
