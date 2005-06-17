97a15efe5d9062f9111cd54f8d67be428ce2cfd0
kipp <kipp>
2005-06-17 22:27:20 +0000

    Big clean up. Original: 7b9fa0bdaccd308aa72db17cafd985f0aa3364e4

--- lal/packages/burstsearch/test/ThresholdsTest.c
+++ lal/packages/burstsearch/test/ThresholdsTest.c
@@ -31,452 +31,207 @@
  */
 
 
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include <unistd.h>
 
 #include <lal/LALErrno.h>
 #include <lal/LALStdlib.h>
 #include <lal/Thresholds.h>
 
-
-#define _CODES(x) #x
-#define CODES(x) _CODES(x)
-
-
-NRCSID (MAIN, "$Id$");
-
+NRCSID(MAIN, "$Id$");
 
 extern char *optarg;
-extern int   optind;
-
-INT4 lalDebugLevel = 0;   /* set to 2 to get full status information for tests */
-INT4 verbose    = 1;
-
-static void
-Usage (const char *program, int exitflag);
-
-static void
-ParseOptions (int argc, char *argv[]);
-
-static void
-TestStatus (LALStatus *status, const char *expectedCodes, int exitCode);
+extern int optind, opterr, optopt;
 
 
-
-int
-main (int argc, char *argv[])
-{
-  static LALStatus    status;
-  ChisqCdfIn       input1;
-  Chi2ThresholdIn  input2;
-  RhoThresholdIn   input3;
-
-  REAL8 chi2;
-  REAL8 dof;
-  REAL8 rho;
-  REAL8 alpha;
-  REAL8 alpha1;
-  REAL8 alpha2;
-  REAL8 beta;
-  REAL8 temp1;
-  REAL8 temp2;
-
-  /*
-   *
-   * Parse the command line options
-   *
-   */
-
-
-  ParseOptions (argc, argv);
-
-  /* 
-   *  Check to make sure the functions return the correct values.  
-   *  First time around 
-   */
-
-  chi2 = 2.3l;
-  dof = 8.0;
-  rho = 2.2;
-  input1.chi2 = chi2;
-  input1.dof = dof;
-  input1.nonCentral = rho*rho;
-
-  alpha1 = XLALChisqCdf(input1.chi2, input1.dof);
-  if(XLALGetBaseErrno())
-  	exit(1);
-  alpha2 = XLALOneMinusChisqCdf(input1.chi2, input1.dof);
-  beta = XLALNoncChisqCdf(input1.chi2, input1.dof, input1.nonCentral);
-
-  alpha = 1.0 - alpha1;
-  if(verbose)
-    {
-      printf("-- Test 1 of XLALChisqCdf(), XLALNoncChisqCdf(), XLALChi2Threshold() and LALRhoThreshold() --\n");
-      printf("chi2 is %f and dof is %f\n",chi2,dof);
-      printf("rho is %f\n",rho);
-      printf("alpha is %f, should be 0.970406\n",alpha);
-      printf("alpha is %f, should be 0.970406\n",alpha2);
-      printf("beta is %f, should be 0.00439452\n",beta);
-    }
-
-  /* test were the correct values obtained */
-  if( (fabs(alpha - 0.970406) > 1e-5) || (fabs(beta - 0.00439452) > 1e-7))
-    {
-      fprintf( stderr, "Incorrect values returned\n");
-      return 1;
-    }
-      
-  input2.falseAlarm = alpha;
-  input2.dof = dof;
-  temp1 = XLALChi2Threshold(input2.dof, input2.falseAlarm);
-  input3.chi2 = chi2;
-  input3.dof = dof;
-  input3.falseDismissal=beta;
-  LALRhoThreshold( &status, &temp2, &input3);
-  TestStatus (&status, CODES(0), 1);
-
-  if(verbose)
-    {
-      printf("Working backwards, chi2 is %f\n",temp1);
-      printf("Working backwards, rho is %f\n",temp2);      
-    }
-  
-  
-  /* test were the correct values obtained */
-  if( (fabs(temp1-chi2) > 1e-5*chi2) || (fabs(temp2 - rho) > 1e-5*rho))
-    {
-      fprintf( stderr, "Incorrect values returned\n");
-      return 1;
-    }
-
-  /* 
-   *  Check to make sure the functions return the correct values.  
-   *  Second time around 
-   */
-  chi2 = 12.3l;
-  dof = 3.1;
-  rho = 2.2;
-  input1.chi2 = chi2;
-  input1.dof = dof;
-  input1.nonCentral = rho*rho;
-
-  alpha1 = XLALChisqCdf(input1.chi2, input1.dof);
-  if(XLALGetBaseErrno())
-  	exit(1);
-  beta = XLALNoncChisqCdf(input1.chi2, input1.dof, input1.nonCentral);
-  if(XLALGetBaseErrno())
-  	exit(1);
-
-  alpha = 1.0 - alpha1;
-  if(verbose)
-    {
-      printf("-- Test 2 of XLALChisqCdf(), XLALNoncChisqCdf(), XLALChi2Threshold() and LALRhoThreshold() --\n");
-      printf("chi2 is %f and dof is %f\n",chi2,dof);
-      printf("rho is %f\n",rho);
-      printf("alpha is %f, should be 0.007066\n",alpha);
-      printf("beta is %f, should be 0.822575\n",beta);
-    }
-
-  /* test were the correct values obtained */
-  if( (fabs(alpha - 0.007066) > 1e-7) || (fabs(beta - 0.822575) > 1e-6))
-    {
-      fprintf( stderr, "Incorrect values returned\n");
-      return 1;
-    }
-      
-  input2.falseAlarm = alpha;
-  input2.dof = dof;
-  temp1 = XLALChi2Threshold(input2.dof, input2.falseAlarm);
-  input3.chi2 = chi2;
-  input3.dof = dof;
-  input3.falseDismissal=beta;
-  LALRhoThreshold( &status, &temp2, &input3);
-  TestStatus (&status, CODES(0), 1);
-
-  if(verbose)
-    {
-      printf("Working backwards, chi2 is %f\n",temp1);
-      printf("Working backwards, rho is %f\n",temp2);      
-    }
-  
-  /* test were the correct values obtained */
-  if( (fabs(temp1-chi2) > 1e-5*chi2) || (fabs(temp2 - rho) > 1e-5*rho))
-    {
-      fprintf( stderr, "Incorrect values returned\n");
-      return 1;
-    }
-
-
-
-  /*
-   *
-   * Check to make sure that correct error codes are generated.
-   *
-   */
-
-#ifndef LAL_NDEBUG
-  if ( ! lalNoDebug )
-  {
-
-  if (verbose || lalDebugLevel)
-  {
-    printf ("\n===== Check Errors =====\n");
-  }
-
-
-  /* one of the arguments is a null pointer */
-
-  if (verbose)
-  {
-    printf ("\n----- Null Pointer Error: Code 1 (8 times) \n");
-  }
-
-  LALRhoThreshold( &status, &temp2, NULL);
-  TestStatus (&status, CODES(LAL_NULL_ERR), 1);
-
-  LALRhoThreshold( &status, NULL, &input3);
-  TestStatus (&status, CODES(LAL_NULL_ERR), 1);
-
-
-
-
-  /* Arguments are non-postive */
-
-  if (verbose)
-  {
-    printf ("\n----- Negative arguments Error: Code 2 (8 times) \n");
-  }
-
-  input1.chi2 *= -1.0;
-  alpha1 = XLALChisqCdf(input1.chi2, input1.dof);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  alpha1 = XLALNoncChisqCdf(input1.chi2, input1.dof, input1.nonCentral);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  input1.chi2 *= -1.0;  /* set it back to positive for remaining tests */
-
-  input1.dof *= -1.0;
-  alpha1 = XLALChisqCdf(input1.chi2, input1.dof);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  alpha1 = XLALNoncChisqCdf(input1.chi2, input1.dof, input1.nonCentral);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  input1.dof *= -1.0;
-
-  input1.nonCentral *= -1.0;
-  alpha1 = XLALNoncChisqCdf(input1.chi2, input1.dof, input1.nonCentral);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  input1.nonCentral *= -1.0;
-
-  input2.dof *= -1;
-  temp1 = XLALChi2Threshold(input2.dof, input2.falseAlarm);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  input2.dof *= -1;
-  
-  input3.dof *= -1;
-  LALRhoThreshold( &status, &temp2, &input3);
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  input3.dof *= -1;
-
-  input3.chi2 *= -1;
-  LALRhoThreshold( &status, &temp2, &input3);
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  input3.chi2 *= -1;
-
-
-
-
-  /*
-   * 
-   *  Maximum iterations exceeded error: this test no longer performed
-   *  since GSL is used to compute these distributions
-   *
-   */
-
-  /* reset parameters to original values */
-  input1.dof = dof;
-  input1.chi2 = chi2;
-  
-  /* 
-   *  There is no test here for exceeding the maximum number of
-   *  iterations in XLALNoncChisqCdf() since I could not find a set
-   *  of parameters which caused this condition to occur.
-   */
-
-
-
-  /* Supplied probabilities must lie between 0 and 1 */
-
-  if (verbose)
-  {
-    printf ("\n----- Probability out of range Error: Code 8 (4 times) \n");
-  }
-
-
-  input2.falseAlarm = -1.0;
-  temp1 = XLALChi2Threshold(input2.dof, input2.falseAlarm);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  input2.falseAlarm = 2.0;
-  temp1 = XLALChi2Threshold(input2.dof, input2.falseAlarm);
-  if(XLALGetBaseErrno() != XLAL_EDOM)
-  	exit(1);
-  XLALClearErrno();
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  /* set it back to original value for remaining tests */
-  input2.falseAlarm= alpha;  
-
-
-
-  input3.falseDismissal = -1.0;
-  LALRhoThreshold (&status, &temp2, &input3);
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  input3.falseDismissal = 2.0;
-  LALRhoThreshold (&status, &temp2, &input3);
-  TestStatus (&status, CODES(LAL_RANGE_ERR), 1);
-  /* set it back to original value for remaining tests */
-  input3.falseDismissal= beta;  
-
-  }
-#endif
-
-  LALCheckMemoryLeaks ();
-
-  if(verbose)  printf("PASS: all tests\n");
-
-  return 0;
-}
-
-
+int verbose = 1;
 
 
 /*
- * TestStatus ()
- *
- * Routine to check that the status code status->statusCode agrees with one of
- * the codes specified in the space-delimited string ignored; if not,
- * exit to the system with code exitcode.
- *
- */
-static void
-TestStatus (LALStatus *status, const char *ignored, int exitcode)
-{
-  char  str[64];
-  char *tok;
-
-  if (verbose)
-  {
-    /* REPORTSTATUS (status);*/
-  }
-
-  if (strncpy (str, ignored, sizeof (str)))
-  {
-    if ((tok = strtok (str, " ")))
-    {
-      do
-      {
-        if (status->statusCode == atoi (tok))
-        {
-          return;
-        }
-      }
-      while ((tok = strtok (NULL, " ")));
-    }
-    else
-    {
-      if (status->statusCode == atoi (tok))
-      {
-        return;
-      }
-    }
-  }
-
-  fprintf (stderr, "\nExiting to system with code %d\n", exitcode);
-  exit (exitcode);
-}
-
-
-/*
- * Usage ()
+ * Usage()
  *
  * Prints a usage message for program program and exits with code exitcode.
- *
  */
-static void
-Usage (const char *program, int exitcode)
+
+static void Usage(const char *program, int exitcode)
 {
-  fprintf (stderr, "Usage: %s [options]\n", program);
-  fprintf (stderr, "Options:\n");
-  fprintf (stderr, "  -h         print this message\n");
-  fprintf (stderr, "  -q         quiet: run silently\n");
-  fprintf (stderr, "  -v         verbose: print extra information\n");
-  fprintf (stderr, "  -d level   set lalDebugLevel to level\n");
-  exit (exitcode);
+	fprintf(stderr, "Usage: %s [options]\n", program);
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, "  -h         print this message\n");
+	fprintf(stderr, "  -q         quiet: run silently\n");
+	fprintf(stderr, "  -v         verbose: print extra information\n");
+	fprintf(stderr, "  -d level   set lalDebugLevel to level\n");
+	exit(exitcode);
 }
 
 
 /*
- * ParseOptions ()
+ * ParseOptions()
  *
  * Parses the argc - 1 option strings in argv[].
- *
  */
-static void
-ParseOptions (int argc, char *argv[])
+
+static void ParseOptions(int argc, char *argv[])
 {
-  while (1)
-  {
-    int c = -1;
+	int c;
 
-    c = getopt (argc, argv, "hqvd:");
-    if (c == -1)
-    {
-      break;
-    }
+	while(1) {
+		c = getopt(argc, argv, "hqvd:");
+		if(c == -1)
+			break;
+		switch(c) {
+		case 'd':
+			lalDebugLevel = atoi(optarg);
+			break;
 
-    switch (c)
-    {
-      case 'd': /* set debug level */
-        lalDebugLevel = atoi (optarg);
-        break;
+		case 'v':
+			++verbose;
+			break;
 
-      case 'v': /* verbose */
-        ++verbose;
-        break;
+		case 'q':
+			freopen("/dev/null", "w", stderr);
+			freopen("/dev/null", "w", stdout);
+			break;
 
-      case 'q': /* quiet: run silently (ignore error messages) */
-        freopen ("/dev/null", "w", stderr);
-        freopen ("/dev/null", "w", stdout);
-        break;
+		case 'h':
+			Usage(argv[0], 0);
+			break;
 
-      case 'h':
-        Usage (argv[0], 0);
-        break;
+		default:
+			Usage(argv[0], 1);
+		}
 
-      default:
-        Usage (argv[0], 1);
-    }
+	}
 
-  }
+	if(optind < argc)
+		Usage(argv[0], 1);
 
-  if (optind < argc)
-  {
-    Usage (argv[0], 1);
-  }
+	return;
+}
 
-  return;
+
+/*
+ * Check the output of functions
+ */
+
+#define CHECKOUTPUT(msg, expr, value, acc) { \
+	REAL8 result = expr; \
+	if(fabs(result - value) > acc) { \
+		fprintf(stderr, msg ": expected %.11g, got %.11g\n", value, result); \
+		exit(1); \
+	} \
+	if(XLALGetBaseErrno()) { \
+		fprintf(stderr, msg ": returned error\n"); \
+		exit(1); \
+	} \
+};
+
+
+#define CHECKERROR(msg, expr, error) { \
+	REAL8 result = expr; \
+	if(!XLALIsREAL8FailNaN(result)) { \
+		fprintf(stderr, msg ": failed to return REAL8FailNaN\n"); \
+		exit(1); \
+	} \
+	if(XLALGetBaseErrno() != error) { \
+		fprintf(stderr, msg ": expected error %d, got %d\n", error, XLALGetBaseErrno()); \
+		exit(1); \
+	} \
+	XLALClearErrno(); \
+};
+
+
+/*
+ * Entry point
+ */
+
+int main(int argc, char *argv[])
+{
+	REAL8 chi2;
+	REAL8 dof;
+	REAL8 rho;
+	REAL8 falseAlarm;
+	REAL8 falseDismissal;
+
+	/*
+	 * Parse the command line options
+	 */
+
+	ParseOptions(argc, argv);
+
+
+	/* 
+	 *  Check to make sure the functions return the correct values.  
+	 *  First time around 
+	 */
+
+	chi2 = 2.3l;
+	dof = 8.0;
+	rho = 2.2;
+
+	/* check forward functions */
+	CHECKOUTPUT("1.0 - XLALChisqCdf(chi2, dof)", 1.0 - XLALChisqCdf(chi2, dof), 0.970406, 1e-5);
+	CHECKOUTPUT("XLALOneMinusChisqCdf(chi2, dof)", XLALOneMinusChisqCdf(chi2, dof), 0.970406, 1e-5);
+	CHECKOUTPUT("XLALNoncChisqCdf(chi2, dof, rho * rho)", XLALNoncChisqCdf(chi2, dof, rho * rho), 0.00439452, 1e-7);
+	CHECKOUTPUT("XLALChi2Threshold(dof, 1.0 - XLALChisqCdf(chi2, dof))", XLALChi2Threshold(dof, 1.0 - XLALChisqCdf(chi2, dof)), chi2, 1e-5);
+	CHECKOUTPUT("XLALRhoThreshold(chi2, dof, XLALNoncChisqCdf(chi2, dof, rho * rho))", XLALRhoThreshold(chi2, dof, XLALNoncChisqCdf(chi2, dof, rho * rho)), rho, 1e-5);
+
+
+	/* 
+	 *  Check to make sure the functions return the correct values.  
+	 *  Second time around 
+	 */
+
+	chi2 = 12.3l;
+	dof = 3.1;
+	rho = 2.2;
+
+	CHECKOUTPUT("1.0 - XLALChisqCdf(chi2, dof)", 1.0 - XLALChisqCdf(chi2, dof), 0.007066, 1e-7);
+	CHECKOUTPUT("XLALNoncChisqCdf(chi2, dof, rho * rho)", XLALNoncChisqCdf(chi2, dof, rho * rho), 0.822575, 1e-6);
+	CHECKOUTPUT("XLALChi2Threshold(dof, 1.0 - XLALChisqCdf(chi2, dof))", XLALChi2Threshold(dof, 1.0 - XLALChisqCdf(chi2, dof)), chi2, 1e-5);
+	CHECKOUTPUT("XLALRhoThreshold(chi2, dof, XLALNoncChisqCdf(chi2, dof, rho * rho))", XLALRhoThreshold(chi2, dof, XLALNoncChisqCdf(chi2, dof, rho * rho)), rho, 1e-5);
+
+
+	/*
+	 * Check to make sure that correct error codes are generated.
+	 */
+
+#ifndef LAL_NDEBUG
+	falseAlarm = 0.970406;
+	falseDismissal = 0.00439452;
+	if(!lalNoDebug) {
+		if(verbose || lalDebugLevel)
+			printf("\n===== Check Errors =====\n");
+
+		CHECKERROR("XLALChisqCdf(-chi2, dof)", XLALChisqCdf(-chi2, dof), XLAL_EDOM);
+		CHECKERROR("XLALNoncChisqCdf(-chi2, dof, rho * rho)", XLALNoncChisqCdf(-chi2, dof, rho * rho), XLAL_EDOM);
+		CHECKERROR("XLALChisqCdf(chi2, -dof)", XLALChisqCdf(chi2, -dof), XLAL_EDOM);
+		CHECKERROR("XLALNoncChisqCdf(chi2, -dof, rho * rho)", XLALNoncChisqCdf(chi2, -dof, rho * rho), XLAL_EDOM);
+		CHECKERROR("XLALNoncChisqCdf(chi2, dof, -rho * rho)", XLALNoncChisqCdf(chi2, dof, -rho * rho), XLAL_EDOM);
+		CHECKERROR("XLALChi2Threshold(-dof, falseAlarm)", XLALChi2Threshold(-dof, falseAlarm), XLAL_EDOM);
+		CHECKERROR("XLALRhoThreshold(chi2, -dof, falseDismissal)", XLALRhoThreshold(chi2, -dof, falseDismissal), XLAL_EDOM);
+		CHECKERROR("XLALRhoThreshold(-chi2, dof, falseDismissal)", XLALRhoThreshold(-chi2, dof, falseDismissal), XLAL_EDOM);
+
+		/* There is no test here for exceeding the maximum number
+		 * of iterations in XLALNoncChisqCdf() since I could not
+		 * find a set of parameters which caused this condition to
+		 * occur. */
+
+		/* Supplied probabilities must lie between 0 and 1 */
+		CHECKERROR("XLALChi2Threshold(dof, -falseAlarm)", XLALChi2Threshold(dof, -falseAlarm), XLAL_EDOM);
+		CHECKERROR("XLALChi2Threshold(dof, 2.0)", XLALChi2Threshold(dof, 2.0), XLAL_EDOM);
+		CHECKERROR("XLALRhoThreshold(chi2, dof, -falseDismissal)", XLALRhoThreshold(chi2, dof, -falseDismissal), XLAL_EDOM);
+		CHECKERROR("XLALRhoThreshold(chi2, dof, -2.0)", XLALRhoThreshold(chi2, dof, -2.0), XLAL_EDOM);
+	}
+#endif
+
+	LALCheckMemoryLeaks();
+
+	if(verbose)
+		printf("PASS: all tests\n");
+
+	return 0;
 }
