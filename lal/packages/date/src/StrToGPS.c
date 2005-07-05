2245d8fa91b67e4d991729bc4178fb852467936e
kipp <kipp>
2005-07-05 19:27:50 +0000

    - Preserve errno (the C specification allows errno to be changed in a function   call even if the call is successful, but we might as well play nice). - Disallow strings of the form "15.+45" (i.e. don't allow a sign in the   fractional part). Original: bfb2affb7506f1e24371560c50b3b7d2def94936

--- lal/packages/date/src/StrToGPS.c
+++ lal/packages/date/src/StrToGPS.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <lal/Date.h>
@@ -7,31 +8,38 @@ int XLALStrToGPS(LIGOTimeGPS *time, const char *nptr, char **endptr)
 {
 	const char *func = "XLALStrToGPS";
 	char *p, *q;
-	int i;
+	int olderrno, i;
 
-	/* clear C library errno so we can check for failures */
+	/* save and clear C library errno so we can check for failures */
+	olderrno = errno;
 	errno = 0;
 
 	/* parse the integer part */
 	time->gpsSeconds = strtol(nptr, &p, 10);
+	time->gpsNanoSeconds = 0;
 
-	/* check for a fractional part */
+	/* check for and parse a fractional part */
 	if(*p == '.') {
-		/* parse the fractional part */
-		p++;
-		time->gpsNanoSeconds = strtol(p, &q, 10);
-		for(i = q - p - 9; i > 0; time->gpsNanoSeconds /= 10, i--);
-		for(; i < 0; time->gpsNanoSeconds *= 10, i++);
-	} else {
+		if(isdigit(*++p)) {
+			time->gpsNanoSeconds = strtol(p, &q, 10);
+			for(i = q - p - 9; i > 0; i--)
+				time->gpsNanoSeconds /= 10;
+			for(; i < 0; i++)
+				time->gpsNanoSeconds *= 10;
+		} else
+			q = p;
+	} else
 		q = p;
-		time->gpsNanoSeconds = 0;
-	}
 
+	/* save pointer to first character after converted characters */
 	if(endptr)
 		*endptr = q;
 
+	/* check for failures and restore errno if there weren't any */
 	if(errno == ERANGE)
 		XLAL_ERROR(func, XLAL_ERANGE);
-	
+	errno = olderrno;
+
+	/* success */
 	return(0);
 }
