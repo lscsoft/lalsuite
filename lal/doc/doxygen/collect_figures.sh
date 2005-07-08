9332b5da523503b67affd081212fd9835376c568
reinhard <reinhard>
2005-07-08 13:31:45 +0000

    - Makefile for convencience - bash-script hack to collect all eps- and png figures into ./figures and ./latex - make doxygen use ./figures as IMAGE_PATH Original: 47ae86b6044daa045dd4a477655b7230fcaf3c0b

---
+++ lal/doc/doxygen/collect_figures.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cwd=`pwd`;
+pkgdir="$cwd/../../packages";
+
+epsfigures=`find $pkgdir -name "*.eps" -print | grep -e"[^.].*[.]eps$"`; 
+pngfigures=`find $pkgdir -name "*.png" -print`;
+figdir="./figures";
+latexdir="./latex";
+
+if [ ! -d "$latexdir" ]; then
+    mkdir -p $latexdir;
+fi
+
+for i in $epsfigures $cwd/*.eps; do
+    ln -sf $i $figdir >& /dev/null;
+    ln -sf $i $latexdir >& /dev/null;## hack to get around doxygen missing some eps-figures for latex
+done
+
+for i in $pngfigures $cwd/*.png; do
+    ln -sf $i $figdir >& /dev/null;
+done
+echo
