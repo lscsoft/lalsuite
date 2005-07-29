6359c5844bd062b516d306cad2f3178dd31ab16e
pshawhan <pshawhan>
2005-07-29 00:39:37 +0000

    Correct description of algorithm as currently implemented.  (Documentation discrepancy discovered by Sarah Caudill.) Original: dcf236ac08130c5bf88c4d3baef9e430985341f6

--- lal/packages/bank/src/LALInspiralValidTemplate.c
+++ lal/packages/bank/src/LALInspiralValidTemplate.c
@@ -25,16 +25,26 @@ be kept in the template list.
 Given the parameter values $\tau_{0}$ and $\tau_{2(3)}$ this code 
 checks to see if they correspond to physical values of the masses of 
 a binary and their symmetric mass ratio $\eta.$ The parameter values
-will be accepted as valid parameters {\em eventhough} they
-may not lie within the search space but their
-span does. At the moment the code allows extra templates only
+will be accepted as valid parameters {\em even though} they
+may not lie within the search space but their span does, as described below.
+At the moment the code allows extra templates only
 in the positive-$\tau_{2(3)}$ direction only. We have found
 that placing templates in other directions is redundant.
 
 
 \subsubsection*{Algorithm}
-Compute the coordinates at the corners of the ambiguity rectangle.
-Accept if at least one of those points corresponds to the search space.
+%-- The following is not an accurate description of the current algorithm:
+%Compute the coordinates at the corners of the ambiguity rectangle.
+%Accept if at least one of those points corresponds to the search space.
+
+Consider the point $(\tau_0,\tau_{2(3)})$ describing the template, and
+also a point at $(\tau_0,\tau_{2(3)}$$-$$\mbox{\texttt{bankParams.dx1/2}})$ ,
+{\it i.e.}\ displaced in the negative $\tau_{2(3)}$ direction.
+\texttt{bankParams.dx1} is calculated from the metric and corresponds
+to the vertical spacing between the horizontal rows of templates being
+considered.
+Accept the template if at least one of those points is within the search space.
+
 \subsubsection*{Uses}
 
 \subsubsection*{Notes}
