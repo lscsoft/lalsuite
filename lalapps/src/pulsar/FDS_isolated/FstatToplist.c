f477475c6d88aba4f92d448ab732fd12e755c4e8
bema <bema>
2005-08-09 15:14:39 +0000

    move the test stuff into separate program Original: 28c53d26c150920fd1048e73e285910904394fef

--- lalapps/src/pulsar/FDS_isolated/FstatToplist.c
+++ lalapps/src/pulsar/FDS_isolated/FstatToplist.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "FstatToplist.h"
 #include <lal/StringInput.h>
 
@@ -186,81 +185,4 @@ int read_toplist_from_fp(toplist*l, FILE*fp) {
 	lines++;
     }
     return 0;
-}
-
-/* short test for the basic functions of this lib. */
-int test_toplist(UINT8 n, char*filename) {
-    REAL8 epsilon=1e-5;
-    FILE*fp;
-    toplist *tl=NULL, *tl2=NULL;
-    FstatsClusterOutput FstatLine;
-    UINT8 i;
-
-    if(create_toplist(&tl,n)) {
-	LALPrintError("Couldn't create toplist tl\n");
-	return(-1);
-    }
-    if(create_toplist(&tl2,n)) {
-	LALPrintError("Couldn't create toplist tl2\n");
-	free_toplist(&tl);
-	return(-1);
-    }
-
-    for(i=0;i<2*n;i++) {
-	FstatLine.Freq = (double)rand() / (double)RAND_MAX;
-	FstatLine.f1dot = (double)rand() / (double)RAND_MAX;
-	FstatLine.Alpha = (double)rand() / (double)RAND_MAX;
-	FstatLine.Delta = (double)rand() / (double)RAND_MAX;
-	FstatLine.Nbins = rand();
-	FstatLine.mean = (double)rand() / (double)RAND_MAX;
-	FstatLine.std = (double)rand() / (double)RAND_MAX;
-	FstatLine.max = (double)rand() / (double)RAND_MAX;
-
-	insert_into_toplist(tl, FstatLine);
-    }
-
-    
-    fp=fopen(filename,"w");
-    if(!fp) {
-	LALPrintError("Couldn't open file %s for writing\n",filename);
-	free_toplist(&tl);
-	free_toplist(&tl2);
-	return(-2);
-    }
-
-    if(write_toplist_to_fp(tl,fp)<0) {
-	LALPrintError("Couldn't write toplist\n",filename);
-	fclose(fp);
-	free_toplist(&tl);
-	free_toplist(&tl2);
-	return(-2);
-    }
-
-    fclose(fp);
-
-    fp=fopen(filename,"r");
-    if(!fp) {
-	LALPrintError("Couldn't open file %s for reading\n",filename);
-	free_toplist(&tl);
-	free_toplist(&tl2);
-	return(-2);
-    }
-
-    read_toplist_from_fp(tl2,fp);
-
-    for(i=0;i<n;i++)
-	if((tl->data[i].Freq  - tl2->data[i].Freq > epsilon) ||
-	   (tl->data[i].f1dot - tl2->data[i].f1dot > epsilon) ||
-	   (tl->data[i].Alpha - tl2->data[i].Alpha > epsilon) ||
-	   (tl->data[i].Delta - tl2->data[i].Delta > epsilon) ||
-	   (tl->data[i].Nbins - tl2->data[i].Nbins > epsilon) ||
-	   (tl->data[i].mean  - tl2->data[i].mean > epsilon) ||
-	   (tl->data[i].std   - tl2->data[i].std > epsilon) ||
-	   (tl->data[i].max   - tl2->data[i].max > epsilon))
-	    LALPrintError("line %d differs\n",i);
-
-    free_toplist(&tl);
-    free_toplist(&tl2);
-
-    return(0);
 }
