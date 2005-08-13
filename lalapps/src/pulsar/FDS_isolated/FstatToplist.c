7febc0a47810cea8a6f2088baff2b74ecd162e20
bema <bema>
2005-08-13 16:49:07 +0000

    added write_toplist_item_to_fp() and atomic_write_toplist_to_file(), renamed toplist type to toplist_t Original: a146fb318eee85caf3435b15dee86b3d4ee895be

--- lalapps/src/pulsar/FDS_isolated/FstatToplist.c
+++ lalapps/src/pulsar/FDS_isolated/FstatToplist.c
@@ -1,5 +1,6 @@
 #include "FstatToplist.h"
-#include <lal/StringInput.h>
+#include <lal/StringInput.h> /* for LAL_REAL8_FORMAT etc. */
+#include <stdio.h> /* for rename */
 
 RCSID("$Id$");
 
@@ -16,7 +17,7 @@ int finite(double);
 
 /* creates a toplist with length elements,
    returns -1 on error (usually out of memory), else 0 */
-int create_toplist(toplist**tl, UINT8 length) {
+int create_toplist(toplist_t**tl, UINT8 length) {
     UINT8 i;
 
     toplist *thetoplist = malloc(sizeof(toplist));
@@ -49,7 +50,7 @@ int create_toplist(toplist**tl, UINT8 length) {
 
 
 /* frees the space occupied by the toplist */
-void free_toplist(toplist**l) {
+void free_toplist(toplist_t**l) {
     if(l)
 	if(*l){
 	    if((**l).data)
@@ -65,7 +66,7 @@ void free_toplist(toplist**l) {
    In the latter case, remove the smallest element from the toplist and
    look for the now smallest one.
    Returns 1 if the element was actually inserted, 0 if not. */
-int insert_into_toplist(toplist*tl, FstatsClusterOutput elem) {
+int insert_into_toplist(toplist_t*tl, FstatsClusterOutput elem) {
  
     /* check if the toplist is full, if not, just add the new element */
     if (tl->elems < tl->length) {
@@ -99,7 +100,7 @@ int insert_into_toplist(toplist*tl, FstatsClusterOutput elem) {
 /* Writes the toplist to an (already open) filepointer
    Returns the number of written charactes
    Returns something <0 on error */
-int write_toplist_to_fp(toplist*tl, FILE*fp) {
+int write_toplist_to_fp(toplist_t*tl, FILE*fp) {
    UINT8 i,c=0;
    for(i=0;i<tl->elems;i++)
        c += fprintf(fp,"%e %e %e %e %d %e %e %1.15e\n",
@@ -130,7 +131,7 @@ static int _toplist_qsort_function(const void *pa, const void *pb) {
 
 /* (q)sort the toplist according to the sorting function.
    This actually only updates the "sorted" pointers */
-void sort_toplist(toplist*l) {
+void sort_toplist(toplist_t*l) {
     qsort(l->sorted,
 	  l->elems,
 	  sizeof(l->sorted),
@@ -140,7 +141,7 @@ void sort_toplist(toplist*l) {
 /* reads a (created!) toplist from an open filepointer
    returns -1 if the file contained a syntax error, -2 if given an improper toplist
 */
-int read_toplist_from_fp(toplist*l, FILE*fp) {
+int read_toplist_from_fp(toplist_t*l, FILE*fp) {
     char inline[256]; /* buffer for reading a line */
     UINT4 items, lines; /* number of items read from a line, linecounter */
     char lastchar;    /* last character of a line read, should be newline */
@@ -223,4 +224,35 @@ int read_toplist_from_fp(toplist*l, FILE*fp) {
 	lines++;
     }
     return 0;
+}
+
+/* writes an FstatsClusterOutput line to an open filepointer.
+   Returns the number of chars written */
+int write_toplist_item_to_fp(FstatsClusterOutput fline,fp) {
+    return(fprintf(fp,"%e %e %e %e %d %e %e %1.15e\n",
+		   fline.Freq,
+		   fline.f1dot,
+		   fline.Alpha,
+		   fline.Delta,
+		   fline.Nbins,
+		   fline.mean,
+		   fline.std,
+		   fline.max));
+}
+
+/* writes the given toplitst to a temporary file, then renames the
+   temporary file to filename. The name of the temporary file is
+   derived from the filename by appending ".tmp". Returns the number
+   of chars written or -1 if the temp file could not be opened. */
+int atomic_write_toplist_to_file(toplist *l, char *filename,) {
+    char tempname[256];
+    strncpy(tempname,filename,sizeof(tempname)-4);
+    strcat(tempname,".tmp");
+    FILE *fpnew=fopen(tempname, "w");
+    if(!fpnew)
+	return -1;
+    length = write_toplist_to_fp(fpnew, l);
+    fclose(fpnew);
+    rename(tempname, filename);
+    return length;
 }
--- lalapps/src/pulsar/FDS_isolated/FstatToplist.h
+++ lalapps/src/pulsar/FDS_isolated/FstatToplist.h
@@ -10,32 +10,44 @@ typedef struct {
     UINT8 smallest; /* index of the smallest element in the toplist */
     FstatsClusterOutput *data;    /* points to the actual data */
     FstatsClusterOutput **sorted; /* an array of sorted pointers to data */
-} toplist;
+} toplist_t;
 
 /* creates a toplist with length elements,
    returns -1 on error (usually out of memory), else 0 */
-extern int create_toplist(toplist**, UINT8);
+extern int create_toplist(toplist_t**, UINT8);
 
 /* frees the space occupied by the toplist */
-extern void free_toplist(toplist**);
+extern void free_toplist(toplist_t**);
 
 /* Inserts an element in to the toplist either if there is space left
    or the element is larger than the smallest element in the toplist.
    In the latter case, remove the smallest element from the toplist and
    look for the now smallest one.
    Returns 1 if the element was actually inserted, 0 if not. */
-extern int insert_into_toplist(toplist*, FstatsClusterOutput);
+extern int insert_into_toplist(toplist_t*, FstatsClusterOutput);
 
 /* Writes the toplist to an (already open) filepointer
    Returns the number of written charactes
    Returns something <0 on error */
-extern int write_toplist_to_fp(toplist*, FILE*);
+extern int write_toplist_to_fp(toplist_t*, FILE*);
 
 /* reads a (created!) toplist from an open filepointer
-   returns -1 if the file contained a syntax error, -2 if given an improper toplist */
-extern int read_toplist_from_fp(toplist*, FILE*);
+   returns -1 if the file contained a syntax error,
+   -2 if given an improper toplist */
+extern int read_toplist_from_fp(toplist_t*, FILE*);
 
-extern void sort_toplist(toplist*);
+/* sorts the toplist with an internal sorting function,
+   used before finally writing it */
+extern void sort_toplist(toplist_t*);
+
+/* writes an FstatsClusterOutput line to an open filepointer.
+   Returns the number of chars written */
+extern int write_toplist_item_to_fp(FstatsClusterOutput,FILE*);
+
+/* writes the given toplitst to a temporary file, then renames the
+   temporary file to filename. The name of the temporary file is
+   derived from the filename by appending ".tmp". Returns the number
+   of chars written or -1 if the temp file could not be opened. */
+extern int atomic_write_toplist_to_file(toplist_t*,char*);
 
 #endif /* FSTATTOPLIST_H */
-
