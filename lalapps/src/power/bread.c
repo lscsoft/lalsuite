789c056864c68003fdd5659eb3eb852b954c56fc
kipp <kipp>
2005-08-16 17:24:00 +0000

    Make bread faster. Original: abb9f136383764cd6115f072c8eadbc63d77d7d9

--- lalapps/src/power/bread.c
+++ lalapps/src/power/bread.c
@@ -555,7 +555,11 @@ static BOOLEAN keep_this_event(SnglBurstTable *event, struct options_t options)
 static SnglBurstTable *free_this_event(SnglBurstTable *event)
 {
 	SnglBurstTable *next = event ? event->next : NULL;
+#ifdef RUN_REALLY_REALLY_REALLY_SLOWLY
 	LALFree(event);
+#else
+	free(event);
+#endif
 	return(next);
 }
 
