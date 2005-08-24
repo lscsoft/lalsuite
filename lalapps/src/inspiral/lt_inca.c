1a350b91e402605ae065d824d414d36d06553993
dietz <dietz>
2005-08-24 17:48:36 +0000

    removed stat() call from code Original: 7d2e51b19309a75154707335648db66c4ebb7a96

--- lalapps/src/inspiral/lt_inca.c
+++ lalapps/src/inspiral/lt_inca.c
@@ -16,7 +16,6 @@
 #include <string.h>
 #include <getopt.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <lal/LALStdio.h>
 #include <lal/LALStdlib.h>
 #include <lal/Date.h>
@@ -603,17 +602,8 @@ int main( int argc, char *argv[] )
   {
     for( i = optind; i < argc; ++i )
     {
-      struct stat infileStatus;
       INT4 numFileTriggers = 0;
-      SnglInspiralTable *inputData = NULL;
-
-      /* if the named input file does not exist, exit with an error */
-      if ( stat( argv[i], &infileStatus ) == -1 )
-      {
-        fprintf( stderr, "Error opening input file %s\n", argv[i] );
-        perror( "failed to stat() file" );
-        exit( 1 );
-      }
+      SnglInspiralTable *inputData = NULL;     
 
       if ( vrbflg ) fprintf( stdout, 
           "storing input file name %s in search summvars table\n", argv[i] );
