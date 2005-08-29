186f6543475039e11a19b43b06c3aebd0b82d1c2
kipp <kipp>
2005-08-29 21:37:36 +0000

    Handle negative times, and times in scientific notation. Original: b62ce129c44de18491de735059e4434b8d9745e4

--- lal/packages/date/src/StrToGPS.c
+++ lal/packages/date/src/StrToGPS.c
@@ -4,6 +4,89 @@
 #include <lal/Date.h>
 #include <lal/LALDatatypes.h>
 
+/*
+ * Check if a string is a number in scientific notation.
+ */
+
+static int isscientific(const char *s)
+{
+	int radix_count = 0;
+	const char *p;
+
+	/* skip leading white space */
+	while(isspace(*s))
+		s++;
+
+	/* allow a sign */
+	if(*s == '+' || *s == '-')
+		s++;
+
+	/* check for a hex prefix */
+	if(*s == '0' && (*(s + 1) == 'X' || *(s + 1) == 'x')) {
+		/* skip hex prefix */
+		s += 2;
+
+		/* mark current location */
+		p = s;
+
+		/* allow hex digits and the radix character.  FIXME: radix
+		 * character should be obtained from current locale .*/
+		while(isxdigit(*s) || *s == '.') {
+			radix_count += *s == '.';
+			s++;
+		}
+
+		/* if no digits were found, or more than one radix was found,
+		 * or the next character is not the exponent prefix the answer
+		 * is no */
+		if(p == s || radix_count > 1 || (*s != 'P' && *s != 'p'))
+			return(0);
+
+		/* skip exponent prefix */
+		s++;
+
+		/* allow a sign */
+		if(*s == '+' || *s == '-')
+			s++;
+
+		/* if the next character is not a hex digit then the answer is
+		 * no */
+		return(isxdigit(*s));
+	} else {
+		/* mark current location */
+		p = s;
+
+		/* allow decimal digits and the radix character.  FIXME: radix
+		 * character should be obtained from current locale. */
+		while(isdigit(*s) || *s == '.') {
+			radix_count += *s == '.';
+			s++;
+		}
+
+		/* if no digits were found, or more than one radix was found,
+		 * or the next character is not the exponent prefix the answer
+		 * is no */
+		if(p == s || radix_count > 1 || (*s != 'E' && *s != 'e'))
+			return(0);
+
+		/* skip exponent prefix */
+		s++;
+
+		/* allow a sign */
+		if(*s == '+' || *s == '-')
+			s++;
+
+		/* if the next character is not a decimal digit then the answer
+		 * is no */
+		return(isdigit(*s));
+	}
+}
+
+
+/*
+ * Parse an ASCII string into a LIGOTimeGPS structure.
+ */
+
 int XLALStrToGPS(LIGOTimeGPS *time, const char *nptr, char **endptr)
 {
 	const char *func = "XLALStrToGPS";
@@ -14,22 +97,33 @@ int XLALStrToGPS(LIGOTimeGPS *time, const char *nptr, char **endptr)
 	olderrno = errno;
 	errno = 0;
 
-	/* parse the integer part */
-	time->gpsSeconds = strtol(nptr, &p, 10);
-	time->gpsNanoSeconds = 0;
+	/* parse as a double if in scientific notation */
+	if(isscientific(nptr)) {
+		XLALFloatToGPS(time, strtod(nptr, &q));
+	} else {
+		/* parse the integer part */
+		time->gpsSeconds = strtol(nptr, &p, 10);
+		time->gpsNanoSeconds = 0;
 
-	/* check for and parse a fractional part */
-	if(*p == '.') {
-		if(isdigit(*++p)) {
-			time->gpsNanoSeconds = strtol(p, &q, 10);
-			for(i = q - p - 9; i > 0; i--)
-				time->gpsNanoSeconds /= 10;
-			for(; i < 0; i++)
-				time->gpsNanoSeconds *= 10;
+		/* check for and parse a fractional part */
+		if(*p == '.') {
+			if(isdigit(*++p)) {
+				time->gpsNanoSeconds = strtol(p, &q, 10);
+				/* move the decimal place to the correct location */
+				for(i = q - p - 9; i > 0; i--)
+					time->gpsNanoSeconds /= 10;
+				for(; i < 0; i++)
+					time->gpsNanoSeconds *= 10;
+				/* non-integer negative times require a correction */
+				if((time->gpsSeconds < 0) && time->gpsNanoSeconds) {
+					time->gpsSeconds--;
+					time->gpsNanoSeconds = 1000000000 - time->gpsNanoSeconds;
+				}
+			} else
+				q = p;
 		} else
 			q = p;
-	} else
-		q = p;
+	}
 
 	/* save pointer to first character after converted characters */
 	if(endptr)
