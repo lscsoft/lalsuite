c44762e477df78606364343a61d2d78df53097d4
reinhard <reinhard>
2005-08-30 16:58:04 +0000

    fixed bug in byte-counting in read_toplist_from_fp()    [fgets()  reads  in  at  most  one less than size characters from stream] => checkpointing seems to work ok now! Original: c13b2d3e2e255c5643ba4c9eb8d8601b64888170

--- lalapps/src/pulsar/FDS_isolated/FstatToplist.c
+++ lalapps/src/pulsar/FDS_isolated/FstatToplist.c
@@ -176,7 +176,7 @@ int read_toplist_from_fp(toplist_t*l, FILE*fp, UINT4*checksum, UINT4 maxbytes) {
 	maxbytes--;
 
     lines=1;
-    while(fgets(line,min(sizeof(line)-1,maxbytes-chars),fp)) {
+    while(fgets(line,min(sizeof(line)-1, maxbytes - chars + 1),fp)) {
 
 	len = strlen(line);
 	chars += len;
@@ -204,7 +204,7 @@ int read_toplist_from_fp(toplist_t*l, FILE*fp, UINT4*checksum, UINT4 maxbytes) {
 
 	/* check the values scanned */
 	if (
-	    items != 9 ||
+	    items != 6 ||
 
 	    !finite(FstatLine.Freq)	||
 	    !finite(FstatLine.f1dot)	||
