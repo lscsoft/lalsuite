73228e5e1b88b8a878e0724f382d68ab4b481c31
duncan <duncan>
2005-08-31 17:20:13 +0000

    removed obsolete files Original: 664cd5776900102e38318725ae89784f037ac4b4

--- lalapps/src/ring/ring_test_pipe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-d=${srcdir:-.}
-cmd="./lalapps_ring_pipe -f S2_H1_dag.ini -l . -d -r"
-echo $cmd
-eval $cmd || exit $?
-
-exit 0
--- lalapps/src/ring/ring_test_sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-d=${srcdir:-.}
-cmd="./lalapps_ring --verbose --debug-level=1 --sample-rate=4096 --filter-segsz=8192 --filter-speclen=0 --filter-flow=0.001 --filter-fhighpass=-1 --filter-fmin=150 --filter-fmax=151 --filter-qmin=10 --filter-qmax=11 --filter-maxmm=0.1 --filter-thresh=4 --bank-end-template=0 --output-format=ascii --test-gaussian-data --test-white-spectrum --test-zero-data --test-inject=3.0,150,10,8,0 --strain-data --write-data --write-filter-output --write-format=ascii"
-echo $cmd
-eval $cmd || exit $?
-
-exit 0
