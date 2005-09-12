7c9922257412b25fe51ff0aa9684ec5cc7e25187
reinhard <reinhard>
2005-09-12 13:21:29 +0000

    mhph... I meant isfinite(), not isnormal()... Original: b9f1689d07f4b6158846000458c5c220c0c27542

--- lal/packages/pulsar/src/ComputeFstat.c
+++ lal/packages/pulsar/src/ComputeFstat.c
@@ -290,8 +290,8 @@ XLALComputeFaFb ( Fcomponents *FaFb,
       Fb.re += b * realQXP;
       Fb.im += b * imagQXP;
       
-#ifdef HAVE_ISNORMAL
-      if ( !isnormal(Fa.re) || !isnormal(Fa.im) || !isnormal(Fb.re) || !isnormal(Fb.im) )
+#ifdef HAVE_ISFINITE
+      if ( !isfinite(Fa.re) || !isfinite(Fa.im) || !isfinite(Fb.re) || !isfinite(Fb.im) )
 	{
 	  LALPrintError("\nnon-normal number encountered in SFT-loop alpha=%d!\n", alpha );
 	  LALPrintError("Fa = %f + i %f, Fb = %f + i %f\n\n", Fa.re, Fa.im, Fb.re, Fb.im );
