26dd7730d3025397bb6dc85e72b00794d694ce41
spxcar <spxcar>
2005-09-16 14:40:49 +0000

    Replacing previous Runge-Kutta with an adaptive version Original: ae2f05480eb77d76e3b8fbcb547737ebf506426b

--- lal/packages/inspiral/src/LALRungeKutta4.c
+++ lal/packages/inspiral/src/LALRungeKutta4.c
@@ -1,18 +1,20 @@
-/*  <lalVerbatim file="LALRungeKutta4CV">
-Author: Sathyaprakash, B. S.
+/*  <lalVerbatim file="LALRungeKutta4AdaptCV">
+Author: Robinson, C. A.
 $Id$
 </lalVerbatim>  */
 
 /*  <lalLaTeX>
 
-\subsection{Module \texttt{LALRungeKutta4.c}}
+\subsection{Module \texttt{LALRungeKutta4Adapt.c}}
 
-The code \texttt{LALRungeKutta4.c} solves a system of $n$ coupled first--order differential equations.
+The code \texttt{LALRungeKutta4Adapt.c} solves a system of $n$ coupled first--order differential equations.
+Internally, it uses the gsl routines for performing adaptive step evolution of the system, but to the outside
+user, it returns results for a fixed step size.
 
 \subsubsection*{Prototypes}
 \vspace{0.1in}
-\input{LALRungeKutta4CP}
-\idx{LALRungeKutta4()}
+\input{LALRungeKutta4AdaptCP}
+\idx{LALRungeKutta4Adapt()}
 
 \subsubsection*{Description}
 
@@ -25,22 +27,31 @@ None.
 
 \subsubsection*{Notes}
 
-\vfill{\footnotesize\input{LALRungeKutta4CV}}
+\vfill{\footnotesize\input{LALRungeKutta4AdaptCV}}
 
 </lalLaTeX>  */
 
 
-
-
-
-
-
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_odeiv.h>
 #include <lal/LALInspiral.h>
 #include <lal/LALStdlib.h>
 
-NRCSID (LALRUNGEKUTTA4C, "$Id$");
+struct RungeGSLParams {
+  rk4In *input;
+  void  *params;
+};
 
-/*  <lalVerbatim file="LALRungeKutta4CP"> */
+static int derivativeGSLWrapper(
+                                REAL8 t,
+                                const REAL8 y[],
+                                REAL8 dydx[],
+                                void *params);
+
+
+NRCSID (LALRUNGEKUTTA4ADAPTC, "$Id$");
+
+/*  <lalVerbatim file="LALRungeKutta4AdaptCP"> */
 void 
 LALRungeKutta4(
    LALStatus   *status,
@@ -51,9 +62,17 @@ LALRungeKutta4(
 { /* </lalVerbatim>  */
 
    INT4 i;
-   REAL8 xh,hh,h6;
-
-   INITSTATUS(status, "LALRungeKutta4", LALRUNGEKUTTA4C);
+   REAL8 t = 0.0;
+   struct RungeGSLParams gslParams;
+   REAL8 y[input->n];
+   REAL8 h = input->h;
+   const gsl_odeiv_step_type * type = gsl_odeiv_step_rk4;
+   gsl_odeiv_step *step;
+   gsl_odeiv_control *control;
+   gsl_odeiv_evolve *evolve;
+   gsl_odeiv_system sys; 
+  
+   INITSTATUS(status, "LALRungeKutta4Adapt", LALRUNGEKUTTA4ADAPTC);
    ATTATCHSTATUSPTR(status);
 
    ASSERT (yout, status, LALINSPIRALH_ENULL, LALINSPIRALH_MSGENULL);
@@ -61,33 +80,69 @@ LALRungeKutta4(
    ASSERT (input, status, LALINSPIRALH_ENULL, LALINSPIRALH_MSGENULL);
    ASSERT (params, status, LALINSPIRALH_ENULL, LALINSPIRALH_MSGENULL);
 
-   hh = input->h*0.5;
-   h6 = input->h/6.0;
-   xh = input->x+hh;
+  /* Initialise GSL integrator */
+  step    = gsl_odeiv_step_alloc(type, input->n);
+  control = gsl_odeiv_control_standard_new(0.0, 1.0e-12, 0.5, 0.5);
+  evolve  = gsl_odeiv_evolve_alloc(input->n);
 
-   for (i=0;i<=input->n-1;i++) 
-      input->yt->data[i]=input->y->data[i]+hh*input->dydx->data[i];
 
-   input->function(input->yt, input->dyt, params);
 
-   for (i=0;i<=input->n-1;i++) 
-      input->yt->data[i]=input->y->data[i]+hh*input->dyt->data[i];
+  gslParams.input = input;
+  gslParams.params = params;
 
-   input->function(input->yt, input->dym, params);	
+  sys.function = derivativeGSLWrapper;
+  sys.jacobian =  NULL;
+  sys.dimension = input->n; 
+  sys.params    = &gslParams;
 
-   for (i=0;i<=input->n-1;i++) {
-      input->yt->data[i]=input->y->data[i]+input->h*input->dym->data[i];
-      input->dym->data[i] += input->dyt->data[i];
-   }
-   input->function(input->yt, input->dyt, params);
 
-   for (i=0;i<=input->n-1;i++)
-      yout->data[i]=
-         input->y->data[i] + 
-         h6*(input->dydx->data[i] + 
-         input->dyt->data[i] + 
-         2.0*input->dym->data[i]);
+  for (i = 0; i < input->n; i++)
+    y[i] = input->y->data[i];
 
-   DETATCHSTATUSPTR(status);
-   RETURN (status);
+   /* Evolve the system */
+  while (t < input->h)
+  {
+    int gslStatus = gsl_odeiv_evolve_apply(evolve, control, step, &sys,
+				&t, input->h, &h, y);
+     /*printf("h = %e, t = %e\n", h, t);*/
+    if (gslStatus != GSL_SUCCESS)
+    {  
+  	gsl_odeiv_evolve_free(evolve);
+   	gsl_odeiv_control_free(control);
+   	gsl_odeiv_step_free(step);
+        ABORT(status, LALINSPIRALH_ESTOPPED, LALINSPIRALH_MSGESTOPPED);
+     }  
+  }
+
+  for (i=0; i<input->n; i++)
+     yout->data[i] = y[i];
+
+  gsl_odeiv_evolve_free(evolve);
+  gsl_odeiv_control_free(control);
+  gsl_odeiv_step_free(step);
+   
+  DETATCHSTATUSPTR(status);
+  RETURN (status);
+}
+
+
+/* A simple wrapper function to allow GSL to use the LAL
+   derivative functions */
+int derivativeGSLWrapper(
+				REAL8 t, 
+				const REAL8 y[], 
+				REAL8 dydx[],
+				void *params)
+{
+  struct RungeGSLParams *in = (struct RungeGSLParams *)params;
+  REAL8Vector yVect;
+  REAL8Vector dyVect;
+
+  yVect.length = dyVect.length = in->input->n;
+  yVect.data = (REAL8 *)LALMalloc(in->input->n * sizeof(REAL8));
+  memcpy(yVect.data, y, in->input->n * sizeof(REAL8));
+  dyVect.data = dydx;
+  in->input->function(&yVect, &dyVect, in->params);
+  LALFree(yVect.data);
+  return GSL_SUCCESS;
 }
