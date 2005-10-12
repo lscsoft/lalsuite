3c2ebce9061daedb28c74bbc4deab8e96d8dd8c3
vdbroeck <vdbroeck>
2005-10-12 16:41:16 +0000

    adding include file for spin bank Original: a8f18bc7b46023f4bffe8d93fc8af262d8317750

--- lal/packages/bank/include/LALInspiralBCVSpinBank.h
+++ lal/packages/bank/include/LALInspiralBCVSpinBank.h
@@ -1,4 +1,40 @@
+/* <lalVerbatim file="LALInspiralBCVSpinBankHV">
+
+Author: Tagoshi, H., Van Den Broeck, C., Jones, G.
 $Id$
+
+</lalVerbatim> */
+
+/* <lalLaTeX>
+
+\section{Header \texttt{LALInspiralBCVSpinBank.h}}
+\label{s:LALInspiralBCVSpinBank.h}
+
+Header file for the template placement codes.
+
+\subsection*{Synopsis}
+\begin{verbatim}
+#include <lal/LALInspiralBCVSpinBank.h>
+\end{verbatim}
+
+\noindent This header file covers routines that are used in template placement
+for spinning black hole binaries using phenomenological BCV templates.
+
+</lalLaTeX> */
+
+#include <lal/LIGOMetadataTables.h>
+#include <lal/LALConstants.h>
+#include <lal/LALStdlib.h>
+
+
+void
+LALInspiralBCVSpinBank(
+    LALStatus         	 *status,
+    SnglInspiralTable   **tiles,
+    INT4      		 *ntiles,
+    InspiralCoarseBankIn *coarseIn
+    );
+
 int BCVspin_metric(
    /*input*/
    int N,double *Sn,double fmin,double fmax,double beta,
