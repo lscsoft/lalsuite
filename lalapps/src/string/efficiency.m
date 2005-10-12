5d09f0834eeda49d6133fdbbe39c2659940a43e7
siemens <siemens>
2005-10-12 18:42:27 +0000

    too many improvements to list Original: 621555b850ffa7bc81c8b6db5280c6899cef6e43

--- lalapps/src/string/efficiency.m
+++ lalapps/src/string/efficiency.m
@@ -4,7 +4,7 @@ path(path,'/home/siemens/ligotools/matlab')
 
 % Coincident efficiencies
 
-cd /scratch2/xavi/CosmicStrings0
+cd /scratch2/xavi/CosmicStrings0/
 
 H1ctrigfound0 = readMeta('H1cinjfound.xml','sim_burst');
 H2ctrigfound0 = readMeta('H2cinjfound.xml','sim_burst');
@@ -14,177 +14,95 @@ H1cinjmade0 = readMeta('H1cinj.xml','sim_burst');
 H2cinjmade0 = readMeta('H2cinj.xml','sim_burst');
 L1cinjmade0 = readMeta('L1cinj.xml','sim_burst');
 
-cd /scratch2/xavi/CosmicStrings1
+% gps = 793154935;
+% figure
+% semilogy((H1cinjmade0.geocent_peak_time+H1cinjmade0.geocent_peak_time_ns*1e-9)-gps,H1cinjmade0.hpeak,'rx'); grid on
+% hold on;
+% semilogy((H1ctrigfound0.geocent_peak_time+H1ctrigfound0.geocent_peak_time_ns*1e-9)-gps,H1ctrigfound0.hpeak,'ko'); grid on
 
-H1ctrigfound1 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound1 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound1 = readMeta('L1cinjfound.xml','sim_burst');
+H1foundpeaks=[H1ctrigfound0.hpeak];
+H2foundpeaks=[H2ctrigfound0.hpeak];
+L1foundpeaks=[L1ctrigfound0.hpeak];
 
-H1cinjmade1 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade1 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade1 = readMeta('L1cinj.xml','sim_burst');
+H1madepeaks=[H1cinjmade0.hpeak];
+H2madepeaks=[H2cinjmade0.hpeak];
+L1madepeaks=[L1cinjmade0.hpeak];
 
-cd /scratch2/xavi/CosmicStrings2
+dlnA=0.7;
+lnA=-0.46:dlnA:7;
 
-H1ctrigfound2 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound2 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound2 = readMeta('L1cinjfound.xml','sim_burst');
+nH1found=histc(log(H1foundpeaks),lnA);
+nH2found=histc(log(H2foundpeaks),lnA);
+nL1found=histc(log(L1foundpeaks),lnA);
 
-H1cinjmade2 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade2 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade2 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings3
-
-H1ctrigfound3 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound3 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound3 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade3 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade3 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade3 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings4
-
-H1ctrigfound4 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound4 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound4 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade4 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade4 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade4 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings5
-
-H1ctrigfound5 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound5 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound5 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade5 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade5 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade5 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings6
-
-H1ctrigfound6 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound6 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound6 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade6 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade6 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade6 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings7
-
-H1ctrigfound7 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound7 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound7 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade7 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade7 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade7 = readMeta('L1cinj.xml','sim_burst');
-
-cd /scratch2/xavi/CosmicStrings8
-
-H1ctrigfound8 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound8 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound8 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade8 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade8 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade8 = readMeta('L1cinj.xml','sim_burst');
-
-
-cd /scratch2/xavi/CosmicStrings9
-
-H1ctrigfound9 = readMeta('H1cinjfound.xml','sim_burst');
-H2ctrigfound9 = readMeta('H2cinjfound.xml','sim_burst');
-L1ctrigfound9 = readMeta('L1cinjfound.xml','sim_burst');
-
-H1cinjmade9 = readMeta('H1cinj.xml','sim_burst');
-H2cinjmade9 = readMeta('H2cinj.xml','sim_burst');
-L1cinjmade9 = readMeta('L1cinj.xml','sim_burst');
-
-H1foundpeaks=[H1ctrigfound0.hpeak;H1ctrigfound1.hpeak;H1ctrigfound2.hpeak;H1ctrigfound3.hpeak;H1ctrigfound4.hpeak;...
-    H1ctrigfound5.hpeak;H1ctrigfound6.hpeak;H1ctrigfound7.hpeak;H1ctrigfound8.hpeak;H1ctrigfound9.hpeak]
-
-H2foundpeaks=[H2ctrigfound0.hpeak;H2ctrigfound1.hpeak;H2ctrigfound2.hpeak;H2ctrigfound3.hpeak;H2ctrigfound4.hpeak;...
-    H2ctrigfound5.hpeak;H2ctrigfound6.hpeak;H2ctrigfound7.hpeak;H2ctrigfound8.hpeak;H2ctrigfound9.hpeak]
-
-L1foundpeaks=[L1ctrigfound0.hpeak;L1ctrigfound1.hpeak;L1ctrigfound2.hpeak;L1ctrigfound3.hpeak;L1ctrigfound4.hpeak;...
-    L1ctrigfound5.hpeak;L1ctrigfound6.hpeak;L1ctrigfound7.hpeak;L1ctrigfound8.hpeak;L1ctrigfound9.hpeak]
-
-
-H1madepeaks=[H1cinjmade0.hpeak;H1cinjmade1.hpeak;H1cinjmade2.hpeak;H1cinjmade3.hpeak;H1cinjmade4.hpeak;...
-    H1cinjmade5.hpeak;H1cinjmade6.hpeak;H1cinjmade7.hpeak;H1cinjmade8.hpeak;H1cinjmade9.hpeak]
-
-H2madepeaks=[H2cinjmade0.hpeak;H2cinjmade1.hpeak;H2cinjmade2.hpeak;H2cinjmade3.hpeak;H2cinjmade4.hpeak;...
-    H2cinjmade5.hpeak;H2cinjmade6.hpeak;H2cinjmade7.hpeak;H2cinjmade8.hpeak;H2cinjmade9.hpeak]
-
-L1madepeaks=[L1cinjmade0.hpeak;L1cinjmade1.hpeak;L1cinjmade2.hpeak;L1cinjmade3.hpeak;L1cinjmade4.hpeak;...
-    L1cinjmade5.hpeak;L1cinjmade6.hpeak;L1cinjmade7.hpeak;L1cinjmade8.hpeak;L1cinjmade9.hpeak]
-
-A= -0.2:0.2:2;
-nH1found=histc(log10(H1foundpeaks),A);
-nH2found=histc(log10(H2foundpeaks),A);
-nL1found=histc(log10(L1foundpeaks),A);
-
-nH1made=histc(log10(H1madepeaks),A);
-nH2made=histc(log10(H2madepeaks),A);
-nL1made=histc(log10(L1madepeaks),A);
+nH1made=histc(log(H1madepeaks),lnA);
+nH2made=histc(log(H2madepeaks),lnA);
+nL1made=histc(log(L1madepeaks),lnA);
  
 eH1=nH1found./nH1made;
 eH2=nH2found./nH2made;
 eL1=nL1found./nL1made;
 
-%figure
-plot(A,eH1,'-')
+eH1=eH1(1:size(eH1,1)-2);
+eH2=eH2(1:size(eH2,1)-2);
+eL1=eL1(1:size(eL1,1)-2);
+
+lnA=lnA(1:size(lnA,2)-2);
+
+figure
+plot(lnA,eH1,'-')
 hold on
-plot(A,eH1,'bo')
-plot(A,eH2,'go')
-plot(A,eH2,'g-')
-plot(A,eL1,'ro')
-plot(A,eL1,'r-')
-ylabel('Efficiency')
-xlabel('log10(Injected amplitude)')
-title('Triple coincident efficiencies for H1 (blue), H2 (green), L1 (red)')
-
-% Find interpolated A50%
-for i = 2:size(A,2)
-   elH1=eH1(i-1);
-   ehH1=eH1(i);
-   if elH1 < 0.5
-       if ehH1 > 0.5
-           A50H1=A(i-1)+(0.5-elH1)*(A(i)-A(i-1))/(ehH1-elH1)
-       end
-   end
-
-   elH2=eH2(i-1);
-   ehH2=eH2(i);
-   if elH2 < 0.5
-       if ehH2 > 0.5
-           A50H2=A(i-1)+(0.5-elH2)*(A(i)-A(i-1))/(ehH2-elH2)
-       end
-   end
-   
+plot(lnA,eH1,'bo')
+plot(lnA,eH2,'go')
+plot(lnA,eH2,'g-')
+plot(lnA,eL1,'ro')
+plot(lnA,eL1,'r-')
+plot(lnA,eH1,'-')
+plot(lnA,eH1,'bo')
+ylabel('\epsilon (A^*,A)','FontSize',12)
+xlabel('ln(A)','FontSize',12)
+title('Triple coincident efficiencies for H1 (blue), H2 (green), L1 (red)','FontSize',12)
 
 
-   elL1=eL1(i-1);
-   ehL1=eL1(i);
-   if elL1 < 0.5
-       if ehL1 > 0.5
-           A50L1=A(i-1)+(0.5-elL1)*(A(i)-A(i-1))/(ehL1-elL1)
-       end
-   end
-  
+%Now we can compute the excluded regions as a function of Gmu
 
+t0=10^(17.5);  %age of the universe
+zeq=10^(3.94);
+aeq=(zeq)^(-4/3) * 2^(-1/3); %value of a at zeq
+
+c=1;           %Average no of cusps per oscillation
+Gamma = 50;    %Gravitational back reaction constant
+
+f=75;          %Lowest high frequency cutoff we can detect
+p=1e-6;           %Probability of reconnection
+
+Gmu=logspace(-13,-5,8); %Vector with values of Gmu
+
+n=1;           %power law for Gamma*Gmu (as in my PRD paper)
+epsilon=1;     %smal-scale structure pre-factor (as in Damour and Vilenkin, 2005 PRD)
+
+A=exp(lnA)*10^(-20);  %turn vector of ln(A) back into A; and re-scale by 10^-20 factor
+
+for i=1:size(Gmu,2)
+        
+    alpha=epsilon*(Gamma*Gmu(i))^n;   %value of the size of teh small-scale structure
+    a=A./(t0^(-1/3)*Gmu(i)*(alpha^(2/3)));   %Reduced amplitude vector      
+    b=10^2 * c * alpha^(-5/3)*(p*Gamma*Gmu(i))^(-1) * t0^(-1) * (f*t0)^(-2/3);
+    dRdlnA=b*aeq^(33/40)*a.^(-11/5) .* ((1+1/aeq*a).^(33/40)).*(1+a).^(-13/8); %Rate per logarithmic interval of amplitude 
+    R(i)=dlnA*sum(eH1.*dRdlnA');
+
+    R(i)=R(i)+dRdlnA(size(dRdlnA,2));   %This last addition is for events with amplitude larger than the largest amplitude 
+                             %(which I assume we detect with efficiency 1)
 end
 
-fid=fopen('/home/siemens/lscsoft/lalapps/src/ring/eff_data2.txt','a');
-fprintf(fid,'%e %e %e\n',A50H1,A50H2,A50L1);
 
-fclose(fid);
+T=204211;
 
-plot(A50H1,0.5,'bx')
-plot(A50H2,0.5,'gx')
-plot(A50L1,0.5,'rx') 
+figure(2)
+loglog(Gmu,R,'b'); hold on
+loglog(Gmu,2.303/T,'r')
+xlabel('G\mu')
+ylabel('Rate[Hz]')
+title('Playground upper limit','FontSize',14)
+xlabel('G\mu','FontSize',14)
+ylabel('Rate [Hz]','FontSize',14)
