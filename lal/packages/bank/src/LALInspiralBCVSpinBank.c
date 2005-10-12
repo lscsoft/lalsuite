ee6c7b6ea467406fa05af993772a208ae96019ca
vdbroeck <vdbroeck>
2005-10-12 15:21:29 +0000

    Adding new file which will contain metric code written by Tagoshi Original: ab9c3c78b43693546080df3393a0c97fcecb8fbf

---
+++ lal/packages/bank/src/LALInspiralBCVSpinBank.c
@@ -0,0 +1,18 @@
+/*  <lalVerbatim file="LALInspiralBCVSpinBankCV">
+Author: Tagoshi, H, Van Den Broeck, C, Jones, G.
+$Id$
+</lalVerbatim>  */
+
+/*  <lalLaTeX>
+\subsection{Module \texttt{LALInspiralCreateCoarseBank.c}}
+\subsubsection*{Prototypes}
+\subsubsection*{Description}
+\subsubsection*{Algorithm}
+\subsubsection*{Uses}
+\begin{verbatim}
+\end{verbatim}
+\subsubsection*{Notes}
+\clearpage
+</lalLaTeX>  */
+
+
