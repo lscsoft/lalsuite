7e8bc6b76228101389a5bd54025524aa1b2c7a8c
siemens <siemens>
2005-10-12 18:41:50 +0000

    generate coincidences file for binj find using all injection files (not merged coincident trigger files); changed start and end times to include all GPS times Original: 28296d0f4927d2214527c5e456f872e5f0c33b04

--- lalapps/src/string/eff_script.sh
+++ lalapps/src/string/eff_script.sh
@@ -2,29 +2,29 @@
 
 #must add a check here for the command line argument
 
-echo L1ctriggers.xml > L1.txt
-echo H1ctriggers.xml > H1.txt
-echo H2ctriggers.xml > H2.txt
+ls -1 coincidences/L1*L1H1* > L1.txt
+ls -1 coincidences/H1*H1L1* > H1.txt
+ls -1 coincidences/H2*H2L1* > H2.txt
 ls -1 HL-* > inj.txt
 
 ~/lscsoft/lalapps/src/power/lalapps_binj_find --input-trig H1.txt --input-burstinj inj.txt \
 --output-trig H1cout.xml --output-inj-made H1cinj.xml --output-inj-found H1cinjfound.xml \
---best-peaktime --compare-choice comparebytime  --noplayground --gps-start-time  795169179 \
---gps-end-time 795211620 --printresult --max-confidence $1  >& /dev/null
+--best-peaktime --compare-choice comparebytime  --noplayground --gps-start-time  0 \
+--gps-end-time 999999999 --printresult --max-confidence $1  >& /dev/null
 
 mv BinjFindResults.dat BinjFindResultsH1triple.dat
 
 ~/lscsoft/lalapps/src/power/lalapps_binj_find --input-trig H2.txt --input-burstinj inj.txt \
 --output-trig H2cout.xml --output-inj-made H2cinj.xml --output-inj-found H2cinjfound.xml \
---best-peaktime --compare-choice comparebytime --noplayground --gps-start-time  795169179 \
---gps-end-time 795211620 --printresult --max-confidence $1  >& /dev/null
+--best-peaktime --compare-choice comparebytime --noplayground --gps-start-time 0 \
+--gps-end-time 999999999 --printresult --max-confidence $1  >& /dev/null
 
 mv BinjFindResults.dat BinjFindResultsH2triple.dat
 
 ~/lscsoft/lalapps/src/power/lalapps_binj_find --input-trig L1.txt --input-burstinj inj.txt \
 --output-trig L1cout.xml --output-inj-made L1cinj.xml --output-inj-found L1cinjfound.xml \
---best-peaktime --compare-choice comparebytime --noplayground --gps-start-time 795169179 \
---gps-end-time 795211620 --printresult --max-confidence $1  >& /dev/null
+--best-peaktime --compare-choice comparebytime --noplayground --gps-start-time 0 \
+--gps-end-time 999999999 --printresult --max-confidence $1  >& /dev/null
 
 mv BinjFindResults.dat BinjFindResultsL1triple.dat
 
