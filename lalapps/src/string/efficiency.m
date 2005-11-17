352a5c773153375131e571c379a402b5d3cf246f
siemens <siemens>
2005-11-17 23:04:34 +0000

    added theta function cutoff for the rate Original: 8dbc083e6be704ac6e41d9eb5926792ad6d220c7

--- lalapps/src/string/efficiency.m
+++ lalapps/src/string/efficiency.m
@@ -28,7 +28,7 @@ H1madepeaks=[H1cinjmade0.hpeak];
 H2madepeaks=[H2cinjmade0.hpeak];
 L1madepeaks=[L1cinjmade0.hpeak];
 
-dlnA=0.35;
+dlnA=0.5;
 lnA=-0.46:dlnA:7;
 
 nH1found=histc(log(H1foundpeaks),lnA);
@@ -57,22 +57,22 @@ deL1=deL1(1:size(deL1,1)-2);
 
 lnA=lnA(1:size(lnA,2)-2);
 
-figure
-plot(lnA,eH1,'-')
-hold on
-plot(lnA,eH1,'bo')
-errorbar(lnA,eH1,deH1,'b')
-plot(lnA,eH2,'go')
-plot(lnA,eH2,'g-')
-errorbar(lnA,eH2,deH2,'g')
-plot(lnA,eL1,'ro')
-plot(lnA,eL1,'r-')
-errorbar(lnA,eL1,deL1,'r')
-plot(lnA,eH1,'-')
-plot(lnA,eH1,'bo')
-ylabel('\epsilon (A^*=1.841,A)','FontSize',12)
-xlabel('ln(A)','FontSize',12)
-title('Triple coincident efficiencies for H1 (blue), H2 (green), L1 (red)','FontSize',12)
+%figure
+%plot(lnA,eH1,'-')
+%hold on
+%plot(lnA,eH1,'bo')
+%errorbar(lnA,eH1,deH1,'b')
+%plot(lnA,eH2,'go')
+%plot(lnA,eH2,'g-')
+%errorbar(lnA,eH2,deH2,'g')
+%plot(lnA,eL1,'ro')
+%plot(lnA,eL1,'r-')
+%errorbar(lnA,eL1,deL1,'r')
+%plot(lnA,eH1,'-')
+%plot(lnA,eH1,'bo')
+%ylabel('\epsilon (A^*=1.841 x 10^{-20},A)','FontSize',12)
+%xlabel('ln(A/10^{-20} s^{-1/3})','FontSize',12)
+%title('Triple coincident efficiencies for H1 (blue), H2 (green), L1 (red)','FontSize',12)
 
 % Find interpolated A50%
 for i = 2:size(lnA,2)
@@ -100,7 +100,7 @@ p=1e-3;        %Probability of reconnection
 
 Gmu=logspace(-13,-5,100); %Vector with values of Gmu
 
-n=3/2;           %power law for Gamma*Gmu (as in my PRD paper)
+n=5/2;         %power law for Gamma*Gmu (as in my PRD paper)
 epsilon=1;     %smal-scale structure pre-factor (as in Damour and Vilenkin, 2005 PRD)
 
 A=exp(lnA)*10^(-20);  %turn vector of ln(A) into A; and re-scale by 10^-20 factor
@@ -111,18 +111,25 @@ for i=1:size(Gmu,2)
     a=A./(t0^(-1/3)*Gmu(i)*(alpha^(2/3)));   %Reduced amplitude vector      
     b=10^2 * c * alpha^(-5/3)*(p*Gamma*Gmu(i))^(-1) * t0^(-1) * (f*t0)^(-2/3);
     dRdlnA=b*aeq^(33/40)*a.^(-11/5) .* ((1+1/aeq*a).^(33/40)).*(1+a).^(-13/8); %Rate per logarithmic interval of amplitude 
-    R(i)=dlnA*sum(eH1.*dRdlnA');
 
-    R(i)=R(i)+dRdlnA(size(dRdlnA,2));   %This last addition is for events with amplitude larger than the largest amplitude 
+    %apply theta fn cutoff
+    theta0=(alpha*f*t0).^(-1/3);
+    theta=aeq^(3/40)*theta0.* a.^(-1/5) .* (1+1/aeq*a).^(3/40) .*  (1+a).^(1/8);
+
+    eH1p=eH1.*stepfun(1./theta',1);
+
+    R(i)=dlnA*sum(eH1p.*dRdlnA');
+
+%    R(i)=R(i)+dRdlnA(size(dRdlnA,2));   
+                             %This last addition is for events with amplitude larger than the largest amplitude 
                              %(which I assume we detect with efficiency 1);
                              %it makes no difference whether we add it in or
                              %not actually
-%remember to put in the theat fn cutoff here at some point
 end
 
 T=1364984;
 
-figure(3)
+figure
 loglog(Gmu,R,'r'); hold on
 loglog(Gmu,2.303/T,'r')
 xlabel('G\mu')
