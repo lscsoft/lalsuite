a40f54f6a32fdee264354098353b6f8ccd65776b
siemens <siemens>
2005-11-18 19:42:49 +0000

    latest Original: f3ab428b2d5c871f3e8f42625cffa58192366fcb

--- lalapps/src/string/plotsurvivors.m
+++ lalapps/src/string/plotsurvivors.m
@@ -11,12 +11,13 @@ cd /scratch2/xavi/CosmicStrings0/plotdata/
 
 t=0:1/16384:15-1/16384;
 
-N=1;
-
-[i,j]=find(t>)
 
 for k=1:N
-   
+
+    trigger_time=7+H1ctrig_foreground.peak_time_ns(k)*1e-9;
+    [i1,j1]=find( t > (trigger_time-0.05) );
+    [i2,j2]=find( t(j1) < (trigger_time+0.05) );
+    
     data_fileH1=['H1data',num2str(k),'.txt'];
     data_fileH2=['H2data',num2str(k),'.txt'];
     data_fileL1=['L1data',num2str(k),'.txt'];
@@ -25,5 +26,12 @@ for k=1:N
     H2data=load (data_fileH2);
     L1data=load (data_fileL1);
 
+    figure
+    subplot (3,1,1)
+    plot(t(j1(j2)),H1data(j1(j2)),'b')
+    subplot (3,1,2)
+    plot(t(j1(j2)),H2data(j1(j2)),'g')
+    subplot (3,1,3)
+    plot(t(j1(j2)),L1data(j1(j2)),'r')
 end
 
