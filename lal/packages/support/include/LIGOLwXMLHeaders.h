a28edb8b9150b2b6f62defea2655f790ea4a7752
saikat <saikat>
2005-12-01 21:35:58 +0000

    Added two columns peak_dof and event_id to sngl_burst table: PR 939 Original: c61d8630412dd78d8dace442d1da9ec8c7ab9622

--- lal/packages/support/include/LIGOLwXMLHeaders.h
+++ lal/packages/support/include/LIGOLwXMLHeaders.h
@@ -180,10 +180,12 @@ fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:amplitude\" Type=\"real_
 fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:snr\" Type=\"real_4\"/>\n", fp ) == EOF || \
 fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:confidence\" Type=\"real_4\"/>\n", fp ) == EOF || \
 fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:clusterT\" Type=\"real_4\"/>\n", fp ) == EOF || \
+fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:peak_dof\" Type=\"real_4\"/>\n", fp ) == EOF || \
+fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:event_id\" Type=\"int_8s\"/>\n", fp ) == EOF || \
 fputs( "      <Stream Name=\"sngl_burstgroup:sngl_burst:table\" Type=\"Local\" Delimiter=\",\">\n", fp ) == EOF )
 
 #define SNGL_BURST_ROW \
-"         \"process:process_id:0\",\"%s\",\"%s\",\"%s\",%d,%d,%d,%d,%e,%e,%e,%e,%e,%e,%e"
+"         \"process:process_id:0\",\"%s\",\"%s\",\"%s\",%d,%d,%d,%d,%e,%e,%e,%e,%e,%e,%e,%e,%lld"
 
 #define PRINT_LIGOLW_XML_SNGL_INSPIRAL(fp) ( \
 fputs( "   <Table Name=\"sngl_inspiralgroup:sngl_inspiral:table\">\n", fp ) == EOF || \
