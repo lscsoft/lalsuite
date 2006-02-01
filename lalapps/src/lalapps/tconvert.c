60ea85246652260e83a53098b4fc8c371739a83e
jolien <jolien>
2006-02-01 22:38:14 +0000

    modified to have many more arguments and to take utc input by default Original: 1b7f92fcaebeebf2743dfb920b46361c53beed8d

--- lalapps/src/lalapps/tconvert.c
+++ lalapps/src/lalapps/tconvert.c
@@ -17,6 +17,10 @@
 
 #define MAX_DATE_STRING_LENGTH 256
 
+/* Longitudes of LHO and LLO taken from LIGO-T980044-08 */
+#define LHO_LONGITUDE_RAD_E (M_PI*(-119.0+(24.0+27.5657/60.0)/60.0)/180.0)
+#define LLO_LONGITUDE_RAD_E (M_PI*(-90.0+(46.0+27.2654/60.0)/60.0)/180.0)
+
 const char * skip_space( const char *s )
 {
   while ( s && *s && isspace( *s ) )
@@ -50,18 +54,23 @@ void usage( const char *program, int exitcode );
 char * parse_options( char *buf, int buflen, int argc, char **argv );
 int    unix_to_gps( time_t unix_sec );
 time_t gps_to_unix( int gps_sec, int *leap );
+void set_zone( const char *zone );
+void reset_zone( void );
+void set_zone_lho( void );
+void set_zone_llo( void );
 
 /* standard formats */
 const char *iso_8601_format = "%Y-%m-%dT%H:%M:%S%z";
 const char *rfc_822_format = "%a, %d %b %Y %H:%M:%S %z";
 const char *unix_date_format = "%a %b %d %H:%M:%S %Z %Y";
 const char *output_date_format = NULL;
+const char *tz = NULL;
 
 enum { GPS_EPOCH, UNIX_EPOCH } epoch = GPS_EPOCH; /* default is GPS epoch */
 enum { SIDEREAL_HMS, SIDEREAL_RAD } sidereal_format = SIDEREAL_HMS; /* default isHH:MM:SS */
-enum { OUTPUT_DEFAULT, OUTPUT_DATE, OUTPUT_SECS, OUTPUT_JD, OUTPUT_MJD, OUTPUT_LEAPS, OUTPUT_GMST } 
-    output_type = OUTPUT_DEFAULT; 
-int utc_flag = 0; /* default is local time */
+enum { OUTPUT_DEFAULT, OUTPUT_DATE, OUTPUT_SECS, OUTPUT_JD, OUTPUT_MJD, OUTPUT_LEAPS, OUTPUT_GMST } output_type = OUTPUT_DEFAULT; 
+enum { SITE_UNSPECIFIED, SITE_LHO, SITE_LLO } site = SITE_UNSPECIFIED;
+int utc_flag = 1; /* default is universal time rather than local time */
 
 int verbose = 0; /* default is brief output */
 
@@ -87,6 +96,8 @@ int main( int argc, char *argv[] )
       perror( "parse options" );
     usage( argv[0], 1 );
   }
+  if ( tz )
+    set_zone( tz );
   
   while ( arg )
   {
@@ -112,25 +123,33 @@ int main( int argc, char *argv[] )
     {
       if ( output_type == OUTPUT_DEFAULT && ! verbose ) 
         output_type = OUTPUT_SECS;
+
+      if ( utc_flag )
+        set_zone( "00" );  /* make zone UTC for date parsing unless local */
+
       if ( is_blank( arg ) )
         unix_sec = get_date( "now", 0 );
       else
         unix_sec = get_date( arg, 0 );
-      /* need to check to see if there was a leap second */
-    leap = 0;
-    if ( strstr( arg, ":59:60" ) ) /* aha! there was one! */
-    {
-      --unix_sec; /* to distinguish this from next day */
-      leap = 1; /* add the leap second back in later */
-    }
 
-    if ( unix_sec < 0 )
-    {
-      fprintf( stderr, "could not parse date string: %s\n", arg );
-      exit( 1 );
-    }
-    gps_sec = unix_to_gps( unix_sec );
-    gps_sec += leap; /* the leap second */
+      if ( utc_flag )
+        set_zone( tz ); /* if we set zone, set it back now */
+
+      /* need to check to see if there was a leap second */
+      leap = 0;
+      if ( strstr( arg, ":59:60" ) ) /* aha! there was one! */
+      {
+        --unix_sec; /* to distinguish this from next day */
+        leap = 1; /* add the leap second back in later */
+      }
+
+      if ( unix_sec < 0 )
+      {
+        fprintf( stderr, "could not parse date string: %s\n", arg );
+        exit( 1 );
+      }
+      gps_sec = unix_to_gps( unix_sec );
+      gps_sec += leap; /* the leap second */
     }
 
     output( gps_sec, output_type );
@@ -196,6 +215,67 @@ void output_date( int gps_sec )
 
   strftime( date_string, sizeof( date_string ), fmt, tp );
   puts( date_string );
+
+  return;
+}
+
+void output_date_utc( int gps_sec )
+{
+  int utc_flag_save = utc_flag;
+  utc_flag = 1;
+  if ( verbose )
+    printf( "Coordinated Universal Time: " );
+  output_date( gps_sec );
+  utc_flag = utc_flag_save;
+  return;
+}
+
+void output_date_local( int gps_sec )
+{
+  int utc_flag_save = utc_flag;
+  utc_flag = 0;
+  if ( verbose )
+    printf( "Local Time: " );
+  output_date( gps_sec );
+  utc_flag = utc_flag_save;
+  return;
+}
+
+void output_lho_date( int gps_sec )
+{
+  set_zone_lho();
+  if ( verbose )
+    printf( "LHO " );
+  output_date_local( gps_sec );
+  set_zone( tz );
+}
+
+void output_llo_date( int gps_sec )
+{
+  set_zone_llo();
+  if ( verbose )
+    printf( "LLO " );
+  output_date_local( gps_sec );
+  set_zone( tz );
+}
+
+void output_date_site( int gps_sec )
+{
+  switch ( site )
+  {
+    case SITE_LHO:
+      output_lho_date( gps_sec );
+      break;
+    case SITE_LLO:
+      output_llo_date( gps_sec );
+      break;
+    case SITE_UNSPECIFIED:
+      output_date( gps_sec );
+      break;
+    default:
+      fprintf( stderr, "internal error: unrecognized site\n" );
+      exit( 1 );
+  }
   return;
 }
 
@@ -235,38 +315,92 @@ void output_leaps( int gps_sec )
   return;
 }
 
+void output_sidereal_time( double sidereal_time_rad )
+{
+  double sidereal_time_hrs;
+  double sidereal_time_min;
+  double sidereal_time_sec;
+  while ( sidereal_time_rad >= 2.0 * LAL_PI )
+    sidereal_time_rad -= 2.0 * LAL_PI;
+  while ( sidereal_time_rad < 0 )
+    sidereal_time_rad += 2.0 * LAL_PI;
+  switch ( sidereal_format )
+  {
+    case SIDEREAL_HMS:
+      sidereal_time_hrs = 12.0 * sidereal_time_rad / LAL_PI;
+      sidereal_time_min = 60.0 * modf( sidereal_time_hrs, &sidereal_time_hrs );
+      sidereal_time_sec = 60.0 * modf( sidereal_time_min, &sidereal_time_min );
+      sidereal_time_sec = floor( sidereal_time_sec + 0.5 ); /* round to nearest second */
+      if ( verbose )
+        printf( "Sidereal Time (HH:MM:SS) = " );
+      printf( "%02.0f:%02.0f:%02d\n", sidereal_time_hrs, sidereal_time_min, (int)sidereal_time_sec );
+      break;
+    case SIDEREAL_RAD:
+      if ( verbose )
+        printf( "Sidereal Time (radians) = " );
+      printf( "%.6f\n", sidereal_time_rad );
+      break;
+    default:
+      fprintf( stderr, "internal error: unrecognized sidereal time format\n" );
+      exit( 1 );
+  }
+  return;
+}
+
 void output_gmst( int gps_sec )
 {
   LIGOTimeGPS ligo_time;
   double gmst_rad;
-  double gmst_hrs;
-  double gmst_min;
-  double gmst_sec;
   ligo_time.gpsSeconds = gps_sec;
   ligo_time.gpsNanoSeconds = 0;
   gmst_rad = XLALGreenwichMeanSiderealTime( &ligo_time );
-  while ( gmst_rad >= 2.0 * LAL_PI )
-    gmst_rad -= 2.0 * LAL_PI;
-  while ( gmst_rad < 0 )
-    gmst_rad += 2.0 * LAL_PI;
-  switch ( sidereal_format )
+  if ( verbose )
+    printf( "Greenwich Mean " );
+  output_sidereal_time( gmst_rad );
+  return;
+}
+
+void output_local_sidereal_time_lho( int gps_sec )
+{
+  LIGOTimeGPS ligo_time;
+  double sidereal_time_rad;
+  ligo_time.gpsSeconds = gps_sec;
+  ligo_time.gpsNanoSeconds = 0;
+  sidereal_time_rad  = XLALGreenwichMeanSiderealTime( &ligo_time );
+  sidereal_time_rad += LHO_LONGITUDE_RAD_E;
+  if ( verbose )
+    printf( "LHO Local " );
+  output_sidereal_time( sidereal_time_rad );
+}
+
+void output_local_sidereal_time_llo( int gps_sec )
+{
+  LIGOTimeGPS ligo_time;
+  double sidereal_time_rad;
+  ligo_time.gpsSeconds = gps_sec;
+  ligo_time.gpsNanoSeconds = 0;
+  sidereal_time_rad  = XLALGreenwichMeanSiderealTime( &ligo_time );
+  sidereal_time_rad += LLO_LONGITUDE_RAD_E;
+  if ( verbose )
+    printf( "LLO Local " );
+  output_sidereal_time( sidereal_time_rad );
+}
+
+void output_sidereal_time_site( int gps_sec )
+{
+  switch ( site )
   {
-    case SIDEREAL_HMS:
-      gmst_hrs = 12.0 * gmst_rad / LAL_PI;
-      gmst_min = 60.0 * modf( gmst_hrs, &gmst_hrs );
-      gmst_sec = 60.0 * modf( gmst_min, &gmst_min );
-      gmst_sec = floor( gmst_sec + 0.5 ); /* round to nearest second */
-      if ( verbose )
-        printf( "Greenwich Mean Sidereal Time (HH:MM:SS) = " );
-      printf( "%02.0f:%02.0f:%02d\n", gmst_hrs, gmst_min, (int)gmst_sec );
+    case SITE_LHO:
+      output_local_sidereal_time_lho( gps_sec );
       break;
-    case SIDEREAL_RAD:
-      if ( verbose )
-        printf( "Greenwich Mean Sidereal Time (radians) = " );
-      printf( "%.6f\n", gmst_rad );
+    case SITE_LLO:
+      output_local_sidereal_time_llo( gps_sec );
+      break;
+    case SITE_UNSPECIFIED:
+      output_gmst( gps_sec );
       break;
     default:
-      fprintf( stderr, "internal error: unrecognized sidereal time format\n" );
+      fprintf( stderr, "internal error: unrecognized site\n" );
       exit( 1 );
   }
   return;
@@ -277,7 +411,7 @@ void output( int gps_sec, int type )
   switch ( type )
   {
     case OUTPUT_DATE:
-      output_date( gps_sec );
+      output_date_site( gps_sec );
       break;
     case OUTPUT_SECS:
       output_secs( gps_sec );
@@ -292,13 +426,16 @@ void output( int gps_sec, int type )
       output_leaps( gps_sec );
       break;
     case OUTPUT_GMST:
-      output_gmst( gps_sec );
+      output_sidereal_time_site( gps_sec );
       break;
     case OUTPUT_DEFAULT:
       if ( verbose )
       {
         int save_epoch = epoch;
-        output_date( gps_sec );
+        output_date_utc( gps_sec );
+        output_date_local( gps_sec );
+        output_lho_date( gps_sec );
+        output_llo_date( gps_sec );
         epoch = GPS_EPOCH;
         output_secs( gps_sec );
         epoch = UNIX_EPOCH;
@@ -308,6 +445,8 @@ void output( int gps_sec, int type )
         output_jd( gps_sec );
         output_mjd( gps_sec );
         output_gmst( gps_sec );
+        output_local_sidereal_time_lho( gps_sec );
+        output_local_sidereal_time_llo( gps_sec );
         break;
       }
       /* fall-through */
@@ -364,17 +503,107 @@ int unix_to_gps( time_t unix_sec )
   return gps_sec;
 }
 
+/* Sets TZ environment to a particular zone string, e.g., "US/Pacific".
+ * Lots of static variables here... these need to be static since they
+ * become part of the environment.  The argument is copied.
+ * Pass NULL to this function to reset TZ to its original value.
+ * Note: this function is ugly and a little bit dangerous, perhaps.*/
+#define MAX_ENV_SIZE 1024
+void set_zone( const char *zone )
+{
+#ifdef HAVE_SETENV
+  static int first = 1;
+  static char *tz_env_orig = NULL;
+  if ( first )
+  {
+    first = 0;
+    tz_env_orig = getenv( "TZ" );
+  }
+
+  if ( zone )
+    setenv( "TZ", zone, 1 );
+  else /* reset to original */
+  {
+    if ( tz_env_orig )
+      setenv( "TZ", tz_env_orig, 1 );
+    else
+      unsetenv( "TZ" );
+  }
+
+  return;
+#else
+  static int first = 1;
+  static char tz_equals_string[] = "TZ=";
+  static char tz_string[] = "TZ";
+  static char tz_env[MAX_ENV_SIZE];
+  static char *tz_env_orig = NULL;
+
+  if ( first )
+  {
+    first = 0;
+    tz_env_orig = getenv( "TZ" );
+  }
+
+  if ( zone )
+  {
+    strncpy( tz_env, "TZ=", sizeof( tz_env ) - 1 );
+    strncat( tz_env, zone, sizeof( tz_env ) - strlen( tz_env ) - 1 );
+    putenv( tz_env );
+  }
+  else /* reset to original */
+  {
+    if ( tz_env_orig ) /* TZ was originally set */
+    {
+      strncpy( tz_env, "TZ=", sizeof( tz_env ) - 1 );
+      strncat( tz_env, tz_env_orig, sizeof( tz_env ) - strlen( tz_env ) - 1 );
+      putenv( tz_env );
+    }
+    else /* try to reset */
+    {
+      putenv( tz_equals_string ); /* set TZ to blank in case can't unset */
+      putenv( tz_string ); /* this will unset TZ if possible */
+      strcpy( tz_equals_string, "" ); /* a real hack at unsetting otherwise */
+    }
+  }
+
+  return;
+#endif
+}
+
+void reset_zone( void )
+{
+  set_zone( NULL );
+  return;
+}
+
+void set_zone_lho( void )
+{
+  set_zone( "PST8PDT" );
+  return;
+}
+
+void set_zone_llo( void )
+{
+  set_zone( "CST6CDT" );
+  return;
+}
+
+
 void usage( const char *program, int exitcode )
 {
-  const char *usages = "Usage:\t%s [OPTIONS] DATE_STRING\n"
-                       "  or: \t%s [OPTIONS] SEC_SINCE_EPOCH\n";
-  const char *synopsis = "Convert between various representations of time\n";
+  const char *name = "Name:\n\t%s\n\n";
+  const char *synopsis = "Synopsis:\n\t%s [OPTIONS] DATE_STRING\n\t%s [OPTIONS] TIMER_SECONDS\n\n";
+  const char *description = "Description:\n\
+        Convert between various representations of time.\n\n";
   const char *options = "Options:\n\n\
+        -d, --date\n\
+                output a date string [default when input is timer seconds]\n\
+        \n\
         -fFORMAT, --format=FORMAT\n\
                 output format for date string [see date(1)]\n\
         \n\
         -G, --gps-epoch\n\
-                use GPS epoch (1980-01-06 00:00:00 UTC)\n\
+                use GPS epoch for timer (1980-01-06 00:00:00 UTC) [default]\n\
         \n\
         -I, --iso-8601\n\
                 output date string in ISO 8601 format\n\
@@ -384,6 +613,9 @@ void usage( const char *program, int exitcode )
         \n\
         -l, --leap-seconds\n\
                 output number of leap seconds (TAI-UTC)\n\
+        \n\
+        -L, --local\n\
+                date strings formatted for local time (current TZ setting)\
         \n\
         -m, --mjd, --modified-julian-day\n\
                 output modified Julian day\n\
@@ -397,19 +629,54 @@ void usage( const char *program, int exitcode )
         -S, --stdin\n\
                 read from standard input\n\
         \n\
+        -t, --timer\n\
+                output timer seconds [default when input is a date string]\n\
+        \n\
         -u, --utc\n\
-                print Coordinated Universal Time\n\
+                date strings formatted for Coordinated Universal Time [default]\n\
         \n\
         -U, --unix-epoch\n\
-                use Unix epoch (1970-01-01 00:00:00 UTC)\n\
+                use Unix epoch for timer (1970-01-01 00:00:00 UTC)\n\
+        \n\
+        -zTZ, --zone=TZ\n\
+                set time zone to TZ (use with --local)\n\
+        \n\
+        -ZSITE, --site=SITE\n\
+                set site to SITE which is [valid sites: LHO | LLO]\n\
         \n\
         --help  display this help and exit\n\
         \n\
         --version\n\
-                output version information and exit\n";
-  fprintf( stdout, usages, program, program );
-  fprintf( stdout, "%s", synopsis );
+                output version information and exit\n\n";
+  const char *environment = "Environment Variables:\n\
+        TZ      The timezone to use when parsing or displaying dates.\n\n";
+  const char *examples = "Examples:\n\
+        Get current time in GPS seconds:\n\
+        \n\
+                %s now\n\
+        \n\
+        Get local time corresponding to GPS time 800000000 in RFC 2822 format:\n\
+        \n\
+                %s --rfc-2822 --local 800000000\n\
+        \n\
+        Find the local sidereal time at LHO three hours ago:\n\
+        \n\
+                %s --sidereal-time --site=LHO 3 hours ago\n\
+        \n\
+        Get the Julian day of Y2K (UTC is default unless --local is used):\n\
+        \n\
+                %s --julian-day Jan 1, 2000\n\
+        \n\
+        Find out all about now:\n\
+        \n\
+                %s --verbose\n\
+        \n";
+  fprintf( stdout, name, program );
+  fprintf( stdout, synopsis, program, program );
+  fprintf( stdout, "%s", description );
   fprintf( stdout, "%s", options );
+  fprintf( stdout, "%s", environment );
+  fprintf( stdout, examples, program, program, program, program, program );
   exit( exitcode );
 }
 
@@ -418,26 +685,31 @@ char * parse_options( char *buf, int buflen, int argc, char **argv )
 {
   struct option long_options[] =
   {
+    { "date",           no_argument,            0,      'd'     },
+    { "format",         required_argument,      0,      'f'     },
+    { "gps-epoch",      no_argument,            0,      'G'     },
     { "help",           no_argument,            0,      'h'     },
+    { "iso-8601",       no_argument,            0,      'I'     },
+    { "jd",             no_argument,            0,      'j'     },
+    { "julian-day",     no_argument,            0,      'j'     },
+    { "leap-seconds",   no_argument,            0,      'l'     },
+    { "local",          no_argument,            0,      'L'     },
+    { "mjd",            no_argument,            0,      'm'     },
+    { "modified-julian-day",  no_argument,      0,      'm'     },
+    { "rfc-2822",       no_argument,            0,      'R'     },
+    { "sidereal-time",  optional_argument,      0,      's'     },
+    { "stdin",          no_argument,            0,      'S'     },
     { "utc",            no_argument,            0,      'u'     },
     { "universal",      no_argument,            0,      'u'     },
     { "unix-epoch",     no_argument,            0,      'U'     },
-    { "gps-epoch",      no_argument,            0,      'G'     },
-    { "jd",             no_argument,            0,      'j'     },
-    { "julian-day",     no_argument,            0,      'j'     },
-    { "mjd",            no_argument,            0,      'm'     },
-    { "modified-julian-day",  no_argument,      0,      'm'     },
-    { "leap-seconds",   no_argument,            0,      'l'     },
-    { "format",         required_argument,      0,      'f'     },
-    { "iso-8601",       no_argument,            0,      'I'     },
-    { "rfc-8286",       no_argument,            0,      'R'     },
-    { "sidereal-time",  optional_argument,      0,      's'     },
-    { "stdin",          no_argument,            0,      'S'     },
+    { "timer",          no_argument,            0,      't'     },
     { "verbose",        no_argument,            0,      'v'     },
     { "version",        no_argument,            0,      'V'     },
+    { "zone",           required_argument,      0,      'z'     },
+    { "site",           required_argument,      0,      'Z'     },
     { 0, 0, 0, 0 }
   };
-  char args[] = "+f:GhIjlmRsSuUvVW:";
+  char args[] = "+df:GhIjlLmRsSuUvVW:z:Z:";
   char *program = argv[0];
 
   while ( 1 )
@@ -461,12 +733,20 @@ char * parse_options( char *buf, int buflen, int argc, char **argv )
         }
         break;
 
+      case 'd': /* date */
+        output_type = OUTPUT_DATE;
+        break;
+
       case 'f': /* format */
         output_date_format = optarg;
         break;
 
       case 'G': /* gps-epoch */
         epoch = GPS_EPOCH;
+        break;
+
+      case 'h': /* help */
+        usage( program, 0 );
         break;
 
       case 'I': /* iso-8601 */
@@ -479,6 +759,10 @@ char * parse_options( char *buf, int buflen, int argc, char **argv )
 
       case 'l': /* leap-seconds */
         output_type = OUTPUT_LEAPS;
+        break;
+
+      case 'L': /* local time */
+        utc_flag = 0;
         break;
 
       case 'm': /* mjd */
@@ -511,6 +795,10 @@ char * parse_options( char *buf, int buflen, int argc, char **argv )
         fp = stdin;
         break;
 
+      case 't': /* timer */
+        output_type = OUTPUT_SECS;
+        break;
+
       case 'u': /* utc */
         utc_flag = 1;
         break;
@@ -526,6 +814,23 @@ char * parse_options( char *buf, int buflen, int argc, char **argv )
       case 'V': /* version */
         PRINT_VERSION( "tconvert" );
         exit( 0 );
+        break;
+
+      case 'z': /* zone */
+        tz = optarg;
+        break;
+
+      case 'Z': /* site */
+        if ( strcmp( optarg, "LHO" ) == 0 || strcmp( optarg, "lho" ) == 0 )
+          site = SITE_LHO;
+        else if ( strcmp( optarg, "LLO" ) == 0 || strcmp( optarg, "llo" ) == 0 )
+          site = SITE_LLO;
+        else
+        {
+          fprintf( stderr, "unrecognized detector site %s\n", optarg );
+          fprintf( stderr, "expect either \"LHO\" or \"LLO\"\n" );
+          exit( 1 );
+        }
         break;
 
       case '?':
