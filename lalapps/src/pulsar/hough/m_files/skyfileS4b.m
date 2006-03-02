55a4829e3d1e6aff8a82eda1127d8237fa6aa2b3
sintes <sintes>
2006-03-02 18:57:37 +0000

    new file Original: 9020501138e7217ea73257f1d4d3c5421b88b546

---
+++ lalapps/src/pulsar/hough/m_files/skyfileS4b.m
@@ -0,0 +1,82 @@
+% $Id$
+%
+%  Matlab script to create one skyfile 
+
+fileoutput= 'skyfileS4b';
+fid = fopen(fileoutput, 'w');
+
+%north pole
+ fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', 0.0000, pi/2.0, 0.4, 0.4 );
+ 
+%delta 1.179 \Delta alpha = 1.0472 6 patches
+ 
+delta = 1.179;
+DRa=1.0472;
+ 
+for j=0:5
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+%delta 0.786 \Delta alpha = 0.5236 , 12 patches
+ 
+delta = 0.786;
+DRa=0.5236;
+ 
+for j=0:11
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+%delta 0.393 \Delta alpha = 0.393 , 16 patches
+ 
+delta = 0.393;
+DRa=0.393;
+ 
+for j=0:15
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+
+%delta 0.0 \Delta alpha = 0.393 , 16 patches
+ 
+delta = 0.0;
+DRa=0.393;
+ 
+for j=0:15
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+
+%delta -0.393 \Delta alpha = 0.393 , 16 patches
+ 
+delta = -0.393;
+DRa=0.393;
+ 
+for j=0:15
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+
+%delta -0.786 \Delta alpha = 0.5236 , 12 patches
+ 
+delta = -0.786;
+DRa=0.5236;
+ 
+for j=0:11
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+%delta -1.179 \Delta alpha = 1.0472 6 patches
+ 
+delta = -1.179;
+DRa=1.0472;
+ 
+for j=0:5
+  fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', j*DRa, delta, DRa, 0.393 );
+end
+
+
+%south pole
+ fprintf(fid,'%7.5f %7.5f %7.5f %7.5f \n', 0.0000, -pi/2.0, 0.4, 0.4 );
+ 
+fclose(fid);
+
