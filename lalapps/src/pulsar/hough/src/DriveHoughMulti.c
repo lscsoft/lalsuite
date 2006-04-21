dab3913211495dff4757be4528d16cf539be9770
badri <badri>
2006-04-21 12:47:52 +0000

    small edit Original: 5d0ea2b060876278291d444da2f5a509640ed5f4

--- lalapps/src/pulsar/hough/src/DriveHoughMulti.c
+++ lalapps/src/pulsar/hough/src/DriveHoughMulti.c
@@ -543,7 +543,7 @@ int main(int argc, char *argv[]){
   
 
   /* print relative weights of ifos to stdout */  
-  if ( uvar_printWeights && uvar_weighNoise )
+  if ( uvar_printWeights )
     {
       UINT4 iIFO, iSFT, numsft, j;
       REAL8 *sumweights=NULL;
