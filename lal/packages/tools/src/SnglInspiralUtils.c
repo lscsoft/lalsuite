30f3313d3a40fcbf25e4ee0907b92633d124beea
thomas <thomas>
2006-04-26 20:38:07 +0000

    formatting.do not need bug report for that. Original: e817c39fdc52ab692b1ea4a22be4feb54e900c60

--- lal/packages/tools/src/SnglInspiralUtils.c
+++ lal/packages/tools/src/SnglInspiralUtils.c
@@ -1104,10 +1104,10 @@ LALBCVCVetoSingleInspiral(
       veto = 1;
     }
 
-  if (  (tmpEvent->psi0 < alphafParams.psi0cut))
-  {
-  veto =0;
-  }
+    if (  (tmpEvent->psi0 < alphafParams.psi0cut))
+    {
+      veto =0;
+    }
 	
     if ( veto == 0 )
     {
