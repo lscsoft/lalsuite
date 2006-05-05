ed9f4e1f65027aa1dcd2120c4a8e8fa4c9b4e963
bema <bema>
2006-05-05 12:42:59 +0000

    completely AT&T syntax, almost working, %edx still gets the wrong value Original: 2fd0e1397832fb6218cd2252893d4808954339dc

--- lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86gAss.c
+++ lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86gAss.c
@@ -9,6 +9,8 @@
 	    REAL4 XRes=0.0, XIms=0.0;   /* sums of Xa.re and Xa.im */
 	    REAL4 XResX=0.0, XImsX=0.0;   /* sums of Xa.re and Xa.im */
 
+	    REAL4 accFreq = 1.0; /* accumulating frequency factor, becomes common denominator */
+
 	    /* prepare values for SSE */
 	    REAL4 tempFreqX = tempFreq1; /* REAL4 because of SSE */
 	    COMPLEX8 *Xalpha_kX = Xalpha_k; /* -> SSE values */
@@ -33,60 +35,63 @@
 	    /* The SSE part is coded in Assembler */
 	    __asm __volatile
 	      (
-	       "mov %2,%%edx \n\t"
-	       "movss %3,%%xmm0 \n\t"
+	       "mov %2,%%edx             \n\t"
+	       "movss %3,%%xmm0          \n\t"
 
-	       /*
-		 one can write intel syntax in here, but the external references
-		 (input and output variables) have to be dealt with outside
-		 these directives in AT&T syntax :-(
-	       */
+	       "jmp contcode             \n"
+	       "V1100:                   \n\t"
+	       ".float 1.0               \n\t"
+	       ".float 1.0               \n\t"
+	       ".float 0.0               \n\t"
+	       ".float 0.0               \n"
+	       "V2222:                   \n\t"
+	       ".float 2.0               \n\t"
+	       ".float 2.0               \n\t"
+	       ".float 2.0               \n\t"
+	       ".float 2.0               \n"
+	       "V4444:                   \n\t"
+	       ".float 4.0               \n\t"
+	       ".float 4.0               \n\t"
+	       ".float 4.0               \n\t"
+	       ".float 4.0               \n"
+	       "contcode:                \n\t"
+	       "movups V1100,%%xmm5      \n\t"
+	       "movups V2222,%%xmm6      \n\t"
+	       "movups V4444,%%xmm7      \n\t"
 
-	       ".intel_syntax             \n\t"
-
-	       "SHUFPS XMM0,XMM0,0      \n\t" /* XMM0: f   f   f   f */
-	       "SUBPS XMM0,[PAIR]       \n\t" /* XMM0: f-1 f-1 f   f */
-	       "XORPS XMM2,XMM2         \n\t" /* XMM2 will collect the low-precision values */
+	       "shufps $0,%%xmm0,%%xmm0  \n\t" /* XMM0: f   f   f   f */
+	       "subps %%xmm5,%%xmm0      \n\t" /* XMM0: f-1 f-1 f   f */
+	       "xorps %%xmm2,%%xmm2      \n\t" /* XMM2 will collect the low-precision values */
 
 	       /* calculation (for 28 REAL4 values (7x(2 ReIm pairs))) */
 	       /* one SSE register will consist 4 REAL4 values */
 	       /* 4 REAL4 vaules = 2 ReIm pairs */
  
-	       ".rept 7                 \n\t" /* for (i=0; i<7; i++) { */
-	       "RCPPS XMM1,XMM0         \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
-	       "MULPS XMM1,(EDX)        \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
-	       "ADDPS XMM2,XMM1         \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
-	       "SUBPS XMM0,[2222]       \n\t" /* XMM2: f-3 f-3 f-2 f-2 */
-	       "ADD   EDX,8             \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
-	       ".endr                   \n\t" /* } for */
+	       ".rept 7                  \n\t" /* for (i=0; i<7; i++) { */
+	       "rcpps %%xmm0,%%xmm1      \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       "mulps (%%edx),%%xmm1     \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       "addps %%xmm1,%%xmm2      \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+	       "subps %%xmm6,%%xmm0      \n\t" /* XMM2: f-3 f-3 f-2 f-2 */
+	       "addl  $16,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
+	       ".endr                    \n\t" /* } for */
  
-	       "SUBPS XMM0,[4444]       \n\t" /* JUMP OVER FPU CALCULATED VALUES */
-	       "ADD   EDX,16            \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
- 
-	       ".rept 7                 \n\t" /* for (i=0; i<7; i++) { */
-	       "RCPPS XMM1,XMM0         \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
-	       "MULPS XMM1,(EDX)        \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
-	       "ADDPS XMM2,XMM1         \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
-	       "SUBPS XMM0,[2222]       \n\t" /* XMM2: f-3 f-3 f-2 f-2 */
-	       "ADD   EDX,8             \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
-	       ".endr                   \n\t" /* } for */
+	       "subps %%xmm7,%%xmm0      \n\t" /* JUMP OVER FPU CALCULATED VALUES */
+	       "addl  $32,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
 
- 
-	       /* XMM2 consists the low precision values */
+ 	       /* XMM2 consists the low precision values */
 	       /* XRes and Xims consist the high precision values */
  
-	       "MOVHLPS XMM3,XMM2       \n\t" /* XMM3: ? ? C_ImH C_ReH */
-	       "ADDPS XMM2,XMM3         \n\t" /* XMM2: - - C_Im C_Re */
+	       "movhlps %%xmm2,%%xmm3    \n\t" /* XMM3: ? ? C_ImH C_ReH */
+	       "addps %%xmm3,%%xmm2      \n\t" /* XMM2: - - C_Im C_Re */
 
-	       ".att_syntax             \n\t"
-
-	       "movss %0,%%xmm2         \n\t" /* SAVE Re part */
-	       "shufps xmm2,xmm2,$55h   \n\t" /* XMM2: C_Im C_Im C_Im C_Im */
-	       "movss %1,%%xmm2         \n\t" /* SAVE Im part */
+	       "movss %%xmm2,%0          \n\t" /* SAVE Re part */
+	       "shufps $1,%%xmm2,%%xmm2  \n\t" /* XMM0: f   f   f   f */
+	       "movss %%xmm2,%1          \n\t" /* SAVE Im part */
  
-	       : "=m" (XResX), "=m" (XImsX)        /* output (here: to memory)*/
+	       : "=m" (XResX), "=m" (XImsX)       /* output (here: to memory)*/
 	       : "m" (Xalpha_kX), "m" (tempFreqX) /* input (here: from memory)*/
-	       : "%xmm0","%xmm1","%xmm2","%xmm3","edx" /* clobbered registers */
+	       : "xmm0","xmm1","xmm2","xmm3",
+	         "xmm5","xmm6","xmm7","edx"       /* clobbered registers */
 	       );
 
 	    /* And last, we add the single and double precision values */
