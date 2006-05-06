40a07e5e1ee66b45ac4ecb22196ff62372a3b0ed
bema <bema>
2006-05-06 15:54:47 +0000

    make it work with Apples assembler Original: fad5b4f8c0037d9e9fccf07b874f4073ff4b6587

--- lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86gAss.c
+++ lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86gAss.c
@@ -27,9 +27,17 @@
 		Xalpha_k ++;
 	      } /* for k < klim */
 	    
-	    XRes /= accFreq;
-	    XIms /= accFreq;
+	    accFreq = 1.0 / accFreq;
+	    XRes *= accFreq;
+	    XIms *= accFreq;
 
+#ifdef __APPLE__ /* specials for Apples assembler */
+#define AD_FLOAT ".single "
+#define AD_ASCII ".ascii "
+#else /* standard x86 gas */
+#define AD_FLOAT ".float "
+#define AD_ASCII ".string "
+#endif
 	    /* The SSE part is coded in Assembler */
 	    __asm __volatile
 	      (
@@ -37,23 +45,23 @@
 	       "movss %3,%%xmm0          \n\t"
 
 	       "jmp contcode             \n"
-	       ".string \"$Id$\"\n"
+	       AD_ASCII "\"$Id$\"\n"
 
 	       "V1100:                   \n\t"
-	       ".float 0.0               \n\t"
-	       ".float 0.0               \n\t"
-	       ".float 1.0               \n\t"
-	       ".float 1.0               \n"
+	       AD_FLOAT "0.0             \n\t"
+	       AD_FLOAT "0.0             \n\t"
+	       AD_FLOAT "1.0             \n\t"
+	       AD_FLOAT "1.0             \n"
 	       "V2222:                   \n\t"
-	       ".float 2.0               \n\t"
-	       ".float 2.0               \n\t"
-	       ".float 2.0               \n\t"
-	       ".float 2.0               \n"
+	       AD_FLOAT "2.0             \n\t"
+	       AD_FLOAT "2.0             \n\t"
+	       AD_FLOAT "2.0             \n\t"
+	       AD_FLOAT "2.0             \n"
 	       "V4444:                   \n\t"
-	       ".float 4.0               \n\t"
-	       ".float 4.0               \n\t"
-	       ".float 4.0               \n\t"
-	       ".float 4.0               \n"
+	       AD_FLOAT "4.0             \n\t"
+	       AD_FLOAT "4.0             \n\t"
+	       AD_FLOAT "4.0             \n\t"
+	       AD_FLOAT "4.0             \n"
 	       "contcode:                \n\t"
 	       "movups V1100,%%xmm5      \n\t"
 	       "movups V2222,%%xmm6      \n\t"
@@ -66,29 +74,35 @@
 	       /* calculation (for 28 REAL4 values (7x(2 ReIm pairs))) */
 	       /* one SSE register will consist 4 REAL4 values */
 	       /* 4 REAL4 vaules = 2 ReIm pairs */
- 
-	       ".rept 7                  \n\t" /* for (i=0; i<7; i++) { */
-	       "movlps (%%edx),%%xmm4    \n\t"
-	       "movhps 8(%%edx),%%xmm4   \n\t"
-	       "rcpps %%xmm0,%%xmm1      \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
-	       "subps %%xmm6,%%xmm0      \n\t" /* XMM2: f-3 f-3 f-2 f-2 */
-	       "mulps %%xmm4,%%xmm1      \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
-	       "addl  $16,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
+
+	       /* we want to do this 14 times */
+#define ADD4SSE \
+	       "movlps (%%edx),%%xmm4    \n\t"\
+	       "movhps 8(%%edx),%%xmm4   \n\t"\
+	       "rcpps %%xmm0,%%xmm1      \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */\
+	       "subps %%xmm6,%%xmm0      \n\t" /* XMM2: f-3 f-3 f-2 f-2 */\
+	       "mulps %%xmm4,%%xmm1      \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */\
+	       "addl  $16,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */\
 	       "addps %%xmm1,%%xmm2      \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
-	       ".endr                    \n\t" /* } for */
+
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
  
 	       "subps %%xmm7,%%xmm0      \n\t" /* JUMP OVER FPU CALCULATED VALUES */
 	       "addl  $32,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
 
-	       ".rept 7                  \n\t" /* for (i=0; i<7; i++) { */
-	       "movlps (%%edx),%%xmm4    \n\t"
-	       "movhps 8(%%edx),%%xmm4   \n\t"
-	       "rcpps %%xmm0,%%xmm1      \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
-	       "subps %%xmm6,%%xmm0      \n\t" /* XMM2: f-3 f-3 f-2 f-2 */
-	       "mulps %%xmm4,%%xmm1      \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
-	       "addl  $16,%%edx          \n\t" /* Xalpha_kX = Xalpha_kX + 2; */
-	       "addps %%xmm1,%%xmm2      \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
-	       ".endr                    \n\t" /* } for */
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
+	       ADD4SSE
  
  	       /* XMM2 consists the low precision values */
 	       /* XRes and Xims consist the high precision values */
