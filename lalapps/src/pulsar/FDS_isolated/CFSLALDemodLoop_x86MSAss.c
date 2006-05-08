f94635f8829993f9726640383d3bc0052c53741e
bema <bema>
2006-05-08 11:43:47 +0000

    works Original: a102ede375b318c5bef60a011cf3f25a52e97050

--- lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86MSAss.c
+++ lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_x86MSAss.c
@@ -1,5 +1,11 @@
           {
-            COMPLEX8 *Xalpha_k = Xalpha + sftIndex;
+	    typedef struct R44Vtag {
+	      REAL4 a,b,c,d;
+	    } R44V;
+
+	    NRCSID (CFSLOOPx86MAS, "$Id$");
+
+	    COMPLEX8 *Xalpha_k = Xalpha + sftIndex;
 
 	    REAL4 tsin2pi = tsin * (REAL4)OOTWOPI;
 	    REAL4 tcos2pi = tcos * (REAL4)OOTWOPI;
@@ -12,6 +18,17 @@
 	    /* prepare values for SSE */
 	    REAL4 tempFreqX = tempFreq1; /* REAL4 because of SSE */
 	    COMPLEX8 *Xalpha_kX = Xalpha_k; /* -> SSE values */
+	    R44V V0011,V2222;
+	    
+	    V0011.a = 0.0;
+	    V0011.b = 0.0;
+	    V0011.c = 1.0;
+	    V0011.d = 1.0;
+
+	    V2222.a = 2.0;
+	    V2222.b = 2.0;
+	    V2222.c = 2.0;
+	    V2222.d = 2.0;
 
 	    tempFreq1 = tempFreq1 - 14;
 	    Xalpha_k = Xalpha_k + 14; /* -> FPU values */
@@ -31,84 +48,147 @@
 	    XRes *= accFreq;
 	    XIms *= accFreq;
 
-#define AD_FLOAT ".float "
-#define AD_ASCII ".string "
-
 	    /* The SSE part is coded in Assembler */
-	    __asm __volatile
-	      (
-	       "mov %2,%%edx\n\t"
-	       "movss %3,%%xmm0\n\t"
+	    __asm {
+	       MOV    EBX,Xalpha_kX
+	       MOVSS  XMM0,tempFreqX
 
-	       "jmp contcode\n"
-	       AD_ASCII "\"$Id$\"\n"
-
-	       "V0011:\n\t"
-	       AD_FLOAT "0.0\n\t"
-	       AD_FLOAT "0.0\n\t"
-	       AD_FLOAT "1.0\n\t"
-	       AD_FLOAT "1.0\n"
-	       "V2222:\n\t"
-	       AD_FLOAT "2.0\n\t"
-	       AD_FLOAT "2.0\n\t"
-	       AD_FLOAT "2.0\n\t"
-	       AD_FLOAT "2.0\n"
-	       "contcode:\n\t"
-
-	       "MOVUPS XMM5,V0011\n\t"
-	       "SHUFPS XMM0,XMM0,0\n\t" /* XMM0: f   f   f   f */
-	       "SUBPS  XMM0,XMM5\n\t" /* XMM0: f-1 f-1 f   f */
-	       "XORPS  XMM2,XMM2\n\t" /* XMM2 will collect the low-precision values */
-	       "MOVUPS XMM5,V2222\n\t"
+	       MOVUPS XMM5,V0011
+	       SHUFPS XMM0,XMM0,0 /* XMM0: f   f   f   f */
+	       SUBPS  XMM0,XMM5 /* XMM0: f-1 f-1 f   f */
+	       XORPS  XMM2,XMM2 /* XMM2 will collect the low-precision values */
+	       MOVUPS XMM5,V2222
 
 	       /* calculation (for 28 REAL4 values (7x(2 ReIm pairs))) */
 	       /* one SSE register will consist 4 REAL4 values */
 	       /* 4 REAL4 vaules = 2 ReIm pairs */
 
-	       /* we want to do this 14 times */
-#define ADD4SSE \
-	       "MOVLPS XMM4,[EDX]\n\t"\
-	       "MOVHPS XMM4,[EDX+8]\n\t"\
-	       "RCPPS  XMM1,XMM0\n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */\
-	       "SUBPS  XMM0,XMM5\n\t" /* XMM2: f-3 f-3 f-2 f-2 */\
-	       "MULPS  XMM1,XMM4\n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */\
-	       "ADD    EDX,16\n\t" /* Xalpha_kX = Xalpha_kX + 2; */\
-	       "ADDPS  XMM2,XMM1\n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+     	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
 
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
- 
-	       "SUBPS  XMM0,XMM5\n\t" /* JUMP OVER FPU CALCULATED VALUES */
-	       "ADD    EDX,32\n\t" /* Xalpha_kX = Xalpha_kX + 2; */
-	       "SUBPS  XMM0,XMM5\n\t" /* JUMP OVER FPU CALCULATED VALUES */
+     	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
 
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
-	       ADD4SSE
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
  
+	       SUBPS  XMM0,XMM5 /* JUMP OVER FPU CALCULATED VALUES */
+	       ADD    EBX,32 /* Xalpha_kX = Xalpha_kX + 2; */
+	       SUBPS  XMM0,XMM5 /* JUMP OVER FPU CALCULATED VALUES */
+
+	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+  	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
+ 	       MOVLPS XMM4,[EBX]
+	       MOVHPS XMM4,[EBX+8]
+	       RCPPS  XMM1,XMM0 /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */
+	       SUBPS  XMM0,XMM5 /* XMM2: f-3 f-3 f-2 f-2 */
+	       MULPS  XMM1,XMM4 /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */
+	       ADD    EBX,16 /* Xalpha_kX = Xalpha_kX + 2; */
+	       ADDPS  XMM2,XMM1 /* XMM2: C_ImH C_ReH C_ImL C_ReL */
+
  	       /* XMM2 consists the low precision values */
 	       /* XRes and Xims consist the high precision values */
  
-	       "MOVHLPS XMM3,XMM2\n\t" /* XMM3: ? ? C_ImH C_ReH */
-	       "ADDPS   XMM2,XMM3\n\t" /* XMM2: - - C_Im C_Re */
+	       MOVHLPS XMM3,XMM2 /* XMM3: ? ? C_ImH C_ReH */
+	       ADDPS   XMM2,XMM3 /* XMM2: - - C_Im C_Re */
 
-	       "MOVSS   %0,XMM2\n\t" /* SAVE Re part */
-	       "SHUFPS  XMM2,XMM2,1\n\t" /* XMM0: f   f   f   f */
-	       "MOVSS   %1,XMM2\n\t" /* SAVE Im part */
- 
-	       : "=m" (XResX), "=m" (XImsX)       /* output  (here: to memory)*/
-	       : "m" (Xalpha_kX), "m" (tempFreqX) /* input (here: from memory)*/
-	       : "edx"                            /* clobbered registers */
-	       );
+	       MOVSS   XResX,XMM2 /* SAVE Re part */
+	       SHUFPS  XMM2,XMM2,1 /* XMM0: f   f   f   f */
+	       MOVSS   XImsX,XMM2 /* SAVE Im part */
+		 }
 
 	    /* And last, we add the single and double precision values */
 	    
