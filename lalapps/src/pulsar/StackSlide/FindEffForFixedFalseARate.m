634c36b7cb26295016f805dc8173c9f710e872b1
gmendell <gmendell>
2006-05-08 18:52:01 +0000

    Change option so that if threshold is given as an arguments, then search xml files are not used, only MC xml filesare used. Original: 62a46afe28cc912e35584b7dc7fc69da0bc76f5f

--- lalapps/src/pulsar/StackSlide/FindEffForFixedFalseARate.m
+++ lalapps/src/pulsar/StackSlide/FindEffForFixedFalseARate.m
@@ -1,5 +1,5 @@
-function FindEffForFixedFalseARate(jobID,fileList,injectedH0,falseAlarmRate,numSFT,inputThreshold,outputFile,graphOption)
-% Usage: FindEffForFixedFalseARate(jobID,fileList,injectedH0,falseAlarmRate,numSFT,inputThreshold,outputFile,graphOption)
+function FindEffForFixedFalseARate(jobID,fileList,injectedH0,falseAlarmRate,inputThreshold,outputFile,graphOption)
+% Usage: FindEffForFixedFalseARate(jobID,fileList,injectedH0,falseAlarmRate,inputThreshold,outputFile,graphOption)
 % jobID: ID number of the job to be processed.
 % fileLIst: entire list of files for all jobs.  This files has the format <node letter+jobID> <filename>, e.g,:
 %
@@ -22,8 +22,7 @@ function FindEffForFixedFalseARate(jobID,fileList,injectedH0,falseAlarmRate,numS
 % searchXMLfile: file with list of files with output of StackSlide Search
 % listMCxmlFiles: file with list of files with output of corresponding Monte Carlo Simulation
 % injectedH0: value of signal amplitude h_0 injected during Monte Carlo Simulation
-% falseAlarmRate: falseAlarmRate used to determine threshold for efficiency measurement..
-% numSFT: Not used.
+% falseAlarmRate: falseAlarmRate used to determine threshold for efficiency measurement.
 % inputThreshold: if NOT > 0 get threshold from the falseAlarmRate; otherwise use this as the threshold.
 % outputFile: name of output file
 % graphOption: if > 0 then display plots
@@ -35,9 +34,6 @@ if (ischar(injectedH0))
 end
 if (ischar(falseAlarmRate))
     falseAlarmRate=str2num(falseAlarmRate);
-end
-if (ischar(numSFT))
-    numSFT=str2num(numSFT);
 end
 if (ischar(inputThreshold))
     inputThreshold=str2num(inputThreshold);
@@ -57,7 +53,7 @@ jobIDB = sprintf('B%s',jobID);
 [jobIDList, entireFileList] = textread(fileList,'%s %s');
 entireFileListLength = length(entireFileList);
 
-% Find the search and Monte Carlo (mc) files:
+% Find the search and Monte Carlo (mc) files. The search files are not needed if inputThreshold > 0.
 countA = 0;
 countB = 0;
 for i=1:entireFileListLength
@@ -70,36 +66,62 @@ for i=1:entireFileListLength
     end
 end
 
-%searchFileList = textRead(listSearchXMLfiles,'%s');
-searchFileListLength = length(searchFileList);
+% The search files are not used if inputThreshold > 0.
+if (inputThreshold > 0)
+  falseARateThreshold = inputThreshold;
+else
+  if (countA < 1)
+    fprintf('\nError: search files must exist if inputThreshold == 0.\n');
+    return;
+  end
+  searchFileListLength = length(searchFileList);
+  freqs = [];
+  RAs = [];
+  DECs = [];
+  fDerivs = [];
+  eventPowers = [];
+  widths = [];
+  totalNumTemplates = 0.0;
+  for i=1:searchFileListLength;
+    fileName = char(searchFileList(i));
+    if (exist(fileName,'file') < 1)
+       fprintf('\nError: search files must exist if inputThreshold == 0.\n');
+       return;
+    end
+    tbl = readMeta(fileName,'searchsummary_stackslidesfts');  
+    numBins = tbl.num_bins
+    numSums = tbl.num_sums
+    totalNumTemplates = totalNumTemplates + (numBins*numSums)
+    tbl = readMeta(fileName,'sngl_stackslideperiodic');
+    freqs = [ freqs; tbl.frequency ];
+    RAs = [ RAs; tbl.sky_ra ];
+    DECs = [ DECs; tbl.sky_dec ];
+    fDerivs = [ fDerivs; tbl.fderiv_1 ];
+    eventPowers = [eventPowers; tbl.power];
+    widths = [ widths; tbl.width_bins ];    
+  end
+  eventPowers = sort(eventPowers);
+  lengthEventPowers = length(eventPowers);
+  iFARateBin = lengthEventPowers - round(falseAlarmRate*totalNumTemplates)
+  falseARateThreshold = eventPowers(iFARateBin)
+end
+
+% Read in the Monte Carlo Simulation Files:
+%mcFileList = textRead(listMCxmlFiles,'%s');
+mcFileListLength = length(mcFileList);
+power = [];
 start_freqs = [];
 bands = [];
-freqs = [];
-RAs = [];
-DECs = [];
-fDerivs = [];
-eventPowers = [];
-widths = [];
-fileName = char(searchFileList(1)); % get the IFO name for the first file
-tbl = readMeta(fileName,'searchsummary_stackslidesfts');
-IFO =char(tbl.ifo);
-totalNumTemplates = 0.0;
-for i=1:searchFileListLength;
-  fileName = char(searchFileList(i));
-  tbl = readMeta(fileName,'searchsummary_stackslidesfts');  
+for i=1:mcFileListLength;
+  fileName = char(mcFileList(i));
+  tbl = readMeta(fileName,'searchsummary_stackslidesfts');
+  IFO =char(tbl.ifo); % ifo should be the same in all files
   start_freqs = [ start_freqs; tbl.start_freq ];
   bands = [ bands; tbl.band ];
-  numBins = tbl.num_bins
-  numSums = tbl.num_sums
-  totalNumTemplates = totalNumTemplates + (numBins*numSums)
   tbl = readMeta(fileName,'sngl_stackslideperiodic');
-  freqs = [ freqs; tbl.frequency ];
-  RAs = [ RAs; tbl.sky_ra ];
-  DECs = [ DECs; tbl.sky_dec ];
-  fDerivs = [ fDerivs; tbl.fderiv_1 ];
-  eventPowers = [eventPowers; tbl.power];
-  widths = [ widths; tbl.width_bins ];    
+  power = [ power; tbl.power ];  
 end
+powerLength = length(power);
 
 % Open file for output
 fid = fopen(outputFile,'w');
@@ -110,31 +132,14 @@ startFreq = min(start_freqs);
 searchBand = maxFreq + bands(iMaxFreq) - startFreq;
 fprintf('\nIFO, startFreq, band = %s, %g, %g\n',IFO, startFreq,searchBand);
 fprintf(fid,'\nIFO, startFreq, band = %s, %g, %g\n',IFO, startFreq,searchBand);
- 
-% Set up overall loudest event, and print loudest event for each frequency band.
+
 if (inputThreshold > 0)
-  falseARateThreshold = inputThreshold;
   fprintf('\nfalseARateThreshold was input as = %g\n\n',falseARateThreshold);
   fprintf(fid,'\nfalseARateThreshold was input as = %g\n\n',falseARateThreshold);
-else 
- eventPowers = sort(eventPowers);
- lengthEventPowers = length(eventPowers);
- iFARateBin = lengthEventPowers - round(falseAlarmRate*totalNumTemplates)
- falseARateThreshold = eventPowers(iFARateBin)
- fprintf('\nfalseARateThreshold found is: %g \n',falseARateThreshold);
- fprintf(fid,'\nfalseARateThreshold event found is: %g \n',falseARateThreshold);
+else
+  fprintf('\nfalseARateThreshold found is: %g \n',falseARateThreshold);
+  fprintf(fid,'\nfalseARateThreshold event found is: %g \n',falseARateThreshold);
 end
-
-% Read in the Monte Carlo Simulation Files:
-%mcFileList = textRead(listMCxmlFiles,'%s');
-mcFileListLength = length(mcFileList);
-power = [];
-for i=1:mcFileListLength;
-  fileName = char(mcFileList(i));
-  tbl = readMeta(fileName,'sngl_stackslideperiodic');
-  power = [ power; tbl.power ];
-end
-powerLength = length(power);
 
 % find efficiency for finding power with injections above search LE: 
 louderPowersLength = length(find(power>falseARateThreshold));
@@ -148,19 +153,6 @@ if graphOption > 0
   nBins = 50;
   [y,x] = hist(power,nBins);
   hist(power,nBins);
-  %plot(x,y,'+');
-  %C = (numSTK^numSTK)/factorial(numSTK-1);
-  %C = 1;
-  %xplot = min(x) + [0:960]*(max(x) - min(x))/999.0;
-  %yPDF = C*xplot.^(numSFT-1).*exp(-1.0*numSFT*xplot);
-  %yPDFint = trapz(xplot,yPDF);
-  %yint = trapz(x,y);
-  %yPDF = yint*yPDF/yPDFint;
-  %xlabel('STACKSLIDE POWER');
-  %ylabel('NUMBER');
-  %hold
-  %plot(xplot,yPDF,'r');
-  %hold
 end
 
 fclose(fid);
