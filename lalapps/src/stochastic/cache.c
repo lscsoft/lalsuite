95fcd21d9c5254a1b380751af31b18ab33b0fb87
tania <tania>
2006-06-07 07:24:23 +0000

    small program to generate cache files for project1a data Original: b9356ed25d9eede7a0a8770f2bc7d2a6f6eb6cc9

---
+++ lalapps/src/stochastic/cache.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define length 16
+                                                                               
+main()
+ {
+
+  int i, start, stop, N;
+  FILE *pf1,*pf2;
+  pf = fopen ("VirgoP1a.cache","w");
+  pf = fopen ("LIGOP1a.cache","w");
+  start = 782758813;
+  stop = 782769553;
+  N = ceil ((stop - start)/length) + 1;  
+  for (i =0; i < N; i++)
+    {
+     
+      fprintf(pf1,"V2 SIM %d 60 file://localhost/home/tania/lscsoft/src/lalapps/src/sgwb/data/PROJECT1a/simulated_virgo_data/SIM/V2-SIM-%d-60.gwf\n",start,start);
+
+   
+     fprintf(pf2,"H SIM %d 60 file://localhost/home/tania/lscsoft/src/lalapps/src/sgwb/data/PROJECT1a/simulated_ligo_noise/SIM/H-SIM-%d-60.gwf\n",start,start);
+     start = start+60;
+    
+    }
+  fclose(pf1);fclose(pf2);
+
+ 
+
+ }  
