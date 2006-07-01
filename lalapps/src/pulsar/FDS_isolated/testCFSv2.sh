eb6a678381a82bde370750bcc584fa3841bc09b3
reinhard <reinhard>
2006-07-01 22:05:21 +0000

    add a little test for parameter-estimation Original: 13dc37f6cf62f474f99d76b732c2a7fda027cdc7

--- lalapps/src/pulsar/FDS_isolated/testCFSv2.sh
+++ lalapps/src/pulsar/FDS_isolated/testCFSv2.sh
@@ -25,9 +25,9 @@ if [ -z "$LAL_DATA_PATH" ]; then
 fi
 
 # ---------- fixed parameter of our test-signal
-Tsft=60;
-startTime=711595933
-refTime=711595933  ## $startTime
+Tsft=1800;
+startTime=711595934
+refTime=701595833  ## $startTime
 duration=144000		## 40 hours
 
 mfd_FreqBand=2.0;
@@ -36,20 +36,20 @@ Alpha=2.0
 Delta=-0.5
 
 aPlus=1.0e-20
-aCross=0.4e-20
+aCross=4e-21
 
-psi=0
-phi0=0
+psi=0.6
+phi0=1.5
 
-freq=100.0
+freq=100.12345
 mfd_fmin=$(echo $freq $mfd_FreqBand | awk '{printf "%g", $1 - $2 / 2.0}');
 
 echo "mfd_fmin = $mfd_fmin"
 
-f1dot=1e-8
+f1dot=0
 df1dot=0.3e-8	## search about 3 spindown-values
 
-noiseSqrtSh=3e-20
+noiseSqrtSh=8e-20   ##3e-20
 
 ## ------------------------------------------------------------
 
@@ -131,6 +131,7 @@ echo " STEP 3: run CFS_v2 with perfect match"
 echo "----------------------------------------------------------------------"
 echo
 cmdline="$cfsv2_code $cfs_CL --outputFstat=Fstat_v2.dat --TwoFthreshold=0 $extra_args";
+cmdline="$cmdline --outputLoudest=loudest.dat";
 echo $cmdline;
 
 if ! eval time $cmdline; then
