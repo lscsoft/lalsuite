bd919e05cdbe41e518e84b25b86e3c4f22a3758e
badri <badri>
2006-07-04 16:44:12 +0000

    small edits Original: 205db7337bf7cb6546cdc678e395babc14d13fdd

--- lalapps/src/pulsar/hough/src2/ValidateHS.m
+++ lalapps/src/pulsar/hough/src2/ValidateHS.m
@@ -1,6 +1,6 @@
 
 
-load HS_fstatVec1.txt
+load out/HS_fstatVec1.txt
 
 
 Freq=HS_fstatVec1(:,1);
@@ -18,23 +18,26 @@ maxEndTime=820631477;
 refTime=600000000;
 DataFiles="/local_data/badkri/fakesfts/H-1_H1*.sft";
 
-index = 1;
-
-cmdline = sprintf("ComputeFStatistic_v2 --Freq=%.12g --f1dot=%.12g \
---FreqBand=%.12g --f1dotBand=%.12g --dFreq=%.12g --df1dot=%.12g \
---dAlpha=%.12g  --dDelta=%.12g --minStartTime=%d --maxEndTime=%d --refTime=%d \
---DataFiles=%s --Alpha=%.12g --Delta=%.12g --AlphaBand=0.1 --DeltaBand=0.1 \
---ephemDir=/local_data/badkri/lscsoft/share/lal/ --ephemYear=05-09 \
---TwoFthreshold=0 --gridType=0 --outputLabel=CFSv2 \
---outputFstat=CFSv2 -v1", Freq(index), f1dot(index), FreqBand, f1dotBand, dFreq, df1dot, \
-		  dAlpha, dDelta, minStartTime, maxEndTime, refTime, DataFiles, \
-		  Alpha(index), Delta(index))  
-
-[output, status] = system(cmdline)
-
 load CFSv2
 
-F_CFSv2 = CFSv2(1,7);
-F_HS = 2*HS_fstatVec1(index,5);
-diff=F_CFSv2 - F_HS
+for index=1:length(Freq) 
+  
+  cmdline = sprintf("ComputeFStatistic_v2 --Freq=%.12g --f1dot=%.12g \
+  --FreqBand=%.12g --f1dotBand=%.12g --dFreq=%.12g --df1dot=%.12g \
+  --dAlpha=%.12g  --dDelta=%.12g --minStartTime=%d --maxEndTime=%d --refTime=%d \
+  --DataFiles=%s --Alpha=%.12g --Delta=%.12g --AlphaBand=0.1 --DeltaBand=0.1 \
+  --ephemDir=/local_data/badkri/lscsoft/share/lal/ --ephemYear=05-09 \
+      --TwoFthreshold=0 --gridType=0 --outputLabel=CFSv2 \
+--outputFstat=CFSv2 -v1", Freq(index), f1dot(index), FreqBand, f1dotBand, dFreq, df1dot, \
+		    dAlpha, dDelta, minStartTime, maxEndTime, refTime, DataFiles, \
+		    Alpha(index), Delta(index))  
+  
+  [output, status] = system(cmdline)
+    
+  F_CFSv2(index) = CFSv2(1 ,7);
+  F_HS(index) = 2*HS_fstatVec1(index,5);
+
+endfor
+
+diff=F_CFSv2 - F_HS;
 
