7308b9c7f7c43114088ffd57579d49c5fbe6146c
spxcar <spxcar>
2006-07-17 15:50:35 +0000

    Fixing slight bg in SnglInspiralTable reading code. Original: ad09e0718e9158cf3b649d65c836609f1aeb4f87

--- lal/packages/support/src/LIGOLwXMLRead.c
+++ lal/packages/support/src/LIGOLwXMLRead.c
@@ -1151,6 +1151,7 @@ LALSnglInspiralTableFromLIGOLw (
       /* parse the contents of the row into the InspiralTemplate structure */
       for ( j = 0; tableDir[j].name; ++j )
       {
+        if ( tableDir[j].pos < 0 ) continue;
         REAL4 r4colData = env->ligo_lw.table.elt[tableDir[j].pos].data.real_4;
         REAL8 r8colData = env->ligo_lw.table.elt[tableDir[j].pos].data.real_8;
         INT4  i4colData = env->ligo_lw.table.elt[tableDir[j].pos].data.int_4s;
@@ -1577,6 +1578,7 @@ InspiralTmpltBankFromLIGOLw (
       /* parse the contents of the row into the InspiralTemplate structure */
       for ( j = 0; tableDir[j].name; ++j )
       {
+        if ( tableDir[j].pos < 0 ) continue;
         REAL4 colData = env->ligo_lw.table.elt[tableDir[j].pos].data.real_4;
         INT4 i4colData = env->ligo_lw.table.elt[tableDir[j].pos].data.int_4s;
         UINT8 i8colData = env->ligo_lw.table.elt[tableDir[j].pos].data.int_8s;
