8ee2a742acd01db3dc2ab15030d2d6e64fd18529
bema <bema>
2006-07-31 07:48:16 +0000

    this is not yet optimal, checked in for testing only Original: 7c31a3bce131037a1ad411c16d39b2bae99e1266

--- lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_ndp_vect.c
+++ lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_ndp_vect.c
@@ -7,6 +7,7 @@
 	    REAL4 tsin2pi = tsin * (REAL4)OOTWOPI;
 	    REAL4 tcos2pi = tcos * (REAL4)OOTWOPI;
 	    REAL4 XRes, XIms;
+	    REAL8 combAF;
 
 	    /* The main idea of the vectorization is that
 
@@ -26,10 +27,14 @@
 	       times.
  	    */
 
-	    REAL4 tFreq[4]; /* tempFreq0 as a vector */
-	    REAL4 tFint[4]; /* the integer part of the former tempFreq1 */
+	    REAL4 tFreq[4]; /* tempFreq1 as a vector */
 	    REAL4 aFreq[4]; /* accFreq   as a vector */
 	    REAL4  Xsum[4]; /* vector holding partial sums */
+
+	    /* the same in double precision, two elements at once */
+	    REAL8 tFreqD[2]; /* tempFreq1 as a vector */
+	    REAL8 aFreqD[2]; /* accFreq   as a vector */
+	    REAL8  XsumD[2]; /* vector holding partial sums */
 
 	    /* init vectors */
 	    aFreq[0] = 1.0;
@@ -37,20 +42,20 @@
 	    aFreq[2] = 1.0;
 	    aFreq[3] = 1.0;
 
-	    tFreq[0] = tempFreq0;
-	    tFreq[1] = tempFreq0;
-	    tFreq[2] = tempFreq0;
-	    tFreq[3] = tempFreq0;
-
-	    tFint[0] = klim/2 - 1;
-	    tFint[1] = klim/2 - 1;
-	    tFint[2] = klim/2 - 2;
-	    tFint[3] = klim/2 - 2;
+	    tFreq[0] = tempFreq0 + klim/2 - 1;
+	    tFreq[1] = tempFreq0 + klim/2 - 1;
+	    tFreq[2] = tempFreq0 + klim/2 - 2;
+	    tFreq[3] = tempFreq0 + klim/2 - 2;
 
 	    Xsum[0] = 0.0;
 	    Xsum[1] = 0.0;
 	    Xsum[2] = 0.0;
 	    Xsum[3] = 0.0;
+
+	    aFreqD[0] = 1.0;
+	    aFreqD[1] = 1.0;
+	    XsumD[0] = 0.0;
+	    XsumD[1] = 0.0;
 
 	    /* Vectorized version of the "hot loop" */
 	    /* This loop is now unrolled manually */
@@ -59,14 +64,37 @@
 	      UINT4 ve;
 #define VEC_LOOP(n)\
 	      for(ve=0;ve<4;ve++) {\
-                Xsum[ve] = Xsum[ve] * (tFreq[ve] + tFint[ve]) + (Xalpha_kR4+n)[ve] * aFreq[ve];\
-		aFreq[ve] *= (tFreq[ve] + tFint[ve]);\
-                tFint[ve] -= 2.0;\
+                Xsum[ve] = Xsum[ve] * tFreq[ve] + (Xalpha_kR4+n)[ve] * aFreq[ve];\
+		aFreq[ve] *= tFreq[ve];\
+                tFreq[ve] -= 2.0;\
+	      }
+
+#define VEC_LOOP_D(n)\
+	      for(ve=0;ve<2;ve++) {\
+                XsumD[ve] = XsumD[ve] * tFreqD[ve] + (Xalpha_kR4+n)[ve] * aFreqD[ve];\
+		aFreqD[ve] *= tFreqD[ve];\
+                tFreqD[ve] -= 1.0;\
 	      }
 
 	      VEC_LOOP(00+0); VEC_LOOP(00+4); VEC_LOOP(00+8); VEC_LOOP(00+12); 
-	      VEC_LOOP(16+0); VEC_LOOP(16+4); VEC_LOOP(16+8); VEC_LOOP(16+12); 
-	      VEC_LOOP(32+0); VEC_LOOP(32+4); VEC_LOOP(32+8); VEC_LOOP(32+12); 
+	      VEC_LOOP(16+0); VEC_LOOP(16+4); VEC_LOOP(16+8);
+
+              /* calculating the inner elements
+		 VEC_LOOP(16+12); VEC_LOOP(32+0);
+		 in double precision */
+
+	      /* skip the values in single precision calculation */
+	      for(ve=0;ve<4;ve++)
+                tFreq[ve] -= 4.0;
+		
+	      tFreqD[0] = tempFreq0 + klim/2 - 14;
+	      tFreqD[1] = tempFreq0 + klim/2 - 14;
+	      VEC_LOOP_D(16+12);
+	      VEC_LOOP_D(16+14);
+	      VEC_LOOP_D(32+00);
+	      VEC_LOOP_D(32+08);
+
+	      VEC_LOOP(32+4); VEC_LOOP(32+8); VEC_LOOP(32+12); 
 	      VEC_LOOP(48+0); VEC_LOOP(48+4); VEC_LOOP(48+8); VEC_LOOP(48+12); 
 	    }
 
@@ -76,6 +104,11 @@
 	    aFreq[1] *= aFreq[2];
 	    XRes = (Xsum[0] * aFreq[2] + Xsum[2] * aFreq[0]) / aFreq[1];
 	    XIms = (Xsum[1] * aFreq[2] + Xsum[3] * aFreq[0]) / aFreq[1];
+
+	    /* combine with double precision calculation */
+	    aFreqD[1] *= aFreq[1];
+	    XRes = (XRes * aFreqD[0] + XsumD[0] * aFreq[1]) / aFreqD[1];
+	    XIms = (XIms * aFreqD[0] + XsumD[1] * aFreq[1]) / aFreqD[1];
 
             realXP = tsin2pi * XRes - tcos2pi * XIms;
             imagXP = tcos2pi * XRes + tsin2pi * XIms;
