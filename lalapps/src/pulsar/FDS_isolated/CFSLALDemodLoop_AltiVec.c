1133f925bedc87bcd774916969cfe8fec3168022
bema <bema>
2006-08-16 23:25:04 +0000

    much better! (a bit of cleanup, too) Original: c3520e8759f59de048696963940eb4584b148d44

--- lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_AltiVec.c
+++ lalapps/src/pulsar/FDS_isolated/CFSLALDemodLoop_AltiVec.c
@@ -34,11 +34,10 @@
 	       precision, such as SSE or AltiVec
  	    */
 
-	    float XsumS[4]  __attribute__ ((aligned (16))); /* for output */
-	    float align[4]  __attribute__ ((aligned (16))); /* for output */
-	    /* vector unsigned char permute_v = vec_lvsl( 0, (float *) Xalpha_k ); */
-	    vector float fdval, reTFreq;
-	    vector float Xsum = {0,0,0,0};
+	    float XsumS[4]  __attribute__ ((aligned (16))); /* aligned output */
+	    vector unsigned char perm; /* = vec_lvsl(0,(float*)Xalpha_k); */
+	    vector float fdval, reTFreq, load1, load2;
+	    vector float Xsum  = {0,0,0,0};
 	    vector float four2 = {2,2,2,2};
 	    vector float four4 = {4,4,4,4};
 	    vector float tFreq = { ((float)(tempFreq0 + klim/2 - 1)),
@@ -53,45 +52,19 @@
 	    REAL8 tFreqD, aFreqD = 1;
 	    REAL8 XsumD[2] = {0,0}; /* vector holding partial sums */
 
-	    /* 
-	       tFreq[0] = tempFreq0 + klim/2 - 1; tFreq[1] = tFreq[0];
-	       tFreq[2] = tempFreq0 + klim/2 - 2; tFreq[3] = tFreq[2];
-
-	       aFreqD = 1.0;
-	       XsumD[0]  = 0.0;  XsumD[1] = 0.0;
-	    */
-
 	    /* Vectorized version of the Kernel loop */
 	    /* This loop has now been unrolled manually */
-
             /* for(k=0; k < klim / 2; k++) { */
 	    {
-	      UINT4 ve; /* this var should be optimized away... */
-
-	      /* single precision vector loop */
+	      /* single precision vector "loop" (isn't actually a loop anymore) */
 #define VEC_LOOP(n)\
-              for(ve=0;ve<4;ve++)\
-	        align[ve]=(Xalpha_kR4+(n))[ve];\
-	      fdval   = vec_ld(0,align);\
+              load1   = vec_ld(0,(Xalpha_kR4+(n)));\
+	      perm    = vec_lvsl(0,(Xalpha_kR4+(n)));\
+              load2   = vec_ld(0,(Xalpha_kR4+(n)+4));\
+	      fdval   = vec_perm(load1,load2,perm);\
 	      reTFreq = vec_re(tFreq);\
 	      tFreq   = vec_sub(tFreq,four2);\
 	      Xsum    = vec_madd(fdval, reTFreq, Xsum);
-
-#if 0
-#define ADD4SSEA(a,b) \
-                  "movlps "#a"(%[Xalpha_kX]),%%xmm4 \n\t"\
-		  "movhps "#b"(%[Xalpha_kX]),%%xmm4 \n\t"\
-		  "rcpps %%xmm0,%%xmm1       \n\t" /* XMM1: 1/(f-1) 1/(f-1) 1/f 1/f */\
-                  "subps %%xmm5,%%xmm0       \n\t" /* XMM0: f-3 f-3 f-2 f-2 */\
-                  "mulps %%xmm4,%%xmm1       \n\t" /* XMM1: ImH/(f-1) ReH/(f-1) ImL/f ReL/f */\
-                  "addps %%xmm1,%%xmm2       \n\t" /* XMM2: C_ImH C_ReH C_ImL C_ReL */
-
-	      mm0 = tFreq;
-	      mm1 = reTF;
-	      mm2 = Xsum;
-	      mm4 = tmp;
-	      mm5 = (2 2 2 2);
-#endif
 
 	      /* non-vectorizing double-precision "loop" */
 #define VEC_LOOP_D(n)\
@@ -100,6 +73,7 @@
 	      aFreqD *= tFreqD;\
               tFreqD -= 1.0;
 
+	      /* seven single-precision calculations first */
 	      VEC_LOOP(00+0); VEC_LOOP(00+4); VEC_LOOP(00+8); VEC_LOOP(00+12); 
 	      VEC_LOOP(16+0); VEC_LOOP(16+4); VEC_LOOP(16+8);
 
@@ -112,12 +86,11 @@
 
 	      tFreqD = tempFreq0 + klim/2 - 15; /* start at the 14th element */
 
-	      /* double precision vectorization */
-	      VEC_LOOP_D(16+12);
-	      VEC_LOOP_D(16+14);
-	      VEC_LOOP_D(32+0);
-	      VEC_LOOP_D(32+2);
+	      /* double precision calculations */
+	      VEC_LOOP_D(16+12); VEC_LOOP_D(16+14);
+	      VEC_LOOP_D(32+0);  VEC_LOOP_D(32+2);
 
+	      /* the rest is done in single precision again */
                               VEC_LOOP(32+4); VEC_LOOP(32+8); VEC_LOOP(32+12); 
 	      VEC_LOOP(48+0); VEC_LOOP(48+4); VEC_LOOP(48+8); VEC_LOOP(48+12); 
 	    }
@@ -125,7 +98,7 @@
 	    /* output the vector */
 	    vec_st(Xsum,0,XsumS);
 
-	    /* conbination of three partial sums: */
+	    /* conbination of the three partial sums: */
 	    combAF  = 1.0 / aFreqD;
 	    XRes = XsumD[0] * combAF + XsumS[0] + XsumS[2];
 	    XIms = XsumD[1] * combAF + XsumS[1] + XsumS[3];
