7e3adb93cfc51a674322eda80e041ae75955e570
channa <channa>
2006-08-23 18:36:41 +0000

    This code will add signal based veto capabilities to the ringdown search - IT IS NOT ready for production use and therefore has not been added to the build.  In order to build this code use the the automake file Makefile.am.veto Original: 1b4c2e66617690397d347dddf7ede82a7da1eb16

---
+++ lalapps/src/ring/ringveto.h
@@ -0,0 +1,131 @@
+#include <lal/LALDatatypes.h>
+#include <lal/LIGOMetadataTables.h>
+#include <lal/RealFFT.h>
+#include <lal/Ring.h>
+
+enum { write_frame, write_ascii };
+
+struct ring_params {
+  char        *programName;
+  char        *cvsRevision;
+  char        *cvsSource;
+  char        *cvsDate;
+  char         ifoName[3];
+  INT4         randomSeed;
+  LIGOTimeGPS  startTime;
+  LIGOTimeGPS  endTime;
+  INT8         trigStartTimeNS;
+  INT8         trigEndTimeNS;
+  LIGOTimeGPS  frameDataStartTime;
+  REAL8        frameDataDuration;
+  REAL8        duration;
+  const char  *channel;
+  const char  *calibCache;
+  const char  *dataCache;
+  const char  *injectFile;
+  REAL8        sampleRate;
+  REAL8        padData;
+  REAL8        segmentDuration;
+  REAL8        strideDuration;
+  REAL8        truncateDuration;
+  UINT4        numOverlapSegments;
+  REAL4        dynRangeFac;
+  REAL4        lowCutoffFrequency;
+  REAL4        highpassFrequency;
+  REAL4        invSpecLen;
+  char         bankFile[256];
+  RingTemplateBankInput bankParams;
+  REAL4        bankMinFrequency;
+  REAL4        bankMaxFrequency;
+  REAL4        bankMinQuality;
+  REAL4        bankMaxQuality;
+  REAL4        bankMaxMismatch;
+  REAL4        bankTemplatePhase;
+  REAL4        threshold;
+  REAL4        maximizeEventDuration;
+  const char  *segmentsToDoList;
+  const char  *templatesToDoList;
+  UINT4        numEvents;
+  char         outputFile[256];
+  int          geoData;
+  REAL8        geoHighpassFrequency;
+  REAL8        geoScale;
+  /* flags */
+  int          strainData;
+  int          simData;
+  int          zeroData;
+  int          whiteSpectrum;
+  int          bankOnly;
+  int          getData;
+  int          getResponse;
+  int          getSpectrum;
+  int          getBank;
+  int          doFilter;
+  /* write intermediate result flags */
+  int          writeRawData;
+  int          writeProcessedData;
+  int          writeResponse;
+  int          writeSpectrum;
+  int          writeInvSpectrum;
+  int          writeSegment;
+  int          writeFilterOutput;
+};
+
+typedef struct tagRingDataSegments
+{
+  UINT4                    numSgmnt;
+  COMPLEX8FrequencySeries *sgmnt;
+}
+RingDataSegments;
+
+/* Ring Veto Structure */
+struct tagRingVetoTemplateBank {
+  RingTemplateBank *bank;
+  struct tagRingVetoTemplateBank *next;
+}; 
+typedef struct tagRingVetoTemplateBank RingVetoTemplateBank; 
+
+/* Ring veto results structure - I need to store ~10 result vectors */
+/* and the template variances in one structure in order to compute */
+/* the cross correlations and construct the chisq bank time series */
+
+struct tagRingVetoResults {
+  REAL4TimeSeries            result;
+  REAL8			     sigma;
+  REAL8			     quality;
+  REAL8			     frequency;			
+  struct tagRingVetoResults  *next;
+};
+typedef struct tagRingVetoResults RingVetoResults;
+
+/* routines in ring_option */
+int ring_parse_options( struct ring_params *params, int argc, char **argv );
+int ring_params_sanity_check( struct ring_params *params );
+
+/* routines in ring_output */
+ProcessParamsTable * create_process_params( int argc, char **argv,
+    const char *program );
+int ring_output_events_xml( 
+    char               *outputFile,
+    SnglRingdownTable  *events,
+    ProcessParamsTable *processParamsTable,
+    struct ring_params *params
+    );
+
+/* routines to write intermediate results in ring_output */
+int write_REAL4TimeSeries( REAL4TimeSeries *series );
+int write_REAL4FrequencySeries( REAL4FrequencySeries *series );
+int write_COMPLEX8FrequencySeries( COMPLEX8FrequencySeries *series );
+int write_bank( RingTemplateBank *bank );
+
+/* routines in ring_filter */
+SnglRingdownTable * ringveto_filter(
+    RingDataSegments         *segments,
+    RingVetoTemplateBank         *bank,
+    REAL4FrequencySeries     *invSpectrum,
+    REAL4FFTPlan             *fwdPlan,
+    REAL4FFTPlan             *revPlan,
+    struct ring_params       *params
+    );
+
+
---
+++ lalapps/src/ring/ringveto_filter.c
@@ -0,0 +1,372 @@
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALStdio.h>
+#include <lal/LALConstants.h>
+#include <lal/LIGOMetadataTables.h>
+#include <lal/AVFactories.h>
+#include <lal/VectorOps.h>
+#include <lal/Units.h>
+#include <lal/TimeFreqFFT.h>
+#include <lal/Ring.h>
+#include <lal/Date.h>
+
+#include "lalapps.h"
+#include "errutil.h"
+#include "gpstime.h"
+#include "ringveto.h"
+
+RCSID( "$Id$" );
+
+static REAL8 compute_template_variance(
+    COMPLEX8FrequencySeries  *stilde,
+    REAL4FrequencySeries     *invspec,
+    REAL8                     dynRangeFac
+    );
+
+static int filter_segment_template(
+    REAL4TimeSeries          *result,
+    COMPLEX8FrequencySeries  *rtilde,
+    COMPLEX8FrequencySeries  *stilde,
+    COMPLEX8FrequencySeries  *segment,
+    REAL4FFTPlan             *plan
+    );
+
+static SnglRingdownTable * find_events(
+    SnglRingdownTable        *events,
+    UINT4                    *numEvents,
+    REAL4TimeSeries          *result,
+    REAL4                     tmpltSigma,
+    REAL4                     tmpltFrequency,
+    REAL4                     tmpltQuality,
+    struct ring_params       *params
+    );
+
+SnglRingdownTable * ringveto_filter(
+    RingDataSegments         *segments,
+    RingVetoTemplateBank     *vetobank,
+    REAL4FrequencySeries     *invSpectrum,
+    REAL4FFTPlan             *fwdPlan,
+    REAL4FFTPlan             *revPlan,
+    struct ring_params       *params
+    )
+{
+  SnglRingdownTable       *events = NULL; /* head of linked list of events */
+  RingTemplateBank        *bank = NULL; /* the smaller banks */
+  RingVetoTemplateBank    *firstvetobank = NULL;
+  REAL4TimeSeries          signal;
+  /*REAL4TimeSeries          result;*/
+  RingVetoResults          *Result = NULL;
+  RingVetoResults          *firstResult = NULL;
+  RingVetoResults          *thisResult = NULL;
+  COMPLEX8FrequencySeries  stilde;
+  COMPLEX8FrequencySeries  rtilde;
+  
+  UINT4 segmentLength;
+  UINT4 sgmnt;
+  UINT4 tmplt;
+
+  if ( ! params->doFilter )
+    return NULL;
+
+  segmentLength = floor( params->segmentDuration * params->sampleRate + 0.5 );
+
+
+  memset( &signal, 0, sizeof( signal ) );
+  /* changed to the structure Result not just the timeseries result */
+  /* memset( Result, 0, sizeof( Result ) );*/ /* not necessary */
+  memset( &stilde, 0, sizeof( stilde ) );
+  memset( &rtilde, 0, sizeof( rtilde ) );
+
+  signal.deltaT = 1.0/params->sampleRate;
+  signal.sampleUnits = lalStrainUnit;
+  rtilde.deltaF = 1.0 / params->segmentDuration;
+  signal.data = XLALCreateREAL4Vector( segmentLength );
+  stilde.data = XLALCreateCOMPLEX8Vector( segmentLength/2 + 1 );
+  rtilde.data = XLALCreateCOMPLEX8Vector( segmentLength/2 + 1 );
+  
+
+  firstvetobank = vetobank; /* just remember the first one */
+  /* loop over all elements in the template bank */
+  UINT4 numEvents = 0;
+  while (vetobank->next){
+    bank = vetobank->bank;
+    for ( sgmnt = 0; sgmnt < segments->numSgmnt; ++sgmnt )
+    {
+      /* set up the Results structure to store all the ~10 templates */
+      Result = (RingVetoResults *) calloc(1, sizeof(RingVetoResults));
+      firstResult = Result; /* remember the first one */
+      for ( tmplt = 0; tmplt < bank->numTmplt; ++tmplt )
+      {
+        /* changed to Result from result - extra layer of pointers... */
+        Result->result.data = XLALCreateREAL4Vector( segmentLength );
+        SnglRingdownTable *thisTmplt = bank->tmplt + tmplt;
+        REAL8 sigma;
+    
+        verbose( "creating template %d\n", tmplt );
+
+        /* make template and fft it MAYBE I COULD SNIP THIS for */
+        /* the cross correlations ?*/
+        XLALComputeRingTemplate( &signal, thisTmplt );
+        LALSnprintf( signal.name, sizeof(signal.name), "TMPLT_%u", tmplt );
+        /*write_REAL4TimeSeries( &signal );*/
+        XLALREAL4TimeFreqFFT( &stilde, &signal, fwdPlan );
+        LALSnprintf( stilde.name, sizeof(stilde.name), "TMPLT_%u_FFT", tmplt );
+        /*    write_COMPLEX8FrequencySeries( &stilde );    */
+
+        /* compute sigma for this template MAKE SURE AND SAVE THIS*/
+        /* to memory so that all the subbanks variances are available */
+        sigma = sqrt( compute_template_variance( &stilde, invSpectrum,
+            params->dynRangeFac ) );
+        Result->sigma = sigma; /* store this value for all these tmplts */
+
+        verbose( "  filtering segment %d against template %d\n", sgmnt, tmplt );
+
+        /* filter the segment with the template */
+        filter_segment_template( &(Result->result), &rtilde, &stilde,
+            &segments->sgmnt[sgmnt], revPlan );
+
+        /* write filter output if requested */
+        if ( params->writeFilterOutput )
+        { /* guess we better normalize it so it is SNR-like... */
+          REAL4 snrFactor = 2 * params->dynRangeFac / sigma;
+          UINT4 k;
+          for ( k = 0; k < Result->result.data->length; ++k )
+            Result->result.data->data[k] *= snrFactor;
+          write_REAL4TimeSeries( &(Result->result) );
+        }
+        /* save some additional template parameters and add a link */
+        Result->quality = thisTmplt->quality;
+        Result->frequency = thisTmplt->frequency;
+        Result = Result->next = 
+                 (RingVetoResults *) calloc(1, sizeof(RingVetoResults));
+
+      } /* end loop over templates */
+      Result->next = NULL; /* terminate the linked list */
+      Result = firstResult;
+      /* Add a for loop here that computes the cross correlation */
+      /* remember to make a time series which is shorter than the */
+      /* 256 second signal - that is way too much time to pad and */
+      /* fft */
+
+      /* search through results for threshold crossings and record events */
+      /* REWRITE this to loop over ALL RESULTS!!! */
+      while(Result->next){
+        events = find_events( events, &numEvents, &Result->result, Result->sigma,
+            Result->frequency, Result->quality, params );
+        params->numEvents += numEvents;
+        /* check the chisq value for each event */
+        /* NEED SOME FUNCTION CALL HERE! */
+        verbose( "found %u event%s in template %u and segment%d\n", numEvents,
+          numEvents == 1 ? "" : "s", tmplt, sgmnt );
+        Result = Result->next;
+      }
+      /* clean up all the results */
+      Result = firstResult;
+      while (Result->next){
+        thisResult = Result;
+        Result = Result->next;
+        XLALDestroyREAL4Vector(thisResult->result.data);
+        free(thisResult);
+      }
+    } /* end loop over segments */
+  vetobank = vetobank->next;
+  } /* end loop over sub banks */
+
+  /* tell the world what we saw */
+  verbose( "found %u event%s\n", params->numEvents,
+      params->numEvents == 1 ? "" : "s" );
+
+  /* cleanup */
+  /*XLALDestroyREAL4Vector( result.data );*/
+  XLALDestroyCOMPLEX8Vector( rtilde.data );
+  XLALDestroyCOMPLEX8Vector( stilde.data );
+  XLALDestroyREAL4Vector( signal.data );
+  vetobank = firstvetobank; /* make sure you send back the head */
+  return events;
+}
+
+static REAL8 compute_template_variance(
+    COMPLEX8FrequencySeries  *stilde,
+    REAL4FrequencySeries     *invspec,
+    REAL8                     dynRangeFac
+    )
+{
+  UINT4 k;
+  REAL8 var;
+
+  var = 0;
+  for ( k = 0; k < stilde->data->length; ++k )
+  {
+    REAL8 re = stilde->data->data[k].re;
+    REAL8 im = stilde->data->data[k].im;
+    var += invspec->data->data[k] * (re*re + im*im);
+  }
+
+  var *= 4.0 * dynRangeFac * dynRangeFac * stilde->deltaF;
+  return var;
+}
+
+static int filter_segment_template(
+    REAL4TimeSeries          *result,
+    COMPLEX8FrequencySeries  *rtilde,
+    COMPLEX8FrequencySeries  *stilde,
+    COMPLEX8FrequencySeries  *segment,
+    REAL4FFTPlan             *plan
+    )
+{
+  char *s;
+
+  /* name of rtilde */
+  LALSnprintf( rtilde->name, sizeof( rtilde->name ), "%s_%s",
+      segment->name, stilde->name );
+  /* name of result is the same but without the _FFT */
+  strncpy( result->name, rtilde->name, sizeof( result->name ) - 1 );
+  /* make sure that the string ends with _FFT */
+  s = result->name + strlen( result->name ) - strlen( "_FFT" );
+  if ( strcmp( s, "_FFT" ) == 0 )
+    *s = 0; /* it does: terminate here */
+
+  /* multiply segment by filter and store in fft of result */
+  XLALCCVectorMultiplyConjugate( rtilde->data, segment->data, stilde->data );
+  XLALUnitMultiply( &rtilde->sampleUnits, &segment->sampleUnits, &stilde->sampleUnits );
+  rtilde->epoch = segment->epoch;
+  /*write_COMPLEX8FrequencySeries( rtilde );*/
+
+  /* inverse fft to obtain result */
+  XLALREAL4FreqTimeFFT( result, rtilde, plan );
+ /*write_REAL4TimeSeries( result );*/
+  result->epoch = rtilde->epoch;
+  return 0;
+}
+
+
+
+/* NOTE: numEvents must be number of events _FROM_CURRENT_TEMPLATE_ so far. */
+/* It must be set to zero when filtering against a new template is started. */
+static SnglRingdownTable * find_events(
+    SnglRingdownTable  *events,
+    UINT4              *numEvents,
+    REAL4TimeSeries    *result,
+    REAL4               tmpltSigma,
+    REAL4               tmpltFrequency,
+    REAL4               tmpltQuality,
+    struct ring_params *params
+    )
+{
+  const REAL4 efolds = 10.0; /* number of efolds of ringdown in template */
+  SnglRingdownTable *thisEvent = events; /* the current event */
+  REAL4 snrFactor; /* factor to convert from filter result to snr */
+  REAL4 threshold; /* modified threshold on filter result (rather than snr) */
+  REAL4 filterDuration;
+  INT8  lastTimeNS;
+  INT8  gapTimeNS;
+  UINT4 segmentStride;
+  UINT4 eventCount = 0;
+  UINT4 jmin;
+  UINT4 jmax;
+  UINT4 j;
+/*  LALStatus             status = blank_status;*/
+ /* LALMSTUnitsAndAcc     gmstUnits = { MST_HRS, LALLEAPSEC_STRICT };*/
+
+  /* compute filter duration: sum of rindown duration and spec trunc duration */
+  filterDuration  = efolds * tmpltQuality / (LAL_PI * tmpltFrequency);
+  filterDuration += params->truncateDuration;
+  
+  /* gap time: filter duration unless explicitly specified */
+  if ( params->maximizeEventDuration < 0 ) /* maximize based on duration*/
+    gapTimeNS = sec_to_ns( filterDuration );
+  else /* maximize with specified duration */
+    gapTimeNS = sec_to_ns( params->maximizeEventDuration );
+
+  /* determine if we are maximizing over current event or not */
+  /* set lastTimeNS to be time of last event or before any possible */
+  /* event in this segment depending on whether there was a previous */
+  /* event from this template */
+  if ( *numEvents ) /* continue maximizing over the most recent event */
+    lastTimeNS = epoch_to_ns( &thisEvent->start_time );
+  else /* set last time to a time before any possible event in this segment */
+    lastTimeNS = epoch_to_ns( &result->epoch ) - gapTimeNS - (INT8)(1000000000);
+
+  /* compute modified threshold on filter output rather than snr */
+  snrFactor = 2 * params->dynRangeFac / tmpltSigma;
+  threshold = params->threshold / snrFactor;
+
+  /* compute start and stop index for scanning time series */
+  segmentStride = floor( params->strideDuration / result->deltaT + 0.5 );
+  jmin = segmentStride/2;
+  jmax = jmin + segmentStride;
+
+  for ( j = jmin; j < jmax; ++j )
+    if ( fabs( result->data->data[j] ) > threshold ) /* threshold crossing */
+    {
+      REAL4 snr;
+      INT8  timeNS;
+      REAL4 sigma;
+      REAL4 amp;
+
+      snr     = fabs( result->data->data[j] ) * snrFactor;
+      timeNS  = epoch_to_ns( &result->epoch );
+      timeNS += sec_to_ns( j * result->deltaT );
+
+      if ( timeNS > lastTimeNS + gapTimeNS ) /* new distinct event */
+      {
+        /* create a new node on the linked list */
+        thisEvent = LALCalloc( 1, sizeof( *thisEvent ) );
+        thisEvent->next = events;
+        events = thisEvent;
+
+        /* copy general information about the filter */
+        strncpy( thisEvent->ifo, params->ifoName, sizeof( thisEvent->ifo ) );
+        strncpy( thisEvent->channel, strchr( params->channel, ':') + 1, 
+            sizeof( thisEvent->channel ) );
+/*       LAL_CALL( LALGPStoGMST1( &status, &(thisEvent->start_time_gmst), 
+                        &(thisEvent->start_time), &gmstUnits ), &status); */ 
+        /*this isnt working properly, will just leave awhile, as it is not
+         * needed */
+
+        
+        thisEvent->frequency = tmpltFrequency;
+        thisEvent->quality = tmpltQuality;
+        thisEvent->mass = ( 1.0 - 0.63 * pow( (2.0 / thisEvent->quality) , (2.0 / 3.0) ) ) *
+            pow( LAL_C_SI, 3.0) / LAL_G_SI / LAL_TWOPI / LAL_MSUN_SI / thisEvent->frequency; 
+        thisEvent->spin = 1.0 - pow( (2.0 / thisEvent->quality) , (20.0 / 9.0) );
+        
+        /* specific information about this threshold crossing */
+        ns_to_epoch( &thisEvent->start_time, timeNS );
+        thisEvent->snr = snr;
+
+        
+        amp = sqrt( 5.0 / 2.0 * 0.01 )  * ( LAL_G_SI * thisEvent->mass * LAL_MSUN_SI / 
+            pow( LAL_C_SI, 2) * 2.0 / LAL_PC_SI /1000000.0 ) * pow( thisEvent->quality, -0.5 ) * 
+            pow( 1.0 + 7.0 / 24.0 / pow( thisEvent->quality, 2.0), -0.5 ) *
+            pow(  1.0 - 0.63 * pow( 1.0 - thisEvent->spin,0.3 ), -0.5);
+        sigma=tmpltSigma * amp;
+        thisEvent->sigma_sq = pow(sigma, 2.0);
+        thisEvent->eff_dist = sigma / thisEvent->snr;
+        thisEvent->amplitude=amp;       
+        ++eventCount;
+      }
+      else if ( snr > thisEvent->snr ) /* maximize within a set of crossings */
+      {
+        /* update to specific information about this threshold crossing */
+        ns_to_epoch( &thisEvent->start_time, timeNS );
+        thisEvent->snr        = snr;
+
+        amp = sqrt( 5.0 / 2.0 * 0.01 )  * ( LAL_G_SI * thisEvent->mass * LAL_MSUN_SI/
+            pow( LAL_C_SI, 2) * 2.0 / LAL_PC_SI /1000000.0 ) * pow( thisEvent->quality, -0.5 ) *
+          pow( 1.0 + 7.0 / 24.0 / pow( thisEvent->quality, 2.0), -0.5 ) *
+          pow(  1.0 - 0.63 * pow( 1.0 - thisEvent->spin,0.3 ), -0.5);
+        sigma=tmpltSigma * amp;
+        thisEvent->eff_dist = sigma / thisEvent->snr;        
+        thisEvent->amplitude = amp;
+      }
+      
+      /* update last threshold crossing time */
+      lastTimeNS = timeNS;
+    }
+
+  *numEvents += eventCount;
+  return events;
+}
