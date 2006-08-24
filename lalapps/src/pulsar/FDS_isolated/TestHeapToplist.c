01b348bc249d6caa4577217d2ff89c206419c059
bema <bema>
2006-08-24 18:15:27 +0000

    oops Original: 1deace9396740a0694d4e555af3cdb023da82403

--- lalapps/src/pulsar/FDS_isolated/TestHeapToplist.c
+++ lalapps/src/pulsar/FDS_isolated/TestHeapToplist.c
@@ -4,7 +4,7 @@
 static int smaller(const void*a,const void*b) { 
   if(*((double*)a) > *((double*)b))
     return(1);
-  else if(*((double*)a) > *((double*)b))
+  else if(*((double*)a) < *((double*)b))
     return(-1);
   else
     return(0);
