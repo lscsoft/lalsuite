13bdde451f2d09f448d52b429af67a28ee484ab9
bema <bema>
2006-08-24 19:50:52 +0000

    bit more effective testing Original: 5a1f774783a2f4d7c6d5e471b666e6b0d17a36f2

--- lalapps/src/pulsar/FDS_isolated/TestHeapToplist.c
+++ lalapps/src/pulsar/FDS_isolated/TestHeapToplist.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "HeapToplist.h"
 
+#define elem_t float
+
 static int smaller(const void*a,const void*b) { 
-  if(*((double*)a) > *((double*)b))
+  if(*((elem_t*)a) < *((elem_t*)b))
     return(1);
-  else if(*((double*)a) < *((double*)b))
+  else if(*((elem_t*)a) > *((elem_t*)b))
     return(-1);
   else
     return(0);
 }
 
 static void print_elem(const void*e){
-  printf(" %d",*((double*)e));
+  printf("%f ",*((elem_t*)e));
 }
 
 main(int argc, char**argv){
-  double elem;
+  elem_t elem;
   int i;
   toplist_t *l;
-  create_toplist(&l,10,sizeof(double),smaller);
+  create_toplist(&l,5,sizeof(elem_t),smaller);
   for(i=0;i<10;i++){
-    elem = i;
+    elem = rand()/(double)RAND_MAX;
     insert_into_toplist(l,&elem);
+    go_through_toplist(l,print_elem); printf("\n");
   }
-  go_through_toplist(l,print_elem);
   free_toplist(&l);
 }
 
