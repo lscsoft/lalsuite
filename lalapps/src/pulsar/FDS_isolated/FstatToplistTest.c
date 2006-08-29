14a7066503ae76ff289fa79e8db7228073ae60f9
bema <bema>
2006-08-29 12:43:32 +0000

    some more testing... Original: 4ec116c64613ea62b5786ce732c38df31a39e64b

--- lalapps/src/pulsar/FDS_isolated/FstatToplistTest.c
+++ lalapps/src/pulsar/FDS_isolated/FstatToplistTest.c
@@ -2,14 +2,43 @@
 #include "HeapToplist.h"
 #include "FstatToplist.h"
 
+static int fstat_smaller(const void*a, const void*b) {
+  if     (((FstatOutputEntry*)a)->Fstat < ((FstatOutputEntry*)b)->Fstat)
+    return(1);
+  else if(((FstatOutputEntry*)a)->Fstat > ((FstatOutputEntry*)b)->Fstat)
+    return(-1);
+  else if(((FstatOutputEntry*)a)->Freq  < ((FstatOutputEntry*)b)->Freq)
+    return(1);
+  else if(((FstatOutputEntry*)a)->Freq  > ((FstatOutputEntry*)b)->Freq)
+    return(-1);
+  else
+    return(0);
+}
+
+static void print_fstat(const void*e){
+  fprintf(stderr,"%e %e %e %e %e\n",
+	  ((FstatOutputEntry*)e)->Freq,
+	  ((FstatOutputEntry*)e)->f1dot,
+	  ((FstatOutputEntry*)e)->Alpha,
+	  ((FstatOutputEntry*)e)->Delta,
+	  ((FstatOutputEntry*)e)->Fstat);
+}
+
 /* short test for the basic functions of this lib. */
-int test_fstat_toplist(UINT8 n, char*filename) {
+int test_fstat_toplist(UINT8 n, UINT8 m, char*filename) {
     REAL8 epsilon=1e-5;
     FILE*fp;
     toplist_t *tl=NULL, *tl2=NULL;
     FstatOutputEntry FstatLine;
+    FstatOutputEntry *ll, *llt;
     UINT8 i,ins=0;
     UINT4 checksum;
+
+    ll=(FstatOutputEntry*)malloc(m*sizeof(FstatOutputEntry));
+    if(ll == NULL) {
+	LALPrintError("Couldn't create list ll\n");
+	return(-1);
+    }
 
     fprintf(stderr,"creating first toplist...\n");
     if(create_fstat_toplist(&tl,n)) {
@@ -35,13 +64,15 @@ int test_fstat_toplist(UINT8 n, char*filename) {
     checksum = 0;
 
     fprintf(stderr,"filling first toplist...\n");
-    for(i=0;i<n*2;i++) {
+    for(i=0;i<m;i++) {
 	FstatLine.Freq = (double)rand() / (double)RAND_MAX;
 	FstatLine.f1dot = (double)rand() / (double)RAND_MAX;
 	FstatLine.Alpha = (double)rand() / (double)RAND_MAX;
 	FstatLine.Delta = (double)rand() / (double)RAND_MAX;
 	FstatLine.Fstat = (double)rand() / (double)RAND_MAX;
 
+	ll[i]= FstatLine;
+	
 	if(insert_into_fstat_toplist(tl, FstatLine)) {
 	    ins++;
 	    write_fstat_toplist_item_to_fp(FstatLine,fp,&checksum);
@@ -50,7 +81,7 @@ int test_fstat_toplist(UINT8 n, char*filename) {
 	if(i==n)
 	    fprintf(stderr,"array filled now\n");
     }
-    fprintf(stderr,"%d inserts actually done of %d\n",(int)ins,2*(int)n);
+    fprintf(stderr,"%d inserts actually done of %d\n",(int)ins,m);
 
     fclose(fp);
 
@@ -71,6 +102,52 @@ int test_fstat_toplist(UINT8 n, char*filename) {
     fclose(fp);
 
     fprintf(stderr,"checksum: %d\n",checksum);
+
+    /*
+      for(i=0;i<m;i++)
+      print_fstat((void*)&(ll[i]));
+    */
+
+    fprintf(stderr,"sort the reference list and get the top n elements\n");
+    qsort_toplist(tl,fstat_smaller);
+    qsort(ll,m,sizeof(FstatOutputEntry),fstat_smaller);
+
+    /*
+      for(i=0;i<m;i++)
+      print_fstat((void*)&(ll[i]));
+      fprintf(stderr,"...\n");
+      go_through_toplist(tl,print_fstat);
+    */
+
+    fprintf(stderr,"comparing...\n");
+    for(i=0;i<n;i++)
+      if((((FstatOutputEntry*)toplist_elem(tl,i))->Freq  -
+	  ll[i].Freq > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->f1dot -
+	  ll[i].f1dot > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Alpha -
+	  ll[i].Alpha > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Delta -
+	  ll[i].Delta > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Fstat -
+	  ll[i].Fstat > epsilon)) {
+	
+	LALPrintError("line %d differs\n",i);
+	    fprintf(stderr,"%e %e %e %e %e\n",
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Freq,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->f1dot,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Alpha,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Delta,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Fstat);
+	    fprintf(stderr,"%e %e %e %e %e\n",
+		    ll[i].Freq,
+		    ll[i].f1dot,
+		    ll[i].Alpha,
+		    ll[i].Delta,
+		    ll[i].Fstat);
+	}
+
+
 
     sort_fstat_toplist(tl);
 
@@ -100,30 +177,30 @@ int test_fstat_toplist(UINT8 n, char*filename) {
 
     fprintf(stderr,"comparing...\n");
     for(i=0;i<n;i++)
-      if((((FstatOutputEntry*)(tl->heap[i]))->Freq  -
-	  ((FstatOutputEntry*)(tl2->heap[i]))->Freq > epsilon) ||
-	 (((FstatOutputEntry*)(tl->heap[i]))->f1dot -
-	  ((FstatOutputEntry*)(tl2->heap[i]))->f1dot > epsilon) ||
-	 (((FstatOutputEntry*)(tl->heap[i]))->Alpha -
-	  ((FstatOutputEntry*)(tl2->heap[i]))->Alpha > epsilon) ||
-	 (((FstatOutputEntry*)(tl->heap[i]))->Delta -
-	  ((FstatOutputEntry*)(tl2->heap[i]))->Delta > epsilon) ||
-	 (((FstatOutputEntry*)(tl->heap[i]))->Fstat -
-	  ((FstatOutputEntry*)(tl2->heap[i]))->Fstat > epsilon)) {
+      if((((FstatOutputEntry*)toplist_elem(tl,i))->Freq  -
+	  ((FstatOutputEntry*)toplist_elem(tl2,i))->Freq > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->f1dot -
+	  ((FstatOutputEntry*)toplist_elem(tl2,i))->f1dot > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Alpha -
+	  ((FstatOutputEntry*)toplist_elem(tl2,i))->Alpha > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Delta -
+	  ((FstatOutputEntry*)toplist_elem(tl2,i))->Delta > epsilon) ||
+	 (((FstatOutputEntry*)toplist_elem(tl,i))->Fstat -
+	  ((FstatOutputEntry*)toplist_elem(tl2,i))->Fstat > epsilon)) {
 	
 	LALPrintError("line %d differs\n",i);
 	    fprintf(stderr,"%e %e %e %e %e\n",
-		    ((FstatOutputEntry*)(tl->heap[i]))->Freq,
-		    ((FstatOutputEntry*)(tl->heap[i]))->f1dot,
-		    ((FstatOutputEntry*)(tl->heap[i]))->Alpha,
-		    ((FstatOutputEntry*)(tl->heap[i]))->Delta,
-		    ((FstatOutputEntry*)(tl->heap[i]))->Fstat);
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Freq,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->f1dot,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Alpha,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Delta,
+		    ((FstatOutputEntry*)toplist_elem(tl,i))->Fstat);
 	    fprintf(stderr,"%e %e %e %e %e\n",
-		    ((FstatOutputEntry*)(tl2->heap[i]))->Freq,
-		    ((FstatOutputEntry*)(tl2->heap[i]))->f1dot,
-		    ((FstatOutputEntry*)(tl2->heap[i]))->Alpha,
-		    ((FstatOutputEntry*)(tl2->heap[i]))->Delta,
-		    ((FstatOutputEntry*)(tl2->heap[i]))->Fstat);
+		    ((FstatOutputEntry*)toplist_elem(tl2,i))->Freq,
+		    ((FstatOutputEntry*)toplist_elem(tl2,i))->f1dot,
+		    ((FstatOutputEntry*)toplist_elem(tl2,i))->Alpha,
+		    ((FstatOutputEntry*)toplist_elem(tl2,i))->Delta,
+		    ((FstatOutputEntry*)toplist_elem(tl2,i))->Fstat);
 	}
 
     fprintf(stderr,"cleanup...\n");
@@ -136,9 +213,14 @@ int test_fstat_toplist(UINT8 n, char*filename) {
 int main(int argc,char**argv) {
     char*name="Toplist.test";
     int n=10000;
+    int m=0;
     if(argc>1)
-	n=atoi(argv[1]);
+      n=atoi(argv[1]);
     if(argc>2)
-	name=argv[2];
-    return(test_fstat_toplist(n,name));
+      m=atoi(argv[2]);
+    if(argc>3)
+      name=argv[3];
+    if(m<n)
+      m=2*n;
+    return(test_fstat_toplist(n,m,name));
 }
