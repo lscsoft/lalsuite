221287a430917281d5ac86361e631bf07c8741a2
siemens <siemens>
2006-09-20 18:08:40 +0000

    latest Original: 8c09b1e9764f57ba88f63f61c78de7dfe6c318ba

--- lalapps/src/string/contour_plotter.m
+++ lalapps/src/string/contour_plotter.m
@@ -1,42 +1,86 @@
 clear
-load gamma.dat
+load gammaInitialLIGO.dat
+
+gamma=gammaInitialLIGO;
 
 T=365*24*3600;
+%T=1363412.0;
+
+numberofevents=-log(1-0.90);
+%numberofevents=1.0000001
 
 epsi=gamma(:,3);
 Gmu=gamma(:,4);
-rate=gamma(:,5);
-
-figure;loglog(10^-8,10^-8); hold on
-p=logspace(-4,0, 40);
-
-for i=1:size(gamma,1)
- 
-     if(gamma(i,5)*T > -log(1-.9))
-        plot(gamma(i,4),gamma(i,3), 'ro')
-     else
-        plot(gamma(i,4),gamma(i,3), 'go')        
-    end
- 
-end
 
 Gmu2 = unique(gamma(:,4))';
 eps2 = unique(gamma(:,3));
-Gmumat2 = kron(Gmu2,ones(size(eps2)));
-epsmat2 = kron(eps2,ones(size(Gmu2)));
+
 rate = zeros(length(eps2),length(Gmu2));
+
+p=1;
 for kk = 1:length(Gmu2)
   for jj = 1:length(eps2)
     cut = gamma(:,4) == Gmu2(kk) & gamma(:,3) == eps2(jj);
     if sum(cut) == 1
-      rate(jj,kk) = gamma(cut,5);
+      rate(jj,kk) = gamma(cut,5)/p;
     else
       disp('PROBLEM')
     end
   end
 end
+figure(1);loglog(10^-12,1); hold on; loglog(10^-12,10^-12); loglog(10^-6,10^-12);loglog(10^-6,1);grid on;
+xlabel('G\mu','FontSize',14)
+ylabel('\epsilon','FontSize',14)
+title('p=1','FontSize',14)
+contour(Gmu2,eps2,rate*T, [numberofevents],'r*');
 
-contour(Gmumat2,epsmat2,rate*T, [-log(1-0.997) -log(1-0.95)  -log(1-0.68)],'r*');
+p=1e-1;
+for kk = 1:length(Gmu2)
+  for jj = 1:length(eps2)
+    cut = gamma(:,4) == Gmu2(kk) & gamma(:,3) == eps2(jj);
+    if sum(cut) == 1
+      rate(jj,kk) = gamma(cut,5)/p;
+    else
+      disp('PROBLEM')
+    end
+  end
+end
+figure(2);loglog(10^-12,1); hold on; loglog(10^-12,10^-12); loglog(10^-6,10^-12);loglog(10^-6,1);grid on;
+xlabel('G\mu','FontSize',14)
+ylabel('\epsilon','FontSize',14)
+title('p=10^{-1}','FontSize',14)
+contour(Gmu2,eps2,rate*T, [numberofevents],'r*');
 
+p=1e-2;
+for kk = 1:length(Gmu2)
+  for jj = 1:length(eps2)
+    cut = gamma(:,4) == Gmu2(kk) & gamma(:,3) == eps2(jj);
+    if sum(cut) == 1
+      rate(jj,kk) = gamma(cut,5)/p;
+    else
+      disp('PROBLEM')
+    end
+  end
+end
+figure(3);loglog(10^-12,1); hold on; loglog(10^-12,10^-12); loglog(10^-6,10^-12);loglog(10^-6,1);grid on;
+xlabel('G\mu','FontSize',14)
+ylabel('\epsilon','FontSize',14)
+title('p=10^{-2}','FontSize',14)
+contour(Gmu2,eps2,rate*T, [numberofevents],'r*');
 
-
+p=1e-3;
+for kk = 1:length(Gmu2)
+  for jj = 1:length(eps2)
+    cut = gamma(:,4) == Gmu2(kk) & gamma(:,3) == eps2(jj);
+    if sum(cut) == 1
+      rate(jj,kk) = gamma(cut,5)/p;
+    else
+      disp('PROBLEM')
+    end
+  end
+end
+figure(4);loglog(10^-12,1); hold on; loglog(10^-12,10^-12); loglog(10^-6,10^-12);loglog(10^-6,1);grid on;
+xlabel('G\mu','FontSize',14)
+ylabel('\epsilon','FontSize',14)
+title('p=10^{-3}','FontSize',14)
+contour(Gmu2,eps2,rate*T, [numberofevents],'r*');
