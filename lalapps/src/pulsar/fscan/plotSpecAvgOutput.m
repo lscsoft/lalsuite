3119d1320c7f18de4633ce1e15ad33386499df1e
gmendell <gmendell>
2006-10-10 23:53:51 +0000

    Add option to also generate StackSlide type plots and txt output. Original: f547765ae4780de2fc98af1c6be29c95a41a1ef3

--- lalapps/src/pulsar/fscan/plotSpecAvgOutput.m
+++ lalapps/src/pulsar/fscan/plotSpecAvgOutput.m
@@ -1,6 +1,20 @@
-function plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks)
-% usage: plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks)
-%
+function plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks,medBins)
+% usage: plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks,medBins)
+% 
+% inputFileName  -- the name of the file with data to input; this file is the output from spec_avg.
+% outputFileName -- the name of the output plot to generate (the extension determines the type, e.g., .pdf)
+% chanName       -- the name of the channel used to generate the spectrogram data
+% tStart         -- the start time of the data
+% tEnd           -- the end time of the data
+% fStart         -- the start frequency of the data
+% fEnd           -- the end frequency of the data
+% effTBase       -- the effective time base line: 1/effTBase gives the frequency resolution of the plot
+%                   so that a effTBase of 10 seconds means deltaF = 0.1 Hz.
+% deltaFTicks    -- the change in frequency between major tick marks (e.g., 5 Hz)
+% medBins        -- if > 0 then this many frequency bins is used to find a running median normalized averaged
+%                   StackSlide style output spectrum. The spectrograms is given as subplot(2,1,1) and this
+%                   spectrum as subplot(2,1,2).  Also, .txt is appended to the plot name, and f versus the
+%                   normalized averaged power is output into this text file.
 
 % Convert relevant strings to numbers.
 if (ischar(tStart))
@@ -21,12 +35,18 @@ end
 if (ischar(deltaFTicks))
     deltaFTicks=str2num(deltaFTicks);
 end
+if (ischar(medBins))
+    medBins=str2num(medBins);
+end
 
 xIn = load(inputFileName);
 
 x = transpose(log10(xIn));
 
 figure(1);
+if (medBins > 0)
+   subplot(2,1,1)
+end
 imagesc(flipud(x));
 % Show ticks every deltaFTicks Hz:
 deltaTicks = deltaFTicks*effTBase;
@@ -43,4 +63,45 @@ title(titleString);
 xlabel('SFT number (see table for corresponding date)');
 ylabel('Frequency (Hz)');
 colorbar;
+
+if (medBins > 0)
+  subplot(2,1,2)
+  % Produce StackSlide style output:
+  % normalize each row using a median value every 10 bins:
+  jMax = length(xIn(:,1));
+  kMax = length(xIn(1,:));
+  medBinso2 = floor(medBins/2);
+  kMaxmmedBinso2 = kMax - medBinso2;
+  kMaxmmedBins = kMax - medBins;
+  for j = 1:jMax
+    for k = 1:kMax
+        if ( (k > medBinso2) & (k < kMaxmmedBinso2) )
+           xss(j,k) = xIn(j,k)/ median( xIn(j,k - medBinso2:k + medBinso2) );
+        elseif ( (k <= medBinso2) & (k < kMaxmmedBins) )
+           xss(j,k) = xIn(j,k)/ median( xIn(j,k:k + medBins) );
+        elseif ( (k > medBins) & (k >= kMaxmmedBinso2) )
+           xss(j,k) = xIn(j,k)/ median( xIn(j,k - medBins:k) );
+        else
+           xss(j,k) = xIn(j,k)/ median( xIn(j,:) );
+        end
+    end
+  end
+  for k = 1:kMax
+    xout(k) = sum(xss(:,k))/kMax;
+  end
+  fk = fStart + (0:kMax-1)/effTBase;
+  semilogy(fk,xout)
+  %set(gca, 'XTick', vecTicks);
+  %set(gca, 'XTickLabel', vecFLabels);
+  titleString = sprintf('Spectrum for %s; averaged over GPS %d - %d s.',chanName,tStart,tEnd);
+  title(titleString);
+  ylabel('Normalized Average Power');
+  xlabel('Frequency (Hz)');
+  outputTextFile = sprintf('%s.txt',outputFileName);
+  fid = fopen(outputTextFile,'w');  
+  for k = 1:kMax
+      fprintf(fid,'%f %f\n',fk(k),xout(k));
+  end
+  fclose(fid);
+end
 saveas(1, outputFileName);
--- lalapps/src/pulsar/fscan/runPlotSpecAvgOutput.csh
+++ lalapps/src/pulsar/fscan/runPlotSpecAvgOutput.csh
@@ -1,9 +1,10 @@
 #!/bin/tcsh
 #
-#
-# run plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks)
+# run plotSpecAvgOutput(inputFileName,outputFileName,chanName,tStart,tEnd,fStart,fEnd,effTBase,deltaFTicks,medBins)
+# The path to plotSpecAvgOutput must be in the MATLABPATH  env variable.
+# See plotSpecAvgOutput.m for meaning of command line arguments.
 $MATLAB_ROOT/bin/matlab -nodisplay -nodesktop -nojvm << EOF
-plotSpecAvgOutput('$1','$2','$3','$4','$5','$6','$7','$8','$9')
+plotSpecAvgOutput('$1','$2','$3','$4','$5','$6','$7','$8','$9','$10')
 exit
 EOF
 
