4ba04e37b78bf0305c0e9438f5abbc1e4f14ff2a
reinhard <reinhard>
2006-10-19 19:47:48 +0000

    check template-grids of CFSv2 against CFSv1 [using 30% tolerance on F-values, which happens for points with F<<1 ] Original: d28a4645be52dc49cc2524b7ca184c93f75f2a60

---
+++ lalapps/src/pulsar/FDS_isolated/testGridv2.sh
@@ -0,0 +1,164 @@
+#!/bin/sh
+
+## take user-arguments for CFS-v2:
+extra_args="$@"
+
+##---------- names of codes and input/output files  
+mfd_code="lalapps_Makefakedata"
+cfs_code="lalapps_ComputeFStatistic"
+cfsv2_code="ComputeFStatistic_v2"
+cmp_code="compareFstats"
+
+SFTdir="./testSFTs"
+
+# test if LAL_DATA_PATH has been set ... needed to locate ephemeris-files
+if [ -z "$LAL_DATA_PATH" ]; then
+    echo
+    echo "Need environment-variable LAL_DATA_PATH to point "
+    echo "to your ephemeris-directory (e.g. /usr/local/share/lal)"
+    if [ -n "$LAL_PREFIX" ]; then
+	echo "You have LAL_PREFIX set, I suggest setting 'LAL_DATA_PATH=\$LAL_PREFIX/share/lal'"
+    fi
+    echo
+    exit 1
+fi
+
+# ---------- fixed parameter of our test-signal
+Tsft=1800;
+startTime=711595934
+refTime=701595833  ## $startTime
+duration=144000		## 40 hours
+
+Alpha=2.0
+AlphaBand=0.1
+dAlpha=0.04
+Delta=-0.5
+DeltaBand=0.1
+dDelta=0.04
+
+h0=1
+cosi=-0.3
+
+psi=0.6
+phi0=1.5
+
+Freq=100.12345
+FreqBand=0.1
+dFreq=0.04
+f1dot=-1e-13
+f1dotBand=1e-14
+df1dot=4e-15
+
+mfd_FreqBand=4.0;
+mfd_fmin=$(echo $Freq $mfd_FreqBand | awk '{printf "%g", $1 - $2 / 2.0}');
+
+echo "mfd_fmin = $mfd_fmin"
+
+noiseSqrtSh=5
+
+## ------------------------------------------------------------
+
+if [ "$noiseSqrtSh" != 0 ]; then
+    sqrtSh=$noiseSqrtSh
+    haveNoise=true;
+else
+    sqrtSh=1;	## for SemiAnalyticF signal-only case
+    haveNoise=false;
+fi
+
+IFO=LLO
+
+##--------------------------------------------------
+## test starts here
+##--------------------------------------------------
+
+echo
+echo "----------------------------------------------------------------------"
+echo " STEP 1: Generate Fake Signal"
+echo "----------------------------------------------------------------------"
+echo
+if [ ! -d "$SFTdir" ]; then
+    mkdir $SFTdir;
+else
+    rm -f $SFTdir/*;
+fi
+
+# this part of the command-line is compatible with SemiAnalyticF:
+saf_CL=" --Alpha=$Alpha --Delta=$Delta --IFO=$IFO --Tsft=$Tsft --startTime=$startTime --duration=$duration --h0=$h0 --cosi=$cosi --psi=$psi --phi0=$phi0"
+# concatenate this with the mfd-specific switches:
+mfd_CL="${saf_CL} --fmin=$mfd_fmin --Band=$mfd_FreqBand --Freq=$Freq --outSFTbname=$SFTdir/testSFT --f1dot=$f1dot --refTime=$refTime --outSFTv1"
+if [ "$haveNoise" = true ]; then
+    mfd_CL="$mfd_CL --noiseSqrtSh=$sqrtSh";
+fi
+
+cmdline="$mfd_code $mfd_CL";
+echo $cmdline;
+if ! eval $cmdline; then
+    echo "Error.. something failed when running '$mfd_code' ..."
+    exit 1
+fi
+
+echo 
+##echo -n "Running '$saf_code' ... "
+##cmdline="$saf_code $saf_CL --sqrtSh=$sqrtSh"	
+##echo $cmdline
+##if ! resF=`eval $cmdline 2> /dev/null`; then
+##    echo "Error ... something failed running '$saf_code' ..."
+##    exit 1;
+##fi
+##echo  "ok."
+##res2F=`echo $resF | awk '{printf "%g", 2.0 * $1}'`
+##echo "The SemiAnalyticF calculations predicts: 2F = $res2F"
+  
+echo 
+echo "----------------------------------------------------------------------"
+echo "STEP 2: run CFS_v1 "
+echo "----------------------------------------------------------------------"
+echo
+
+## common cmdline-options for v1 and v2    
+cfs_CL="--IFO=$IFO --Freq=$Freq --FreqBand=$FreqBand --dFreq=$dFreq --Alpha=$Alpha --AlphaBand=$AlphaBand --dAlpha=$dAlpha --Delta=$Delta --DeltaBand=$DeltaBand --dDelta=$dDelta --f1dot=$f1dot --f1dotBand=$f1dotBand --df1dot=$df1dot --DataFiles='$SFTdir/testSFT*' --refTime=$refTime"
+if [ "$haveNoise" = false ]; then
+    cfs_CL="$cfs_CL --SignalOnly"
+fi
+    
+cmdline="$cfs_code $cfs_CL  --outputFstat=Fstat_v1.dat --expLALDemod=1 --Fthreshold=0";
+echo $cmdline;
+
+if ! eval time $cmdline; then
+    echo "Error.. something failed when running '$cfs_code' ..."
+    exit 1
+fi
+
+echo    
+echo "----------------------------------------------------------------------"
+echo " STEP 3: run CFS_v2 "
+echo "----------------------------------------------------------------------"
+echo
+cmdline="$cfsv2_code $cfs_CL --outputFstat=Fstat_v2.dat --TwoFthreshold=0 $extra_args";
+cmdline="$cmdline --outputLoudest=loudest.dat";
+echo $cmdline;
+
+if ! eval time $cmdline; then
+    echo "Error.. something failed when running '$cfs_code' ..."
+    exit 1;
+fi
+
+
+echo
+echo "----------------------------------------"
+echo " STEP 4: Comparing results: "
+echo "----------------------------------------"
+echo
+
+echo
+cmdline="$cmp_code -1 ./Fstat_v1.dat -2 ./Fstat_v2.dat --clusterFiles=0 --Ftolerance=0.3"
+echo $cmdline
+if ! eval $cmdline; then
+    echo "OUCH... files differ. Something might be wrong..."
+    exit 2
+else
+    echo "OK."
+fi
+
+echo
