c6adf71870f0190a53f767eb335a6b75c65c0163
reinhard <reinhard>
2006-10-27 15:24:39 +0000

    just defining empty initializers for structs defined in PulsarDataTypes.h Original: 56b36348977d0b8e35c2b14b3badbc883919e873

---
+++ lal/packages/pulsar/src/PulsarDataTypes.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2006 Reinhard Prix
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the 
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA  02111-1307  USA
+ */
+
+/** \author R. Prix
+ * \file 
+ * \brief
+ * Provides empty initializers for pulsar struct data-types. 
+ *
+ */
+
+/*---------- INCLUDES ----------*/
+#include "PulsarDataTypes.h"
+#include "ComputeFstat.h"
+
+
+NRCSID( PULSARDATATYPESC, "$Id$");
+
+/*---------- local DEFINES ----------*/
+
+/*----- Macros ----- */
+
+/*----- SWITCHES -----*/
+
+/*---------- internal types ----------*/
+
+/*---------- Global variables ----------*/
+
+/* empty struct initializers */
+const BinaryOrbitParams empty_BinaryOrbitParams;
+const PulsarAmplitudeParams empty_PulsarAmplitudeParams;
+const PulsarSpinRange empty_PulsarSpinRange;
+const PulsarDopplerParams empty_PulsarDopplerParams;
+const PulsarParams empty_PulsarParams;
+const PulsarCandidate empty_PulsarCandidate;
+
+/* initializers for frequently used non-pulsar types */
+const LIGOTimeGPS empty_LIGOTimeGPS;
+const BarycenterInput empty_BarycenterInput;
+const EphemerisData empty_EphemerisData;
+const SkyPosition empty_SkyPosition;
+
+/*---------- internal prototypes ----------*/
+
+/*==================== FUNCTION DEFINITIONS ====================*/
