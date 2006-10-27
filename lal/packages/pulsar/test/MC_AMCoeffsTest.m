18c1d8328ce8616987bcb2f4e9f4e88d17fb7dd9
reinhard <reinhard>
2006-10-27 10:22:50 +0000

    use test-wrapper to make sure we use right LAL-version Original: fee978a4868569b9a344ee35dc7b53b3e150681c

--- lal/packages/pulsar/test/MC_AMCoeffsTest.m
+++ lal/packages/pulsar/test/MC_AMCoeffsTest.m
@@ -5,7 +5,7 @@
 
 numTrials = 2000;
 
-cmd = "./.libs/NewGetAMCoeffsTest >> amCoeffsTest.dat";
+cmd = "./NewGetAMCoeffsTest >> amCoeffsTest.dat";
 
 printf ("Running %d trials: ", numTrials );
 for i=1:numTrials
@@ -27,32 +27,32 @@ hold off;
 clg;
 ## avgerr01
 hist ( res(:,4), 100 );		
-title ("Average error 0 - 1");
+title ("Average error LALComputeAM() - LALGetAMCoeffs()");
 plot2eps ( "avgerr_01.eps");
 
 ## avgerr02
 hist ( res(:,5), 100 );		
-title ("Average error 0 - 2");
+title ("Average error LALComputeAM() -  LALNewGetAMCoeffs()");
 plot2eps ( "avgerr_02.eps");
 
 ## avgerr12
 hist ( res(:,6), 100 );		
-title ("Average error 1 - 2");
+title ("Average error LALGetAMCoeffs() -  LALNewGetAMCoeffs()");
 plot2eps ( "avgerr_12.eps");
 
 ## maxerr01
 hist ( res(:,7), 100 );		
-title ("Maximal error 0 - 1");
+title ("Maximal error LALComputeAM() - LALGetAMCoeffs()");
 plot2eps ( "maxerr_01.eps");
 
 ## maxerr02
 hist ( res(:,8), 100 );		
-title ("Maximal error 0 - 2");
+title ("Maximal error LALComputeAM() -  LALNewGetAMCoeffs()");
 plot2eps ( "maxerr_02.eps" );
 
 ## maxerr12
 hist ( res(:,9), 100 );		
-title ("Maximal error 1 - 2");
+title ("Maximal error LALGetAMCoeffs() -  LALNewGetAMCoeffs()");
 plot2eps ( "maxerr_12.eps" );
 
 
