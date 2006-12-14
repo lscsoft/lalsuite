c338f2b05b822c2ed3530b2a08d28700f5c4b08e
bema <bema>
2006-12-14 13:50:11 +0000

    for compiling as C++ Original: 3d7e11a14312816eead136092d33af2a10cd8b27

--- lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
+++ lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
@@ -3,6 +3,11 @@
    $Id$
 */
 
+/* BOINC includes */
+#include <sys/types.h>
+#include <unistd.h>
+#include "filesys.h"
+
 /* linking proper functions to the hooks in HierarchicalSearch.c */
 #define SET_CHECKPOINT(filename,rac,dec,tpl_count,tpl_total)\
         set_checkpoint(filename,rac,dec,tpl_count,tpl_total);
@@ -10,6 +15,12 @@
         get_checkpoint(filename);
 #define SHOW_PROGRESS(rac,dec,tpl_count,tpl_total)\
         show_progress(rac,dec,tpl_count,tpl_total);
+
+#define fopen boinc_fopen
+
+#ifdef  __cplusplus
+extern "C"{
+#endif
 
 /* function prototypes, they are defined in boinc_extras.c */
 extern void set_checkpoint(char*filename,double rac,double dec, long tpl_count, long tpl_total);
@@ -19,8 +30,9 @@ extern void show_progress(double rac, double dec, long tpl_count, long tpl_total
 /* the main() function of HierarchicalSerach.c becomes the main_hierarchical_search()
    the real main() function of the BOINC App is defined in boinc_extras.c
 */
-#define MAIN main_hierarchical_search
+extern int MAIN(int,char**);
 
-#define fopen boinc_fopen
-
+#ifdef  __cplusplus
+}
+#endif
 
