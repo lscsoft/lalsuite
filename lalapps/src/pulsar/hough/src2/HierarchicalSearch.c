ba79cb5cfade739fbe192992d05c28ba320007f8
bema <bema>
2007-01-07 20:30:07 +0000

    Oops! Original: 130b43ef1b7cbbec91589075ca2db0a3478fb543

--- lalapps/src/pulsar/hough/src2/HierarchicalSearch.c
+++ lalapps/src/pulsar/hough/src2/HierarchicalSearch.c
@@ -1401,7 +1401,7 @@ int MAIN( int argc, char *argv[]) {
   if ( uvar_printCand1 )
     {
       LALFree(fnameSemiCohCand);
-      if (fprintf(fpSemiCoh,"%DONE\n") < 0)
+      if (fprintf(fpSemiCoh,"\%DONE\n") < 0)
 	fprintf(stderr, "Error writing end marker\n");
       fclose(fpSemiCoh);
     }
