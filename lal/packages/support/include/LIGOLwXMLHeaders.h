9f27d7175309dbc68280e4b04966b0260d1558e2
kipp <kipp>
2007-02-09 04:16:47 +0000

    change %e formats to %g in sngl_burst table Original: dda079928d16abf49225b354203fe657f7588db6

--- lal/packages/support/include/LIGOLwXMLHeaders.h
+++ lal/packages/support/include/LIGOLwXMLHeaders.h
@@ -177,7 +177,7 @@ fputs( "      <Column Name=\"sngl_burstgroup:sngl_burst:event_id\" Type=\"ilwd:c
 fputs( "      <Stream Name=\"sngl_burstgroup:sngl_burst:table\" Type=\"Local\" Delimiter=\",\">\n", fp ) == EOF )
 
 #define SNGL_BURST_ROW \
-"         \"process:process_id:0\",\"%s\",\"%s\",\"%s\",%d,%d,%d,%d,%e,%e,%e,%e,%e,%e,%e,\"sngl_burst:event_id:%u\""
+"         \"process:process_id:0\",\"%s\",\"%s\",\"%s\",%d,%d,%d,%d,%g,%g,%g,%g,%g,%g,%g,\"sngl_burst:event_id:%u\""
 
 #define PRINT_LIGOLW_XML_SNGL_RINGDOWN(fp) ( \
 fputs( "   <Table Name=\"sngl_ringdowngroup:sngl_ringdown:table\">\n", fp ) == EOF || \
