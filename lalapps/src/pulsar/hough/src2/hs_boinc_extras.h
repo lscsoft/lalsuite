1b0a80f5e9807ef0794ef0d3a6fe93ff274f944f
bema <bema>
2007-02-19 11:51:11 +0000

    - make checkpointing the default now - removed unused code Original: 04f07fcaaf260009407211041d2ca90fb8c07206

--- lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
+++ lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
@@ -20,19 +20,13 @@ NRCSID(HSBOINCEXTRASHRCSID,"$Id$");
 #define fopen boinc_fopen
 
 #ifndef HS_CHECKPOINTING
-#define HS_CHECKPOINTING 0
+#define HS_CHECKPOINTING 1
 #endif
 
 #if (HS_CHECKPOINTING)
 #define GET_CHECKPOINT init_and_read_checkpoint
-
-#if 0
-#define SET_CHECKPOINT
-#define INSERT_INTO_FSTAT_TOPLIST add_candidate_and_checkpoint
-#else
 #define SET_CHECKPOINT set_checkpoint()
 #define INSERT_INTO_FSTAT_TOPLIST add_checkpoint_candidate
-#endif
 
 #else
 #define SET_CHECKPOINT
