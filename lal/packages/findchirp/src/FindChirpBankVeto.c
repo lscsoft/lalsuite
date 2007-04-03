3723cbf2aee8d547bcdf08f5da906c222a4a21dc
duncan <duncan>
2007-04-03 20:31:54 +0000

    initial entry from version 1.1.2.7 Original: 69ff75771977e3a242b24de072db6953bcab856b

---
+++ lal/packages/findchirp/src/FindChirpBankVeto.c
@@ -0,0 +1,291 @@
+/*----------------------------------------------------------------------- 
+ * 
+ * File Name: FindChirpBankVeto.c
+ *
+ * Author: Brown D. A., and Hanna, C.
+ * 
+ * Revision: $Id$
+ * 
+ *-----------------------------------------------------------------------
+ */
+
+#if 0 
+<lalVerbatim file="FindChirpBankVetoCV">
+Author: Brown, D. A., and Hanna, C.
+$Id$
+</lalVerbatim> 
+
+<lalLaTeX>
+\vfill{\footnotesize\input{FindChirpBankVetoCV}}
+</lalLaTeX> 
+#endif
+
+#include <lal/LALStdlib.h>
+#include <lal/LALConstants.h>
+#include <lal/AVFactories.h>
+#include <lal/DataBuffer.h>
+#include <lal/LALInspiral.h>
+#include <lal/FindChirp.h>
+
+NRCSID (FINDCHIRPBANKVETOC, "$Id$");
+
+static int compareTemplate (const void * a, const void * b);
+
+/* <lalVerbatim file="FindChirpBankVetoCP"> */
+FindChirpSubBank*
+XLALFindChirpCreateSubBanks(
+    UINT4                      *maxSubBankSize,
+    UINT4                       subBankSize,
+    UINT4                       bankSize,
+    InspiralTemplate           *bankHead
+    )
+/* </lalVerbatim> */
+{
+  static const char *func = "XLALFindChirpCreateSubBanks";
+  UINT4                 i;
+  UINT4                 numSubBanks = 0;
+  UINT4                 subBankRemainder = 0;
+  UINT4                *bankSizes = NULL;
+  FindChirpSubBank     *subBankHead = NULL;
+  FindChirpSubBank     *thisSubBank = NULL;
+  InspiralTemplate     *thisTmplt = NULL;
+  InspiralTemplate     *nextTmplt = NULL;
+
+  numSubBanks = bankSize / subBankSize;
+
+  if ( ! numSubBanks )
+  {
+    /* the bank is smaller than the subbank size, so return the entire */
+    /* template bank as the subbank                                    */
+    subBankHead = (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    subBankHead->bankHead = bankHead;
+    subBankHead->subBankSize = bankSize;
+    *maxSubBankSize = bankSize;
+    return subBankHead;
+  }
+
+  subBankRemainder = bankSize % subBankSize;
+
+  /* create an array of subbank sizes with the minimum size */
+  bankSizes = (UINT4 *) LALCalloc( numSubBanks, sizeof(UINT4) );
+
+  for ( i = 0; i < numSubBanks; ++i )
+  {
+    bankSizes[i] = subBankSize;
+  }
+
+  /* disperse the remainder through the subbanks */
+  while( subBankRemainder )
+  {
+    for ( i = 0; i < numSubBanks; ++i )
+    {
+      if ( ! subBankRemainder )
+      {
+        break;
+      }
+      ++bankSizes[i];
+      --subBankRemainder;
+    }
+  }
+
+  /* allocate storage for the subbanks */
+  *maxSubBankSize = 0;
+  for ( i = 0; i < numSubBanks; ++i )
+  {
+    if ( ! subBankHead )
+    {
+      thisSubBank = subBankHead = 
+        (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    }
+    else
+    {
+      thisSubBank = thisSubBank->next = 
+        (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    }
+    
+    thisSubBank->subBankSize = bankSizes[i];
+
+    /* store the size of the biggest bank */
+    if ( bankSizes[i] > *maxSubBankSize )
+    {
+      *maxSubBankSize = bankSizes[i];
+    }
+  }
+
+  /* chop up the template bank into subbanks */
+  for ( thisSubBank = subBankHead, nextTmplt = bankHead; thisSubBank; 
+      thisSubBank = thisSubBank->next )
+  {
+    thisTmplt = nextTmplt;
+    thisSubBank->bankHead = thisTmplt;
+    for ( i = 0; i < thisSubBank->subBankSize - 1; ++i )
+    {
+      thisTmplt = thisTmplt->next;
+    }
+    nextTmplt = thisTmplt->next;
+    thisTmplt->next = NULL;
+  }
+
+  LALFree( bankSizes );
+
+  return subBankHead;
+}
+
+void 
+XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData, 
+                    FindChirpSubBank *vetoBank,
+                    REAL4 dynRange, 
+                    REAL4 fLow,
+                    REAL4 deltaF,
+                    REAL4 deltaT )
+
+{
+
+  UINT4 i,j,k,iMax,jMax;
+  UINT4 iSize = bankVetoData->length;
+  UINT4 tmpLen = bankVetoData->fcInputArray[0]->fcTmplt->data->length;
+  REAL4 ABr, ABi, Br, Bi, sqResp;
+  UINT4 stIX = floor( fLow / deltaF );
+  /*  fprintf(stderr, "tmpLen %d, resp->length %d, spec->length %d\n",
+         tmpLen, bankVetoData->resp->length, bankVetoData->spec->length); */
+  for ( i = 0; i < iSize; i++ )
+  {
+    for ( j = i; j < iSize; j++ )
+    {
+      if ( i >= vetoBank->subBankSize ) 
+      {
+        bankVetoData->ccMat->data[i*iSize + j] = 0;
+        /* bankVetoData->normMat->data[j] = 0; */
+      }
+      else
+      {
+        iMax = bankVetoData->fcInputArray[i]->fcTmplt->tmplt.fFinal / deltaF;
+        jMax = bankVetoData->fcInputArray[j]->fcTmplt->tmplt.fFinal / deltaF;
+        ABr = ABi = 0;
+        for ( k = stIX; k < tmpLen; k++ )
+        {
+          /*if ( (k > iMax) || (k > jMax) ) break;*/
+          sqResp = ( bankVetoData->resp->data[k].re * 
+                     bankVetoData->resp->data[k].re +
+                     bankVetoData->resp->data[k].im *
+                     bankVetoData->resp->data[k].im ) * dynRange * dynRange;
+
+          if ( bankVetoData->spec->data[k] != 0 && sqResp != 0 )
+          {
+            Br = bankVetoData->fcInputArray[j]->fcTmplt->data->data[k].re /
+                 ( bankVetoData->spec->data[k] * sqResp );
+            Bi = bankVetoData->fcInputArray[j]->fcTmplt->data->data[k].im /
+                 ( bankVetoData->spec->data[k] * sqResp );
+            ABr += bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].re*Br+
+                   bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].im*Bi;
+            ABi += bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].re*Bi-
+                   bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].im*Br; 
+          }
+        }  
+        bankVetoData->ccMat->data[i*iSize + j] = sqrt(ABr*ABr+ ABi*ABi); /*
+           sqrt( bankVetoData->fcInputArray[i]->fcTmplt->norm * 
+                 bankVetoData->fcInputArray[j]->fcTmplt->norm );*/
+
+        bankVetoData->ccMat->data[j*iSize + i] = sqrt(ABr*ABr + ABi*ABi);  /* 
+           sqrt( bankVetoData->fcInputArray[i]->fcTmplt->norm *
+                 bankVetoData->fcInputArray[j]->fcTmplt->norm );*/
+
+        /*fprintf(stderr,"ccMat[%d,%d] = %f, iMax %d, jMax %d\n", i,j,
+                bankVetoData->ccMat->data[i*iSize + j], iMax, jMax); */
+        
+      }
+    }
+  }
+}
+
+REAL4
+XLALComputeBankVeto( FindChirpBankVetoData *bankVetoData,
+                     UINT4 i,
+                     UINT4 snrIX,
+                     UINT4 *dof)
+{
+
+  REAL4 iSNR = 0;
+  REAL4 jSNR = 0;
+  REAL4 chisq = 0;
+  UINT4 j = 0;
+  REAL4 ii,jj,ij,denomFac;
+  UINT4 iSize = bankVetoData->length;
+  
+  iSNR = sqrt( ( bankVetoData->qVecArray[i]->data[snrIX].re *
+                 bankVetoData->qVecArray[i]->data[snrIX].re +
+                 bankVetoData->qVecArray[i]->data[snrIX].im *
+                 bankVetoData->qVecArray[i]->data[snrIX].im ) * 
+                 bankVetoData->fcInputArray[i]->fcTmplt->norm ); 
+  *dof = 0;
+  /*fprintf(stderr, "norm %e norm %e \n", bankVetoData->normMat->data[i], 
+          bankVetoData->fcInputArray[i]->fcTmplt->norm);
+  fprintf(stderr, "iSNR %f\n", iSNR); */
+  if (iSize == 1) return 0;
+
+  for (j = 0; j < iSize; j++)
+  {
+    ij = bankVetoData->ccMat->data[i*iSize + j];
+    ii = bankVetoData->ccMat->data[i*iSize + i];
+    jj = bankVetoData->ccMat->data[j*iSize + j];
+
+    denomFac = ij/sqrt(ii*jj) - sqrt(ii*jj)/ij; 
+               /* ( (ij / ii) - (jj / ij) ) ; */
+               /* bankVetoData->fcInputArray[j]->fcTmplt->norm /
+               bankVetoData->fcInputArray[i]->fcTmplt->norm; */
+    if ( denomFac != 0.0 )
+    {
+      jSNR = sqrt( ( bankVetoData->qVecArray[j]->data[snrIX].re *
+                     bankVetoData->qVecArray[j]->data[snrIX].re +
+                     bankVetoData->qVecArray[j]->data[snrIX].im *
+                     bankVetoData->qVecArray[j]->data[snrIX].im ) * 
+                     bankVetoData->fcInputArray[j]->fcTmplt->norm ); 
+      chisq += (iSNR - sqrt(ii*jj) / ij * jSNR) / denomFac *
+               (iSNR - sqrt(ii*jj) / ij * jSNR) / denomFac;
+      (*dof)++;
+    }
+  }
+  return chisq;
+}
+
+
+InspiralTemplate * 
+XLALFindChirpSortTemplates( InspiralTemplate *bankHead, UINT4 num ){
+
+  InspiralTemplate **bankArray = NULL;
+  InspiralTemplate *bankFirst = NULL;
+  UINT4 i = 0;
+  bankFirst = bankHead;
+  bankArray = (InspiralTemplate **) LALCalloc(num, sizeof(InspiralTemplate *));
+
+
+  for (i = 0; (i < num); bankHead = bankHead->next, i++)
+  {
+    bankArray[i] = bankHead; /* populate pointer array */
+  }
+
+  qsort(bankArray, num, sizeof(InspiralTemplate *), compareTemplate);
+
+  bankFirst = bankHead = bankArray[0];
+  /* repopulate linked list */
+  for (i=1; i < num; i++)
+  {
+    bankHead = bankHead->next = bankArray[i];
+  }
+  bankHead->next = NULL;
+  LALFree(bankArray);
+  return bankFirst;
+}
+
+static int compareTemplate (const void * a, const void * b)
+{
+  REAL4 mVal1 =   (*((InspiralTemplate**)a))->mass1 +
+                  (*((InspiralTemplate**)a))->mass2 ;
+  REAL4 mVal2 =   (*((InspiralTemplate**)b))->mass1 +
+                  (*((InspiralTemplate**)b))->mass2 ;
+
+  if ( mVal1 > mVal2 ) return 1;
+  if ( mVal1 == mVal2 ) return 0;
+  if ( mVal1 < mVal2 ) return -1;
+}
+
