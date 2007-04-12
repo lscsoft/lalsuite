f55dc9a59cb188a3aca6d5b05a7c343dac06682d
sfairhur <sfairhur>
2007-04-12 23:26:24 +0000

    Change the way we extend from inspiral to ringdown Original: 28058bf7d7037b9d7eddb700af6359f6b48f950f

--- lal/packages/inject/src/GenerateInspRing.c
+++ lal/packages/inject/src/GenerateInspRing.c
@@ -34,17 +34,21 @@ XLALGenerateInspRing(
 {
   static const char *func = "XLALClusterSnglInspiralTable";
 
-  REAL4 *a, *f, *shift; /* pointers to generated amplitude and frequency data */
+  REAL4 *a, *f; /* pointers to generated amplitude and frequency data */
+  REAL4 *shift = NULL; /* pointers to generated shift (polarization) data */
   REAL8 *phi;   /* pointer to generated phase data */
 
   /* length of the waveform */
   INT4 inputLength;
   INT4 outputLength;
-  INT4 linearLength;
+  INT4 mergerLength;
   INT4 ringLength;
 
   /* waveform parameters */
   REAL8 phase;
+  REAL8 mergerPhase;
+  REAL8 phi0;
+  REAL4 dt; 
   REAL4 ampPlus;
   REAL4 ampPlusDot;
   REAL4 a2Plus;
@@ -53,11 +57,15 @@ XLALGenerateInspRing(
   REAL4 a2Cross;
   REAL4 freq;
   REAL4 freqDot;
+  REAL4 freq0;
+  REAL4 freqDot0;
   REAL4 polarization;
   REAL4 polDot;
   REAL4 dampFac;
   REAL4 phaseFac;
   REAL4 A, lambda;
+  REAL4 tToRing;
+  REAL4 tRing;
   INT4 n, N;
 
   /* ringdown details */
@@ -98,20 +106,30 @@ XLALGenerateInspRing(
   inputLength = waveform->a->data->length;
 
   /* record the final frequency, amplitude and phase */
+  dt = waveform->f->deltaT;
+
   ampPlus = waveform->a->data->data[2*inputLength - 2];
   ampPlusDot = ampPlus - waveform->a->data->data[2*inputLength - 4];
 
   ampCross = waveform->a->data->data[2*inputLength - 1];
   ampCrossDot = ampCross - waveform->a->data->data[2*inputLength - 3];
 
-  freq = waveform->f->data->data[inputLength - 1];
-  freqDot = freq - waveform->f->data->data[inputLength - 2];
+  freq0 = waveform->f->data->data[inputLength - 1];
+  freqDot0 = freq0 - waveform->f->data->data[inputLength - 2];
+  freqDot0 /= dt;
 
   phase = waveform->phi->data->data[inputLength - 1];
 
-  polarization = waveform->shift->data->data[inputLength - 1];
-  polDot = polarization - waveform->shift->data->data[inputLength - 2];
 
+  if ( waveform->shift )
+  {
+    polarization = waveform->shift->data->data[inputLength - 1];
+    polDot = polarization - waveform->shift->data->data[inputLength - 2];
+  }
+
+  XLALPrintInfo(
+      "Final inspiral frequency = %.2f Hz, fdot = %.2f Hz/s\n"
+      "\n", freq0, freqDot0);
 
   /*
    *
@@ -161,7 +179,7 @@ XLALGenerateInspRing(
   totalAngMom = pow(Jx * Jx + Jy * Jy + Jz * Jz, 0.5);
 
   XLALPrintInfo( "Approximate orbital ang mom = %.2f\n", orbAngMom);
-  XLALPrintInfo( "Approximate total angular momentum of inspiral:"
+  XLALPrintInfo( "Approximate total angular momentum of inspiral:\n"
       "Jx = %.2f, Jy = %.2f, Jz = %.2f\n", Jx, Jy, Jz);
   XLALPrintInfo( "Estimated Final Spin = %.2f\n", totalAngMom );
 
@@ -179,17 +197,9 @@ XLALGenerateInspRing(
     ringInj->mass;
   ringInj->quality = 2.0 * pow( ( 1.0 - ringInj->spin ), -0.45 );
 
-  XLALPrintInfo( "Estimated Frequency = %.2f, Quality = %.2f\n", 
-      ringInj->frequency, ringInj->quality );
-
-  /* XXX For now, we don't acutally correctly calculate the ring epsilon, 
-   * amplitude or hrss
-   ringInj->epsilon = 0.01 * (16 * inspiralInj->eta * inspiralInj->eta); 
-   ringInj->amplitude = XLALBlackHoleRingAmplitude( ringInj->frequency,
-   ringInj->quality, ringInj->distance, ringInj->epsilon );
-   ringInj->hrss = ringInj->amplitude * sqrt( 2 / LAL_PI / ringInj->frequency ) 
-   * pow( ( 2.0 * pow( ringInj->quality, 3.0 ) + ringInj->quality ) / 
-   ( 1.0 + 4.0 * pow ( ringInj->quality, 2 ) ) , 0.5); XXX */
+  XLALPrintInfo( 
+      "Estimated Ringdown Frequency = %.2f Hz, and Quality = %.2f\n"
+      "\n", ringInj->frequency, ringInj->quality );
 
   /*
    *
@@ -197,29 +207,66 @@ XLALGenerateInspRing(
    *
    */
 
-  /* We extend the frequency as follows:
-   * 1) Increase the frequency linearly to time T s.t.
-   *      frequency = 0.75 * ringdown frequency
-   * 2) After that, exponentially approach the ring frequency.
+  /* We use a very simple model to extend the frequency of the waveform.
+   * This is done in two parts:
+   *
+   * 1) Continue the frequency by assuming that fdot is proportional f^(11/3)
+   *
+   *    using this, we obtain the frequency and phase at any later time 
+   *    given f, fdot at t=0 as:
+   *
+   *    f(t) = f(0) * ( 1 - (8 fdot(0) t / 3 f(0) ) )^(-3/8)
+   *
+   *    phi(t) = phi0 - (2 pi) * 3/5 * f(0)^2 / fdot(0) *
+   *                     (1 - (8 fdot(0) t / 3 f(0) ) )^(5/8)
+   *
+   *    where phi0 = phi(0) + ( 2 pi ) * ( 3 f(0)^2 / 5 fdot(0) )
+   *
+   * 
+   * 2) At the point before the ring frequency would be exceeded, 
+   *    exponentially approach the ring frequency.
    *    -- The exponential decay is determined by matching the derivative
    */
 
-  /* We extend the amplitude as follows:
-   * 1) Use a parabolic fit to the amplitude which fits 3 data points:
-   * f(0), f'(0) from inspiral f'(2T) / f(2T) from ringdown
-   */
+  /* calculate the number of points to reach 0.9 * ringdown frequency */
+  tToRing = 3 * freq0 / ( 8 * freqDot0 ) * 
+    (1 - pow( freq0 / (0.9 * ringInj->frequency), 8.0/3.0) );
+  mergerLength = ceil( tToRing / dt ) - 1;
+  phi0 = phase + LAL_TWOPI * 3 * freq0 * freq0 / (5 * freqDot0);
+  
+  mergerPhase = phi0 - phase
+    - ( LAL_TWOPI) * (3.0 * freq0 * freq0 ) / ( 5.0 * freqDot0 ) * 
+    pow( 1 - ( 8.0 * freqDot0 * tToRing) / ( 3.0 * freq0 ) , 5.0/8.0 );
 
-  /* calculate the number of points needed to get to 0.75 * ringFreq */
-  linearLength = ceil( (0.75 * ringInj->frequency - freq) / freqDot);
+
+  XLALPrintInfo( "Adding by hand a merger and ringdown\n");
+  XLALPrintInfo( "Time to reach 0.9 of ringdown frequency = %.4f seconds,\n"
+      "%d data points, %.2f radians in GW phase\n" 
+      "We then add the same length to asymptote to ringdown values\n",
+      tToRing, mergerLength, mergerPhase);
 
   /* calculate number of additional points necessary to damp by exp(12). */
-  phaseFac = LAL_TWOPI * ringInj->frequency * waveform->a->deltaT;
+  phaseFac = LAL_TWOPI * ringInj->frequency * dt;
   dampFac = exp( - phaseFac / (2 * ringInj->quality));
+
   ringLength = ceil((24 * ringInj->quality)/( phaseFac));  
+  tRing = ringLength * dt;
 
-  outputLength = inputLength + 2 * linearLength + ringLength;
+  XLALPrintInfo( "Time to damp the ringdown by exp(12) = %.4f seconds,\n"
+      "%d data points, %.2f radians in GW phase\n"
+      "\n", tRing, ringLength, ringLength * phaseFac);
 
-  /* extend the data structures */
+  /* Total extra length is merger length + ringdown length + some length over
+   * which we smooth the merger to the ring */
+
+  outputLength = inputLength + 2 * mergerLength - 1 + ringLength;
+
+  /* 
+   *
+   * extend the data structures
+   *
+  */
+
   waveform->a->data->length = outputLength;
   waveform->a->data->data = (REAL4 *) XLALRealloc( ( waveform->a->data->data ), 
       2*outputLength*sizeof(REAL4) );  
@@ -269,15 +316,21 @@ XLALGenerateInspRing(
     shift = &(waveform->shift->data->data[inputLength]);
   }
 
-  /* 
-   * set frequency, phase and shift (if needed) up to the start of ringdown
+  /*
+   *
+   * generate frequency, phase and shift (if needed) for merger and ringdown
+   *
    */
 
+
   /* run frequency close to ring frequency */
-  for ( n = 0; n < linearLength; n++ )
+  for ( n = 1; n < mergerLength + 1; n++ )
   {
-    phase = *(phi++) = phase + LAL_TWOPI * freq * waveform->a->deltaT;
-    freq = *(f++) = freq + freqDot;
+    REAL4 factor = 1 - (8.0 * freqDot0 * n * dt) / (3.0 * freq0 );
+    freq = *(f++) = freq0 * pow( factor , -3.0/8.0 );
+    phase = *(phi++) = phi0 - 
+      LAL_TWOPI * 3.0 * freq0 * freq0 / (5.0 * freqDot0) * 
+      pow( factor , 5.0/8.0);
     if ( shift )
     {
       polarization = *(shift++) = polarization + polDot;
@@ -285,34 +338,47 @@ XLALGenerateInspRing(
   }
 
   /* converge to ring frequency and constant polarization */
-  /* fit to f = f_ring - A * exp( - lambda * n )
-   * then A = freq + fDot
-   *      A * lambda = fDot
+  /* fit to freq = f_ring - A * exp( - lambda * t )
+   * then A = f_ring - freq
+   *      A * lambda = freqDot
    */
-  A = ringInj->frequency - (freq + freqDot);
+  freqDot = (freq - *(f - 2)) / dt;
+  A = ringInj->frequency - freq;
   lambda = freqDot / A;
 
-  for ( n = 0; n < linearLength + ringLength; n++ )
+  XLALPrintInfo(
+      "Starting to asymptote to ringdown\n"
+      "Final 'merger' frequency = %.2f Hz, fdot = %.2f Hz/s\n", 
+      freq, freqDot); 
+  XLALPrintInfo(
+      "Frequency evolution fitted to freq = f_ring - A exp(-lambda t)\n"
+      "A = %.2f, lambda = %e\n"
+      "\n", A, lambda); 
+
+  for ( n = 1; n < mergerLength + ringLength; n++ )
   {
-    phase = *(phi++) = phase + LAL_TWOPI * freq * waveform->a->deltaT;
-    freq = *(f++) = ringInj->frequency - A * exp( - n * lambda );
+    phase = *(phi++) = phase + LAL_TWOPI * freq * dt;
+    freq = *(f++) = ringInj->frequency - A * exp( - n * dt * lambda );
     if ( shift )
     {
-      polDot /= exp( - lambda );
+      polDot /= exp( - lambda * dt);
       polarization = *(shift++) = polarization + polDot;
     }
   }
 
   /*
-   * set amplitude up to the start of ringdown
+   *
+   * set amplitude for merger and ringdown
+   *
    */
 
-  /* we fit with a quadratic.  The three pieces of data we need to fit are:
+  /* From end of inspiral to start of ringdown, we fit the amplitudes with
+   * a quadratic.  The three pieces of data we need to fit are:
    * 1) The initial amplitude 
    * 2) The initial amplitude derivative
-   * 3) The final amplitude derivative / amplitude
+   * 3) The ringdown amplitude derivative / amplitude =  1 - dampFac
    *
-   * so, if A_i = a_0 + a_1 * i + a_2 * i^2,
+   * so, if A(n) = a_0 + a_1 * n + a_2 * n^2,
    * 
    * a_0 = amp
    * a_1 = ampDot
@@ -326,10 +392,8 @@ XLALGenerateInspRing(
    *       (1 - dampFac) * N^2 + 2 * N 
    */
 
-  N = 2 * linearLength;
-
-  ampPlus += ampPlusDot;
-  ampCross += ampCrossDot;
+  /* the number of points */
+  N = 2 * mergerLength;
 
   a2Plus = ((dampFac - 1) * ( ampPlus + N * ampPlusDot ) - ampPlusDot) /
     ( 2*N - N*N*(dampFac - 1) );
@@ -338,16 +402,17 @@ XLALGenerateInspRing(
 
 
   /* quadratic part */
-  for ( n = 0; n < N; n++ )
+  for ( n = 1; n < N; n++ )
   {
     *(a++) = ampPlus + ampPlusDot * n + a2Plus * n * n;
     *(a++) = ampCross + ampCrossDot * n + a2Cross * n * n;
   }
 
-  /* ringdown part */
-  ampPlus = ampPlus + ampPlusDot * N + a2Plus * N * N;
-  ampCross = ampCross + ampCrossDot * N + a2Cross * N * N;
+  /* set the final amplitudes */
+  ampPlus = *(a - 2);
+  ampCross = *(a - 1);
 
+  /* ringdown part */
   for ( n = 0; n < ringLength; n++ )
   {
     ampPlus = *(a++) = ampPlus * dampFac;
