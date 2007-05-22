b1cd89bf79b9abdd7413965694dd079ad5018300
bema <bema>
2007-05-22 14:01:29 +0000

    where did this LAL got lost? Original: af5469bea76582acdbe510c7c2accf81d56ba3cf

--- lal/packages/inspiral/src/LALPrintTimeseries.c
+++ lal/packages/inspiral/src/LALPrintTimeseries.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include <lal/RCSID.h>
+#include <lal/LALRCSID.h>
 NRCSID (LALPRINTTIMESERIESC,"$Id$");
 
 void LALPrintTimeseries (int n, double *signal, double delta, double t0) ;
