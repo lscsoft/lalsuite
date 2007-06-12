a073b321d962ac135742915acc99df8de8abb0ce
bema <bema>
2007-06-12 13:28:05 +0000

    some more doxygenizing Original: 2f1f769bddbf3d0231f5ba49004f187b0622a60e

--- lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
+++ lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
@@ -38,7 +38,7 @@ NRCSID(HSBOINCEXTRASHRCSID,"$Id$");
 
 /* linking proper functions to the hooks in HierarchicalSearch.c */
 
-/* use a local copy of ComputeFStatFreqBand() and realted functions for E@H-specific optimizations */
+/* use a local copy of ComputeFStatFreqBand() and related functions for E@H-specific optimizations */
 #ifndef EAH_OPTIMIZATION
 #define COMPUTEFSTATFREQBAND ComputeFStatFreqBand
 #else
@@ -105,56 +105,57 @@ extern LALStatus *global_status;
 
 /* function prototypes, they are defined in boinc_extras.c */
 
-/* allows the App to register another output file to be put into the
-   zip archive that is sent back to the server */
+/** allows the App to register another output file to be put into the
+    zip archive that is sent back to the server */
 extern void register_output_file(char*filename);
 
-/* show progress of the App.
-   NOTE: This also set the count & total (skypos) for checkpointing */
+/** show progress of the App.
+    NOTE: This also set the count & total (skypos) for checkpointing */
 extern void show_progress(double rac, double dec, UINT4 count, UINT4 total);
 
-/* inits checkpointing for the toplist and reads the last checkpoint if present
-   This expects all passed variables (toplist, total, count) to be already initialized.
-   The variables are modified only if a previous checkpoint was found.
-   If *cptname (name of the checkpoint file) is NULL,
-   the name is constructed by appending ".cpt" to the output filename.
-   The FILE* should be the one that checpointed_fopen() above has returned.
+/** inits checkpointing for the toplist and reads the last checkpoint if present
+    This expects all passed variables (toplist, total, count) to be already initialized.
+    The variables are modified only if a previous checkpoint was found.
+    If *cptname (name of the checkpoint file) is NULL,
+    the name is constructed by appending ".cpt" to the output filename.
+    The FILE* should be the one that checpointed_fopen() above has returned.
 
-   The function returns
-     0 if no checkpoint could be found,
-    -1 if a checkpoint was found but it or the previous output couldn't be read,
-    -2 if an error occured (out of memory),
-     1 if a checkpoint was found and previous output could be read
-     2 a previously written end marker was detected
+    The function returns
+    0 if no checkpoint could be found,
+   -1 if a checkpoint was found but it or the previous output couldn't be read,
+   -2 if an error occured (out of memory),
+    1 if a checkpoint was found and previous output could be read
+    2 a previously written end marker was detected
 */
 extern int init_and_read_checkpoint(toplist_t*toplist, UINT4*count,
 				     UINT4 total, char*outputname, char*cptname);
 
-/* This corresponds to insert_into_fstat_toplist().
-   It inserts a candidate into the toplist, updates the file
-   and "compacts" it if necessary (i.e. bytes > maxsize).
-   NOTE that the toplist parameter is just a dummy to make the interface
-        compatible to insert_into_fstat_toplist(). The operation is
-        actually performed on the toplist passed to the least recent call
-        of init_and_read_checkpoint(), which, however, should be the same
-        in all reasonable cases. */
+/** This corresponds to insert_into_fstat_toplist().
+    It inserts a candidate into the toplist, updates the file
+    and "compacts" it if necessary (i.e. bytes > maxsize).
+    NOTE that the toplist parameter is just a dummy to make the interface
+         compatible to insert_into_fstat_toplist(). The operation is
+         actually performed on the toplist passed to the least recent call
+         of init_and_read_checkpoint(), which, however, should be the same
+         in all reasonable cases. */
 extern int add_candidate_and_checkpoint (toplist_t*toplist, FstatOutputEntry cand);
 
-/* replacement for add_candidate_and_checkpoint(), currently being tested */
+/** replacement for add_candidate_and_checkpoint() */
 extern int add_checkpoint_candidate (toplist_t*toplist, FstatOutputEntry cand);
 extern void set_checkpoint(void);
 
 
-/* does the final (compact) write of the file and cleans up checkpointing stuff
-   The checkpoint file remains there in case something goes wrong during the rest */
+/** does the final (compact) write of the file and cleans up checkpointing stuff
+    The checkpoint file remains there in case the App gets inteerupted afterwards
+    but befor boinc_finish was called */
 extern void write_and_close_checkpointed_file (void);
 
-/* LALApps error handler for BOINC */
+/** LALApps error handler for BOINC */
 int BOINC_LAL_ErrHand (LALStatus*, const char*, const char*, const int, volatile const char*);
 
 
-/* the main() function of HierarchicalSerach.c becomes the extern MAIN(),
-   the real main() function of the BOINC App is defined in boinc_extras.c
+/** the main() function of HierarchicalSerach.c becomes the extern MAIN(),
+    the real main() function of the BOINC App is defined in boinc_extras.c
 */
 extern int MAIN(int,char**);
 
