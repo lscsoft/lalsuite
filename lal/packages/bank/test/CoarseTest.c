9de1fd3f75ae97d57190252700018f5b17259793
sathya <sathya>
2007-06-18 13:44:42 +0000

    removing the conflict Original: c9ffd616e6ef31deb483b01d07ec6d2e754e5c47

--- lal/packages/bank/test/CoarseTest.c
+++ lal/packages/bank/test/CoarseTest.c
@@ -167,11 +167,8 @@ main ( void )
    coarseIn = (InspiralCoarseBankIn *)LALMalloc(sizeof(InspiralCoarseBankIn));
    fineIn = (InspiralFineBankIn *)LALMalloc(sizeof(InspiralFineBankIn));
 
-<<<<<<< CoarseTest.c
    coarseIn->gridSpacing = Hexagonal;
-=======
    /* fill the coarseBankIn structure */
->>>>>>> 1.25
    coarseIn->mMin = 1.0;
    coarseIn->mMax = 30.0;
    coarseIn->MMax = coarseIn->mMax * 2.0;
