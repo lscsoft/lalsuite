57bc81d14e18e48562dd4ef06c784b5c3efb3793
bema <bema>
2007-08-21 12:32:03 +0000

    start new line after writing message to stderr Original: b46d3c96a456f2ff3e42da0794438dbbe7068f00

--- lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
+++ lalapps/src/pulsar/hough/src2/hs_boinc_extras.h
@@ -82,7 +82,7 @@ LocalComputeFStatFreqBand ( LALStatus *status,
 #define GET_CHECKPOINT(toplist,total,count,outputname,cptname)\
   { int ret = init_and_read_checkpoint(toplist,total,count,outputname,cptname);\
     if(ret < 0) {\
-      fprintf(stderr,HIERARCHICALSEARCH_MSGCHECKPT);\
+      fprintf(stderr, HIERARCHICALSEARCH_MSGCHECKPT "\n");\
       return(HIERARCHICALSEARCH_ECHECKPT);\
     } else if (ret == 2) {\
       return(HIERARCHICALSEARCH_ENORM);\
