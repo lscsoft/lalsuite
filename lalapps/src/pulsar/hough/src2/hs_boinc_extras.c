c0bb320f462e016b1a2bb892d13391d3434a5caa
bema <bema>
2007-09-12 12:42:25 +0000

    fixing... Original: 3763010de8727c7cc7a816126182e1e68297320d

--- lalapps/src/pulsar/hough/src2/hs_boinc_extras.c
+++ lalapps/src/pulsar/hough/src2/hs_boinc_extras.c
@@ -1210,7 +1210,7 @@ void attach_gdb() {
 #ifdef __GLIBC__
   char cmd[256];
   pid_t pid=getpid();
-  snprintf(cmd,"gdb -pid %d -ex gcore --args %s", pid, global_argv[0], sizeof(cmd)); 
+  snprintf(cmd, , sizeof(cmd), "gdb -pid %d -ex gcore --args %s", pid, global_argv[0]); 
   system(cmd);
   sleep(20);
 #endif
