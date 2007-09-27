cbf96f59fc871cce9218d023d7f542466342f682
kipp <kipp>
2007-09-27 00:51:14 +0000

    - Edit some comments and variable names to make the correspondence between   this code and the technical documentation a little more clear. - Discard tiles with non-real-valued h_rss. Original: 62eed94b3aa3817c475b0d7eecdfd8233fadf179

--- lal/packages/burstsearch/src/FreqSeriesToTFPlane.c
+++ lal/packages/burstsearch/src/FreqSeriesToTFPlane.c
@@ -134,7 +134,7 @@ int XLALFreqSeriesToTFPlane(
 		XLAL_ERROR(func, XLAL_EFUNC);
 
 #if 0
-	/* diagnostic code to dump data for the s_{k} histogram */
+	/* diagnostic code to dump data for the \hat{s}_{k} histogram */
 	{
 	unsigned k;
 	FILE *f = fopen("sk.dat", "a");
@@ -144,8 +144,8 @@ int XLALFreqSeriesToTFPlane(
 	}
 #endif
 #if 0
-	/* diagnostic code to dump data for the s_{k} s^{*}_{k'} histogram
-	 * */
+	/* diagnostic code to dump data for the \hat{s}_{k}
+	 * \hat{s}^{*}_{k'} histogram */
 	{
 	unsigned k, dk;
 	FILE *f = fopen("sksk.dat", "a");
@@ -284,23 +284,24 @@ SnglBurstTable *XLALComputeExcessPower(
 
 	for(channels = plane->tiles.min_channels; channels <= plane->tiles.max_channels; channels *= 2) {
 	for(channel_end = (channel = 0) + channels; channel_end <= plane->channels; channel_end = (channel += channels / plane->tiles.inv_fractional_stride) + channels) {
-		/* the mean square of the "virtual channel" */
-		const double sum_mean_square = channels * plane->deltaF / plane->fseries_deltaF + XLALREAL8SequenceSum(plane->twice_channel_overlap, channel, channels - 1);
-		/* the mean square of the "uwsum" quantity computed below,
-		 * which is proportional to an approximation of the
+		/* the mean square of the "virtual channel", \mu^{2} in the
+		 * algorithm description */
+		const double sample_mean_square = channels * plane->deltaF / plane->fseries_deltaF + XLALREAL8SequenceSum(plane->twice_channel_overlap, channel, channels - 1);
+		/* the mean square of the "uwapprox" quantity computed
+		 * below, which is proportional to an approximation of the
 		 * unwhitened time series. */
-		double uwsum_mean_square;
+		double uwapprox_mean_square;
 		/* true unwhitened mean square for this channel.  the ratio
-		 * of this to uwsum_mean_square is the correction factor to
-		 * be applied to uwsum^{2} to convert it to an
+		 * of this to uwapprox_mean_square is the correction factor to
+		 * be applied to uwapprox^{2} to convert it to an
 		 * approximation of the square of the unwhitened channel */
 		const double unwhitened_mean_square = XLALREAL8SequenceSumSquares(plane->unwhitened_rms, channel, channels) + XLALREAL8SequenceSum(plane->unwhitened_cross, channel, channels - 1);
 		unsigned c;
 
-		/* compute uwsum_mean_square */
-		uwsum_mean_square = XLALREAL8SequenceSumSquares(plane->unwhitened_rms, channel, channels);
+		/* compute uwapprox_mean_square */
+		uwapprox_mean_square = XLALREAL8SequenceSumSquares(plane->unwhitened_rms, channel, channels);
 		for(c = channel; c < channel_end - 1; c++)
-			uwsum_mean_square += plane->twice_channel_overlap->data[c] * plane->unwhitened_rms->data[c] * plane->unwhitened_rms->data[c + 1] * plane->fseries_deltaF / plane->deltaF;
+			uwapprox_mean_square += plane->twice_channel_overlap->data[c] * plane->unwhitened_rms->data[c] * plane->unwhitened_rms->data[c + 1] * plane->fseries_deltaF / plane->deltaF;
 
 	/* start with at least 2 degrees of freedom */
 	for(length = 2 / (channels * plane->tiles.dof_per_pixel); length <= plane->tiles.max_length; length *= 2) {
@@ -317,24 +318,33 @@ SnglBurstTable *XLALComputeExcessPower(
 
 		/* compute sum of squares, and unwhitened sum of squares */
 		for(t = start + stride / 2; t < end; t += stride) {
-			double sum = 0;
-			double uwsum = 0;
+			double sample = 0;
+			double uwapprox = 0;
 
-			/* compute snr and dewhitened amplitude for this
-			 * "virtual pixel". */
+			/* compute the whitened and (approximate)
+			 * unwhitened time series samples for this t */
 			for(c = channel; c < channel_end; c++) {
-				sum += plane->channel[c]->data[t];
-				uwsum += plane->channel[c]->data[t] * plane->unwhitened_rms->data[c] * sqrt(plane->fseries_deltaF / plane->deltaF);
+				sample += plane->channel[c]->data[t];
+				uwapprox += plane->channel[c]->data[t] * plane->unwhitened_rms->data[c] * sqrt(plane->fseries_deltaF / plane->deltaF);
 			}
 
-			sumsquares += sum * sum;
-			uwsumsquares += uwsum * uwsum;
-		}
-		/* normalization to give each pixel a mean square of 1.0 */
-		sumsquares /= sum_mean_square;
+#if 0
+			/* diagnostic code to dump data for the s_{j} histogram */
+			{
+			FILE *f = fopen("sj.dat", "a");
+			fprintf(f, "%g\n", uwapprox / sqrt(uwapprox_mean_square));
+			fclose(f);
+			}
+#endif
 
-		/* compute statistical confidence, see if it's above
-		 * threshold */
+			sumsquares += sample * sample;
+			uwsumsquares += uwapprox * uwapprox;
+		}
+		/* normalization to give each sample a mean square of 1.0 */
+		sumsquares /= sample_mean_square;
+		uwsumsquares /= uwapprox_mean_square;
+
+		/* compute statistical confidence */
 		/* FIXME:  the 0.62 is an empirically determined
 		 * degree-of-freedom fudge factor.  figure out what its
 		 * origin is, and account for it correctly.  it's most
@@ -345,12 +355,11 @@ SnglBurstTable *XLALComputeExcessPower(
 		confidence = -XLALlnOneMinusChisqCdf(sumsquares * .62, tile_dof * .62);
 		if(XLALIsREAL8FailNaN(confidence))
 			XLAL_ERROR_NULL(func, XLAL_EFUNC);
-		if(confidence >= confidence_threshold) {
-			SnglBurstTable *oldhead;
 
-			/* normalization to give each unwhitened pixel a
-			 * mean square of 1.0 */
-			uwsumsquares /= uwsum_mean_square;
+		/* record tiles whose statistical confidence is above
+		 * threshold and that have real-valued h_rss */
+		if((confidence >= confidence_threshold) && (uwsumsquares >= tile_dof)) {
+			SnglBurstTable *oldhead;
 
 			/* compute h_rss */
 			h_rss = sqrt((uwsumsquares - tile_dof) * unwhitened_mean_square * stride * plane->deltaT);
