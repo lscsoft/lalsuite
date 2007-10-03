b8ac216de64a8cffe6409c92d2972523e1f3bfc3
jolien <jolien>
2007-10-03 14:49:11 +0000

    Added possibly missing prototypes. Original: 5520e07c58818b55a8478b59cdb2c72584740961

--- lal/packages/std/src/LALComplex.c
+++ lal/packages/std/src/LALComplex.c
@@ -32,6 +32,15 @@ gh
 #define LAL_NO_COMPLEX_MACROS
 #include <lal/LALComplex.h>
 
+
+/* prototype non C89 functions -- they're probably in libm but
+ * compiling with -ansi will get rid of prototypes */
+
+double hypot(double x, double y);
+double log1p(double x);
+double acosh(double x);
+double atanh(double x);
+
 NRCSID (LALCOMPLEXC,"$Id$");
 
 
