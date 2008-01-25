fd7505c56cb9e96e6417ad5d71dc9137508131d4
kipp <kipp>
2008-01-25 07:43:20 +0000

    Add XLALGenerateSimBurst(). Original: 3a1209e189dce93f6db07401aee0635b536506a2

--- lal/packages/inject/src/GenerateBurst.c
+++ lal/packages/inject/src/GenerateBurst.c
@@ -54,6 +54,57 @@ NRCSID(GENERATEBURSTC, "$Id$");
 
 
 /*
+ * Generate the + and x time series for a single sim_burst table row.
+ * Note:  only the row pointed to by sim_burst is processed, the linked
+ * list is not iterated over.
+ */
+
+
+static int XLALGenerateSimBurst(
+	REAL8TimeSeries **hplus,
+	REAL8TimeSeries **hcross,
+	const SimBurst *sim_burst,
+	double delta_t
+)
+{
+	static const char func[] = "XLALGenerateSimBurst";
+
+	if(!strcmp(sim_burst->waveform, "BTLWNB")) {
+		/* E_{GW} is in M_{sun}, so we multiply by (M_{sun} c^2) to
+		 * convert to energy, which is then multiplied by (4 G /
+		 * c^3) to convert to a value of \int \dot{h}^{2} \diff t.
+		 * */
+		double int_hdot_squared_dt = sim_burst->egw_over_rsquared * LAL_MSUN_SI * 4 * LAL_G_SI / LAL_C_SI;
+
+		/* the waveform number is interpreted as the seed for GSL's
+		 * Mersenne twister random number generator */
+		gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
+		if(!rng)
+			XLAL_ERROR(func, XLAL_ENOMEM);
+		gsl_rng_set(rng, sim_burst->waveform_number);
+
+		if(XLALGenerateBandAndTimeLimitedWhiteNoiseBurst(hplus, hcross, sim_burst->duration, sim_burst->frequency, sim_burst->bandwidth, int_hdot_squared_dt, delta_t, rng)) {
+			gsl_rng_free(rng);
+			XLAL_ERROR(func, XLAL_EFUNC);
+		}
+		gsl_rng_free(rng);
+	} else if(!strcmp(sim_burst->waveform, "StringCusp")) {
+		if(XLALGenerateStringCusp(hplus, hcross, sim_burst->amplitude, sim_burst->frequency, delta_t))
+			XLAL_ERROR(func, XLAL_EFUNC);
+	} else if(!strcmp(sim_burst->waveform, "SineGaussian")) {
+		if(XLALSimBurstSineGaussian(hplus, hcross, sim_burst->q, sim_burst->frequency, sim_burst->hrss, sim_burst->pol_ellipse_e, sim_burst->pol_ellipse_angle, delta_t))
+			XLAL_ERROR(func, XLAL_EFUNC);
+	} else
+		/* unrecognized waveform */
+		XLAL_ERROR(func, XLAL_EINVAL);
+
+	/* done */
+
+	return 0;
+}
+
+
+/*
  * Convenience wrapper to iterate over the entries in a sim_burst linked
  * list and inject them into a time series.  Passing NULL for the response
  * disables it (input time series is strain).
@@ -95,35 +146,8 @@ int XLALBurstInjectSignals(
 		/* construct the h+ and hx time series for the injection
 		 * waveform */
 
-		if(!strcmp(sim_burst->waveform, "BTLWNB")) {
-			/* E_{GW} is in M_{sun}, so we multiply by (M_{sun}
-			 * c^2) to convert to energy, which is then
-			 * multiplied by (4 G / c^3) to convert to a value
-			 * of \int \dot{h}^{2} \diff t. */
-			double int_hdot_squared_dt = sim_burst->egw_over_rsquared * LAL_MSUN_SI * 4 * LAL_G_SI / LAL_C_SI;
-
-			/* the waveform number is interpreted as the seed
-			 * for GSL's Mersenne twister random number
-			 * generator */
-			gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
-			if(!rng)
-				XLAL_ERROR(func, XLAL_ENOMEM);
-			gsl_rng_set(rng, sim_burst->waveform_number);
-
-			if(XLALGenerateBandAndTimeLimitedWhiteNoiseBurst(&injection_hplus, &injection_hcross, sim_burst->duration, sim_burst->frequency, sim_burst->bandwidth, int_hdot_squared_dt, series->deltaT, rng)) {
-				gsl_rng_free(rng);
-				XLAL_ERROR(func, XLAL_EFUNC);
-			}
-			gsl_rng_free(rng);
-		} else if(!strcmp(sim_burst->waveform, "StringCusp")) {
-			if(XLALGenerateStringCusp(&injection_hplus, &injection_hcross, sim_burst->amplitude, sim_burst->frequency, series->deltaT))
-				XLAL_ERROR(func, XLAL_EFUNC);
-		} else if(!strcmp(sim_burst->waveform, "SineGaussian")) {
-			if(XLALSimBurstSineGaussian(&injection_hplus, &injection_hcross, sim_burst->q, sim_burst->frequency, sim_burst->hrss, sim_burst->pol_ellipse_e, sim_burst->pol_ellipse_angle, series->deltaT))
-				XLAL_ERROR(func, XLAL_EFUNC);
-		} else
-			/* unrecognized waveform */
-			XLAL_ERROR(func, XLAL_EINVAL);
+		if(XLALGenerateSimBurst(&injection_hplus, &injection_hcross, sim_burst, series->deltaT))
+			XLAL_ERROR(func, XLAL_EFUNC);
 
 		/* project the wave strain onto the detector's response
 		 * tensor to produce the injection strain as seen in the
