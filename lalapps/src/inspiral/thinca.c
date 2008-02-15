3838a58c9acc3e5842302024757690d8a3b8abb3
dkeppel <dkeppel>
2008-02-15 18:14:45 +0000

    Merging changes made between 1.73.2.4 and 1.73.2.5 on cbc_s5_1yr_20070129 branch onto the head. Original: 0f266bdafa5e3d5d1c142106f1fa2259a3a12e6e

--- lalapps/src/inspiral/thinca.c
+++ lalapps/src/inspiral/thinca.c
@@ -2068,8 +2068,13 @@ int main( int argc, char *argv[] )
               "Kept %d non-H1H2 coincs in slide.\n", numCoincInSlide );
         }
 
-        for (numCoincInSlide = 1, thisCoinc = coincInspiralList; 
-            thisCoinc->next; ++numCoincInSlide, thisCoinc = thisCoinc->next );
+        numCoincInSlide = 0;
+        thisCoinc = coincInspiralList;
+        while( thisCoinc )
+        {
+          ++numCoincInSlide;
+          thisCoinc = thisCoinc->next;
+        }
         
         if ( vrbflg ) fprintf( stdout,
             "%d coincident triggers found in slide.\n", numCoincInSlide );
@@ -2102,7 +2107,7 @@ int main( int argc, char *argv[] )
       LAL_CALL( LALExtractSnglInspiralFromCoinc( &status, &slideOutput, 
             coincInspiralList, &startCoinc, slideNum), &status );
 
-      if ( numSlides ) 
+      if ( numSlides && coincInspiralList ) 
       {
  
         /* the output triggers should be slid back to original time */
