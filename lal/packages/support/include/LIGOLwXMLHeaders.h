1f4a497d4ec56d0b0ec49797cf4a32dd83614169
kipp <kipp>
2008-03-28 06:31:34 +0000

    Oops, too many filter_id columns in the header. Original: 0db1bce232358b97e8cc734e4aa5ea28401c107c

--- lal/packages/support/include/LIGOLwXMLHeaders.h
+++ lal/packages/support/include/LIGOLwXMLHeaders.h
@@ -384,7 +384,6 @@ fputs( "      <Column Name=\"filtergroup:filter:process_id\" Type=\"ilwd:char\"/
 fputs( "      <Column Name=\"filtergroup:filter:program\" Type=\"lstring\"/>\n" , fp ) == EOF || \
 fputs( "      <Column Name=\"filtergroup:filter:start_time\" Type=\"int_4s\"/>\n" , fp ) == EOF || \
 fputs( "      <Column Name=\"filtergroup:filter:filter_name\" Type=\"lstring\"/>\n" , fp ) == EOF || \
-fputs( "      <Column Name=\"filtergroup:filter:filter_id\" Type=\"ilwd:char\"/>\n" , fp ) == EOF || \
 fputs( "      <Column Name=\"filtergroup:filter:comment\" Type=\"lstring\"/>\n" , fp ) == EOF || \
 fputs( "      <Column Name=\"filtergroup:filter:filter_id\" Type=\"ilwd:char\"/>\n" , fp ) == EOF || \
 fputs( "      <Stream Name=\"filtergroup:filter:table\" Type=\"Local\" Delimiter=\",\">\n", fp ) == EOF )
