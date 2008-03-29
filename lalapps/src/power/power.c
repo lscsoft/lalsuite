49cc9bb39a8c402c74c33b63203596a7a41595e8
kipp <kipp>
2008-03-29 01:10:53 +0000

    Remove XLALPrintProgressBar(), now lives in LAL. Original: fcd6372b578fb7d6cd7317a7dfb5dc8c7817344c

--- lalapps/src/power/power.c
+++ lalapps/src/power/power.c
@@ -1411,21 +1411,6 @@ static void output_results(LALStatus *stat, char *file, const char *ifo, Metadat
  */
 
 
-static int XLALPrintProgressBar(double fraction)
-{
-	static const char func[] = "XLALPrintProgressBar";
-	static const char mrk[] = "+++++++++++++++++++++++++++++++++++++++++++++++++)";
-	static const char spc[] = "-------------------------------------------------)";
-	int l = sizeof(mrk)/sizeof(*mrk) - 1;
-	int offset = fraction * l + 0.5;
-
-	if(fraction < 0 || fraction > 1)
-		XLAL_ERROR(func, XLAL_EDOM);
-
-	return XLALPrintInfo("[%s%s %.1f%%", mrk + l - offset, spc + offset, 100.0 * fraction);
-}
-
-
 static void print_progress_bar(const char *func, const LIGOTimeGPS *start, const LIGOTimeGPS *end, const LIGOTimeGPS *t)
 {
 	XLALPrintInfo("%s: ", func);
