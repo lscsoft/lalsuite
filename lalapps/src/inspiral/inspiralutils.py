828f7e1bc1342353d61736af1ff09fdff94fd49e
sfairhur <sfairhur>
2008-04-25 08:55:39 +0000

    merging differences between s5_2yr_lowcbc_20080314 and  s5_2yr_lowcbc_20080425 onto head Original: 14331ed73b08c7a11f67f5b807845341b213e779

--- lalapps/src/inspiral/inspiralutils.py
+++ lalapps/src/inspiral/inspiralutils.py
@@ -241,22 +241,26 @@ def findSegmentsToAnalyze(config,ifo,generate_segments=True,\
     dq_url = config.get("segments","dq-server-url")
     dq_segdb_file = config.get("segments", ifo.lower() + '-dq-file')
 
-    print "Downloading DQ segment file " + dq_segdb_file + " from " \
-        + dq_url + " to " + dqSegFile + " ...",
-    sys.stdout.flush()
-    dqSegFile, info = urllib.urlretrieve(dq_url + '/' + dq_segdb_file,
-        dqSegFile)
-    print "done"
+    if dq_segdb_file == "":
+      print >>sys.stderr, "warning: no file provided to %s-dq-file; " \
+        "running without data quality" % ifo.lower()
+    else:
+      print "Downloading DQ segment file " + dq_segdb_file + " from " \
+          + dq_url + " to " + dqSegFile + " ...",
+      sys.stdout.flush()
+      dqSegFile, info = urllib.urlretrieve(dq_url + '/' + dq_segdb_file,
+          dqSegFile)
+      print "done"
 
-    print "Generating cat 1 veto segments for " + ifo + " ...",
-    sys.stdout.flush()
-    vetoFiles = veto_segments(ifo, config, sciSegFile, dqSegFile, [1], \
-        generate_segments )
-    print "done"
+      print "Generating cat 1 veto segments for " + ifo + " ...",
+      sys.stdout.flush()
+      vetoFiles = veto_segments(ifo, config, sciSegFile, dqSegFile, [1], \
+          generate_segments )
+      print "done"
 
-    # remove cat 1 veto times
-    vetoSegs = segmentsUtils.fromsegwizard(open(vetoFiles[1])).coalesce()
-    sciSegs = sciSegs.__and__(vetoSegs.__invert__())
+      # remove cat 1 veto times
+      vetoSegs = segmentsUtils.fromsegwizard(open(vetoFiles[1])).coalesce()
+      sciSegs = sciSegs.__and__(vetoSegs.__invert__())
 
     if use_available_data:
       dfSegs = datafind_segments(ifo, config)
@@ -607,6 +611,78 @@ def plot_setup(plotDir, config, logPath, stage, injectionSuffix,
 
 
 ##############################################################################
+# Function to set up zero/slide plots:
+def zeroSlidePlots(dag, plotDir, config, logPath, zerolagSuffix, slideSuffix, 
+    cacheFile, doDagCategories, parentDags = None, vetoParentDags = None, 
+    vetoCat = 2):
+  """
+  set up plots for zero lag and time slides
+  dag       = the dag
+  plotDir   = directory in to set up plots
+  config    = config file
+  logPath   = location where log files will be written
+  zerolagSuffix   = the string to restrict to for zero lag
+  slideSuffix     = the string to restrict to for time slides
+  cacheFile       = the input cache file for plotting
+  doCategories    = dag categories turned on
+  parentDags      = the name of the parent dag to add
+  vetoParentDags  = the name of the veto parent dag to add
+  vetoCat         = veto category
+  """
+  # single ifo
+  plotcp = copy.deepcopy(config)
+  plotcp.add_section("plot-arguments")
+  plotcp.set("plot-arguments","plotinspiralrange","")
+  plotcp.set("plot-arguments","plotnumtemplates","")
+  plotcp.set("plot-arguments","plotinspiral","")
+  plotcp.set("plot-arguments","write-script","")
+  playPlotNode = plot_setup(plotDir, plotcp, logPath, "both", \
+      "", zerolagSuffix, slideSuffix, slideSuffix, cacheFile, tag="SNGL" )
+  if doDagCategories:
+    playPlotNode.set_category('plotting')
+  dag.add_node(playPlotNode)
+  if parentDags:
+    for thisDag in parentDags: 
+      playPlotNode.add_parent(thisDag)
+
+  # coincs
+  plotcp = copy.deepcopy(config)
+  plotcp.add_section("plot-arguments")
+  plotcp.set("plot-arguments","plotthinca","")
+  plotcp.set("plot-arguments","write-script","")
+  playPlotCoincNode = plot_setup(plotDir, plotcp, logPath, \
+      "first", "", zerolagSuffix, slideSuffix, slideSuffix, cacheFile, \
+      tag="COINC" )
+  if doDagCategories:
+    playPlotCoincNode.set_category('plotting')
+  dag.add_node(playPlotCoincNode)
+  if parentDags:
+    for thisDag in parentDags: 
+      playPlotCoincNode.add_parent(thisDag)
+  if not doDagCategories:
+    playPlotCoincNode.add_parent(playPlotNode)
+
+  # coincs second (require DQ)
+  vetoString = "_CAT_" + str(vetoCat) + "_VETO"
+  plotcp = copy.deepcopy(config)
+  plotcp.add_section("plot-arguments")
+  plotcp.set("plot-arguments","plotthinca","")
+  plotcp.set("plot-arguments","write-script","")
+  playPlotVetoNode = plot_setup(plotDir, plotcp, logPath, \
+      "second", "", zerolagSuffix + vetoString, slideSuffix + vetoString, \
+      slideSuffix + vetoString, cacheFile, tag="COINC_CAT_" + str(vetoCat) )
+  if doDagCategories:
+    playPlotVetoNode.set_category('plotting')
+  dag.add_node(playPlotVetoNode)
+  if vetoParentDags:
+    for thisDag in vetoParentDags:
+      playPlotVetoNode.add_parent(thisDag)
+  if not doDagCategories:
+    playPlotVetoNode.add_parent(playPlotCoincNode)
+
+  return dag
+
+##############################################################################
 # Function to set up lalapps_followup_pipe
 def followup_setup(followupDir, config, opts, hipeDir):
   """
@@ -677,8 +753,9 @@ def followup_setup(followupDir, config, opts, hipeDir):
   f.close()
 
   # add job to dag
-  followupJob = pipeline.CondorDAGManJob(followupDag,followupDir)
+  followupJob = pipeline.CondorDAGManJob(followupDag, followupDir)
   followupNode = pipeline.CondorDAGNode(followupJob)
+
 
   # write the pre-script to run lalapps_followup_pipe at the appropriate time
   f = open(followupDag + ".pre","w")
@@ -710,7 +787,8 @@ def fix_rescue(dagNode):
     os.symlink("../rescue.sh", "rescue.sh")
   dagNode.set_post_script( "rescue.sh")
   dagNode.add_post_script_arg( "$RETURN" )
-  dagNode.add_post_script_arg( dagNode.job().get_sub_file().rstrip(".condor.sub") )
+  dagNode.add_post_script_arg(
+      dagNode.job().get_sub_file().rstrip(".condor.sub") )
 
 def write_rescue():
   # Write the rescue post-script
