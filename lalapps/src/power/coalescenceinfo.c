bf539efc8130809a7d3275814941887e2ee2b113
kipp <kipp>
2008-04-25 19:53:07 +0000

    Move all code from lal/packages/tools/src/BurstUtils.c here, the only place where it's used. Original: 1ab1bc234009837ed8da0e4e4f42f8ee14bca1b9

--- lalapps/src/power/coalescenceinfo.c
+++ lalapps/src/power/coalescenceinfo.c
@@ -1,36 +1,37 @@
 /*
-*  Copyright (C) 2007 Saikat Ray-Majumder
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with with program; see the file COPYING. If not, write to the
-*  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-*  MA  02111-1307  USA
-*/
+ * Copyright (C) 2007 Saikat Ray-Majumder
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with with program; see the file COPYING. If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ */
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <lal/LALStdio.h>
 #include <lal/LALStdlib.h>
 #include <lal/Date.h>
 #include <lal/LIGOLwXML.h>
 #include <lal/LIGOLwXMLRead.h>
+#include <lal/LIGOMetadataTables.h>
 #include <lal/LIGOMetadataUtils.h>
 #include <lal/FrameCache.h>
 #include <lal/LALInspiral.h>
 #include <lalapps.h>
-#include <lal/BurstUtils.h>
 
 
 RCSID("$Id$");
@@ -45,6 +46,60 @@ RCSID("$Id$");
 #define TRUE  1
 #define FALSE 0
 #define MSUNINSEC ( 4.9255*1e-6 )  /* solar mass in sec */
+
+
+
+static REAL4
+XLALMergerFrequency(
+	InspiralTemplate *params
+)
+{
+	REAL4 fmerger;
+    
+	fmerger = 205 * (20/params->totalMass);
+
+	return(fmerger);
+}
+
+
+static REAL4
+XLALQnrFrequency(
+	InspiralTemplate *params
+)
+{
+	REAL4 fqnr;
+    
+	fqnr = 1320 * (20/params->totalMass);
+
+	return(fqnr);
+}
+
+
+static REAL4
+XLALMergerEnergyFraction(
+	InspiralTemplate *params
+)
+{
+	REAL4 e_merger = 0.0;
+    
+	e_merger = 0.1 * 16 * params->mu * params->mu /params->totalMass;
+
+	return(e_merger);
+}
+
+
+static void 
+XLALMergerDuration(
+	InspiralTemplate *params,
+	MergerDurationLimits *dur
+)
+{
+	dur->high = 50 * params->totalMass;
+	dur->low = 10 * params->totalMass;
+}
+
+
+
 
 /*
  * Command-line options
