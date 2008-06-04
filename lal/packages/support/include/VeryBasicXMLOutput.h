28ed28385dd0a5b04c83f2f7b1f4f448fb4e6421
kwwette <kwwette>
2008-06-04 15:07:13 +0000

    Functions to do basic XML output for debugging, test programs, etc Original: 4da228935fa9d72756e7c84d568d20d5e4bbadd9

---
+++ lal/packages/support/include/VeryBasicXMLOutput.h
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (C) 2008 Karl Wette
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+/**
+ * \author Karl Wette
+ * \file
+ * \brief Some very basic XML output routines, might
+          be useful for debugging or test programs
+ */
+
+#ifndef _VERYBASICXMLOUTPUT_H
+#define _VERYBASICXMLOUTPUT_H
+
+#include <stdio.h>
+
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+
+#include <lal/LALRCSID.h>
+#include <lal/LALDatatypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  NRCSID(VERYBASICXMLOUTPUTH, "$Id$");
+
+  /**
+   * Information for XML functions
+   */
+  typedef struct tagVeryBasicXMLOutput {
+
+    /* XML file */
+    FILE *file;
+
+    /* Indent level */
+    INT4 indent;
+
+  } VeryBasicXMLOutput;
+  extern const VeryBasicXMLOutput empty_VeryBasicXMLOutput;
+
+  /**
+   * Functions
+   */
+  void XLAL_VBXMLO_Header(VeryBasicXMLOutput*, INT4, INT4);
+  void XLAL_VBXMLO_Indent(VeryBasicXMLOutput*);
+  void XLAL_VBXMLO_BeginTag(VeryBasicXMLOutput*, const char*, const char*, ...);
+  void XLAL_VBXMLO_EndTag(VeryBasicXMLOutput*, const char*);
+  void XLAL_VBXMLO_Tag(VeryBasicXMLOutput*, const char*, const char*, const char*, ...);
+  void XLAL_VBXMLO_Printf(VeryBasicXMLOutput*, const char*, ...);
+  void XLAL_VBXMLO_gsl_vector(VeryBasicXMLOutput*, gsl_vector*, const char*, const char*, ...);
+  void XLAL_VBXMLO_gsl_vector_int(VeryBasicXMLOutput*, gsl_vector_int*, const char*, const char*, ...);
+  void XLAL_VBXMLO_gsl_matrix(VeryBasicXMLOutput*, gsl_matrix*, const char*, const char*, ...);
+  void XLAL_VBXMLO_gsl_matrix_Trans(VeryBasicXMLOutput*, gsl_matrix*, const char*, const char*, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lal/packages/support/src/VeryBasicXMLOutput.c
@@ -0,0 +1,296 @@
+/*
+ *  Copyright (C) 2008 Karl Wette
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+/**
+ * \author Karl Wette
+ * \file
+ * \brief Some very basic XML output routines, might
+          be useful for debugging or test programs 
+*/
+
+#include "VeryBasicXMLOutput.h"
+
+#include <stdarg.h>
+
+NRCSID(VERYBASICXMLOUTPUTC, "$Id");
+
+const VeryBasicXMLOutput empty_VeryBasicXMLOutput = {NULL, 0};
+
+/**
+ * Write XML header
+ */
+void XLAL_VBXMLO_Header(
+			VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			INT4 major,              /**< [in] Major revision */
+			INT4 minor               /**< [in] Minor revision */
+			)
+{
+
+  if (!xml->file) 
+    return;
+
+  /* Print header */
+  fprintf(xml->file, "<?xml version=\"%i.%i\"?>\n", major, minor);
+
+  /* Set indent */
+  xml->indent = 0;
+  
+}
+
+/**
+ * Print the appropriate indent
+ */
+void XLAL_VBXMLO_Indent(
+			VeryBasicXMLOutput *xml /**< [in] XML structure */
+			)
+{
+
+  int i;
+
+  if (!xml->file) 
+    return;
+
+  for (i = 0; i < xml->indent; ++i)
+    fprintf(xml->file, "  ");
+
+}  
+
+/**
+ * Begin an XML tag
+ */
+static void VBXMLO_BeginTag(
+			    VeryBasicXMLOutput *xml,
+			    const char *name,
+			    const char *attributes,
+			    va_list va
+			    )
+{
+  
+  if (!xml->file) 
+    return;
+
+  XLAL_VBXMLO_Indent(xml);
+  fprintf(xml->file, "<%s", name);
+  if (attributes) {
+    fprintf(xml->file, " ");
+    vfprintf(xml->file, attributes, va);
+  }
+  fprintf(xml->file, ">\n");
+
+  ++xml->indent;
+
+}
+void XLAL_VBXMLO_BeginTag(
+			  VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			  const char *name,        /**< [in] Tag name */
+			  const char *attributes,  /**< [in] Optional attribute format */
+			  ...                      /**< [in] Attribute arguments */
+			  )
+{
+
+  va_list va;
+  
+  if (!xml->file) 
+    return;
+
+  va_start(va, attributes);
+  VBXMLO_BeginTag(xml, name, attributes, va);
+  va_end(va);
+
+}
+
+/**
+ * End an XML tag
+ */
+void XLAL_VBXMLO_EndTag(
+			VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			const char *name         /**< [in] Tag name */
+			)
+{
+
+  if (!xml->file) 
+    return;
+
+  --xml->indent;
+  XLAL_VBXMLO_Indent(xml);
+  fprintf(xml->file, "</%s>\n", name);
+
+}
+
+/**
+ * Print a complete XML tag
+ */
+void XLAL_VBXMLO_Tag(
+		     VeryBasicXMLOutput *xml, /**< [in] XML structure */
+		     const char *name,        /**< [in] Tag name */
+		     const char *attributes,  /**< [in] Optional attribute format */
+		     const char *format,      /**< [in] Contents format */
+		     ...                      /**< [in] Attribute and contents arguments */
+		     )
+{
+
+  va_list va;
+  
+  if (!xml->file) 
+    return;
+
+  va_start(va, format);
+  VBXMLO_BeginTag(xml, name, attributes, va);
+  XLAL_VBXMLO_Indent(xml);
+  vfprintf(xml->file, format, va);
+  va_end(va);
+  fprintf(xml->file, "\n");
+  XLAL_VBXMLO_EndTag(xml, name);  
+
+}
+
+/**
+ * Print a formatted string
+ */
+void XLAL_VBXMLO_Printf(
+			VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			const char *format,      /**< [in] Format */
+			...                      /**< [in] Arguments */
+			)
+{
+
+  va_list va;
+
+  if (!xml->file) 
+    return;
+
+  va_start(va, format);
+  vfprintf(xml->file, format, va);
+  va_end(va);
+
+}
+
+/**
+ * Print a gsl_vector
+ */
+void XLAL_VBXMLO_gsl_vector(
+			    VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			    gsl_vector *v,           /**< [in] Vector */
+			    const char *name,        /**< [in] Tag name */
+			    const char *attributes,  /**< [in] Optional attribute format */
+			    ...                      /**< [in] Attribute arguments */
+			    )
+{
+
+  va_list va;
+  size_t i;
+
+  if (!xml->file) 
+    return;
+
+  va_start(va, attributes);
+  XLAL_VBXMLO_BeginTag(xml, name, attributes, va);
+  va_end(va);
+  XLAL_VBXMLO_Indent(xml);
+  for (i = 0; i < v->size; ++i)
+    fprintf(xml->file, "% 0.18g ", gsl_vector_get(v, i));
+  fprintf(xml->file, "\n");
+  XLAL_VBXMLO_EndTag(xml, name);
+
+}
+
+/**
+ * Print a gsl_vector_int
+ */
+void XLAL_VBXMLO_gsl_vector_int(
+				VeryBasicXMLOutput *xml, /**< [in] XML structure */
+				gsl_vector_int *v,       /**< [in] Vector */
+				const char *name,        /**< [in] Tag name */
+				const char *attributes,  /**< [in] Optional attribute format */
+				...                      /**< [in] Attribute arguments */
+				)
+{
+  
+  va_list va;
+  size_t i;
+  
+  if (!xml->file) 
+    return;
+
+  va_start(va, attributes);
+  XLAL_VBXMLO_BeginTag(xml, name, attributes, va);
+  va_end(va);
+  XLAL_VBXMLO_Indent(xml);
+  for (i = 0; i < v->size; ++i)
+    fprintf(xml->file, "% i ", gsl_vector_int_get(v, i));
+  fprintf(xml->file, "\n");
+  XLAL_VBXMLO_EndTag(xml, name);
+
+}
+
+/**
+ * Print a gsl_matrix by rows
+ */
+void XLAL_VBXMLO_gsl_matrix(
+			    VeryBasicXMLOutput *xml, /**< [in] XML structure */
+			    gsl_matrix *m,           /**< [in] Matrix */
+			    const char *name,        /**< [in] Tag name */
+			    const char *attributes,  /**< [in] Optional attribute format */
+			    ...                      /**< [in] Attribute arguments */
+			    )
+{
+  va_list va;
+  size_t i;
+
+  if (!xml->file) 
+    return;
+
+  va_start(va, attributes);
+  XLAL_VBXMLO_BeginTag(xml, name, attributes, va);
+  va_end(va);
+  for (i = 0; i < m->size1; ++i) {
+    gsl_vector_view row = gsl_matrix_row(m, i);
+    XLAL_VBXMLO_gsl_vector(xml, &row.vector, "row", NULL);
+  }
+  XLAL_VBXMLO_EndTag(xml, name);
+
+}
+
+/**
+ * Print a gsl_matrix by columns
+ */
+void XLAL_VBXMLO_gsl_matrix_Trans(
+				  VeryBasicXMLOutput *xml, /**< [in] XML structure */
+				  gsl_matrix *m,           /**< [in] Matrix */
+				  const char *name,        /**< [in] Tag name */
+				  const char *attributes,  /**< [in] Optional attribute format */
+				  ...                      /**< [in] Attribute arguments */
+				  )
+{
+  va_list va;
+  size_t j;
+  
+  if (!xml->file) 
+    return;
+  
+  va_start(va, attributes);
+  XLAL_VBXMLO_BeginTag(xml, name, attributes, va);
+  va_end(va);
+  for (j = 0; j < m->size2; ++j) {
+    gsl_vector_view column = gsl_matrix_column(m, j);
+    XLAL_VBXMLO_gsl_vector(xml, &column.vector, "column", NULL);
+  }
+  XLAL_VBXMLO_EndTag(xml, name);
+
+}
