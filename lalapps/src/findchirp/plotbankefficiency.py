1bb59d5a69204c4d15a38e296a17e60494952a54
thomas <thomas>
2008-07-07 17:00:33 +0000

    improvments Original: 2aa9bd3d3c9b4e875fd15338c0a7bda87f146a12

--- lalapps/src/findchirp/plotbankefficiency.py
+++ lalapps/src/findchirp/plotbankefficiency.py
@@ -7,16 +7,27 @@ __name__ = "plotbankefficiency"
 __title__ = "Figure of merits for BankEfficiency results."
   
 import getopt, sys, os, re, glob, exceptions, dircache, string
-from types import *
 from optparse import *
 from matplotlib.ticker import FormatStrFormatter
 from numpy import *
 from pylab import *
-from matplotlib import *
+from matplotlib import rc
 from inspiraltools  import *
 
-#import scipy
+#this is for the colorbar labelsize. Do not know why it works but it works!
+rc('ytick',labelsize=15)
 
+
+def setTickSize(fontsize):
+  """
+  An alias fonction to set the fontsize of the ticks 
+  """
+  ax = gca()
+  for tick in ax.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(fontsize)
+  for tick in ax.yaxis.get_major_ticks():
+    tick.label1.set_fontsize(fontsize)
+    
 
 def fastoptionRelated(results):
   if opts.verbose:
@@ -38,8 +49,8 @@ def fastoptionRelated(results):
     name = __name__+'_scatter_ecc_sim_totalmass_sim_fastoption_ratio'
     plotting.scatter(results['ecc_sim'],results['totalmass_sim'],\
       results['nfast']/(results['nfast_max']+1), markersize=30, alpha=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'TotalMass ($M_\odot$)')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'TotalMass ($M_\odot$)',fontsize=opts.fontsize)
     title('Ratio between the total number of filter and the number of the filter that gives the best SNR')
     mysavefig(opts, name+'.png')
   except:pass
@@ -49,15 +60,15 @@ def fastoptionRelated(results):
     name = __name__+'_scatter_ecc_sim_totalmass_sim'
     plotting.scatter(results['ecc_sim'],results['totalmass_sim'],\
       results['nfast_max'], markersize=30, alpha=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'TotalMass ($M_\odot$)')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'TotalMass ($M_\odot$)',fontsize=opts.fontsize)
     mysavefig(opts, name+'_nfast_max.png')
   except:pass
   try:
     plotting.scatter(results['ecc_sim'],results['totalmass_sim'],\
       results['nfast'], markersize=30, alpha=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'TotalMass ($M_\odot$)')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'TotalMass ($M_\odot$)',fontsize=opts.fontsize)
     mysavefig(opts, name+'_nfast.png')
   except:pass
   
@@ -72,8 +83,8 @@ def EMatchRelated(results):
   try:
     clf()
     plotting.plot(results['ematch'],results['snr'])
-    xlabel('EMatch', size='x-large')
-    ylabel(r'Overlap (\%)', size='x-large')
+    xlabel('EMatch',fontsize=opts.fontsize)
+    ylabel(r'Overlap (\%)', fontsize=opts.fontsize)
     plotting.hold = True
     plotting.plot([min(results['ematch']), max(results['ematch'])],[ematch,ematch], marker='r',linewidth=2)
     mysavefig(opts,name + '.png')
@@ -93,8 +104,8 @@ def accuraciesRelated(results):
     try:
       plotting.plot(results[param]-results[param+'_sim'], results['snr'],  \
               markersize=10, marker='o', linewidth=0)
-      xlabel(r'$\Delta$  ' + param)
-      ylabel(r'$\rho$')
+      xlabel(r'$\Delta$  ' + param,fontsize=opts.fontsize)
+      ylabel(r'$\rho$',fontsize=opts.fontsize)
       mysavefig(opts, name+'.png')
     except:
       print 'Problem inside plot '+name
@@ -113,8 +124,8 @@ def HistogramRelated(results):
     name = __name__+'_hist_'+this
     try:
       plotting.plot_histogram_and_fit(results[this],100,fit=False)
-      xlabel(label, size='x-large')
-      ylabel(r'\#', size='x-large')
+      xlabel(label, fontsize=opts.fontsize)
+      ylabel(r'\#', fontsize=opts.fontsize)
       mysavefig(opts,name+'.png')
     except:
       print 'Problem inside plot_histogram_and_fit' + name
@@ -138,8 +149,8 @@ def BankRelated(results, bank):
   except:
     pass
   plotting.hold = False
-  xlabel(r'$\tau_0$ (seconds) ', size='x-large')
-  ylabel(r'$\tau_3$ (seconds)', size='x-large')
+  xlabel(r'$\tau_0$ (seconds) ', fontsize=opts.fontsize)
+  ylabel(r'$\tau_3$ (seconds)', fontsize=opts.fontsize)
   legend(['Simulated injection','template''s position'])
   title('Position of the injections')
   mysavefig(opts,'plotbankefficiency_bank.png')
@@ -157,8 +168,8 @@ def SNRRelated(results):
     name = __name__+'_snr_versus_'+param
     try:
       plotting.plot(results[param],results['snr'])
-      xlabel(r'Injected '+label, size='x-large')
-      ylabel(r'Overlap (\%)', size='x-large')
+      xlabel(r'Injected '+label,fontsize=opts.fontsize)
+      ylabel(r'Overlap (\%)',fontsize=opts.fontsize)
       mysavefig(opts, name + '.png')
     except:
       print 'Problem inside plotting.plot '+ name
@@ -172,8 +183,8 @@ def comparisonRelated(results, results2):
   try:
     clf()
     plotting.plot(results['totalmass_sim'], results['snr']/results2['snr'])
-    xlabel('total mass')
-    ylabel('SNR in file 1 /  SNR in file 2')
+    xlabel('total mass',fontsize=opts.fontsize)
+    ylabel('SNR in file 1 /  SNR in file 2',fontsize=opts.fontsize)
     mysavefig(opts, 'plotbankefficiency_compare_totalmass_versus_snr_ratio.png')
   except: 
     print 'Problem in comparisonRelated. Maybe the two files do not have the same size.'
@@ -184,8 +195,8 @@ def comparisonRelated(results, results2):
     plotting.hold = True
     plotting.plot(results2['totalmass_sim'], results2['snr'], marker='rx')
     plotting.hold = False
-    xlabel('total mass')
-    ylabel('SNR in file 1 /  SNR in file 2')
+    xlabel('total mass',fontsize=opts.fontsize)
+    ylabel('SNR in file 1 /  SNR in file 2',fontsize=opts.fontsize)
     mysavefig(opts, 'plotbankefficiency_compare_totalmass_versus_snr.png')
   except: 
     print 'Problem in comparisonRelated. Maybe the two files do not have the same size.'
@@ -197,8 +208,8 @@ def bcvRelated(results):
     print 'BCV related plotting ---------------------------------------------- '
   try:
     plotting.plot(results['totalmass_sim'],results['alpha_f'])
-    xlabel('total mass')
-    ylabel('alpha\_F')
+    xlabel('total mass',fontsize=opts.fontsize)
+    ylabel('alpha\_F',fontsize=opts.fontsize)
     mysavefig(opts, 'plotbankefficiency_alphaf_vs_totalmass.png')
   except:pass
 
@@ -210,13 +221,18 @@ def eccentricityRelated(results, results2, nbin=40):
 
   totMass = results['totalmass_sim']
   snr = results['snr'] 
-  
+  ecc   = xml.getvalue('--signal-eccentricity-range')
+  eccRange = [float(x) for x in ecc.split(" ")]
+  eccMin = eccRange[0]
+  eccMax = eccRange[1]*1.01
+ 
   # Scatter plots of the eccentricity versus SNR and total mass
   name = __name__ + '_scatter_snr_versus_totalmass_ecc'
   try:
-    plotting.scatter(results['ecc_sim'],totMass, snr,  markersize=30, alpha=1,vmin=0,vmax=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'TotalMass ($M_\odot$)')
+    plotting.scatter(results['ecc_sim'],totMass, snr,  markersize=30, alpha=1,\
+                     vmin=0,vmax=1)
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'TotalMass ($M_\odot$)',fontsize=opts.fontsize)
     mysavefig(opts,name+'.png')
   except:
     print 'Problem inside scatter1 ' +name
@@ -229,8 +245,8 @@ def eccentricityRelated(results, results2, nbin=40):
     try:
       plotting.scatter(results['ecc_sim'],totMass, snr/snr2,  \
           markersize=30, alpha=1,vmin=0.8,vmax=1.2)
-      xlabel(r'Eccentricity')
-      ylabel(r'TotalMass ($M_\odot$)')
+      xlabel(r'Eccentricity',fontsize=opts.fontsize)
+      ylabel(r'TotalMass ($M_\odot$)',fontsize=opts.fontsize)
       mysavefig(opts, name+'.png')
     except:
       print 'Problem inside scatter2 ' +name,
@@ -254,8 +270,8 @@ def eccentricityRelated(results, results2, nbin=40):
   name = __name__+'_snr_versus_ecc'
   try:
     plotting.plot(results['ecc_sim'],snr,'ob')
-    xlabel(r'Eccentricity')
-    ylabel(r'Overlap(\%)')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'Overlap(\%)',fontsize=opts.fontsize)
     #pylab.axis([0,0.4,0.7,1])
     mysavefig(opts,name+'.png')
   except:
@@ -271,8 +287,8 @@ def eccentricityRelated(results, results2, nbin=40):
   try:
     plotting.surf(results['ecc_sim'], totMass, snr,\
                   xbin=nbin,ybin=nbin,vmin=0.5,vmax=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'Total mass $(M_\odot)$')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'Total mass $(M_\odot)$',fontsize=opts.fontsize)
     mysavefig(opts,name+'.png')
   except:
     print 'Problem inside surf ' + name
@@ -283,22 +299,28 @@ def eccentricityRelated(results, results2, nbin=40):
   for ecc in ['ecc_sim','ecc_sim_fl']:
     for this_param,label in zip(['totalmass_sim','chirpmass_sim'],\
         [r'Total mass $(M_\odot)$',r'$\mathcal{M} (M_\odot)$']):
-      try:
+#      try:
         name = __name__ + '_contour_snr_versus_'+this_param+'_'+ecc
-        plotting.contourf(results[ecc], results[this_param], snr, \
-            xbin=nbin,ybin=nbin, xmin=0, xmax=0.4, vmin=0.5,vmax=1)
-        xlabel(r'Eccentricity')
-        ylabel(label)
+        h = plotting.contourf(results[ecc], results[this_param], snr, \
+            xbin=nbin,ybin=nbin, xmin=eccMin, xmax=eccMax, vmin=0.5,vmax=1,\
+            colorbar_title=r'Overlap(\%)',fontsize=opts.fontsize)
+        xlabel(r'Eccentricity',fontsize=opts.fontsize)
+        ylabel(label,fontsize=opts.fontsize)
+        setTickSize(opts.fontsize/1.2)
+        
         mysavefig(opts,name +'.png')
         
         name = __name__ + '_contour_detectability_versus_'+this_param+'_'+ecc
-        plotting.contourf(results[ecc], results[this_param], (snr)**(3.),\
-                      xbin=nbin, ybin=nbin, vmin=0.5,vmax=1,colormap='cube')
-        xlabel(r'Eccentricity')
-        ylabel(label)
+        h = plotting.contourf(results[ecc], results[this_param], (snr)**(3.),\
+                      xbin=nbin, ybin=nbin, xmin=eccMin, xmax=eccMax, vmin=0.5,vmax=1,colormap='cube',\
+                      colorbar_title=r'Overlap(\%)',fontsize=opts.fontsize)
+        xlabel(r'Eccentricity',fontsize=opts.fontsize)
+        ylabel(label,fontsize=opts.fontsize)
+        setTickSize(opts.fontsize/1.2)
+        
         mysavefig(opts,name +'.png')        
-      except: 
-        pass
+ #     except: 
+ #       pass
   
     
   
@@ -320,8 +342,8 @@ def eccentricityRelated(results, results2, nbin=40):
     clf()
     plotting.scatter(results['ecc_sim'],results['totalmass_sim'],\
                      results['tau3_sim']/results['tau3'],markersize=30, alpha=1)
-    xlabel(r'Eccentricity')
-    ylabel(r'Total mass $(M_\odot)$')
+    xlabel(r'Eccentricity',fontsize=opts.fontsize)
+    ylabel(r'Total mass $(M_\odot)$',fontsize=opts.fontsize)
     title(r'Simulated $\tau_3$ divided by estimated $\tau_3$')
     mysavefig(opts,name+'.png')  
   except:
@@ -336,7 +358,7 @@ def mysavefig(opts, title):
   newtitle = title.replace('plotbankefficiency', \
                         'plotbankefficiency_'+opts.user_tag)
   if opts.verbose:
-    print '---saving picture ' + newtitle
+    print 'saving picture ' + newtitle
   
   savefig(newtitle)
 ##############################################################################
@@ -384,6 +406,8 @@ def ParseParameters():
       default=False, help="skip plots related to histograms")
   parser.add_option("--nbin",action="store",\
       default=40, help="set binning of the contour plots")
+  parser.add_option("--fontsize",action="store",type="float",\
+      default=20, help="set fontsize of the labels")
   
   
   
@@ -393,86 +417,88 @@ def ParseParameters():
   return opts,args
   
   
+
+if __name__ == 'plotbankefficiency': 
   
+  command_line = sys.argv[1:]
+  (opts,args) = ParseParameters()
+
+  # Change to Agg back-end if show() will not be called
+  if not opts.show_plot:
+    import matplotlib
+    matplotlib.use('Agg')
+  from pylab import *
+
+  rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+  rc('text', usetex=True)
   
-command_line = sys.argv[1:]
-(opts,args) = ParseParameters()
-
-# Change to Agg back-end if show() will not be called
-if not opts.show_plot:
-  import matplotlib
-  matplotlib.use('Agg')
-from pylab import *
-
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-rc('text', usetex=True)
-
-# -------------------------------------------------------- THE DATA
-# Read the XML file
-xml = ReadXMLFiles(opts)
-# Read the tables (bankefficiency, sngl-inspiral, process_params)
-results, bank, params,values = xml.getTables()
-# extract some parameters
-signal   = xml.getvalue("--signal ")
-template = xml.getvalue("--template ")
-ematch   = xml.getvalue('--e-match')
-
+  # -------------------------------------------------------- THE DATA
+  # Read the XML file
+  xml = ReadXMLFiles(opts)
+  # Read the tables (bankefficiency, sngl-inspiral, process_params)
+  results, bank, params,values = xml.getTables()
+  # extract some parameters
+  signal   = xml.getvalue("--signal ")
+  template = xml.getvalue("--template ")
+  ematch   = xml.getvalue('--e-match')
+  ecc   = xml.getvalue('--signal-eccentricity-range')
+  eccRange = [float(x) for x in ecc.split(" ")]
 # ---------------------------------------------------------- THE FILE TO COMPARE
-if opts.compare_with is not None:
-  opts.glob = opts.compare_with
-  xml2      = ReadXMLFiles(opts)
-  results2,bank2,param2,values2 = xml2.getTables()
-else: 
-  results2 = None
+  if opts.compare_with is not None:
+    opts.glob = opts.compare_with
+    xml2      = ReadXMLFiles(opts)
+    results2,bank2,param2,values2 = xml2.getTables()
+  else: 
+    results2 = None
 
-# ----------------------------------------------------------- PLOTTING
+  # ----------------------------------------------------------- PLOTTING
 
-plotting = Plotting(opts.verbose)
-plotting.settitle(signal +' injections')
-#------------------------------------------------------ overlap versus parameter
+  plotting = Plotting(opts.verbose)
+  plotting.settitle(signal +' injections')
+  #------------------------------------------------------ overlap versus parameter
 
-if opts.skip_snr is False:
-  try: SNRRelated(results)
-  except: print 'Problem in SNRRelated)'
+  if opts.skip_snr is False:
+    try: SNRRelated(results)
+    except: print 'Problem in SNRRelated)'
 
-if opts.skip_bank is False:
-  try: BankRelated(results,bank)
-  except: print 'Problem in BankRelated'
+  if opts.skip_bank is False:
+    try: BankRelated(results,bank)
+    except: print 'Problem in BankRelated'
   
-if opts.skip_histogram is False:  
-  try: HistogramRelated(results)
-  except: print 'Problem in HistogramRelated'
+  if opts.skip_histogram is False:  
+    try: HistogramRelated(results)
+    except: print 'Problem in HistogramRelated'
 
-# --------------------------------------------------- specific to  Eccentricity
-if opts.skip_eccentricity is False:
-  if signal=='Eccentricity':
-    try: eccentricityRelated(results,results2,nbin=opts.nbin)
-    except:print 'Problem in eccentricityRelated'
-# --------------------------------------------------- specific to  BCV
-if opts.skip_bcv is False:
-  if template=='BCV':
-    bcvRelated(results)
+  if opts.skip_eccentricity is False:
+    if signal=='Eccentricity':
+    #try:
+      eccentricityRelated(results,results2,nbin=opts.nbin)
+    #except:print 'Problem in eccentricityRelated'
+
+  if opts.skip_bcv is False:
+    if template=='BCV':
+      bcvRelated(results)
 
 # --------------------------------------------------- compare-with a second file
-if results2 is not None:
-  try: comparisonRelated(results, results2)
-  except: print 'Problem in comparison related'
+  if results2 is not None:
+    try: comparisonRelated(results, results2)
+    except: print 'Problem in comparison related'
   
-if opts.skip_accuracy is False:
-  try: accuraciesRelated(results)
-  except: print 'Problem in accuraciesRelated'
+  if opts.skip_accuracy is False:
+    try: accuraciesRelated(results)
+    except: print 'Problem in accuraciesRelated'
 
-if opts.skip_fast is False:
-  try: fastoptionRelated(results)
-  except: print 'Problem in fast option related'
+  if opts.skip_fast is False:
+    try: fastoptionRelated(results)
+    except: print 'Problem in fast option related'
 
-if opts.skip_ematch is False:
-  try: EMatchRelated(results)
-  except:print 'Problem in EMatchRelated'
+  if opts.skip_ematch is False:
+    try: EMatchRelated(results)
+    except:print 'Problem in EMatchRelated'
 
 
-if opts.show_plot:
-  show()
+  if opts.show_plot:
+    show()
 
 
 
