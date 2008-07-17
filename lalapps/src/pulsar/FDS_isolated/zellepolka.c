46f91461f7a074426d040d7de4160cddb7c85160
hpletsch <hpletsch>
2008-07-17 20:59:24 +0000

    added simpler E@H data seg id's Original: 379216e0ef2b72049caa352081764ed47afa5708

--- lalapps/src/pulsar/FDS_isolated/zellepolka.c
+++ lalapps/src/pulsar/FDS_isolated/zellepolka.c
@@ -3357,14 +3357,43 @@ void print_info_of_cell_and_ifo_S5R1a( LALStatus *lalStatus,
 	  case 5790:
 	  case 5949:
 	  case 5768:
+	  case 0:
+          case 1:
+          case 2:
+          case 3:
+          case 4:
+          case 5:
+          case 6:
+          case 7:
+          case 8:
+          case 9:
+          case 10:
+          case 11:
+          case 12:
+          case 13:
+          case 14:
+          case 15:
+          case 16:
+          case 17:
+          case 18:
+          case 19:
+          case 20:
+          case 21:
 	    cH1++;
             break;
+
 	  case 6284:
 	  case 5843:
 	  case 6500:
 	  case 5925:
 	  case 6744:
 	  case 5835:
+          case 22:
+          case 23:
+          case 24:
+          case 25:
+          case 26:
+          case 27:
             cL1++;
             break;
           }
