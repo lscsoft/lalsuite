9eeafed3ef73ac80c575545aaf33a1022ee87e9b
shinkee <shinkee>
2008-08-22 21:42:35 +0000

    Adding the Cuda Complex FFT plan structure Original: a2d14347057f8f824f07c3b909c24a6f2b78e4ea

---
+++ lal/packages/fft/include/CudaPlan.h
@@ -0,0 +1,29 @@
+#ifndef _CUDAPLAN_H
+#define _CUDAPLAN_H
+
+#include <lal/LALDatatypes.h>
+#include <fftw3.h>
+#include <cufft.h>
+
+/* This is only included by CudaComplexFFT.c
+ * or any other file that needs to access the
+ * FFT plan structure.
+ */
+
+struct tagCOMPLEX8FFTPlan
+{
+  INT4       sign;
+  UINT4      size;
+  cufftHandle plan;
+  COMPLEX8  *d_input;
+  COMPLEX8  *d_output;
+};
+
+struct tagCOMPLEX16FFTPlan
+{
+  INT4       sign;
+  UINT4      size;
+  fftw_plan  plan;
+};
+
+#endif	/* _CUDAPLAN_H */
