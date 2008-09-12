28600ab488ac30911b53de11bc56fcbbefef3cbf
mpitkin <mpitkin>
2008-09-12 12:52:25 +0000

    Versions that work Original: 7e30fe23370eadd8c44cc5380748947d6241a46e

--- lalapps/src/pulsar/TDS_isolated/pulsar_parameter_mismatch.py
+++ lalapps/src/pulsar/TDS_isolated/pulsar_parameter_mismatch.py
@@ -23,7 +23,7 @@ class mismatchJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
   """
   def __init__(self,cp):
     # cp = ConfigParser object from which options are read.
-    self.__executable = cp.get('exec', 'parameter')#gets the executable from.ini
+    self.__executable = cp.get('exec', 'executable')#gets the executable from.ini
     self.__universe = cp.get('condor', 'universe')
     pipeline.CondorDAGJob.__init__(self, self.__universe, self.__executable)
     pipeline.AnalysisJob.__init__(self, cp)
@@ -53,12 +53,29 @@ class mismatchNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     self.__param_file = None
     self.__output_dir = None
     self.__pulsar = None
-    self.__input_dir = None
     self.__iterations = None
     self.__earthephem = None
     self.__sunephem = None
     self.__covfile = None
-     
+    self.__deltat = None
+    self.__start = None
+    self.__timespan = None
+
+  def set_deltat(self,deltat):
+    # set time steps
+    self.add_var_opt('deltat',deltat)
+    self.__deltat = deltat
+
+  def set_start(self,start):
+    # set start time
+    self.add_var_opt('start',start)
+    self.__start = start
+
+  def set_timespan(self,timespan):
+    # set time span
+    self.add_var_opt('timespan',timespan)
+    self.__timespan = timespan
+
   def set_output_dir(self,output_dir):
     # set output directory
     self.add_var_opt('output-dir',output_dir)
@@ -74,11 +91,6 @@ class mismatchNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     self.add_var_opt('par-file', param_file)
     self.__param_file = param_file
   
-  def set_input_dir(self, input_dir):
-    # set the input directory
-    self.add_var_opt('input-dir', input_dir)
-    self.__input_dir = input_dir
-
   def set_pulsar(self,pulsar):
     # set pulsar name
     self.add_var_opt('pulsar',pulsar)
@@ -96,10 +108,10 @@ class mismatchNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
 
   def set_earth(self, earthephem):
     # set the earth ephemeris file
-    self.add_var_opt('earth-ephem', earthephem)
+    self.add_var_opt('earth', earthephem)
     self.__earthephem = earthephem
 
   def set_sun(self, sunephem):
     # set the sun ephemeris file
-    self.add_var_opt('sun-ephem', sunephem)
+    self.add_var_opt('sun', sunephem)
     self.__sunephem = sunephem
