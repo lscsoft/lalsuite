48f1b8dde4a5f62ee63737a04cf3b5e7bd409648
mpitkin <mpitkin>
2008-09-12 08:34:59 +0000

    Scripts for creating dag to calculate parameter mismatch Original: 2adb8600baa33f41a03ed657a93a0c5aa81bd2bc

---
+++ lalapps/src/pulsar/TDS_isolated/pulsar_parameter_mismatch.py
@@ -0,0 +1,105 @@
+"""
+  Classes needed for the mismatch pipeline.
+  
+  Matt Pitkin 11/09/08
+  
+  $Id$
+"""
+
+__author__ = 'Matt Pitkin <matthew@astro.gla.ac.uk>'
+__date__ = '$Date$'
+__version__ = '$Revision$'
+
+import string
+import exceptions
+import os
+from glue import pipeline
+
+# And begin ...
+
+class mismatchJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
+  """
+  A lalapps_heterodyne_pulsar job to coarse heterodyne the data.
+  """
+  def __init__(self,cp):
+    # cp = ConfigParser object from which options are read.
+    self.__executable = cp.get('exec', 'parameter')#gets the executable from.ini
+    self.__universe = cp.get('condor', 'universe')
+    pipeline.CondorDAGJob.__init__(self, self.__universe, self.__executable)
+    pipeline.AnalysisJob.__init__(self, cp)
+   
+    self.add_condor_cmd('WantRemoteIO','false')
+    self.add_condor_cmd('local_files','*')
+    
+    # set log files for job
+    self.set_stdout_file('logs/mismatch-$(cluster).out')
+    self.set_stderr_file('logs/mismatch-$(cluster).err')
+    self.set_sub_file('mismatch.sub')
+
+class mismatchNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
+  """
+  A heterodyneNode runs an instance of lalapps_heterodyne_pulsar in coarse heterodyne mode in
+  a condor DAG.
+  """
+  def __init__(self,job):
+    """
+    job = A CondorDAGJob that can run an instance of lalapps_CalculateSensitivity.
+    """
+    pipeline.CondorDAGNode.__init__(self,job)
+    pipeline.AnalysisNode.__init__(self)
+    
+    # initilise job variables
+    self.__detector = None
+    self.__param_file = None
+    self.__output_dir = None
+    self.__pulsar = None
+    self.__input_dir = None
+    self.__iterations = None
+    self.__earthephem = None
+    self.__sunephem = None
+    self.__covfile = None
+     
+  def set_output_dir(self,output_dir):
+    # set output directory
+    self.add_var_opt('output-dir',output_dir)
+    self.__output_dir = output_dir
+    
+  def set_detector(self, detector):
+    # set detectors
+    self.add_var_opt('detector', detector)
+    self.__detector = detector
+    
+  def set_param_file(self, param_file):
+    # set pulsar parameter file
+    self.add_var_opt('par-file', param_file)
+    self.__param_file = param_file
+  
+  def set_input_dir(self, input_dir):
+    # set the input directory
+    self.add_var_opt('input-dir', input_dir)
+    self.__input_dir = input_dir
+
+  def set_pulsar(self,pulsar):
+    # set pulsar name
+    self.add_var_opt('pulsar',pulsar)
+    self.__pulsar = pulsar
+    
+  def set_covfile(self,covfile):
+    # set the covariance matrix file
+    self.add_var_opt('covariance',covfile)
+    self.__covfile = covfile
+
+  def set_iterations(self, iterations):
+    # set the number of MCMC iterations
+    self.add_var_opt('iterations', iterations)
+    self.__iterations = iterations
+
+  def set_earth(self, earthephem):
+    # set the earth ephemeris file
+    self.add_var_opt('earth-ephem', earthephem)
+    self.__earthephem = earthephem
+
+  def set_sun(self, sunephem):
+    # set the sun ephemeris file
+    self.add_var_opt('sun-ephem', sunephem)
+    self.__sunephem = sunephem
