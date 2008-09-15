fa3803676a3e59c5252be544b24fc7bfda87a36f
kwwette <kwwette>
2008-09-15 20:27:47 +0000

    Added script to compute upper limits for Fstat using MC integration of analytic signal Original: c383c0d0acece29020009582a6d10cdc78abbe5d

---
+++ lalapps/src/pulsar/FDS_isolated/ComputeFStatAnalyticMonteCarloUpperLimit.c
@@ -0,0 +1,380 @@
+/*
+ *  Copyright (C) 2008 Karl Wette
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+/**
+ * \author Karl Wette
+ * \file
+ * \brief Computes an upper limit using Monte Carlo integration
+          of the analytic F statistic signal model
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_sf_hyperg.h>
+#include <gsl/gsl_sf_gamma.h>
+
+#include <lal/LALRCSID.h>
+#include <lal/LALStdlib.h>
+#include <lal/LALError.h>
+#include <lal/LogPrintf.h>
+#include <lal/UserInput.h>
+#include <lal/SFTfileIO.h>
+#include <lal/LALInitBarycenter.h>
+#include <lal/DetectorStates.h>
+#include <lal/NormalizeSFTRngMed.h>
+#include <lal/ComputeFstat.h>
+#include <lalapps.h>
+
+RCSID("$Id$");
+
+REAL8 hyperg_0F1_reg(REAL8 c, REAL8 x);
+REAL8 pdf_ncx2_4(REAL8 lambda, REAL8 x);
+REAL8 d_pdf_ncx2_4(REAL8 lambda, REAL8 x);
+
+#define TRUE  (1==1)
+#define FALSE (1==0)
+
+const REAL8 min_cosi = 0;
+const REAL8 max_cosi = 1;
+const REAL8 min_psi = -LAL_PI_4;
+const REAL8 max_psi =  LAL_PI_4;
+
+int main(int argc, char *argv[]) {
+
+  LALStatus status = blank_status;
+  BOOLEAN help = FALSE;
+
+  REAL8 alpha = 0.0;
+  REAL8 delta = 0.0;
+  REAL8 freq = 0.0;
+  REAL8 band = 0.0;
+  REAL8 twoFs = 0.0;
+  CHAR *sft_pattern = NULL;
+  CHAR *ephem_dir = NULL;
+  CHAR *ephem_year = NULL;
+  INT4 rng_med_win = 50;
+  REAL8 max_rel_err = 1.0e-3;
+  REAL8 h0_brake = 0.5;
+  INT4 MC_trials = 10000;
+  REAL8 MC_trial_fac = 2.0;
+  REAL8 FDR = 0.05;
+  CHAR *output_file = NULL;
+
+  FILE *fp = NULL;
+  CHAR *cmdline = NULL;
+  SFTCatalog *catalog = NULL;
+  MultiSFTVector *sfts = NULL;
+  EphemerisData ephemeris = empty_EphemerisData;
+  MultiDetectorStateSeries *detector_states = NULL;
+  MultiPSDVector *rng_med = NULL;
+  MultiNoiseWeights *noise_weights = NULL;
+  MultiAMCoeffs *AM_coeffs = NULL;
+  REAL8 A_coeff = 0.0;
+  REAL8 B_coeff = 0.0;
+  REAL8 C_coeff = 0.0;
+  gsl_rng *rng = NULL;
+  REAL8 MC_int_vol = 0.0;
+  REAL8 h0 = 0.0;
+  REAL8 h0_prev = 0.0;
+  REAL8 dh0 = 0.0;
+  INT4 h0_iter = 0;
+  INT4 MC_iter = 0;
+    
+  /* Initialise LAL error handler, debug level and log level */
+  lal_errhandler = LAL_ERR_EXIT;
+  LAL_CALL(LALGetDebugLevel(&status, argc, argv, 'v'), &status);
+  LogSetLevel(lalDebugLevel);
+  
+  /* Register command line arguments */
+  LAL_CALL(LALRegisterBOOLUserVar  (&status, "help",        'h', UVAR_HELP,     "Print this help message", &help), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "alpha",       'a', UVAR_REQUIRED, "Right ascension in radians", &alpha), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "delta",       'd', UVAR_REQUIRED, "Declination in radians", &delta), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "freq",        'f', UVAR_REQUIRED, "Starting frequency", &freq), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "band",        'b', UVAR_REQUIRED, "Frequency band", &band), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "loudest-2F",  'F', UVAR_REQUIRED, "Loudest 2F value in this band", &twoFs), &status);
+  LAL_CALL(LALRegisterSTRINGUserVar(&status, "sft-patt",    'D', UVAR_REQUIRED, "File pattern of the input SFTs", &sft_pattern), &status);
+  LAL_CALL(LALRegisterSTRINGUserVar(&status, "ephem-dir",   'E', UVAR_OPTIONAL, "Directory containing ephemeris files", &ephem_dir), &status);
+  LAL_CALL(LALRegisterSTRINGUserVar(&status, "ephem-year",  'y', UVAR_REQUIRED, "Year suffix for ephemeris files", &ephem_year), &status);
+  LAL_CALL(LALRegisterINTUserVar   (&status, "rng-med-win", 'k', UVAR_OPTIONAL, "Size of the running median window", &rng_med_win), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "max-rel-err", 'e', UVAR_OPTIONAL, "Maximum error in h0 relative to previous value", &max_rel_err), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "h0-brake",    'b', UVAR_OPTIONAL, "h0 cannot change by more than this fraction of itself", &h0_brake), &status);
+  LAL_CALL(LALRegisterINTUserVar   (&status, "init-MC-tri", 'I', UVAR_OPTIONAL, "Initial number of MC int. trials", &MC_trials), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "MC-tri-incr", 'i', UVAR_OPTIONAL, "Multiply number of MC int. trials by this after each step", &MC_trial_fac), &status);
+  LAL_CALL(LALRegisterREALUserVar  (&status, "false-dism",  'R', UVAR_OPTIONAL, "Target false dismissal rate", &FDR), &status);
+  LAL_CALL(LALRegisterSTRINGUserVar(&status, "output-file", 'o', UVAR_OPTIONAL, "Output file for the upper limit and other info (defaults to stdout)", &output_file), &status);
+
+  /* Get command line arguments */
+  LAL_CALL(LALUserVarReadAllInput(&status, argc, argv), &status);
+  if (help)
+    return EXIT_SUCCESS;
+
+  /* Open the output file */
+  if (LALUserVarWasSet(&output_file)) {
+    if ((fp = fopen(output_file, "wb")) == NULL) {
+      LALPrintError("Couldn't open output file '%s'\n", output_file);
+      return EXIT_FAILURE;
+    }
+  }
+  else {
+    fp = stdout;
+  }
+  LAL_CALL(LALUserVarGetLog(&status, &cmdline, UVAR_LOGFMT_CMDLINE), &status);
+  fprintf(fp, "%%%% %s\n%%%% %s\n", rcsid, cmdline);
+  LALFree(cmdline);
+  fprintf(fp, "freq=%0.3f band=%0.3f\n", freq, band);
+  
+  /* Load the SFTs */
+  {
+    SFTConstraints constraints = empty_SFTConstraints;
+    REAL8 extra = 0.0, fmin = 0.0, fmax = 0.0;
+    
+    /* Load the catalog */
+    LogPrintf(LOG_DEBUG, "Loading SFT catalog ... ");
+    LAL_CALL(LALSFTdataFind(&status, &catalog, sft_pattern, &constraints), &status);
+    if (!catalog || catalog->length == 0) {
+      LALPrintError("Couldn't find SFTs matching '%s'\n", sft_pattern);
+      return EXIT_FAILURE;
+    }
+    LogPrintfVerbatim(LOG_DEBUG, "done: %i SFTs starting at GPS %i\n",
+		      catalog->length, catalog->data[0].header.epoch.gpsSeconds);
+    
+    /* Determine the frequency range */
+    extra = catalog->data[0].header.deltaF * (rng_med_win/2 + 1);
+    fmin = freq - extra;
+    fmax = freq + extra + band;
+    
+    /* Load the SFTs */
+    LogPrintf(LOG_DEBUG, "Loading SFTs (%f to %f) ... ", fmin, fmax);
+    LAL_CALL(LALLoadMultiSFTs(&status, &sfts, catalog, fmin, fmax), &status);
+    LogPrintfVerbatim(LOG_DEBUG, "done\n");
+  }
+  
+  /* Load the ephemeris data */
+  {
+    size_t buf = strlen(ephem_year) + 20;
+    if (ephem_dir)
+      buf += strlen(ephem_dir);
+    
+    /* Allocate memory */
+    if ((ephemeris.ephiles.earthEphemeris = (CHAR*)XLALCalloc(buf, sizeof(CHAR))) == NULL ||
+	(ephemeris.ephiles.sunEphemeris = (CHAR*)XLALCalloc(buf, sizeof(CHAR))) == NULL) {
+      LALPrintError("Couldn't allocate memory\n");
+      return EXIT_FAILURE;
+    }
+    
+    /* Create the file names */	
+    if (ephem_dir) {
+      LALSnprintf(ephemeris.ephiles.earthEphemeris, buf, "%s/earth%s.dat", ephem_dir, ephem_year);
+      LALSnprintf(ephemeris.ephiles.sunEphemeris, buf, "%s/sun%s.dat", ephem_dir, ephem_year);
+    }
+    else {
+      LALSnprintf(ephemeris.ephiles.earthEphemeris, buf, "earth%s.dat", ephem_year);
+      LALSnprintf(ephemeris.ephiles.sunEphemeris, buf, "sun%s.dat", ephem_year);
+    }
+    
+    /* Get the leap seconds */
+    ephemeris.leap = (INT2)XLALGPSLeapSeconds(catalog->data[0].header.epoch.gpsSeconds);
+    if (xlalErrno != XLAL_SUCCESS) {
+      LALPrintError("XLALGPSLeapSeconds failed\n");
+      return EXIT_FAILURE;
+    }
+    
+    /* Load ephemeris */
+    LogPrintf(LOG_DEBUG, "Loading ephemeris ... ");
+    LAL_CALL(LALInitBarycenter(&status, &ephemeris), &status);
+    LogPrintfVerbatim(LOG_DEBUG, "done\n");
+  }
+  
+  /* Get the detector states */
+  LogPrintf(LOG_DEBUG, "Calculating detector states ... ");
+  LAL_CALL(LALGetMultiDetectorStates(&status, &detector_states, sfts, &ephemeris), &status);
+  LogPrintfVerbatim(LOG_DEBUG, "done\n");
+  
+  /* Normalise SFTs and compute noise weights */
+  {
+    LogPrintf(LOG_DEBUG, "Normalising SFTs and computing noise weights ... ");
+    LAL_CALL(LALNormalizeMultiSFTVect(&status, &rng_med, sfts, rng_med_win), &status);
+    LAL_CALL(LALComputeMultiNoiseWeights(&status, &noise_weights, rng_med, rng_med_win, 0), &status);
+    LogPrintfVerbatim(LOG_DEBUG, "done\n");
+  }
+  
+  /* Compute the AM coefficients */
+  {
+    SkyPosition sky;
+    sky.system = COORDINATESYSTEM_EQUATORIAL;
+    sky.longitude = alpha;
+    sky.latitude = delta;
+    LogPrintf(LOG_DEBUG, "Calculating AM coefficients ... ");
+    LAL_CALL(LALGetMultiAMCoeffs(&status, &AM_coeffs, detector_states, sky), &status);
+    if (XLALWeighMultiAMCoeffs(AM_coeffs, noise_weights) != XLAL_SUCCESS) {
+      LALPrintError("XLALWeighMultiAMCoeffs failed\n");
+      return EXIT_FAILURE;
+    }
+    LogPrintfVerbatim(LOG_DEBUG, "done\n");
+    A_coeff = AM_coeffs->Mmunu.Ad * AM_coeffs->Mmunu.Sinv_Tsft;
+    B_coeff = AM_coeffs->Mmunu.Bd * AM_coeffs->Mmunu.Sinv_Tsft;
+    C_coeff = AM_coeffs->Mmunu.Cd * AM_coeffs->Mmunu.Sinv_Tsft;
+  }      
+  
+  /* Initialise the random number generator */
+  {
+    unsigned long seed;
+    FILE *fpr = NULL;
+    if ((rng = gsl_rng_alloc(gsl_rng_mt19937)) == NULL) {
+      LALPrintError("Couldn't allocate a gsl_rng\n");
+      return EXIT_FAILURE;
+    }
+    if ((fpr = fopen("/dev/random", "r")) == NULL) {
+      LALPrintError("Couldn't open '/dev/random'\n");
+      return EXIT_FAILURE;
+    }
+    fread(&seed, sizeof(seed), 1, fpr);
+    fclose(fpr);
+    gsl_rng_set(rng, seed);
+  }
+  
+  /* Compute the volume of the Monte Carlo integration volume:
+     integrate over twoF but average pver cosi,psi (hence not present) */
+  MC_int_vol = (twoFs - 0);
+
+  /* Compute first guess at h0 */
+  h0 = twoFs * pow(A_coeff * B_coeff - C_coeff * C_coeff, -0.25);
+  h0_prev = GSL_POSINF;
+  dh0 = 0.0;
+
+  /* Macro for the relative error in h0 */
+#define H0_ERROR fabs((h0 - h0_prev) / h0)
+
+  /* Begin Newton-Raphson iteration to find h0 */
+  for (h0_iter = 0; H0_ERROR > max_rel_err; ++h0_iter) {
+
+    /* Integrand and its derivative w.r.t. h0 */
+    REAL8 J = 0.0;
+    REAL8 dJ = 0.0;
+
+    /* Target values of integrand */
+    const REAL8 J0 = 1 - FDR;
+
+    LogPrintf(LOG_DEBUG, "Beginning h0 loop %i with error=%0.4e\n", h0_iter, H0_ERROR);
+
+    fprintf(fp, "MC_trials=%i ", MC_trials);
+
+    /* Begin Monte Carlo integration to find J and dJ */
+    MC_iter = MC_trials;
+    while (MC_iter--) {
+
+      /* Generate random cosi, psi, and twoF */
+      const REAL8 cosi = gsl_ran_flat(rng, min_cosi, max_cosi);
+      const REAL8 psi = gsl_ran_flat(rng, min_psi, max_psi);
+      const REAL8 twoF = gsl_ran_flat(rng, 0, twoFs);
+
+      /* Compute the amplitude coefficients vector A */
+      const REAL8 A1 = 0.5 * h0 * (1 + cosi * cosi) * cos(2 * psi);
+      const REAL8 A2 = 0.5 * h0 * (1 + cosi * cosi) * sin(2 * psi);
+      const REAL8 A3 =      -h0 *             cosi  * sin(2 * psi);
+      const REAL8 A4 =       h0 *             cosi  * cos(2 * psi);
+
+      /* Compute the optimal signal to noise ratio rho^2 */
+      const REAL8 rho2 = 0.5 * (
+				A1 * A1 * A_coeff +
+				A2 * A2 * B_coeff +
+				A1 * A2 * C_coeff +
+				A2 * A1 * C_coeff +
+				A3 * A3 * A_coeff +
+				A4 * A4 * B_coeff +
+				A3 * A4 * C_coeff +
+				A4 * A3 * C_coeff
+				);
+
+      /* Add to the integrand and its derivative w.r.t. h0 */
+      J  += pdf_ncx2_4(rho2, twoF);
+      dJ += 2.0 * rho2 / h0 * d_pdf_ncx2_4(rho2, twoF);
+
+    }
+
+    /* Finalise the integrand and derivative: multiply by trial element */
+    J  *= MC_int_vol / MC_trials;
+    dJ *= MC_int_vol / MC_trials;
+
+    /* Compute the increment in h0 from Newton-Raphson */
+    dh0 = (J0 - J) / dJ;
+
+    /* Limit the increment in h0 to |h0 * h0_brake| */
+    dh0 = GSL_SIGN(dh0) * GSL_MIN(fabs(dh0), fabs(h0 * h0_brake));
+
+    /* Increment h0 (saving old value) */
+    h0_prev = h0;
+    h0 += dh0;
+
+    fprintf(fp, "h0=%0.4e\n", h0);
+
+    /* Increment the number of MC trials */
+    MC_trials *= MC_trial_fac;
+
+    LogPrintf(LOG_DEBUG, "Ending    h0 loop %i with error=%0.4e, J-J0=% 0.4e, dh0=% 0.4e\n", H0_ERROR, h0_iter, J - J0, dh0);
+
+  }
+  
+  /* Close the output file */
+  if (LALUserVarWasSet(&output_file)) {
+    fclose(fp);
+  }
+  
+  /* Cleanup */
+  LALDestroyUserVars(&status);
+  LAL_CALL(LALDestroySFTCatalog(&status, &catalog), &status);
+  LAL_CALL(LALDestroyMultiSFTVector(&status, &sfts), &status);
+  XLALFree(ephemeris.ephiles.earthEphemeris);
+  XLALFree(ephemeris.ephiles.sunEphemeris);
+  LALFree(ephemeris.ephemE);
+  LALFree(ephemeris.ephemS);
+  XLALDestroyMultiDetectorStateSeries(detector_states);
+  LAL_CALL(LALDestroyMultiPSDVector(&status, &rng_med), &status);
+  LAL_CALL(LALDestroyMultiNoiseWeights(&status, &noise_weights), &status);
+  XLALDestroyMultiAMCoeffs(AM_coeffs);
+  gsl_rng_free(rng);
+  LALCheckMemoryLeaks();
+  
+  return EXIT_SUCCESS;
+  
+}
+
+/* Regularized hypergeometric function 0F1 */
+REAL8 hyperg_0F1_reg(REAL8 c, REAL8 x) {
+  return gsl_sf_hyperg_0F1(c, x) / gsl_sf_gamma(c);
+}
+
+/* PDF of non-central chi-square with 4 degrees of freedom */
+REAL8 pdf_ncx2_4(REAL8 lambda, REAL8 x) {
+  return 0.25 * exp(-0.5 * (x + lambda)) * x * hyperg_0F1_reg(2, 0.25 * x * lambda);
+}
+
+/* Derivative of the PDF of non-central chi-square with 4 degrees of freedom w.r.t. lambda */
+REAL8 d_pdf_ncx2_4(REAL8 lambda, REAL8 x) {
+  return 0.125 * exp(-0.5 * (x + lambda)) * x * (
+						 0.5 * x * hyperg_0F1_reg(3, 0.25 * x * lambda) 
+						 - hyperg_0F1_reg(2, 0.25 * x * lambda)
+						 );
+}
