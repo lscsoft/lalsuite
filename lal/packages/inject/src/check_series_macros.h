0795e8ce0b3e57c316ae06c0bb68091c76295c75
kipp <kipp>
2008-10-09 06:40:41 +0000

    Update for inversion of XLALUnitCompare()'s truthedness. Original: eade09da291f5aa62a6fa185a8dbf5cce8db9ccc

--- lal/packages/inject/src/check_series_macros.h
+++ lal/packages/inject/src/check_series_macros.h
@@ -15,6 +15,6 @@
 		if ( XLALGPSCmp( &(s1)->epoch, &(s2)->epoch ) != 0 ) XLAL_ERROR_VAL( func, XLAL_ETIME, val ); \
 		if ( fabs( (s1)->deltaT - (s2)->deltaT ) > LAL_REAL8_EPS ) XLAL_ERROR_VAL( func, XLAL_ETIME, val ); \
 		if ( fabs( (s1)->f0 - (s2)->f0 ) > LAL_REAL8_EPS ) XLAL_ERROR_VAL( func, XLAL_EFREQ, val ); \
-		if ( XLALUnitCompare( &(s1)->sampleUnits, &(s2)->sampleUnits ) != 1 ) XLAL_ERROR_VAL( func, XLAL_EUNIT, val ); \
+		if ( XLALUnitCompare( &(s1)->sampleUnits, &(s2)->sampleUnits ) ) XLAL_ERROR_VAL( func, XLAL_EUNIT, val ); \
 		if ( (s1)->data->length != (s1)->data->length ) XLAL_ERROR_VAL( func, XLAL_EBADLEN, val ); \
 	} while (0)
