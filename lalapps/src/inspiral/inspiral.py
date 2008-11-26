1ff85a0f382a03de49bde42001c71f615e243ab6
sfairhur <sfairhur>
2008-11-26 10:57:58 +0000

    merging changes between s5_2yr_lowcbc_20080918 and s5_2yr_lowcbc_20081126 Original: 36713b85aa5c3e06436ef604a4a70abad47e92da

--- lalapps/src/inspiral/inspiral.py
+++ lalapps/src/inspiral/inspiral.py
@@ -1621,6 +1621,33 @@ class PlotInspmissedNode(InspiralPlottingNode):
 
 #############################################################################
 
+class PlotInspfoundJob(InspiralPlottingJob):
+  """
+   A plotinspfound job. The static options are read from the section
+  [plotinspfound] in the ini file. The stdout and stderr from the job
+  are directed to the logs directory. The path to the executable is
+  determined from the ini file.
+  """
+  def __init__(self,cp,dax=False):
+    """
+    cp = ConfigParser object from which the options are read.
+    """
+    exec_name = 'plotinspfound'
+    sections = ['plotinspfound']
+    extension = 'html'
+    InspiralPlottingJob.__init__(self,cp,sections,exec_name,extension,dax)
+
+class PlotInspfoundNode(InspiralPlottingNode):
+  """
+  A PlotInspfoundNode tuns an instance of the plotinspiral code in a Condor Dag.  """
+  def __init__(self,job):
+    """
+    job = A CondorDafJob that can run an instance of plotinspfound.
+    """
+    InspiralPlottingNode.__init__(self,job)
+
+#############################################################################
+
 class PlotEffdistcutJob(InspiralPlottingJob):
   """
   A ploteffdistcut job. The static options are read from the section
