cc60441fbc11bec82eeec399b09efc23d88daa50
siemens <siemens>
2009-03-31 18:11:20 +0000

    updated cosmological parameters Original: 4c190fef67b7449728ebe5f7cb2256a81a47be21

--- lalapps/src/string/cs_lambda_cosmo.h
+++ lalapps/src/string/cs_lambda_cosmo.h
@@ -30,6 +30,6 @@
 #include "cs_cosmo.h"
 
 #define LAMBDA_Z_EQ 5440.0		/* redshift of matter-radiation equilib: 10^3.94 */
-#define LAMBDA_H_0 2.4e-18		/* Hubble constant (s^-1) */
-#define LAMBDA_OMEGA_M 0.25
-#define LAMBDA_OMEGA_R 4.6e-5
+#define LAMBDA_H_0 2.27e-18		/* Hubble constant (s^-1) */
+#define LAMBDA_OMEGA_M 0.279
+#define LAMBDA_OMEGA_R 8.5e-5
