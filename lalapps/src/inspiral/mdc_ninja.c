1743a5d64e61f5736e3d56a9ec9ad78465adda7f
Adam Mercer <adam.mercer@ligo.org>
2009-04-01 13:47:51 -0500

    remove $Id$ from comment header Original: ba997f2db70c17ce690e72f5c163c6debcd5f765

--- lalapps/src/inspiral/mdc_ninja.c
+++ lalapps/src/inspiral/mdc_ninja.c
@@ -16,8 +16,6 @@
  * along with with program; see the file COPYING. If not, write to the
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA  02111-1307  USA
- *
- * Revision: $Id$
  */
 
 #include <math.h>
