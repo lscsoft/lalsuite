7076f3e8fbc4551a25ccef95538d17b312e29cbb
Reinhard Prix <reinhard.prix@ligo.org>
2009-04-09 13:27:06 +0200

    fixed bug in handling multiple gaps Original: 40baf81798bf95d8aa998c4d69272403856f4db7

--- lalapps/src/pulsar/FDS_isolated/compare.m
+++ lalapps/src/pulsar/FDS_isolated/compare.m
@@ -13,18 +13,20 @@ Tspan0 = Tsft * round ( Tspan / Tsft );		%% make Tspan' integer multiple of Tsft
 
 numTimeSamples0 = round(Tspan0 / dt0);
 tdd0 = zeros(numTimeSamples0,2);
-tdd0(:,1) = tddOrig(1:numTimeSamples0,1);	%% truncate original timeseries to Tspan'
-tdd0(:,2) = tddOrig(1:numTimeSamples0,2);
+num = min ( numTimeSamples0, Norig );
+tdd0(1:num,1) = tddOrig(1:num,1);	%% truncate original timeseries to Tspan'
+tdd0(1:num,2) = tddOrig(1:num,2);
 
 %% nock out timesamples falling into gaps, if any
 gaps = diff ( ts(:,1) ) - Tsft;
 indGaps = find ( gaps != 0 )
 
 tddGaps0 = tdd0;
-for i = indGaps
-  ii = round ( i * Tsft / dt0 );	%% start-index of this gap
-  Ni = round(gaps(i) / dt0);		%% number of gap-bins for this gap
-  tddGaps0 ( ii:(ii+Ni), 2) = 0;		%% set this gap-bin to 0
+for i = indGaps'
+  offset_i = ts(i,1) + Tsft - t0
+  ii = round ( offset_i / dt0 )		%% start-index of this gap
+  Ni = round(gaps(i) / dt0)		%% number of gap-bins for this gap
+  tddGaps0 ( ii:(ii+Ni-1), 2) = 0;	%% set this gap-bin to 0
 endfor
 
 lftGaps0 = fft ( tddGaps0(:,2) );
@@ -59,7 +61,8 @@ plot ( freqBins0(imin0:end), imag(lft0Pos(imin0:end)), "go-;FFT TS NO gaps:Im;" 
 plot ( lft1(:,1), lft1(:,2), "r-x;LFTfromSFTs_Band:Re;" );
 plot ( lft1(:,1), lft1(:,3), "r-*;LFTfromSFTs_Band:Im;" );
 %% axis([101.12 101.13]); %% axis([fmin1  fmax1]);
-axis([12.123 12.124]);
+%%axis([12.123 12.124]);
+axis([12.12 12.125])
 hold off;
 
 
