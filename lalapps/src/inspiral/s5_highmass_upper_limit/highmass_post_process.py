4ab1e53aa526dcd5c1db9710f76843557fc2aea8
Chad Hanna <chad.hanna@ligo.org>
2009-04-16 11:45:18 -0700

    fixed a bad macro variable in the dag Original: 6d9b2299c3957591dee4f4e5df199e3c5659b739

--- lalapps/src/inspiral/s5_highmass_upper_limit/highmass_post_process.py
+++ lalapps/src/inspiral/s5_highmass_upper_limit/highmass_post_process.py
@@ -221,7 +221,7 @@ class ligolw_segments_node(pipeline.CondorDAGNode):
     pipeline.CondorDAGNode.__init__(self,job)
     for k in ifodict.keys():
       print ifodict[k]
-      if ifodict[k]: self.add_var_opt("insert-from-segwizard="+k.upper()+"=",ifodict[k])
+      if ifodict[k]: self.add_var_opt("insert-from-segwizard","="+k.upper()+"="ifodict[k])
     self.add_var_opt("name",name)
     self.add_var_opt("output",output)
     if coalesce: self.add_var_opt("coalesce","")
