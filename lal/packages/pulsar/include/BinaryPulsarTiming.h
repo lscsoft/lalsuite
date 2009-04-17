3e73fac9ed52cec4fb4ddce72438c11f93095137
Matthew Pitkin <matthew.pitkin@ligo.org>
2009-04-17 12:41:59 +0100

    Reverting to original version Original: 73f0ee0722ba350031777063eeebade7fb999a72

--- lal/packages/pulsar/include/BinaryPulsarTiming.h
+++ lal/packages/pulsar/include/BinaryPulsarTiming.h
@@ -51,7 +51,7 @@
     and errors on parameters (if available) */
 /* <lalVerbatim file="BinaryPulsarTimingHV">
    Author: Pitkin, M. D.
-   $Id$
+   $Id: BinaryPulsarTiming.h,v 1.14 2008/11/12 12:53:30 mpitkin Exp $
 */
 /* Matt Pitkin 29/04/04 */
 
@@ -152,8 +152,6 @@ tagBinaryPulsarParams
   REAL8 f1;     /**< frequency first derivative (Hz/s) */
   REAL8 f2;     /**< frequency second derivative (Hz/s^2) */
   REAL8 f3;     /**< frequency third derivative (Hz/s^3) */
-  REAL8 f4;     /**< frequency fourth derivative (Hz/s^4) */
-  REAL8 f5;     /**< frequency fifth derivative (Hz/s^5) */
 
   REAL8 ra;     /**< right ascension (rads) */
   REAL8 dec;    /**< declination (rads) */
@@ -231,12 +229,10 @@ tagBinaryPulsarParams
   REAL8 f1Err;
   REAL8 f2Err;
   REAL8 f3Err;
-  REAL8 f4err;
-  REAL8 f5err;
-
+  
   REAL8 pepochErr;
   REAL8 posepochErr;
-
+  
   REAL8 raErr;
   REAL8 decErr;
   REAL8 pmraErr;
