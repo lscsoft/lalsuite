0eb7d69486675dfae761f104cca4a21e64a9e781
Adam Mercer <adam.mercer@ligo.org>
2009-04-21 15:57:10 -0500

    add XLALAggregationStatFiles() function to check that all the frames files, for the requested data segment, are available. Original: dc57a466dca6ae7e3ce454a30b165b266fa4dd3a

--- lal/packages/framedata/src/Aggregation.c
+++ lal/packages/framedata/src/Aggregation.c
@@ -991,3 +991,70 @@ INT4TimeSeries *XLALAggregationStateVectorWait(CHAR *ifo,
 
   return series;
 }
+
+
+/* check that all frames files, for requested data segment, are
+ * available */
+INT4 XLALAggregationStatFiles(CHAR *ifo,
+    LIGOTimeGPS *start,
+    REAL8 duration)
+{
+  static const char *func = "XLALAggregationStatFiles";
+
+  /* declare variables */
+  LIGOTimeGPS *time_now;
+  FrCache *cache;
+  UINT4 i;
+
+  /* check arguments */
+  if (!ifo)
+    XLAL_ERROR(func, XLAL_EFAULT);
+  if (!start)
+    XLAL_ERROR(func, XLAL_EFAULT);
+
+  /* get current gps time */
+  time_now = XLALGPSTimeNow(time_now);
+  if (time_now == NULL)
+  {
+    /* failed to get current time */
+    XLAL_ERROR(func, XLAL_EFUNC);
+  }
+
+  /* check that requested data is not in the future */
+  if (XLALGPSCmp(time_now, start) == -1)
+  {
+    /* requested time in the future */
+    XLAL_ERROR(func, XLAL_EFUNC);
+  }
+
+  /* generate frame cache for requested data */
+  cache = XLALAggregationFrameCache(ifo, start, duration);
+  if (cache == NULL)
+  {
+    /* failed to get cache */
+    XLAL_ERROR(func, XLAL_EINVAL);
+  }
+
+  /* loop through files in cache */
+  for (i = 0; i < cache->numFrameFiles; i++)
+  {
+    /* declare variables */
+    struct stat file_status;
+    CHAR *filename;
+
+    /* strip file://localhost from url */
+    filename = cache->frameFiles[i].url + 16;
+
+    /* check that file exists */
+    if (stat(filename, &file_status) == -1)
+    {
+      /* file doesn't exist */
+      XLAL_ERROR(func, XLAL_EIO);
+    }
+  }
+
+  /* close cache */
+  XLALFrDestroyCache(cache);
+
+  return 0;
+}
