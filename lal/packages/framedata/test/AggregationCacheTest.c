b6adb2c08227c4438eefeaeed65da718b75c2917
Adam Mercer <adam.mercer@ligo.org>
2009-04-22 16:35:54 -0500

    add AggregationCacheTest test program example code for testing cache related data aggregation functions Original: e5728946541376e6af93e7ff2eb1ba32f613d21d

---
+++ lal/packages/framedata/test/AggregationCacheTest.c
@@ -0,0 +1,198 @@
+/*
+ * AggregationCacheTest.c - test online frame data aggregation routines
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with with program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * Copyright (C) 2009 Adam Mercer
+ */
+
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <lal/LALStdio.h>
+#include <lal/LALDatatypes.h>
+#include <lal/Aggregation.h>
+#include <lal/XLALError.h>
+#include <lal/LIGOMetadataTables.h>
+#include <lal/FrameCache.h>
+#include <lal/FrameStream.h>
+#include <lal/PrintFTSeries.h>
+#include <lal/TimeSeries.h>
+#include <lal/XLALError.h>
+
+/* flags for getopt_long */
+int vrbflg;
+
+/* global variables */
+CHAR *ifo;
+LIGOTimeGPS gps = {0, 0};
+INT4 duration;
+
+/*
+ * helper functions
+ */
+
+/* parse command line options */
+static void parse_options(INT4 argc, CHAR *argv[])
+{
+  int c = -1;
+
+  while(1)
+  {
+    static struct option long_options[] =
+    {
+      /* options that set a flag */
+      {"verbose", no_argument, &vrbflg, 1},
+      /* options that don't set a flag */
+      {"help", no_argument, 0, 'a'},
+      {"debug-level", required_argument, 0, 'b'},
+      {"ifo", required_argument, 0, 'c'},
+      {"gps-start-time", required_argument, 0, 'd'},
+      {"duration", required_argument, 0, 'e'},
+      {0, 0, 0, 0}
+    };
+
+    /* getopt_long stores the option here */
+    int option_index = 0;
+    size_t optarg_len;
+
+    /* parse options */
+    c = getopt_long_only(argc, argv, "ab:c:d:e:", long_options, &option_index);
+
+    if (c == -1)
+    {
+      /* end of options, break loop */
+      break;
+    }
+
+    switch(c)
+    {
+      case 0:
+        /* if this option sets a flag, do nothing else now */
+        if (long_options[option_index].flag != 0)
+        {
+          break;
+        }
+        else
+        {
+          fprintf(stderr, "error parsing option %s with argument %s\n", \
+              long_options[option_index].name, optarg);
+          exit(1);
+        }
+        break;
+
+      case 'a':
+        /* help */
+        fprintf(stdout, "Usage: AggregationCacheTest [options]\n");
+        fprintf(stdout, " --help                 print this message\n");
+        fprintf(stdout, " --verbose              run in verbose mode\n");
+        fprintf(stdout, " --debug-level N        set lalDebugLevel\n");
+        fprintf(stdout, " --ifo IFO              set IFO\n");
+        fprintf(stdout, " --gps-start-time GPS   set GPS start time\n");
+        fprintf(stdout, " --duration TIME        set data duration\n");
+        exit(0);
+        break;
+
+      case 'b':
+        /* set debug level */
+        lalDebugLevel = atoi(optarg);
+        break;
+
+      case 'c':
+        /* set ifo */
+        optarg_len = strlen(optarg) + 1;
+        ifo = (CHAR *)calloc(optarg_len, sizeof(CHAR));
+        memcpy(ifo, optarg, optarg_len);
+        break;
+
+      case 'd':
+        /* set gps start time */
+        gps.gpsSeconds = atoi(optarg);
+        gps.gpsNanoSeconds = 0;
+        break;
+
+      case 'e':
+        /* set duration */
+        duration = atoi(optarg);
+        break;
+
+      case '?':
+        exit(1);
+        break;
+
+      default:
+        fprintf(stderr, "unknown error while parsing options\n");
+        exit(1);
+    }
+  }
+
+  if (optind < argc)
+  {
+    fprintf(stderr, "extraneous command line arguments:\n");
+    while(optind < argc)
+    {
+      fprintf(stderr, "%s\n", argv[optind++]);
+    }
+    exit(1);
+  }
+
+  return;
+}
+
+
+/* main program entry point */
+INT4 main(INT4 argc, CHAR *argv[])
+{
+  /* declare variables */
+  FrCache *cache;
+  CHAR *type;
+  CHAR filename[FILENAME_MAX];
+
+  /* parse command line options */
+  parse_options(argc, argv);
+
+  /* get frame cache */
+  cache = XLALAggregationFrameCache(ifo, &gps, duration);
+  if (cache == NULL)
+  {
+    fprintf(stderr, "error: failed to get frame cache\n");
+    exit(xlalErrno);
+  }
+
+  /* get frame type */
+  type = XLALAggregationFrameType(ifo);
+  if (type == NULL)
+  {
+    fprintf(stderr, "error: failed to get frame type\n");
+    exit(xlalErrno);
+  }
+  /* create name for cache file */
+  LALSnprintf(filename, FILENAME_MAX, "%c-%s-%d-%d.cache", ifo[0], \
+      type, gps.gpsSeconds, duration);
+
+  /* save cache */
+  XLALFrExportCache(cache, filename);
+
+  /* free memory */
+  free(ifo);
+
+  /* check for memory leaks */
+  LALCheckMemoryLeaks();
+
+  return 0;
+}
