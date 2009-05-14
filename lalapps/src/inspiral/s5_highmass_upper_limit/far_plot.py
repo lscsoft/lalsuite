d9d23db8eea32ed371cf4245c2787507c2370c8d
Chad Hanna <chad.hanna@ligo.org>
2009-05-14 16:35:04 -0700

    made it multi database compatible Original: 7125047ce8a1f6ab677eeeec4cd23e7895ff978b

--- lalapps/src/inspiral/s5_highmass_upper_limit/far_plot.py
+++ lalapps/src/inspiral/s5_highmass_upper_limit/far_plot.py
@@ -1,5 +1,7 @@
 import scipy
 import numpy
+import matplotlib
+matplotlib.use('Agg')
 import pylab
 from math import *
 import sys
@@ -22,33 +24,53 @@ def sigma_regions(m, s, nlist):
     fix_numbers(out[n])
   return out
 
-connection = sqlite3.connect(dbtables.get_connection_filename('FULL_DATACAT_3.sqlite', tmp_path = None, verbose = True))
-cursor = connection.cursor()
-zero_query = 'SELECT 1.0 / coinc_inspiral.false_alarm_rate FROM coinc_inspiral JOIN coinc_event ON (coinc_event.coinc_event_id == coinc_inspiral.coinc_event_id) WHERE (coinc_event.instruments == "H1,H2,L1" AND NOT EXISTS (SELECT * FROM time_slide WHERE time_slide.time_slide_id == coinc_event.time_slide_id AND time_slide.offset !=0 ) )ORDER BY coinc_inspiral.false_alarm_rate;'
-slide_query = 'SELECT 1.0 / coinc_inspiral.false_alarm_rate FROM coinc_inspiral JOIN coinc_event ON (coinc_event.coinc_event_id == coinc_inspiral.coinc_event_id) WHERE (coinc_event.instruments == "H1,H2,L1" AND EXISTS (SELECT * FROM time_slide WHERE time_slide.time_slide_id == coinc_event.time_slide_id AND time_slide.offset !=0 ) )ORDER BY coinc_inspiral.false_alarm_rate;'
-zero_ifar = [ifar for ifar, in cursor.execute(zero_query)]
-slide_ifar = [ifar for ifar, in cursor.execute(slide_query)]
-pylab.figure(1)
-pylab.loglog(zero_ifar, numpy.arange(len(zero_ifar)) + 1, linewidth=2)
-pylab.hold(1)
+def get_ifos(cursor):
+  query = "SELECT distinct(instruments) FROM coinc_event;"
+  return [ifo for ifo, in cursor.execute(query )]
+  
+def connect(fname):
+  connection = sqlite3.connect(dbtables.get_connection_filename('FULL_DATACAT_3.sqlite', tmp_path = None, verbose = True))
+  cursor = connection.cursor()
+  return cursor
 
-#FIXME compensation should be ratio of live times
-m = (numpy.arange(len(slide_ifar)) + 1) / 100.00
+def extract_from_db(file, zero_ifar, slide_ifar):
+  cursor = connect(sys.argv[1])
+  ifos = get_ifos(cursor)
+  for ifo in ifos:
+    try: zero_ifar[ifo]
+    except: zero_ifar[ifo] = []
+    try: slide_ifar[ifo]
+    except: slide_ifar[ifo] = []
+    zero_ifar[ifo] += [ifar for ifar, in cursor.execute('SELECT 1.0 / coinc_inspiral.false_alarm_rate FROM coinc_inspiral JOIN coinc_event ON (coinc_event.coinc_event_id == coinc_inspiral.coinc_event_id) WHERE (coinc_event.instruments == ? AND NOT EXISTS (SELECT * FROM time_slide WHERE time_slide.time_slide_id == coinc_event.time_slide_id AND time_slide.offset !=0 ) )ORDER BY coinc_inspiral.false_alarm_rate;', (ifo,))]
+    slide_ifar[ifo] += [ifar for ifar, in cursor.execute('SELECT 1.0 / coinc_inspiral.false_alarm_rate FROM coinc_inspiral JOIN coinc_event ON (coinc_event.coinc_event_id == coinc_inspiral.coinc_event_id) WHERE (coinc_event.instruments == ? AND EXISTS (SELECT * FROM time_slide WHERE time_slide.time_slide_id == coinc_event.time_slide_id AND time_slide.offset !=0 ) )ORDER BY coinc_inspiral.false_alarm_rate;', (ifo,) )]
+  cursor.close()
 
+zero_ifar = {}
+slide_ifar = {}
+for file in sys.argv[1:]:
+  extract_from_db(file, zero_ifar, slide_ifar)
 
-pylab.loglog(slide_ifar, m, 'k', linewidth=2)
-#m = numpy.array(range(len(slide_ifar))) / 100.00
-s = numpy.sqrt(m)
-x = numpy.array(slide_ifar)
-xc = numpy.concatenate((x, x[::-1]))
-y = sigma_regions(m, s, [1,2,3])
-pylab.fill(xc, y[1], alpha=0.25, facecolor=[0.25, 0.25, 0.25])
-pylab.fill(xc, y[2], alpha=0.25, facecolor=[0.5, 0.5, 0.5])
-pylab.fill(xc, y[3], alpha=0.25, facecolor=[0.75, 0.75, 0.75])
-pylab.xlim( x[-1], x[0] )
-pylab.ylim(0.01, len(zero_ifar) )
-pylab.legend(("zero lag", "expected background, N", "1*sqrt(N)", "2*sqrt(N)","3*sqrt(N)"))
-pylab.hold(0)
-pylab.savefig("far.png")
-#pylab.show()
-
+figcnt = 0
+#FIXME fails if there is no background in a given ifo combo
+for ifos in zero_ifar.keys():
+  figcnt += 1
+  pylab.figure(figcnt)
+  pylab.loglog(zero_ifar[ifos], numpy.arange(len(zero_ifar[ifos])) + 1, linewidth=2)
+  pylab.hold(1)
+  #FIXME compensation should be ratio of live times
+  m = (numpy.arange(len(slide_ifar[ifos])) + 1) / 100.00
+  pylab.loglog(slide_ifar[ifos], m, 'k', linewidth=2)
+  s = numpy.sqrt(m)
+  x = numpy.array(slide_ifar[ifos])
+  xc = numpy.concatenate((x, x[::-1]))
+  y = sigma_regions(m, s, [1,2,3])
+  pylab.fill(xc, y[1], alpha=0.25, facecolor=[0.25, 0.25, 0.25])
+  pylab.fill(xc, y[2], alpha=0.25, facecolor=[0.5, 0.5, 0.5])
+  pylab.fill(xc, y[3], alpha=0.25, facecolor=[0.75, 0.75, 0.75])
+  pylab.xlim( x[-1], x[0] )
+  pylab.ylim(0.01, len(zero_ifar[ifos]) )
+  pylab.ylabel('Number')
+  pylab.xlabel('IFAR based Rank')
+  pylab.legend(("zero lag", "expected background, N", "1*sqrt(N)", "2*sqrt(N)","3*sqrt(N)"), loc="lower left")
+  pylab.hold(0)
+  pylab.savefig(sys.argv[1]+'_'+''.join(ifos.split(','))+"_far.png")
