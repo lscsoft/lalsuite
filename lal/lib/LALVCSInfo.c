1f6e632e515795f76b75ff45d7175842022dac08
Adam Mercer <adam.mercer@ligo.org>
2009-05-14 13:25:51 -0500

    initial commit of git versioning code Original: c97393e9b61b0b8ae3585dfaacb25e977ffacc2a

---
+++ lal/lib/LALVCSInfo.c
@@ -0,0 +1,35 @@
+/*
+ * LALVCSInfo.c - LAL VCS Information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with with program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ *
+ * Copyright (C) 2009 Adam Mercer
+ */
+
+#include "LALVCSInfo.h"
+
+/* global variables for vcs information */
+const char *lalVCSId = LAL_VCS_ID;
+const char *lalVCSDate = LAL_VCS_DATE;
+const char *lalVCSTag = LAL_VCS_TAG;
+const char *lalVCSStatus = LAL_VCS_STATUS;
+
+/* library vcs information structure */
+struct tagLALVCSInfo lalLibraryVCSInfo = {LAL_VCS_ID, LAL_VCS_DATE, LAL_VCS_TAG, LAL_VCS_STATUS};
+
+/*
+ * vim: tw=0 ts=2 et
+ */
