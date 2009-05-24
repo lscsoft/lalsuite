a65e3130a2ba702b0aca45bbf28984a885b78b7b
Reinhard Prix <reinhard.prix@ligo.org>
2009-05-24 11:14:55 +0200

    modularized testFstat_v3.c some more    - added helper function XLALgenerateRandomData() to generate random data with gaps Original: 4c4c0f3401de2725bf585e02e6e5c0946a2c40a7

--- lalapps/src/pulsar/FDS_isolated/testFstat_v3.c
+++ lalapps/src/pulsar/FDS_isolated/testFstat_v3.c
@@ -35,6 +35,7 @@
 
 /* GSL includes */
 #include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 
 /* LAL-includes */
@@ -44,6 +45,7 @@
 #include <lal/TimeSeries.h>
 #include <lal/ComplexFFT.h>
 
+#include <lal/GeneratePulsarSignal.h>
 #include <lal/ComputeFstat.h>
 #include "Fstat_v3.h"
 
@@ -60,6 +62,11 @@
 #define TEST_FAILED     1
 #define TEST_ABORTED    2
 
+/* ----- Macros ----- */
+#define MYMAX(x,y) ( (x) > (y) ? (x) : (y) )
+#define MYMIN(x,y) ( (x) < (y) ? (x) : (y) )
+
+
 /* ---------- local types ---------- */
 
 /*---------- Global variables ----------*/
@@ -69,11 +76,12 @@
 int main(int argc, char *argv[]);
 
 int test_XLALSFTVectorToCOMPLEX8TimeSeries(void);
+int XLALgenerateRandomData ( REAL4TimeSeries **ts, SFTVector **sfts );
 
 /*---------- empty initializers ---------- */
 static LALUnit empty_LALUnit;
 
-int lalDebugLevel = 0;
+int lalDebugLevel = 1;
 
 /*----------------------------------------------------------------------*/
 /* Main Function starts here */
@@ -84,19 +92,26 @@ int lalDebugLevel = 0;
 int main(int argc, char *argv[])
 {
   const CHAR *fn = "testFstat_v3";
-
+  REAL4TimeSeries *ts = NULL;
+  SFTVector *sfts = NULL;
   int res1;
 
-   if ( (res1 = test_XLALSFTVectorToCOMPLEX8TimeSeries()) != TEST_PASSED )
+
+  LogPrintf (LOG_NORMAL, "%s: Now testing XLALSFTVectorToCOMPLEX8TimeSeries() ... ", fn);
+  if ( (res1 = XLALgenerateRandomData(&ts, &sfts)) != TEST_PASSED )
     {
-      LogPrintf (LOG_CRITICAL, "%s: test of XLALSFTVectorToCOMPLEX8TimeSeries() failed\n\n");
+      LogPrintfVerbatim (LOG_CRITICAL, "failed.\n\n");
       return res1;
     }
   else
     {
-      LogPrintf (LOG_NORMAL, "%s: test of XLALSFTVectorToCOMPLEX8TimeSeries() succeeded\n\n");
+      LogPrintfVerbatim (LOG_NORMAL, "succeeded.\n\n");
     }
 
+  XLALDestroyREAL4TimeSeries ( ts );
+  XLALDestroySFTVector ( sfts );
+
+  LALCheckMemoryLeaks();
 
   return TEST_PASSED;
 
@@ -104,53 +119,162 @@ int main(int argc, char *argv[])
 
 
 
-/** Unit test for XLALSFTVectorToCOMPLEX8TimeSeries() function.
- * Returns: TEST_PASSED, TEST_FAILED, or TEST_ABORTED
+/**
+ * function to generate random time-series with gaps, and corresponding SFTs
  */
 int
-test_XLALSFTVectorToCOMPLEX8TimeSeries ( void )
+XLALgenerateRandomData ( REAL4TimeSeries **ts, SFTVector **sfts )
 {
-  const CHAR *fn = "test_XLALSFTVectorToCOMPLEX8TimeSeries()";
+  const CHAR *fn = "XLALgenerateRandomData()";
 
-  REAL4TimeSeries *inputTS;
-  LIGOTimeGPS epoch = { 714180733, 0 };
+  LIGOTimeGPS epoch0 = { 714180733, 0 };
+  UINT4 numSFTs = 19;
+
   REAL8 fmax = 10.0;
-  REAL8 deltaT = 1.0/(2.0 * fmax);
-  REAL8 duration = 10.0 * 3600;	/* 10 hours */
-  UINT4 numSamples = (UINT4)(duration / deltaT);	/* round down */
   REAL8 Tsft = 1800.0;
-  UINT4 numSFTs = (UINT4)(duration / Tsft) - 2;		/* leave at least 2 Tsft as gaps */
-  UINT4 i_gap1, i_gap2, i_gap3;				/* leave 3 gaps at random indices */
+  REAL8 dFreq;
+
+  REAL8 deltaT;
+  REAL8 duration;
+  UINT4 numFreqBins;
+  REAL8 BandEff;		/* effective frequency band of SFTs (integer number of bins) */
+  UINT4 numSamplesSFT;		/* number of samples in one SFTs (either time-domain or frequency-domain) */
+  UINT4 numSamplesTS;		/* number of samples in the full time-series (including gaps) */
+
+  REAL4TimeSeries *outTS;	/* input timeseries, generated randomly, with 2 random gaps */
+  REAL4 *TSdata;		/* convenience pointer to outTS->data->data */
+  SFTVector *outSFTs;		/* SFT vector created from input timeseries */
+  LIGOTimeGPSVector *timestampsSFT;	/* vector of SFT timestamps */
+
+  UINT4 i1, i2, iGap1, iGap2;	/* leave 2 gaps at random indices */
+  UINT4 numBinsGap1, numBinsGap2;
+  UINT4 thisBin;
+
+  UINT4 iSFT, iBin;
+
   time_t now;
   gsl_rng * r;
 
+  /* input sanity checks */
+  if ( !ts || !sfts ) {
+    XLALPrintError ("%s: NULL input pointers\n", fn );
+    XLAL_ERROR (fn, XLAL_EINVAL );
+  }
+  if ( (*ts != NULL) || (*sfts != NULL) ) {
+    XLALPrintError ("%s: output pointers not initialized to NULL.\n", fn);
+    XLAL_ERROR (fn, XLAL_EINVAL );
+  }
+
+  /* prepare sampling constants */
+  dFreq         = 1.0 / Tsft;
+  numFreqBins   = (UINT4)(fmax / dFreq);
+  BandEff       = numFreqBins * dFreq;
+  deltaT        = 1.0 / (2.0 * BandEff);
+  numSamplesSFT = 2 * numFreqBins;		/* real-valued timeseries */
+
   /* prepare random-number generator */
-  if ( (r = gsl_rng_alloc (gsl_rng_taus)) == NULL )
+  if ( (r = gsl_rng_alloc (gsl_rng_taus2)) == NULL )
     {
-      XLALPrintError ("%s: failed to gsl_rng_alloc (gsl_rng_taus)\n");
+      XLALPrintError ("%s: failed to gsl_rng_alloc (gsl_rng_taus2)\n");
       return TEST_ABORTED;
     }
   now = time(NULL);
   gsl_rng_set (r, (unsigned long int)now);	/* sets random-number seed */
 
+  /* ----- set up 2 unique gaps ----- */
+  i1 = gsl_rng_uniform_int (r, numSFTs );	/* uniform integer between [0, n-1] inclusive */
+  do {
+    i2 = gsl_rng_uniform_int (r, numSFTs );
+  } while (i2 == i1);
+  iGap1 = MYMIN ( i1, i2 );
+  iGap2 = MYMAX ( i1, i2 );
 
+  /* 2 gaps of gap-lengths between 0 and 1/3 Tdata */
+  numBinsGap1 = gsl_rng_uniform_int (r, numSamplesSFT * numSFTs / 3 );
+  numBinsGap2 = gsl_rng_uniform_int (r, numSamplesSFT * numSFTs / 3 );
 
-  /* ----- (non-heterodyned) random noise timeseries with gaps ----- */
-  if ( (inputTS = XLALCreateREAL4TimeSeries ("input TS", &epoch, 0, deltaT, &empty_LALUnit, numSamples )) == NULL )
+  numSamplesTS = numSFTs * numSamplesSFT + numBinsGap1 + numBinsGap2;
+  duration = numSamplesTS * deltaT;
+
+  /* ----- allocate timeseries ----- */
+  if ( (outTS = XLALCreateREAL4TimeSeries ("input TS", &epoch0, 0, deltaT, &empty_LALUnit, numSamplesTS )) == NULL )
     {
-      XLALPrintError ("%s: XLALCreateREAL4TimeSeries() failed for numSamples = %d\n", numSamples );
-      return TEST_ABORTED;
+      XLALPrintError ("%s: XLALCreateREAL4TimeSeries() failed for numSamples = %d\n", numSamplesTS );
+      XLAL_ERROR ( fn, XLAL_EFUNC );
     }
 
-  /* initialized to zero (to allow for gaps) */
-  memset ( inputTS->data->data, 0, inputTS->data->length * sizeof ( inputTS->data->data[0] ) );
+  TSdata = outTS->data->data;
+  /* initialize timeseries to zero (to allow for gaps) */
+  memset ( TSdata, 0, outTS->data->length * sizeof ( outTS->data->data[0] ) );
 
+  /* also set up corresponding SFT timestamps vector */
+  if ( (timestampsSFT = XLALCalloc (1, sizeof( *timestampsSFT )) ) == NULL ) {
+    XLALPrintError ("%s: Failed to XLALCalloc %d bytes\n", fn, sizeof( *timestampsSFT ));
+    XLAL_ERROR ( fn, XLAL_ENOMEM );
+  }
 
+  timestampsSFT->length = numSFTs;
+  if ( (timestampsSFT->data = XLALCalloc (numSFTs, sizeof (*timestampsSFT->data) )) == NULL) {
+    XLALPrintError ("%s: Failed to allocate %d x %d bytes\n", fn, numSFTs, sizeof (*timestampsSFT->data) );
+    XLALFree ( timestampsSFT );
+    XLAL_ERROR ( fn, XLAL_ENOMEM );
+  }
+
+  /* ----- set up random-noise timeseries with gaps ---------- */
+  thisBin = 0;	/* running bin-counter */
+  for ( iSFT=0; iSFT < numSFTs; iSFT ++ )
+    {
+      /* skip gaps */
+      if ( iSFT == iGap1 )
+	thisBin += numBinsGap1;
+      if ( iSFT == iGap2 )
+	thisBin += numBinsGap2;
+
+      /* record this SFT's timestamp */
+      timestampsSFT->data[iSFT] = epoch0;
+      timestampsSFT->data[iSFT].gpsSeconds += thisBin * deltaT;
+
+      /* generate all data-points of this SFT */
+      for ( iBin=0; iBin < numSamplesSFT; iBin++ ) {
+	TSdata[thisBin++] = (REAL4)gsl_ran_gaussian (r, 1.0);	/* unit-variance Gaussian noise */
+	/* TSdata[thisBin++] = (REAL4)( 2.0 * gsl_rng_uniform (r) - 1.0 );	*//* random-number in [-1, 1] */
+      }
+    } /* for iSFT < numSFTs */
+
+  if ( thisBin != numSamplesTS ) {
+    XLALPrintError ("\n%f: sanity check failed, thisBin = %d does not agree with numSamplesTS = %d\n", fn, thisBin, numSamplesTS );
+    XLAL_ERROR ( fn, XLAL_EBADLEN );
+
+  }
+
+  /* ----- generate SFTs from this timeseries ---------- */
+  {
+    SFTParams sftParams = {0};
+    LALStatus status = {0};
+
+    sftParams.Tsft = Tsft;
+    sftParams.timestamps = timestampsSFT;
+    sftParams.noiseSFTs = NULL;
+    sftParams.make_v2SFTs = 1;
+    sftParams.window = NULL;
+
+    outSFTs = NULL;
+    LALSignalToSFTs(&status, &outSFTs, outTS, &sftParams);
+    if ( status.statusCode ) {
+      XLALPrintError("\n%s: LALSignalToSFTs() failed, with LAL error %d\n", fn, status.statusCode );
+      XLAL_ERROR ( fn, XLAL_EFUNC );
+    }
+  } /* turn timeseries into SFTs */
 
   /* free memory */
   gsl_rng_free (r);
-  XLALDestroyREAL4TimeSeries ( inputTS );
+  XLALFree ( timestampsSFT->data );
+  XLALFree ( timestampsSFT );
 
-  return TEST_PASSED;
+  /* return timeseries and SFTvector */
+  (*ts)   = outTS;
+  (*sfts) = outSFTs;
 
-} /* test_XLALSFTVectorToCOMPLEX8TimeSeries() */
+  return XLAL_SUCCESS;
+
+} /* XLALgenerateRandomData() */
