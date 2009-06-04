02ff5b8240fb5fa8a6f78dcf3586890e561d0d0e
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2009-06-04 15:54:51 +0200

    removed obsolete files (old checkpointing documentation etc) Original: 3816ec36fd3e730a7b667f2f9b0a6adb47dfe602

--- lalapps/src/pulsar/hough/src2/HS_Checkpointing.sh
+++
@@ -1,2 +0,0 @@
-cmd=`sed -n 's|.*<a class="code" href="\(.*\)">\(.*\)</a>.*|s%="\2"%="\1"%g;|p' | sort -u`
-echo '</body></html>' | cat *.map - | sed "$cmd" | cat HS_Checkpointing.body - > HS_Checkpointing.html
