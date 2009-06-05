feda8a68bb6f97e2c008f8108190e150e82ef099
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2009-06-05 12:20:55 +0200

    added gas code - not working yet Original: 95e5edd78e7e3c3f5129467daf588b11400e1f89

--- lalapps/src/pulsar/hough/src2/EinsteinAtHome/sincos.h
+++ lalapps/src/pulsar/hough/src2/EinsteinAtHome/sincos.h
@@ -2,19 +2,25 @@
   This file defines:
   - a macro SINCOS_TRIM_X(y,x) which trims the value x to interval [0..2)
   - global REAL4 arrays sincosLUTbase[] and sincosLUTdiff[] as lookup tables
-  - a function void local_sin_cos_2PI_LUT_init(void) that initailizes the lookup tables
-  - a function local_sin_cos_2PI_LUT_trimmed(*sin,*cos,x)
-  - macros SINCOS_STEP1..6 for the individual steps of the local_sin_cos_2PI_LUT_trimmed() function
+  - a function void local_sin_cos_2PI_LUT_init(void) that inits these
+  - a function local_sin_cos_2PI_LUT_trimmed(*sin,*cos,x) that uses the
+    lookup tables to evaluate sin and cos values of 2*Pi*x if x is
+    already trimmed to the interval [0..2)
+  - macros SINCOS_STEP1..6 for the individual steps of
+    local_sin_cos_2PI_LUT_trimmed() (to be mixed into the hotloop code)
   - a type ux_t for a variable ux to be used in these macros
-  - the macros SINCOS_LUT_RES, SINCOS_ADDS, SINCOS_MASK1, SINCOS_MASK2, SINCOS_SHIFT
+  - macros SINCOS_LUT_RES, SINCOS_ADDS, SINCOS_MASK1, SINCOS_MASK2, SINCOS_SHIFT
 
-  The following compile-switches (macros) are used:
-  _MSC_VER, _ARCH_PPC, LAL_NDEBUG, __BIG_ENDIAN__
+  The following macros determine the code that is actually built:
+  _MSC_VER       : are we using the Microsoft compiler that doesn't know C99?
+  _ARCH_PPC      : are we compiling for PowerPC?
+  LAL_NDEBUG     : are we compiling in LAL debug mode?
+  __BIG_ENDIAN__ : has the architecture big-endian byt order?
 */
 
 
 /*
- the way of trimming x to the interval [0..1) for the sin_cos_LUT functions
+ the way of trimming x to the interval [0..2) for the sin_cos_LUT functions
  give significant differences in speed, so we provide various ways here.
  We also record the way we are using for logging
 */
@@ -37,18 +43,114 @@
 #endif
 
 
-
-/* sin/cos Lookup tables */
-#define SINCOS_LUT_RES 1024 /* should be multiple of 4 */
-
-static REAL4 sincosLUTbase[SINCOS_LUT_RES+SINCOS_LUT_RES/4];
-static REAL4 sincosLUTdiff[SINCOS_LUT_RES+SINCOS_LUT_RES/4];
-
+/* sincos constants */
 #define SINCOS_ADDS  402653184.0
 #define SINCOS_MASK1 0xFFFFFF
 #define SINCOS_MASK2 0x003FFF
 #define SINCOS_SHIFT 14
 
+/* sin/cos Lookup tables */
+#define SINCOS_LUT_RES 1024 /* should be multiple of 4 */
+
+/* global VARIABLES to be used in (global) macros */
+static REAL4 sincosLUTbase[SINCOS_LUT_RES+SINCOS_LUT_RES/4];
+static REAL4 sincosLUTdiff[SINCOS_LUT_RES+SINCOS_LUT_RES/4];
+/* shift cos tables 90 deg. to sin table */
+static const REAL4* cosLUTbase = sincosLUTbase + (SINCOS_LUT_RES/4);
+static const REAL4* cosLUTdiff = sincosLUTdiff + (SINCOS_LUT_RES/4);
+
+/* allow to read the higher bits of a 'double' as an 'int' */
+union {
+  REAL8 asreal;
+  struct {
+#ifdef __BIG_ENDIAN__
+    INT4 dummy;
+    INT4 intval;
+#else
+    INT4 intval;
+    INT4 dummy;
+#endif
+  } as2int;
+} sincosUX;
+ 
+static INT4 sincosI, sincosN;
+
+
+/* LUT evaluation */
+
+#ifndef SINCOS_X87_GNU_ASS
+
+
+/* x must already been trimmed to interval [0..2) */
+/* - syntactic sugar -|   |- here is the actual code - */
+#define SINCOS_PROLOG
+#define SINCOS_STEP1(x)   sincosUX.asreal = x + SINCOS_ADDS;
+#define SINCOS_STEP2      sincosI = sincosUX.as2int.intval & SINCOS_MASK1;
+#define SINCOS_STEP3      sincosN = sincosUX.as2int.intval & SINCOS_MASK2;
+#define SINCOS_STEP4      sincosI = sincosI >> SINCOS_SHIFT;
+#define SINCOS_STEP5(s)   *s = sincosLUTbase[sincosI] + sincosN * sincosLUTdiff[sincosI];
+#define SINCOS_STEP6(c)   *c = cosLUTbase[sincosI]    + sincosN * cosLUTdiff[sincosI];
+#define SINCOS_EPILOG(s,c,x)
+
+
+
+#else /* the very same in x87 gcc inline assembler */
+
+
+#define SINCOS_VARIABLES				\
+    static REAL8 sincos_adds = SINCOS_ADDS;		\
+    static REAL4 *scd 	     = &(sincosLUTdiff[0]);	\
+    static REAL4 *scb 	     = &(sincosLUTbase[0]);	\
+    static REAL8 tmp;
+#define SINCOS_PROLOG        { SINCOS_VARIABLES __asm __volatile (
+#define SINCOS_TRIM1(x)					\
+  "fldl    %[" #x "]      \n\t" /* st: x */			\
+  "fistpll %[tmp]         \n\t" /* tmp=(INT8)(round((x)) */	\
+  "fld1                   \n\t" /* st: 1.0 */			\
+  "fildll  %[tmp]         \n\t" /* st: 1.0;(round((x))*/ 
+#define SINCOS_TRIM2(x)						\
+  "fsubrp  %%st,%%st(1)   \n\t" /* st: 1.0 -round(x) */			\
+  "faddl   %[" #x "]      \n\t" /* st: x -round(x)+1.0*/		\
+  "faddl   %[sincos_adds] \n\t" /* ..continue lin. sin/cos as below */	\
+  "fstpl   %[tmp]         \n\t"
+#define SINCOS_STEP1(x)							\
+  "fldl  %[" #x "]        \n\t" /*st:x */				\
+  "faddl %[sincos_adds]   \n\t" /*st:x+A */				\
+  "fstpl %[tmp]           \n\t"
+#define SINCOS_STEP2							\
+  "mov   %[tmp],%%eax     \n\t" /* x +A ->eax (ix)*/			\
+  "mov   %%eax,%%edx      \n\t"						\
+  "and   $0x3fff,%%eax    \n\t"	/* n  = ix & SINCOS_MASK2 */
+#define SINCOS_STEP3						\
+  "mov   %%eax,%[tmp]     \n\t"					\
+  "mov   %[scd], %%eax    \n\t"					\
+  "and   $0xffffff,%%edx  \n\t" /* i  = ix & SINCOS_MASK1;*/
+#define SINCOS_STEP4							\
+  "fildl %[tmp]           \n\t"						\
+  "sar   $0xe,%%edx       \n\t" /* i  = i >> SINCOS_SHIFT;*/		\
+  "fld   %%st             \n\t" /* st: n; n; */				\
+  "fmuls (%%eax,%%edx,4)  \n\t"
+#define SINCOS_STEP5(s)							\
+  "mov   %[scb], %%edi    \n\t"						\
+  "fadds (%%edi,%%edx,4)  \n\t" /*st:sincosLUTbase[i]+n*sincosLUTdiff[i]; n*/ \
+  "add   $0x100,%%edx     \n\t" /*edx+=SINCOS_LUT_RES/4*/		\
+  "fstps %[" #s "]        \n\t" /*(*sin)=sincosLUTbase[i]+n*sincosLUTdiff[i]*/
+#define SINCOS_STEP6(c)							\
+  "fmuls (%%eax,%%edx,4)  \n\t"						\
+  "fadds (%%edi,%%edx,4)  \n\t"						\
+  "fstps %[" #c "]        \n\t" /*(*cos)=cosbase[i]+n*cosdiff[i];*/
+#define SINCOS_OUTPUT(s,c)   [s] "=m" (s), [c] "=m" (c), [tmp] "=m" (tmp)
+#define SINCOS_INPUT(x)      [scd] "m" (scd), [scb] "m" (scb), [sincos_adds] "m" (sincos_adds), [x] "m" (x)
+#define SINCOS_REGISTERS     "st","st(1)","st(2)","eax","edx","edi","cc"
+#define SINCOS_EPILOG(s,c,x) : SINCOS_OUTPUT(s,c) : SINCOS_INPUT(x) : SINCOS_REGISTERS ); }
+
+
+#endif
+
+
+/* FUNCTIONS */
+
+/* initailize LUTs */
 static void local_sin_cos_2PI_LUT_init (void)
 {
   static const REAL8 step = LAL_TWOPI / (REAL8)SINCOS_LUT_RES;
@@ -68,23 +170,7 @@ static void local_sin_cos_2PI_LUT_init (void)
 }
 
 
-/* weird type definition to read the higher bits of a 'double' as an 'int' */
-typedef
-union {
-  REAL8 asreal;
-  struct {
-    INT4 high;
-    INT4 low;
-  } as2int;
-} ux_t;
-
-
-/* x must already been trimmed to interval [0..2) */
-static int local_sin_cos_2PI_LUT_trimmed (REAL4 *sin2pix, REAL4 *cos2pix, REAL8 x) {
-  static const REAL4* cosbase = sincosLUTbase + (SINCOS_LUT_RES/4);
-  static const REAL4* cosdiff = sincosLUTdiff + (SINCOS_LUT_RES/4);
-  INT4  i, n;
-  ux_t ux;
+static int local_sin_cos_2PI_LUT_trimmed (REAL4 *s, REAL4 *c, REAL8 x) {
 
 #ifndef LAL_NDEBUG
   if(x > SINCOS_ADDS) {
@@ -96,26 +182,14 @@ static int local_sin_cos_2PI_LUT_trimmed (REAL4 *sin2pix, REAL4 *cos2pix, REAL8 
   }
 #endif
 
-  /* v-- syntactic sugar      v--- down here is what actually happens */
-
-#define SINCOS_STEP1(ux,x)    ux.asreal = x + SINCOS_ADDS
-  SINCOS_STEP1(ux,x);
-#ifdef __BIG_ENDIAN__
-#define SINCOS_STEP2(i,ux)    i = ux.as2int.low & SINCOS_MASK1
-#define SINCOS_STEP3(n,ux)    n = ux.as2int.low & SINCOS_MASK2
-#else
-#define SINCOS_STEP2(i,ux)    i = ux.as2int.high & SINCOS_MASK1
-#define SINCOS_STEP3(n,ux)    n = ux.as2int.high & SINCOS_MASK2
-#endif
-  SINCOS_STEP2(i,ux);
-  SINCOS_STEP3(n,ux);
-#define SINCOS_STEP4(i)       i = i >> SINCOS_SHIFT
-  SINCOS_STEP4(i);
-  
-#define SINCOS_STEP5(s,i,n)   s = sincosLUTbase[i] + n * sincosLUTdiff[i]
-  SINCOS_STEP5(*sin2pix,i,n);
-#define SINCOS_STEP6(c,i,n)   c = cosbase[i]       + n * cosdiff[i];
-  SINCOS_STEP6(*cos2pix,i,n);
+  SINCOS_PROLOG
+  SINCOS_STEP1(x)
+  SINCOS_STEP2
+  SINCOS_STEP3
+  SINCOS_STEP4
+  SINCOS_STEP5(s)
+  SINCOS_STEP6(c)
+  SINCOS_EPILOG(s,c,x)
 
   return XLAL_SUCCESS;
 }
