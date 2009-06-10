392bdbaa075a40142315cb99a263232839c2e13e
Adam Mercer <adam.mercer@ligo.org>
2009-06-10 16:45:34 -0500

    make lal build much quieter Original: b71e76efe9e11c37f65ea309022214728715bc5d

--- lal/misc/doc.mk
+++ lal/misc/doc.mk
@@ -3,23 +3,20 @@
 	@ if test ! -f .adoc/main.tex ; then \
 	    pkgdir=`cd .. && pwd` ; \
 	    package=`basename $$pkgdir` ; \
-	    echo "creating main.tex" ; \
 	    sed "s/@package@/$$package/" $(top_srcdir)/misc/main.tex.in > .adoc/main.tex ; \
 	  fi
 	@ for file in $(DOCSOURCEFILES) ; do \
 	    filepath=`cd $(srcdir) && pwd`/$$file ; \
 	    if test ! -f $(top_builddir)/doc/autodoc/$$file ; then \
 	      cmd="$(LN_S) $$filepath $(top_builddir)/doc/autodoc" ; \
-	      echo "$$cmd" ; \
 	      $$cmd ; \
 	    fi ; \
 	    if test ! -f .adoc/$$file ; then \
 	      cmd="$(LN_S) $$filepath .adoc" ; \
-	      echo "$$cmd" ; \
 	      $$cmd ; \
 	    fi ; \
 	  done
-	cd .adoc && ${LATEX} main ${TEXFLAGS} && ${MKIND} main \
+	@ cd .adoc && ${LATEX} main ${TEXFLAGS} && ${MKIND} main \
 	  && ${LATEX} main ${TEXFLAGS} && ${LATEX} main ${TEXFLAGS}
 	@ for file in main.dvi main.pdf main.ps ; do \
 	    if test -f .adoc/$$file ; then \
@@ -30,19 +27,16 @@
 
 ../include/.dvi-dep:
 	@set fnord $(MAKEFLAGS); amf=$$2; \
-	echo "Making dvi in ../include"; \
 	(cd ../include && $(MAKE) $(AM_MAKEFLAGS) dvi ) \
          || case "$$amf" in *=*) exit1;; *k*) fail=yes;; *) exit 1;; esac; \
 	test -z "$$fail"
 ../src/.dvi-dep:
 	@set fnord $(MAKEFLAGS); amf=$$2; \
-	echo "Making dvi in ../include"; \
 	(cd ../src && $(MAKE) $(AM_MAKEFLAGS) dvi ) \
          || case "$$amf" in *=*) exit1;; *k*) fail=yes;; *) exit 1;; esac; \
 	test -z "$$fail"
 ../test/.dvi-dep:
 	@set fnord $(MAKEFLAGS); amf=$$2; \
-	echo "Making dvi in ../include"; \
 	(cd ../test && $(MAKE) $(AM_MAKEFLAGS) dvi ) \
          || case "$$amf" in *=*) exit1;; *k*) fail=yes;; *) exit 1;; esac; \
 	test -z "$$fail"
