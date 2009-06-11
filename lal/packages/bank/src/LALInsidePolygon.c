cbdb06a39669bc1997b8333f630709bd2a927d46
Adam Mercer <adam.mercer@ligo.org>
2009-06-11 17:20:31 -0500

    don't shadow global declaration of y0 use x,y for the coordinates to be consistent with the function prototype Original: b5fd5361dbfdb36aa8c7a5b3c54db62ae75a84aa

--- lal/packages/bank/src/LALInsidePolygon.c
+++ lal/packages/bank/src/LALInsidePolygon.c
@@ -26,7 +26,7 @@ $Id$
 
 \subsection{Module \texttt{LALInsidePolygon.c}}
 
-Module to check whether a point with coordinates (x0,y0) is inside
+Module to check whether a point with coordinates (x,y) is inside
 a polygon defined by the vectors (vx, vy), which size (n) must be
 provided. The functions returns 1 if the point is inside or 0 otherwise.
 
@@ -37,7 +37,7 @@ provided. The functions returns 1 if the point is inside or 0 otherwise.
 \begin{itemize}
    \item \texttt{vx, vy} Input, two arrays of floats defining the polygon.
    \item \texttt{n} Input, the size of the vectors.
-   \item \texttt{x0, y0} Input, the coordinate of the point.
+   \item \texttt{x, y} Input, the coordinate of the point.
    \item \texttt{valid} Output, 0 if outside and 1 if inside.
 \end{itemize}
 
@@ -63,8 +63,8 @@ void LALInsidePolygon(  LALStatus          *status,
                         REAL4              *inputx,
                         REAL4              *inputy,
                         INT4               n,
-                        REAL4              x0,
-                        REAL4              y0,
+                        REAL4              x,
+                        REAL4              y,
                         INT4               *valid)
 
 
@@ -78,9 +78,9 @@ void LALInsidePolygon(  LALStatus          *status,
    {
      int i, j, c = 0;
      for (i = 0, j = n-1; i < n; j = i++) {
-       if ((((inputy[i] <= y0) && (y0 < inputy[j])) ||
-	    ((inputy[j] <= y0) && (y0 < inputy[i]))) &&
-	   (x0 < (inputx[j] - inputx[i]) * (y0 - inputy[i]) / (inputy[j] - inputy[i]) + inputx[i]))
+       if ((((inputy[i] <= y) && (y < inputy[j])) ||
+	    ((inputy[j] <= y) && (y < inputy[i]))) &&
+	   (x < (inputx[j] - inputx[i]) * (y - inputy[i]) / (inputy[j] - inputy[i]) + inputx[i]))
 	 c = !c;
      }
      *valid = c;
