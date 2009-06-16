8aa0a97020e635fe57811af5995a070e7e4e816b
Adam Mercer <adam.mercer@ligo.org>
2009-06-16 15:38:19 -0500

    don't shadow local declaration of psi Original: 1c9b1da838c28bae168b81adecc296800ff97fb3

--- lal/packages/findchirp/src/FindChirpSPTemplate.c
+++ lal/packages/findchirp/src/FindChirpSPTemplate.c
@@ -277,10 +277,10 @@ LALFindChirpSPTemplate (
     for ( k = kmin; k < kmax ; ++k )
     {
       REAL4 x_0 = x1 * xfac[k];
-      REAL4 psi = c0 * ( x_0 * ( c20 + x_0 * ( c15 + x_0 * (c10 + x_0 * x_0 ) ) )
+      REAL4 psi_0 = c0 * ( x_0 * ( c20 + x_0 * ( c15 + x_0 * (c10 + x_0 * x_0 ) ) )
                   + c25 - c25Log * log(x_0) + (1.0/x_0) * ( c30 - c30Log * log(x_0)
                   + (1.0/x_0) * ( c35 - (1.0/x_0) * c40P * log(x_0) ) ) );
-      REAL4 psi1 = psi + psi0;
+      REAL4 psi1 = psi_0 + psi0;
       REAL4 psi2;
 
       /* range reduction of psi1 */
