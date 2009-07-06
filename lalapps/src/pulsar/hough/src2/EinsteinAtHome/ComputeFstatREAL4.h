81941c1a8ee35038b04029f23ec9db5e7eb685a8
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2009-07-06 12:19:05 +0200

    fixed include path Original: bc56397c6df2330c1e3dd9d8a0b2ccf74e2a7f5a

--- lalapps/src/pulsar/hough/src2/EinsteinAtHome/ComputeFstatREAL4.h
+++ lalapps/src/pulsar/hough/src2/EinsteinAtHome/ComputeFstatREAL4.h
@@ -30,7 +30,7 @@ extern "C" {
 #include <lal/DetectorStates.h>
 #include <lal/ComputeFstat.h>
 
-#include "../hough/src2/HierarchicalSearch.h"
+#include "../HierarchicalSearch.h"
 
 /* ---------- exported defines and macros ---------- */
 
