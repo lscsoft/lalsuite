23ace7a9978299f43dbff19c5cc9bae0f0629371
Chad Hanna <chad.hanna@ligo.org>
2009-07-14 07:34:08 -0700

    changed image size Original: 813e0c0f030580318cb2932fa3af28aa4849a110

--- lalapps/src/inspiral/s5_highmass_upper_limit/make_summary_page.py
+++ lalapps/src/inspiral/s5_highmass_upper_limit/make_summary_page.py
@@ -14,12 +14,12 @@ class wiki(object):
 
   def image_link(self,path,webserver):
     thumb = "thumb_" + path
-    command = 'convert ' + path + ' -resize 400x400 ' + thumb
+    command = 'convert ' + path + ' -resize 300x300 ' + thumb
     print command
     popen = subprocess.Popen(command.split())
     popen.communicate()
     status = popen.returncode
-    s = '[[ImageLink('+webserver+'/'+thumb+','+webserver+'/'+path+',width=400][,alt=none])]]'
+    s = '[[ImageLink('+webserver+'/'+thumb+','+webserver+'/'+path+',width=300][,alt=none])]]'
     self.file.write(s)
 
   def image_table(self,image_list, webserver):
