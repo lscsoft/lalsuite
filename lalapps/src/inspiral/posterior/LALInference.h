443cd8b6a4b93781f87c186f44b7021475c696b5
Christian Roever <christian.roever@ligo.org>
2009-07-16 11:56:21 +0200

    added "printVariables()" function. Original: 1aace71f21796e7b02599eec0003a30465d8f754

--- lalapps/src/inspiral/posterior/LALInference.h
+++ lalapps/src/inspiral/posterior/LALInference.h
@@ -80,6 +80,7 @@ void removeVariable(LALVariables *vars,const char *name);
 int  checkVariable(LALVariables *vars,const char *name);
 void destroyVariables(LALVariables *vars);
 void copyVariables(LALVariables *origin, LALVariables *target);
+void printVariables(LALVariables *var);
 
 //Wrapper for template computation 
 //(relies on LAL libraries for implementation) <- could be a #DEFINE ?
