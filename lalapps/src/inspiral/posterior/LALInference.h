0bb75e0b007510a3963c44b92b28ba755655a76b
Christian Roever <christian.roever@ligo.org>
2009-07-16 11:10:51 +0200

    dded "copyVariables()" function. Original: fd9b4664ef2dc32e2876385ceb277df16583d81b

--- lalapps/src/inspiral/posterior/LALInference.h
+++ lalapps/src/inspiral/posterior/LALInference.h
@@ -79,6 +79,7 @@ void addVariable(LALVariables * vars, const char * name, void * value,
 void removeVariable(LALVariables *vars,const char *name);
 int  checkVariable(LALVariables *vars,const char *name);
 void destroyVariables(LALVariables *vars);
+void copyVariables(LALVariables *origin, LALVariables *target);
 
 //Wrapper for template computation 
 //(relies on LAL libraries for implementation) <- could be a #DEFINE ?
