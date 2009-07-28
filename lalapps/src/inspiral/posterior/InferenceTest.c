7101e1aa75f440ee159cc59f4049d8d0131ac978
Vivien Raymond <vivien.raymond@ligo.org>
2009-07-28 11:26:26 -0500

    output textfile of the data Original: 67d3adf9d1986ed2ef3645a8c3f9bedd8d46d2ed

--- lalapps/src/inspiral/posterior/InferenceTest.c
+++ lalapps/src/inspiral/posterior/InferenceTest.c
@@ -152,6 +152,20 @@ int main(int argc, char *argv[]){
 				  runstate->data->timeModelhCross->data->data[i]);
 	  }
 	  fclose(testout);
+	  testout=fopen("PSD.txt","w");
+	  for (i=0;i<runstate->data->oneSidedNoisePowerSpectrum->data->length;i++){
+		  fprintf(testout,"%g %g\n",i*runstate->data->oneSidedNoisePowerSpectrum->deltaF,
+				  runstate->data->oneSidedNoisePowerSpectrum->data->data[i]);
+	  }
+	  fclose(testout);
+	  testout=fopen("noise_TD.txt","w");
+	  for (i=0;i<runstate->data->timeData->data->length;i++){
+		  fprintf(testout,"%10.10lf %g\n",runstate->data->timeData->epoch.gpsSeconds+(1e-9*runstate->data->timeData->epoch.gpsNanoSeconds)+i*runstate->data->timeData->deltaT,
+				  runstate->data->timeData->data->data[i]);
+	  }
+	  fclose(testout);
+	  
+	  
 /* 
 //  templateStatPhase() test: 
     REAL8 mc   = 2.0;
