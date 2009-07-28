2c740230fdb9345c45d139112641767be266a86e
Adam Mercer <adam.mercer@ligo.org>
2009-07-28 11:59:39 -0500

    Revert "use absolute srcdir and builddir paths" This reverts commit b630b8978d4e2aefd1c87c2a9b2ac37f61003002. the autotools on CentOS are very old and don't understand the absolute path versions of the srcdir and builddir variables Original: ee91fa17ecb649008b574a0db9296d0242070aa6

--- lal/misc/include.mk
+++ lal/misc/include.mk
@@ -1,9 +1,10 @@
 ## This file gets automatically appended to the end of the Makefile
 include-link:
 	@for file in $(HEADERS) ; do \
-		targetdir="$(abs_top_builddir)/include/lal"; \
+		sourcedir=`cd $(srcdir) && pwd`; \
+		targetdir=`cd $(top_builddir)/include/lal && pwd`; \
 		if test ! -r $$targetdir/$$file ; then \
 			rm -f $$targetdir/$$file; \
-			$(LN_S) $(abs_srcdir)/$$file $$targetdir; \
+			$(LN_S) $$sourcedir/$$file $$targetdir; \
 		fi; \
 	done
