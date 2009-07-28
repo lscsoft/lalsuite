f3f559dfa374d6365717ca41b0d8be28d025758a
Adam Mercer <adam.mercer@ligo.org>
2009-07-28 15:38:04 +0100

    use absolute srcdir and builddir paths Original: b630b8978d4e2aefd1c87c2a9b2ac37f61003002

--- lal/misc/include.mk
+++ lal/misc/include.mk
@@ -1,10 +1,9 @@
 ## This file gets automatically appended to the end of the Makefile
 include-link:
 	@for file in $(HEADERS) ; do \
-		sourcedir=`cd $(srcdir) && pwd`; \
-		targetdir=`cd $(top_builddir)/include/lal && pwd`; \
+		targetdir="$(abs_top_builddir)/include/lal"; \
 		if test ! -r $$targetdir/$$file ; then \
 			rm -f $$targetdir/$$file; \
-			$(LN_S) $$sourcedir/$$file $$targetdir; \
+			$(LN_S) $(abs_srcdir)/$$file $$targetdir; \
 		fi; \
 	done
