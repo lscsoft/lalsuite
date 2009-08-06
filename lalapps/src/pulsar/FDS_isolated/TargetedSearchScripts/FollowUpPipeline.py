468f2ca5980003adca550a40d040920fec3f81c3
Joseph Betzwieser <joseph.betzwieser@ligo.org>
2009-08-06 12:41:22 -0700

    Latest working version Original: c935cfbe8da0474b66c49132e691f077060d5d5c

--- lalapps/src/pulsar/FDS_isolated/TargetedSearchScripts/FollowUpPipeline.py
+++ lalapps/src/pulsar/FDS_isolated/TargetedSearchScripts/FollowUpPipeline.py
@@ -31,21 +31,23 @@ Usage: FollowUpPipeline.py [options]
 
   -h, --help               display this message
   -o, --outputDirectory    Location where the output files will be placed, e.g. ./
-  -c, --configFile         File which has default parameters for the follow-ups, i.e. ./followup_config.ini
+  -C, --configFile         File which has default parameters for the follow-ups, i.e. ./followup_config.ini
   -d, --dataFile           File containing candidates to be examined, i.e. ./stage2.csv
   -s, --searchName         Label to be applied to the search files, i.e. Powerflux_S5_outliers
+  -L, --logFileDirectory   Location where the condor .sub log file will be placed
 """
   print >> sys.stderr, msg
 
 ################################################
 #This section reads in the command line arguments
-shortop = "ho:c:d:s:"
+shortop = "ho:C:d:s:L:"
 longop = [
   "help",
   "outputDirectory=",
   "configFile",
   "dataFile=",
   "searchName=",
+  "logFileDirectory=",
   ]
 
 try:
@@ -61,17 +63,49 @@ for o, a in opts:
     sys.exit(0)
   elif o in ("-o","--outputDirectory"):
     output_directory = str(a)
-  elif o in ("-c","--configFile"):
+  elif o in ("-C","--configFile"):
     config_file = str(a)
   elif o in ("-d","--dataFile"):
-    data_file = str(a)
+    user_data_file = str(a)
   elif o in ("-s","--searchName"):
     search_name = str(a)
+  elif o in ("-L","--LogFileDirectory"):
+    log_file_directory = str(a)
   else:
     print >> sys.stderr, "Unknown option:", o
     usage()
     sys.exit(1)
 
+#################################################
+#Makes a folder for this search
+if os.path.exists(output_directory):
+  print "Warning: " + output_directory + " exists but still continuing with DAG generation"
+else:
+  try:
+    os.mkdir(output_directory)
+  except:
+    print "Error encountered trying to make directory: " + output_directory 
+    sys.exit(1)
+
+false_alarm_directory = output_directory.rstrip('/') + 'false_alarms'
+if os.path.exists(false_alarm_directory):
+  print "Warning: " + false_alarm_directory + " exists but still continuing"
+else:
+  try:
+    os.mkdir(false_alarm_directory)
+  except:
+    print "Error encountered trying to make directory: " + false_alarm_directory
+    sys.exit(1)
+    
+event_directory = output_directory.rstrip('/') + 'events'
+if os.path.exists(event_directory):
+  print "Warning: " + event_directory + " exists but still continuing"
+else:
+  try:
+    os.mkdir(event_directory)
+  except:
+    print "Error encountered trying to make directory: " + event_directory
+    sys.exit(1)
 ###########################################
 #Create a log file with the input parameters
 run_log_file = ''.join([output_directory.rstrip('/'),'/FollowUpPipeline.log'])
@@ -84,8 +118,11 @@ rlf.close()
 #Parses the config file, if any
 #The config file is overriden by command line parameters
 
-# Import the Configuration File into config
-config = __import__(config_file);
+config_file = os.path.abspath(config_file)
+sys.path.append(os.path.dirname(config_file))
+config = __import__(os.path.basename(config_file).strip('.py'));
+
+
 
 # Variables Structure/Dictionary
 Vars = {}
@@ -103,7 +140,6 @@ try:
 except:
   print "delta_band cannot be read"
   sys.exit(1)
-    
     
 # Band in Frequency (Hz)
 try:
@@ -201,25 +237,40 @@ try:
   Vars['max_number_of_jobs'] = config.max_number_of_jobs
 except:
   print "max_number_of_jobs cannot be read"
-  sys.exit(1)    
+  sys.exit(1)
+
+# Code to be used for search
+try:
+  Vars['code'] = config.code
+except:
+  print "code (the configuration variable) cannot be read"
+  sys.exit(1)
+
+# Directory containing the python scripts
+try:
+  Vars['python_dir'] = config.python_dir
+except:
+  print "python_dir cannot be read"
+  sys.exit(1)
+
+# Initial coherence time
+try:
+  Vars['start_coherence_time'] = config.start_coherence_time
+except:
+  print "start_coherence_time cannot be read"
+  sys.exit(1)
+  
 
 #############################
 #End user input section
 
-#################################################
-#Makes a folder for this search iteration and moves to it
-base_label = output_label + '_' + str(iteration)
-
-run_directory = "./" + base_label + "_run/"
-os.mkdir(run_directory)
   
 ###############################################################################
 #This function parses the data file and generates the .dag file which is submitted on the cluster
 
 def parse_Powerflux(data_file):
-
-  main_dag_file_name = ''.join([output_directory.rstrip('/'),search_name,'.dag'])
-  main_sub_file_name = ''.join([output_directory.rstrip('/'),search_name,'.sub'])
+  main_dag_file_name = ''.join([output_directory.rstrip('/'),'/',search_name,'.dag'])
+  main_sub_file_name = ''.join([output_directory.rstrip('/'),'/',search_name,'.sub'])
 
   dag_file = open(main_dag_file_name,'w')
 
@@ -228,70 +279,71 @@ def parse_Powerflux(data_file):
   dag_file.write('# Dag for base_name = ' + search_name + '\n')
   job = 0
   for line in data:
-    alpha = line.split('\t')[22]
-    delta = line.split('\t')[23]
-    f0 = line.split('\t')[20]
-    f1 = line.split('\t')[21]
-    f2 = 0
-    f3 = 0
-    outlier_number = line.split('\t')[0]
-    base_name = search_name + '_' + outlier_number
+    if line[0] != '"':
+      alpha = line.split('\t')[22]
+      delta = line.split('\t')[23]
+      f0 = line.split('\t')[20]
+      f1 = line.split('\t')[21]
+      f2 = 0
+      f3 = 0
+      outlier_number = line.split('\t')[0]
+      base_name = search_name + '_' + outlier_number
 
-    arg_list = ' '.join(['argList="',
-                         '-o',base_name,
-                         '-C',config_file,
-                         '-i','0',
-                         '-a',alpha,
-                         '-d',delta,
-                         '-z',str(Vars['alpha_band']),
-                         '-c', str(Vars['delta_band']),
-                         '-f', f0,
-                         '-b',str(Vars['f0_band']),
-                         '-D', str(list_files),
-                         '-I', str(ifos),
-                         '-S', str(Vars['start_time']),
-                         '-E', str(Vars['end_time']),
-                         '-T', str(Vars['param_time']),
-                         '-s', f1,
-                         '-m', str(Vars['f1_band']),
-                         '--F2', str(f2),
-                         '--F2Band', str(Vars['f2_band']),
-                         '--F3', str(f3),
-                         '--F3Band', str(Vars['f3_band']),
-                         '-M', str(Vars['mismatch']),
-                         '-e', str(Vars['ephem_dir']),
-                         '-y', str(Vars['ephem_year']),
-                         '-F', str(cutoff2F),
-                         '-X', str(search_code),
-                         '-L', str(user_log),
-                         '"'])
+      arg_list = ' '.join(['argList="',
+                           '-o',base_name,
+                           '-C',config_file,
+                           '-i','0',
+                           '-a',alpha,
+                           '-d',delta,
+                           '-z',str(Vars['alpha_band']),
+                           '-c', str(Vars['delta_band']),
+                           '-f', f0,
+                           '-b',str(Vars['f0_band']),
+                           '-T', str(Vars['param_time']),
+                           '-s', f1,
+                           '-m', str(Vars['f1_band']),
+                           '--F2', str(f2),
+                           '--F2Band', str(Vars['f2_band']),
+                           '--F3', str(f3),
+                           '--F3Band', str(Vars['f3_band']),
+                           '-t',str(Vars['start_coherence_time']),
+                           '-L', str(log_file_directory),
+                           '"'])
+      
+      
+      dag_file.write('JOB A' + str(job) + ' ' + main_sub_file_name +'\n')
+      dag_file.write('VARS A' + str(job) + ' ' + 'JobID="' + str(job) + '" ' + arg_list + '\n')
+      dag_file.write('\n')
+      job += 1
 
-                           
-    dag_file.write('JOB A' + str(job) + ' ' + sub_file_name +'\n')
-    dag_file.write('VARS A' + str(job) + ' ' + 'JobID="' + str(job) + '" ' + arg_list + '\n')
-    dag_file_handle.write('\n')
-    job += 1
+  dag_file.close()
 
-dag_file_handle.close()
+  ########################################################
+  #This step creates the .sub file(s) called by the .dag file
 
-########################################################
-#This step creates the .sub file(s) called by the .dag file
+  sub_file = open(main_sub_file_name,'w')
 
-sub_file = open(main_sub_file_name,'w')
+  #This sub file is used to perform the actual search
+  sub_file.write('universe= vanilla \n')
+  sub_file.write('executable = ' + Vars['python_dir'].rstrip('/') + '/' + 'FstatFollowUp.py' +'\n')
+  sub_file.write('output = node_' + search_name + '_A.out.$(JobID)\n')
+  sub_file.write('error = node_' + search_name + '_A.err.$(JobID)\n')
+  sub_file.write('log = ' + log_file_directory.rstrip('/') + '/' + search_name + '.log\n' )
+  sub_file.write('\n')
+  sub_file.write('arguments = $(arglist)\n')
+  sub_file.write('queue\n')
 
-#This sub file is used to perform the actual search
-sub_file.write('universe= vanilla \n')
-sub_file.write('executable = ' + code +'\n')
-sub_file.write('output = node_' + base_label + '_A.out.$(JobID)\n')
-sub_file.write('error = node_' + base_label + '_A.err.$(JobID)\n')
-sub_file.write('log = ' + log_file_directory.rstrip('/') + '/' + base_label + '.log\n' )
-sub_file.write('\n')
-sub_file.write('arguments = $(arglist)\n')
-sub_file.write('queue\n')
+  sub_file.close()
 
-sub_file_handle.close()
 
+###################################
+#Calls the correct function to parse the data file.
+#Only one type of file and function at the moment
+
+parse_Powerflux(user_data_file)
 
 
 ######################################
 #This step submits the job to Condor
+
+
