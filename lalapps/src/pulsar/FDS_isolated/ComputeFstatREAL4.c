lscsoft/lalsuite-archive

commit 13519eb73ae71cbbdaf3d9aa54d4d3649c83b0e8

move determination of spindown-order outside of central (kernel) function
   - spdnOrder is a constant so we need to determine this only once in the
     higher-level function

lalapps/src/pulsar/FDS_isolated/ComputeFstatREAL4.c
@@ -164,6 +164,12 @@ XLALComputeFStatFreqBandVector (   REAL4FrequencySeriesVector *fstatBandV, 		/**
   /* the remaining spins are simply down-cast to REAL4 */
   for ( s=1; s < PULSAR_MAX_SPINS; s ++ )
     fkdot4.fkdot[s] = (REAL4) doppler->fkdot[s];
+  UINT4 maxs;
+  /* find highest non-zero spindown-entry */
+  for ( maxs = PULSAR_MAX_SPINS - 1;  maxs > 0 ; maxs --  )
+    if ( fkdot4.fkdot[maxs] != 0 )
+      break;
+  fkdot4.spdnOrder = maxs;
 
   /* make sure sin/cos lookup-tables are initialized */
   init_sin_cos_LUT_REAL4();
@@ -498,7 +504,6 @@ XLALComputeFaFbREAL4 ( FcomponentsREAL4 *FaFb,		/**< [out] single-IFO Fa/Fb for 
   static const char *fn = "XLALComputeFaFbREAL4()";
 
   UINT4 alpha;                 	/* loop index over SFTs */
-  UINT4 spdnOrder;		/* maximal spindown-orders */
   UINT4 numSFTs;		/* number of SFTs (M in the Notes) */
   COMPLEX8 Fa, Fb;
   REAL4 Tsft; 			/* length of SFTs in seconds */
@@ -543,11 +548,6 @@ XLALComputeFaFbREAL4 ( FcomponentsREAL4 *FaFb,		/**< [out] single-IFO Fa/Fb for 
   REAL4 df = fkdot4->fkdot[0];
   REAL4 tau = 1.0f / df;
   REAL4 Freq = f0 + df;
-
-  /* find highest non-zero spindown-entry */
-  for ( spdnOrder = PULSAR_MAX_SPINS - 1;  spdnOrder > 0 ; spdnOrder --  )
-    if ( fkdot4->fkdot[spdnOrder] )
-      break;
 
   Fa.re = 0.0f;
   Fa.im = 0.0f;
@@ -607,7 +607,7 @@ XLALComputeFaFbREAL4 ( FcomponentsREAL4 *FaFb,		/**< [out] single-IFO Fa/Fb for 
 
         /* higher-order spindowns */
         Tas = deltaT;
-	for (s=1; s <= spdnOrder; s++)
+	for (s=1; s <= fkdot4->spdnOrder; s++)
 	  {
 	    REAL4 fsdot = fkdot4->fkdot[s];
 	    Dphi_alpha_rem += fsdot * Tas * inv_fact[s]; 	/* here: Tas = DT^s */

lalapps/src/pulsar/FDS_isolated/ComputeFstatREAL4.h
@@ -40,6 +40,7 @@ extern "C" {
 typedef struct {
   REAL4 FreqMain;			/**< "main" part of frequency fkdot[0], normally just the integral part */
   REAL4 fkdot[PULSAR_MAX_SPINS];	/**< remaining spin-parameters, including *fractional* part of Freq = fkdot[0] */
+  UINT4 spdnOrder;			/**< highest non-zero spindown order (ie maximal value of 'k' in fkdot = df^(k)/dt^k */
 } PulsarSpinsREAL4;
 
 

lscsoft/lalsuite

commit a1f801ae275d2b01c371a458cac8265e19e6a5ee

updated XLALDriverFstatREAL4() to work correctly with pre-computed spindown-order    - this fixes CFSv2 to work correctly after patch 13519eb73ae71cbbdaf3d9aa54d4d3649c83b0e8,      which broke testCFSv2GPU.sh Original: bef789994cc5ddb966a8bed90af628cbdefb4635

lalapps/src/pulsar/FDS_isolated/ComputeFstatREAL4.c
@@ -393,7 +393,13 @@ XLALDriverFstatREAL4 ( REAL4 *Fstat,	                 		/**< [out] Fstatistic va
   /* the remaining spins are simply down-cast to REAL4 */
   for ( s=1; s < PULSAR_MAX_SPINS; s ++ )
     fkdot4.fkdot[s] = (REAL4) doppler->fkdot[s];
-
+  /* find largest non-zero spindown order */
+  UINT4 maxs;
+  for ( maxs = PULSAR_MAX_SPINS - 1;  maxs > 0 ; maxs --  )
+    if ( doppler->fkdot[maxs] != 0 )
+      break;
+  fkdot4.spdnOrder = maxs;
+  fkdot4.refTime = doppler->refTime;
 
   /* call the core function to compute one multi-IFO F-statistic */
 