2beac9bdd820778e0e9184d068787e73278230bd
Chad Hanna <chad.hanna@ligo.org>
2009-08-11 21:23:22 -0700

    got bankveto working again Original: fc37b3c611178e6ab94cb314400073e093726145

--- lal/packages/findchirp/src/FindChirpBankVeto.c
+++ lal/packages/findchirp/src/FindChirpBankVeto.c
@@ -55,12 +55,12 @@ NRCSID (FINDCHIRPBANKVETOC, "$Id$");
 
 static int compareTemplateByLevel (const void * a, const void * b);
 static int compareTemplateByChirpMass (const void * a, const void * b);
-static int compareTemplateByEta (const void * a, const void * b);
+static int compareTemplateByMass (const void * a, const void * b);
 static int  breakUpRegions(InspiralTemplate *bankHead, UINT4 num, UINT4 subbanksize );
 static InspiralTemplate *
 XLALFindChirpSortTemplatesByChirpMass( InspiralTemplate *bankHead, UINT4 num );
 static InspiralTemplate *
-XLALFindChirpSortTemplatesByEta( InspiralTemplate *bankHead, UINT4 num );
+XLALFindChirpSortTemplatesByMass( InspiralTemplate *bankHead, UINT4 num );
 static InspiralTemplate *
 XLALFindChirpSortTemplatesByLevel( InspiralTemplate *bankHead, UINT4 num);
 
@@ -200,27 +200,30 @@ XLALComputeFullChisq(
 
 {
   UINT4 k;
-  REAL4 chisqnorm, tmp, C, chisq, angle, snr, snrk;
+  REAL4 chisqnorm, tmp, C, chisq, angle, snri, snrk;
   /* input, params and norm are unused */
   UNUSED(input);
   UNUSED(params);
   /* test isn't being done */
+
   if (!bankVetoData->acorrMat) return 0.0;
   chisq = 0;
   chisqnorm = 0;
 
   angle = atan2(q[snrIX].im, q[snrIX].re);
-  snr = q[snrIX].re * cos(angle) + q[snrIX].im * sin(angle);
-
+  snri = q[snrIX].re * cos(angle) + q[snrIX].im * sin(angle);
+  *dof = 0;
   /*Start at k=1 because k=0 is Gauranteed to be zero by construction */
-  for (k = 1; k < bankVetoData->acorrMatSize; k++)
+  for (k = 0; k < bankVetoData->acorrMatSize; k++)
   {
-    snrk = q[snrIX-k].re * cos(angle) + q[snrIX-k].im * sin(angle);
+    /* we skipped saving the first sample of the autocorrelation which is why */
+    /* we need the -1 here */
+    snrk = q[snrIX-k-1].re * cos(angle) + q[snrIX-k-1].im * sin(angle);
     C = bankVetoData->acorrMat->data[i * bankVetoData->acorrMatSize + k];
-    *dof++;
+    (*dof) += 1;
     /*FIXME what? This should never happen, and it probably doesn't but if it did it would screw the whole value */
     if (C >=1 ) continue;
-    tmp = snrk - C * snr;
+    tmp = snrk - C * snri;
     chisq += tmp * tmp * norm;
     chisqnorm += 1.0 / (1.0 - C * C);
   }
@@ -230,6 +233,7 @@ XLALComputeFullChisq(
     fprintf(stderr,"chisq < 0");
     exit(1);
   }
+
   return chisq;  
 }
 
@@ -246,13 +250,11 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
   UINT4 i,j,k,iMax,jMax,correctFlag;
   UINT4 iSize = bankVetoData->length;
   UINT4 tmpLen = bankVetoData->fcInputArray[0]->fcTmplt->data->length;
-  REAL4 ABr, ABi, Br, Bi, sqResp;
+  REAL8 ABr, ABi, Br, Bi, sqResp, normfac;
   UINT4 stIX = floor( fLow / deltaF );
-  /*char fname[30];
-  FILE *FP = NULL;*/
 
   /* FIXME this should be a command line argument */
-  bankVetoData->acorrMatSize = 30; /*30 points of autocorrelation function stored */
+  bankVetoData->acorrMatSize = 200; /*200 points of autocorrelation function stored */
 
   /* if there isn't alread memory allocated for workspace and the autocorrelation 
    * then do it
@@ -272,9 +274,14 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
     {
       /* FIXME find an XLAL Function to do this?*/
       if (i == j) memset(bankVetoData->workspace->data, 0, bankVetoData->workspace->length * sizeof(COMPLEX8));
-      if ( i >= vetoBank->subBankSize )
+      if ( (i >= vetoBank->subBankSize) || (j >= vetoBank->subBankSize) )
       {
-        bankVetoData->ccMat->data[i*iSize + j] = 0;
+        bankVetoData->ccMat->data[i*iSize + j].re = 0.0;
+        bankVetoData->ccMat->data[j*iSize + i].re = 0.0;
+        bankVetoData->ccMat->data[i*iSize + j].im = 0.0;
+        bankVetoData->ccMat->data[j*iSize + i].im = 0.0;
+        bankVetoData->normMat->data[i*iSize + j] = 0.0;
+        bankVetoData->normMat->data[j*iSize + i] = 0.0;
       }
       else
       {
@@ -292,8 +299,6 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
 
           if ( bankVetoData->spec->data[k] != 0 && sqResp != 0 )
           {
-            /* FIXME set spec to 1 for testing */
-             /*bankVetoData->spec->data[k] = 1.0; */
             /* Convention is that i is 'in the data' */
             Br = bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].re /
                  ( bankVetoData->spec->data[k] * sqResp ) *
@@ -312,7 +317,7 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
           /* if the templates are the same then do an autocorrelation function */
           if (i==j) 
           { 
-            bankVetoData->workspace->data[k].re = Br * bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].re 
+            bankVetoData->workspace->data[k].re = (REAL4) Br * bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].re 
                                                 + Bi * bankVetoData->fcInputArray[i]->fcTmplt->data->data[k].im;
             bankVetoData->workspace->data[k].im = 0.0; /* must be 0 */
           }
@@ -321,19 +326,12 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
         if (i==j)
         {
           XLALREAL4ReverseFFT( bankVetoData->acorr, bankVetoData->workspace, bankVetoData->revplan );
-          /*fprintf(stderr, "\n storing autocorrelation \n");
-          sprintf(fname, "chisqtest%d.txt", i);
-          FP = fopen(fname, "w");
-          for (k=0; k < bankVetoData->acorr->length; k++)
-          {
-            fprintf(FP,"%d %e\n",k, bankVetoData->acorr->data[k] / bankVetoData->acorr->data[0]);
-          }
-          fclose(FP);*/
-	  for (k=0; k < bankVetoData->acorrMatSize; k++)
+          /* since the normalized first sample of the auto correlation is always 1, lets skip it */
+	  for (k=1; k < bankVetoData->acorrMatSize + 1; k++)
           {
             /* Save the last acorrMatSize samples */
-            bankVetoData->acorrMat->data[i*bankVetoData->acorrMatSize + k] = bankVetoData->acorr->data[k] / bankVetoData->acorr->data[0];
-            if (bankVetoData->acorrMat->data[i*bankVetoData->acorrMatSize + k] > 1.0) 
+            bankVetoData->acorrMat->data[i*bankVetoData->acorrMatSize + k-1] = bankVetoData->acorr->data[k] / bankVetoData->acorr->data[0];
+            if (bankVetoData->acorrMat->data[i*bankVetoData->acorrMatSize + k-1] > 1.0) 
             {
               fprintf(stderr, "Autocorrelation > 1.0\n");
               exit(1);
@@ -341,15 +339,31 @@ XLALBankVetoCCMat ( FindChirpBankVetoData *bankVetoData,
            
           }
         }
-        bankVetoData->ccMat->data[i*iSize + j] = sqrt(ABr*ABr+ABi*ABi);
-        bankVetoData->ccMat->data[j*iSize + i] = sqrt(ABr*ABr+ABi*ABi);
+        bankVetoData->ccMat->data[i*iSize + j].re = (REAL4) ABr;
+        bankVetoData->ccMat->data[j*iSize + i].re = (REAL4) ABr;
+        bankVetoData->ccMat->data[i*iSize + j].im = (REAL4) ABi;
+        bankVetoData->ccMat->data[j*iSize + i].im = (REAL4) -ABi;
+
         /* This is hermitian so the imaginary flips sign */
-        bankVetoData->normMat->data[i*iSize + j] = atan2(-ABi, ABr);
+        bankVetoData->normMat->data[i*iSize + j] = (REAL4) ABr;
         /* the convention is that i is in the data */
-        bankVetoData->normMat->data[j*iSize + i] = atan2(ABi, ABr);
+        bankVetoData->normMat->data[j*iSize + i] = (REAL4) ABr;
       }
     }
-  }
+  } /* end loop over templates */
+
+  for ( i = 0; i < iSize; i++ ) {
+    if (i >= vetoBank->subBankSize) break;
+    for ( j = i; j < iSize; j++ ) {
+      if (j >= vetoBank->subBankSize) break;
+      normfac = (REAL8) 1.0 / sqrt(  bankVetoData->normMat->data[i*iSize + i] * bankVetoData->normMat->data[j*iSize + j]);
+      bankVetoData->ccMat->data[i*iSize + j].re *= (REAL4) normfac;
+      if (j != i) bankVetoData->ccMat->data[j*iSize + i].re *= (REAL4) normfac;
+      bankVetoData->ccMat->data[i*iSize + j].im *= (REAL4) normfac;
+      if (j != i) bankVetoData->ccMat->data[j*iSize + i].im *= (REAL4) normfac;
+    }
+  } /* end norm loop */
+
 }
 
 
@@ -360,69 +374,72 @@ XLALComputeBankVeto( FindChirpBankVetoData *bankVetoData,
                      UINT4 *dof)
 {
 
+  
+  REAL8 iSNR_r = 0;
+  REAL8 iSNR_i = 0;
+  REAL8 iSNR = 0;
+  REAL8 jSNR_r = 0;
+  REAL8 jSNR_i = 0;
+  REAL8 chisq = 0;
+  REAL8 phi = 0;
+  UINT8 j = 0;
+  COMPLEX16 ij;
+  REAL8 ijsq;
 
-  REAL4 iSNR_r = 0;
-  REAL4 iSNR_i = 0;
-  REAL4 jSNR_r = 0;
-  REAL4 jSNR_i = 0;
-  REAL4 iSNR = 0;
-  REAL4 jSNR = 0;
-  REAL4 chisq = 0;
-  UINT4 j = 0;
-  REAL4 ii,jj,ji,denomFac,phi_i, phi_j, chi_i, chi_r;
+  REAL8 denomFac, bankNorm, chi_i, chi_r;
   UINT4 iSize = bankVetoData->length;
   iSNR_r = bankVetoData->qVecArray[i]->data[snrIX].re
          * sqrt(bankVetoData->fcInputArray[i]->fcTmplt->norm);
   iSNR_i = bankVetoData->qVecArray[i]->data[snrIX].im
          * sqrt(bankVetoData->fcInputArray[i]->fcTmplt->norm);
-  iSNR = sqrt(iSNR_r*iSNR_r + iSNR_i*iSNR_i);
-
-  phi_i =  atan2(iSNR_i,  iSNR_r);
-
+  iSNR = sqrt(iSNR_r * iSNR_r + iSNR_i * iSNR_i);
+  phi = atan2(iSNR_i, iSNR_r);
+  bankNorm = 0.0;
+  denomFac = 0.0;
   *dof = 0;
   if (iSize == 1) return 0;
 
   for (j = 0; j < iSize; j++)
   {
-    ji = bankVetoData->ccMat->data[j*iSize + i];
-    ii = bankVetoData->ccMat->data[i*iSize + i];
-    jj = bankVetoData->ccMat->data[j*iSize + j];
-    phi_j = bankVetoData->normMat->data[j*iSize + i];
-    denomFac =  1.0 - (ji * ji / ii / jj) ;
+    if (i == j) continue;
+    ij.re = bankVetoData->ccMat->data[i*iSize + j].re * cos(phi) - bankVetoData->ccMat->data[i*iSize + j].im * sin(phi);
+    ij.im = bankVetoData->ccMat->data[i*iSize + j].im * cos(phi) + bankVetoData->ccMat->data[i*iSize + j].re * sin(phi);
+    ijsq = ij.re * ij.re + ij.im * ij.im;
+    if ( ijsq == 0 ) continue; 
+    denomFac =  1.0 / (1.0 - ijsq);
 
-    if ( denomFac != 0.0 && ji != 0.0 && ii != 0 && jj != 0 )
+    if ( denomFac > 0.0 )
     {
+      bankNorm += denomFac;
       jSNR_r = bankVetoData->qVecArray[j]->data[snrIX].re
              * sqrt(bankVetoData->fcInputArray[j]->fcTmplt->norm);
       jSNR_i = bankVetoData->qVecArray[j]->data[snrIX].im
              * sqrt(bankVetoData->fcInputArray[j]->fcTmplt->norm);
-      jSNR = sqrt(jSNR_r*jSNR_r + jSNR_i*jSNR_i);
-
-      chi_r = (jSNR_r*cos(0.0-phi_j) - jSNR_i*sin(0.0-phi_j)) -
-              ji / sqrt(ii*jj) * (iSNR_r);
-
-      chi_i = (jSNR_r*sin(0.0-phi_j) + jSNR_i*cos(0.0-phi_j)) -
-              ji / sqrt(ii*jj) * (iSNR_i);
-      /* Previously I used the modulus only */
-      /*  chisq += (jSNR - ji / sqrt(ii*jj) * iSNR)                        */
-      /*         * (jSNR - ji / sqrt(ii*jj) * iSNR) / denomFac;            */
-
-      /* Now I use the real and imaginary parts seperately */
-      chisq += chi_r*chi_r/denomFac + chi_i*chi_i/denomFac;
-
+      
+      chi_r = jSNR_r - ij.re * (iSNR);
+      chi_i = jSNR_i - ij.im * (iSNR);
+      chisq += chi_r*chi_r + chi_i*chi_i;
       (*dof)++;
     }
+    else 
+    {
+      fprintf(stderr, "denom fac < 0 ij.re %e partial denom_fac %e\n", ij.re, denomFac);
+      exit(1);
+    }
   }
-  return chisq;
+
+  /*FIXME Normalization now not necessarily chisquare distributed */
+  return (REAL4) chisq / bankNorm;
 }
 
 
 InspiralTemplate *
 XLALFindChirpSortTemplates( InspiralTemplate *bankHead, UINT4 num, UINT4 subbanksize)
   {
-  bankHead = XLALFindChirpSortTemplatesByEta(bankHead,num);
-  breakUpRegions(bankHead, num, subbanksize );
-  return XLALFindChirpSortTemplatesByLevel(bankHead,num);
+  bankHead = XLALFindChirpSortTemplatesByMass(bankHead,num);
+  return bankHead;
+  /*breakUpRegions(bankHead, num, subbanksize );
+  return XLALFindChirpSortTemplatesByLevel(bankHead,num);*/
   }
 
 
@@ -454,7 +471,7 @@ XLALFindChirpSortTemplatesByChirpMass( InspiralTemplate *bankHead, UINT4 num )
 }
 
 static InspiralTemplate *
-XLALFindChirpSortTemplatesByEta( InspiralTemplate *bankHead, UINT4 num )
+XLALFindChirpSortTemplatesByMass( InspiralTemplate *bankHead, UINT4 num )
   {
   InspiralTemplate **bankArray = NULL;
   InspiralTemplate *bankFirst = NULL;
@@ -467,7 +484,7 @@ XLALFindChirpSortTemplatesByEta( InspiralTemplate *bankHead, UINT4 num )
     bankArray[i] = bankHead; /* populate pointer array */
   }
 
-  qsort(bankArray, num, sizeof(InspiralTemplate *), compareTemplateByEta);
+  qsort(bankArray, num, sizeof(InspiralTemplate *), compareTemplateByMass);
 
   bankFirst = bankHead = bankArray[0];
   /* repopulate linked list */
@@ -490,6 +507,7 @@ static int  breakUpRegions(InspiralTemplate *bankHead, UINT4 num, UINT4 subbanks
   for (i = 0; i < num; bankHead = bankHead->next, i++)
     {
     bankHead->level = (UINT4) (i % subbanknum);
+    /*fprintf(stderr, "%d %e %e\n", bankHead->level, bankHead->chirpMass, bankHead->eta);*/
     }
   return 0;
   }
@@ -550,10 +568,10 @@ static int compareTemplateByChirpMass (const void * a, const void * b)
   return 0;
 }
 
-static int compareTemplateByEta (const void * a, const void * b)
+static int compareTemplateByMass (const void * a, const void * b)
 {
-  const REAL4 mVal1 = ((const InspiralTemplate*)a)->eta;
-  const REAL4 mVal2 = ((const InspiralTemplate*)b)->eta;
+  const REAL4 mVal1 = ((const InspiralTemplate*)a)->mass1 + ((const InspiralTemplate*)a)->mass2;
+  const REAL4 mVal2 = ((const InspiralTemplate*)b)->mass1 + ((const InspiralTemplate*)b)->mass2;
 
   if ( mVal1 > mVal2 ) return 1;
   if ( mVal1 == mVal2 ) return 0;
