36f470473f83ade95294f1c707272c202e260741
Lisa Goggin <lisa.goggin@ligo.org>
2009-08-25 13:44:11 -0500

    added a function to calculate the maximum time error. This is need for ligolw_rinca. Original: 62edbc92591e1c3b238c2410f0250301928c4645

--- lal/packages/ring/include/Ring.h
+++ lal/packages/ring/include/Ring.h
@@ -193,6 +193,7 @@ REAL4 XLALBlackHoleRingAmplitude( REAL4 f, REAL4 Q, REAL4 r, REAL4 epsilon );
 REAL4 XLALBlackHoleRingEpsilon( REAL4 f, REAL4 Q, REAL4 r, REAL4 amplitude );
 REAL4 XLAL2DRingMetricDistance( REAL4 fa, REAL4 fb, REAL4 Qa, REAL4 Qb );
 REAL4 XLAL3DRingMetricDistance( REAL4 fa, REAL4 fb, REAL4 Qa, REAL4 Qb, REAL8 dt );
+REAL4 XLALRingdownTimeError( const SnglRingdownTable *table,  REAL4 ds_sq );
 
 int XLALComputeRingTemplate(
     REAL4TimeSeries *output, SnglRingdownTable *input );
