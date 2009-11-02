a51a8bcfaece52d41ee6575d14771d8a08212c4f
Stephen Privitera <stephen.privitera@ligo.org>
2009-11-02 09:46:38 -0800

    Added create subbank call to FindChirpBankVetoTest.c Original: 7628ad601a82705da8651757687933f0af7494c0

--- lal/packages/findchirp/test/FindChirpBankVetoTest.c
+++ lal/packages/findchirp/test/FindChirpBankVetoTest.c
@@ -25,7 +25,6 @@ int main(int argc, char ** argv)
     CHAR ccOutFileName[100];
     sprintf(tmpltBankFileName,"%s","in.xml");
     sprintf(sortedBankFileName,"%s","sorted.xml");
-    sprintf(subBankFileName,"%s","sub.xml");
     sprintf(ccOutFileName,"%s","ccOut.xml");
 
     /* Constructs needed for writing xml output */
@@ -36,11 +35,13 @@ int main(int argc, char ** argv)
     /* Input to sub bank functions */
     InspiralTemplate *bankHead = NULL;
     InspiralTemplate *bank = NULL;
+    FindChirpSubBank *subBankHead = NULL;
+    FindChirpSubBank *subBank = NULL;
+
     UINT4 numberTemplates;
     UINT4 subBankSize = SUBBANKSIZE;
     UINT4 maxSubBankSize = SUBBANKSIZE;
-
-    FindChirpSubBank *subBankHead = NULL;
+    UINT4 numSubBanks = 0;
 
     xmlStreamOut = XLALOpenLIGOLwXMLFile(sortedBankFileName);
 
@@ -75,13 +76,6 @@ int main(int argc, char ** argv)
 
     XLALCloseLIGOLwXMLFile(xmlStreamOut);    
 
-    while ( templateBank.snglInspiralTable )
-    {
-	tmplt = templateBank.snglInspiralTable;
-	templateBank.snglInspiralTable = templateBank.snglInspiralTable->next;
-	LALFree( tmplt );
-    }
-
     subBankHead = XLALFindChirpCreateSubBanks(&maxSubBankSize,
 					      subBankSize,
 					      numberTemplates,
@@ -89,8 +83,44 @@ int main(int argc, char ** argv)
 
     printf("maxsubbank size = %d\n",maxSubBankSize);
 
+    numSubBanks = numberTemplates/subBankSize;
 
 
+    UINT4 i=1;
+    for (subBank=subBankHead; subBank; subBank = subBank->next)
+    {
+	fprintf(stderr,"%d\n",i);
+	tmplt = templateBank.snglInspiralTable;
+	for ( bank = subBankHead->bankHead; bank; bank = bank->next)
+	{
+	    tmplt->mass1 = bank->mass1;
+	    tmplt->mass2 = bank->mass2;
+
+	    tmplt->snr = 3.7;
+	    tmplt->chisq = 3.8;
+	    tmplt->chisq_dof = 100;
+	    tmplt->bank_chisq = 3.9;
+	    tmplt->bank_chisq_dof = 100;
+	    tmplt->cont_chisq = 4.0;
+	    tmplt->cont_chisq_dof = 100;
+	    tmplt = tmplt->next;
+	}
+	tmplt->next = NULL;
+	fprintf(stderr,"%d\n",i);
+	sprintf(subBankFileName,"%s%d%s","subBank",i,".xml");
+	xmlStreamOut = XLALOpenLIGOLwXMLFile(subBankFileName);
+	XLALWriteLIGOLwXMLSnglInspiralTable(xmlStreamOut,templateBank.snglInspiralTable);
+	XLALCloseLIGOLwXMLFile(xmlStreamOut);	
+	i++;
+	if (i == 10) break;
+    }
+
+    while ( templateBank.snglInspiralTable )
+    {
+	tmplt = templateBank.snglInspiralTable;
+	templateBank.snglInspiralTable = templateBank.snglInspiralTable->next;
+	LALFree( tmplt );
+    }
 
 
 }
