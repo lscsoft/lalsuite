bf00ac8338b62ac044d1fe0fa1dae09e8c89b137
Kipp Cannon <kipp.cannon@ligo.org>
2009-11-05 17:33:22 -0800

    Fix PR2916 port TestLeapSecs unit test to XLAL leap seconds function Original: 5e063fe85b03e5cd48b43b5a2901292c32579a45

--- lal/packages/date/test/TestLeapSecs.c
+++ lal/packages/date/test/TestLeapSecs.c
@@ -27,274 +27,76 @@
 
 INT4 lalDebugLevel = 0;
 
-NRCSID (LALTESTLEAPSECSC, "$Id$");
+NRCSID(LALTESTLEAPSECSC, "$Id$");
 
-typedef struct gps_leap_sec {
-  time_t gps;       /* GPS time when leap sec was introduced */
-  INT4   tai_utc;   /* GPS-UTC at GPS time gps */
-} gps_leap_sec_t;
+static int do_test(LIGOTimeGPS gps, int tai_utc_before, int tai_utc_after)
+{
+	int result = 0;
+	int lal_tai_utc;
+
+	if(lalDebugLevel > 2)
+		printf("TestLeapSecs: BEFORE LEAP SECOND ADDED\n");
+	lal_tai_utc = XLALLeapSeconds(gps.gpsSeconds - 1);
+	if(lalDebugLevel > 0)
+		printf("\tGPS = %9d;    TAI-UTC = %d\n", gps.gpsSeconds - 1, lal_tai_utc);
+
+	if(XLALGetBaseErrno() && lalDebugLevel > 0) {
+		XLAL_PERROR("do_test()");
+		result = -1;
+	}
+	if(lal_tai_utc != tai_utc_before) {
+		if(lalDebugLevel > 0)
+			fprintf(stderr, "TestLeapSecs: XLALLeapSeconds() returned wrong value: expected %d, got %d\n", tai_utc_before, lal_tai_utc);
+		result = -1;
+	}
+
+	if(lalDebugLevel > 2)
+		printf("TestLeapSecs: AFTER LEAP SECOND ADDED\n");
+	lal_tai_utc = XLALLeapSeconds(gps.gpsSeconds);
+	if(lalDebugLevel > 0)
+		printf("\tGPS = %9d;    TAI-UTC = %d\n\n", gps.gpsSeconds, lal_tai_utc);
+
+	if(XLALGetBaseErrno() && lalDebugLevel > 0) {
+		XLAL_PERROR("do_test()");
+		result = -1;
+	}
+	if(lal_tai_utc != tai_utc_after) {
+		if(lalDebugLevel > 0)
+			fprintf(stderr, "TestLeapSecs: XLALLeapSeconds() returned wrong value: expected %d, got %d\n", tai_utc_after, lal_tai_utc);
+		result = -1;
+	}
+	return result;
+}
+
 
 int main(int argc, char *argv[])
 {
-  static LALStatus    status;
-  static const gps_leap_sec_t leapsec_data[] =
-    {
-      {0,         19},  /* 1980-Jan-06 */
-      {46828801,  20},  /* 1981-Jul-01 */
-      {78364802,  21},  /* 1982-Jul-01 */
-      {109900803, 22},  /* 1983-Jul-01 */
-      {173059204, 23},  /* 1985-Jul-01 */
-      {252028805, 24},  /* 1988-Jan-01 */
-      {315187206, 25},  /* 1990-Jan-01 */
-      {346723207, 26},  /* 1991-Jan-01 */
-      {393984008, 27},  /* 1992-Jul-01 */
-      {425520009, 28},  /* 1993-Jul-01 */
-      {457056010, 29},  /* 1994-Jul-01 */
-      {504489611, 30},  /* 1996-Jan-01 */
-      {551750412, 31},  /* 1997-Jul-01 */
-      {599184013, 32},  /* 1999-Jan-01 */
-    };
-  static const INT4 num_data = sizeof(leapsec_data)/sizeof(gps_leap_sec_t);
-  LIGOTimeGPS         gpsTime = {0, 0};
-  INT4                gps_utc = 0;
-  INT4                tai_utc = 0;
-  INT4                i = 0;
-  LALLeapSecFormatAndAcc formatAndAcc = {LALLEAPSEC_GPSUTC, LALLEAPSEC_STRICT};
+	struct {
+		LIGOTimeGPS gps;	/* GPS time when leap sec was introduced */
+		int tai_utc;	/* GPS-UTC at GPS time gps */
+	} leapsec_data[] = {
+		{{        0, 0}, 19},	/* 1980-Jan-06 */
+		{{ 46828800, 0}, 20},	/* 1981-Jul-01 */
+		{{ 78364801, 0}, 21},	/* 1982-Jul-01 */
+		{{109900802, 0}, 22},	/* 1983-Jul-01 */
+		{{173059203, 0}, 23},	/* 1985-Jul-01 */
+		{{252028804, 0}, 24},	/* 1988-Jan-01 */
+		{{315187205, 0}, 25},	/* 1990-Jan-01 */
+		{{346723206, 0}, 26},	/* 1991-Jan-01 */
+		{{393984007, 0}, 27},	/* 1992-Jul-01 */
+		{{425520008, 0}, 28},	/* 1993-Jul-01 */
+		{{457056009, 0}, 29},	/* 1994-Jul-01 */
+		{{504489610, 0}, 30},	/* 1996-Jan-01 */
+		{{551750411, 0}, 31},	/* 1997-Jul-01 */
+		{{599184012, 0}, 32},	/* 1999-Jan-01 */
+	};
+	unsigned i;
 
+	if(argc > 1)
+		lalDebugLevel = atoi(argv[1]);
 
-  if (argc > 1)
-    lalDebugLevel = atoi(argv[1]);
+	for(i = 1; i < sizeof(leapsec_data) / sizeof(*leapsec_data); i++)
+		do_test(leapsec_data[i].gps, leapsec_data[i-1].tai_utc, leapsec_data[i].tai_utc);
 
-
-  /*
-   * To run the test, compute GPS-UTC just before and just after the time
-   * that the leap second is added
-   */
-
-  if (lalDebugLevel > 0)
-    printf("TestLeapSecs: TESTING GPS-UTC\n");
-
-  gpsTime.gpsSeconds = 0;
-  LALLeapSecs(&status, &gps_utc, &gpsTime, &formatAndAcc);
-
-  if (lalDebugLevel > 0)
-    printf("\tGPS = %9d;    GPS-UTC = %d\n", gpsTime.gpsSeconds,
-           gps_utc);
-
-  if (status.statusCode && lalDebugLevel > 0)
-    {
-      fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-              __LINE__, LALTESTLEAPSECSC);
-      REPORTSTATUS(&status);
-      return status.statusCode;
-    }
-
-  if (gps_utc != 0)
-    {
-      if (lalDebugLevel > 0)
-        {
-          fprintf(stderr,
-                  "TestLeapSecs: LALLeapSecs returned wrong value: expected 0, got %d; i = %d\n",
-                  gps_utc, i);
-          REPORTSTATUS(&status);
-        }
-      return 1;
-    }
-
-  gpsTime.gpsSeconds = 1;
-  LALLeapSecs(&status, &gps_utc, &gpsTime, &formatAndAcc);
-  if (lalDebugLevel > 0)
-    printf("\tGPS = %9d;    GPS-UTC = %d\n", gpsTime.gpsSeconds,
-           gps_utc);
-
-  if (status.statusCode && lalDebugLevel > 0)
-    {
-      fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-              __LINE__, LALTESTLEAPSECSC);
-      REPORTSTATUS(&status);
-      return status.statusCode;
-    }
-
-  if (gps_utc != 0)
-    {
-      if (lalDebugLevel > 0)
-        {
-          fprintf(stderr,
-                  "TestLeapSecs: LALLeapSecs returned wrong value: expected 0, got %d; i = %d\n",
-                  gps_utc, i);
-          REPORTSTATUS(&status);
-        }
-      return 1;
-    }
-
-
-
-  for (i = 1; i < num_data; ++i)
-    {
-      if (lalDebugLevel > 2)
-        {
-          printf("TestLeapSecs: BEFORE LEAP SECOND ADDED\n");
-        }
-
-      gpsTime.gpsSeconds = leapsec_data[i].gps - 1;
-      LALLeapSecs(&status, &gps_utc, &gpsTime, &formatAndAcc);
-      if (lalDebugLevel > 0)
-        printf("\tGPS = %9d;    GPS-UTC = %d\n", gpsTime.gpsSeconds,
-               gps_utc);
-
-      if (status.statusCode && lalDebugLevel > 0)
-        {
-          fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-                  __LINE__, LALTESTLEAPSECSC);
-          REPORTSTATUS(&status);
-          return status.statusCode;
-        }
-
-      if (gps_utc != leapsec_data[i-1].tai_utc - 19)
-        {
-          if (lalDebugLevel > 0)
-            {
-              fprintf(stderr,
-                      "TestLeapSecs: LALLeapSecs returned wrong value: expected %d, got %d; i = %d\n",
-                      leapsec_data[i-1].tai_utc - 19, gps_utc, i);
-              REPORTSTATUS(&status);
-            }
-          return 1;
-        }
-
-      if (lalDebugLevel > 2)
-        {
-          printf("TestLeapSecs: AFTER LEAP SECOND ADDED\n");
-        }
-
-      gpsTime.gpsSeconds += 2;
-      LALLeapSecs(&status, &gps_utc, &gpsTime, &formatAndAcc);
-      if (lalDebugLevel > 0)
-        printf("\tGPS = %9d;    GPS-UTC = %d\n\n", gpsTime.gpsSeconds,
-               gps_utc);
-
-      if (status.statusCode && lalDebugLevel > 0)
-        {
-          fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-                  __LINE__, LALTESTLEAPSECSC);
-          REPORTSTATUS(&status);
-          return status.statusCode;
-        }
-
-      if (gps_utc != leapsec_data[i].tai_utc - 19)
-        {
-          if (lalDebugLevel > 0)
-            {
-              fprintf(stderr,
-                      "TestLeapSecs: LALLeapSecs returned wrong value: expected %d, got %d; i = %d\n",
-                      leapsec_data[i-1].tai_utc - 19, gps_utc, i);
-              REPORTSTATUS(&status);
-            }
-          return 1;
-        }
-    } /* for */
-
-
-  formatAndAcc.accuracy = LALLEAPSEC_LOOSE;
-  gpsTime.gpsSeconds = 701654353 + 4;
-
-  LALLeapSecs(&status, &gps_utc, &gpsTime, &formatAndAcc);
-  if (lalDebugLevel > 0)
-    printf("\tGPS = %9d;    GPS-UTC = %d\n", gpsTime.gpsSeconds,
-           gps_utc);
-
-  if (status.statusCode && lalDebugLevel > 0)
-    {
-      fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-              __LINE__, LALTESTLEAPSECSC);
-      REPORTSTATUS(&status);
-      return status.statusCode;
-    }
-
-  if (gps_utc != 13)
-    {
-      if (lalDebugLevel > 0)
-        {
-          fprintf(stderr,
-                  "TestLeapSecs: LALLeapSecs returned wrong value: expected 13, got %d; i = %d\n",
-                  gps_utc, i);
-          REPORTSTATUS(&status);
-        }
-      return 1;
-    }
-
-  if (lalDebugLevel > 0)
-    printf("\nTestLeapSecs: TESTING TAI-UTC\n\n");
-
-  /*
-   * Test TAI-UTC
-   */
-  formatAndAcc.format = LALLEAPSEC_TAIUTC;
-
-  for (i = 1; i < num_data; ++i)
-    {
-      if (lalDebugLevel > 2)
-        {
-          printf("TestLeapSecs: BEFORE LEAP SECOND ADDED\n");
-        }
-
-      gpsTime.gpsSeconds = leapsec_data[i].gps - 1;
-      LALLeapSecs(&status, &tai_utc, &gpsTime, &formatAndAcc);
-      if (lalDebugLevel > 0)
-        printf("\tGPS = %9d;    TAI-UTC = %d\n", gpsTime.gpsSeconds,
-               tai_utc);
-
-      if (status.statusCode && lalDebugLevel > 0)
-        {
-          fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-                  __LINE__, LALTESTLEAPSECSC);
-          REPORTSTATUS(&status);
-          return status.statusCode;
-        }
-
-      if (tai_utc != leapsec_data[i-1].tai_utc)
-        {
-          if (lalDebugLevel > 0)
-            {
-              fprintf(stderr,
-                      "TestLeapSecs: LALLeapSecs returned wrong value: expected %d, got %d; i = %d\n",
-                      leapsec_data[i-1].tai_utc, tai_utc, i);
-              REPORTSTATUS(&status);
-            }
-          return 1;
-        }
-
-      if (lalDebugLevel > 2)
-        {
-          printf("TestLeapSecs: AFTER LEAP SECOND ADDED\n");
-        }
-
-      gpsTime.gpsSeconds += 2;
-      LALLeapSecs(&status, &tai_utc, &gpsTime, &formatAndAcc);
-      if (lalDebugLevel > 0)
-        printf("\tGPS = %9d;    TAI-UTC = %d\n\n", gpsTime.gpsSeconds,
-              tai_utc);
-
-      if (status.statusCode && lalDebugLevel > 0)
-        {
-          fprintf(stderr, "TestLeapSecs: LALLeapSecs() failed, line %i, %s\n",
-                  __LINE__, LALTESTLEAPSECSC);
-          REPORTSTATUS(&status);
-          return status.statusCode;
-        }
-
-      if (tai_utc != leapsec_data[i].tai_utc)
-        {
-          if (lalDebugLevel > 0)
-            {
-              fprintf(stderr,
-                      "TestLeapSecs: LALLeapSecs returned wrong value: expected %d, got %d; i = %d\n",
-                      leapsec_data[i-1].tai_utc, tai_utc, i);
-              REPORTSTATUS(&status);
-            }
-          return 1;
-        }
-    } /* for */
-
-  return 0;
+	return 0;
 }
