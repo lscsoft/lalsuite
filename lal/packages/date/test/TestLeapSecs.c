41f0cec8690893f87ee98e89eaafd908b89d5b5d
Kipp Cannon <kipp.cannon@ligo.org>
2009-11-05 18:03:17 -0800

    Fix PR2916 port TestLeapSecs unit test to XLAL leap seconds function Original: 250dc479de8b059fe0dce803c94806b68b5fde62

--- lal/packages/date/test/TestLeapSecs.c
+++ lal/packages/date/test/TestLeapSecs.c
@@ -24,21 +24,22 @@
 
 #include <lal/LALStdlib.h>
 #include <lal/Date.h>
+#include <XLALLeapSeconds.h>
 
 INT4 lalDebugLevel = 0;
 
 NRCSID(LALTESTLEAPSECSC, "$Id$");
 
-static int do_test(LIGOTimeGPS gps, int tai_utc_before, int tai_utc_after)
+static int do_test(int gpssec, int tai_utc_before, int tai_utc_after)
 {
 	int result = 0;
 	int lal_tai_utc;
 
 	if(lalDebugLevel > 2)
 		printf("TestLeapSecs: BEFORE LEAP SECOND ADDED\n");
-	lal_tai_utc = XLALLeapSeconds(gps.gpsSeconds - 1);
+	lal_tai_utc = XLALLeapSeconds(gpssec - 1);
 	if(lalDebugLevel > 0)
-		printf("\tGPS = %9d;    TAI-UTC = %d\n", gps.gpsSeconds - 1, lal_tai_utc);
+		printf("\tGPS = %9d;    TAI-UTC = %d\n", gpssec - 1, lal_tai_utc);
 
 	if(XLALGetBaseErrno() && lalDebugLevel > 0) {
 		XLAL_PERROR("do_test()");
@@ -52,9 +53,9 @@ static int do_test(LIGOTimeGPS gps, int tai_utc_before, int tai_utc_after)
 
 	if(lalDebugLevel > 2)
 		printf("TestLeapSecs: AFTER LEAP SECOND ADDED\n");
-	lal_tai_utc = XLALLeapSeconds(gps.gpsSeconds);
+	lal_tai_utc = XLALLeapSeconds(gpssec);
 	if(lalDebugLevel > 0)
-		printf("\tGPS = %9d;    TAI-UTC = %d\n\n", gps.gpsSeconds, lal_tai_utc);
+		printf("\tGPS = %9d;    TAI-UTC = %d\n\n", gpssec, lal_tai_utc);
 
 	if(XLALGetBaseErrno() && lalDebugLevel > 0) {
 		XLAL_PERROR("do_test()");
@@ -71,32 +72,13 @@ static int do_test(LIGOTimeGPS gps, int tai_utc_before, int tai_utc_after)
 
 int main(int argc, char *argv[])
 {
-	struct {
-		LIGOTimeGPS gps;	/* GPS time when leap sec was introduced */
-		int tai_utc;	/* GPS-UTC at GPS time gps */
-	} leapsec_data[] = {
-		{{        0, 0}, 19},	/* 1980-Jan-06 */
-		{{ 46828800, 0}, 20},	/* 1981-Jul-01 */
-		{{ 78364801, 0}, 21},	/* 1982-Jul-01 */
-		{{109900802, 0}, 22},	/* 1983-Jul-01 */
-		{{173059203, 0}, 23},	/* 1985-Jul-01 */
-		{{252028804, 0}, 24},	/* 1988-Jan-01 */
-		{{315187205, 0}, 25},	/* 1990-Jan-01 */
-		{{346723206, 0}, 26},	/* 1991-Jan-01 */
-		{{393984007, 0}, 27},	/* 1992-Jul-01 */
-		{{425520008, 0}, 28},	/* 1993-Jul-01 */
-		{{457056009, 0}, 29},	/* 1994-Jul-01 */
-		{{504489610, 0}, 30},	/* 1996-Jan-01 */
-		{{551750411, 0}, 31},	/* 1997-Jul-01 */
-		{{599184012, 0}, 32},	/* 1999-Jan-01 */
-	};
-	unsigned i;
+	int i;
 
 	if(argc > 1)
 		lalDebugLevel = atoi(argv[1]);
 
-	for(i = 1; i < sizeof(leapsec_data) / sizeof(*leapsec_data); i++)
-		do_test(leapsec_data[i].gps, leapsec_data[i-1].tai_utc, leapsec_data[i].tai_utc);
+	for(i = 1; i < numleaps; i++)
+		do_test(leaps[i].gpssec, leaps[i-1].taiutc, leaps[i].taiutc);
 
 	return 0;
 }
