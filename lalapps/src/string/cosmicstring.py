e43c7b832c17002787ef33e4e76e4b86cb5c8983
Kipp Cannon <kipp.cannon@ligo.org>
2010-01-08 16:16:51 -0800

    port datafind and binj jobs to power.py library Original: 234f2109f548b9910f9d783a82bc12462265e769

--- lalapps/src/string/cosmicstring.py
+++ lalapps/src/string/cosmicstring.py
@@ -50,7 +50,7 @@ class StringNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
     """
     pipeline.CondorDAGNode.__init__(self,job)
     pipeline.AnalysisNode.__init__(self)
-    self.__usertag = job.get_config('pipeline','user-tag')
+    self.__usertag = job.get_config('pipeline','user_tag')
 
   def get_output(self):
     """
@@ -64,51 +64,3 @@ class StringNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
 
     return basename + '-' + str(self.get_start()) + '-' + \
       str(self.get_end() - self.get_start()) + '.xml'
-    
-class InjJob(pipeline.CondorDAGJob, pipeline.AnalysisJob):
-  """
-  A lalapps_binj job used by the power pipeline. The static options
-  are read from the sections [data] and [power] in the ini file. The
-  stdout and stderr from the job are directed to the logs directory. The job
-  runs in the universe specified in the ini file. The path to the executable
-  is determined from the ini file.
-  """
-  def __init__(self,cp):
-    """
-    cp = ConfigParser object from which options are read.
-    """
-    self.__executable = cp.get('condor','injection')
-    self.__universe = 'local'
-    pipeline.CondorDAGJob.__init__(self,self.__universe,self.__executable)
-    pipeline.AnalysisJob.__init__(self,cp)
-
-    for sec in ['injection']:
-      self.add_ini_opts(cp,sec)
-
-    self.set_stdout_file('logs/inj-$(macrochannel)-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).out')
-    self.set_stderr_file('logs/inj-$(macrochannel)-$(macrogpsstarttime)-$(macrogpsendtime)-$(cluster)-$(process).err')
-    self.set_sub_file('injection.sub')
-
-class InjNode(pipeline.CondorDAGNode, pipeline.AnalysisNode):
-  """
-  A InjNode runs an instance of binj code
-  """
-  def __init__(self,job):
-    """
-    job = A CondorDAGJob that can run an instance of lalapps_power.
-    """
-    pipeline.CondorDAGNode.__init__(self,job)
-    pipeline.AnalysisNode.__init__(self)
-    
-  def get_output(self):
-    """
-    Returns the file name of output from the power code. This must be kept
-    synchronized with the name of the output file in power.c.
-    """
-    if not self.get_start() or not self.get_end():
-      raise InjError, "Start time or end time has not been set"
-
-    basename = 'HL-INJECTIONS-'+ str(self.get_start()) + \
-               '-' +str(self.get_end() - self.get_start()) + '.xml'
-
-    return basename
