af5947df2fc183ce0aa665aa51a4d047789935ac
Leo Singer <leo.singer@ligo.org>
2010-03-25 10:46:02 -0700

    First commit of pynds, an NDS client library Original: d262779af4facfe73a52e56ac37e8902d7c9527d

---
+++ pynds/nds/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+Wrapper for John Zweizig's NDS1/NDS2 library
+
+"""
+__author__       = "Leo Singer <leo.singer@ligo.org>"
+__organization__ = ["LIGO", "California Institute of Technology"]
+__copyright__    = "Copyright 2010, Leo Singer"
+
+from nds_ext import *
---
+++ pynds/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import os.path
+import sys
+import numpy
+
+## {{{ http://code.activestate.com/recipes/502261/ (r1)
+def pkgconfig(*packages, **kw):
+    import commands
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+## end of http://code.activestate.com/recipes/502261/ }}}
+
+setup(
+    name='pynds',
+    author='Leo Singer',
+    author_email='leo.singer@ligo.org',
+    description='NDS1/NDS2 client',
+    version='1.0',
+    packages=['nds'],
+    ext_modules=[
+        Extension('nds_ext', ['nds_ext.cpp'],
+            **pkgconfig('nds2-client',
+                library_dirs=['/opt/local/lib'],
+                include_dirs=['/opt/local/include']+[numpy.get_include()],
+                libraries=['boost_python-mt']
+            )
+        )
+    ]
+)
