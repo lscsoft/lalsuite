2240b93fefc280e78558926c8062c4f357bd6604
Leo Singer <leo.singer@ligo.org>
2010-03-26 02:07:45 -0700

    Added timestamp property and iterator functionality Original: eb2923721936d4d6d867d36e67628f7692ed621d

--- pynds/nds/__init__.py
+++ pynds/nds/__init__.py
@@ -7,4 +7,38 @@ __author__       = "Leo Singer <leo.singer@ligo.org>"
 __organization__ = ["LIGO", "California Institute of Technology"]
 __copyright__    = "Copyright 2010, Leo Singer"
 
-from nds_ext import *
+import nds_ext
+from nds_ext import channel_type, nds_version
+
+class daq_iterator:
+    def __init__(self, daq, start, stop, stride):
+        self.__daq = daq
+        self.__start = start
+        self.__stop = stop
+        self.__stride = stride
+        self.__started = False
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.__started:
+            self.__daq.recv_next()
+        else:
+            self.__daq.request_data(self.__start, self.__stop, self.__stride)
+            self.__started = True
+        return self.__daq.unpack()
+
+class daq(nds_ext.daq):
+    def __init__(self, host, port, version=None):
+        if version is None:
+            super(daq, self).__init__(host, port)
+        else:
+            super(daq, self).__init__(host, port, version)
+        self.host = host
+        self.port = port
+    def seek(self, start, stop, stride):
+        return daq_iterator(self, start, stop, stride)
+    def request_channels(self, channels):
+        for channel in channels:
+            self.request_channel(channel)
+
+del nds_ext
