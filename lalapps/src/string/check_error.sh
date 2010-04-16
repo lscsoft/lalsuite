8dc6ff8797e6cd9b33ef34d4f1fad06c871ee2c5
Florent Robinet <robinet@lal.in2p3.fr>
2010-04-16 17:05:52 +0200

    Add a new test Original: a0e34a853c50c0af3db9c60d70135babc18c20c8

--- lalapps/src/string/check_error.sh
+++ lalapps/src/string/check_error.sh
@@ -5,15 +5,29 @@ echo "Check job outputs and search for errors..."
 here=`pwd`
 
 for d in injections* noninjections; do
+
+    if [ ! -d ${d} ]; then continue; fi
     
     cd ${d}/logs
 
+    # check if error files are empty
     for errfile in datafind*.err lalapps_binj*.out lalapps_StringSearch*.err burca*.err; do
 
 	if [ -s ${errfile} ]; then
 	    echo "*****************************************************"
 	    echo "***** ${d}/logs/${errfile} :"
 	    cat ${errfile}
+	fi
+    done
+
+
+    # check if the string job went to the end
+    for outfile in lalapps_StringSearch*.out; do
+    
+	if ! grep -q "StringJob is done" ${outfile}; then
+	    echo "*****************************************************"
+	    echo "***** ${d}/logs/${outfile} :"
+	    echo "      JOB aborted !"
 	fi
     done
 
