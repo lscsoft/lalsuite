dc6e6537a5782a8318cf12cf709467d973dcf9d0
Florent Robinet <robinet@lal.in2p3.fr>
2010-04-21 10:33:41 +0200

    Add more checks Original: 9f0996844a6d5477274139bbd4d4e3754d2c51e5

--- lalapps/src/string/check_error.sh
+++ lalapps/src/string/check_error.sh
@@ -4,7 +4,7 @@ echo "Check job outputs and search for errors..."
 
 here=`pwd`
 
-for d in injections* noninjections; do
+for d in injections* training* noninjections; do
 
     if [ ! -d ${d} ]; then continue; fi
     
@@ -31,6 +31,23 @@ for d in injections* noninjections; do
 	    echo "      JOB aborted or not finished !"
 	fi
     done
+
+    # check the number of files consistency
+    n_add=`ls ligolw_add*.out | wc | awk '{print $1}'`
+    n_burca=`ls ligolw_burca*.out | wc | awk '{print $1}'`
+    n_sqlite=`ls ligolw_sqlite*.out | wc | awk '{print $1}'`
+
+    if [ ${n_add} -ne ${n_burca} ]; then 
+	echo "*****************************************************"
+	echo "***** ${d}/logs/ : missing ligolw_add or ligolw_burca jobs"
+    fi
+
+    if [ ${n_add} -ne ${n_sqlite} ]; then 
+	echo "*****************************************************"
+	echo "***** ${d}/logs/ : missing ligolw_add or ligolw_sqlite jobs"
+    fi
+
+
 
     cd ${here}
 
