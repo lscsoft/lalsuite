3ee920e091f0b0bc7c127d74fab210e2f2c8b25a
Adam Mercer <adam.mercer@ligo.org>
2010-05-13 11:01:45 -0500

    ignore return from vfprintf() in LALRaise() Original: 6bd5ef716a970202cf4af899a30c2cf821d66333

--- lal/packages/std/src/LALError.c
+++ lal/packages/std/src/LALError.c
@@ -228,7 +228,7 @@ LALRaise( int sig, const char *fmt, ... )
 { /* </lalVerbatim> */
   va_list ap;
   va_start( ap, fmt );
-  vfprintf( stderr, fmt, ap );
+  (void) vfprintf( stderr, fmt, ap );
   va_end( ap );
   return raise( sig );
 }
