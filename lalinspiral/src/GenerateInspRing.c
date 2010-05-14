a3494d36bf24db6f58b77fdb69e0444e2c49d6cd
Sarah Caudill <sarah.caudill@ligo.org>
2010-05-14 14:52:04 -0500

    Changed imr_ring_inject enum. Original: 91153c3bc6487ba77dbf81c1b8f56203604f5e59

--- lalinspiral/src/GenerateInspRing.c
+++ lalinspiral/src/GenerateInspRing.c
@@ -527,7 +527,7 @@ XLALGenerateInspRing(
   ringInj->amplitude = sqrt( amp*amp);
 
   /* zero out inspiral and merger if we only want to inject a ringdown*/
-  if ( injectSignalType ==  imr_ring_inject ||
+  if ( injectSignalType ==  LALRINGDOWN_IMR_RING_INJECT ||
       strstr(inspiralInj->waveform, "KludgeRingOnly") )
   {
     for ( n = 0; n < 2.0*(inputLength + mergerLength + endMerger ); n++ )
