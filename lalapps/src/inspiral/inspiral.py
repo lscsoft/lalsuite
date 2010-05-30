140077d82617576616001c6f5f10b57667579141
Duncan Brown <duncan.brown@ligo.org>
2010-05-30 16:01:30 -0400

    change pipeline generation code so that coire and sire input files are passed as args Original: 3241a61a98dda59eb5bf1c407e56d6fc6b0ce720

--- lalapps/src/inspiral/inspiral.py
+++ lalapps/src/inspiral/inspiral.py
@@ -1341,18 +1341,6 @@ class SireNode(InspiralAnalysisNode):
     """
     return self.__ifo_tag
 
-  def set_glob(self, file_glob):
-    """
-    Sets the glob name
-    """
-    self.add_var_opt('glob',file_glob)
-
-  def set_input(self, input_file):
-    """
-    Sets the input file name
-    """
-    self.add_var_opt('input',input_file)
-
   def get_output(self):
     """
     get the name of the output file
@@ -1493,18 +1481,6 @@ class CoireNode(InspiralAnalysisNode):
     Returns the IFO tag string
     """
     return self.__ifo_tag
-
-  def set_glob(self, file_glob):
-    """
-    Sets the glob name
-    """
-    self.add_var_opt('glob',file_glob)
-
-  def set_input(self, input_file):
-    """
-    Sets the input file name
-    """
-    self.add_var_opt('input',input_file)
 
   def set_output_tag(self):
     fname = self.job().get_exec_name().upper() 
