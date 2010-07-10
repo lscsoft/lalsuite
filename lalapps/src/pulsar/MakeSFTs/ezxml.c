79c25602fdba6daf0964f42bd5f444f311445eed
Reinhard Prix <reinhard.prix@ligo.org>
2010-07-10 13:00:55 +0200

    attempted fix of const-cast warning in ezxml.[ch]    - this http://gcc.gnu.org/gcc-4.5/changes.html seems to explain it,     casts from char** to const char** are unsafe Original: d5122b2b70bcb134d0fcaa6202233259e6805c64

--- lalapps/src/pulsar/MakeSFTs/ezxml.c
+++ lalapps/src/pulsar/MakeSFTs/ezxml.c
@@ -169,15 +169,15 @@ ezxml_t ezxml_get(ezxml_t xml, ...)
 
 /* returns a null terminated array of processing instructions for the given */
 /* target */
-const char **ezxml_pi(ezxml_t xml, const char *target)
+char **ezxml_pi(ezxml_t xml, const char *target)
 {
     ezxml_root_t root = (ezxml_root_t)xml;
     int i = 0;
 
-    if (! root) return (const char **)EZXML_NIL;
+    if (! root) return (char **)EZXML_NIL;
     while (root->xml.parent) root = (ezxml_root_t)root->xml.parent; /* root tag */
     while (root->pi[i] && strcmp(target, root->pi[i][0])) i++; /* find target */
-    return (const char **)((root->pi[i]) ? root->pi[i] + 1 : EZXML_NIL);
+    return (char **)((root->pi[i]) ? root->pi[i] + 1 : EZXML_NIL);
 }
 
 /* set an error string and return root */
