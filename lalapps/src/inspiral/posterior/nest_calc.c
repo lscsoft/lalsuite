b05bf553a9e1420d83ef5b12c6a275bb9a5ace8e
John Veitch <john.veitch@ligo.org>
2010-07-14 17:40:52 +0200

    Change tolerance to 0.5 Original: fa8749cdd271bf6024a6fff4526d256a530a43cb

--- lalapps/src/inspiral/posterior/nest_calc.c
+++ lalapps/src/inspiral/posterior/nest_calc.c
@@ -17,6 +17,7 @@
 #include <float.h>
 
 #define infosafe 1.5
+#define TOLERANCE 0.5
 
 gsl_matrix *cov_mat;
 
@@ -228,7 +229,7 @@ REAL8 nestZ(UINT4 Nruns, UINT4 Nlive, LALMCMCParameter **Live, LALMCMCInput *MCM
 	/*while(((REAL8)i)<=((REAL8)Nlive) || logLmax+logw > logZ-5)*/  /* This termination condition: when remaining prior can't
 	 account for more than exp(-5) of the evidence, even
 	 if entire support is at Lmax */
-	while(((REAL8)i)<=((REAL8)Nlive) || logadd(logZ,logLmax+logw+log((REAL8)Nlive))-logZ > 0.1 )
+	while(((REAL8)i)<=((REAL8)Nlive) || logadd(logZ,logLmax+logw+log((REAL8)Nlive))-logZ > TOLERANCE )
 	{
 		minpos=0;
 		/* Find minimum likelihood sample to replace */
