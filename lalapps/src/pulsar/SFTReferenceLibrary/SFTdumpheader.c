35cc803490a6896f4d8a1f416739bf13b67805df
Karl Wette <karl.wette@ligo.org>
2010-08-02 09:26:06 +0200

    Added GPL copyright notices, copying rest of lalapps Original: 1050212dd7564e1f9499407f50760fb21c47ab55

--- lalapps/src/pulsar/SFTReferenceLibrary/SFTdumpheader.c
+++ lalapps/src/pulsar/SFTReferenceLibrary/SFTdumpheader.c
@@ -1,4 +1,33 @@
-/* $Id$ */
+/*
+ *  Copyright (C) 2004, 2008 Bruce Allen, Reinhard Prix
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+/** 
+ *  \author Bruce Allen, Reinhard Prix
+ *  \file
+ *  \brief
+ *  Dump the header information from a set of SFT files
+ *
+ *  The exit status will be zero if all SFTs are valid.  The exit status
+ *  will be non-zero if any of the SFTs was invalid.  grep SFTE
+ *  SFTReferenceLibrary.h will show the return values.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
--- lalapps/src/pulsar/SFTReferenceLibrary/SFTwrite.c
+++ lalapps/src/pulsar/SFTReferenceLibrary/SFTwrite.c
@@ -1,4 +1,36 @@
-/* $Id$ */
+/*
+ *  Copyright (C) 2004 Bruce Allen
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with with program; see the file COPYING. If not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+/** 
+ *  \author Bruce Allen
+ *  \file
+ *  \brief
+ *  Makes a test SFT (called SFT-test)
+ *
+ *  You can do this on little-endian and big-endian machines to generate
+ *  both flavors.  This produces a set of good and a set of bad SFTs.  The
+ *  good SFTs are:
+ *  SFT-test[1234567] and SFT-good
+ *  and the bad SFTs are
+ *  SFT-bad[123456789] and SFT-bad1[0-4]
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
