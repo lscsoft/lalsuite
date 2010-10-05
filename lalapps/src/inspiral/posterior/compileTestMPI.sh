193a529890a04c2090bacf8183b457a9af74a7a1
Vivien Raymond <vivien.raymond@ligo.org>
2010-10-05 15:11:58 -0500

    LALInference. compileTestMPI.sh update Original: a6a21bbc40b324b50f95e99092a2f1e63aca0f7a

--- lalapps/src/inspiral/posterior/compileTestMPI.sh
+++ lalapps/src/inspiral/posterior/compileTestMPI.sh
@@ -1,2 +1,2 @@
-openmpicc -Wall -ggdb -std=gnu99 -o InferenceTest LALInference.c InferenceTestMPI.c LALInferenceTemplate.c LALInferenceReadData.c LALInferenceMCMCMPI.c `pkg-config --cflags --libs lal libframe lalsupport libmetaio lalmetaio lalframe fftw3 lalinspiral lalpulsar`
+openmpicc -Wall -ggdb -std=gnu99 -o InferenceTest LALInference.c LALInferenceMainMPI.c LALInferenceTemplate.c LALInferenceReadData.c LALInferenceMCMCMPI.c `pkg-config --cflags --libs lal libframe lalsupport libmetaio lalmetaio lalframe fftw3 lalinspiral lalpulsar`
 
