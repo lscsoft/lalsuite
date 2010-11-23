11c6268ec7f2bb54c1e47db0219957f33c3ea796
Reinhard Prix <reinhard.prix@ligo.org>
2010-11-23 15:26:53 +0100

    semi-automatic doxygen conversion of TSpin.c Original: fa439a32b71ce2e28aff0b74cea80660ca47b5c1

--- lalpulsar/src/TSpin.c
+++ lalpulsar/src/TSpin.c
@@ -17,103 +17,86 @@
 *  MA  02111-1307  USA
 */
 
-/**************************************** <lalVerbatim file="TSpinCV">
-Author: Creighton, T. D.
-$Id$
-**************************************************** </lalVerbatim> */
+/**
+\author Creighton, T. D.
+\file
+\ingroup PulsarTimes_h
+\latexonly\label{ss:TSpin.c}\endlatexonly
 
-/********************************************************** <lalLaTeX>
+Computes the rotation-synchronized time coordinate for an object with smoothly-varying spin.
 
-\subsection{Module \texttt{TSpin.c}}
-\label{ss:TSpin.c}
-
-Computes the rotation-synchronized time coordinate for an object with
-smoothly-varying spin.
-
-\subsubsection*{Prototypes}
-\vspace{0.1in}
-\input{TSpinCP}
-\idx{LALTSpin()}
-\idx{LALDTSpin()}
-
-\subsubsection*{Description}
+\par Description
 
 These routines compute the value and derivatives of a time
-transformation $t_s(t)$ from a physical (inertial) time coordinate to
+transformation \f$t_s(t)\f$ from a physical (inertial) time coordinate to
 one that is synchronized with the rotation of an object (such as a
 pulsar).  That is, the rotation period is constant in the new time
 coordinate, while the actual physical rotation rate may be changing
-gradually.  The frequency drift $f(t)$ is characterized by
-frequency-normalized Taylor coefficients $f_k=(k!f)^{-1}\partial^k
-f/\partial t^k$.
+gradually.  The frequency drift \f$f(t)\f$ is characterized by
+frequency-normalized Taylor coefficients \f$f_k=(k!f)^{-1}\partial^k f/\partial t^k\f$.
 
 The routines obey the calling convention presented in the header
-\verb@PulsarTimes.h@, with $n$ variable parameters $\lambda^k=f_k$
-(measured in $\mathrm{Hz}^k$), where $k=1,\ldots,n$.  The only
+\ref PulsarTimes.h, with \f$n\f$ variable parameters \f$\lambda^k=f_k\f$
+(measured in \f$\mathrm{Hz}^k\f$), where \f$k=1,\ldots,n\f$.  The only
 constant parameter field used by these routines is
-\verb@constants->t0@, which is the time when the Taylor coefficients
-$f_k$ are evaluated.  $t_s$ is defined to be zero at that time.
+<tt>constants->t0</tt>, which is the time when the Taylor coefficients
+\f$f_k\f$ are evaluated.  \f$t_s\f$ is defined to be zero at that time.
 
-\verb@*variables@ can be a vector of arbitrary length; for
-consistency, \verb@*dtSpin@ must be one element longer.
+<tt>*variables</tt> can be a vector of arbitrary length; for
+consistency, <tt>*dtSpin</tt> must be one element longer.
 
-\subsubsection*{Algorithm}
+\par Algorithm
 
 If the frequency of a rotating body is varying in some suitably smooth
 manner, then it can be represented as a Taylor series:
-$$
+\f[
 f(t) = f_0 \left(1 + \sum_{k=1}^n f_k [t-t_0]^k \right) \; .
-$$
-It is worth pointing out that the parameters $f_k$ are
+\f]
+It is worth pointing out that the parameters \f$f_k\f$ are
 frequency-normalized Taylor coefficients, so their magnitudes
 represent, not the frequency itself, but the timescale on which the
 frequency is varying.  That is, if the frequency is changing on some
-timescale $\mathcal{T}$, then $f_k$ is typically of order
-$\mathcal{T}^{-k}$.
+timescale \f$\mathcal{T}\f$, then \f$f_k\f$ is typically of order
+\f$\mathcal{T}^{-k}\f$.
 
-The spin-synchronized time $t_s(t)$ counts off intervals of constant
-rotational phase, so it must be proportional to $\int f(t)\,dt$.  The
-integration constant is set by the convention that $t_s(t_0)=0$.  The
-constant of proportionality is set by demanding that, at $t=t_0$, both
-$t_s$ and $t$ measure time at the same rate.  This gives us the
+The spin-synchronized time \f$t_s(t)\f$ counts off intervals of constant
+rotational phase, so it must be proportional to \f$\int f(t)\,dt\f$.  The
+integration constant is set by the convention that \f$t_s(t_0)=0\f$.  The
+constant of proportionality is set by demanding that, at \f$t=t_0\f$, both
+\f$t_s\f$ and \f$t\f$ measure time at the same rate.  This gives us the
 transformation:
-$$
+\f[
 t_s(t) = t-t_0 + \sum_{k=1}^n \frac{f_k}{k+1} (t-t_0)^{k+1} \; .
-$$
+\f]
 The time derivative of this transformation is of course just the the
-factor in the equation for $f(t)$ that we started with:
-$$
-\frac{\partial t_s(t)}{\partial t} = 1 +
-	\sum_{k=1}^n f_k [t-t_0]^k \; .
-$$
-The derivatives with respect to $f_k$ are similarly trivial:
-$$
+factor in the equation for \f$f(t)\f$ that we started with:
+\f[
+\frac{\partial t_s(t)}{\partial t} = 1 + \sum_{k=1}^n f_k [t-t_0]^k \; .
+\f]
+The derivatives with respect to \f$f_k\f$ are similarly trivial:
+\f[
 \frac{\partial t_s(t)}{\partial f_k} = \frac{(t-t_0)^{k+1}}{k+1} \; .
-$$
+\f]
 
-\subsubsection*{Uses}
-\begin{verbatim}
+\par Uses
+\code
 lalDebugLevel
-\end{verbatim}
+\endcode
 
-\subsubsection*{Notes}
-
-\vfill{\footnotesize\input{TSpinCV}}
-
-******************************************************* </lalLaTeX> */
+*/
 
 #include<lal/LALStdlib.h>
 #include<lal/PulsarTimes.h>
 
 NRCSID(TSPINC,"$Id$");
 
-/* <lalVerbatim file="TSpinCP"> */
+
 void
 LALTSpin( LALStatus             *stat,
 	  REAL8                 *tSpin,
 	  REAL8Vector           *variables,
 	  PulsarTimesParamStruc *constants )
-{ /* </lalVerbatim> */
+{
   INT4 i;      /* An index. */
   INT4 k;      /* Another index. */
   REAL8 t;     /* A time variable, t-t0. */
@@ -153,13 +136,13 @@ LALTSpin( LALStatus             *stat,
 }
 
 
-/* <lalVerbatim file="TSpinCP"> */
+
 void
 LALDTSpin( LALStatus             *stat,
 	   REAL8Vector           *dtSpin,
 	   REAL8Vector           *variables,
 	   PulsarTimesParamStruc *constants )
-{ /* </lalVerbatim> */
+{
   INT4 i;       /* An index. */
   INT4 k;       /* Another index. */
   REAL8 t;      /* A time variable, t-t0. */
