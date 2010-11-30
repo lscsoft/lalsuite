5b8794193ba19a751c1ba2a943b28db27b509242
Florent Robinet <robinet@lal.in2p3.fr>
2010-11-30 13:42:53 +0100

    Update S5 pipeline architecture Original: 40cc53b00f9a0d5f3182cd5820630e9f40920368

--- lalapps/src/string/segments/get_chunk_lists.sh
+++ lalapps/src/string/segments/get_chunk_lists.sh
@@ -9,15 +9,15 @@ while read line; do
     if [ "$chunk_start" = "" ]; then continue; fi
     echo "Building segment lists for chunk ${c}..."
     echo $chunk_start $chunk_end > ./tmp
-    segexpr 'intersection(tmp,h1.seg)' -include index,duration > ./S5-H1segments-cat1_${c}.txt
-    segexpr 'intersection(tmp,h2.seg)' -include index,duration > ./S5-H2segments-cat1_${c}.txt
-    segexpr 'intersection(tmp,l1.seg)' -include index,duration > ./S5-L1segments-cat1_${c}.txt
-    segexpr 'intersection(tmp,v1.seg)' -include index,duration > ./S5-V1segments-cat1_${c}.txt
+    segexpr 'intersection(tmp,h1.seg)' -include index,duration > ./S5-H1segments-cat1_c${c}.txt
+    segexpr 'intersection(tmp,h2.seg)' -include index,duration > ./S5-H2segments-cat1_c${c}.txt
+    segexpr 'intersection(tmp,l1.seg)' -include index,duration > ./S5-L1segments-cat1_c${c}.txt
+    segexpr 'intersection(tmp,v1.seg)' -include index,duration > ./S5-V1segments-cat1_c${c}.txt
         
-    h1_lt=`segsum ./S5-H1segments-cat1_${c}.txt`
-    h2_lt=`segsum ./S5-H2segments-cat1_${c}.txt`
-    l1_lt=`segsum ./S5-L1segments-cat1_${c}.txt`
-    v1_lt=`segsum ./S5-V1segments-cat1_${c}.txt`
+    h1_lt=`segsum ./S5-H1segments-cat1_c${c}.txt`
+    h2_lt=`segsum ./S5-H2segments-cat1_c${c}.txt`
+    l1_lt=`segsum ./S5-L1segments-cat1_c${c}.txt`
+    v1_lt=`segsum ./S5-V1segments-cat1_c${c}.txt`
     echo "H1 livetime=${h1_lt} sec"
     echo "H2 livetime=${h2_lt} sec"
     echo "L1 livetime=${l1_lt} sec"
