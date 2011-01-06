24b59e08c6e98d6cbf84271685d653cd87bfe416
Shaun Hooper <hoopes01@student.uwa.edu.au>
2011-01-06 10:29:36 +0800

    I am not sure what has changed since the last commit. Original: 5232212476e9943b3a21819524f0af5a4b0efd0d

--- lalinspiral/src/LALInspiralIIR.c
+++ lalinspiral/src/LALInspiralIIR.c
@@ -1,8 +1,17 @@
 /*
-  This is a code that takes as its arugment a complex time series, or an
-  amplitude and phase time series and produces a set of IIR filters that when
-  combined, approximated recreate the original complex time series.
-  
+
+  This code relates to Infinite Impulse Response filters that correspond to an
+  inspiral waveform.  The idea is that a sum a set of delayed first order IIR
+  filters with one feedback coefficient (a1) and one feedforward (b0)
+  coefficient that will approximate the correlation of the input data and the
+  inspiral waveform. 
+
+  I.E the total impulse response is approximately a time reversed inspiral
+  waveform.
+
+  To generate the IIR set of a1's, b0's and delays, you need to provide a
+  amplitude and phase time series.
+
   Created by Shaun Hooper 2010-05-28
 
 */
@@ -10,7 +19,7 @@
 #include <lal/LALInspiral.h>
 #include <math.h>
 
-int XLALCreateInspiralIIRFilters(REAL8Vector *amp, REAL8Vector *phase, double epsilon, double alpha, double beta, COMPLEX16Vector **a1, COMPLEX16Vector **b0, INT4Vector **delay)
+int XLALInspiralGenerateIIRSet(REAL8Vector *amp, REAL8Vector *phase, double epsilon, double alpha, double beta, COMPLEX16Vector **a1, COMPLEX16Vector **b0, INT4Vector **delay)
 {  
 	UINT4 j, jmax, jstep, k;
 	UINT4 nfilters = 0;
@@ -22,17 +31,12 @@ int XLALCreateInspiralIIRFilters(REAL8Vector *amp, REAL8Vector *phase, double ep
 	jmax = amp->length;
 
 	/* FIXME: Error check that *b0, *a1, *delay ARE null pointers */
+	if(!delay)
+		return 
 
 	phase_ddot = (phase->data[2] -2.0 * phase->data[1] + phase->data[0]) / ( 2.0 * LAL_PI); // Second derivative of the phase at the first data point
 	jstep = (UINT4) floor(sqrt(2.0 * epsilon / phase_ddot) + 0.5);
 	j = jstep;
-
-	resp = XLALCreateCOMPLEX16Vector(jmax);
-	for (i=0; i<jmax; i++)
-		{
-			resp->data[j].re = 0.0;
-			resp->data[j].im = 0.0;
-		}
 
 	while (j < jmax - 2 && jstep != 0) {
 		/* FIXME: Check that the flooring of k really is correct */
@@ -46,12 +50,9 @@ int XLALCreateInspiralIIRFilters(REAL8Vector *amp, REAL8Vector *phase, double ep
 		*delay = XLALResizeINT4Vector(*delay, nfilters);
 
 		/* Record a1, b0 and delay */
-
 		(*a1)->data[nfilters-1] = XLALCOMPLEX16Polar((REAL8) exp(-beta / ((double) jstep)), -phase_dot);		
 		(*b0)->data[nfilters-1] = XLALCOMPLEX16Polar(amp->data[k], phase->data[k] + phase_dot * ((REAL8) (j-(k+1))) );
 		(*delay)->data[nfilters-1] = jmax - j;
-		
-		/* I was thinking about putting the resonse function in here */
 
 		/* Calculate the next data point step */
 		phase_ddot = (phase->data[j+1] - 2.0 * phase->data[j] + phase->data[j-1]) / (2.0 * LAL_PI);
@@ -59,12 +60,12 @@ int XLALCreateInspiralIIRFilters(REAL8Vector *amp, REAL8Vector *phase, double ep
 		j += jstep;
 
 	}
-	
+
 	return 0;
 }
 
 
-int XLALCreateIIRResponseSeries(UINT4 N, COMPLEX16Vector *a1, COMPLEX16Vector *b0, INT4Vector *delay, COMPLEX16Vector **response)
+int XLALInspiralIIRSetResponse(UINT4 N, COMPLEX16Vector *a1, COMPLEX16Vector *b0, INT4Vector *delay, COMPLEX16Vector **response)
 {  
 	UINT4 f, j, nfilters;
 	COMPLEX16 a1f, b0f;
@@ -73,10 +74,10 @@ int XLALCreateIIRResponseSeries(UINT4 N, COMPLEX16Vector *a1, COMPLEX16Vector *b
 	/* FIX ME: Check if a1, b0, delay actually exist */
 	nfilters = a1->length;
 	*response = XLALResizeCOMPLEX16Vector(*response, N);
-	fprintf(stderr, "About to start response vector setup\n");
+
 	for (j = 0; j < N; j++)
 		(*response)->data[j] = XLALCOMPLEX16Rect(0.0, 0.0);
-	fprintf(stderr, "About to looping filters\n");
+
 	for (f = 0; f < nfilters; f++)
 		{
 			a1f = a1->data[f];
@@ -91,7 +92,7 @@ int XLALCreateIIRResponseSeries(UINT4 N, COMPLEX16Vector *a1, COMPLEX16Vector *b
 	return 0;
 }
 
-int XLALIIRFreqSeries(UINT4 j, UINT4 jmax, COMPLEX16 a1, COMPLEX16 b0, INT4 delay, COMPLEX16 *hfcos, COMPLEX16 *hfsin)
+int XLALInspiralGenerateIIRSetFourierTransform(UINT4 j, UINT4 jmax, COMPLEX16 a1, COMPLEX16 b0, INT4 delay, COMPLEX16 *hfcos, COMPLEX16 *hfsin)
 {
 	REAL8 loga1, arga1, pf;
 	COMPLEX16 scl, ft, ftconj;
@@ -112,7 +113,7 @@ int XLALIIRFreqSeries(UINT4 j, UINT4 jmax, COMPLEX16 a1, COMPLEX16 b0, INT4 dela
 	return 0;
 }
 
-int XLALIIRInnerProduct(COMPLEX16Vector *a1, COMPLEX16Vector *b0, INT4Vector *delay, REAL8Vector *psd, REAL8 *ip)
+int XLALInspiralCalculateIIRSetInnerProduct(COMPLEX16Vector *a1, COMPLEX16Vector *b0, INT4Vector *delay, REAL8Vector *psd, REAL8 *ip)
 {
 	UINT4 k, j;
 	COMPLEX16 hA;
