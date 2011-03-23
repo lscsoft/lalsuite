ee77eb29465692c9cf76cb7f52e899a2846af8e4
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2011-03-23 16:37:45 +0100

    fix PulsarSpinsREAL4 and FcomponentsREAL4 to avoid double definitians Original: c7179abf7433409f96df958a87acdecc7c7595f7

--- lalapps/src/pulsar/FDS_isolated/OptimizedCFS/ComputeFstatREAL4.c
+++ lalapps/src/pulsar/FDS_isolated/OptimizedCFS/ComputeFstatREAL4.c
@@ -87,17 +87,10 @@ static const REAL4 inv_fact[PULSAR_MAX_SPINS] = { 1.0, 1.0, (1.0/2.0), (1.0/6.0)
 /* empty initializers  */
 static const LALStatus empty_LALStatus;
 static const AMCoeffs empty_AMCoeffs;
-
-// const SSBtimes empty_SSBtimes;
-// const MultiSSBtimes empty_MultiSSBtimes;
-// const AntennaPatternMatrix empty_AntennaPatternMatrix;
-// const MultiAMCoeffs empty_MultiAMCoeffs;
-// const Fcomponents empty_Fcomponents;
-// const ComputeFBuffer empty_ComputeFBuffer;
-const PulsarSpinsREAL4 empty_PulsarSpinsREAL4;
+static const PulsarSpinsREAL4 empty_PulsarSpinsREAL4;
+static const FcomponentsREAL4 empty_FcomponentsREAL4;
 const ComputeFBufferREAL4 empty_ComputeFBufferREAL4;
 const ComputeFBufferREAL4V empty_ComputeFBufferREAL4V;
-const FcomponentsREAL4 empty_FcomponentsREAL4;
 
 
 /*---------- internal prototypes ----------*/
