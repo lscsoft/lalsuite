53ee9784e08536ccd948061bb915958c8bc9b65e
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2011-03-24 13:41:08 +0100

    added functions to write out OpenCL error codes Original: cc3966862870a97782f31599b76f72000502a4ad

--- lalapps/src/pulsar/FDS_isolated/OptimizedCFS/ComputeFstatREAL4OpenCL.c
+++ lalapps/src/pulsar/FDS_isolated/OptimizedCFS/ComputeFstatREAL4OpenCL.c
@@ -94,12 +94,118 @@ int finite(double x);
 void sin_cos_2PI_LUT_REAL4 (REAL4 *sin2pix, REAL4 *cos2pix, REAL4 x);
 void sin_cos_LUT_REAL4 (REAL4 *sinx, REAL4 *cosx, REAL4 x);
 void init_sin_cos_LUT_REAL4 (void);
+const char *pclerror(cl_int err);
+const char *pclerrorM(cl_int err);
 
 #if USE_OPENCL_KERNEL_CPU
 #include "FStatOpenCLKernel.cl"
 #endif
 
 /*==================== FUNCTION DEFINITIONS ====================*/
+
+const char *pclerror(cl_int err) {
+  switch (err) {
+  case CL_SUCCESS:                          return ("Success");
+  case CL_DEVICE_NOT_FOUND:                 return ("Device not found");
+  case CL_DEVICE_NOT_AVAILABLE:             return ("Device not available");
+  case CL_COMPILER_NOT_AVAILABLE:           return ("Compiler not available");
+  case CL_MEM_OBJECT_ALLOCATION_FAILURE:    return ("Memory object allocation failure");
+  case CL_OUT_OF_RESOURCES:                 return ("Out of resources");
+  case CL_OUT_OF_HOST_MEMORY:               return ("Out of host memory");
+  case CL_PROFILING_INFO_NOT_AVAILABLE:     return ("Profiling information not available");
+  case CL_MEM_COPY_OVERLAP:                 return ("Memory copy overlap");
+  case CL_IMAGE_FORMAT_MISMATCH:            return ("Image format mismatch");
+  case CL_IMAGE_FORMAT_NOT_SUPPORTED:       return ("Image format not supported");
+  case CL_BUILD_PROGRAM_FAILURE:            return ("Program build failure");
+  case CL_MAP_FAILURE:                      return ("Map failure");
+  case CL_INVALID_VALUE:                    return ("Invalid value");
+  case CL_INVALID_DEVICE_TYPE:              return ("Invalid device type");
+  case CL_INVALID_PLATFORM:                 return ("Invalid platform");
+  case CL_INVALID_DEVICE:                   return ("Invalid device");
+  case CL_INVALID_CONTEXT:                  return ("Invalid context");
+  case CL_INVALID_QUEUE_PROPERTIES:         return ("Invalid queue properties");
+  case CL_INVALID_COMMAND_QUEUE:            return ("Invalid command queue");
+  case CL_INVALID_HOST_PTR:                 return ("Invalid host pointer");
+  case CL_INVALID_MEM_OBJECT:               return ("Invalid memory object");
+  case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:  return ("Invalid image format descriptor");
+  case CL_INVALID_IMAGE_SIZE:               return ("Invalid image size");
+  case CL_INVALID_SAMPLER:                  return ("Invalid sampler");
+  case CL_INVALID_BINARY:                   return ("Invalid binary");
+  case CL_INVALID_BUILD_OPTIONS:            return ("Invalid build options");
+  case CL_INVALID_PROGRAM:                  return ("Invalid program");
+  case CL_INVALID_PROGRAM_EXECUTABLE:       return ("Invalid program executable");
+  case CL_INVALID_KERNEL_NAME:              return ("Invalid kernel name");
+  case CL_INVALID_KERNEL_DEFINITION:        return ("Invalid kernel definition");
+  case CL_INVALID_KERNEL:                   return ("Invalid kernel");
+  case CL_INVALID_ARG_INDEX:                return ("Invalid argument index");
+  case CL_INVALID_ARG_VALUE:                return ("Invalid argument value");
+  case CL_INVALID_ARG_SIZE:                 return ("Invalid argument size");
+  case CL_INVALID_KERNEL_ARGS:              return ("Invalid kernel arguments");
+  case CL_INVALID_WORK_DIMENSION:           return ("Invalid work dimension");
+  case CL_INVALID_WORK_GROUP_SIZE:          return ("Invalid work group size");
+  case CL_INVALID_WORK_ITEM_SIZE:           return ("Invalid work item size");
+  case CL_INVALID_GLOBAL_OFFSET:            return ("Invalid global offset");
+  case CL_INVALID_EVENT_WAIT_LIST:          return ("Invalid event wait list");
+  case CL_INVALID_EVENT:                    return ("Invalid event");
+  case CL_INVALID_OPERATION:                return ("Invalid operation");
+  case CL_INVALID_GL_OBJECT:                return ("Invalid OpenGL object");
+  case CL_INVALID_BUFFER_SIZE:              return ("Invalid buffer size");
+  case CL_INVALID_MIP_LEVEL:                return ("Invalid mip-map level");
+  default:                                  return ("Unknown error code");
+  }
+}
+
+const char *pclerrorM(cl_int err) {
+  switch (err) {
+  case CL_SUCCESS:                          return ("CL_SUCCESS");
+  case CL_DEVICE_NOT_FOUND:                 return ("CL_DEVICE_NOT_FOUND");
+  case CL_DEVICE_NOT_AVAILABLE:             return ("CL_DEVICE_NOT_AVAILABLE");
+  case CL_COMPILER_NOT_AVAILABLE:           return ("CL_COMPILER_NOT_AVAILABLE");
+  case CL_MEM_OBJECT_ALLOCATION_FAILURE:    return ("CL_MEM_OBJECT_ALLOCATION_FAILURE");
+  case CL_OUT_OF_RESOURCES:                 return ("CL_OUT_OF_RESOURCES");
+  case CL_OUT_OF_HOST_MEMORY:               return ("CL_OUT_OF_HOST_MEMORY");
+  case CL_PROFILING_INFO_NOT_AVAILABLE:     return ("CL_PROFILING_INFO_NOT_AVAILABLE");
+  case CL_MEM_COPY_OVERLAP:                 return ("CL_MEM_COPY_OVERLAP");
+  case CL_IMAGE_FORMAT_MISMATCH:            return ("CL_IMAGE_FORMAT_MISMATCH");
+  case CL_IMAGE_FORMAT_NOT_SUPPORTED:       return ("CL_IMAGE_FORMAT_NOT_SUPPORTED");
+  case CL_BUILD_PROGRAM_FAILURE:            return ("CL_BUILD_PROGRAM_FAILURE");
+  case CL_MAP_FAILURE:                      return ("CL_MAP_FAILURE");
+  case CL_INVALID_VALUE:                    return ("CL_INVALID_VALUE");
+  case CL_INVALID_DEVICE_TYPE:              return ("CL_INVALID_DEVICE_TYPE");
+  case CL_INVALID_PLATFORM:                 return ("CL_INVALID_PLATFORM");
+  case CL_INVALID_DEVICE:                   return ("CL_INVALID_DEVICE");
+  case CL_INVALID_CONTEXT:                  return ("CL_INVALID_CONTEXT");
+  case CL_INVALID_QUEUE_PROPERTIES:         return ("CL_INVALID_QUEUE_PROPERTIES");
+  case CL_INVALID_COMMAND_QUEUE:            return ("CL_INVALID_COMMAND_QUEUE");
+  case CL_INVALID_HOST_PTR:                 return ("CL_INVALID_HOST_PTR");
+  case CL_INVALID_MEM_OBJECT:               return ("CL_INVALID_MEM_OBJECT");
+  case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:  return ("CL_INVALID_IMAGE_FORMAT_DESCRIPTOR");
+  case CL_INVALID_IMAGE_SIZE:               return ("CL_INVALID_IMAGE_SIZE");
+  case CL_INVALID_SAMPLER:                  return ("CL_INVALID_SAMPLER");
+  case CL_INVALID_BINARY:                   return ("CL_INVALID_BINARY");
+  case CL_INVALID_BUILD_OPTIONS:            return ("CL_INVALID_BUILD_OPTIONS");
+  case CL_INVALID_PROGRAM:                  return ("CL_INVALID_PROGRAM");
+  case CL_INVALID_PROGRAM_EXECUTABLE:       return ("CL_INVALID_PROGRAM_EXECUTABLE");
+  case CL_INVALID_KERNEL_NAME:              return ("CL_INVALID_KERNEL_NAME");
+  case CL_INVALID_KERNEL_DEFINITION:        return ("CL_INVALID_KERNEL_DEFINITION");
+  case CL_INVALID_KERNEL:                   return ("CL_INVALID_KERNEL");
+  case CL_INVALID_ARG_INDEX:                return ("CL_INVALID_ARG_INDEX");
+  case CL_INVALID_ARG_VALUE:                return ("CL_INVALID_ARG_VALUE");
+  case CL_INVALID_ARG_SIZE:                 return ("CL_INVALID_ARG_SIZE");
+  case CL_INVALID_KERNEL_ARGS:              return ("CL_INVALID_KERNEL_ARGS");
+  case CL_INVALID_WORK_DIMENSION:           return ("CL_INVALID_WORK_DIMENSION");
+  case CL_INVALID_WORK_GROUP_SIZE:          return ("CL_INVALID_WORK_GROUP_SIZE");
+  case CL_INVALID_WORK_ITEM_SIZE:           return ("CL_INVALID_WORK_ITEM_SIZE");
+  case CL_INVALID_GLOBAL_OFFSET:            return ("CL_INVALID_GLOBAL_OFFSET");
+  case CL_INVALID_EVENT_WAIT_LIST:          return ("CL_INVALID_EVENT_WAIT_LIST");
+  case CL_INVALID_EVENT:                    return ("CL_INVALID_EVENT");
+  case CL_INVALID_OPERATION:                return ("CL_INVALID_OPERATION");
+  case CL_INVALID_GL_OBJECT:                return ("CL_INVALID_GL_OBJECT");
+  case CL_INVALID_BUFFER_SIZE:              return ("CL_INVALID_BUFFER_SIZE");
+  case CL_INVALID_MIP_LEVEL:                return ("CL_INVALID_MIP_LEVEL");
+  default:                                  return ("UNKNOWN");
+  }
+} 
 
 /** REAL4 and GPU-ready version of ComputeFStatFreqBand(), extended to loop over segments as well.
  *
