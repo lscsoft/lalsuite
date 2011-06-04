33b9ce79ffeaf587874a9be2d474cccec5327f01
Riccardo Sturani <riccardo.sturani@ligo.org>
2011-06-04 23:30:51 +0200

    Typo corrected Original: d7621540204d8a7a06d96e1a6141b421755378b1

--- lalinspiral/src/GenerateInspiral.c
+++ lalinspiral/src/GenerateInspiral.c
@@ -595,7 +595,6 @@ LALGenerateInspiralPopulateInspiral(
   inspiralParams->nEndPad   =  16384;
 
   inspiralParams->massChoice  = m1Andm2;
-  inspiralParams->axisChoice  = ppnParams->axisChoice;
 
   /* spin parameters */
   inspiralParams->sourceTheta = GENERATEINSPIRAL_SOURCETHETA;
