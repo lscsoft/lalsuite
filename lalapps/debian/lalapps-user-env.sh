35807a15df2c7fcfe1340ecf4d233206a58347f7
Steffen Grunewald <steffen.grunewald@aei.mpg.de>
2011-06-27 10:52:30 -0500

    lalapps: rewritten rules to use pyversions Original: 2722d7a000fe4ac1c9c8f881b00437f395e643a1

--- lalapps/debian/lalapps-user-env.sh
+++ lalapps/debian/lalapps-user-env.sh
@@ -1,6 +1,9 @@
 # Source this file to access LALApps
-PATH="/opt/lscsoft/lalapps/bin:${PATH}"
-MANPATH="/opt/lscsoft/lalapps/share/man:${MANPATH}"
+LALAPPS_PREFIX=/opt/lscsoft/lalapps
+#LALAPPS_PREFIX=/usr
+export LALAPPS_PREFIX
+PATH="${LALAPPS_PREFIX}/bin:${PATH}"
+MANPATH="${LALAPPS_PREFIX}/share/man:${MANPATH}"
 PYSITE_PATH=python`python -V 2>&1 | cut -d' ' -f2 | cut -d. -f-2`
-PYTHONPATH="/opt/lscsoft/lalapps/lib/${PYSITE_PATH}/site-packages:${PYTHONPATH}"
+PYTHONPATH="${LALAPPS_PREFIX}/lib/${PYSITE_PATH}/site-packages:${PYTHONPATH}"
 export PATH MANPATH PYTHONPATH
