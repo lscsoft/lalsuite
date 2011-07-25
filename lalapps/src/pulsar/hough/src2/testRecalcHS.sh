cff143d89e5c4a569d9ea57c88bd1a2dba04627f
Reinhard Prix <reinhard.prix@ligo.org>
2011-07-25 15:35:17 +0200

    extended RecalcHS test to run on multi-IFO data Original: 24d5155949185d68958a890d6e30ce91c70573a1

--- lalapps/src/pulsar/hough/src2/testRecalcHS.sh
+++ lalapps/src/pulsar/hough/src2/testRecalcHS.sh
@@ -35,12 +35,12 @@ code_HS="${builddir}/lalapps_HierarchicalSearch"
 code_RC="${builddir}/lalapps_RecalcHSCandidates"
 
 ## ----- parameters
-SFTs="./allSFTs.sft"
+SFTsH1="./allSFTs_H1.sft"
+SFTsL1="./allSFTs_L1.sft"
+
 HS_OUT="./HS_out.dat"
 RC_OUT1="./RC_out1.dat"
 RC_OUT2="./RC_out2.dat"
-RC_OUT3="./RC_out3.dat"
-RC_OUT4="./RC_out4.dat"
 
 Freq0=100.5
 dFreq0=1.3e-5
@@ -49,7 +49,7 @@ nStacks=10
 Tspan=36000	## should be >= nStacks * tStack
 
 echo "----- STEP 1: produce some fake data:"
-cmdline="$code_MFD -I H1 --outSingleSFT --outSFTbname=$SFTs -y 05-09 -G 820108814 --duration=$Tspan --noiseSqrtSh=3e-23 --fmin=100 --Band=1 --randSeed=1 -v1"
+cmdline="$code_MFD -I H1 --outSingleSFT --outSFTbname=$SFTsH1 -y 05-09 -G 820108814 --duration=$Tspan --noiseSqrtSh=3e-23 --fmin=100 --Band=1 --randSeed=1 -v1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -57,8 +57,17 @@ if ! eval $cmdline; then
     exit 1
 fi
 
+cmdline="$code_MFD -I L1 --outSingleSFT --outSFTbname=$SFTsL1 -y 05-09 -G 820108814 --duration=$Tspan --noiseSqrtSh=3e-23 --fmin=100 --Band=1 --randSeed=2 -v1"
+
+echo $cmdline;
+if ! eval $cmdline; then
+    echo "Error.. something failed when running '$cmdline' ..."
+    exit 1
+fi
+
+
 echo "----- STEP 2: '$HS_OUT' -- run original HierarchicalSearch code on this data, producing some candidates:"
-cmdline="$code_HS --method=0 --DataFiles=$SFTs --skyRegion='(1,1)' --Freq=$Freq0 --FreqBand=1e-5 --dFreq=$dFreq0 --tStack=$tStack --nStacksMax=$nStacks --printCand1 --nf1dotRes=1 --semiCohToplist -o $HS_OUT -d1"
+cmdline="$code_HS --method=0 --DataFiles=\"$SFTsH1;$SFTsL1\" --skyRegion='(1,1)' --Freq=$Freq0 --FreqBand=1e-5 --dFreq=$dFreq0 --tStack=$tStack --nStacksMax=$nStacks --printCand1 --nf1dotRes=1 --semiCohToplist -o $HS_OUT -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -67,7 +76,7 @@ if ! eval $cmdline; then
 fi
 
 echo "## ----- STEP 3a: '$RC_OUT1' -- run 'RecalcHSCandidates' on this output candidate file"
-cmdline="$code_RC --DataFiles1=$SFTs --tStack=$tStack --nStacksMax=$nStacks --followupList=$HS_OUT --WU_Freq=$Freq0 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT1 -d1"
+cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$HS_OUT --WU_Freq=$Freq0 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT1 --outputFX=true -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -77,7 +86,7 @@ fi
 
 echo "## ----- STEP 3b: '$RC_OUT2' -- re-run RecalcHSCandidates on its own output to test stability"
 ## NOTE: this time we don't use any HS-frequency bug corrections (--WU_Freq), as RC outputs frequencies correctly!
-cmdline="$code_RC --DataFiles1=$SFTs --tStack=$tStack --nStacksMax=$nStacks --followupList=$RC_OUT1 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT2 -d1"
+cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$RC_OUT1 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT2 --outputFX=true -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -85,33 +94,11 @@ if ! eval $cmdline; then
     exit 1
 fi
 
-echo "## ----- STEP 3c: '$RC_OUT1' -- re-run 'RecalcHSCandidates' on original output candidate file, with outputFX option"
-cmdline="$code_RC --DataFiles1=$SFTs --tStack=$tStack --nStacksMax=$nStacks --followupList=$HS_OUT --WU_Freq=$Freq0 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT3 --outputFX -d1"
-
-echo $cmdline;
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$cmdline' ..."
-    exit 1
-fi
-
-echo "## ----- STEP 3d: '$RC_OUT2' -- re-re-run RecalcHSCandidates, with outputFX option, on its own output to test stability"
-## NOTE: this time we don't use any HS-frequency bug corrections (--WU_Freq), as RC outputs frequencies correctly!
-cmdline="$code_RC --DataFiles1=$SFTs --tStack=$tStack --nStacksMax=$nStacks --followupList=$RC_OUT3 --WU_dFreq=$dFreq0 --fnameout=$RC_OUT4 --outputFX -d1"
-
-echo $cmdline;
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$cmdline' ..."
-    exit 1
-fi
-
-## currently not really testing these results, just output them for user to look at:
+## currently not really testing these results against HS, just output them for user to look at:
 echo "$HS_OUT: --------------------"
 cat $HS_OUT
 echo "$RC_OUT1: --------------------"
 cat $RC_OUT1
-echo "----------------------------------------"
-cat $RC_OUT3
-echo "----------------------------------------"
 
 
 if ! diff -q RC_out1.dat RC_out2.dat ; then
@@ -120,13 +107,7 @@ if ! diff -q RC_out1.dat RC_out2.dat ; then
 else
     echo "$RC_OUT1 and RC_OUT2 are identical ... OK!"
 fi
-if ! diff -q RC_out3.dat RC_out4.dat ; then
-    echo "$RC_OUT3 and RC_OUT4 differ ... something is wrong!"
-    exit 1;
-else
-    echo "$RC_OUT3 and RC_OUT4 are identical ... OK!"
-fi
 
 if [ -z "$NOCLEANUP" ]; then
-    rm $HS_OUT $RC_OUT1 $RC_OUT2 $RC_OUT3 $RC_OUT4 $SFTs
+    rm $HS_OUT $RC_OUT1 $RC_OUT2 $SFTsH1 $SFTsL1
 fi
