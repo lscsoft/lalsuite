9fc2d6e948cf3982c5ba66edbd2dfd4aee76baad
Karl Wette <karl.wette@ligo.org>
2011-08-16 09:34:26 +0200

    Moved GSL inline flags from header to code body - Can't use these in header because GSL headers may have been included   before this header, and so GSL inline definitions such as INLINE_FUN   won't be defined if gsl_inline.h was already included. Original: 575c76005065957fbaff01e4883177ab5729460c

--- lalpulsar/src/TransientCW_utils.h
+++ lalpulsar/src/TransientCW_utils.h
@@ -40,10 +40,6 @@ extern "C" {
 
 /* ---------- System includes ---------- */
 /* gsl-includes */
-#define GSL_RANGE_CHECK_OFF 1
-#define GSL_C99_INLINE 1
-#define HAVE_INLINE 1
-
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 
