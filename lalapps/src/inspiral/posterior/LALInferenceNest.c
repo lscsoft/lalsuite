26386f2f51620b2307035aa64f3197d7bcbb2ad5
Adam Mercer <adam.mercer@ligo.org>
2011-08-16 10:14:24 -0500

    remove unused variables Original: 613686eb1a77ac70ff252b9f5d10fb26f856853b

--- lalapps/src/inspiral/posterior/LALInferenceNest.c
+++ lalapps/src/inspiral/posterior/LALInferenceNest.c
@@ -317,8 +317,8 @@ void initVariables(LALInferenceRunState *state)
 	REAL8 mcMin=1.0;
 	REAL8 mcMax=20.5;
 	REAL8 logmcMax,logmcMin,mMin=1.0,mMax=30.0;
-	REAL8 a_spin2_max=1.0, a_spin1_max=1.0;
-	REAL8 a_spin2_min=0.0, a_spin1_min=0.0;
+	REAL8 a_spin1_max=1.0;
+	REAL8 a_spin1_min=0.0;
 	REAL8 phi_spin1_min=-LAL_PI;
 	REAL8 phi_spin1_max=LAL_PI;
 	REAL8 theta_spin1_min=-LAL_PI/2.0;
