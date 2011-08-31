9f1f087cad25c15141696c5de3232e6826db4c17
Reinhard Prix <reinhard.prix@ligo.org>
2011-09-01 00:43:02 +0200

    make Recalc test use a non-trivial Hough skypatch Original: 967fa5a74ca6e613ece047e3e0b8bf56c1173166

--- lalapps/src/pulsar/hough/src2/testRecalcHS.sh
+++ lalapps/src/pulsar/hough/src2/testRecalcHS.sh
@@ -70,8 +70,11 @@ if ! eval $cmdline; then
 fi
 
 
+patch=0.8
+pixel=2
+
 echo "----- STEP 2: '$HS_OUT' -- run original HierarchicalSearch code on this data, producing some candidates:"
-cmdline="$code_HS --method=0 --DataFiles=\"$SFTsH1;$SFTsL1\" --skyRegion='(1,1)' --Freq=$Freq0 --FreqBand=$FreqBand --dFreq=$dFreq0 --f1dot=$f1dot --f1dotBand=$f1dotBand --df1dot=$df1dot --tStack=$tStack --nStacksMax=$nStacks --printCand1 --nf1dotRes=1 --semiCohPatchX=0.012405 --semiCohPatchY=0.012405 --semiCohToplist -o $HS_OUT -d1"
+cmdline="$code_HS --method=0 --DataFiles=\"$SFTsH1;$SFTsL1\" --skyRegion='(1,1)' --Freq=$Freq0 --FreqBand=$FreqBand --dFreq=$dFreq0 --f1dot=$f1dot --f1dotBand=$f1dotBand --df1dot=$df1dot --tStack=$tStack --nStacksMax=$nStacks --printCand1 --nf1dotRes=1 --semiCohPatchX=$patch --semiCohPatchY=$patch --pixelFactor=$pixel --semiCohToplist -o $HS_OUT -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -80,7 +83,7 @@ if ! eval $cmdline; then
 fi
 
 echo "## ----- STEP 3a: '$RC_OUT1' -- run 'RecalcHSCandidates' on this output candidate file"
-cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$HS_OUT --WU_Freq=$Freq0 --WU_dFreq=$dFreq0 --semiCohPatchX=0.012405 --semiCohPatchY=0.012405 --fnameout=$RC_OUT1 --outputFX=true -d1"
+cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$HS_OUT --WU_Freq=$Freq0 --WU_dFreq=$dFreq0 --semiCohPatchX=$patch --semiCohPatchY=$patch --pixelFactor=$pixel --fnameout=$RC_OUT1 --outputFX=true -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -90,7 +93,7 @@ fi
 
 echo "## ----- STEP 3b: '$RC_OUT2' -- re-run RecalcHSCandidates on its own output to test stability"
 ## NOTE: this time we don't use any HS-frequency bug corrections (--WU_Freq), as RC outputs frequencies correctly!
-cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$RC_OUT1 --WU_dFreq=$dFreq0 --semiCohPatchX=0.012405 --semiCohPatchY=0.012405 --fnameout=$RC_OUT2 --outputFX=true -d1"
+cmdline="$code_RC --DataFiles1=\"$SFTsH1;$SFTsL1\" --tStack=$tStack --nStacksMax=$nStacks --followupList=$RC_OUT1 --WU_dFreq=$dFreq0 --semiCohPatchX=$patch --semiCohPatchY=$patch --pixelFactor=$pixel --fnameout=$RC_OUT2 --outputFX=true -d1"
 
 echo $cmdline;
 if ! eval $cmdline; then
@@ -100,9 +103,9 @@ fi
 
 ## currently not really testing these results against HS, just output them for user to look at:
 echo "$HS_OUT: --------------------"
-cat $HS_OUT
+sort $HS_OUT
 echo "$RC_OUT1: --------------------"
-cat $RC_OUT1
+sort $RC_OUT1
 
 sort $RC_OUT1 > s1.dat
 sort $RC_OUT2 > s2.dat
