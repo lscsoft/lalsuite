13c2f75aa778f8a367bf83a3f9e6d737f1afc2f8
Adam Mercer <adam.mercer@ligo.org>
2011-11-16 10:28:41 -0600

    fix alignment warnings when using clang (Xcode-4.2) Original: c1077613250741338b7760081c5465cdb064ed8f

--- lalapps/src/pulsar/GCT/GCTtoplist.c
+++ lalapps/src/pulsar/GCT/GCTtoplist.c
@@ -349,7 +349,7 @@ int write_gctFStat_toplist_to_fp(toplist_t*tl, FILE*fp, UINT4*checksum) {
   if(checksum)
     *checksum = 0;
   for(i=0;i<tl->elems;i++)
-    if ((r = write_gctFStat_toplist_item_to_fp(*((GCTtopOutputEntry*)(tl->heap[i])), fp, checksum)) < 0) {
+    if ((r = write_gctFStat_toplist_item_to_fp(*((GCTtopOutputEntry*)(void*)(tl->heap[i])), fp, checksum)) < 0) {
       LogPrintf (LOG_CRITICAL, "Failed to write toplistitem to output fp: %d: %s\n",
 		 errno,strerror(errno));
 #ifdef _MSC_VER
