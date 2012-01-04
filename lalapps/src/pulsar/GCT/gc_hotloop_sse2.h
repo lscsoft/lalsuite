05abbc3360c01846b50a1031ad4b274841e25b2a
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2012-01-04 15:11:23 +0100

    added gc_hotloop_no_nc() (GC hotloop w/o numbercount calculation) Original: 9db4725494cde128112674edd0155f9bfa023ca1

--- lalapps/src/pulsar/GCT/gc_hotloop_sse2.h
+++ lalapps/src/pulsar/GCT/gc_hotloop_sse2.h
@@ -1,4 +1,5 @@
 static inline void gc_hotloop (REAL4 * fgrid2F, REAL4 * cgrid2F, UCHAR * fgridnc, REAL4 TwoFthreshold, UINT4 length  ) __attribute__ ((hot));
+static inline void gc_hotloop_no_nc (REAL4 * fgrid2F, REAL4 * cgrid2F, UCHAR * fgridnc, REAL4 TwoFthreshold, UINT4 length  ) __attribute__ ((hot));
 
 #ifdef __APPLE__
 
@@ -180,6 +181,97 @@ for( ; ifreq_fg +16 < length; ifreq_fg+=16 ) {
 	    fgridnc[0] += (TwoFthreshold < cgrid2F[0]);
 	    fgridnc++;
 #endif
+	    fgrid2F++;
+	    cgrid2F++;
+  } /* for( ifreq_fg = 0; ifreq_fg < finegrid.freqlength; ifreq_fg++ ) { */
+
+}
+
+void gc_hotloop_no_nc(REAL4 * fgrid2F, REAL4 * cgrid2F, UCHAR * fgridnc, REAL4 TwoFthreshold, UINT4 length  )  {
+  UINT4 ifreq_fg;
+
+  REAL4 VTTTT[4] __attribute__ ((aligned (16))) = { TwoFthreshold,TwoFthreshold,TwoFthreshold,TwoFthreshold };
+
+  /* ensure alignment on fine grid. note fgridnc is UCHAR*
+     while fgrid2F is UINT4*  */
+
+  int offset = ((UINT4)fgridnc & 0xf) ;
+
+  if(offset != 0) offset = 16-offset;
+
+  for(ifreq_fg=0; offset > 0 && ifreq_fg < length; ifreq_fg++, offset-- ) {
+
+	    fgrid2F[0] += cgrid2F[0] ;
+	    fgrid2F++;
+	    cgrid2F++;
+
+  } /* for( ifreq_fg = 0; ifreq_fg < finegrid.freqlength; ifreq_fg++ ) { */
+
+
+for( ; ifreq_fg +16 < length; ifreq_fg+=16 ) {
+    /* unrolled loop (16 iterations of original loop) */
+#ifdef EXP_NO_ASM
+
+#pragma ivdep
+  for(int j=0 ; j < 16; j++ ) {
+	    fgrid2F[0] += cgrid2F[0] ;
+	    fgrid2F++;
+	    cgrid2F++;
+  }
+
+#else
+    __asm __volatile (
+         "MOVUPS  (%[cg2F]),%%xmm2 \n\t"  /* load coarse grid values, possibly unaligned */
+         "MOVAPS  (%[fg2F]),%%xmm3 \n\t"
+         "MOVUPS  0x10(%[cg2F]),%%xmm4 \n\t"
+         "MOVUPS  0x20(%[cg2F]),%%xmm5 \n\t"
+         "MOVUPS  0x30(%[cg2F]),%%xmm6 \n\t"
+
+         /* Loop iterations 1...4 */
+
+         "ADDPS   %%xmm2,%%xmm3 \n\t"     /* Add four coarse grid 2F values to fine grid sums */
+         "MOVAPS  %%xmm3,(%[fg2F]) \n\t"  /* store 4 values in fine grid 2F sum array */
+
+         /* Loop iterations 5...8  (same as above) */
+
+         "MOVAPS  0x10(%[fg2F]),%%xmm2 \n\t"
+         "ADDPS   %%xmm4,%%xmm2 \n\t"
+         "MOVAPS  %%xmm2,0x10(%[fg2F]) \n\t"
+
+         /* Loop iterations 9...12  (same as above) */
+
+         "MOVAPS  0x20(%[fg2F]),%%xmm4 \n\t"
+         "ADDPS   %%xmm5,%%xmm4 \n\t"
+         "MOVAPS  %%xmm4,0x20(%[fg2F]) \n\t"
+
+         /* Loop iterations 13...16  (same as above) */
+
+         "MOVAPS  0x30(%[fg2F]),%%xmm2 \n\t"
+         "ADDPS   %%xmm6,%%xmm2 \n\t"
+         "MOVAPS  %%xmm2,0x30(%[fg2F]) \n\t"
+
+/*  ---------------------------------------------------*/
+:
+      /* output */
+
+      :
+      /* input */
+      [cg2F]       "r"  (cgrid2F),
+      [fg2F]       "r"  (fgrid2F)
+
+      : /* clobbered */
+      "xmm0","xmm1","xmm2","xmm3","xmm4","xmm5","xmm6","xmm7","memory"
+
+    ) ;
+
+#endif // EXP_No_ASM
+    fgrid2F+=16;
+    cgrid2F+=16;
+
+  }
+  /* take care of remaining iterations, length  modulo 16 */
+  for( ; ifreq_fg < length; ifreq_fg++ ) {
+	    fgrid2F[0] += cgrid2F[0] ;
 	    fgrid2F++;
 	    cgrid2F++;
   } /* for( ifreq_fg = 0; ifreq_fg < finegrid.freqlength; ifreq_fg++ ) { */
