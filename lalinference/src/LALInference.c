823a5356ee2141fd2673bb7a5f5cf6639789fa11
Adam Mercer <adam.mercer@ligo.org>
2012-01-13 13:51:30 -0600

    use correct format specifier for size_t fixes build on 32bit systems Original: 1c3b77c2ea7ac3c69aedc09cc7f0ee4eda332788

--- lalinference/src/LALInference.c
+++ lalinference/src/LALInference.c
@@ -1396,7 +1396,7 @@ static void printVector(REAL8 *pt, size_t dim, FILE *stream) {
 void LALInferencePrintCell(LALInferenceKDCell *cell, size_t dim, FILE *stream);
 void LALInferencePrintCell(LALInferenceKDCell *cell, size_t dim, FILE *stream) {
   if (cell != NULL) {
-    fprintf(stream, "  Cell: %p, npts = %ld\n    Left = %p, right = %p\n", cell, cell->npts, cell->left, cell->right);
+    fprintf(stream, "  Cell: %p, npts = %zd\n    Left = %p, right = %p\n", cell, cell->npts, cell->left, cell->right);
 
     fprintf(stream,   "    Left bounds: ");
     printVector(cell->lowerLeft, dim, stream);
@@ -1418,7 +1418,7 @@ void LALInferencePrintKDTree(LALInferenceKDTree *tree, FILE *stream);
 void LALInferencePrintKDTree(LALInferenceKDTree *tree, FILE *stream) {
   fprintf(stream, "KDTree: %p\n", tree);
   if (tree != NULL) {
-    fprintf(stream, "  npts = %ld\n  ndim = %ld\n", tree->npts, tree->ndim);
+    fprintf(stream, "  npts = %zd\n  ndim = %zd\n", tree->npts, tree->ndim);
     size_t i;
     for (i = 0; i < tree->npts; i++) {
       fprintf(stream, "    pt: ");
