a8d6c8bd5d02dbcb3e681ee0fe1bb006e89a6732
Reinhard Prix <reinhard.prix@ligo.org>
2012-02-10 17:10:29 +0100

    improved testCFSv2 script to cover some frequency-band    - the plan is to including timing-info output in a future patch    - fixed random-seed    - refs #346 Original: 2b6bee9f4663cd9ad99137cfe26b0453d28ba3b5

--- lalapps/src/pulsar/FDS_isolated/testCFSv2.sh
+++ lalapps/src/pulsar/FDS_isolated/testCFSv2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## take user-arguments for CFS-v2:
-extra_args="$@"
+## make sure we work in 'C' locale here to avoid awk sillyness
+LC_ALL_old=$LC_ALL
+export LC_ALL=C
 
 builddir="./";
 injectdir="../Injections/"
@@ -10,9 +11,15 @@ injectdir="../Injections/"
 mfd_code="${injectdir}lalapps_Makefakedata_v4"
 saf_code="${builddir}lalapps_SemiAnalyticF"
 cfs_code="${builddir}lalapps_ComputeFStatistic"
-cfsv2_code="${builddir}lalapps_ComputeFStatistic_v2"
 cmp_code="${builddir}lalapps_compareFstats"
+## allow user to specify a different CFSv2 version to test by passing as cmdline-argument
+if test $# -eq 0 ; then
+    cfsv2_code="${builddir}lalapps_ComputeFStatistic_v2"
+else
+    cfsv2_code="$@"
+fi
 
+extra_args=""
 SFTdir="./testSFTs"
 
 # test if LAL_DATA_PATH has been set ... needed to locate ephemeris-files
@@ -29,7 +36,12 @@ if [ -z "$LAL_DATA_PATH" ]; then
     fi
 fi
 
-Ftolerance=0.05
+## without noise-weights, CFSv1 and CFSv2 should agree extremely well,
+Ftolerance_NWoff=0.01
+## with noise-weights to calculations differ, so we need much more tolerance
+Ftolerance_NWon=0.1
+
+Dterms=8
 # ---------- fixed parameter of our test-signal
 Tsft=1800;
 startTime=711595934
@@ -48,9 +60,26 @@ psi=0.6
 phi0=1.5
 
 Freq=100.12345
+f1dot=-1e-10;
+
+## mfd-specific bands
 mfd_fmin=$(echo $Freq $mfd_FreqBand | awk '{printf "%g", $1 - $2 / 2.0}');
 
-f1dot=-1e-10;
+## cfs search bands
+NFreq=100;
+cfs_FreqBand=$(echo $duration | awk '{printf "%.16g", 1.0 / $1 }');	## fix band to 1/T so we're close to signal peak always
+cfs_Freq=$(echo $Freq $cfs_FreqBand | awk '{printf "%.16g", $1 - $2 / 2.0}');
+cfs_dFreq=$(echo $cfs_FreqBand $NFreq | awk '{printf "%.16g", $1 / $2 }');
+
+## unfortunately CFSv1 has a different band-convention resulting in one more frequency-bin
+## so we compensate for that by inputting a slightly smaller band in CFSv1 to get the same
+## bins for comparisong
+cfs_FreqBand_v1=$cfs_FreqBand  ##$(echo $cfs_FreqBand $cfs_dFreq | awk '{printf "%g", $1 - 0.5 * $2}' )
+
+cfs_f1dotBand=0;
+cfs_f1dot=$(echo $f1dot $cfs_f1dotBand | awk '{printf "%.16g", $1 - $2 / 2.0}');
+Nf1dot=10
+cfs_df1dot=1;##$(echo $cfs_f1dotBand $Nf1dot | awk '{printf "%g", $1 / $2}');
 
 noiseSqrtSh=5
 
@@ -89,7 +118,7 @@ if [ "$haveNoise" = true ]; then
     mfd_CL="$mfd_CL --noiseSqrtSh=$sqrtSh";
 fi
 
-cmdline="$mfd_code $mfd_CL"
+cmdline="$mfd_code $mfd_CL --randSeed=1"
 echo $cmdline;
 if ! eval "$cmdline &> /dev/null"; then
     echo "Error.. something failed when running '$mfd_code' ..."
@@ -100,12 +129,11 @@ echo
 echo -n "Running '$saf_code' ... "
 cmdline="$saf_code $saf_CL --sqrtSh=$sqrtSh"
 echo $cmdline
-if ! resF=`eval "$cmdline  &> /dev/null"`; then
+if ! resF=`eval "$cmdline  2> /dev/null"`; then
     echo "Error ... something failed running '$saf_code' ..."
     exit 1;
 fi
-echo  "ok."
-res2F=`echo $resF | awk '{printf "%g", 2.0 * $1}'`
+res2F=$(echo $resF | awk '{printf "%g", 2.0 * $1}')
 echo "The SemiAnalyticF calculations predicts: 2F = $res2F"
 
 echo
@@ -115,12 +143,12 @@ echo "----------------------------------------------------------------------"
 echo
 outfile_v1="Fstat_v1.dat";
 ## common cmdline-options for v1 and v2
-cfs_CL="--IFO=$IFO --Freq=$Freq --Alpha=$Alpha --Delta=$Delta --f1dot=$f1dot --DataFiles='$SFTdir/testSFT*' --refTime=$refTime"
+cfs_CL="--IFO=$IFO --Alpha=$Alpha --Delta=$Delta --Freq=$cfs_Freq --dFreq=$cfs_dFreq --f1dot=$cfs_f1dot --f1dotBand=$cfs_f1dotBand --df1dot=$cfs_df1dot --DataFiles='$SFTdir/testSFT*' --refTime=$refTime --Dterms=${Dterms}"
 if [ "$haveNoise" = false ]; then
     cfs_CL="$cfs_CL --SignalOnly"
 fi
 
-cmdline="$cfs_code $cfs_CL  --outputFstat=$outfile_v1 --expLALDemod=0 --Fthreshold=0"
+cmdline="$cfs_code $cfs_CL  --outputFstat=$outfile_v1 --expLALDemod=0 --Fthreshold=0 --FreqBand=$cfs_FreqBand_v1"
 echo $cmdline;
 
 if ! eval "$cmdline &> /dev/null"; then
@@ -134,7 +162,7 @@ echo " STEP 3: run CFS_v2 with perfect match"
 echo "----------------------------------------------------------------------"
 echo
 outfile_v2NWon="Fstat_v2NWon.dat";
-cmdlineNoiseWeightsOn="$cfsv2_code $cfs_CL --outputFstat=$outfile_v2NWon --TwoFthreshold=0 --UseNoiseWeights=true $extra_args"
+cmdlineNoiseWeightsOn="$cfsv2_code $cfs_CL --outputFstat=$outfile_v2NWon --TwoFthreshold=0 --FreqBand=$cfs_FreqBand --UseNoiseWeights=true $extra_args"
 echo $cmdlineNoiseWeightsOn;
 if ! eval "$cmdlineNoiseWeightsOn &> /dev/null"; then
     echo "Error.. something failed when running '$cfs_code' ..."
@@ -142,7 +170,7 @@ if ! eval "$cmdlineNoiseWeightsOn &> /dev/null"; then
 fi
 
 outfile_v2NWoff="Fstat_v2NWoff.dat";
-cmdlineNoiseWeightsOff="$cfsv2_code $cfs_CL --outputFstat=$outfile_v2NWoff --TwoFthreshold=0 --UseNoiseWeights=false $extra_args"
+cmdlineNoiseWeightsOff="$cfsv2_code $cfs_CL --outputFstat=$outfile_v2NWoff --TwoFthreshold=0 --FreqBand=$cfs_FreqBand --UseNoiseWeights=false $extra_args"
 echo $cmdlineNoiseWeightsOff;
 if ! eval "$cmdlineNoiseWeightsOff &> /dev/null"; then
     echo "Error.. something failed when running '$cfs_code' ..."
@@ -153,35 +181,31 @@ echo
 echo "----------------------------------------"
 echo " STEP 4: Comparing results: "
 echo "----------------------------------------"
-echo
-echo "----- CFS v1: "
-cat $outfile_v1 | grep -v "%%"
-echo "----- CFS v2 WITH noise-weights: "
-cat $outfile_v2NWon | grep -v "%%"
-echo "----- CFS v2 WITHOUT noise-weights: "
-cat $outfile_v2NWoff | grep -v "%%"
-
 
 echo
-cmdline="$cmp_code -1 ./$outfile_v1 -2 ./$outfile_v2NWoff --clusterFiles=0 --Ftolerance=$Ftolerance"
-echo $cmdline
+cmdline="$cmp_code -1 ./$outfile_v1 -2 ./$outfile_v2NWoff --clusterFiles=0 --Ftolerance=$Ftolerance_NWoff"
+echo -n $cmdline
 if ! eval $cmdline; then
-    echo "OUCH... files differ. Something might be wrong..."
+    echo "==> OUCH... files differ. Something might be wrong..."
     exit 2
 else
-    echo "OK."
+    echo "	==> OK."
 fi
 
-cmdline="$cmp_code -1 ./$outfile_v1 -2 ./$outfile_v2NWon --clusterFiles=0 --Ftolerance=$Ftolerance"
-echo $cmdline
+cmdline="$cmp_code -1 ./$outfile_v1 -2 ./$outfile_v2NWon --clusterFiles=0 --Ftolerance=$Ftolerance_NWon"
+echo -n $cmdline
 if ! eval $cmdline; then
-    echo "OUCH... files differ. Something might be wrong..."
+    echo "==> OUCH... files differ. Something might be wrong..."
     exit 2
 else
-    echo "OK."
+    echo "	==> OK."
 fi
+echo
 
 ## clean up files
 if [ -z "$NOCLEANUP" ]; then
     rm -rf $SFTdir $outfile_v1 $outfile_v2NWon $outfile_v2NWoff Fstats Fstats.log
 fi
+
+## restore original locale, just in case someone source'd this file
+export LC_ALL=$LC_ALL_old
