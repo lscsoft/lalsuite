1cf30c39abf34906d8c2fc52d3730924f50a0269
Evan Goetz <evan.goetz@ligo.org>
2012-02-15 10:14:14 +0100

    Added comments, updated dates, made some changes for testing UL code Original: ce585f872261337976be2eaa3059382ee25a7aa1

--- lalapps/src/pulsar/TwoSpect/TwoSpect.c
+++ lalapps/src/pulsar/TwoSpect/TwoSpect.c
@@ -1,5 +1,5 @@
 /*
-*  Copyright (C) 2010, 2011 Evan Goetz
+*  Copyright (C) 2010, 2011, 2012 Evan Goetz
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
