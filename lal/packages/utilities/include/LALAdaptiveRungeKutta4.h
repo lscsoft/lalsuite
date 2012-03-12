a796239cceffd76e188c0df7fbda8af7bc2cb46e
Evan Ochsner <evan.ochsner@ligo.org>
2012-03-12 01:30:16 -0500

    doxygenated LALNewAdaptiveRungeKutta4 in header file Original: 4399a31c8798c9f1b0624e419e4e9f3f3bbfc3ea

--- lal/packages/utilities/include/LALAdaptiveRungeKutta4.h
+++ lal/packages/utilities/include/LALAdaptiveRungeKutta4.h
@@ -67,15 +67,32 @@ int XLALAdaptiveRungeKutta4( ark4GSLIntegrator *integrator,
                          REAL8 tinit, REAL8 tend, REAL8 deltat,
                          REAL8Array **yout
                          );
-
-/* Should be nearly identical output to ARK4, but interpolates within
-   the integration evolution, instead of collecting non-equally-spaced
-   samples and using splines. */
-int XLALNewAdaptiveRungeKutta4( ark4GSLIntegrator *integrator,
-                                void *params,
-                                REAL8 *yinit,
-                                REAL8 tinit, REAL8 tend_in, REAL8 deltat,
-                                REAL8Array **yout );
+/**
+ * Fourth-order Runge-Kutta ODE integrator using Runge-Kutta-Fehlberg (RKF45)
+ * steps with adaptive step size control.  Intended for use in various 
+ * waveform generation routines such as SpinTaylorT4 and various EOB models.
+ * 
+ * The method is described in
+ *
+ * Abramowitz & Stegun, Handbook of Mathematical Functions, Tenth Printing, 
+ * National Bureau of Standards, Washington, DC, 1972 
+ * (available online at http://people.math.sfu.ca/~cbm/aands/ )
+ * 
+ * This function also includes "on-the-fly" interpolation of the differential
+ * equations at regular intervals in-between integration steps. This
+ * "on-the-fly" interpolation method is derived and described in the 
+ * Mathematica notebook "RKF_with_interpolation.nb"
+ *
+ * FIXME: Place NB in DCC, CVS or similar?
+ */
+int XLALNewAdaptiveRungeKutta4( ark4GSLIntegrator *integrator, /**< struct holding dydt, stopping test, stepper, etc. */
+                                void *params, /**< params struct used to compute dydt and stopping test */
+                                REAL8 *yinit, /**< pass in initial values of all variables - overwritten to final values */
+                                REAL8 tinit, /**< integration start time */
+                                REAL8 tend_in, /**< maximum integration time */
+                                REAL8 deltat, /**< step size for evenly sampled output */
+                                REAL8Array **yout /**< array holding the evenly sampled output */
+                                );
 
 #if 0
 { /* so that editors will match succeeding brace */
