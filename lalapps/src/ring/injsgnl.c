32c22168a1e54352e6eb07904b4e3a4f6d87925c
Ian Harry <ian.harry@ligo.org>
2012-04-26 07:40:16 -0700

    Enable NINJA injections for ringdown and GRB codes (cherry picked from commit 828d1ab2fe292696d67665f8db03e177b3981e97) Original: c41080beeb795bd9b417834ebd246ec94719eec0

--- lalapps/src/ring/injsgnl.c
+++ lalapps/src/ring/injsgnl.c
@@ -23,12 +23,15 @@
 #include <lal/LALStdio.h>
 #include <lal/AVFactories.h>
 #include <lal/GenerateRing.h>
+#include <lal/GenerateInspiral.h>
 #include <lal/LIGOLwXMLInspiralRead.h>
 #include <lal/LIGOLwXMLRingdownRead.h>
 #include <lal/LIGOLwXML.h>
 #include <lal/LIGOMetadataRingdownUtils.h>
 #include <lal/Units.h>
 #include <lal/FindChirp.h>
+#include <lal/LALSimInspiral.h>
+#include <lal/LALInspiral.h>
 
 #include "lalapps.h"
 #include "injsgnl.h"
@@ -71,6 +74,7 @@ int ring_inject_signal(
   CHAR                  fname[FILENAME_MAX];
   MetadataTable         ringinjections;
   LIGOLwXMLStream       xmlfp;
+  Approximant injApproximant;
  
   /* copy injectFile to injFile (to get rid of const qual) */
   strncpy( injFile, injectFile, sizeof( injFile ) - 1 );
@@ -141,7 +145,21 @@ int ring_inject_signal(
               response, injectSignalType ), &status );
         break;
       case LALRINGDOWN_EOBNR_INJECT: case LALRINGDOWN_PHENOM_INJECT:
-        LAL_CALL( LALFindChirpInjectSignals( &status, series, injectList, response ), &status );
+        // Check if these are NINJA injections
+        if (XLALGetApproximantFromString( injectList->waveform,
+                                   &injApproximant ) == XLAL_FAILURE)
+        {
+          fprintf( stderr, "could not parse approximant from sim_inspiral.waveform\n" );
+          exit( 1 );
+        }
+        if (injApproximant == NumRelNinja2)
+        {
+          XLALSimInjectNinjaSignals(series,ifoName,1./responseScale,injectList);
+        }
+        else
+        {
+          LAL_CALL( LALFindChirpInjectSignals( &status, series, injectList, response ), &status );
+        }
         break;
       default:
         error( "unrecognized injection signal type\n" );
