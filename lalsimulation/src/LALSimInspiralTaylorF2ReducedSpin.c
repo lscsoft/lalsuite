ad6165d45e12777806a0ddaed0d82bd5b3185819
Nickolas Fotopoulos <nickolas.fotopoulos@ligo.org>
2012-07-09 17:45:29 -0700

    TaylorF2ReducedSpin: Add check for deltaF <= 0 Original: 333d08540d78cd41cc5275436e54c0a955a927fa

--- lalsimulation/src/LALSimInspiralTaylorF2ReducedSpin.c
+++ lalsimulation/src/LALSimInspiralTaylorF2ReducedSpin.c
@@ -131,6 +131,7 @@ int XLALSimInspiralTaylorF2ReducedSpin(
     /* check inputs for sanity */
     if (*htilde) XLAL_ERROR(XLAL_EFAULT);
     if (phiStart < 0) XLAL_ERROR(XLAL_EDOM);
+    if (deltaF <= 0) XLAL_ERROR(XLAL_EDOM);
     if (m1_SI <= 0) XLAL_ERROR(XLAL_EDOM);
     if (m2_SI <= 0) XLAL_ERROR(XLAL_EDOM);
     if (fabs(chi) > 1) XLAL_ERROR(XLAL_EDOM);
