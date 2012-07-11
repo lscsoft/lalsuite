6c01f3f22bab533dec5949fde2f181d1de963214
Adam Mercer <adam.mercer@ligo.org>
2012-07-11 16:32:05 -0500

    don't ignore return values from freopen() Original: 4e09dea7c618e0b05245dadeb9c6af4a9e0ab6b5

--- lal/packages/stats/test/LALMomentTest.c
+++ lal/packages/stats/test/LALMomentTest.c
@@ -359,6 +359,8 @@ static void Usage (const char *program, int exitcode)
 
 static void ParseOptions (int argc, char *argv[])
 {
+  FILE *fp;
+
   while (1)
   {
     int c = -1;
@@ -380,8 +382,8 @@ static void ParseOptions (int argc, char *argv[])
         break;
 
       case 'q': /* quiet: run silently (ignore error messages) */
-        freopen ("/dev/null", "w", stderr);
-        freopen ("/dev/null", "w", stdout);
+        fp = freopen ("/dev/null", "w", stderr);
+        fp = freopen ("/dev/null", "w", stdout);
         break;
 
       case 'h':
