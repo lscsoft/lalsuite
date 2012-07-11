0800273d60bd3ece61aa3b0bc3a47de2c47b4d2d
Adam Mercer <adam.mercer@ligo.org>
2012-07-11 17:00:29 -0500

    don't ignore return values from freopen() Original: 8519234ecc0d7013c5da579f4c8d466b09064f8d

--- lalstochastic/test/StochasticOptimalFilterNormalizationTest.c
+++ lalstochastic/test/StochasticOptimalFilterNormalizationTest.c
@@ -1376,6 +1376,8 @@ static void Usage (const char *program, int exitcode)
 static void
 ParseOptions (int argc, char *argv[])
 {
+  FILE *fp;
+
   while (1)
   {
     int c = -1;
@@ -1429,8 +1431,8 @@ ParseOptions (int argc, char *argv[])
         break;
 
       case 'q': /* quiet: run silently (ignore error messages) */
-        freopen ("/dev/null", "w", stderr);
-        freopen ("/dev/null", "w", stdout);
+        fp = freopen ("/dev/null", "w", stderr);
+        fp = freopen ("/dev/null", "w", stdout);
         break;
 
       case 'h':
