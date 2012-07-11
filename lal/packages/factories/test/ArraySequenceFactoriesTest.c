d31c1fda302c9400bfc2057bdcaa09a55cfbdec2
Adam Mercer <adam.mercer@ligo.org>
2012-07-11 16:20:53 -0500

    don't ignore return value of freopen() Original: 57b59c81e206f50ddef62664feba9dda8039d08d

--- lal/packages/factories/test/ArraySequenceFactoriesTest.c
+++ lal/packages/factories/test/ArraySequenceFactoriesTest.c
@@ -228,6 +228,8 @@ Usage (const char *program, int exitcode)
 static void
 ParseOptions (int argc, char *argv[])
 {
+  FILE *fp;
+
   while (1)
   {
     int c = -1;
@@ -249,8 +251,8 @@ ParseOptions (int argc, char *argv[])
         break;
 
       case 'q': /* quiet: run silently (ignore error messages) */
-        freopen ("/dev/null", "w", stderr);
-        freopen ("/dev/null", "w", stdout);
+        fp = freopen ("/dev/null", "w", stderr);
+        fp = freopen ("/dev/null", "w", stdout);
         break;
 
       case 'h':
