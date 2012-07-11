5ff240c9376b9576bd36e325edd5b24b585bc5d1
Adam Mercer <adam.mercer@ligo.org>
2012-07-11 17:01:09 -0500

    don't ignore return values from freopen() Original: 8831e6cd325ffa7933c236f87732f191071d70fe

--- lalstochastic/test/OverlapReductionFunctionTest.c
+++ lalstochastic/test/OverlapReductionFunctionTest.c
@@ -649,6 +649,8 @@ Usage (const char *program, int exitcode)
 static void
 ParseOptions (int argc, char *argv[])
 {
+  FILE *fp;
+
   while (1)
   {
     int c = -1;
@@ -694,8 +696,8 @@ ParseOptions (int argc, char *argv[])
         break;
 
       case 'q': /* quiet: run silently (ignore error messages) */
-        freopen ("/dev/null", "w", stderr);
-        freopen ("/dev/null", "w", stdout);
+        fp = freopen ("/dev/null", "w", stderr);
+        fp = freopen ("/dev/null", "w", stdout);
         break;
 
       case 'h':
