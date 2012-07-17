5c1e4698f327bf341adc76dbbac680b3ad3b64f8
Alex Ayerdi <alex.ayerdi@ligo.org>
2012-07-17 15:44:46 -0500

    Modification of CUDA code in LALInference Original: 3cefa9b85d5ca77621756292a863c739ef827038

--- lalinference/src/LALInferenceUndecomposedFreqDomainLogLikelihood_GPU.cu
+++ lalinference/src/LALInferenceUndecomposedFreqDomainLogLikelihood_GPU.cu
@@ -45,110 +45,18 @@ void checkCUDAError(const char *msg)
     }                         
 }
 
-__global__ void chisquared_LogLikelihood_Kernel(REAL8 *d_sum, int lower, int dataSize,
-						COMPLEX16 *freqModelhPlus_Data,
-						COMPLEX16 *freqModelhCross_Data,
-						COMPLEX16 *freqData_Data,
-						REAL8 *oneSidedNoisePowerSpectrum_Data,
-						double FplusScaled,
-						double FcrossScaled,
-						double deltaF,
-						double twopit,
-						double deltaT,
-						double TwoDeltaToverN)
-{
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (idx < dataSize)
-	{
-		
-		__shared__ REAL8 ssum[MAX_THREADS];
-
-		idx += lower; //accounts for the shift that was made in the original loop
-
-		memset(ssum, 0, MAX_THREADS * sizeof(*ssum));
-
-		int tid = threadIdx.x;
-		int bid = blockIdx.x;
-
-		REAL8 plainTemplateReal = FplusScaled * freqModelhPlus_Data[idx].re  
-                          	          +  FcrossScaled * freqModelhCross_Data[idx].re;
-		REAL8 plainTemplateImag = FplusScaled * freqModelhPlus_Data[idx].im
-					  +  FcrossScaled * freqModelhCross_Data[idx].im;
-		
-                
-		/* do time-shifting...             */
-		/* (also un-do 1/deltaT scaling): */
-		double f = ((double) idx) * deltaF;
-
-		/* real & imag parts of  exp(-2*pi*i*f*deltaT): */
-		double re = cos(twopit * f);
-		double im = - sin(twopit * f);
-
-		REAL8 templateReal = (plainTemplateReal*re - plainTemplateImag*im) / deltaT;
-		REAL8 templateImag = (plainTemplateReal*im + plainTemplateImag*re) / deltaT;
-		double dataReal     = freqData_Data[idx].re / deltaT;
-		double dataImag     = freqData_Data[idx].im / deltaT;
-		
-		/* compute squared difference & 'chi-squared': */
-		double diffRe       = dataReal - templateReal;         // Difference in real parts...
-		double diffIm       = dataImag - templateImag;         // ...and imaginary parts, and...
-				
-		double diffSquared  = diffRe*diffRe + diffIm*diffIm ;  // ...squared difference of the 2 complex figures.
-		
-		ssum[tid] = ((TwoDeltaToverN * diffSquared) / oneSidedNoisePowerSpectrum_Data[idx]);
-
-		/*****   REDUCTION    *****/
-		
-		__syncthreads(); //all the temps should have data before we add them up
-
-		for (int i = blockDim.x / 2; i > 0; i >>= 1) { /* per block */
-			if (tid < i)
-			   ssum[tid] += ssum[tid + i];
-
-			__syncthreads();
-		}
-
-		d_sum[bid] = ssum[0];
-		
-	}
-}
-
-__global__ void chisquared_LogLikelihood_Kernel_2(double *d_re, double *d_im, double deltaF, double twopit, int lower, int shift, double dataSize)
+__global__ void chisquared_LogLikelihood_Kernel(double *d_re, double *d_im, double deltaF, double twopit, int lower, double dataSize)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	
 	if (idx < dataSize)
 	{
 		idx += lower;
-
 		double f = ((double) idx) * deltaF;
 
-		d_re[idx - lower + shift] = cos(twopit * f);
-		d_im[idx - lower + shift] = - sin(twopit * f);
+		d_re[idx - lower] = cos(twopit * f);
+		d_im[idx - lower] = - sin(twopit * f);
 	}
-}
-
-__global__ void reduction_Kernel(REAL8 *d_temp, REAL8 *d_sum, int dataSize)
-{
-	__shared__ REAL8 ssum[MAX_THREADS];
-
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	int tid = threadIdx.x;
-	int bid = blockIdx.x;
-	
-	ssum[tid] = (idx < dataSize) ? d_temp[idx] : 0;
-
-	__syncthreads();
-
-	for (int i = blockDim.x / 2; i > 0; i >>= 1) { /* per block */
-		if (tid < i)
-		   ssum[tid] += ssum[tid + i];
-
-		__syncthreads();
-	}
-
-	if (tid == 0) d_sum[bid] = ssum[0];
 }
 
 REAL8 LALInferenceUndecomposedFreqDomainLogLikelihood_GPU (LALInferenceVariables *currentParams, LALInferenceIFOData * data, 
@@ -169,9 +77,7 @@ REAL8 LALInferenceUndecomposedFreqDomainLogLikelihood_GPU (LALInferenceVariables
   double Fplus, Fcross;
   double FplusScaled, FcrossScaled;
   double diffRe, diffIm, diffSquared;
-  double dataReal, dataImag;
   REAL8 loglikeli;
-  REAL8 plainTemplateReal, plainTemplateImag;
   REAL8 templateReal, templateImag;
   int i, lower, upper;
   LALInferenceIFOData *dataPtr;
@@ -376,25 +282,7 @@ REAL8 LALInferenceUndecomposedFreqDomainLogLikelihood_GPU (LALInferenceVariables
     dim3 dimBlock(numThreadsPerBlock);
 
     size_t memSize = dataSize * sizeof(double);
-
-    //for concurrent streams
-    //cudaMallocHost of host data
-    //cudaMalloc of device data
-    //make two streams cudaStream_t stream1 cudaStream_t stream2
-    //create the streams cudaStreamCreate(&stream1)  cudaStreamCreate(&stream2)
-    //create event handlers  cudaEvent_t start_event, stop_event;
-    //create the event cudaEventCreate(&start_event) cudaEventCreate(&stop_event);
-    //start recording the event cudaEventRecord(start_event, 0);
-    //launch kernel to streams kernel<<<dimGrid,dimBlocks,0,stream1>>>(args);  kernel<<<dimGrid,dimBlocks,0,stream1>>>(args);
-    //optional: after each launch you need an event cudaEventRecord(kernelEvent[i], streams[i])
-    //copy the data cudaMemcpyAsync(host, device, sizeof(memorySize), cudaMemcpyDeviceToHost, stream1/2)
-    //record the event cudaEventRecord(stop_event, 0)
-    //cudaEventSynchronize(stop_event)
-    //cudaStreamDestroy(stream1/2);
-    //cudaEventDestroy(start_event);
-    //cudaEventDestroy(stop_event);
-    //cudaFreeHost(host);
-    //cudaFree(device);
+    size_t realMemSize = dataSize * sizeof(REAL8);
 
     double *d_re;
     double *d_im;
@@ -402,64 +290,50 @@ REAL8 LALInferenceUndecomposedFreqDomainLogLikelihood_GPU (LALInferenceVariables
     double *h_re;
     double *h_im;
 
-    //h_re = (double *)malloc(memSize);
-    //h_im = (double *)malloc(memSize);
-
-    cudaMallocHost((void **)&h_re, memSize);
-    cudaMallocHost((void **)&h_im, memSize);
+    h_re = (double *)malloc(memSize);
+    h_im = (double *)malloc(memSize);
 
     cudaMalloc((void **)&d_re, memSize);
     cudaMalloc((void **)&d_im, memSize);
 
-    cudaStream_t stream1;
-    cudaStream_t stream2;
-    cudaStreamCreate(&stream1);
-    cudaStreamCreate(&stream2);
+    REAL8 *h_plainTemplateReal;
+    h_plainTemplateReal = (REAL8 *)malloc(realMemSize);
 
-    cudaEvent_t start_event, stop_event;
-    cudaEventCreate(&start_event);
-    cudaEventCreate(&stop_event);
+    REAL8 *h_plainTemplateImag;
+    h_plainTemplateImag = (REAL8 *)malloc(realMemSize);
 
-    cudaEventRecord(start_event, 0);
+    REAL8 *h_dataReal;
+    h_dataReal = (REAL8 *)malloc(realMemSize);
 
-    chisquared_LogLikelihood_Kernel_2<<<dimGrid, dimBlock, 0, stream1>>>(d_re, d_im, deltaF, twopit, lower, 0, dataSize / 2);
+    REAL8 *h_dataImag;
+    h_dataImag = (REAL8 *)malloc(realMemSize);
 
-    chisquared_LogLikelihood_Kernel_2<<<dimGrid, dimBlock, 0, stream2>>>(d_re, d_im, deltaF, twopit, lower + (dataSize / 2), dataSize / 2, dataSize / 2);
-
-    // block until the device has completed
-    //cudaThreadSynchronize();
-
-    //checkCUDAError("kernel execution");
-
-    cudaMemcpyAsync(h_re, d_re, memSize, cudaMemcpyDeviceToHost, stream1);
-    cudaMemcpyAsync(h_im, d_im, memSize, cudaMemcpyDeviceToHost, stream2);
-
-    cudaEventRecord(stop_event, 0);
-    cudaEventSynchronize(stop_event);
-
-    cudaStreamDestroy(stream1);
-    cudaStreamDestroy(stream2);
-    cudaEventDestroy(start_event);
-    cudaEventDestroy(stop_event);
-
-    //checkCUDAError("cudaMemcpy");
+    chisquared_LogLikelihood_Kernel<<<dimGrid, dimBlock>>>(d_re, d_im, deltaF, twopit, lower, dataSize);
 
     for (i=lower; i<=upper; ++i){
       /* derive template (involving location/orientation parameters) from given plus/cross waveforms: */
-      plainTemplateReal = FplusScaled * dataPtr->freqModelhPlus->data->data[i].re  
+      h_plainTemplateReal[i - lower] = FplusScaled * dataPtr->freqModelhPlus->data->data[i].re  
                           +  FcrossScaled * dataPtr->freqModelhCross->data->data[i].re;
-      plainTemplateImag = FplusScaled * dataPtr->freqModelhPlus->data->data[i].im  
+      h_plainTemplateImag[i - lower] = FplusScaled * dataPtr->freqModelhPlus->data->data[i].im  
                           +  FcrossScaled * dataPtr->freqModelhCross->data->data[i].im;
-
+      h_dataReal[i - lower]     = dataPtr->freqData->data->data[i].re / deltaT;
+      h_dataImag[i - lower]     = dataPtr->freqData->data->data[i].im / deltaT;
+    }
+    // block until the device has completed
+    cudaThreadSynchronize();
+    
+    cudaMemcpy(h_re, d_re, memSize, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_im, d_im, memSize, cudaMemcpyDeviceToHost);
+    
+    for (i=lower; i<=upper; ++i){
       /* real & imag parts of  exp(-2*pi*i*f*deltaT): */
-      templateReal = (plainTemplateReal*h_re[i - lower] - plainTemplateImag*h_im[i - lower]) / deltaT;
-      templateImag = (plainTemplateReal*h_im[i - lower] + plainTemplateImag*h_re[i - lower]) / deltaT;
-      dataReal     = dataPtr->freqData->data->data[i].re / deltaT;
-      dataImag     = dataPtr->freqData->data->data[i].im / deltaT;
+      templateReal = (h_plainTemplateReal[i - lower]*h_re[i - lower] - h_plainTemplateImag[i - lower]*h_im[i - lower]) / deltaT;
+      templateImag = (h_plainTemplateReal[i - lower]*h_im[i - lower] + h_plainTemplateImag[i - lower]*h_re[i - lower]) / deltaT;
+
       /* compute squared difference & 'chi-squared': */
-      diffRe       = dataReal - templateReal;         // Difference in real parts...
-      diffIm       = dataImag - templateImag;         // ...and imaginary parts, and...
-      diffSquared  = diffRe*diffRe + diffIm*diffIm ;  // ...squared difference of the 2 complex figures.
+      diffRe       = h_dataReal[i - lower] - templateReal;         // Difference in real parts...
+      diffIm       = h_dataImag[i - lower] - templateImag;         // ...and imaginary parts, and...
+      diffSquared  = diffRe*diffRe + diffIm*diffIm;  // ...squared difference of the 2 complex figures.
       REAL8 temp = ((TwoDeltaToverN * diffSquared) / dataPtr->oneSidedNoisePowerSpectrum->data->data[i]);
       
       chisquared  += temp;
@@ -468,90 +342,12 @@ REAL8 LALInferenceUndecomposedFreqDomainLogLikelihood_GPU (LALInferenceVariables
     
     cudaFree(d_re);
     cudaFree(d_im);
-    cudaFreeHost(h_re);
-    cudaFreeHost(h_im);
-
-    //free(h_re);
-    //free(h_im);
-
-    //size_t sumMemSize = numBlocks * sizeof(REAL8);
-    //size_t complexMemSize = (upper + 1) * sizeof(COMPLEX16); 
-    //size_t realMemSize = (upper + 1) * sizeof(REAL8);
-
-    //the host pointer to be used for the overall summation in host
-    //REAL8 *h_sum;   
-
-    //h_sum = (REAL8 *)malloc(sumMemSize);
-    //memset(h_sum, 0, sumMemSize);
-
-    //the device pointer for the per-block reduction summation
-    //REAL8 *d_sum;   
-
-    //device pointers for data from dataPtr
-    //COMPLEX16 *d_freqModelhPlus_data;
-    //COMPLEX16 *d_freqModelhCross_data;
-    //COMPLEX16 *d_freqData_data;
-    //REAL8 *d_oneSidedNoisePowerSpectrum_data;
-
-    //device memory allocation
-    //cudaMalloc((void **)&d_sum, sumMemSize);
-   
-    //cudaMalloc((void **)&d_freqModelhPlus_data, complexMemSize);
-    //cudaMalloc((void **)&d_freqModelhCross_data, complexMemSize);
-    //cudaMalloc((void **)&d_freqData_data, complexMemSize);
-    //cudaMalloc((void **)&d_oneSidedNoisePowerSpectrum_data, realMemSize);
-
-    //cudaMemcpy(d_freqModelhPlus_data, dataPtr->freqModelhPlus->data->data, complexMemSize, cudaMemcpyHostToDevice);
-    //cudaMemcpy(d_freqModelhCross_data, dataPtr->freqModelhCross->data->data, complexMemSize, cudaMemcpyHostToDevice);
-    //cudaMemcpy(d_freqData_data, dataPtr->freqData->data->data, complexMemSize, cudaMemcpyHostToDevice);
-    //cudaMemcpy(d_oneSidedNoisePowerSpectrum_data, dataPtr->oneSidedNoisePowerSpectrum->data->data, realMemSize, cudaMemcpyHostToDevice);
-    
-    //cudaEventRecord(start, 0);
-
-    //launch the kernel
-    //chisquared_LogLikelihood_Kernel<<<dimGrid, dimBlock>>>(d_sum, lower, dataSize,
-							//d_freqModelhPlus_data,
-							//d_freqModelhCross_data,
-							//d_freqData_data,
-							//d_oneSidedNoisePowerSpectrum_data,
-	    				   		//FplusScaled,
-            				   		//FcrossScaled,
-            				   		//deltaF,
-            				   		//twopit,
-            				   		//deltaT,
-            				   		//TwoDeltaToverN);
-
-    //chisquared_LogLikelihood_Kernel<<<dimGrid, dimBlock>>>(d_re, d_im);
-
-    // block until the device has completed
-    //cudaThreadSynchronize();
-
-    // check if kernel execution generated an error
-    //checkCUDAError("kernel execution");
-
-    //cudaEventRecord(stop, 0);
-    //cudaEventSynchronize(stop);
-
-    //cudaMemcpy(h_sum, d_sum, sumMemSize, cudaMemcpyDeviceToHost);
-
-    //checkCUDAError("cudaMemcpy");
-
-    //for (i = 0; i < numBlocks; i++)
-    //{
-    //    chisquared += h_sum[i];
-    //    dataPtr->loglikelihood -= h_sum[i];
-    //}
-    
-    //cudaFree(d_sum);
-    //cudaFree(d_freqModelhPlus_data);
-    //cudaFree(d_freqModelhCross_data);
-    //cudaFree(d_freqData_data);
-    //cudaFree(d_oneSidedNoisePowerSpectrum_data);
-    //free(h_sum);
-
-    //cudaEventElapsedTime(&time, start, stop);
-    //printf ("Time for the kernel: %f ms\n", time);
-    //totalTime += time;
+    free(h_re);
+    free(h_im);
+    free(h_plainTemplateReal);
+    free(h_plainTemplateImag);
+    free(h_dataReal);
+    free(h_dataImag);
 
     dataPtr = dataPtr->next;
 	//fclose(testout);
