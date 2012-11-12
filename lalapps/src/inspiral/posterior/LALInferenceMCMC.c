51ba439d45da72d5877c14531ef76d379ad04354
Benjamin Farr <bfarr@u.northwestern.edu>
2012-11-12 00:23:33 -0600

    lalinference_mcmc: fixed bug in setting time prior width Original: cf90e8fe8d0297a261490402bf87b30869227b4e

--- lalapps/src/inspiral/posterior/LALInferenceMCMC.c
+++ lalapps/src/inspiral/posterior/LALInferenceMCMC.c
@@ -1010,6 +1010,12 @@ void initVariables(LALInferenceRunState *state)
     if(strstr(ppt->value,"SMOOTH")) bookends=LALINFERENCE_SMOOTH;
   }
 
+  /* Over-ride time prior if specified */
+  ppt=LALInferenceGetProcParamVal(commandLine,"--dt");
+  if(ppt){
+    dt=atof(ppt->value);
+  }
+
   /* Over-ride end time if specified */
   ppt=LALInferenceGetProcParamVal(commandLine,"--trigtime");
   if(ppt && !analytic){
@@ -1113,12 +1119,6 @@ void initVariables(LALInferenceRunState *state)
     start_lambda2 = atof(ppt->value);
   }
   
-  /* Over-ride time prior if specified */
-  ppt=LALInferenceGetProcParamVal(commandLine,"--dt");
-  if(ppt){
-    dt=atof(ppt->value);
-  }
-
   /* Over-ride Distance min if specified */
   ppt=LALInferenceGetProcParamVal(commandLine,"--Dmin");
   if(ppt){
