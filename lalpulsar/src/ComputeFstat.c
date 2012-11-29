lscsoft/lalsuite-archive

commit a6571a5ce7610ef9f756bf7d2798fe35bf9766f7

implemented Miroslav's 3rd step: new option SSBPREC_RELATIVISTICOPT for LALGetSSBTimes()
   - produces binary equivalent SSB-timing numbers, should gain ~60%
     speed for fully buffered cases (and ~27% for unbuffered ones)
   - refs #347

lalpulsar/src/ComputeFstat.c
@@ -95,6 +95,7 @@ const MultiSSBtimes empty_MultiSSBtimes;
 const Fcomponents empty_Fcomponents;
 const ComputeFParams empty_ComputeFParams;
 const ComputeFBuffer empty_ComputeFBuffer;
+const BarycenterBuffer empty_BarycenterBuffer;
 
 static const LALStatus blank_status;
 
@@ -1463,6 +1464,9 @@ LALGetSSBtimes (LALStatus *status,		/**< pointer to LALStatus structure */
   delta = pos.latitude;
   refTimeREAL8 = GPS2REAL8(refTime);
 
+  BarycenterInput baryinput = empty_BarycenterInput;
+  BarycenterBuffer bBuffer = empty_BarycenterBuffer;
+
   /*----- now calculate the SSB transformation in the precision required */
   switch (precision)
     {
@@ -1489,22 +1493,22 @@ LALGetSSBtimes (LALStatus *status,		/**< pointer to LALStatus structure */
       break;
 
     case SSBPREC_RELATIVISTIC:	/* use LALBarycenter() to get SSB-times and derivative */
+
+      baryinput.site = DetectorStates->detector;
+      baryinput.site.location[0] /= LAL_C_SI;
+      baryinput.site.location[1] /= LAL_C_SI;
+      baryinput.site.location[2] /= LAL_C_SI;
+
+      baryinput.alpha = alpha;
+      baryinput.delta = delta;
+      baryinput.dInv = 0;
+
       for (i=0; i < numSteps; i++ )
 	{
-	  BarycenterInput baryinput = empty_BarycenterInput;
 	  EmissionTime emit;
 	  DetectorState *state = &(DetectorStates->data[i]);
 
 	  baryinput.tgps = state->tGPS;
-	  baryinput.site = DetectorStates->detector;
-	  /* ARGHHH!!! */
-	  baryinput.site.location[0] /= LAL_C_SI;
-	  baryinput.site.location[1] /= LAL_C_SI;
-	  baryinput.site.location[2] /= LAL_C_SI;
-
-	  baryinput.alpha = alpha;
-	  baryinput.delta = delta;
-	  baryinput.dInv = 0;
 
 	  TRY ( LALBarycenter(status->statusPtr, &emit, &baryinput, &(state->earthState)), status);
 
@@ -1513,7 +1517,34 @@ LALGetSSBtimes (LALStatus *status,		/**< pointer to LALStatus structure */
 
 	} /* for i < numSteps */
 
+    case SSBPREC_RELATIVISTICOPT:	/* use optimized version XLALBarycenterOpt() */
+
+      baryinput.site = DetectorStates->detector;
+      baryinput.site.location[0] /= LAL_C_SI;
+      baryinput.site.location[1] /= LAL_C_SI;
+      baryinput.site.location[2] /= LAL_C_SI;
+
+      baryinput.alpha = alpha;
+      baryinput.delta = delta;
+      baryinput.dInv = 0;
+
+      for ( i=0; i < numSteps; i++ )
+        {
+          EmissionTime emit;
+          DetectorState *state = &(DetectorStates->data[i]);
+          baryinput.tgps = state->tGPS;
+
+          if ( XLALBarycenterOpt ( &emit, &baryinput, &(state->earthState), &bBuffer ) != XLAL_SUCCESS ) {
+            XLALPrintError ("XLALBarycenterOpt() failed with xlalErrno = %d\n", xlalErrno );
+            ABORT (status, COMPUTEFSTATC_EXLAL, COMPUTEFSTATC_MSGEXLAL);
+          }
+
+          tSSB->DeltaT->data[i] = GPS2REAL8 ( emit.te ) - refTimeREAL8;
+          tSSB->Tdot->data[i] = emit.tDot;
+
+        } /* for i < numSteps */
       break;
+
     default:
       XLALPrintError ("\n?? Something went wrong.. this should never be called!\n\n");
       ABORT (status, COMPUTEFSTATC_EINPUT, COMPUTEFSTATC_MSGEINPUT);

lalpulsar/src/ComputeFstat.h
@@ -124,6 +124,7 @@ typedef struct tagFcomponents {
 typedef enum {
   SSBPREC_NEWTONIAN,		/**< simple Newtonian: \f$\tau = t + \vec{r}\cdot\vec{n}/c\f$ */
   SSBPREC_RELATIVISTIC,		/**< detailed relativistic: \f$\tau=\tau(t; \vec{n}, \vec{r})\f$ */
+  SSBPREC_RELATIVISTICOPT,  	/**< optimized relativistic, numerically equivalent to #SSBPREC_RELATIVISTIC, but faster */
   SSBPREC_LAST			/**< end marker */
 } SSBprecision;
 

lscsoft/lalsuite

commit 5a0a73e1d34b373d3c5dfda69ce8b110bf72b56d

fix broken switch statement in LALGetSSBtimes()    - this bug was introduced in commit      a6571a5ce7610ef9f756bf7d2798fe35bf9766f7, vetted on Redmine in #347    - missing 'break' causes fall-through from SSBprecision option      SSBPREC_RELATIVISTIC into SSBPREC_RELATIVISTICOPT, which      mysteriously seems to fail for HS-GCT ...    - fixes #631, but    - this fixes the *default* behaviour (SSBPREC_RELATIVISTIC), but      still fails for --SSBprecision=2    - refs #633, #347, #631 Original: d18a08d83d6555310c9eb2745043e613b0f0a04d

lalpulsar/src/ComputeFstat.c
@@ -1517,6 +1517,8 @@ LALGetSSBtimes (LALStatus *status,		/**< pointer to LALStatus structure */
 
 	} /* for i < numSteps */
 
+      break;
+
     case SSBPREC_RELATIVISTICOPT:	/* use optimized version XLALBarycenterOpt() */
 
       baryinput.site = DetectorStates->detector;