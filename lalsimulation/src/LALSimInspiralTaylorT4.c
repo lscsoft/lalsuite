fc5cffead757ee0afc39e02f1c80dc4f80df1368
Evan Ochsner <evan.ochsner@ligo.org>
2012-12-04 18:07:50 -0600

    Removed unintended semicolond refs #648, #649 Original: 8a7419cdeb4a84082b3eeea3dd2b9ee35e9fffef

--- lalsimulation/src/LALSimInspiralTaylorT4.c
+++ lalsimulation/src/LALSimInspiralTaylorT4.c
@@ -357,16 +357,16 @@ XLALSimInspiralTaylorT4Setup(
     ak->aat12   = 0.;
     if( interactionFlags >= LAL_SIM_INSPIRAL_INTERACTION_TIDAL_5PN)
     {
-        akdEF->ETa5 = XLALSimInspiralPNEnergy_10PNTidalCoeff(ak->chi2,ak->chi1,lambda1);
+        akdEF->ETa5 = XLALSimInspiralPNEnergy_10PNTidalCoeff(ak->chi2,ak->chi1,lambda1)
                     + XLALSimInspiralPNEnergy_10PNTidalCoeff(ak->chi1,ak->chi2,lambda2);
-        ak->aat10   = XLALSimInspiralTaylorT4AngularAccel_10PNTidalCoeff(ak->chi1,lambda1);
+        ak->aat10   = XLALSimInspiralTaylorT4AngularAccel_10PNTidalCoeff(ak->chi1,lambda1)
                     + XLALSimInspiralTaylorT4AngularAccel_10PNTidalCoeff(ak->chi2,lambda2);
     }
     if( interactionFlags >= LAL_SIM_INSPIRAL_INTERACTION_TIDAL_6PN ) 
     {
-        akdEF->ETa6 = XLALSimInspiralPNEnergy_12PNTidalCoeff(ak->chi2,ak->chi1,lambda1);
+        akdEF->ETa6 = XLALSimInspiralPNEnergy_12PNTidalCoeff(ak->chi2,ak->chi1,lambda1)
                     + XLALSimInspiralPNEnergy_12PNTidalCoeff(ak->chi1,ak->chi2,lambda2);
-        ak->aat12   = XLALSimInspiralTaylorT4AngularAccel_12PNTidalCoeff(ak->nu,ak->chi1,lambda1);
+        ak->aat12   = XLALSimInspiralTaylorT4AngularAccel_12PNTidalCoeff(ak->nu,ak->chi1,lambda1)
                     + XLALSimInspiralTaylorT4AngularAccel_12PNTidalCoeff(ak->nu,ak->chi2,lambda2);
     }
 
