2c1b711341c0a3e10afc05175e7524087461b73c
Matthew Pitkin <matthew.pitkin@ligo.org>
2012-12-08 23:56:17 +0000

    Added script to post process output of pulsar MCMC code Original: 860cedb90258d7574c961e8709664a6920725759

---
+++ lalapps/src/pulsar/TDS_isolated/pulsarMCMCPostProc.py
@@ -0,0 +1,537 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       pulsarMCMCPostProc.py
+#
+#       Copyright 2012
+#       Matthew Pitkin <matthew.pitkin@ligo.org>,
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#
+
+# This is based heavily on cbcBayesPostProc.py written by:
+#       Benjamin Aylott <benjamin.aylott@ligo.org>,
+#       Benjamin Farr <bfarr@u.northwestern.edu>,
+#       Will M. Farr <will.farr@ligo.org>,
+#       John Veitch <john.veitch@ligo.org>
+
+#standard library imports
+import sys
+import os
+import re
+import shutil
+import math
+
+#related third party imports
+import numpy as np
+
+import matplotlib
+matplotlib.use("Agg")
+
+#local application/library specific imports
+from pylal import bayespputils as bppu
+from pylal import git_version
+
+from lalapps import pulsarpputils as pppu
+
+import urllib2
+from BeautifulSoup import BeautifulSoup as bs
+
+__author__="Matthew Pitkin <matthew.pitkin@ligo.org>"
+__version__= "git id %s"%git_version.id
+__date__= git_version.date
+    
+# main function
+if __name__=='__main__':
+  from optparse import OptionParser
+  
+  description = \
+"""This script is for creating a results output page and plots for the known
+   pulsar analysis. It uses inputs from the old MCMC code
+   lalapps_pulsar_parameter_estimation."""
+
+  epilog = " An example of usage for a case when three nested sampling runs \
+have been performed for two interferometers (H1 and L1): \
+"+os.path.basename(sys.argv[0])+" --ifo H1 --data \
+nest1_H1.txt,nest2_H1.txt,nest3_H1.txt --ifo L1 --data \
+nest1_L1.txt,nest2_L1.txt,nest3_L1.txt --parfile J0534-2200.par --Nlive 1000 \
+--priorfile priors.txt --histbins 50 --outpath \
+/home/me/public_html/J0534-2200"
+  usage = "Usage: %prog [options]"
+  
+  parser = OptionParser( usage = usage, description = description,
+                         version = __version__, epilog = epilog )
+  
+  parser.add_option("-o", "--outpath", dest="outpath", help="The path for "
+                    "the analysis output", metavar="DIR")
+  
+  """
+   data will be read in in the following format:
+     multiple MCMC directories from each IFO would be entered as follows:
+     --ifo H1 --Bkfiles /home/user/finehetsH1
+     --ifo L1 --Bkfiles /home/user/finehetsL1
+     --mcmcdirs /home/user/pdfs1,/home/user/pdfs2,/home/user/pdfs3
+  """
+  parser.add_option("-m","--mcmcdirs", dest="mcmcdirs",\
+                    help="A list "\
+                    "of MCMC directories containing chain for all IFOs")
+  
+  # Turn on 2D kdes
+  #parser.add_option("--twodkdeplots", action="store_true", default=False,
+  #                  dest="twodkdeplots")
+  
+  # Turn on R convergence tests
+  #parser.add_option("--RconvergenceTests", action="store_true", default=False,
+  #                  dest="RconvergenceTests")
+  
+  # get pulsar .par file
+  parser.add_option("-p", "--parfile", dest="parfile", help="The "
+                    "directory containing several, or an individual, "
+                    "TEMPO-style pulsar parameter file used in the analysis. "
+                    "[required]", metavar="PNAME.par", default=None)
+ 
+  # get heterodyned data files used for analysis
+  parser.add_option("-B", "--Bkfiles", dest="Bkfiles", action="append", 
+                    help="A directory containing the heterodyned data files "
+                    "for a given detector [required]", default=None)
+  
+  # get list of the detectors used (same order as Bk files)
+  parser.add_option("-i", "--ifo", dest="ifos", help="The individual "
+                    "interferometers from which analysis output, and data "
+                    "files have been supplied. [required]", action="append",
+                    default=None)
+  
+  # get number of bins for histogramming (default = 50)
+  parser.add_option("-b", "--histbins", dest="histbins", help="The number of " \
+                    "bins for histrogram plots. [default = 50]", default=50)
+  
+  # parse input options
+  (opts, args) = parser.parse_args()
+  
+  # check that output path has been given
+  if not opts.__dict__['outpath']:
+    print "Must specify an output path"
+    parser.print_help()
+    exit(-1)
+  else:
+    outpath = opts.outpath
+  
+  # check that some data has been given
+  if not opts.__dict__['mcmcdirs']:
+    print "Must specify MCMC chain directories"
+    parser.print_help()
+    exit(-1)
+    
+  # check that parfile has been given
+  if not opts.__dict__['parfile']:
+    print "Must specify a pulsar TEMPO .par file"
+    parser.print_help()
+    exit(-1)
+       
+  if not opts.__dict__['ifos']:
+    print "Must specify the interferometers analysed"
+    parser.print_help()
+    exit(-1)
+  else:
+    ifos = opts.ifos
+  
+  if not opts.__dict__['Bkfiles']:
+    print "Must specify the heterodyned data files"
+    parser.print_help()
+    exit(-1)
+  else:
+    Bkfiles = opts.Bkfiles
+
+  # create output directory if it doesn't exist
+  if not os.path.isdir(outpath):
+    try:
+      os.mkdir(outpath)
+    except:
+      print >> sys.stderr, "Cannot create output directory %s" % outpath
+      sys.exit(1)
+    
+  # check that number of ifos is the same as the number of data lists
+  nifos = len(ifos)
+  ndata = len(Bkfiles)
+  
+  if nifos != ndata:
+    print "Number of IFOs and data lists are not equal"
+    exit(-1)
+  
+  # split MCMC directories
+  mcmcdirs = (opts.mcmcdirs).split(',')
+  
+  # check if parfile is a single file or a directory
+  param_files = []
+  if os.path.isfile(opts.parfile):
+    param_files.append(opts.parfile)
+  else:
+    if os.path.isdir(opts.parfile):
+      # list the parameter files in the directory
+      pfiles = os.listdir(opts.parfile)
+      
+      for f in pfiles:
+        param_files.append(opts.parfile + '/' + f)
+    else:
+      print >> sys.stderr, "No par file or directory specified!"
+      sys.exit(1)
+  
+  # create CSS
+  try:
+    cssfile = os.path.join(outpath, 'psr.css')
+    css = open(cssfile, 'w')
+  except:
+    print >> sys.stderr, "Cannot open CSS file %s" % cssfile
+    sys.exit(1)
+  
+  # create header name style
+  cssh1 = \
+"""h1 {
+  margin: 0 0 16px 0;
+  padding: 0 0 16px 0;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: 0px;
+  border-bottom: 1px solid #999;
+  font-family: Tahoma, Sans-Serif;
+  text-shadow: 2px 2px 2px #ccc;
+}
+  
+"""
+  css.write(cssh1)
+  
+  # create par file style
+  csspar = \
+""".par {
+  float: left;
+  padding: 10px;
+  border: 1px solid #999;
+  box-shadow: 3px 3px 3px #888;
+  font-family: monospace;
+}
+  
+"""
+  css.write(csspar)
+  
+  css.close()
+  
+  # loop through par files to produce plots
+  for parfile in param_files:
+    # read data from par file
+    try:
+      par = pppu.psr_par(parfile)
+    except:
+      print >> sys.stderr, "Par file %s could not be opened!" % parfile
+      continue # move on to next pulsar
+   
+    pname = par['PSRJ']
+    if not pname:
+      print >> sys.stderr, "No PSRJ value in par file %s" % parfile
+      continue # move on to next pulsar
+    
+    # get time series and PSD plots
+    Bkdata = []
+    nofile = False
+    for i, ifo in enumerate(ifos):
+      Bkdata.append(Bkfiles[i] + '/finehet_' + pname + '_' + ifo)
+      
+      # check files exist if not then skip the pulsar
+      if not os.path.isfile(Bkdata[i]):
+        nofile = True
+        break
+    if nofile:
+      continue
+    
+    # check required parameters
+    f0 = par['F0']
+    if not f0:
+      print >> sys.stderr, "No F0 value in par file %s" % parfile
+      continue # move on to next pulsar
+      
+    f1 = par['F1']
+    if not f1:
+      print >> sys.stderr, "No F1 value in par file %s" % parfile
+      continue # move on to next pulsar
+    
+    # create output directory for pulsar
+    puldir = os.path.join(outpath, pname)
+    if not os.path.isdir(puldir):
+      os.mkdir(puldir)
+    
+    # attempt to get pulsar distance and proper motion corrected age from the
+    # ATNF catalogue
+    ages = None
+    dists = None
+    try:
+      atnfurl = \
+'http://www.atnf.csiro.au/people/pulsar/psrcat/proc_form.php?Dist=Dist&Age_i=\
+Age_i&startUserDefined=true&c1_val=&c2_val=&c3_val=&c4_val=&sort_attr=&\
+sort_order=asc&condition=&pulsar_names=' + re.sub('\+', '%2B', pname) + \
+'&ephemeris=selected&\
+submit_ephemeris=Get+Ephemeris&coords_unit=raj%2Fdecj&radius=&coords_1=\
+&coords_2=&style=Long+with+last+digit+error&no_value=*&fsize=3&x_axis=&x_scale=\
+linear&y_axis=&y_scale=linear&state=query'
+      soup = bs(urllib2.urlopen(atnfurl).read())
+      pdat = soup.pre # data exists in the pre html environment
+      
+      for line in pdat:
+        vals = line.split('\n') # split at any new lines
+        
+        for row in vals:
+          if 'DIST' in row:
+            dists = row.split() # split row at whitespace
+            
+          if 'AGE_I' in row:
+            ages = row.split()
+    except:
+      print >> sys.stderr, 'Problem accessing ATNF!'
+      sys.exit(1)
+      
+    if ages:
+      if len(ages) > 1:
+        try:
+          age = float(ages[1])
+        except:
+          print >> sys.stderr, "Age not a number!"
+          sys.exit(1)
+        f1sd = -f0/(2*age*365.25*86400)
+      else:
+        f1sd = f1
+    # check if f1 is positive - in which case set based on conservative age of
+    # tau = f0/(2*f1) = 10^9 years (for GC pulsars)
+    elif f1 > 0 and pname != 'J1824-2452A':
+      f1sd = -f0/(2*1e9*365.25*86400) # set f1 for spin-down limi calc
+    else:
+      f1sd = f1
+   
+    # first try getting a distance from a par file
+    dist = par['DIST']
+    # if not available look in download value
+    if not dist:
+      # see if it was in the ATNF catalogue
+      if dists:
+        if len(dists) > 1:
+          try:
+            dist = float(dists[1])
+          except:
+            print >> sys.stderr, "Distance not a number!"
+            sys.exit(1)
+        else:
+          print >> sys.stderr, "Distance not a number!"
+          sys.exit(1)
+    
+    # create html page
+    htmlppage = os.path.join(puldir, 'index.html')
+    try:
+      htmlpout = open(htmlppage, 'w')
+    except:
+      print >> sys.stderr, 'Could not open html page %s' % htmlppage
+     
+    htmlpout.write('<!DOCTYPE html>\n')
+    htmlpout.write('<html lang="en">\n\n') 
+    
+    htmlphead = \
+"""
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="description" content="PSR %s"/>
+  <meta charset="UTF-8"> 
+
+  <title>PSR %s</title>
+
+  <link rel="stylesheet" type="text/css" href="../%s"/>
+ 
+  </head>
+""" % (pname, pname, cssfile)
+    htmlpout.write(htmlphead)
+    
+    htmlpout.write('<body>\n')
+    
+    # print out pulsar name to file
+    htmlpout.write('<h1>PSR ' + pname + '</h1>\n')
+    
+    # copy par file to pulsar directory
+    shutil.copy(parfile, os.path.join(puldir, pname + '.par'))
+    
+    # print out par file info
+    htmlpout.write('<div class="par">\n')
+    htmlpout.write('<a href="'+pname+'.par">\n')
+    htmlpout.write('<table border="0">\n')
+    for param in ['RAJ', 'DECJ', 'F0', 'F1', 'F2', 'PEPOCH']:
+      pa = par[param]
+      if pa:
+        htmlpout.write('<tr>\n<td>'+param+'</td>\n')
+        htmlpout.write('<td>'+str(pa)+'</td>\n</tr>')
+    htmlpout.write('</table></a>\n')
+    htmlpout.write('</div>\n\n')
+    
+    # get spin-down upper limit
+    sdlim = pppu.spin_down_limit(f0, f1sd, dist)
+    
+    # get time series and PSD plots
+    Bkdata = []
+    plotpsds = False
+    for i, ifo in enumerate(ifos):
+      Bkdata.append(Bkfiles[i] + '/finehet_' + pname + '_' + ifo)
+    Bkfigs, psdfigs = pppu.plot_Bks_ASDs( Bkdata, ifos, \
+plotpsds=plotpsds, removeoutlier=8 )
+    
+    # output plots of time series and psd
+    for i, ifo in enumerate(ifos):
+      figname = 'Bk'+'_'+ifo+'.png'
+      Bkplotpath = os.path.join(puldir, figname)
+      Bkfigs[i].savefig(Bkplotpath)
+      
+      if plotpsds:
+        figname = 'ASD'+'_'+ifo+'.png'
+        ASDplotpath = os.path.join(puldir, figname)
+        psdfigs[i].savefig(ASDplotpath)
+    
+    # loop over detectors
+    ifosNew = ifos
+    if nifos > 1:
+      ifosNew.append('Joint')
+    
+    poslist = []
+    
+    for i, ifo in enumerate(ifosNew):
+      # read in MCMC chains
+      mcmc = []
+      neffs = [] # number of effective samples for each chain
+      for chaindir in mcmcdirs:
+        cfile = chaindir + '/' + 'MCMCchain_' + pname + '_' + ifo
+        
+        if os.path.isfile(cfile):
+          # load MCMC chain
+          mcmcChain = np.loadtxt(cfile, comments='%')
+          
+          # find number of effective samples for the chain
+          neffstmp = []
+          for j in range(1, mcmcChain.shape[1]):
+            neff, acl, acf = bppu.effectiveSampleSize(mcmcChain[:,j])
+            neffstmp.append(neff)
+          
+          # get the minimum effective sample size
+          neffs.append(min(neffstmp))
+          
+          nskip = math.ceil(mcmcChain.shape[0]/min(neffstmp))
+          
+          # output every nskip (independent) value
+          mcmc.append(mcmcChain[::nskip,:])
+        
+        else:
+          print >> sys.stderr, "File %s does not exist!" % cfile
+          sys.exit(1)
+        
+      # output data to common results format
+      # get first line of MCMC chain file for header names
+      cf = open(cfile, 'r')
+      headers = cf.readline()
+      headers = cf.readline() # column names are on the second line
+      # remove % from start
+      headers = re.sub('%', '', headers)
+      # remove rads
+      headers = re.sub('rads', '', headers)
+      # remove other brackets e.g. around (iota)
+      headers = re.sub('[()]', '', headers)
+      cf.close()
+          
+      # output full data to common format
+      comfile = os.path.join(puldir, 'common_tmp.dat')
+      try:
+        cf = open(comfile, 'w')
+      except:
+        print >> sys.stderr, "Can't open commom posterior file!"
+        sys.exit(1)
+          
+      cf.write(headers)
+      for narr in mcmc:
+        for j in range(0, narr.shape[0]):
+          mline = narr[j,:]
+          strmline = " ".join(str(x) for x in mline) + '\n'
+          cf.write(strmline)
+      cf.close()
+          
+      # read in as common object
+      peparser = bppu.PEOutputParser('common')
+      cf = open(comfile, 'r')
+      commonResultsObj = peparser.parse(cf)
+      cf.close()
+          
+      # remove temporary file
+      os.remove(comfile)
+          
+      # create posterior class
+      pos = bppu.Posterior( commonResultsObj, SimInspiralTableEntry=None, \
+                          votfile=None )
+      
+      poslist.append(pos)
+    
+    # get posterior plots
+    # h0
+    bounds = [0, float("inf")]
+    ul = 0.95
+    histbins = 30
+    h0Fig, ulvals = pppu.plot_posterior_hist( poslist, 'h0', ifosNew, \
+                                              bounds, histbins, \
+                                              ul, overplot=True )
+    figname = 'h0post.png'
+    h0plotpath = os.path.join(puldir, figname)
+    h0Fig[0].savefig(h0plotpath)
+    
+    # convert h0 uls into ellipticity and spin-down ratio
+    ell = []
+    sdrat = []
+    for h0ul in ulvals:
+      ell.append(pppu.h0_to_ellipticity(h0ul, f0, dist))
+      sdrat.append(h0ul/sdlim)
+    
+    # print limits to html file
+    
+    
+    # phi0
+    bounds = [0, 2*math.pi]
+    phi0Fig, ulvals = pppu.plot_posterior_hist( poslist, 'phi0', ifosNew, \
+                                        bounds, histbins, \
+                                        0, overplot=True )
+    figname = 'phi0post.png'
+    phi0plotpath = os.path.join(puldir, figname)
+    phi0Fig[0].savefig(phi0plotpath)
+    
+    # cos(iota)
+    bounds = [-1, 1]
+    ciFig, ulvals = pppu.plot_posterior_hist( poslist, 'cosiota', ifosNew, \
+                                      bounds, histbins, \
+                                      0, overplot=True )
+    figname = 'cipost.png'
+    ciplotpath = os.path.join(puldir, figname)
+    ciFig[0].savefig(ciplotpath)
+    
+    # psi
+    bounds = [-math.pi/4, math.pi/4]
+    psiFig, ulvals = pppu.plot_posterior_hist( poslist, 'psi', ifosNew, \
+                                       bounds, histbins, \
+                                       0, overplot=True )
+    figname = 'psipost.png'
+    psiplotpath = os.path.join(puldir, figname)
+    psiFig[0].savefig(psiplotpath)
+    
+    htmlpout.write('</body>\n\n')
+    
+    htmlpout.write('</html>\n')
+    htmlpout.close()
+ 
+  
--- lalapps/src/pulsar/TDS_isolated/pulsarpputils.py
+++ lalapps/src/pulsar/TDS_isolated/pulsarpputils.py
@@ -426,7 +426,7 @@ class psr_prior:
 #(Hz), spin-down (Hz/s) and distance (kpc). The canonical value of moment of
 # inertia of 1e38 kg m^2 is used
 def spin_down_limit(freq, fdot, dist):
-  hsd = math.sqrt((5./2.)*(G/C**3)*I38*math.abs(fdot)*freq)/(dist*KPC)
+  hsd = math.sqrt((5./2.)*(G/C**3)*I38*math.fabs(fdot)*freq)/(dist*KPC)
   
   return hsd
   
@@ -480,7 +480,7 @@ def phipsiconvert(phipchain, psipchain):
 # that will be output
 def plot_posterior_hist(poslist, param, ifos,
                         parambounds=[float("-inf"), float("inf")], 
-                        nbins=50, upperlimit=0):
+                        nbins=50, upperlimit=0, overplot=False):
   # create list of figures
   myfigs = []
   
@@ -496,11 +496,11 @@ def plot_posterior_hist(poslist, param, ifos,
   rc('font', size=12)
   
   # ifos line colour specs
-  coldict = {'H1': 'b', 'H2': 'r', 'L1': 'g', 'V1': 'c', 'G1': 'm'}
+  coldict = {'H1': 'b', 'H2': 'r', 'L1': 'g', 'V1': 'c', 'G1': 'm', 'Joint':'k'}
   
   # some parameter names for special LaTeX treatment in figures
   paramdict = {'H0': '$h_0$', 'COSIOTA': '$\cos{\iota}$', 'PSI':
-               '$\psi~{\rm(rads)}$', 'PHI0': '$\phi_0~{\rm(rads)}$', 'RA':
+               '$\psi~(rads)$', 'PHI0': '$\phi_0~(rads)$', 'RA':
                '$\alpha$', 'DEC': '$\delta$'}
  
   # param name for axis label
@@ -511,11 +511,20 @@ def plot_posterior_hist(poslist, param, ifos,
   
   # loop over ifos
   for idx, ifo in enumerate(ifos):
-    myfig = plt.figure(figsize=(4,3.5),dpi=200)
+    # check whether to plot all figures on top of each other
+    if overplot and idx == 0:
+      myfig = plt.figure(figsize=(4,3.5),dpi=200)
+      plt.hold(True)
+    elif not overplot:
+      myfig = plt.figure(figsize=(4,3.5),dpi=200)
     
     pos = poslist[idx]
     
-    pos_samps = pos[param].samples
+    # check for cosiota
+    if 'cosiota' in param:
+      pos_samps = np.cos(pos['iota'].samples)
+    else:
+      pos_samps = pos[param].samples
  
     # get a normalised histogram for each
     n, bins = hist_norm_bounds( pos_samps, int(nbins), parambounds[0], \
@@ -523,11 +532,16 @@ def plot_posterior_hist(poslist, param, ifos,
     
     # plot histogram
     plt.plot(bins, n, color=coldict[ifo])
+    
+    if 'h0' not in param:
+      plt.xlim(parambounds[0], parambounds[1])
+    
     plt.xlabel(r''+paraxis, fontsize=14, fontweight=100)
     plt.ylabel(r'Probability Density', fontsize=14, fontweight=100)
     myfig.subplots_adjust(left=0.18, bottom=0.15) # adjust size
-    
-    myfigs.append(myfig)
+   
+    if not overplot:
+      myfigs.append(myfig)
     
     # if upper limit is needed then integrate posterior using trapezium rule
     if upperlimit != 0:
@@ -541,6 +555,9 @@ def plot_posterior_hist(poslist, param, ifos,
       # use spline interpolation to find the value at 'upper limit'
       intf = interp1d(ct, bins, kind='cubic')
       ulvals.append(intf(float(upperlimit)))
+  
+  if overplot:
+    myfigs.append(myfig)
   
   return myfigs, ulvals
   
@@ -637,7 +654,7 @@ def tukey_window(N, alpha=0.5):
 # create a function for plotting the absolute value of Bk data (read in from
 # data files) and an averaged 1 day amplitude spectral density spectrogram for
 # each IFO
-def plot_Bks_ASDs( Bkdata, ifos ):
+def plot_Bks_ASDs( Bkdata, ifos, plotpsds=True, removeoutlier=None ):
   # create list of figures
   Bkfigs = []
   psdfigs = []
@@ -659,38 +676,39 @@ def plot_Bks_ASDs( Bkdata, ifos ):
   for i, ifo in enumerate(ifos):
     # get data for given ifo
     try:
-      dfile = open(Bkdata[i])
+      Bk = np.loadtxt(Bkdata[i])
     except:
       print "Could not open file ", Bkdata[i]
       exit(-1)
-      
+   
     # should be three lines in file
     gpstime = []
-    Bk = []
+    
+    # remove outliers at Xsigma by working out sigma from the peak of the
+    # distribution of the log absolute value
+    if removeoutlier:
+      n, binedges = np.histogram(np.log(np.fabs(np.concatenate((Bk[:,1], \
+Bk[:,2])))), 50)
+      j = n.argmax(0)
       
-    Bkabs = [] # absolute Bk value
-      
+      # standard devaition estimate
+      stdest = math.exp((binedges[j]+binedges[j+1])/2)
+
+      # get values within +/-8 sigma
+      # test real parts
+      vals = np.where(np.fabs(Bk[:,1]) < removeoutlier*stdest)
+      Bknew = Bk[vals,:][-1]
+      # test imag parts
+      vals = np.where(np.fabs(Bknew[:,2]) < removeoutlier*stdest)
+      Bk = Bknew[vals,:][-1]
+   
+    gpstime = Bk[:,0]
+    
     # minimum time step between points (should generally be 60 seconds)
-    mindt = float("inf")
-      
-    for line in dfile.readlines():
-      sl = line.split()
-        
-      gpstime.append(float(sl[0]))
-      Bk.append(complex(float(sl[1]), float(sl[2])))
-        
-      # get absolute value
-      Bkabs.append(math.sqrt(Bk[-1].real**2 + Bk[-1].imag**2))
-        
-      # check time step
-      if len(gpstime) > 1:
-        dt = gpstime[-1] - gpstime[-2]
+    mindt = min(np.diff(gpstime))
+    
+    Bkabs = np.sqrt(Bk[:,1]**2 + Bk[:,2]**2)
           
-        if dt < mindt:
-          mindt = dt
-      
-    dfile.close()
-      
     # plot the time series of the data
     Bkfig = plt.figure(figsize=(10,4), dpi=200)
     Bkfig.subplots_adjust(bottom=0.12, left=0.06, right=0.94)
@@ -704,73 +722,74 @@ def plot_Bks_ASDs( Bkdata, ifos ):
     plt.xlim(tms[0], tms[-1])
     
     Bkfigs.append(Bkfig)
-      
-    # create PSD by splitting data into days, padding with zeros to give a
-    # sample a second, getting the PSD for each day and combining them
-    totlen = gpstime[-1] - gpstime[0] # total data length
-      
-    # check mindt is an integer and greater than 1
-    if math.fmod(mindt, 1) != 0. or mindt < 1:
-      print "Error time steps between data points must be integers"
-      exit(-1)
-        
-    # loop over data, splitting it up
-    mr = int(math.ceil(totlen/86400))
     
-    count = 0
-    npsds = 0
-    totalpsd = np.zeros(86400) # add sum of PSDs
-    for i in range(0, mr):
-      datachunk = np.zeros(86400, dtype=complex)
+    if plotpsds:
+      # create PSD by splitting data into days, padding with zeros to give a
+      # sample a second, getting the PSD for each day and combining them
+      totlen = gpstime[-1] - gpstime[0] # total data length
+      
+      # check mindt is an integer and greater than 1
+      if math.fmod(mindt, 1) != 0. or mindt < 1:
+        print "Error time steps between data points must be integers"
+        exit(-1)
+        
+      # loop over data, splitting it up
+      mr = int(math.ceil(totlen/86400))
+    
+      count = 0
+      npsds = 0
+      totalpsd = np.zeros(86400) # add sum of PSDs
+      for i in range(0, mr):
+        datachunk = np.zeros(86400, dtype=complex)
        
-      gpsstart = int(gpstime[count])
+        gpsstart = int(gpstime[count])
       
-      prevcount = count
+        prevcount = count
       
-      for gt in gpstime[count:-1]:
-        if gt >= gpsstart+86400:
-          break
-        else:
-          datachunk[gt-gpsstart] = Bk[count]
-          count += 1
+        for gt in gpstime[count:-1]:
+          if gt >= gpsstart+86400:
+            break
+          else:
+            datachunk[gt-gpsstart] = complex(Bk[count,1], Bk[count,2])
+            count += 1
       
-      # only include the PSD if the chunk is more than 25% full of data
-      pf = float(count-prevcount)*mindt/86400.
+        # only include the PSD if the chunk is more than 25% full of data
+        pf = float(count-prevcount)*mindt/86400.
       
-      if pf > 0.25:
-        # get the PSD using a Tukey window with alpha = 0.25
-        win = tukey_window(86400, alpha=0.25)
+        if pf > 0.25:
+          # get the PSD using a Tukey window with alpha = 0.25
+          win = tukey_window(86400, alpha=0.25)
         
-        Fs = 1 # sample rate in Hz
+          Fs = 1 # sample rate in Hz
         
-        psd, freqs, t = specgram(datachunk, NFFT=86400, Fs=Fs, window=win)
+          psd, freqs, t = specgram(datachunk, NFFT=86400, Fs=Fs, window=win)
       
-        # add psd onto total value
-        totalpsd = map(lambda x, y: x+y, totalpsd, psd)
+          # add psd onto total value
+          totalpsd = map(lambda x, y: x+y, totalpsd, psd)
       
-        # count number of psds
-        npsds = npsds+1
+          # count number of psds
+          npsds = npsds+1
       
-    # average the PSD and convert to amplitude spectral density
-    totalpsd = map(lambda x: math.sqrt(x/npsds), totalpsd)
+      # average the PSD and convert to amplitude spectral density
+      totalpsd = map(lambda x: math.sqrt(x/npsds), totalpsd)
     
-    # plot PSD
-    psdfig = plt.figure(figsize=(4,3.5), dpi=200)
-    psdfig.subplots_adjust(left=0.18, bottom=0.15)
+      # plot PSD
+      psdfig = plt.figure(figsize=(4,3.5), dpi=200)
+      psdfig.subplots_adjust(left=0.18, bottom=0.15)
     
-    # get the indices to plot in the actual frequency range 
-    df = freqs[1]-freqs[0]
-    minfbin = int((math.fabs(freqs[0])-1./(2.*mindt))/df)
-    maxfbin = len(freqs) - minfbin
+      # get the indices to plot in the actual frequency range 
+      df = freqs[1]-freqs[0]
+      minfbin = int((math.fabs(freqs[0])-1./(2.*mindt))/df)
+      maxfbin = len(freqs) - minfbin
     
-    plt.plot(freqs[minfbin:maxfbin], totalpsd[minfbin:maxfbin],
-             color=coldict[ifo])
-    plt.xlim(freqs[minfbin], freqs[maxfbin])
-    plt.xlabel(r'Frequency (Hz)', fontsize=14, fontweight=100)
-    plt.ylabel(r'$h/\sqrt{\rm Hz}$', fontsize=14, fontweight=100)
-    plt.title(r'ASD for ' + ifo.upper(), fontsize=14)
+      plt.plot(freqs[minfbin:maxfbin], totalpsd[minfbin:maxfbin],
+               color=coldict[ifo])
+      plt.xlim(freqs[minfbin], freqs[maxfbin])
+      plt.xlabel(r'Frequency (Hz)', fontsize=14, fontweight=100)
+      plt.ylabel(r'$h/\sqrt{\rm Hz}$', fontsize=14, fontweight=100)
+      plt.title(r'ASD for ' + ifo.upper(), fontsize=14)
     
-    psdfigs.append(psdfig)
+      psdfigs.append(psdfig)
       
   return Bkfigs, psdfigs
 
