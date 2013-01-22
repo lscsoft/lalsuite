3c5a7e373976e3c179e5c85626a71b4a23f401d8
Jolien Creighton <jolien.creighton@ligo.org>
2013-01-21 18:01:57 -0600

    support spaces in path Original: 0c3e1d2ed4fddc682f16a6e350b33fee70579e7c

--- lalframe/test/mkcatalog.sh
+++ lalframe/test/mkcatalog.sh
@@ -6,26 +6,28 @@ fail() {
 }
 
 srcdir=${2:-.}
-dirname=`cd $srcdir && pwd`
-test -d ${dirname:-/dev/null/nowhere} || fail
-destdir=`test -d ${3:-$srcdir} && cd ${3:-$srcdir} && pwd || echo "$3"`
-outfile=${1:-"-"}
-if test $outfile = "-"; then
+dirname=`cd "${srcdir}" && pwd`
+test -d "${dirname:-/dev/null/nowhere}" || fail
+destdir=`test -d "${3:-"${srcdir}"}" && cd "${3:-"${srcdir}"}" && pwd || echo "${3}"`
+outfile="${1:-"-"}"
+if test "${outfile}" = "-"; then
   out=''
 else
-  test -f $outfile && rm -f $outfile
-  out=' >> $outfile'
+  test -f "${outfile}" && rm -f "${outfile}"
+  out=' >> "${outfile}"'
 fi
-frfiles=`ls $dirname/*.gwf`
-test -n "$frfiles" || fail
+frfiles=`ls "${dirname}"/*.gwf`
+test -n "${frfiles}" || fail
 
-for file in $frfiles; do
-  basename=`basename $file .gwf`
+IFS='
+'
+for file in ${frfiles}; do
+  basename=`basename "${file}" .gwf`
   IFS_save="$IFS"
   IFS=-
   command='printf "%s %s %s %s "'
   eval $command $basename $out || fail
   IFS="$IFS_save"
-  command='echo "file://localhost$destdir/$basename.gwf"'
+  command='echo "file://localhost${destdir}/${basename}.gwf"'
   eval $command $out || fail
 done
