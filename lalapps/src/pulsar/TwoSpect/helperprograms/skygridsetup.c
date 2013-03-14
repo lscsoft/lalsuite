163b0c6b3f397a1f21c3139b11e8c2ed9496be0e
Evan Goetz <evan.goetz@ligo.org>
2013-03-14 11:43:38 +0100

    Updated copyright date Original: 4d9563f2c0f4db8abf3430a59232cbc73c640172

--- lalapps/src/pulsar/TwoSpect/helperprograms/skygridsetup.c
+++ lalapps/src/pulsar/TwoSpect/helperprograms/skygridsetup.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011 Evan Goetz
+ *  Copyright (C) 2011, 2013 Evan Goetz
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
