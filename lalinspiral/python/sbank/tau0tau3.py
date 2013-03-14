744f5708778948ef402a0b8aac394297505cb6da
Stephen Privitera <stephen.privitera@ligo.org>
2013-03-14 13:00:35 -0700

    sbank: remove redundant code from bad copy/paste Original: c6467f0deacae9fab3308684ceb985fcb7447ac5

--- lalinspiral/python/sbank/tau0tau3.py
+++ lalinspiral/python/sbank/tau0tau3.py
@@ -123,27 +123,6 @@ def set_default_constraints(constraints):
     constraints['mtotal'] = (mtotal_min, mtotal_max)
 
     # mratio can be given or inferred from component mass limits
-    if mass1_min is None or mass1_max is None:
-        raise ValueError("Must specify both minimum and maximum mass1.")
-
-    # component mass2 can be specified independently
-    # if not specified, symmetry will be assumed
-    mass2_min, mass2_max = constraints.setdefault('mass2', (None, None))
-    if mass2_min is None:
-        mass2_min = mass1_min
-    if mass2_max is None:
-        mass2_max = mass1_max
-    constraints['mass2'] = (mass2_min, mass2_max)
-
-    # mtotal can be given or inferred from component mass limits
-    mtotal_min, mtotal_max = constraints.setdefault('mtotal', (None, None))
-    if mtotal_min is None:
-        mtotal_min = mass1_min + mass2_min
-    if mtotal_max is None:
-        mtotal_max = mass1_max + mass2_max
-    constraints['mtotal'] = (mtotal_min, mtotal_max)
-
-    # mratio can be given or inferred from component mass limits
     qmin, qmax = constraints.setdefault('mratio', (None, None))
     if qmin is None:
         qmin = max(mass1_min, mtotal_min - mass2_max) / mass2_max
