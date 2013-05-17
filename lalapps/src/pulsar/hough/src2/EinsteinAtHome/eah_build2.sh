a97f77024c507def8201132aa7c3e1330ba77e80
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2013-05-17 13:44:40 +0200

    E@H: fix testHS call Original: 1a2e22b66a5d70880070d60a032f811b1f5264c9

--- lalapps/src/pulsar/hough/src2/EinsteinAtHome/eah_build2.sh
+++ lalapps/src/pulsar/hough/src2/EinsteinAtHome/eah_build2.sh
@@ -668,8 +668,7 @@ if [ .$check = .true ]; then
     log_and_do cp ../eah_Makefakedata_v4$ext lalapps_Makefakedata_v4
     log_and_do cp ../eah_PredictFStat$ext lalapps_PredictFStat
     log_and_do cp ../eah_ComputeFStatistic_v2$ext lalapps_ComputeFStatistic_v2
-    log_and_do cp "$INSTALL"/share/lalpulsar/*05-09.dat .
-    NOCLEANUP=1 PATH="$PWD:$PATH" \
+    LALPULSAR_DATADIR="$INSTALL/share/lalpulsar" NOCLEANUP=1 PATH="$PWD:$PATH" \
 	log_and_do ../source/lalsuite/lalapps/src/pulsar/GCT/testHS.sh $wine "$check_app" --Dterms=8
     log_and_show "==========================================="
     log_and_show "Test passed"
