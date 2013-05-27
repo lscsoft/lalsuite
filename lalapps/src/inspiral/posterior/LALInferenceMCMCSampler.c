f075873498214a35d1478cf50c98ac831f4cefd9
Tyson Littenberg <tyson.littenberg@ligo.org>
2013-05-27 17:34:36 -0500

    lalinference:  fixing the last fix... Original: 35c13a8317ba9724f478ed3572d303529a0f89a4

--- lalapps/src/inspiral/posterior/LALInferenceMCMCSampler.c
+++ lalapps/src/inspiral/posterior/LALInferenceMCMCSampler.c
@@ -1014,8 +1014,7 @@ UINT4 LALInferencePTswap(LALInferenceRunState *runState, REAL8 *ladder, INT4 i, 
       }
     }
   }
-  LALInferenceClearVariables(adjCurrentParams);
-  XLALFree(adjCurrentParams);
+  XLALDestroyREAL8Vector(adjParameters);
 
   /* Return values for colder chain: 0=nothing happened; 1=swap proposed, not accepted; 2=swap proposed & accepted */
   if (swapProposed) {
