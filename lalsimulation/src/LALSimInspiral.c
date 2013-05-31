e73855121b8aa075c6a8e11543b96c4045fc5cd4
Evan Ochsner <evan.ochsner@ligo.org>
2013-05-31 14:00:05 -0500

    Add SpinTaylorF2 as ImplementedFDApproximant Original: 8143c5ef17ff80c2c2b22e75f76d37b9951dd0a3

--- lalsimulation/src/LALSimInspiral.c
+++ lalsimulation/src/LALSimInspiral.c
@@ -2602,7 +2602,7 @@ int XLALSimInspiralImplementedFDApproximants(
         case IMRPhenomC:
         //case TaylorR2F4:
         case TaylorF2:
-        //case SpinTaylorF2:
+        case SpinTaylorF2:
         case TaylorF2RedSpin:
         case TaylorF2RedSpinTidal:
             return 1;
