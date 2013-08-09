f2a0d9f69c727f3322d562e437510b0cb8b61008
Duncan Macleod <duncan.macleod@ligo.org>
2013-08-09 12:33:28 -0700

    lal.utils: added cache module     - lal.utils.cache provides one function to convert GLUE Caches into       LAL Caches Original: 5b18c3471fa250f864b12099ffd88e8ac20ad6ae

--- lal/python/utils/cache.py
+++ lal/python/utils/cache.py
@@ -14,14 +14,36 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""This module provides utilities for generating LAL objects in python
+"""Modules extending the Cache file functionality from LAL
 """
 
+import tempfile
+import os
 
 from .. import git_version
+from ..lal import CacheImport
+
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __version__ = git_version.id
 __date__ = git_version.date
 
-from .series import *
-from .cache import *
+__all__ = ['lalcache_from_gluecache']
+
+def lalcache_from_gluecache(cache):
+    """Convert a glue.lal.Cache object to a lal.Cache object.
+    Writes cache to temporary file and reads to Cache.
+
+    @param cache
+        LAL cache object from GLUE to convert
+    @type cache glue.lal.Cache
+
+    @returns a lal.Cache object representing the same data
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as t:
+        cache = cache
+        for e in cache:
+            e.segment = type(e.segment)(int(e.segment[0]), int(e.segment[1]))
+        cache.tofile(t)
+        frcache = CacheImport(t.name)
+    os.remove(t.name)
+    return frcache
