fc6d28e8e943bc4b63b58fb69a7696461b2ed6dd
Karl Wette <karl.wette@ligo.org>
2013-08-21 15:21:02 +0200

    ComputeFstat_Demod: implement XLALSetupFstat_Demod() and ComputeFstat_Demod() - XLALSetupFstat_Demod() sets up demod data and buffers - ComputeFstat_Demod() calls the LAL ComputeFstat() function in a loop - also implement FstatInputData_Demod, and its destructor function - refs #1000 Original: f5b2d5fe96fe5f59f4cb8f7d84c1b3e54240e153

--- lalpulsar/src/ComputeFstat_Demod.c
+++ lalpulsar/src/ComputeFstat_Demod.c
@@ -21,14 +21,21 @@
 // included from ComputeFstat.c
 
 struct tagFstatInputData_Demod {
+  MultiSFTVector *multiSFTs;			// Input multi-detector SFTs
+  MultiDetectorStateSeries *multiDetStates;	// Multi-detector state series
+  ComputeFParams params;			// Additional parameters for ComputeFStat()
+  ComputeFBuffer buffer;			// Internal buffer for ComputeFStat()
 };
 
 static inline void
 DestroyFstatInputData_Demod(
-  UNUSED FstatInputData_Demod* demod
+  FstatInputData_Demod* demod
   )
 {
-  XLAL_ERROR_VOID( XLAL_EFAILED, "Unimplemented!" );
+  XLALDestroyMultiSFTVector(demod->multiSFTs);
+  XLALDestroyMultiDetectorStateSeries(demod->multiDetStates);
+  XLALEmptyComputeFBuffer(&demod->buffer);
+  XLALFree(demod);
 }
 
 FstatInputData*
@@ -47,18 +54,128 @@ XLALSetupFstat_Demod(
   XLAL_CHECK_NULL(Dterms > 0, XLAL_EINVAL);
 
   // Check common input and allocate input data struct
-  UNUSED FstatInputData* input = SetupFstat_Common(multiSFTs, multiWeights, edat, SSBprec);
+  FstatInputData* input = SetupFstat_Common(multiSFTs, multiWeights, edat, SSBprec);
+  XLAL_CHECK_NULL(input != NULL, XLAL_EFUNC);
 
-  XLAL_ERROR_NULL( XLAL_EFAILED, "Unimplemented!" );
+  // Allocate demodulation input data struct
+  FstatInputData_Demod *demod = XLALCalloc(1, sizeof(FstatInputData_Demod));
+  XLAL_CHECK_NULL(demod != NULL, XLAL_ENOMEM);
+
+  // Save pointer to input SFTs, set supplied pointer to NULL
+  demod->multiSFTs = *multiSFTs;
+  *multiSFTs = NULL;
+
+  // Calculate the detector states from the SFTs
+  {
+    LALStatus status = empty_status;
+    LALGetMultiDetectorStates(&status, &demod->multiDetStates, demod->multiSFTs, edat);
+    if (status.statusCode) {
+      XLAL_ERROR_NULL(XLAL_EFAILED, "LALGetMultiDetectorStates() failed: %s (statusCode=%i)", status.statusDescription, status.statusCode);
+    }
+  }
+
+  // Set parameters to pass to ComputeFStat()
+  demod->params.Dterms = Dterms;
+  demod->params.SSBprec = SSBprec;
+  demod->params.buffer = NULL;
+  demod->params.bufferedRAA = (demodAM & DEMODAM_BUFFERED_RIGID_ADIABATIC);
+  demod->params.edat = edat;
+  demod->params.upsampling = 1;
+  demod->params.useRAA = (demodAM & DEMODAM_RIGID_ADIABATIC) || (demodAM & DEMODAM_BUFFERED_RIGID_ADIABATIC);
+
+  // Save pointer to demodulation input data
+  input->demod = demod;
+
+  return input;
 
 }
 
 static int
 ComputeFstat_Demod(
-  UNUSED FstatResults* Fstats,
-  UNUSED const FstatInputData_Common *common,
-  UNUSED FstatInputData_Demod* demod
+  FstatResults* Fstats,
+  const FstatInputData_Common *common,
+  FstatInputData_Demod* demod
   )
 {
-  XLAL_ERROR( XLAL_EFAILED, "Unimplemented!" );
+
+  // Check input
+  XLAL_CHECK(Fstats != NULL, XLAL_EFAULT);
+  XLAL_CHECK(common != NULL, XLAL_EFAULT);
+  XLAL_CHECK(demod != NULL, XLAL_EFAULT);
+
+  // Get which F-statistic quantities to compute
+  const FstatQuantities whatToCompute = Fstats->whatWasComputed;
+
+  // Check which quantities can be computed
+  XLAL_CHECK(!(whatToCompute & FSTATQ_FAFB_PER_DET), XLAL_EINVAL, "Demodulation does not currently support Fa & Fb per detector");
+
+  // Set parameters to pass to ComputeFStat()
+  demod->params.returnSingleF = (whatToCompute & FSTATQ_2F_PER_DET);
+  demod->params.returnAtoms = (whatToCompute & FSTATQ_ATOMS_PER_DET);
+
+  // Save local copy of doppler point and starting frequency
+  PulsarDopplerParams thisPoint = Fstats->doppler;
+  const REAL8 fStart = thisPoint.fkdot[0];
+
+  // Call ComputeFStat() for each frequency bin
+  for (UINT4 k = 0; k < Fstats->numFreqBins; ++k) {
+
+    // Set frequency to search at
+    thisPoint.fkdot[0] = fStart + k * Fstats->dFreq;
+
+    // Call ComputeFStat()
+    Fcomponents Fcomp;
+    {
+      LALStatus status = empty_status;
+      ComputeFStat(&status, &Fcomp, &thisPoint, demod->multiSFTs, common->multiWeights, demod->multiDetStates, &demod->params, &demod->buffer);
+      if (status.statusCode) {
+        XLAL_ERROR(XLAL_EFAILED, "ComputeFStat() failed: %s (statusCode=%i)", status.statusDescription, status.statusCode);
+      }
+    }
+
+    // Return multi-detector 2F
+    if (whatToCompute & FSTATQ_2F) {
+      Fstats->twoF[k] = 2.0 * Fcomp.F;   // *** Return value of 2F ***
+    }
+
+    // Return multi-detector Fa & Fb
+    if (whatToCompute & FSTATQ_FAFB) {
+      Fstats->FaFb[k].Fa = Fcomp.Fa;
+      Fstats->FaFb[k].Fb = Fcomp.Fb;
+    }
+
+    // Return 2F per detector
+    if (whatToCompute & FSTATQ_2F_PER_DET) {
+      for (UINT4 X = 0; X < Fstats->numDetectors; ++X) {
+        Fstats->twoFPerDet[X][k] = 2.0 * Fcomp.FX[X];   // *** Return value of 2F ***
+      }
+    }
+
+    // Return Fa & Fb per detector
+    if (whatToCompute & FSTATQ_FAFB_PER_DET) {
+      XLAL_ERROR(XLAL_EFAILED, "Unimplemented!");
+    }
+
+    // Return F-atoms per detector
+    if (whatToCompute & FSTATQ_ATOMS_PER_DET) {
+      XLALDestroyMultiFstatAtomVector(Fstats->multiFatoms[k]);
+      Fstats->multiFatoms[k] = Fcomp.multiFstatAtoms;
+    }
+
+  } // for k < Fstats->numFreqBins
+
+  // Return amplitude modulation coefficients
+  if (demod->buffer.multiCmplxAMcoef != NULL) {
+    Fstats->Mmunu = demod->buffer.multiCmplxAMcoef->Mmunu;
+  } else if (demod->buffer.multiAMcoef != NULL) {
+    Fstats->Mmunu.Ad = demod->buffer.multiAMcoef->Mmunu.Ad;
+    Fstats->Mmunu.Bd = demod->buffer.multiAMcoef->Mmunu.Bd;
+    Fstats->Mmunu.Cd = demod->buffer.multiAMcoef->Mmunu.Cd;
+    Fstats->Mmunu.Ed = 0;
+    Fstats->Mmunu.Dd = demod->buffer.multiAMcoef->Mmunu.Dd;
+    Fstats->Mmunu.Sinv_Tsft = demod->buffer.multiAMcoef->Mmunu.Sinv_Tsft;
+  }
+
+  return XLAL_SUCCESS;
+
 }
