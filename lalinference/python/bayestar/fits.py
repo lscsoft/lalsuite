fbb9e6b213bb232ecba40759dac1aa74f8e2c016
Leo Singer <leo.singer@ligo.org>
2013-08-21 15:17:33 -0700

    Fix comment Original: 765012d9f46f25a630cf92ebdae236a07724c4a8

--- lalinference/python/bayestar/fits.py
+++ lalinference/python/bayestar/fits.py
@@ -212,7 +212,7 @@ def iso8601_to_gps(iso8601):
 
 
 def gps_to_mjd(gps_time):
-    """Convert a floating-point GPS time in seconds to an ISO 8601 date string."""
+    """Convert a floating-point GPS time in seconds to a modified Julian day."""
     gps_seconds_fraction, gps_seconds = math.modf(gps_time)
     jd = lal.JulianDay(lal.GPSToUTC(int(gps_seconds)))
     return jd - lal.XLAL_MJD_REF + gps_seconds_fraction / 86400.
