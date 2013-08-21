3d394c447f07b9341334d8fcc7a2958945e3ca36
Karl Wette <karl.wette@ligo.org>
2013-08-21 15:21:53 +0200

    ComputeFstat_Resamp: implement XLALSetupFstat_Resamp() and ComputeFstat_Resamp() - XLALSetupFstat_Demod() sets up demod data and buffers - ComputeFstat_Demod() calls the LAL ComputeFstatFreqBand_RS() function - also implement FstatInputData_Demod, and its destructor function - refs #1000 Original: 176a797b64c49fd1b03ddd6339fe145f949dec5b

--- lalpulsar/src/ComputeFstat_Resamp.c
+++ lalpulsar/src/ComputeFstat_Resamp.c
@@ -21,14 +21,21 @@
 // included from ComputeFstat.c
 
 struct tagFstatInputData_Resamp {
+  MultiSFTVector *multiSFTs;			// Input multi-detector SFTs
+  ComputeFParams params;			// Additional parameters for ComputeFStatFreqBand_RS()
+  REAL4 *Fout;					// Output array of *F* values passed to ComputeFStatFreqBand_RS()
 };
 
 static inline void
 DestroyFstatInputData_Resamp(
-  UNUSED FstatInputData_Resamp* resamp
+  FstatInputData_Resamp* resamp
   )
 {
-  XLAL_ERROR_VOID( XLAL_EFAILED, "Unimplemented!" );
+  XLALDestroyMultiSFTVector(resamp->multiSFTs);
+  XLALEmptyComputeFBuffer_RS(resamp->params.buffer);
+  XLALFree(resamp->params.buffer);
+  XLALFree(resamp->Fout);
+  XLALFree(resamp);
 }
 
 FstatInputData*
@@ -41,18 +48,126 @@ XLALSetupFstat_Resamp(
 {
 
   // Check common input and allocate input data struct
-  UNUSED FstatInputData* input = SetupFstat_Common(multiSFTs, multiWeights, edat, SSBprec);
+  FstatInputData* input = SetupFstat_Common(multiSFTs, multiWeights, edat, SSBprec);
+  XLAL_CHECK_NULL(input != NULL, XLAL_EFUNC);
 
-  XLAL_ERROR_NULL( XLAL_EFAILED, "Unimplemented!" );
+  // Allocate resampling input data struct
+  FstatInputData_Resamp *resamp = XLALCalloc(1, sizeof(FstatInputData_Resamp));
+  XLAL_CHECK_NULL(resamp != NULL, XLAL_ENOMEM);
+
+  // Save pointer to input SFTs, set supplied pointer to NULL
+  resamp->multiSFTs = *multiSFTs;
+  multiSFTs = NULL;
+
+  // Set parameters to pass to ComputeFStatFreqBand_RS()
+  resamp->params.Dterms = 0;
+  resamp->params.SSBprec = SSBprec;
+  resamp->params.buffer = NULL;
+  resamp->params.bufferedRAA = 0;
+  resamp->params.edat = edat;
+  resamp->params.upsampling = 1;
+  resamp->params.useRAA = 0;
+
+  // Initialise output array of *F* values to NULL
+  resamp->Fout = NULL;
+
+  // Save pointer to resampling input data
+  input->resamp = resamp;
+
+  return input;
 
 }
 
 static int
 ComputeFstat_Resamp(
-  UNUSED FstatResults* Fstats,
-  UNUSED const FstatInputData_Common *common,
-  UNUSED FstatInputData_Resamp* resamp
+  FstatResults* Fstats,
+  const FstatInputData_Common *common,
+  FstatInputData_Resamp* resamp
   )
 {
-  XLAL_ERROR( XLAL_EFAILED, "Unimplemented!" );
+
+  // Check input
+  XLAL_CHECK(Fstats != NULL, XLAL_EFAULT);
+  XLAL_CHECK(common != NULL, XLAL_EFAULT);
+  XLAL_CHECK(resamp != NULL, XLAL_EFAULT);
+
+  // Get which F-statistic quantities to compute
+  const FstatQuantities whatToCompute = Fstats->whatWasComputed;
+
+  // Check which quantities can be computed
+  XLAL_CHECK(!(whatToCompute & FSTATQ_FAFB), XLAL_EINVAL, "Resamping does not currently support Fa & Fb");
+  XLAL_CHECK(!(whatToCompute & FSTATQ_FAFB_PER_DET), XLAL_EINVAL, "Resamping does not currently support Fa & Fb per detector");
+  XLAL_CHECK(!(whatToCompute & FSTATQ_ATOMS_PER_DET), XLAL_EINVAL, "Resamping does not currently support atoms per detector");
+
+  // Set parameters to pass to ComputeFStatFreqBand_RS()
+  resamp->params.returnSingleF = whatToCompute & FSTATQ_2F_PER_DET;
+  resamp->params.returnAtoms = 0;
+
+  // Save local copy of doppler point
+  PulsarDopplerParams thisPoint = Fstats->doppler;
+
+  // (Re)allocate output array of *F* values
+  const UINT4 FoutN = resamp->params.returnSingleF ? (Fstats->numDetectors + 1) : 1;
+  resamp->Fout = XLALRealloc(resamp->Fout, Fstats->numFreqBins * FoutN * sizeof(resamp->Fout[0]));
+  XLAL_CHECK(resamp->Fout != NULL, XLAL_ENOMEM);
+
+  // Create REAL4FrequencySeries to receive 2F values
+  static const REAL4Sequence empty_REAL4Sequence;
+  REAL4Sequence CFSFB_RS_data = empty_REAL4Sequence;
+  CFSFB_RS_data.length = Fstats->numFreqBins * FoutN;
+  CFSFB_RS_data.data = resamp->Fout;
+  static const REAL4FrequencySeries empty_REAL4FrequencySeries;
+  REAL4FrequencySeries CFSFB_RS = empty_REAL4FrequencySeries;
+  CFSFB_RS.deltaF = Fstats->dFreq;
+  CFSFB_RS.data = &CFSFB_RS_data;
+
+  // Call ComputeFStatFreqBand_RS()
+  {
+    LALStatus status = empty_status;
+    ComputeFStatFreqBand_RS(&status, &CFSFB_RS, &thisPoint, resamp->multiSFTs, common->multiWeights, &resamp->params);
+    if (status.statusCode) {
+      XLAL_ERROR(XLAL_EFAILED, "ComputeFStatFreqBand_RS() failed: %s (statusCode=%i)", status.statusDescription, status.statusCode);
+    }
+  }
+  for (UINT4 k = 0; k < Fstats->numFreqBins; ++k) {
+
+    // Return multi-detector 2F
+    if (whatToCompute & FSTATQ_2F) {
+      Fstats->twoF[k] = 2.0 * resamp->Fout[k];   // *** Return value of 2F ***
+    }
+
+    // Return multi-detector Fa & Fb
+    if (whatToCompute & FSTATQ_FAFB) {
+      XLAL_ERROR(XLAL_EFAILED, "Unimplemented!");
+    }
+
+    // Return 2F per detector
+    if (whatToCompute & FSTATQ_2F_PER_DET) {
+      for (UINT4 X = 0; X < Fstats->numDetectors; ++X) {
+        Fstats->twoFPerDet[X][k] = 2.0 * resamp->Fout[(X+1)*Fstats->numFreqBins + k];   // *** Return value of 2F ***
+      }
+    }
+
+    // Return Fa & Fb per detector
+    if (whatToCompute & FSTATQ_FAFB_PER_DET) {
+      XLAL_ERROR(XLAL_EFAILED, "Unimplemented!");
+    }
+
+    // Return F-atoms per detector
+    if (whatToCompute & FSTATQ_ATOMS_PER_DET) {
+      XLAL_ERROR(XLAL_EFAILED, "Unimplemented!");
+    }
+
+  } // for k < Fstats->numFreqBins
+
+  // Resampling cannot currently return amplitude modulation coefficients
+  Fstats->Mmunu.Ad = NAN;
+  Fstats->Mmunu.Bd = NAN;
+  Fstats->Mmunu.Cd = NAN;
+  Fstats->Mmunu.Ed = NAN;
+  Fstats->Mmunu.Dd = NAN;
+  Fstats->Mmunu.Sinv_Tsft = NAN;
+
+  return XLAL_SUCCESS;
+
 }
