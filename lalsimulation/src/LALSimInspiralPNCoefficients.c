34b2f4b96dd8f54d678896922be79083c0ac52d4
Andrew Lundgren <andrew.lundgren@LIGO.org>
2013-11-16 15:09:19 +0100

    Fixed the 4PN energy log term. Original: 1f0ee7ac91c881aa54801141573dd82f7d31fac1

--- lalsimulation/src/LALSimInspiralPNCoefficients.c
+++ lalsimulation/src/LALSimInspiralPNCoefficients.c
@@ -71,14 +71,21 @@ XLALSimInspiralPNEnergy_8PNCoeff(
         REAL8 eta)
 {
         return (-39.69/1.28 + (-123.671/5.76 + 9.037/1.536 *LAL_PI*LAL_PI+ 1792./15.*log(2)+89.6/1.5*LAL_GAMMA)* eta + (-498.449/3.456 +31.57/5.76*LAL_PI*LAL_PI)*eta*eta + 3.01/17.28 *eta*eta*eta + 7.7/3110.4*eta*eta*eta*eta);
-        /*see arXiv:1305.4884, or eq.(26) of arXiv:1309.3474*/
+        /*see arXiv:1305.4884, or eq.(26) of arXiv:1309.3474
+          note that in getting a4 from PRD 62, 084011 (2000),
+          the first reference is using the fact that \omega_{static} = 0
+          (see arXiv:gr-qc/0105038) */
 }
 
 static REAL8 UNUSED
 XLALSimInspiralPNEnergy_8PNLogCoeff(
         REAL8 eta)
 {
-	return 448./15.*eta;
+	return 896./15.*eta;
+        /* arXiv:1305.4884 has a misprint, it should have a factor of nu
+           See for instance arXiv:1002.0726
+           Also note that this term is usually given as 448*log(x)/15
+           since x=v^2 the log(v) term is twice this */
 }
 
 static REAL8 UNUSED
