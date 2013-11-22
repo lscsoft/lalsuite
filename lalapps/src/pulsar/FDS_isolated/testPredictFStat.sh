2159370fa1d0dd9d9d746e929a7090a85647df1d
Reinhard Prix <reinhard.prix@ligo.org>
2013-11-22 20:38:52 +0100

    cosmetic cleanups in testPredictFStat.sh test-script Original: 51864b355e1c6c08ec9775cc123a5219cc638b3a

--- lalapps/src/pulsar/FDS_isolated/testPredictFStat.sh
+++ lalapps/src/pulsar/FDS_isolated/testPredictFStat.sh
@@ -10,16 +10,20 @@ builddir="./";
 injectdir="../Injections/"
 
 ##---------- names of codes and input/output files
-mfd_code="${injectdir}lalapps_Makefakedata_v4"
-saf_code="${builddir}lalapps_SemiAnalyticF"
-pfs_code="${builddir}lalapps_PredictFStat"
+mfd_code="lalapps_Makefakedata_v4"
+saf_code="lalapps_SemiAnalyticF"
+pfs_code="lalapps_PredictFStat"
+
+mfd_path="${injectdir}${mfd_code}"
+saf_path="${builddir}${saf_code}"
+pfs_path="${builddir}${pfs_code}"
 
 SFTdir="./testPredictFStat_sfts"
 
 if [ -n "${LALPULSAR_DATADIR}" ]; then
-    mfd_code="${mfd_code} -E ${LALPULSAR_DATADIR}"
-    saf_code="${saf_code} -E ${LALPULSAR_DATADIR}"
-    pfs_code="${pfs_code} -E ${LALPULSAR_DATADIR}"
+    mfd_path="${mfd_path} -E ${LALPULSAR_DATADIR}"
+    saf_path="${saf_path} -E ${LALPULSAR_DATADIR}"
+    pfs_path="${pfs_path} -E ${LALPULSAR_DATADIR}"
 else
     echo
     echo "Need environment-variable LALPULSAR_DATADIR to be set to"
@@ -29,7 +33,6 @@ else
     exit 1
 fi
 
-Ftolerance=0.05
 # ---------- fixed parameter of our test-signal
 Tsft=1800;
 startTime=711595934
@@ -51,22 +54,20 @@ mfd_fmin=$(echo $Freq $mfd_FreqBand | awk '{printf "%g", $1 - $2 / 2.0}');
 
 noiseSqrtSh=1
 
-IFO=LHO
-
-
-##--------------------------------------------------
-## test starts here
-##--------------------------------------------------
+IFO=H1
 
 echo
-echo "----------------------------------------------------------------------"
-echo " STEP 1: Generate Fake Signal"
-echo "----------------------------------------------------------------------"
-echo
+echo "----- Test parameters: ----------"
+echo "startTime=${startTime}; duration=${duration}; Tsft=${Tsft}"
+echo "h0=${h0}; cosi=${cosi}; psi=${psi}; phi0=${phi0}"
+echo "Alpha=${Alpha}; Delta=${Delta}; Freq=${Freq}"
+echo "sqrtSn=${noiseSqrtSh}; IFO=${IFO}; mfd_FreqBand=${mfd_FreqBand}"
+echo "--------------------------------"
+
 if [ ! -d "$SFTdir" ]; then
-    mkdir $SFTdir;
+    mkdir $SFTdir
 else
-    rm -f $SFTdir/*;
+    rm -f ${SFTdir}/*
 fi
 
 # this part of the command-line is compatible with SemiAnalyticF:
@@ -74,92 +75,101 @@ saf_CL=" --Alpha=$Alpha --Delta=$Delta --IFO=$IFO --Tsft=$Tsft --startTime=$star
 # concatenate this with the mfd-specific switches:
 mfd_CL="${saf_CL} --fmin=$mfd_fmin --Band=$mfd_FreqBand --Freq=$Freq --outSFTbname=$SFTdir --noiseSqrtSh=$noiseSqrtSh --randSeed=1"
 
-cmdline="$mfd_code $mfd_CL";
-echo $cmdline;
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$mfd_code' ..."
+## ---------- Run MFDv4 ----------
+cmdline="$mfd_path $mfd_CL";
+if [ "$DEBUG" ]; then echo $cmdline; fi
+echo -n "Running ${mfd_code} ... "
+if ! eval "$cmdline 2> /dev/null"; then
+    echo "FAILED:"
+    echo $cmdline
     exit 1
+else
+    echo "OK."
 fi
 
-echo
-cmdline="$saf_code $saf_CL  --sqrtSh=$noiseSqrtSh"
-echo $cmdline
+## ---------- Run SemiAnalyticF ----------
+cmdline="$saf_path $saf_CL  --sqrtSh=$noiseSqrtSh"
+if [ "$DEBUG" ]; then echo $cmdline; fi
+echo -n "Running ${saf_code} ... "
 if ! tmp=`eval $cmdline 2> /dev/null`; then
-    echo "Error ... something failed running '$saf_code' ..."
+    echo "FAILED:"
+    echo $cmdline
     exit 1;
+else
+    echo "OK."
 fi
-echo  "ok."
 resSAF=`echo $tmp | awk '{printf "%g", 2.0 * $1}'`
 
-echo
-echo "----------------------------------------------------------------------"
-echo "STEP 2: run PFS once with noise-SFTs"
-echo "----------------------------------------------------------------------"
-echo
-outfile_pfs="__tmp_PFS.dat";
-
-## common cmdline-options for v1 and v2
-pfs_CL=" --Alpha=$Alpha --Delta=$Delta --IFO=$IFO --h0=$h0 --cosi=$cosi --psi=$psi --Freq=$Freq --DataFiles='$SFTdir/*' --outputFstat=$outfile_pfs"
-cmdline="$pfs_code $pfs_CL"
-echo $cmdline
-if ! tmp=`eval $cmdline`; then
-    echo "Error.. something failed when running '$pfs_code' ..."
-    exit 1
-fi
-resPFS2=`echo $tmp | awk '{printf "%g", $1}'`
-
-echo
-echo "----------------------------------------------------------------------"
-echo " STEP 3: run PFS without using noise-floor estimation (assume Sh=1)"
-echo "----------------------------------------------------------------------"
-echo
-outfile_pfs2="__tmp_PFS2.dat";
-
-## common cmdline-options for v1 and v2
-pfs_CL=" --Alpha=$Alpha --Delta=$Delta --IFO=$IFO --h0=$h0 --cosi=$cosi --psi=$psi --Freq=$Freq --DataFiles='$SFTdir/*' --outputFstat=$outfile_pfs2 --SignalOnly"
-cmdline="$pfs_code $pfs_CL"
-echo $cmdline
-if ! tmp=`eval $cmdline`; then
-    echo "Error.. something failed when running '$pfs_code' ..."
-    exit 1
+pfs_CL_common=" --Alpha=$Alpha --Delta=$Delta --IFO=$IFO --h0=$h0 --cosi=$cosi --psi=$psi --Freq=$Freq --DataFiles=\"${SFTdir}/*.sft\""
+## ---------- Run PredictFStat{NoiseWeights} ----------
+outfile_pfs1="__tmp_PFS1.dat";
+pfs_CL="${pfs_CL_common} --outputFstat=$outfile_pfs1"
+cmdline="$pfs_path $pfs_CL"
+if [ "$DEBUG" ]; then echo $cmdline; fi
+echo -n "Running ${pfs_code}{NoiseWeights} ... "
+if ! tmp=`eval ${cmdline}`; then
+    echo "FAILED:"
+    echo $cmdline
+    exit 1;
+else
+    echo "OK."
 fi
 resPFS1=`echo $tmp | awk '{printf "%g", $1}'`
 
+## ---------- Run PredictFStat{SignalOnly} ----------
+outfile_pfs0="__tmp_PFS0.dat";
+pfs_CL="${pfs_CL_common} --outputFstat=$outfile_pfs0 --SignalOnly"
+cmdline="$pfs_path $pfs_CL"
+if [ "$DEBUG" ]; then echo $cmdline; fi
+echo -n "Running ${pfs_code}{SignalOnly} ... "
+if ! tmp=`eval $cmdline`; then
+    echo "FAILED:"
+    echo $cmdline
+    exit 1;
+else
+    echo "OK."
+fi
+resPFS0=`echo $tmp | awk '{printf "%g", $1}'`
+
+## ---------- Comparing results ----------
 echo
-echo "----------------------------------------"
-echo " STEP 4: Comparing results: "
-echo "----------------------------------------"
-echo
-echo "SemiAnalyticF:        2F_SA  = $resSAF"
-echo "PredictFStat [Sh=1]:  2F_PF1 = $resPFS1"
-echo "PredictFStat [SFTs]:  2F_PF2 = $resPFS2"
+echo "SemiAnalyticF:              2F_SA  = $resSAF"
+echo "PredictFStat{SignalOnly}:   2F_PF0 = $resPFS0"
+echo "PredictFStat{NoiseWeights}: 2F_PF1 = $resPFS1"
 echo
 
-eps1=$(echo $resSAF $resPFS1 | awk '{printf "%d", int ( 1000 * sqrt( ($1 - $2)*($1 - $2) ) / $1 )}');
-eps2=$(echo $resSAF $resPFS2 | awk '{printf "%d", int ( 1000 * sqrt( ($1 - $2)*($1 - $2) ) / $1 )}');
+awk_reldevPercent='{printf "%.1f", 100.0 * sqrt(($1-$2)*($1-$2))/(0.5*($1+$2)) }'
+awk_isgtr='{if($1>$2) {print "1"}}'
+tolerance0=1	## percent
+tolerance1=15	## percent
+
+eps0=$(echo $resSAF $resPFS0 | awk "$awk_reldevPercent");
+eps1=$(echo $resSAF $resPFS1 | awk "$awk_reldevPercent");
 
 res=0;
-if test $eps1 -gt 10; then
-    echo "==> relative deviation F_PF1 wrt F_SA: $eps1/1000 larger than 1% ... FAILED."
-    echo
+fail0=$(echo $eps0 $tolerance0 | awk "$awk_isgtr")
+fail1=$(echo $eps1 $tolerance1 | awk "$awk_isgtr")
+
+echo -n "Relative deviation 2F_PF{SignalOnly}   wrt 2F_SA = ${eps0}% (tolerance = ${tolerance0}%)"
+if [ "$fail0" ]; then
+    echo " ==> FAILED."
     res=1;
 else
-    echo "==> relative deviation F_PF1 wrt F_SA: $eps1/1000 smaller than 1% ... PASSED."
+    echo " ==> OK."
 fi
 
-if test $eps2 -gt 150; then
-    echo "==> relative deviation F_PF2 wrt F_SA: $eps2/1000 larger than 15% ... FAILED."
-    echo
+echo -n "Relative deviation 2F_PF{NoiseWeights} wrt 2F_SA = ${eps1}% (tolerance = ${tolerance1}%)"
+if [ "$fail1" ]; then
+    echo " ==> FAILED."
     res=1;
 else
-    echo "==> relative deviation F_PF2 wrt F_SA: $eps2/1000 smaller than 15% ... PASSED."
-    echo
+    echo " ==> OK."
 fi
+echo
 
 ## clean up files
 if [ -z "$NOCLEANUP" ]; then
-    rm -rf $SFTdir $outfile_pfs $outfile_pfs2;
+    rm -rf $SFTdir $outfile_pfs0 $outfile_pfs1
 fi
 
 exit $res;
-
