46caf39460ac0d1764bfa5be2bfd934d0bcde993
Evan Goetz <evan.goetz@ligo.org>
2014-02-20 14:54:47 +0100

    Make the bisection search only when necessary and add the swap of lists for the 2nd and 3rd steps so that we don't have to redo the first step Original: 8843e7886ab36c3847ed3a5eef37edf5c50bed2c

--- lalapps/src/pulsar/TwoSpect/helperprograms/compareCandidates.c
+++ lalapps/src/pulsar/TwoSpect/helperprograms/compareCandidates.c
@@ -19,7 +19,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+//#include <string.h>
 #include <math.h>
 
 #include <gsl/gsl_sort.h>
@@ -44,9 +44,10 @@ INT4 main(void) {
    FILE *H1CANDS, *L1CANDS;
    char *infile1 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzH1Candidates.dat";
    char *infile2 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzL1Candidates.dat";
-   char *outfile1 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates.dat";
-   char *outfile2 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_reduced.dat";
-   char *outfile3 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_reduced2.dat";
+   char *outfile1 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_output1.dat";
+   char *outfile2 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_output2.dat";
+   char *outfile3 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_final.dat";
+   char *outfile4 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/400-501HzCandidates_output3.dat";
 
    XLAL_CHECK( (H1CANDS = fopen(infile1,"r")) != NULL, XLAL_EIO, "Can't fopen %s", infile1 );
 
@@ -127,10 +128,10 @@ INT4 main(void) {
    XLAL_CHECK( (CANDS = fopen(outfile1,"w")) != NULL, XLAL_EIO, "Can't fopen %s", outfile1 );
 
    //Setup and allocate the solver
-   int status;
+   int status = GSL_CONTINUE;
    int max_iter = 100;
    double x_lo = 0.0, x_hi = (double)(l1count-1);
-   double foundIndex = 0.0;
+   double foundIndex = -1.0;
    gsl_function F;
    F.function = &fdiff;
    const gsl_root_fsolver_type *T = gsl_root_fsolver_brent;
@@ -145,22 +146,24 @@ INT4 main(void) {
 
       //Check that the frequency of the H1 candidate we look at is within the frequency span of the L1 candidates
       if ( (allh1cands_sorted[ii*9] - alll1cands_sorted[0]) >= 0.0 && (allh1cands_sorted[ii*9] - alll1cands_sorted[(l1count-1)*9]) <= 0.0) {
-         //Do a root finding search for the closest L1 candidate in frequency to the H1 candidate frequency
-         int iter = 0;
-         struct solver_params params = {allh1cands_sorted[ii*9], alll1cands_sorted};
-         F.params = &params;
-         XLAL_CHECK( gsl_root_fsolver_set(s, &F, x_lo, x_hi) == GSL_SUCCESS, XLAL_EFUNC );
-         do {
-            iter++;
-            status = gsl_root_fsolver_iterate(s);
-            XLAL_CHECK( status == GSL_SUCCESS, XLAL_EFUNC );
-            foundIndex = gsl_root_fsolver_root(s);
-            status = gsl_root_test_residual(fdiff(foundIndex, &params), 1.05*fdiff_allowed);
-            XLAL_CHECK( status == GSL_SUCCESS || status == GSL_CONTINUE, XLAL_EFUNC );
-         } while (status == GSL_CONTINUE && iter < max_iter);
+         if (foundIndex < 0.0 || status != GSL_SUCCESS || (status==GSL_SUCCESS && fabs(allh1cands_sorted[ii*9]-alll1cands_sorted[(int)round(gsl_root_fsolver_root(s))*9])>=1.05*fdiff_allowed)) {
+            //Do a root finding search for the closest L1 candidate in frequency to the H1 candidate frequency
+            int iter = 0;
+            struct solver_params params = {allh1cands_sorted[ii*9], alll1cands_sorted};
+            F.params = &params;
+            XLAL_CHECK( gsl_root_fsolver_set(s, &F, x_lo, x_hi) == GSL_SUCCESS, XLAL_EFUNC );
+            do {
+               iter++;
+               status = gsl_root_fsolver_iterate(s);
+               XLAL_CHECK( status == GSL_SUCCESS, XLAL_EFUNC );
+               foundIndex = gsl_root_fsolver_root(s);
+               status = gsl_root_test_residual(fdiff(foundIndex, &params), 1.05*fdiff_allowed);
+               XLAL_CHECK( status == GSL_SUCCESS || status == GSL_CONTINUE, XLAL_EFUNC );
+            } while (status == GSL_CONTINUE && iter < max_iter);
+         }
 
          //If the search was successful, then we step through the L1 candidates to find matching candidates
-         if (status == GSL_SUCCESS && iter < max_iter) {
+         if (status == GSL_SUCCESS) {
             jj = (int)round(foundIndex);   //start at the index of the L1 candidate found in the root finding
 
             //int bestmatch = -1;
@@ -168,6 +171,9 @@ INT4 main(void) {
 
             //Step backwards in L1 candidates until we are definitely below the H1 candidate in frequency (or at the start of the L1 list)
             while (jj>0 && (allh1cands_sorted[ii*9]-alll1cands_sorted[jj*9])<1.05*fdiff_allowed) jj--;
+
+            //Set the foundIndex value to the start of where we should search from
+            foundIndex = jj;
 
             //Starting from the L1 candidate below the H1 candidate frequency
             for ( ; jj<l1count; jj++) {
@@ -396,6 +402,117 @@ INT4 main(void) {
    }
 
    fclose(NEWCANDS);
+
+   XLALFree(allcands);
+   XLALFree(allcands_job);
+
+
+   //Swap input values
+   XLAL_CHECK( (CANDS = fopen(outfile1,"r")) != NULL, XLAL_EIO, "Couldn't fopen %s\n", outfile1 );
+
+   count = 0;
+   do {
+      ch = fgetc(CANDS);
+      if (ch == '\n') count++;
+   } while (ch != EOF);
+
+   allcands = NULL;
+   XLAL_CHECK( (allcands = (double*)XLALMalloc(sizeof(double)*count*18)) != NULL, XLAL_ENOMEM );
+   allcands_job = NULL;
+   XLAL_CHECK( (allcands_job = (int*)XLALMalloc(sizeof(int)*count*2)) != NULL, XLAL_ENOMEM );
+
+   //Reset the pointer in the stream
+   rewind(CANDS);
+
+   //Put the data into the array
+   for (ii=0; ii<count; ii++) {
+      fscanf(CANDS, "%la %la %la %la %la %la %la %la %la %d %la %la %la %la %la %la %la %la %la %d", &(allcands[ii*18]), &(allcands[ii*18+1]), &(allcands[ii*18+2]), &(allcands[ii*18+3]), &(allcands[ii*18+4]), &(allcands[ii*18+5]), &(allcands[ii*18+6]), &(allcands[ii*18+7]), &(allcands[ii*18+8]), &(allcands_job[2*ii]), &(allcands[ii*18+9]), &(allcands[ii*18+10]), &(allcands[ii*18+11]), &(allcands[ii*18+12]), &(allcands[ii*18+13]), &(allcands[ii*18+14]), &(allcands[ii*18+15]), &(allcands[ii*18+16]), &(allcands[ii*18+17]), &(allcands_job[ii*2+1]));
+   }
+
+   //Close the stream
+   fclose(CANDS);
+   CANDS = NULL;
+
+   //Open a file to save the output data
+   XLAL_CHECK( (NEWCANDS = fopen(outfile4,"w")) != NULL, XLAL_EIO, "Couldn't fopen %s\n", outfile4 );
+
+   for (ii=0; ii<count; ii++) {
+      if (allcands[ii*18]!=0.0) {
+         int bestcand = ii;
+         double bestcandprob = allcands[ii*18+7];
+         for (jj=0; jj<count; jj++) {
+            if (jj==ii) continue;
+            if (allcands[jj*18]!=0.0 && allcands[jj*18+9] == allcands[ii*18+9] && allcands[jj*18+10] == allcands[ii*18+10] && allcands[jj*18+11] == allcands[ii*18+11]) {
+               if (allcands[jj*18+7]<bestcandprob) {
+                  bestcandprob = allcands[jj*18+7];
+                  allcands[bestcand*18] = 0.0;
+                  bestcand = jj;
+               } else {
+                  allcands[jj*18] = 0.0;
+               }
+            }
+         }
+         fprintf(NEWCANDS, "%f %f %f %f %f %f %g %f %g %d %f %f %f %f %f %f %g %f %g %d\n",  (float)allcands[bestcand*18], (float)allcands[bestcand*18+1], (float)allcands[bestcand*18+2], (float)allcands[bestcand*18+3], (float)allcands[bestcand*18+4], (float)allcands[bestcand*18+5], allcands[bestcand*18+6], (float)allcands[bestcand*18+7], allcands[bestcand*18+8], allcands_job[bestcand*2], (float)allcands[bestcand*18+9], (float)allcands[bestcand*18+10], (float)allcands[bestcand*18+11], (float)allcands[bestcand*18+12], (float)allcands[bestcand*18+13], (float)allcands[bestcand*18+14], allcands[bestcand*18+15], (float)allcands[bestcand*18+16], allcands[bestcand*18+17], allcands_job[bestcand*2+1]);
+         allcands[bestcand*18] = 0.0;
+      }
+   }
+
+   fclose(NEWCANDS);
+   NEWCANDS = NULL;
+
+   XLALFree(allcands);
+   XLALFree(allcands_job);
+
+   XLAL_CHECK( (CANDS = fopen(outfile4,"r")) != NULL, XLAL_EIO, "Couldn't fopen %s\n", outfile4 );
+
+   //Determines number of candidates in the file
+   count = 0;
+   do {
+      ch = fgetc(CANDS);
+      if (ch == '\n') count++;
+   } while (ch != EOF);
+
+   XLAL_CHECK( (allcands = (double*)XLALMalloc(sizeof(double)*count*18)) != NULL, XLAL_ENOMEM );
+   XLAL_CHECK( (allcands_job = (int*)XLALMalloc(sizeof(int)*count*2)) != NULL, XLAL_ENOMEM );
+
+   //Reset the pointer in the stream
+   rewind(CANDS);
+
+   //Put the data into the array
+   for (ii=0; ii<count; ii++) {
+      fscanf(CANDS, "%la %la %la %la %la %la %la %la %la %d %la %la %la %la %la %la %la %la %la %d", &(allcands[ii*18]), &(allcands[ii*18+1]), &(allcands[ii*18+2]), &(allcands[ii*18+3]), &(allcands[ii*18+4]), &(allcands[ii*18+5]), &(allcands[ii*18+6]), &(allcands[ii*18+7]), &(allcands[ii*18+8]), &(allcands_job[2*ii]), &(allcands[ii*18+9]), &(allcands[ii*18+10]), &(allcands[ii*18+11]), &(allcands[ii*18+12]), &(allcands[ii*18+13]), &(allcands[ii*18+14]), &(allcands[ii*18+15]), &(allcands[ii*18+16]), &(allcands[ii*18+17]), &(allcands_job[ii*2+1]));
+   }
+
+   //Close the stream
+   fclose(CANDS);
+   CANDS = NULL;
+
+   //Open a file to save the output data
+   XLAL_CHECK( (NEWCANDS = fopen(outfile3,"a")) != NULL, XLAL_EIO, "Couldn't fopen %s\n", outfile3 );
+
+   for (ii=0; ii<count; ii++) {
+      if (allcands[ii*18]!=0.0) {
+         int bestcand = ii;
+         double bestcandprob = allcands[ii*18+16];
+         for (jj=0; jj<count; jj++) {
+            if (jj==ii) continue;
+            if (allcands[jj*18]!=0.0 && allcands[jj*18] == allcands[ii*18] && allcands[jj*18+1] == allcands[ii*18+1] && allcands[jj*18+2] == allcands[ii*18+2]) {
+               if (allcands[jj*18+16]<bestcandprob) {
+                  bestcandprob = allcands[jj*18+16];
+                  allcands[bestcand*18] = 0.0;
+                  bestcand = jj;
+               } else {
+                  allcands[jj*18] = 0.0;
+               }
+            }
+         }
+         fprintf(NEWCANDS, "%f %f %f %f %f %f %g %f %g %d %f %f %f %f %f %f %g %f %g %d\n",  (float)allcands[bestcand*18], (float)allcands[bestcand*18+1], (float)allcands[bestcand*18+2], (float)allcands[bestcand*18+3], (float)allcands[bestcand*18+4], (float)allcands[bestcand*18+5], allcands[bestcand*18+6], (float)allcands[bestcand*18+7], allcands[bestcand*18+8], allcands_job[bestcand*2], (float)allcands[bestcand*18+9], (float)allcands[bestcand*18+10], (float)allcands[bestcand*18+11], (float)allcands[bestcand*18+12], (float)allcands[bestcand*18+13], (float)allcands[bestcand*18+14], allcands[bestcand*18+15], (float)allcands[bestcand*18+16], allcands[bestcand*18+17], allcands_job[bestcand*2+1]);
+         allcands[bestcand*18] = 0.0;
+      }
+   }
+
+   fclose(NEWCANDS);
+   NEWCANDS = NULL;
 
    XLALFree(allcands);
    XLALFree(allcands_job);
