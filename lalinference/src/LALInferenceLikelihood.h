c8989f92e7f38682b1f6c13e329dfabb37ca647c
Benjamin Farr <bfarr@u.northwestern.edu>
2014-03-26 13:52:15 +0000

    lalinfernece: removed orphaned function prototype in likelihood header Original: ef290d87f8971c1198063b360183cc7f71da65d5

--- lalinference/src/LALInferenceLikelihood.h
+++ lalinference/src/LALInferenceLikelihood.h
@@ -213,9 +213,6 @@ REAL8 LALInferenceMarginalisedTimeLogLikelihood(LALInferenceVariables *currentPa
  */
 void LALInferenceInitLikelihood(LALInferenceRunState *runState);
 
-/** Get the intrinsic parameters from currentParams */
-LALInferenceVariables LALInferenceGetInstrinsicParams(LALInferenceVariables *currentParams);
-
 /** Switch for controlling the removal of lines */
 INT4 LALInferenceLineSwitch(INT4 lineFlag, INT4 Nlines, INT4 *lines_array, INT4 *widths_array, INT4 i);
 
