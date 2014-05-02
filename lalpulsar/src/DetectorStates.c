346a06d0b5c502a23b8ed2c50baf128a4cd0a03f
David Keitel <david.keitel@aei.mpg.de>
2014-05-02 15:18:01 +0200

    XLALParseMultiNoiseFloor: safer parsing of per-IFO values  -use XLALParseStringValueToREAL8() instead of sscanf()   to e.g. error out on "trailing garbage"  -refs #1315 Original: c76e67fcce850e517eb380188b85e58cafdc6dfb

--- lalpulsar/src/DetectorStates.c
+++ lalpulsar/src/DetectorStates.c
@@ -25,6 +25,7 @@
 #include <lal/CWFastMath.h>
 #include <lal/DetectorStates.h>
 #include <lal/LISAspecifics.h>
+#include <lal/ConfigFile.h>
 
 /*---------- local DEFINES ----------*/
 #define TRUE (1==1)
@@ -778,7 +779,7 @@ XLALParseMultiNoiseFloor ( MultiNoiseFloor *multiNoiseFloor,	/**< [out] parsed m
       UINT4 X0 = X % numSqrtSX;		// always = 0 if (numSqrtSX == 1), otherwise = X if (numSqrtSX==numDetectors)
       const char *sqrtSnStr = sqrtSX->data[X0];
       REAL8 sqrtSn;
-      XLAL_CHECK ( sscanf ( sqrtSnStr , "%lf", &sqrtSn ) == 1, XLAL_EINVAL, "Failed to parse '%s' into REAL8\n", sqrtSnStr );
+      XLAL_CHECK ( XLALParseStringValueToREAL8 ( &sqrtSn, sqrtSnStr ) == XLAL_SUCCESS, XLAL_EFUNC );
       XLAL_CHECK ( sqrtSn >= 0, XLAL_EDOM );
       multiNoiseFloor->sqrtSn[X] = sqrtSn;
     } /* for X < numDetectors */
