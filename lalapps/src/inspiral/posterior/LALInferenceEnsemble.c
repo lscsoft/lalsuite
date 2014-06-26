77341b1940fcba8d79f8d36ec036b8edf9fdc893
Benjamin Farr <bfarr@u.northwestern.edu>
2014-06-26 16:33:17 -0500

    lalinference_ensemble: changed copyright info Original: 41856d2c77bbb686ba6df52e118240d52c3a8b00

--- lalapps/src/inspiral/posterior/LALInferenceEnsemble.c
+++ lalapps/src/inspiral/posterior/LALInferenceEnsemble.c
@@ -1,8 +1,7 @@
 /*
- *  LALInferenceMCMC.c:  Bayesian Followup function testing site
+ *  LALInferenceEnsemble.c:  Bayesian Followup function testing site
  *
- *  Copyright (C) 2011 Ilya Mandel, Vivien Raymond, Christian Roever,
- *  Marc van der Sluys, John Veitch and Will M. Farr
+ *  Copyright (C) 2014 Ben Farr
  *
  *
  *  This program is free software; you can redistribute it and/or modify
