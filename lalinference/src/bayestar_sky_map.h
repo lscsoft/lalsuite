4d21bac72260362f8b4bd8c1ddd3bab698010019
Karl Wette <karl.wette@ligo.org>
2014-07-30 20:51:37 +0200

    Remove lalinference/src/bayestar_sky_map.h from SWIG wrappings Original: ac515a83c322cfc63e97e6319a68ac500976c929

--- lalinference/src/bayestar_sky_map.h
+++ lalinference/src/bayestar_sky_map.h
@@ -69,6 +69,7 @@
 #ifndef BAYESTAR_SKY_MAP_H
 #define BAYESTAR_SKY_MAP_H
 
+#ifndef SWIG /* exclude from SWIG interface */
 
 #include <complex.h>
 
@@ -143,5 +144,7 @@ double bayestar_log_likelihood_toa_phoa_snr(
 /* Unit test suite. Return EXIT_SUCCESS if tests passed,
  * or otherwise EXIT_FAILURE. */
 int bayestar_test(void);
+
+#endif /* SWIG */
 
 #endif /* BAYESTAR_SKY_MAP_H */
