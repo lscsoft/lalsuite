d0747e4b8e4b5bf69be3c45e80355a58906c1118
Leo Singer <leo.singer@ligo.org>
2014-08-08 09:44:35 -0700

    Update copyright Original: 2404baa4515ad568f84230ae44ff9ead0f3676fa

--- lalinference/python/bayestar_bin_samples.py
+++ lalinference/python/bayestar_bin_samples.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011  Leo Singer
+# Copyright (C) 2011-2014  Leo Singer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
