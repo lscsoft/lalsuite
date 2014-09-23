f83ead3fd80311dcc75d217c0a174af194ed1f51
Frank Ohme <frank.ohme@ligo.org>
2014-09-23 10:22:46 +0100

    generalizing script to generate reference waveforms for lalsimulation unit test Original: eb5cf35d91d37caf4e7576f3380d44e6669d753b

--- lalsimulation/test/GenerateReferenceWaveform.py
+++ lalsimulation/test/GenerateReferenceWaveform.py
@@ -24,6 +24,7 @@ import unittest
 from optparse import OptionParser, OptionGroup
 import ConfigParser
 from lalsimulation import git_version
+import io
 
 NEW_DATA_STR = '######### NEW DATASET #############\n'
 
@@ -81,15 +82,22 @@ parameterInfo = [['domain', 'string', 'TD', 'Generate either a time (TD) or ' +
         'binary orientation are defined [default 0 (coalescence)]', 'FREF'],
         ['phiref', 'float', 1.2, 'Orbital phase at FREF'+defstr, 'PHIREF']]
 
+
+paramnames = {'TD': ['phiref', 'deltaT', 'm1', 'm2', 'spin1x', 'spin1y',
+                     'spin1z', 'spin2x', 'spin2y', 'spin2z', 'fmin', 'fref',
+                     'distance', 'inclination', 'lambda1', 'lambda2',
+                     'waveformFlags', 'nonGRparams', 'ampOrder', 'phaseOrder'],
+              'FD': ['phiref', 'deltaF', 'm1', 'm2', 'spin1x', 'spin1y',
+                     'spin1z', 'spin2x', 'spin2y', 'spin2z', 'fmin', 'fmax',
+                     'fref', 'distance', 'inclination', 'lambda1', 'lambda2',
+                     'waveformFlags', 'nonGRparams', 'ampOrder', 'phaseOrder']}
+
 parameterDicts = [{'name': p[0], 'type': p[1], 'default': p[2], 'help': p[3],
         'metavar': p[4]} for p in parameterInfo]
 
 defaultDict = {p['name']: p['default'] for p in parameterDicts}
 defSampleRate = defaultDict['sampleRate']  # needed later
 partypes = {p['name']: p['type'] for p in parameterDicts}
-config = ConfigParser.RawConfigParser(defaults = defaultDict)
-config.add_section('approximant')
-config.add_section('parameters')
 
 usage = ('usage: %prog [options]\n' +
     'Generate a file containing a waveform (h+, hx), the parameters used' +
@@ -110,12 +118,12 @@ parser.add_option('-i', '--input', metavar='FILE', default=None,
 group = OptionGroup(parser, 'Approximant details', 'Options that can be ' +
         'specified in input file under section [approximant]')
 
-typeget = {'string': config.get, 'float': config.getfloat, 'int': config.getint}
+typeget = {'string': lambda x: x, 'float': float, 'int': int}
 
 for p in parameterDicts[:2]:
     group.add_option('--' + p['name'],
-            default = typeget[p['type']]('approximant', p['name']),
-            metavar= p['metavar'], type = p['type'], help = p['help'])
+            default = typeget[p['type']](p['default']),
+            metavar = p['metavar'], type = p['type'], help = p['help'])
 parser.add_option_group(group)
 
 # Waveform parameters
@@ -123,7 +131,7 @@ group = OptionGroup(parser, 'Waveform parameters', 'Options that can be ' +
         'specified in input file under section [parameters]')
 for p in parameterDicts[2:]:
     group.add_option('--' + p['name'],
-            default = typeget[p['type']]('parameters', p['name']),
+            default = typeget[p['type']](p['default']),
             metavar = p['metavar'], type = p['type'], help = p['help'])
 parser.add_option_group(group)
 
@@ -135,12 +143,36 @@ if opts.sampleRate != defaultDict['sampleRate']\
     raise ValueError('Specified both --sampleRate and --deltaT. ' +
             'Provide one or the other.')
 
-# Check if input file was specified
-# If so, re-define defaults accordingly and read options again
-if opts.infile:
-    infile = file(opts.infile, 'r')
-    config.readfp(infile)
-    infile.close()
+
+class InputFile:
+    '''When initialized with the filename for the input file,
+    this class contains in the information given in the file in a
+    ConfigParser compatible way. Various datasets are constructed
+    that represent blocks of data between the string
+    ######### NEW DATASET #############'''
+    def __init__(self, filename):
+        infile = open(filename, 'r')
+        self.name = filename
+        self.content = infile.readlines()
+        infile.close()
+        self.size = len(self.content)
+        self.newapproxindex = [i for i in range(self.size)
+                if self.content[i] == NEW_DATA_STR]
+        ConfigParser.RawConfigParser.optionxform = str
+        # prevent ConfigParser to use lower case version of option
+        self.dataset = [ConfigParser.RawConfigParser(defaults = defaultDict)
+                for i in range(len(self.newapproxindex))]
+        self.newapproxindex.append(self.size)
+        for i in range(len(self.newapproxindex) - 1):
+            begin, end = self.newapproxindex[i:(i+2)]
+            filepart = io.BytesIO('\n'.join(self.content[(begin+1):end]))
+            self.dataset[i].readfp(filepart)
+
+
+def updateOptions(config):
+    '''Re-define defaults according to dictionary in config [ConfigParser
+    object] and read options again'''
+    typeget = {'string': config.get, 'float': config.getfloat, 'int': config.getint}
     pardict1 = {p['name']: typeget[p['type']]('approximant', p['name'])
             for p in parameterDicts[:2]}
     pardict2 = {p['name']: typeget[p['type']]('parameters', p['name'])
@@ -148,94 +180,119 @@ if opts.infile:
     defaultDict = dict(pardict1.items() + pardict2.items())
     parser.set_defaults(**defaultDict)
     (opts, args) = parser.parse_args()
+    return opts
 
-# Prepare waveform generation
-optsDict = {p['name']: eval('opts.'+p['name']) for p in parameterDicts}
-inputpar = copy.deepcopy(optsDict)
-inputpar['m1'] *= lal.MSUN_SI
-inputpar['m2'] *= lal.MSUN_SI
-inputpar['distance'] *= (1.e6 * lal.PC_SI)
-inputpar['approximant'] = lalsim.GetApproximantFromString(
+
+def generateWfparameters(opts):
+    '''Turn parameters specified by input file and/or command line options into
+    parameter dictionary that can be used for waveform generation.'''
+    optsDict = vars(opts)
+    inputpar = copy.deepcopy(optsDict)
+    inputpar['m1'] *= lal.MSUN_SI
+    inputpar['m2'] *= lal.MSUN_SI
+    inputpar['distance'] *= (1.e6 * lal.PC_SI)
+    inputpar['approximant'] = lalsim.GetApproximantFromString(
         inputpar['approximant'])
-inputpar.update({'nonGRparams': None, 'waveformFlags': None})
-# nonGRparams and waveformsFlags currently not supported
+    inputpar.update({'nonGRparams': None, 'waveformFlags': None})
+    # nonGRparams and waveformsFlags currently not supported
 
-if (inputpar['sampleRate'] != defSampleRate):
-    inputpar['deltaT'] = 1./inputpar['sampleRate']
-    optsDict['deltaT'] = inputpar['deltaT']
+    if (inputpar['sampleRate'] != defSampleRate):
+        inputpar['deltaT'] = 1./inputpar['sampleRate']
+        optsDict['deltaT'] = inputpar['deltaT']
 
-paramnames = {'TD': ['phiref', 'deltaT', 'm1', 'm2', 'spin1x', 'spin1y',
-                     'spin1z', 'spin2x', 'spin2y', 'spin2z', 'fmin', 'fref',
-                     'distance', 'inclination', 'lambda1', 'lambda2',
-                     'waveformFlags', 'nonGRparams', 'ampOrder', 'phaseOrder'], 
-              'FD': ['phiref', 'deltaF', 'm1', 'm2', 'spin1x', 'spin1y',
-                     'spin1z', 'spin2x', 'spin2y', 'spin2z', 'fmin', 'fmax',
-                     'fref', 'distance', 'inclination', 'lambda1', 'lambda2',
-                     'waveformFlags', 'nonGRparams', 'ampOrder', 'phaseOrder']} 
-
-wfparameters = [inputpar[p] for p in paramnames[inputpar['domain']]]
-wfparameters.append(inputpar['approximant'])
+    return inputpar, optsDict
 
 
-# Generate the waveform
-waveformgenerator={'TD': lalsim.SimInspiralChooseTDWaveform,
-                   'FD': lalsim.SimInspiralChooseFDWaveform}[inputpar['domain']]
-hp, hc = waveformgenerator(*wfparameters)
-assert hp.epoch==hc.epoch
+def generateWaveformData(inputpar):
+    '''Generate waveform data from input parameter dictionary'''
 
-# Open output file
-fp = open(opts.output, 'a')
-fp.write(NEW_DATA_STR)
+    wfparameters = [inputpar[p] for p in paramnames[inputpar['domain']]]
+    wfparameters.append(inputpar['approximant'])
 
-# Write metadata sections of output file
-fp.write('[approximant]\n')
-for p in ['approximant', 'domain']:
-    fp.write(p + ' = ' + optsDict[p] + '\n')
+    waveformgenerator={'TD': lalsim.SimInspiralChooseTDWaveform,
+                       'FD': lalsim.SimInspiralChooseFDWaveform}[inputpar['domain']]
+    hp, hc = waveformgenerator(*wfparameters)
+    assert hp.epoch==hc.epoch
 
-fp.write('\n[auxiliary]\n')
-fp.write('LALSuite-git = ' + git_version.version + '\n')
+    return hp, hc
 
-fp.write('\n[parameters]\n')
 
-formattext = {'float': lambda val: '%.16e' % val, 'string': lambda x: x,
-               'int': lambda val: str(val)}
+def writeData(filename, polarisations, optsDict):
+    '''Write waveform data to file'''
+    hp, hc = polarisations
 
-# make only output parameters that have actually been defined in optsDict
-# (excluding waveformFlags and nonGRparams)
-outputpars = list(set(paramnames[optsDict['domain']]).intersection(
-        set(optsDict.keys())))
+    # Open output file
+    fp = open(filename, 'a')
+    fp.write(NEW_DATA_STR)
 
-for p in outputpars:
-    line = p + ' = ' + formattext[partypes[p]](optsDict[p]) + '\n'
-    fp.write(line)
+    # Write metadata sections of output file
+    fp.write('[approximant]\n')
+    for p in ['approximant', 'domain']:
+        fp.write(p + ' = ' + optsDict[p] + '\n')
 
-# Write the waveform data section
-fp.write('\n[waveform-data]\n')
-line = 'epoch = %.16e\n' % hp.epoch; fp.write(line)
-if optsDict['domain']=='TD':
-    fp.write('hp =')
-    for x in hp.data.data:
-        fp.write(' %.16e' % x)
-    fp.write('\nhc =')
-    for x in hc.data.data:
-        fp.write(' %.16e' % x)
-elif optsDict['domain']=='FD':
-    hpreal = np.real(hp.data.data)
-    hpimag = np.imag(hp.data.data)
-    hcreal = np.real(hc.data.data)
-    hcimag = np.imag(hc.data.data)
-    fp.write('hp_real =')
-    for x in hpreal:
-        fp.write(' %.16e' % x)
-    fp.write('\nhp_imag =')
-    for x in hpimag:
-        fp.write(' %.16e' % x)
-    fp.write('\nhc_real =')
-    for x in hcreal:
-        fp.write(' %.16e' % x)
-    fp.write('\nhc_imag =')
-    for x in hcimag:
-        fp.write(' %.16e' % x)
-fp.write('\n\n\n')
+    fp.write('\n[auxiliary]\n')
+    fp.write('LALSuite-git = ' + git_version.version + '\n')
 
-fp.close()
+    fp.write('\n[parameters]\n')
+
+    formattext = {'float': lambda val: '%.16e' % val, 'string': lambda x: x,
+                   'int': lambda val: str(val)}
+
+    # make only output parameters that have actually been defined in optsDict
+    # (excluding waveformFlags and nonGRparams)
+    outputpars = list(set(paramnames[optsDict['domain']]).intersection(
+            set(optsDict.keys())))
+
+    for p in outputpars:
+        line = p + ' = ' + formattext[partypes[p]](optsDict[p]) + '\n'
+        fp.write(line)
+
+    # Write the waveform data section
+    fp.write('\n[waveform-data]\n')
+    line = 'epoch = %.16e\n' % hp.epoch; fp.write(line)
+    if optsDict['domain']=='TD':
+        fp.write('hp =')
+        for x in hp.data.data:
+            fp.write(' %.16e' % x)
+        fp.write('\nhc =')
+        for x in hc.data.data:
+            fp.write(' %.16e' % x)
+    elif optsDict['domain']=='FD':
+        hpreal = np.real(hp.data.data)
+        hpimag = np.imag(hp.data.data)
+        hcreal = np.real(hc.data.data)
+        hcimag = np.imag(hc.data.data)
+        fp.write('hp_real =')
+        for x in hpreal:
+            fp.write(' %.16e' % x)
+        fp.write('\nhp_imag =')
+        for x in hpimag:
+            fp.write(' %.16e' % x)
+        fp.write('\nhc_real =')
+        for x in hcreal:
+            fp.write(' %.16e' % x)
+        fp.write('\nhc_imag =')
+        for x in hcimag:
+            fp.write(' %.16e' % x)
+    fp.write('\n\n\n')
+
+    fp.close()
+
+    return
+
+
+if __name__ == '__main__':
+    if opts.infile:
+        infile = InputFile(opts.infile)
+        # if parameters are specified by input file, loop through all
+        # datasets of input file, set default accordingly and generate wfs.
+        # NOTE: command line options overwrite parameters in input file
+        # for every dataset
+        for conf in infile.dataset:
+            par, optsdict = generateWfparameters(updateOptions(conf))
+            polarisations = generateWaveformData(par)
+            writeData(opts.output, polarisations, optsdict)
+    else:
+        par, optsdict = generateWfparameters(opts)
+        polarisations = generateWaveformData(par)
+        writeData(opts.output, polarisations, optsdict)
