1c8222d1a249d6281c025ad0bac1e4db11e138c0
ruslan.vaulin <ruslan.vaulinn@ligo.org>
2014-10-29 11:42:54 -0500

    Added functions for querrying glitch-ovl and glitch-sngl_burst coincidences to idq_tables.py. Original: 3de28821eb180cb9f29f4336e55fd9b1a133b7af

--- laldetchar/python/laldetchar/idq/idq_tables.py
+++ laldetchar/python/laldetchar/idq/idq_tables.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 Reed Essick
+# Copyright (C) 2014 Reed Essick, Ruslan Vaulin
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -26,9 +26,10 @@ from glue.ligolw import ligolw
 from glue.ligolw import table
 from glue.ligolw import types as ligolwtypes
 from glue.ligolw import ilwd
+from glue.ligolw import lsctables
 from laldetchar import git_version
 
-__author__ = 'Reed Essick <reed.essick@ligo.org>'
+__author__ = 'Reed Essick <reed.essick@ligo.org>, Ruslan Vaulin <ruslan.vaulin@ligo.org>'
 __version__ = git_version.id
 __date__ = git_version.date
 
@@ -37,6 +38,160 @@ __date__ = git_version.date
 # @{
 
 # contains definitions for iDQ xml table classes and objects
+
+# define types of coincidences within iDQ pipeline
+
+IDQCoincDef = {
+    'idq_glitch<-->sngl_burst': ('idq',0),
+    'idq_glitch<-->ovl_data': ('idq', 1)
+} 
+
+
+def coinc_to_ovl_data(xmldoc):
+    """Function returns list of (idq_glitch_object, ovl_data_object) tuples 
+       where objects in the tuple are mapped to each other via coinc tables.
+	"""
+
+    # get necessary tables from xmldoc
+    coinc_def_table = table.get_table(xmldoc, lsctables.CoincDefTable.tableName)
+    coinc_table = table.get_table(xmldoc, lsctables.CoincTable.tableName)
+    coinc_map_table = table.get_table(xmldoc, lsctables.CoincMapTable.tableName)
+    idq_glitch_table = table.get_table(xmldoc, lsctables.IDQGlitchTable.tableName)
+    ovl_data_table = table.get_table(xmldoc, lsctables.OVLDataTable.tableName)
+    
+    # get coinc_def_ids
+    #coinc_def_id = coinc_def_table.get_coinc_def_id(
+    #            search = IDQCoincDef['idq_glitch<-->ovl_data'][0],
+    #            search_coinc_type = IDQCoincDef['idq_glitch<-->ovl_data'][1],
+    #            create_new = False,
+    #            description = 'idq_glitch<-->ovl_data')
+    coinc_def_ids = [row.coinc_def_id for row in coinc_def_table if row.description == 'idq_glitch<-->ovl_data']
+        
+        
+    
+    # use this id to get all coinc_event ids
+    ovl_coinc_ids = [coinc.coinc_event_id for coinc in coinc_table if coinc.coinc_def_id in coinc_def_ids]
+
+    # convert idq_glitch and ovl tables into dictionaries for a quick lookup
+    glitches = dict([(glitch.event_id, glitch) for glitch in idq_glitch_table])
+    ovl_data = dict([(row.event_id, row) for row in ovl_data_table])
+
+    
+    # create dictionary of connected events in coinc_event_map. 
+    # We can not assume any specific order of rows in the table.
+    connected_events_dict = {}
+    for row in coinc_map_table:
+        try: connected_events_dict[row.coinc_event_id].append(row)
+        except: connected_events_dict[row.coinc_event_id] = [row]
+    
+    glitch_table_name =  table.StripTableName(lsctables.IDQGlitchTable.tableName)
+    ovl_data_table_name = table.StripTableName(lsctables.OVLDataTable.tableName)
+     
+    glitch_ovl_pairs = []
+    for coinc_id in ovl_coinc_ids:
+        # get connectected events for this id
+        connected_events = connected_events_dict[coinc_id]
+        if len(connected_events) == 2:
+            for event in connected_events:
+                if event.table_name == glitch_table_name:
+                    glitch_event = glitches[event.event_id]
+                elif  event.table_name == ovl_data_table_name:
+                    ovl_event = ovl_data[event.event_id]
+                else:
+                    print event.table_name
+                    raise ValueError("Event is not the row of either " + \
+                                     glitch_table_name + \
+                                     " or " + ovl_data_table_name
+                                     )
+        else:
+            raise Exception("Glitch-OVL coincidence must contain exactly 2 events. "\
+                            + str(len(connected_events))+ " events are found instead."
+                            )
+        glitch_ovl_pairs.append((glitch_event, ovl_event))
+    return glitch_ovl_pairs
+            
+        
+    
+def coinc_to_triggers(xmldoc, trigger_types):
+    """ Function returns list of glitch-trigger(s) coincident events. 
+        Coincident event in the list is represented by tuple (glitch_object, [trigger1, trigger2, ...]).
+        trigger_types is the list of trigger type names corresponding to "search" column of the sngl_burst table
+    """
+
+
+    # get necessary tables from xmldoc
+    coinc_def_table = table.get_table(xmldoc, lsctables.CoincDefTable.tableName)
+    coinc_table = table.get_table(xmldoc, lsctables.CoincTable.tableName)
+    coinc_map_table = table.get_table(xmldoc, lsctables.CoincMapTable.tableName)
+    idq_glitch_table = table.get_table(xmldoc, lsctables.IDQGlitchTable.tableName)
+    sngl_burst_table = table.get_table(xmldoc, lsctables.SnglBurstTable.tableName)
+    
+    
+    # get coinc_def_id
+    #coinc_def_id = coinc_def_table.get_coinc_def_id(
+    #            search = IDQCoincDef['idq_glitch<-->sngl_burst'][0],
+    #            search_coinc_type = IDQCoincDef['idq_glitch<-->sngl_burst'][1],
+    #            create_new = False,
+    #            description = 'idq_glitch<-->sngl_burst')
+    coinc_def_ids = [row.coinc_def_id for row in coinc_def_table if row.description == 'idq_glitch<-->sngl_burst']
+    
+    # use this id to get all coinc_event ids
+    trig_coinc_ids = [coinc.coinc_event_id for coinc in coinc_table if coinc.coinc_def_id in coinc_def_ids]
+    
+
+    # convert idq_glitch and sngl_burst tables into dictionaries for a quick lookup
+    glitches = dict([ (glitch.event_id, glitch) for glitch in idq_glitch_table])
+    triggers = dict([ (row.event_id, row) for row in sngl_burst_table if row.search in trigger_types])
+
+    # create dictionary of connected events using coinc_event_map. 
+    # We can not assume any specific order of rows in the table.
+    connected_events_dict = {}
+    for row in coinc_map_table:
+        try: connected_events_dict[row.coinc_event_id].append(row)
+        except: connected_events_dict[row.coinc_event_id] = [row]
+    
+    glitch_table_name = table.StripTableName(lsctables.IDQGlitchTable.tableName)
+    sngl_burst_table_name = table.StripTableName(lsctables.SnglBurstTable.tableName)
+    
+    glitch_trig_tuples = []
+    for coinc_id in trig_coinc_ids:
+        # get connectected events for this id
+        connected_events = connected_events_dict[coinc_id]
+        connected_trigs = []
+        if len(connected_events) >= 2:
+            for event in connected_events:
+                if event.table_name == glitch_table_name:
+                    glitch_event = glitches[event.event_id]
+                elif  event.table_name == sngl_burst_table_name:
+                    try: connected_trigs.append(triggers[event.event_id])
+                    except: # no trigger with that id, it is probably of different type.
+                        pass
+                else:
+                    raise ValueError("Event is not the row of either " + \
+                                     glitch_table_name + " or " + sngl_burst_table_name
+                                     )
+        else:
+            raise Exception("Glitch-Triggers coincidences must contain at least 2 events. " \
+                            + str(len(connected_events))+ " events are found instead."
+                            )
+        glitch_trig_tuples.append((glitch_event, connected_trigs))
+    return glitch_trig_tuples
+
+    
+    
+    
+    
+    
+    
+	
+	
+	
+	
+	
+	
+	
+
+
 #
 # =============================================================================
 #
@@ -44,6 +199,10 @@ __date__ = git_version.date
 #
 # =============================================================================
 #
+
+
+
+
 
 
 GlitchID = ilwd.get_ilwdchar_class(u"glitch", u"event_id")
