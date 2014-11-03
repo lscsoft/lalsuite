012695e9a6a21fabe5613b36d8e0cf08b2d1b0dc
Yi Pan <yi.pan@ligo.org>
2014-11-02 19:22:44 -0500

    introduce SEOBNRv3 precessing EOB model Original: a9565b2291cc2ccdb60b207dc57e515d810c44b1

--- lalsimulation/src/LALSimInspiral.h
+++ lalsimulation/src/LALSimInspiral.h
@@ -89,6 +89,7 @@ typedef enum {
    EOBNRv2HM,		/**< UNDOCUMENTED */
    SEOBNRv1,		/**< Spin-aligned EOBNR model */
    SEOBNRv2,		/**< Spin-aligned EOBNR model v2 */
+   SEOBNRv3,		/**< Spin precessing EOBNR model v3 */
    IMRPhenomA,		/**< Time domain (non-spinning) inspiral-merger-ringdown waveforms generated from the inverse FFT of IMRPhenomFA  */
    IMRPhenomB,		/**< Time domain (non-precessing spins) inspiral-merger-ringdown waveforms generated from the inverse FFT of IMRPhenomFB */
    IMRPhenomFA,		/**< Frequency domain (non-spinning) inspiral-merger-ringdown templates of Ajith et al [Ajith_2007kx] with phenomenological coefficients defined in the Table I of [Ajith_2007xh]*/
