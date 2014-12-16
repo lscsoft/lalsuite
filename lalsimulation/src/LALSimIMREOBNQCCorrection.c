7c47cdfb90356425b7939aaebeab9521dc684ab5
Collin Capano <collin.capano@ligo.org>
2014-12-16 17:51:20 +0100

    Fix segfault in SEOBNRv2 by adjusting requested value for interpolation of chiA if the requested value is outside of the interpolation bounds. Original: 1594edc5077375509b634a57f606acb465e6461b

--- lalsimulation/src/LALSimIMREOBNQCCorrection.c
+++ lalsimulation/src/LALSimIMREOBNQCCorrection.c
@@ -1610,6 +1610,15 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmax( EOBNonQCCoeffs * r
   /* REAL8 eta2 = eta*eta;*/
   REAL8 a3slist[qdim], a4list[qdim], a5list[qdim];
   REAL8 b3list[qdim], b4list[qdim];
+  REAL8 chiA;
+
+  chiA = a/(1.0-2.0*eta);
+  if (chiA < -1.){
+      chiA = -1.;
+  }
+  if (chiA > 0.99){
+      chiA = 0.99;
+  }
 
   memset( coeffs, 0, sizeof( *coeffs ) );
   const double alist[200] = {-1., -0.99, -0.98, -0.97, -0.96, -0.95, -0.94, -0.93, -0.92, -0.91, -0.9, -0.89, -0.88, -0.87, -0.86, -0.85, -0.84, -0.83, -0.82, -0.81, -0.8, -0.79, -0.78, -0.77, -0.76, -0.75, -0.74, -0.73, -0.72, -0.71, -0.7, -0.69, -0.68, -0.67, -0.66, -0.65, -0.64, -0.63, -0.62, -0.61, -0.6, -0.59, -0.58, -0.57, -0.56, -0.55, -0.54, -0.53, -0.52, -0.51, -0.5, -0.49, -0.48, -0.47, -0.46, -0.45, -0.44, -0.43, -0.42, -0.41, -0.4, -0.39, -0.38, -0.37, -0.36, -0.35, -0.34, -0.33, -0.32, -0.31, -0.3, -0.29, -0.28, -0.27, -0.26, -0.25, -0.24, -0.23, -0.22, -0.21, -0.2, -0.19, -0.18, -0.17, -0.16, -0.15, -0.14, -0.13, -0.12, -0.11, -0.1, -0.09, -0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99}; 
@@ -2129,19 +2138,19 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmax( EOBNonQCCoeffs * r
   {
     gsl_spline_init( spline, alist, a3stab[i], adim );
     gsl_interp_accel_reset( acc );
-    a3slist[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a3slist[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a4list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a5tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a5list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a5list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b3tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b3list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b3list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b4list[i] = gsl_spline_eval( spline, chiA, acc );
 //  printf("%.15f\n",a3slist[i]);
   }
 //printf("%.15f\n",a);
@@ -2182,6 +2191,15 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmed( EOBNonQCCoeffs * r
   /* REAL8 eta2 = eta*eta;*/
   REAL8 a3slist[qdim], a4list[qdim], a5list[qdim];
   REAL8 b3list[qdim], b4list[qdim];
+  REAL8 chiA;
+
+  chiA = a/(1.0-2.0*eta);
+  if (chiA < -1.){
+      chiA = -1.;
+  }
+  if (chiA > 0.99){
+      chiA = 0.99;
+  }
 
   memset( coeffs, 0, sizeof( *coeffs ) );
   const double alist[200] = {-1., -0.99, -0.98, -0.97, -0.96, -0.95, -0.94, -0.93, -0.92, -0.91, -0.9, -0.89, -0.88, -0.87, -0.86, -0.85, -0.84, -0.83, -0.82, -0.81, -0.8, -0.79, -0.78, -0.77, -0.76, -0.75, -0.74, -0.73, -0.72, -0.71, -0.7, -0.69, -0.68, -0.67, -0.66, -0.65, -0.64, -0.63, -0.62, -0.61, -0.6, -0.59, -0.58, -0.57, -0.56, -0.55, -0.54, -0.53, -0.52, -0.51, -0.5, -0.49, -0.48, -0.47, -0.46, -0.45, -0.44, -0.43, -0.42, -0.41, -0.4, -0.39, -0.38, -0.37, -0.36, -0.35, -0.34, -0.33, -0.32, -0.31, -0.3, -0.29, -0.28, -0.27, -0.26, -0.25, -0.24, -0.23, -0.22, -0.21, -0.2, -0.19, -0.18, -0.17, -0.16, -0.15, -0.14, -0.13, -0.12, -0.11, -0.1, -0.09, -0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99}; 
@@ -2701,19 +2719,19 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmed( EOBNonQCCoeffs * r
   {
     gsl_spline_init( spline, alist, a3stab[i], adim );
     gsl_interp_accel_reset( acc );
-    a3slist[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a3slist[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a4list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a5tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a5list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a5list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b3tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b3list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b3list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b4list[i] = gsl_spline_eval( spline, chiA, acc );
 //  printf("%.15f\n",a3slist[i]);
   }
 //printf("%.15f\n",a);
@@ -2754,6 +2772,15 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmin( EOBNonQCCoeffs * r
   /* REAL8 eta2 = eta*eta;*/
   REAL8 a3slist[qdim], a4list[qdim], a5list[qdim];
   REAL8 b3list[qdim], b4list[qdim];
+  REAL8 chiA;
+
+  chiA = a/(1.0-2.0*eta);
+  if (chiA < -1.){
+      chiA = -1.;
+  }
+  if (chiA > 0.99){
+      chiA = 0.99;
+  }
 
   memset( coeffs, 0, sizeof( *coeffs ) );
   const double alist[200] = {-1., -0.99, -0.98, -0.97, -0.96, -0.95, -0.94, -0.93, -0.92, -0.91, -0.9, -0.89, -0.88, -0.87, -0.86, -0.85, -0.84, -0.83, -0.82, -0.81, -0.8, -0.79, -0.78, -0.77, -0.76, -0.75, -0.74, -0.73, -0.72, -0.71, -0.7, -0.69, -0.68, -0.67, -0.66, -0.65, -0.64, -0.63, -0.62, -0.61, -0.6, -0.59, -0.58, -0.57, -0.56, -0.55, -0.54, -0.53, -0.52, -0.51, -0.5, -0.49, -0.48, -0.47, -0.46, -0.45, -0.44, -0.43, -0.42, -0.41, -0.4, -0.39, -0.38, -0.37, -0.36, -0.35, -0.34, -0.33, -0.32, -0.31, -0.3, -0.29, -0.28, -0.27, -0.26, -0.25, -0.24, -0.23, -0.22, -0.21, -0.2, -0.19, -0.18, -0.17, -0.16, -0.15, -0.14, -0.13, -0.12, -0.11, -0.1, -0.09, -0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99}; 
@@ -3273,19 +3300,19 @@ UNUSED static int XLALSimIMRGetEOBCalibratedSpinNQCv2chiAmin( EOBNonQCCoeffs * r
   {
     gsl_spline_init( spline, alist, a3stab[i], adim );
     gsl_interp_accel_reset( acc );
-    a3slist[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a3slist[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a4list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, a5tab[i], adim );
     gsl_interp_accel_reset( acc );
-    a5list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    a5list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b3tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b3list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b3list[i] = gsl_spline_eval( spline, chiA, acc );
     gsl_spline_init( spline, alist, b4tab[i], adim );
     gsl_interp_accel_reset( acc );
-    b4list[i] = gsl_spline_eval( spline, a/(1.0-2.0*eta), acc );
+    b4list[i] = gsl_spline_eval( spline, chiA, acc );
 //  printf("%.15f\n",a3slist[i]);
   }
 //printf("%.15f\n",a);
