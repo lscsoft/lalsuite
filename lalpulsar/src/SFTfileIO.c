9ef4ede6e04bb33015f8f5af019f1ad505e8f67e
Reinhard Prix <reinhard.prix@ligo.org>
2015-01-08 20:58:09 +0100

    fix warning in SFTfileIO.c about name-field copying - 'name' field is a fixed-size array, but treated as if string-pointer - warning showed up in clang-3.6 for the first time - fixes #1796 Original: 9c2d6c4ac6650c089baae04bb2919c615971a47c

--- lalpulsar/src/SFTfileIO.c
+++ lalpulsar/src/SFTfileIO.c
@@ -908,8 +908,7 @@ XLALLoadSFTs (const SFTCatalog *catalog,   /**< The 'catalogue' of SFTs to load 
 	/* data is ok, add to SFT */
 	segments[isft].last               = lastBinRead;
 	segments[isft].lastfrom           = locator;
-	if(locatalog.data[catPos].header.name && *(locatalog.data[catPos].header.name))
-	  strcpy(sftVector->data[isft].name, locatalog.data[catPos].header.name);
+        memcpy( sftVector->data[isft].name, locatalog.data[catPos].header.name, sizeof(sftVector->data[isft].name));
 	sftVector->data[isft].sampleUnits = locatalog.data[catPos].header.sampleUnits;
 	memcpy(sftVector->data[isft].data->data + (firstBinRead - firstbin),
 	       thisSFT->data->data,
