lscsoft/lalsuite-archive

commit 4a9645b3d1cc870afee729680e87269f1fb41ac9

Disentangle old CW 'LUT' functions from CWFastMath module
- removed unused and obsolete LogLUT-functions, and moved obsolete
  and deprecated ExpLUT-functions into transientCW-utils, the only
  place where they are used still. [should eventually be replace by
  new vector functions]
- moved SinCosLUT functions into separate header 'SinCosLUT.h', and updated
  all files previously including CWFastMath.h instead
- CWFastMath module should be reserved for vector-math, and eventually
  moved upstream into LAL
- refs #358

lalpulsar/src/TransientCW_utils.h
@@ -48,7 +48,7 @@ extern "C" {
 #include <lal/SFTutils.h>
 #include <lal/PulsarDataTypes.h>
 #include <lal/ComputeFstat.h>
-#include <lal/CWFastMath.h> /* for XLALFastNegExp() */
+#include <lal/SinCosLUT.h> /* for XLALFastNegExp() */
 
 /* ---------- exported API defines ---------- */
 
@@ -121,6 +121,10 @@ pdf1D_t *XLALComputeTransientPosterior_tau ( transientWindowRange_t windowRange,
 
 void XLALDestroyTransientFstatMap ( transientFstatMap_t *FstatMap );
 void XLALDestroyTransientCandidate ( transientCandidate_t *cand );
+
+REAL8 XLALFastNegExp ( REAL8 mx );
+REAL4 XLALFastExpf ( REAL4 x );
+void XLALDestroyExpLUT( void );
 
 /* ---------- Fstat-atoms related functions ----------*/
 int write_MultiFstatAtoms_to_fp ( FILE *fp, const MultiFstatAtomVector *multiAtoms );

lalapps/src/pulsar/TDS_isolated/ppe_models.c
@@ -7,7 +7,7 @@
  */
 
 #include "ppe_models.h"
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 
 #define SQUARE(x) ( (x) * (x) )
 

lalapps/src/pulsar/TDS_isolated/pulsar_parameter_estimation.h
@@ -40,7 +40,7 @@
 #include <lal/MatrixUtils.h>
 #include <lal/LALConstants.h>
 #include <lal/XLALError.h>
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 #include <lal/TimeSeries.h>
 
 #include <lalapps.h>

lalapps/src/pulsar/TwoSpect/templates.c
@@ -29,7 +29,7 @@
 #include <lal/LALMalloc.h>
 #include <lal/Window.h>
 #include <lal/VectorOps.h>
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 
 #include "templates.h"
 #include "cdfwchisq.h"

lalapps/src/pulsar/TwoSpect/vectormath.c
@@ -32,7 +32,7 @@
 #include <math.h>
 
 #include <lal/LALConstants.h>
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 
 #include "vectormath.h"
 #include "templates.h"

lalpulsar/src/CWFastMath.c
@@ -1,6 +1,4 @@
-// Copyright (C) 2014 Karl Wette
-// Copyright (C) 2011, 2014 David Keitel
-// Copyright (C) 2011 Reinhard Prix
+// Copyright (C) 2015 Reinhard Prix
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -19,16 +17,16 @@
 //
 
 /*---------- INCLUDES ----------*/
-#include "CWFastMath.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-#include <gsl/gsl_vector.h>
+
+#include <config.h>
+
+#include "CWFastMath.h"
 
 /*---------- local DEFINES ----------*/
-#define TRUE (1==1)
-#define FALSE (1==0)
 
 /*----- Macros ----- */
 
@@ -42,189 +40,4 @@
 
 /*---------- internal prototypes ----------*/
 
-/* ----- module-local fast lookup-table handling of logarithms ----- */
-
-/**
- * Lookup-table for logarithms log(x)
- * Holds an array 'data' of 'length' for values log(x) for x in the range (0, xmax]
- */
-#define LOGLUT_XMAX 	3.0	// LUT for range (0,numDetectors+1), currently numDetectors = 2 FIXME: get this dynamically
-#define LOGLUT_LENGTH 	2000	// number of LUT values to pre-compute
-static gsl_vector *logLUT = NULL; 	/**< module-global lookup-table for logarithms log(x) */
-#define LOGLUT_DXINV  ((LOGLUT_LENGTH)/(LOGLUT_XMAX))	// 1/dx with dx = xmax/length
-
-static int XLALCreateLogLUT ( void );	/* only ever used internally, destructor is in exported API */
-
-/* ----- module-local fast lookup-table handling of negative exponentials ----- */
-
-/**
- * Lookup-table for negative exponentials e^(-x)
- * Holds an array 'data' of 'length' for values e^(-x) for x in the range [0, xmax]
- */
-#define EXPLUT_XMAX 	20.0	// LUT down to e^(-20) = 2.0612e-09
-#define EXPLUT_LENGTH 	2000	// number of LUT values to pre-compute
-static gsl_vector *expLUT = NULL; 	/**< module-global lookup-table for negative exponentials e^(-x) */
-#define EXPLUT_DXINV  ((EXPLUT_LENGTH)/(EXPLUT_XMAX))	// 1/dx with dx = xmax/length
-
-static int XLALCreateExpLUT ( void );	/* only ever used internally, destructor is in exported API */
-
 /*==================== FUNCTION DEFINITIONS ====================*/
-
-/** Generate a lookup-table logLUT for log(x) over the interval x in (0, xmax], using 'length' points. */
-int
-XLALCreateLogLUT ( void )
-{
-  /* create empty output LUT */
-  gsl_vector *ret;
-  XLAL_CHECK ( ( ret = gsl_vector_alloc ( LOGLUT_LENGTH + 1)) != NULL, XLAL_ENOMEM, "Failed call to gsl_vector_alloc (%i).", LOGLUT_LENGTH +1 );
-
-  /* fill output LUT */
-  REAL8 dx = LOGLUT_XMAX / LOGLUT_LENGTH;
-  UINT4 i;
-  for ( i=0; i <= LOGLUT_LENGTH; i ++ ) {
-    REAL8 xi = i * dx;
-    gsl_vector_set ( ret, i, log( xi ) );
-  } /* for i < length() */
-
-  /* 'return' this by setting the global vector */
-  logLUT = ret;
-
-  return XLAL_SUCCESS;
-
-} /* XLALCreateLogLUT() */
-
-
-/**
- * Destructor function for logLUT_t lookup table
- */
-void
-XLALDestroyLogLUT ( void )
-{
-
-  if ( !logLUT ) {
-    return;
-  }
-
-  gsl_vector_free ( logLUT );
-
-  logLUT = NULL;
-
-  return;
-
-} /* XLALDestroyLogLUT() */
-
-
-/**
- * Fast logarithmic function log(x) using lookup-table (LUT).
- * We need to compute log(x) for x in (0,xmax], typically in a B-stat
- * integral of the form int e^-x dx: this means that small values e^(-x)
- * will not contribute much to the integral and are less important than
- * values close to 1. Therefore we pre-compute a LUT of e^(-x) for x in [0, xmax],
- * in Npoints points, and set e^(-x) = 0 for x < xmax.
- *
- * NOTE: if module-global logLUT=NULL, we create it here
- * NOTE: if argument is outside of (0,xmax], we use math-lib log(x) instead of LUT
- */
-REAL8
-XLALFastLog ( REAL8 x )
-{
-
-  if ( x > LOGLUT_XMAX ) { /* for values bigger than xmax, use normal log function */
-    return log(x);
-  }
-
-  XLAL_CHECK_REAL8 ( x >= 0, XLAL_EDOM, "Negative argument: x=%f.", x );
-
-  /* if lookup table doesn't exist yet: generate it now */
-  XLAL_CHECK_REAL8 ( logLUT || ( XLALCreateLogLUT() == XLAL_SUCCESS), XLAL_EFUNC, "Failed call to XLALCreateLogLUT()." );
-
-  /* find index of closest point xp in LUT to xm */
-  UINT4 i0 = (UINT4) ( x * LOGLUT_DXINV + 0.5 );
-
-  return gsl_vector_get ( logLUT, i0 );
-
-} /* XLALFastLog() */
-
-
-/**
- * Generate an exponential lookup-table expLUT for e^(-x)
- * over the interval x in [0, xmax], using 'length' points.
- */
-int
-XLALCreateExpLUT ( void )
-{
-  /* create empty output LUT */
-  gsl_vector *ret;
-  if ( ( ret = gsl_vector_alloc ( EXPLUT_LENGTH + 1)) == NULL ) {
-    XLALPrintError ("%s: failed to gsl_vector_alloc (%i)\n", __func__, EXPLUT_LENGTH +1 );
-    XLAL_ERROR ( XLAL_ENOMEM );
-  }
-
-  /* fill output LUT */
-  REAL8 dx = EXPLUT_XMAX / EXPLUT_LENGTH;
-  UINT4 i;
-  for ( i=0; i <= EXPLUT_LENGTH; i ++ )
-    {
-      REAL8 xi = i * dx;
-
-      gsl_vector_set ( ret, i, exp( - xi ) );
-
-    } /* for i < length() */
-
-  /* 'return' this by setting the global vector */
-  expLUT = ret;
-
-  return XLAL_SUCCESS;
-
-} /* XLALCreateExpLUT() */
-
-
-/**
- * Destructor function for expLUT_t lookup table
- */
-void
-XLALDestroyExpLUT ( void )
-{
-  if ( !expLUT )
-    return;
-
-  gsl_vector_free ( expLUT );
-
-  expLUT = NULL;
-
-  return;
-
-} /* XLALDestroyExpLUT() */
-
-
-/**
- * Fast exponential function e^-x using lookup-table (LUT).
- * We need to compute exp(-x) for x >= 0, typically in a B-stat
- * integral of the form int e^-x dx: this means that small values e^(-x)
- * will not contribute much to the integral and are less important than
- * values close to 1. Therefore we pre-compute a LUT of e^(-x) for x in [0, xmax],
- * in Npoints points, and set e^(-x) = 0 for x < xmax.
- *
- * NOTE: if module-global expLUT=NULL, we create it here
- * NOTE: if argument is negative, we use math-lib exp(-x) instead of LUT
- */
-REAL8
-XLALFastNegExp ( REAL8 mx )
-{
-  if ( mx > EXPLUT_XMAX )	/* for values smaller than e^(-xmax) we truncate to 0 */
-    return 0.0;
-
-  if ( mx < 0 )
-    return exp ( - mx  );
-
-  /* if lookup table doesn't exist yet: generate it now */
-  if ( !expLUT && ( XLALCreateExpLUT() != XLAL_SUCCESS) ) {
-    XLAL_ERROR_REAL8 ( XLAL_EFUNC );
-  }
-
-  /* find index of closest point xp in LUT to xm */
-  UINT4 i0 = (UINT4) ( mx * EXPLUT_DXINV + 0.5 );
-
-  return gsl_vector_get ( expLUT, i0 );
-
-} /* XLALFastNegExp() */

lalpulsar/src/ComplexAM.c
@@ -43,7 +43,7 @@
 #include <lal/AVFactories.h>
 #include <lal/ComplexAM.h>
 #include <lal/LISAspecifics.h>
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 
 /*---------- local DEFINES ----------*/
 #define TRUE (1==1)

lalpulsar/src/ComputeFstat.c
@@ -38,7 +38,7 @@
 #include <lal/LALComputeAM.h>
 #include <lal/ExtrapolatePulsarSpins.h>
 #include <lal/LFTandTSutils.h>
-#include <lal/CWFastMath.h>
+#include <lal/SinCosLUT.h>
 #include <lal/NormalizeSFTRngMed.h>
 
 // ----- macro definitions

lscsoft/lalsuite

commit 00ef4fb52e31a43bdf5d23132e9b453a00f41f00

TransientCW_utils.h: fixed swig error caused in 4a9645b3d1cc870 - refs #358 Original: 1e0845dd748db01e1c5e697f11a4bd63ae8c96b7

lalpulsar/src/TransientCW_utils.h
@@ -123,7 +123,6 @@ void XLALDestroyTransientFstatMap ( transientFstatMap_t *FstatMap );
 void XLALDestroyTransientCandidate ( transientCandidate_t *cand );
 
 REAL8 XLALFastNegExp ( REAL8 mx );
-REAL4 XLALFastExpf ( REAL4 x );
 void XLALDestroyExpLUT( void );
 
 /* ---------- Fstat-atoms related functions ----------*/