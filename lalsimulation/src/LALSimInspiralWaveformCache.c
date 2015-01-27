1c7b767fe7e6693dac527f84bae3fac2f5f03dcf
Michael Puerrer <Michael.Puerrer@ligo.org>
2015-01-27 23:40:53 +0000

    Add ROMs and IMRPhenomP to XLALSimInspiralChooseFDWaveformSequence() Original: d78c571087e8c6430a22b1221a7dcc2717c2ab92

--- lalsimulation/src/LALSimInspiralWaveformCache.c
+++ lalsimulation/src/LALSimInspiralWaveformCache.c
@@ -20,6 +20,7 @@
 #include <math.h>
 #include <LALSimInspiralWaveformCache.h>
 #include <lal/LALSimInspiral.h>
+#include <lal/LALSimIMR.h>
 #include <lal/FrequencySeries.h>
 #include <lal/Sequence.h>
 #include <lal/LALConstants.h>
@@ -887,39 +888,39 @@ static int StoreFDHCache(LALSimInspiralWaveformCache *cache,
 
 /**
  * Wrapper similar to XLALSimInspiralChooseFDWaveform() for waveforms to be generated a specific freqencies.
- * Only TaylorF2 implemented so far. See XLALSimInspiralTaylorF2Core().
  * Returns the waveform in the frequency domain at the frequencies of the REAL8Sequence frequencies.
  */
 int XLALSimInspiralChooseFDWaveformSequence(
-                                      COMPLEX16FrequencySeries **hptilde,     /**< FD plus polarization */
-                                      COMPLEX16FrequencySeries **hctilde,     /**< FD cross polarization */
-                                      REAL8 phiRef,                           /**< reference orbital phase (rad) */
-                                      REAL8 m1,                               /**< mass of companion 1 (kg) */
-                                      REAL8 m2,                               /**< mass of companion 2 (kg) */
-                                      REAL8 S1x,                              /**< x-component of the dimensionless spin of object 1 */
-                                      REAL8 S1y,                              /**< y-component of the dimensionless spin of object 1 */
-                                      REAL8 S1z,                              /**< z-component of the dimensionless spin of object 1 */
-                                      REAL8 S2x,                              /**< x-component of the dimensionless spin of object 2 */
-                                      REAL8 S2y,                              /**< y-component of the dimensionless spin of object 2 */
-                                      REAL8 S2z,                              /**< z-component of the dimensionless spin of object 2 */
-                                      REAL8 f_ref,                            /**< Reference frequency (Hz) */
-                                      REAL8 r,                                /**< distance of source (m) */
-                                      REAL8 i,                                /**< inclination of source (rad) */
-                                      REAL8 lambda1,                          /**< (tidal deformability of mass 1) / m1^5 (dimensionless) */
-                                      REAL8 lambda2,                          /**< (tidal deformability of mass 2) / m2^5 (dimensionless) */
-                                      LALSimInspiralWaveformFlags *waveFlags, /**< Set of flags to control special behavior of some waveform families. Pass in NULL (or None in python) for default flags */
-                                      LALSimInspiralTestGRParam *nonGRparams, /**< Linked list of non-GR parameters. Pass in NULL (or None in python) for standard GR waveforms */
-                                      int amplitudeO,                         /**< twice post-Newtonian amplitude order */
-                                      int phaseO,                             /**< twice post-Newtonian order */
-                                      Approximant approximant,                /**< post-Newtonian approximant to use for waveform production */
-                                      REAL8Sequence *frequencies              /**< sequence of frequencies for which the waveform will be computed. Pass in NULL (or None in python) for standard f_min to f_max sequence. */
+    COMPLEX16FrequencySeries **hptilde,     /**< FD plus polarization */
+    COMPLEX16FrequencySeries **hctilde,     /**< FD cross polarization */
+    REAL8 phiRef,                           /**< reference orbital phase (rad) */
+    REAL8 m1,                               /**< mass of companion 1 (kg) */
+    REAL8 m2,                               /**< mass of companion 2 (kg) */
+    REAL8 S1x,                              /**< x-component of the dimensionless spin of object 1 */
+    REAL8 S1y,                              /**< y-component of the dimensionless spin of object 1 */
+    REAL8 S1z,                              /**< z-component of the dimensionless spin of object 1 */
+    REAL8 S2x,                              /**< x-component of the dimensionless spin of object 2 */
+    REAL8 S2y,                              /**< y-component of the dimensionless spin of object 2 */
+    REAL8 S2z,                              /**< z-component of the dimensionless spin of object 2 */
+    REAL8 f_ref,                            /**< Reference frequency (Hz) */
+    REAL8 r,                                /**< distance of source (m) */
+    REAL8 i,                                /**< inclination of source (rad) */
+    REAL8 lambda1,                          /**< (tidal deformability of mass 1) / m1^5 (dimensionless) */
+    REAL8 lambda2,                          /**< (tidal deformability of mass 2) / m2^5 (dimensionless) */
+    LALSimInspiralWaveformFlags *waveFlags, /**< Set of flags to control special behavior of some waveform families. Pass in NULL (or None in python) for default flags */
+    LALSimInspiralTestGRParam *nonGRparams, /**< Linked list of non-GR parameters. Pass in NULL (or None in python) for standard GR waveforms */
+    int amplitudeO,                         /**< twice post-Newtonian amplitude order */
+    int phaseO,                             /**< twice post-Newtonian order */
+    Approximant approximant,                /**< post-Newtonian approximant to use for waveform production */
+    REAL8Sequence *frequencies              /**< sequence of frequencies for which the waveform will be computed. Pass in NULL (or None in python) for standard f_min to f_max sequence. */
 )
 {
     int ret;
     unsigned int j;
     REAL8 pfac, cfac;
     REAL8 quadparam1 = 1., quadparam2 = 1.; /* FIXME: This cannot yet be set in the interface */
-    
+    REAL8 LNhatx, LNhaty, LNhatz;
+
     /* General sanity checks that will abort
      *
      * If non-GR approximants are added, include them in
@@ -987,8 +988,98 @@ int XLALSimInspiralChooseFDWaveformSequence(
                 (*hptilde)->data->data[j] *= pfac;
             }
             break;
-            
-            
+
+        case SEOBNRv1_ROM_SingleSpin:
+            /* Waveform-specific sanity checks */
+            if( !XLALSimInspiralWaveformFlagsIsDefault(waveFlags) )
+                ABORT_NONDEFAULT_WAVEFORM_FLAGS(waveFlags);
+            if( !checkTransverseSpinsZero(S1x, S1y, S2x, S2y) )
+                ABORT_NONZERO_TRANSVERSE_SPINS(waveFlags);
+            if( !checkTidesZero(lambda1, lambda2) )
+                ABORT_NONZERO_TIDES(waveFlags);
+            if( f_ref != 0.)
+                XLALPrintWarning("XLAL Warning - %s: This approximant does not use f_ref. The reference phase will be defined at coalescence.\n", __func__);
+
+            ret = XLALSimIMRSEOBNRv1ROMSingleSpinFrequencySequence(hptilde, hctilde, frequencies,
+                    phiRef, f_ref, r, i, m1, m2, XLALSimIMRPhenomBComputeChi(m1, m2, S1z, S2z));
+            break;
+
+        case SEOBNRv1_ROM_DoubleSpin:
+            /* Waveform-specific sanity checks */
+            if( !XLALSimInspiralWaveformFlagsIsDefault(waveFlags) )
+                ABORT_NONDEFAULT_WAVEFORM_FLAGS(waveFlags);
+            if( !checkTransverseSpinsZero(S1x, S1y, S2x, S2y) )
+                ABORT_NONZERO_TRANSVERSE_SPINS(waveFlags);
+            if( !checkTidesZero(lambda1, lambda2) )
+                ABORT_NONZERO_TIDES(waveFlags);
+            if( f_ref != 0.)
+                XLALPrintWarning("XLAL Warning - %s: This approximant does not use f_ref. The reference phase will be defined at coalescence.\n", __func__);
+
+            ret = XLALSimIMRSEOBNRv1ROMDoubleSpinFrequencySequence(hptilde, hctilde, frequencies,
+                    phiRef, f_ref, r, i, m1, m2, S1z, S2z);
+            break;
+
+        case SEOBNRv2_ROM_SingleSpin:
+            /* Waveform-specific sanity checks */
+            if( !XLALSimInspiralWaveformFlagsIsDefault(waveFlags) )
+                ABORT_NONDEFAULT_WAVEFORM_FLAGS(waveFlags);
+            if( !checkTransverseSpinsZero(S1x, S1y, S2x, S2y) )
+                ABORT_NONZERO_TRANSVERSE_SPINS(waveFlags);
+            if( !checkTidesZero(lambda1, lambda2) )
+                ABORT_NONZERO_TIDES(waveFlags);
+            if( f_ref != 0.)
+                XLALPrintWarning("XLAL Warning - %s: This approximant does not use f_ref. The reference phase will be defined at coalescence.\n", __func__);
+
+            ret = XLALSimIMRSEOBNRv2ROMSingleSpinFrequencySequence(hptilde, hctilde, frequencies,
+                    phiRef, f_ref, r, i, m1, m2, XLALSimIMRPhenomBComputeChi(m1, m2, S1z, S2z));
+            break;
+
+        case SEOBNRv2_ROM_DoubleSpin:
+            /* Waveform-specific sanity checks */
+            if( !XLALSimInspiralWaveformFlagsIsDefault(waveFlags) )
+                ABORT_NONDEFAULT_WAVEFORM_FLAGS(waveFlags);
+            if( !checkTransverseSpinsZero(S1x, S1y, S2x, S2y) )
+                ABORT_NONZERO_TRANSVERSE_SPINS(waveFlags);
+            if( !checkTidesZero(lambda1, lambda2) )
+                ABORT_NONZERO_TIDES(waveFlags);
+            if( f_ref != 0.)
+                XLALPrintWarning("XLAL Warning - %s: This approximant does not use f_ref. The reference phase will be defined at coalescence.\n", __func__);
+
+            ret = XLALSimIMRSEOBNRv2ROMDoubleSpinFrequencySequence(hptilde, hctilde, frequencies,
+                    phiRef, f_ref, r, i, m1, m2, S1z, S2z);
+            break;
+
+        /* inspiral-merger-ringdown models */
+        case IMRPhenomP:
+            /* Waveform-specific sanity checks */
+            if( !XLALSimInspiralFrameAxisIsDefault(
+                    XLALSimInspiralGetFrameAxis(waveFlags) ) ) /* Default is LAL_SIM_INSPIRAL_FRAME_AXIS_VIEW : z-axis along direction of GW propagation (line of sight). */
+                ABORT_NONDEFAULT_FRAME_AXIS(waveFlags);
+            if( !XLALSimInspiralModesChoiceIsDefault(          /* Default is (2,2) or l=2 modes. */
+                    XLALSimInspiralGetModesChoice(waveFlags) ) )
+                ABORT_NONDEFAULT_MODES_CHOICE(waveFlags);
+            if( !checkTidesZero(lambda1, lambda2) )
+                ABORT_NONZERO_TIDES(waveFlags);
+            LNhatx = sin(i);
+            LNhaty = 0.;
+            LNhatz = cos(i);
+            /* Tranform to model parameters */
+            REAL8 chi_eff, chip, eta, thetaJ, alpha0;
+            if(f_ref==0.0)
+                f_ref = f_min; /* Default reference frequency is minimum frequency */
+            XLALSimIMRPhenomPCalculateModelParameters(
+                &chi_eff, &chip, &eta, &thetaJ, &alpha0,
+                m1, m2, f_ref,
+                LNhatx, LNhaty, LNhatz,
+                S1x, S1y, S1z,
+                S2x, S2y, S2z);
+            /* Call the waveform driver routine */
+            ret = XLALSimIMRPhenomPFrequencySequence(hptilde, hctilde, frequencies,
+              chi_eff, chip, eta, thetaJ,
+              m1+m2, r, alpha0, phiRef, f_ref);
+            if (ret == XLAL_FAILURE) XLAL_ERROR(XLAL_EFUNC);
+            break;
+
         default:
             XLALPrintError("FD version of approximant not implemented in lalsimulation\n");
             XLAL_ERROR(XLAL_EINVAL);
