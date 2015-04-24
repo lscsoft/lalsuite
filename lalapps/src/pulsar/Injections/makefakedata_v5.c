7d713953a225fdef8ff7277842d218cfe607d6f2
Reinhard Prix <reinhard.prix@ligo.org>
2015-04-24 12:29:07 +0200

    makefakedata_v5: --injectionSources: forgot to update help-string with new input format - help string and in-code comment hadn't been updated to the new input  format convention introduced in  refs #1102 Original: cb84f91847380d0a263ab8e59738714b695c6a4b

--- lalapps/src/pulsar/Injections/makefakedata_v5.c
+++ lalapps/src/pulsar/Injections/makefakedata_v5.c
@@ -131,7 +131,7 @@ typedef struct
   CHAR *ephemSun;		/**< Sun ephemeris file to use */
 
   /* pulsar parameters */
-  LALStringVector *injectionSources;	///< either a file-specification ("@file-pattern") or a config-string defining the sources to inject
+  LALStringVector *injectionSources;	///< Source parameters to inject: comma-separated list of file-patterns and/or direct config-strings ('{...}')
 
   BOOLEAN version;		/**< output version information */
 
@@ -509,7 +509,7 @@ XLALInitUserVars ( UserVariables_t *uvar, int argc, char *argv[] )
   XLALregREALUserStruct (  SFTWindowBeta,        0, UVAR_OPTIONAL, "Window 'beta' parameter required for a few window-types (eg. 'tukey')");
 
   /* pulsar params */
-  XLALregLISTUserStruct( injectionSources,     0, UVAR_OPTIONAL, "Either a file-specification (\"@file-pattern\") or a config-string defining the sources to inject" );
+  XLALregLISTUserStruct( injectionSources,     0, UVAR_OPTIONAL,  "Source parameters to inject: comma-separated list of file-patterns and/or direct config-strings ('{...}')" );
 
   /* noise */
   XLALregSTRINGUserStruct ( noiseSFTs,          'D', UVAR_OPTIONAL, "Noise-SFTs to be added to signal (Used also to set IFOs and timestamps)");
