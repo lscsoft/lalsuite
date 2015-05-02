5367cd66841304c0b18e64cfee1a52ae0f7dda00
Karl Wette <karl.wette@ligo.org>
2015-05-02 12:57:37 +0200

    SuperskyMetricsTest: do not cast away constness when passing reference arrays - Picked up by UWM Jenkings nightly clang 3.6 build Original: ef106c10860440d34f661a1cea13c99f6378b91c

--- lalpulsar/test/SuperskyMetricsTest.c
+++ lalpulsar/test/SuperskyMetricsTest.c
@@ -141,12 +141,12 @@ int main( void )
 
   // Check supersky metrics
   {
-    gsl_matrix_const_view ussky_metric_ref_view = gsl_matrix_const_view_array( (double*)ussky_metric_ref, 5, 5 );
+    gsl_matrix_const_view ussky_metric_ref_view = gsl_matrix_const_view_array( (const double*)ussky_metric_ref, 5, 5 );
     const double err = XLALCompareMetrics( ussky_metric, &ussky_metric_ref_view.matrix ), err_tol = 5e-9;
     XLAL_CHECK_MAIN( err <= err_tol, XLAL_ETOL, "'ussky_metric' check failed: err = %0.3e > %0.3e = err_tol", err, err_tol );
   }
   {
-    gsl_matrix_const_view rssky_metric_ref_view = gsl_matrix_const_view_array( (double*)rssky_metric_ref, 4, 4 );
+    gsl_matrix_const_view rssky_metric_ref_view = gsl_matrix_const_view_array( (const double*)rssky_metric_ref, 4, 4 );
     const double err = XLALCompareMetrics( rssky_metric, &rssky_metric_ref_view.matrix ), err_tol = 5e-9;
     XLAL_CHECK_MAIN( err <= err_tol, XLAL_ETOL, "'rssky_metric' check failed: err = %0.3e > %0.3e = err_tol", err, err_tol );
   }
