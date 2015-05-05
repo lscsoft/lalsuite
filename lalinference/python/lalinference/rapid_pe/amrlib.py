b3255c92d35b3ad5060e7837f1cbbd6743f4b79e
Chris Pankow <chris.pankow@ligo.org>
2015-05-05 12:21:58 -0400

    amrlib.py: Bug fixes     - Import fixing     - Fixed the tau0tau3 conversion functions         - syntax was wrong, prefactor was wrong, and the mass needs to be dimensionless Original: 201af072c02115a566f7cd4ec070fe9dad6cf730

--- lalinference/python/lalinference/rapid_pe/amrlib.py
+++ lalinference/python/lalinference/rapid_pe/amrlib.py
@@ -1,7 +1,7 @@
 import numpy
-import lalsimutils
+import lal
+from lalinference.rapid_pe import lalsimutils
 m1m2 = numpy.vectorize(lalsimutils.m1m2)
-Mceta = numpy.vectorize(lalsimutils.Mceta)
 
 def midpoint(pt1, pt2):
     diff = pt2 - pt1
@@ -12,25 +12,26 @@ def prune_duplicate_pts(pts):
     return numpy.array(list(set([tuple(pt) for pt in pts])))
 
 def transform_m1m2_mceta(m1, m2):
-    return Mceta(m1, m2)
+    return lalsimutils.Mceta(m1, m2)
 
 def transform_mceta_m1m2(mc, eta):
     return m1m2(mc, eta)
 
 __prefac_0 = 5. / 256 / numpy.pi
-__prefac_3 = 1. / 8 / numpy.pi
+__prefac_3 = 1. / 8
+__dim_mass = lal.G_SI / lal.C_SI**3 * lal.MSUN_SI
 def transform_m1m2_tau0tau3(m1, m2, flow=40.):
     mt = m1 + m2
     eta = m1 * m2 / mt**2
-    mt *= numpy.pi * flow
+    mt *= numpy.pi * flow * __dim_mass
     return (__prefac_0 / flow / eta * mt**(-5./3), __prefac_3 / flow / eta * mt**(-2./3))
 
 __prefac_tau = 5. / 32 / numpy.pi
 def transform_tau0tau3_m1m2(tau0, tau3, flow=40.):
-    tau0, tau3 = pts
-    mt = __prefac_tau / flow / numpy.pi * tau3 / tau0
-    eta = 1.0 / 8 / flow / tau3 * (__prefac_tau * tau0 / tau3)**(2./3)
-    return lalsimutils.m1m2(mt, eta)
+    mt = __prefac_tau / numpy.pi / flow * tau3 / tau0
+    eta = 1.0 / 8 / flow / tau3 * (tau0 / __prefac_tau / tau3)**(2./3)
+    m1, m2 = m1m2(mt*eta**(3./5), eta)
+    return m1 / __dim_mass, m2 / __dim_mass
 
 VALID_TRANSFORMS_MASS = { \
     "mchirp_eta": transform_m1m2_mceta,
