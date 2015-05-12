855d04d3718f09303fa63de0a55804d859591add
Karl Wette <karl.wette@ligo.org>
2015-05-12 22:00:46 +0200

    lalframe/src/stat.c: use _BSD_SOURCE to get realpath() - Refs #2097 Original: bf751ceca8df283710a24ec65460aacd890f8044

--- lalframe/src/stat.c
+++ lalframe/src/stat.c
@@ -17,10 +17,13 @@
 *  MA  02111-1307  USA
 */
 
+#define _BSD_SOURCE   /* for realpath() */
+#include <stdlib.h>
+#undef _BSD_SOURCE
+
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <lal/LALFrameU.h>
 
