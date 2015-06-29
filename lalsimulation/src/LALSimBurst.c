b736c5e2b244d2b5a19df30f273d3cd6ec322251
Kipp Cannon <kipp.cannon@ligo.org>
2015-06-29 17:28:17 -0400

    LALSimBurst.c: correct copyright from git log entries Original: ae4371f1df3f020fb5b10b1ded5c5501224b01d7

--- lalsimulation/src/LALSimBurst.c
+++ lalsimulation/src/LALSimBurst.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 J. Creighton, K. Cannon
+ * Copyright (C) 2007--2015 J. Creighton, K. Cannon, K. Wette, R. Prix, A. Mercer
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
