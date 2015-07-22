6f08a2119532ae3c12c6263248319985a5cc30ee
Salvatore Vitale <salvatore.vitale@ligo.org>
2015-07-22 08:34:47 -0700

    Added gaussian prior CE options to the help Original: e1f2a35ee2346e8d4da5388d09f25090fbe8181c

--- lalinference/src/LALInferenceCalibrationErrors.c
+++ lalinference/src/LALInferenceCalibrationErrors.c
@@ -315,6 +315,8 @@ void LALInferenceApplyCalibrationErrors(LALInferenceRunState *state, ProcessPara
  * Constant Calibration Model \n\
   (--MarginalizeConstantCalAmp ) If given, will add a constant value of Amplitude CE per each IFO on the top of the CBC parameters.\n\
   (--MarginalizeConstantCalPha ) If given, will add a constant value of Phase CE per each IFO on the top of the CBC parameters.\n\
+  (--constcal_ampsigma ) If given, will use gaussian prior on the constant amplitude error with this sigma (e.g. 0.05=5%) .\n\
+  (--constcal_phasigma ) If given, will use gaussian prior on the constant phase error with this sigma (e.g. 5=5degs).\n\
  * Spline Calibration Model \n\
   (--enable-spline-calibration)            Enable cubic-spline calibration error model.\n\
   (--spcal-nodes N)           Set the number of spline nodes per detector (default 5)\n\
