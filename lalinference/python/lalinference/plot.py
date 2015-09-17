4c3cc6d016ee5c9165b3ef4de336ad0af5255f36
Leo Singer <leo.singer@nasa.gov>
2015-09-17 16:48:07 -0400

    Explicitly set origin='upper' in sky maps Otherwise, users with customized matplotlibrc files might get upside-down sky maps. Thanks to Will Farr for catching this. Original: 2990f1f45e8ad95c3361bec0db466713ed158663

--- lalinference/python/lalinference/plot.py
+++ lalinference/python/lalinference/plot.py
@@ -493,7 +493,8 @@ def heatmap(func, *args, **kwargs):
 
     # Plot bitmap using imshow.
     aximg = plt.imshow(zz.reshape(xx.shape), aspect=ax.get_aspect(),
-        extent=(xmin, xmax, ymax, ymin), *args, **kwargs)
+        origin='upper', extent=(xmin, xmax, ymax, ymin),
+        *args, **kwargs)
 
     # Hide masked-out values by displaying them in transparent white.
     aximg.cmap.set_bad('w', alpha=0.)
