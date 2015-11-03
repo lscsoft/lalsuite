41f62ea0910171a5971181ac952c9041fa9ac57b
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-11-03 14:25:49 +0000

    ppe_likelihood: allow use of a Fermi-Dirac prior distribution for parameters Original: 6ed00749e184186ad7c5877ba076a1fe989a11bc

--- lalapps/src/pulsar/HeterodyneSearch/ppe_likelihood.c
+++ lalapps/src/pulsar/HeterodyneSearch/ppe_likelihood.c
@@ -492,6 +492,14 @@ REAL8 priorFunction( LALInferenceRunState *runState, LALInferenceVariables *para
           if ( !strcmp(item->name, "C22") ){ C22 = value; }
         }
       }
+      else if( LALInferenceCheckFermiDiracPrior(runState->priorArgs, item->name) ){
+        REAL8 r = 0., sigma = 0.;
+        LALInferenceGetFermiDiracPrior(runState->priorArgs, item->name, &sigma, &r);
+
+        value = (*(REAL8 *)item->value);
+        if ( value < 0. ) { return -DBL_MAX; } /* value must be positive */
+        prior += LALInferenceFermiDiracPrior(value, sigma, r);
+      }
       else if( LALInferenceCheckCorrelatedPrior(runState->priorArgs, item->name) && corlist ){
         /* set item in correct position given the order of the correlation matrix given by corlist */
         REAL8 mu = 0., sigma = 0.;
