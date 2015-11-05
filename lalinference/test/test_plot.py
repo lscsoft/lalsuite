8d596e67f780ecee7bb155f463dd25900d7d1825
Leo Singer <leo.singer@ligo.org>
2015-11-04 23:38:56 -0500

    Restore unit tests: test_plot.py Original: dc75d1c5eb5557427c66bb7a0adc572f0ba2fa6f

--- lalinference/test/test_plot.py
+++ lalinference/test/test_plot.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import matplotlib
+matplotlib.use('agg')
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cbook
@@ -8,15 +10,6 @@ import unittest
 import os
 
 import lalinference.plot
-
-# FIXME: remove when we can depend on Scipy >= 0.8 on SL clusters.
-from scipy.stats import beta
-try:
-    beta.interval
-except AttributeError:
-    scipy_stats_has_interval = False
-else:
-    scipy_stats_has_interval = True
 
 
 def image_comparison(testfunc, filename=None, tolerance=1):
@@ -57,50 +50,51 @@ def image_comparison(testfunc, filename=None, tolerance=1):
 
 
 
-class TestPlot(unittest.TestCase):
+# FIXME: remove try/except/else when we get Scipy >= 0.8 on SL clusters.
+from scipy.stats import beta
+try:
+    beta.interval
+except AttributeError:
+    pass
+else:
+    class TestPlot(unittest.TestCase):
 
-    def setUp(self):
-        # Re-initialize the random seed to make the unit test repeatable
-        np.random.seed(0)
-        self.fig = plt.figure(figsize=(3, 3), dpi=72)
-        self.ax = self.fig.add_subplot(111, projection='pp_plot')
-        # self.ax = lalinference.plot.PPPlot(self.fig, [0.1, 0.1, 0.8, 0.8])
-        # self.fig.add_axes(self.ax)
-        self.p_values = np.arange(1, 20) / 20
+        def setUp(self):
+            # Re-initialize the random seed to make the unit test repeatable
+            np.random.seed(0)
+            self.fig = plt.figure(figsize=(3, 3), dpi=72)
+            self.ax = self.fig.add_subplot(111, projection='pp_plot')
+            # self.ax = lalinference.plot.PPPlot(self.fig, [0.1, 0.1, 0.8, 0.8])
+            # self.fig.add_axes(self.ax)
+            self.p_values = np.arange(1, 20) / 20
 
-    # FIXME: don't skip test once we can depend on Scipy >= 0.8 on SL clusters.
-    @unittest.skipUnless(scipy_stats_has_interval, 'requires Scipy >= 0.8')
-    @image_comparison
-    def test_pp_plot_steps(self):
-        """Test P--P plot with drawstyle='steps'."""
-        self.ax.add_confidence_band(len(self.p_values))
-        self.ax.add_diagonal()
-        self.ax.add_lightning(len(self.p_values), 20, drawstyle='steps')
-        self.ax.add_series(self.p_values, drawstyle='steps')
-        return self.fig
+        @image_comparison
+        def test_pp_plot_steps(self):
+            """Test P--P plot with drawstyle='steps'."""
+            self.ax.add_confidence_band(len(self.p_values))
+            self.ax.add_diagonal()
+            self.ax.add_lightning(len(self.p_values), 20, drawstyle='steps')
+            self.ax.add_series(self.p_values, drawstyle='steps')
+            return self.fig
 
-    # FIXME: don't skip test once we can depend on Scipy >= 0.8 on SL clusters.
-    @unittest.skipUnless(scipy_stats_has_interval, 'requires Scipy >= 0.8')
-    @image_comparison
-    def test_pp_plot_lines(self):
-        """Test P--P plot with drawstyle='steps'."""
-        self.ax.add_confidence_band(len(self.p_values))
-        self.ax.add_diagonal()
-        self.ax.add_lightning(len(self.p_values), 20, drawstyle='lines')
-        self.ax.add_series(self.p_values, drawstyle='lines')
-        self.ax.add_diagonal()
-        return self.fig
+        @image_comparison
+        def test_pp_plot_lines(self):
+            """Test P--P plot with drawstyle='steps'."""
+            self.ax.add_confidence_band(len(self.p_values))
+            self.ax.add_diagonal()
+            self.ax.add_lightning(len(self.p_values), 20, drawstyle='lines')
+            self.ax.add_series(self.p_values, drawstyle='lines')
+            self.ax.add_diagonal()
+            return self.fig
 
-    # FIXME: don't skip test once we can depend on Scipy >= 0.8 on SL clusters.
-    @unittest.skipUnless(scipy_stats_has_interval, 'requires Scipy >= 0.8')
-    @image_comparison
-    def test_pp_plot_default(self):
-        """Test P--P plot with drawstyle='steps'."""
-        self.ax.add_confidence_band(len(self.p_values))
-        self.ax.add_diagonal()
-        self.ax.add_lightning(len(self.p_values), 20)
-        self.ax.add_series(self.p_values)
-        return self.fig
+        @image_comparison
+        def test_pp_plot_default(self):
+            """Test P--P plot with drawstyle='steps'."""
+            self.ax.add_confidence_band(len(self.p_values))
+            self.ax.add_diagonal()
+            self.ax.add_lightning(len(self.p_values), 20)
+            self.ax.add_series(self.p_values)
+            return self.fig
 
 
 if __name__ == '__main__':
