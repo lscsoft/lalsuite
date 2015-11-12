3b279ad13728e77831fad1d8d33f3624eb010003
Stanislav Babak <Stanislav.Babak@aei.mpg.de>
2015-11-12 17:27:29 +0100

    Added useful comment in the debug mode: initial ratio across the RD attachment time Original: d132ea28bccf60d9facceaf395776eee221372d9

--- lalsimulation/src/LALSimIMRSpinPrecEOB.c
+++ lalsimulation/src/LALSimIMRSpinPrecEOB.c
@@ -3722,7 +3722,12 @@ int XLALSimIMRSpinEOBWaveformAll(
 
       if (pass == 0){
 
-           if (debugPK) XLAL_PRINT_INFO("Adjusting RD attachment point... \n");fflush(NULL);
+           if (debugPK){
+               XLAL_PRINT_INFO("Adjusting RD attachment point... \n");fflush(NULL);
+               XLAL_PRINT_INFO("initial ratios: %f,  %f \n", ratio22,  ratio2m2);
+               fflush(NULL);
+           }
+               
            memset( sigReHi->data, 0, sigReHi->length * sizeof( sigReHi->data[0] ));
            memset( sigImHi->data, 0, sigImHi->length * sizeof( sigImHi->data[0] ));
 
