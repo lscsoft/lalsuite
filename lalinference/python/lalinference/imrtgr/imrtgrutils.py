849c9947c5088f788e0954606107607fd0670d3c
Archisman Ghosh <archisman.ghosh@ligo.org>
2015-11-30 12:47:25 +0530

    Changed all af --> chif. Note that this changes the name of the pklz file. Changed integrant --> integrand. Original: 098f8ec5776ad1e0d86c16262408a8c00d5eea46

--- lalinference/python/lalinference/imrtgr/imrtgrutils.py
+++ lalinference/python/lalinference/imrtgr/imrtgrutils.py
@@ -30,35 +30,35 @@ def calc_final_mass_spin(m1, m2, chi1, chi2, fit_formula):
     exit()
   return mf, chif
 
-""" compute the integrant of P(dMf/Mf, daf/chif). """
-def P_integrant(chif, Mf, v1, v2, P_dMfdaf_interp_object, P_Mfaf_imr_interp_object):
+""" compute the integrand of P(dMf/Mf, dchif/chif). """
+def P_integrand(chif, Mf, v1, v2, P_dMfdchif_interp_object, P_Mfchif_imr_interp_object):
 
-  Mf_mat, af_mat = np.meshgrid(Mf, chif)
+  Mf_mat, chif_mat = np.meshgrid(Mf, chif)
 
-  # Create dMf and daf vectors corresponding to the given v1 and v2. These vectors have to be 
+  # Create dMf and dchif vectors corresponding to the given v1 and v2. These vectors have to be 
   # monotonically increasing in order to evaluate the interpolated prob densities. Hence, for 
   # v1, v2 < 0, flip them, evaluate the prob density (in column or row) and flip it back 
   dMf = v1*Mf
-  daf = v2*chif
+  dchif = v2*chif
 
   if v1 < 0.:
     dMf = np.flipud(dMf)
   if v2 < 0.:
-    daf = np.flipud(daf)
-  P_delta = P_dMfdaf_interp_object(dMf, daf)
+    dchif = np.flipud(dchif)
+  P_delta = P_dMfdchif_interp_object(dMf, dchif)
 
   if v1 < 0.:
     P_delta = np.fliplr(P_delta)
   if v2 < 0.:
     P_delta = np.flipud(P_delta)
 
-  P_imr = P_Mfaf_imr_interp_object(Mf, chif)
-  return P_imr*P_delta*abs(Mf_mat*af_mat), P_imr, P_delta
+  P_imr = P_Mfchif_imr_interp_object(Mf, chif)
+  return P_imr*P_delta*abs(Mf_mat*chif_mat), P_imr, P_delta
 
-""" compute P(dMf/Mf, daf/chif). """
-def calc_sum(Mf, chif, v1, v2, P_dMfdaf_interp_object, P_Mfaf_imr_interp_object):
+""" compute P(dMf/Mf, dchif/chif). """
+def calc_sum(Mf, chif, v1, v2, P_dMfdchif_interp_object, P_Mfchif_imr_interp_object):
 
-  Pintg, P_imr, P_delta = P_integrant(chif, Mf, v1, v2, P_dMfdaf_interp_object, P_Mfaf_imr_interp_object)
+  Pintg, P_imr, P_delta = P_integrand(chif, Mf, v1, v2, P_dMfdchif_interp_object, P_Mfchif_imr_interp_object)
   return np.sum(Pintg)
 
 """ gaussian filter of histogram """
@@ -66,7 +66,7 @@ def gf(P):
   return filter.gaussian_filter(P, sigma=2.0)
 
 """ generate prior samples in (Mf, chif) assuming uniform prior in component masses """
-def calc_Mfaf_prior_samples(comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, af_bins, fit_formula, N_sampl, thread):
+def calc_Mfchif_prior_samples(comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, chif_bins, fit_formula, N_sampl, thread):
 
   # generate random samples of the prior uniform in component masses 
   m1_pr = np.random.uniform(comp_mass_prior_min, comp_mass_prior_max, N_sampl)
@@ -78,13 +78,13 @@ def calc_Mfaf_prior_samples(comp_mass_prior_min, comp_mass_prior_max, comp_spin_
   return calc_final_mass_spin(m1_pr, m2_pr, chi1_pr, chi2_pr, fit_formula)
 
 """ calculate the prior distribution in (Mf, chif) assuming uniform prior in component masses """
-def calc_Mfaf_prior(comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, af_bins, fit_formula, N_sampl, num_threads):
+def calc_Mfchif_prior(comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, chif_bins, fit_formula, N_sampl, num_threads):
 
   # generate samples in Mf and chif corresponding to uniform samples in m1, m2. Parallelise the calculation over multiple threads 
   thread_vec = range(num_threads)
   N_sampl = int(N_sampl/num_threads)
   p = Pool(num_threads)
-  func = partial(calc_Mfaf_prior_samples, comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, af_bins, fit_formula, N_sampl)
+  func = partial(calc_Mfchif_prior_samples, comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, comp_spin_max, Mf_bins, chif_bins, fit_formula, N_sampl)
   final_params = p.map(func, thread_vec)
   p.close()
   p.join()
@@ -92,9 +92,9 @@ def calc_Mfaf_prior(comp_mass_prior_min, comp_mass_prior_max, comp_spin_min, com
   final_params = np.array(final_params)
   final_params.reshape(N_sampl*num_threads, 2)
   Mf_pr = np.ravel(final_params[:,0])
-  af_pr = np.ravel(final_params[:,1])
+  chif_pr = np.ravel(final_params[:,1])
 
   # compute the 2D prior distribution in Mf and chif 
-  P_Mfaf_pr, Mf_bins, af_bins = np.histogram2d(Mf_pr, af_pr, bins=(Mf_bins, af_bins), normed=True)
+  P_Mfchif_pr, Mf_bins, chif_bins = np.histogram2d(Mf_pr, chif_pr, bins=(Mf_bins, chif_bins), normed=True)
 
-  return P_Mfaf_pr.T
+  return P_Mfchif_pr.T
