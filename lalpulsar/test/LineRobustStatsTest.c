2155e72b80915cd5245c01b4a205ac6210147184
David Keitel <david.keitel@ligo.org>
2015-12-01 16:45:22 +0100

    LineRobustStatsTest: put pedestrian computation into a function  -refs #2533 Original: 28f09e71282970dbdbb6c375cb63dc10ff6b0538

--- lalpulsar/test/LineRobustStatsTest.c
+++ lalpulsar/test/LineRobustStatsTest.c
@@ -41,6 +41,15 @@ XLALCompareBSGLComputations ( const REAL4 TwoF,
 			     const REAL4 tolerance
 			   );
 
+REAL4
+XLALComputeBSGLPedestrian ( const REAL4 TwoF,
+			    const UINT4 numDetectors,
+			    const REAL4Vector *TwoFX,
+			    const REAL4 Fstar0,
+			    const REAL4 *oLGX,
+			    const BOOLEAN useLogCorrection
+			   );
+
 int
 XLALCheckBSGLDifferences ( const REAL4 diff,
 			  const REAL4 tolerance,
@@ -107,49 +116,9 @@ XLALCompareBSGLComputations ( const REAL4 TwoF,			/**< multi-detector  Fstat */
                           )
 {
 
-  /* need overall prior odds for pedestrian formular */
-  REAL4 oLG = 0.0;
-  if ( oLGX ) {
-    for ( UINT4 X = 0; X < numDetectors; X++ ) {
-      oLG += oLGX[X];
-    }
-  }
-  else { /* if oLGX == NULL, assume oLGX=1/numDetectors for all X  ==> oLG = sumX oLGX = 1*/
-    oLG = 1.0;
-  } // if ( oLGX == NULL )
-
-  /* compute BSGL "the pedestrian way", from Eq. (40) of Keitel, Prix, Papa, Leaci, Siddiqi, PR D 89, 064023 (2014),
-   * explicit formula for numDet=2:
-   * log10 BSGL = F - log ( e^F* + e^{F1}*oLG1/oLG + e^{F2}*oLG2/oLG )
-   */
-  REAL4 BSGL_extcomp_terms[3];
-  BSGL_extcomp_terms[0] = exp(Fstar0)/oLG;
-  REAL4 BSGL_extcomp_maxterm = BSGL_extcomp_terms[0];
-  for (UINT4 X = 0; X < numDetectors; X++) {
-    BSGL_extcomp_terms[1+X] = exp(0.5*TwoFX->data[X]);
-    if ( oLGX ) {
-      BSGL_extcomp_terms[1+X] *= oLGX[X]/oLG;
-    }
-    else {  /* oLGX=NULL is interpreted as oLGX[X]=1/numDetectors=0.5 for all X ==> oLG=1 */
-      BSGL_extcomp_terms[1+X] *= 0.5/oLG;
-    }
-    if ( BSGL_extcomp_terms[1+X] > BSGL_extcomp_maxterm ) {
-      BSGL_extcomp_maxterm = BSGL_extcomp_terms[1+X];
-    }
-  }
-  REAL4 log10BSGL_extcomp_notallterms = 0.5*TwoF - log(BSGL_extcomp_maxterm);
-  REAL4 BSGL_extcomp_denom = 0.0;
-  for (UINT4 X = 0; X < 1+numDetectors; X++) {
-    BSGL_extcomp_denom += BSGL_extcomp_terms[X];
-  }
-  REAL4 log10BSGL_extcomp_allterms = 0.5*TwoF - log( BSGL_extcomp_denom );
-
-  /* these are not the log-Bayes-factor, as computed by XLALComputeBSGL(), so need to correct by log(1+1/oLG) */
-  log10BSGL_extcomp_allterms    += log(1+1/oLG);
-  log10BSGL_extcomp_notallterms += log(1+1/oLG);
-  /* and actually switch to log10 */
-  log10BSGL_extcomp_allterms    *= LAL_LOG10E;
-  log10BSGL_extcomp_notallterms *= LAL_LOG10E;
+  /* pedestrian version, with and without log corrections
+  REAL4 log10BSGL_extcomp_notallterms = XLALComputeBSGLPedestrian ( TwoF, numDetectors, TwoFX, Fstar0, oLGX, FALSE );
+  REAL4 log10BSGL_extcomp_allterms    = XLALComputeBSGLPedestrian ( TwoF, numDetectors, TwoFX, Fstar0, oLGX, TRUE );
 
   /* faster version: use only the leading term of the BSGL denominator sum */
   UINT4 numSegs = 1;
@@ -182,6 +151,69 @@ XLALCompareBSGLComputations ( const REAL4 TwoF,			/**< multi-detector  Fstat */
 
 } /* XLALCompareBSGLComputations() */
 
+/**
+ * compute BSGL "the pedestrian way", from Eq. (40) of Keitel, Prix, Papa, Leaci, Siddiqi, PR D 89, 064023 (2014),
+ * explicit formula for numDet=2:
+ * log10 BSGL = F - log ( e^F* + e^{F1}*oLG1/oLG + e^{F2}*oLG2/oLG )
+ */
+REAL4
+XLALComputeBSGLPedestrian ( const REAL4 TwoF,			/**< multi-detector  Fstat */
+			    const UINT4 numDetectors,		/**< number of detectors */
+			    const REAL4Vector *TwoFX,		/**< vector of single-detector Fstats */
+			    const REAL4 Fstar0,			/**< amplitude prior normalization for lines */
+			    const REAL4 *oLGX,			/**< array of single-detector prior line odds ratio, can be NULL */
+			    const BOOLEAN useLogCorrection	//!< include log-term correction or not
+                          )
+{
+
+  REAL4 log10BSGL_extcomp = 0;
+
+  /* sum up overall prior odds */
+  REAL4 oLG = 0.0;
+  if ( oLGX ) {
+    for ( UINT4 X = 0; X < numDetectors; X++ ) {
+      oLG += oLGX[X];
+    }
+  }
+  else { /* if oLGX == NULL, assume oLGX=1/numDetectors for all X  ==> oLG = sumX oLGX = 1*/
+    oLG = 1.0;
+  } // if ( oLGX == NULL )
+
+  REAL4 denomterms[3];
+  denomterms[0] = exp(Fstar0)/oLG;
+  REAL4 maxterm = denomterms[0];
+  for (UINT4 X = 0; X < numDetectors; X++) {
+    denomterms[1+X] = exp(0.5*TwoFX->data[X]);
+    if ( oLGX ) {
+      denomterms[1+X] *= oLGX[X]/oLG;
+    }
+    else {  /* oLGX=NULL is interpreted as oLGX[X]=1/numDetectors=0.5 for all X ==> oLG=1 */
+      denomterms[1+X] *= 0.5/oLG;
+    }
+    if ( denomterms[1+X] > maxterm ) {
+      maxterm = denomterms[1+X];
+    }
+  }
+
+  if ( !useLogCorrection ) {
+    log10BSGL_extcomp = 0.5*TwoF - log(maxterm);
+  }
+  else {
+    REAL4 BSGL_extcomp_denom = 0.0;
+    for (UINT4 X = 0; X < 1+numDetectors; X++) {
+      BSGL_extcomp_denom += denomterms[X];
+    }
+    log10BSGL_extcomp = 0.5*TwoF - log( BSGL_extcomp_denom );
+  }
+
+  /* this is not yet the log-Bayes-factor, as computed by XLALComputeBSGL(), so need to correct by log(1+1/oLG) */
+  log10BSGL_extcomp += log(1+1/oLG);
+  /* and actually switch to log10 */
+  log10BSGL_extcomp *= LAL_LOG10E;
+
+  return log10BSGL_extcomp;
+
+} /* XLALComputeBSGLPedestrian() */
 
 int
 XLALCheckBSGLDifferences ( const REAL4 diff,
