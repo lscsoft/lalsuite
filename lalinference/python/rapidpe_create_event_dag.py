718e88c276462f5642158e92d6d4a77a18585a77
Chris Pankow <chris.pankow@ligo.org>
2016-01-04 18:42:07 -0600

    rapidpe_ced: restore condor commands option Original: 05b24eea21a7a452aaf651e81cdfead3766f9e9f

--- lalinference/python/rapidpe_create_event_dag.py
+++ lalinference/python/rapidpe_create_event_dag.py
@@ -55,6 +55,7 @@ optp.add_option("--n-copies", default=1, help="Number of copies of each integrat
 optp.add_option("--write-script", action="store_true", help="In addition to the DAG, write a script to this filename to execute the workflow.")
 optp.add_option("--write-eff-lambda", action="store_true", help="Use psi0 column of template bank XML as effective lambda point to calculate in DAG.")
 optp.add_option("--write-deff-lambda", action="store_true", help="Use psi3 column of template bank XML as delta effective lambda point to calculate in DAG.")
+optp.add_option("--condor-command", action="append", help="Append these condor commands to the submit files. Useful for account group information.")
 
 for cat, val in MAXJOBS.iteritems():
     optname = "--maxjobs-%s" % cat.lower().replace("_", "-")
@@ -142,6 +143,7 @@ if opts.write_deff_lambda:
 
 ile_job_type, ile_sub_name = dagutils.write_integrate_likelihood_extrinsic_sub(
         tag='integrate',
+        condor_commands=condor_commands,
         intr_prms=intr_prms,
         log_dir=opts.log_directory,
         cache_file=opts.cache_file,
