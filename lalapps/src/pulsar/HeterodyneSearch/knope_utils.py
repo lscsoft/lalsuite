a1959f3d0648cdff17bbc3aa01d7a63cd44caf45
Matthew Pitkin <matthew.pitkin@ligo.org>
2016-04-15 14:23:38 +0100

    knope_utils: allow DE421 ephemeris Original: 2173a9167c9cad64f8f5c8785fa5735b0515a6b3

--- lalapps/src/pulsar/HeterodyneSearch/knope_utils.py
+++ lalapps/src/pulsar/HeterodyneSearch/knope_utils.py
@@ -306,7 +306,7 @@ class knopeDAG(pipeline.CondorDAG):
 
         # check that .par file contains ephemeris information and units (if not present defaults will be used - see get_ephemeris)
         if psr['EPHEM'] != None:
-          if psr['EPHEM'] not in ['DE200', 'DE405', 'DE414']:
+          if psr['EPHEM'] not in ['DE200', 'DE405', 'DE414', 'DE421']:
             print("Unregconised ephemeris '%s' in '%s'. Skipping this source" % (psr['EPHEM'], par))
             self.skipped_pulsars[par] = "Unregconised ephemeris '%s'" % psr['EPHEM']
             continue
