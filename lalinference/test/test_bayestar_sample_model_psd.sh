3b66c8ca0394419fa2ddbe5706864428612d9dd3
Leo Singer <leo.singer@ligo.org>
2016-06-20 19:35:17 -0400

    Invoke BAYESTAR scripts in build directory during unit tests Original: e5af925c1cc03ba863aff8809a4c719ade29baf8

--- lalinference/test/test_bayestar_sample_model_psd.sh
+++ lalinference/test/test_bayestar_sample_model_psd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 # Nothing fancy: for now, just run the script and check that it does not crash.
-bayestar_sample_model_psd \
+../python/bayestar_sample_model_psd \
     --H1=aLIGOZeroDetHighPower --L1=aLIGOZeroDetHighPower \
     -o test_bayestar_sample_model_psd.xml
