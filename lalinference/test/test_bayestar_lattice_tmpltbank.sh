c5b796f3b2459aaffd9a5b1c85d834a50c1b32c3
Leo Singer <leo.singer@ligo.org>
2016-06-21 11:24:44 -0400

    Port bayestar_lattice_tmpltbank to argparse The script still doesn't work right now. Maybe retire it soon? Original: 19a85f676b93e07e8345e71769df9b2ac707d384

---
+++ lalinference/test/test_bayestar_lattice_tmpltbank.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -e
+# Nothing fancy: for now, just run the script and check that it does not crash.
+../python/bayestar_lattice_tmpltbank -o test_bayestar_lattice_tmpltbank.xml --min-mass 2.7 --max-mass 2.9
