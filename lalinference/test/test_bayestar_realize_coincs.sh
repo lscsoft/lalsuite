20cce1eaf6bd023bcd96b4fdbba2515c9894b11d
Leo Singer <leo.singer@ligo.org>
2016-06-23 00:33:19 -0400

    Move some LALInference script tests to installcheck It's too hard to make these tests work before the package is installed. Original: 8f3216be2f058d780e247da196119a935cc61350

--- lalinference/test/test_bayestar_realize_coincs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-# Nothing fancy: for now, just run the script and check that it does not crash.
-../python/bayestar_realize_coincs --detector H1 L1 \
-    --reference-psd test_bayestar_sample_model_psd.xml \
-    $srcdir/HL-INJECTIONS_1_TEST-1000000000-10.xml \
-    -o test_bayestar_realize_coincs.xml
--- lalinference/test/test_bayestar_sample_model_psd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-# Nothing fancy: for now, just run the script and check that it does not crash.
-../python/bayestar_sample_model_psd \
-    --H1=aLIGOZeroDetHighPower --L1=aLIGOZeroDetHighPower \
-    -o test_bayestar_sample_model_psd.xml
