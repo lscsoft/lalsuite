d17881296248d73586c4f1e6160f3ed46924dad1
Rory Smith <rory.smith@ligo.org>
2016-06-23 11:02:06 -0700

    added missing files lol Original: d03b011cb4c3d795bca7e5e07713718b393b0b67

---
+++ lalinference/src/LALInferencePriorVolumes.h
@@ -0,0 +1,13 @@
+/**
+ * \file DoubleInt.h
+ * \brief Header file for initialisation functions used by LALInference codes
+ *
+ */
+#ifndef LALInferencePriorVolumes_h
+#define LALInferencePriorVolumes_h
+#include <lal/LALInference.h>
+
+double LALInferenceMassPriorVolume(LALInferenceRunState *state);
+double LALInferenceMassDistancePriorVolume(LALInferenceRunState *state);
+
+#endif
