102d228422c67a213664af1f72a2f1c6663f53ec
Leo Singer <leo.singer@ligo.org>
2016-08-05 13:18:34 -0400

    Rename test_bayestar.py to doctests.py Original: 20eb78908f5ae5b1d0b5e3d083db4287a552ecf8

--- lalinference/test/doctests.py
+++ lalinference/test/doctests.py
@@ -15,6 +15,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+"""
+Run doctests for selected LALInference modules.
+"""
+
 import sys
 import doctest
 import numpy as np
