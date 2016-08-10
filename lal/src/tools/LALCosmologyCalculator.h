2e9301c432fe2f0eded871f42be1f0d2f9c44e74
Walter Del Pozzo <walter.delpozzo@ligo.org>
2016-08-10 15:59:35 +0100

    added functions to compute the comoving volume elements and comoving volume Original: 2c1f938c094712e0ebed399332c80851492a04a1

--- lal/src/tools/LALCosmologyCalculator.h
+++ lal/src/tools/LALCosmologyCalculator.h
@@ -79,6 +79,16 @@ double XLALIntegrateHubbleParameter(
             LALCosmologicalParameters *omega, 
             double z);
 
+double XLALComovingVolumeElement(
+                                double z,
+                                void *omega);
+
+double XLALComovingVolume(
+                         LALCosmologicalParameters *omega,
+                         double z,
+                         double zmax);
+
+
 double XLALUniformComovingVolumeDensity(
             double z,
             void *omega);
@@ -87,6 +97,8 @@ double XLALUniformComovingVolumeDistribution(
             LALCosmologicalParameters *omega, 
             double z,
             double zmax);
+
+double XLALIntegrateComovingVolume(LALCosmologicalParameters *omega, double z);
 
 double XLALIntegrateComovingVolumeDensity(LALCosmologicalParameters *omega, double z);
             
