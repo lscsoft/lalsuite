b248c0d96c2282553b60f578f9a5ac91e58aa898
Kipp Cannon <kipp.cannon@ligo.org>
2016-08-11 19:26:30 +0900

    XLALGPSToStr():  edit documentation - explain how to free memory allocated by function Original: 1cc9e08c89fe6a7491a3ff8eb4d38e7c3d3e4c15

--- lal/src/date/StrToGPS.c
+++ lal/src/date/StrToGPS.c
@@ -265,8 +265,8 @@ int XLALStrToGPS(LIGOTimeGPS *t, const char *nptr, char **endptr)
  * LIGOTimeGPS.  If s is not NULL, then the string is written to that
  * location which must be large enough to hold the string plus a 0
  * terminator.  If s is NULL then a new buffer is allocated, and the string
- * written to it.  The return value is the address of the string or NULL on
- * failure.
+ * written to it;  free the buffer with XLALFree().  The return value is
+ * the address of the string or NULL on failure.
  */
 char *XLALGPSToStr(char *s, const LIGOTimeGPS *t)
 {
