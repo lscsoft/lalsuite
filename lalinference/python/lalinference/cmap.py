27b28d50e1accbcae10151729776c2502d8aba71
Leo Singer <leo.singer@ligo.org>
2016-10-14 00:08:47 -0400

    Remove some deprecated shim modules Original: 6987a32c70e9abe85d78ff15bca3c3e1aa04361e

--- lalinference/python/lalinference/cmap.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn(
-    'The module `lalinference.cmap` has been deprecated. Please import '
-    '`lalinference.plot.cmap` instead.')
-from .plot.cmap import *
--- lalinference/python/lalinference/marker.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn(
-    'The module `lalinference.marker` has been deprecated. Please import '
-    '`lalinference.plot.marker` instead.')
-from .plot.marker import *
