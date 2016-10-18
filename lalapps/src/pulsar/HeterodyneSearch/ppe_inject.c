ccc8981fb89bc71873488f6ad7ff3d9cbbe90a66
Max Isi <max.isi@ligo.org>
2016-10-18 19:21:21 +0200

    pulsar ppe_inject.c: non-GR bug fix Original: 1018a3d2632ebbd46ee7edd384de34db884165e3

--- lalapps/src/pulsar/HeterodyneSearch/ppe_inject.c
+++ lalapps/src/pulsar/HeterodyneSearch/ppe_inject.c
@@ -386,10 +386,10 @@ void inject_signal( LALInferenceRunState *runState ){
       UINT4 nonGRval = 1;
       while ( ifo_model ){
         LALInferenceAddVariable( ifo_model->params, "nonGR", &nonGRval, LALINFERENCE_INT4_t, LALINFERENCE_PARAM_FIXED );
-        ifo_model = ifo_model->next;
         if ( !varyphase ){ LALInferenceRemoveVariable( ifo_model->params, "varyphase" ); }
         if ( !varyskypos ){ LALInferenceRemoveVariable( ifo_model->params, "varyskypos" ); }
         if ( !varybinary ){ LALInferenceRemoveVariable( ifo_model->params, "varybinary" ); }
+        ifo_model = ifo_model->next;
       }
       ifo_model = runState->threads[0]->model->ifo;
     }
