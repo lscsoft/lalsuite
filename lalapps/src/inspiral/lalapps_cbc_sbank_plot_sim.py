57fe55849695660cf9579268d86f0e455b6d6aa3
Stephen Privitera <stephen.privitera@ligo.org>
2016-11-28 23:12:27 -0800

    sbank_plot_sim: allow multiple h5 input files Original: f2ce87bb6df470ae17bb99b9d6812e7f4b2ea308

--- lalapps/src/inspiral/lalapps_cbc_sbank_plot_sim.py
+++ lalapps/src/inspiral/lalapps_cbc_sbank_plot_sim.py
@@ -30,8 +30,6 @@ from lalinspiral.sbank.tau0tau3 import m1m2_to_tau0tau3
 
 from h5py import File as H5File
 
-fname = sys.argv[1]
-
 def find_process_id(process_table, prog_name):
     for program, pid in process_table["program", "process_id"]:
         if program == prog_name:
@@ -44,29 +42,31 @@ def find_process_param(pp_table, process_id, param):
             return value
 
 # grab data
-with H5File(fname, "r") as h5file:
-    match = h5file["/match_map"]["match"]
+match = None
+for fname in sys.argv[1:]:
+    with H5File(fname, "r") as h5file:
 
-    # order data by descending match so that low matches are plotted on top
-    order = match.argsort()[::-1]
-    inj_ind = h5file["/match_map"]["inj_index"][order]
-    tmplt_ind = h5file["/match_map"]["best_match_tmplt_index"][order]
-    match = match[order]
+        if match is None:
+            match = np.array(h5file["/match_map"])
+            inj_arr = np.array(h5file["/sim_inspiral"])
+            tmplt_arr = np.array(h5file["/sngl_inspiral"])
 
-    # extract columns of interest and apply row ordering
-    inj_arr = h5file["/sim_inspiral"]["mass1", "mass2", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z", "inclination"][inj_ind]
-    tmplt_arr = h5file["/sngl_inspiral"]["mass1", "mass2", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z"][tmplt_ind]
-    inj_sigmasq = h5file["/match_map"]["inj_sigmasq"]
+        else:
+            match = np.concatenate((np.array(h5file["/match_map"]), match))
+            tmplt_arr = np.concatenate((np.array(h5file["/sngl_inspiral"]), tmplt_arr))
+            inj_arr = np.concatenate((np.array(h5file["/sim_inspiral"]), inj_arr))
 
-    # extract waveforms
-    pid = find_process_id(h5file["/process"], u"lalapps_cbc_sbank_sim")
-    pparams = h5file["/process_params"]
-    inj_approx = find_process_param(pparams, pid, u"--injection-approx"),
-    tmplt_approx = find_process_param(pparams, pid, u"--template-approx")
+inj_sigmasq = match["inj_sigmasq"]
+match = match["match"]
 
-    # get other useful quantities
-    pid = find_process_id(h5file["/process"], u"lalapps_cbc_sbank_sim")
-    flow = float(find_process_param(pparams, pid, u"--flow"))
+order = match.argsort()[::-1]
+match = match[order]
+inj_arr = inj_arr[order]
+tmplt_arr = tmplt_arr[order]
+
+inj_approx = "FIXME"
+tmplt_approx = "FIXME"
+flow = 30 # FIXME
 
 # derived quantities
 # NB: Named fields in dtypes (see ligolw_table_to_array in lalapps_cbc_sbank_sim)
