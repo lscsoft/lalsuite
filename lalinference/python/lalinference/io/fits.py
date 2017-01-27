b0a6cd02e0ec8ef9bcef3987fd55a5a764d970ae
Leo Singer <leo.singer@ligo.org>
2017-01-26 23:12:38 -0500

    Update copyright Original: 4b07bfbe5fe2f6d93b4d662af695f066376749dc

--- lalinference/python/lalinference/io/fits.py
+++ lalinference/python/lalinference/io/fits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2013-2016  Leo Singer
+# Copyright (C) 2013-2017  Leo Singer
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
