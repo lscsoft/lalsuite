8f9b77bdec895479517fe932f04dc825e612de0a
Karl Wette <karl.wette@ligo.org>
2017-02-01 20:00:47 +1300

    Weave/TestSearchShortcuts: test with segment lists spanning ~130, 260 days
    Original lalsuite-weave: 118b65a5f5d6d403ff4da197ecf3072751ef4cc3

--- lalapps/src/pulsar/Weave/TestSearchShortcuts.sh
+++ lalapps/src/pulsar/Weave/TestSearchShortcuts.sh
@@ -1,92 +1,119 @@
 # Perform an interpolating search without/with part of the search being shortcut, and check for consistent results
 
-echo "=== Create search setup with 4 segments ==="
-set -x
-${builddir}/lalapps_WeaveSetup --first-segment=1122332211/90000 --segment-count=4 --detectors=H1,L1 --output-file=WeaveSetup.fits
-set +x
-echo
+for setup in short mid long; do
 
-echo "=== Restrict timestamps to segment list in WeaveSetup.fits ==="
-set -x
-${fitsdir}/lalapps_fits_table_list 'WeaveSetup.fits[segments][col c1=start_s; col2=end_s]' \
-    | awk 'BEGIN { print "/^#/ { print }" } /^#/ { next } { printf "%i <= $1 && $1 <= %i { print }\n", $1, $2 + 1 }' > timestamp-filter.awk
-awk -f timestamp-filter.awk ${srcdir}/timestamps-1.txt > timestamps-1.txt
-awk -f timestamp-filter.awk ${srcdir}/timestamps-2.txt > timestamps-2.txt
-set +x
-echo
+    case ${setup} in
 
-echo "=== Perform interpolating search ==="
-set -x
-${builddir}/lalapps_Weave --output-file=WeaveOutNoShort.fits \
-    --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
-    --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
-    --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
-    --alpha=0.9/1.4 --delta=-1.2/2.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3
-set +x
-echo
+        short)
+            weave_setup_options="--segment-count=4"
+            weave_search_options="--alpha=0.9/1.4 --delta=-1.2/2.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3"
+            ;;
 
-echo "=== Check average number of semicoherent templates per dimension is more than one"
-set -x
-for dim in SSKYA SSKYB NU0DOT NU1DOT; do
-    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" "SEMIAVG ${dim}" > tmp
-    semi_avg_ntmpl_dim=`cat tmp | xargs printf "%d"`
-    expr ${semi_avg_ntmpl_dim} '>' 1
+        mid)
+            weave_setup_options="--segment-count=2 --segment-gap=11130000"
+            weave_search_options="--alpha=0.9/0.4 --delta=-1.2/1.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=7 --coh-max-mismatch=0.4"
+            ;;
+
+        long)
+            weave_setup_options="--segment-count=2 --segment-gap=22350000"
+            weave_search_options="--alpha=0.9/0.4 --delta=-1.2/1.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=5 --coh-max-mismatch=0.2"
+            ;;
+
+        *)
+            echo "$0: unknown setup '${setup}'"
+            exit 1
+
+    esac
+
+    echo "=== Setup '${setup}': Create search setup with ${weave_setup_options} ==="
+    set -x
+    ${builddir}/lalapps_WeaveSetup --first-segment=1122332211/90000 ${weave_setup_options} --detectors=H1,L1 --output-file=WeaveSetup.fits
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Restrict timestamps to segment list in WeaveSetup.fits ==="
+    set -x
+    ${fitsdir}/lalapps_fits_table_list 'WeaveSetup.fits[segments][col c1=start_s; col2=end_s]' \
+        | awk 'BEGIN { print "/^#/ { print }" } /^#/ { next } { printf "%i <= $1 && $1 <= %i { print }\n", $1, $2 + 1 }' > timestamp-filter.awk
+    awk -f timestamp-filter.awk ${srcdir}/timestamps-1.txt > timestamps-1.txt
+    awk -f timestamp-filter.awk ${srcdir}/timestamps-2.txt > timestamps-2.txt
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Perform interpolating search ==="
+    set -x
+    ${builddir}/lalapps_Weave --output-file=WeaveOutNoShort.fits \
+        --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
+        --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
+        --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
+        ${weave_search_options}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Check average number of semicoherent templates per dimension is more than one"
+    set -x
+    for dim in SSKYA SSKYB NU0DOT NU1DOT; do
+        ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" "SEMIAVG ${dim}" > tmp
+        semi_avg_ntmpl_dim=`cat tmp | xargs printf "%d"`
+        expr ${semi_avg_ntmpl_dim} '>' 1
+    done
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Perform interpolating search with all computations shortcut ==="
+    set -x
+    ${builddir}/lalapps_Weave --shortcut=compute --output-file=WeaveOutShortComp.fits \
+        --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
+        --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
+        --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
+        ${weave_search_options}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Perform interpolating search with main search loop shortcut ==="
+    set -x
+    ${builddir}/lalapps_Weave --shortcut=search --output-file=WeaveOutShortSearch.fits \
+        --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
+        --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
+        --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
+        ${weave_search_options}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Check approximate/computed number of semicoherent templates are consistent ==="
+    set -x
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'SEMIAPPX' > tmp
+    semi_ntmpl_no_short=`cat tmp | xargs printf "%d"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'SEMIAPPX' > tmp
+    semi_ntmpl_short_comp=`cat tmp | xargs printf "%d"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutShortSearch.fits[0]" 'SEMIAPPX' > tmp
+    semi_ntmpl_short_search=`cat tmp | xargs printf "%d"`
+    expr ${semi_ntmpl_no_short} '=' ${semi_ntmpl_short_comp}
+    expr ${semi_ntmpl_no_short} '=' ${semi_ntmpl_short_search}
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'SEMICOMP' > tmp
+    semi_ncomp_no_short=`cat tmp | xargs printf "%d"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'SEMICOMP' > tmp
+    semi_ncomp_short_comp=`cat tmp | xargs printf "%d"`
+    expr ${semi_ncomp_no_short} '=' ${semi_ncomp_short_comp}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Check peak memory usage is consistent ==="
+    set -x
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'PEAKMEM' > tmp
+    peak_mem_no_short=`cat tmp | xargs printf "%g"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'PEAKMEM' > tmp
+    peak_mem_short_comp=`cat tmp | xargs printf "%g"`
+    awk "BEGIN { print x = ${peak_mem_short_comp} / ${peak_mem_no_short}; exit ( ( 0.95 < x && x < 1.05 ) ? 0 : 1 ) }"
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Check miscellaneous per-segment information is consistent ==="
+    set -x
+    ${fitsdir}/lalapps_fits_table_list "WeaveOutNoShort.fits[per_seg_info]" > per_seg_info_no_short
+    ${fitsdir}/lalapps_fits_table_list "WeaveOutShortComp.fits[per_seg_info]" > per_seg_info_short_comp
+    diff per_seg_info_no_short per_seg_info_short_comp
+    set +x
+    echo
+
 done
-set +x
-echo
-
-echo "=== Perform interpolating search with all computations shortcut ==="
-set -x
-${builddir}/lalapps_Weave --shortcut=compute --output-file=WeaveOutShortComp.fits \
-    --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
-    --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
-    --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
-    --alpha=0.9/1.4 --delta=-1.2/2.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3
-set +x
-echo
-
-echo "=== Perform interpolating search with main search loop shortcut ==="
-set -x
-${builddir}/lalapps_Weave --shortcut=search --output-file=WeaveOutShortSearch.fits \
-    --toplists=all --toplist-limit=5000 --misc-info --setup-file=WeaveSetup.fits \
-    --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
-    --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
-    --alpha=0.9/1.4 --delta=-1.2/2.3 --freq=55.5/1e-4 --f1dot=-1.5e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3
-set +x
-echo
-
-echo "=== Check approximate/computed number of semicoherent templates are consistent ==="
-set -x
-${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'SEMIAPPX' > tmp
-semi_ntmpl_no_short=`cat tmp | xargs printf "%d"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'SEMIAPPX' > tmp
-semi_ntmpl_short_comp=`cat tmp | xargs printf "%d"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutShortSearch.fits[0]" 'SEMIAPPX' > tmp
-semi_ntmpl_short_search=`cat tmp | xargs printf "%d"`
-expr ${semi_ntmpl_no_short} '=' ${semi_ntmpl_short_comp}
-expr ${semi_ntmpl_no_short} '=' ${semi_ntmpl_short_search}
-${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'SEMICOMP' > tmp
-semi_ncomp_no_short=`cat tmp | xargs printf "%d"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'SEMICOMP' > tmp
-semi_ncomp_short_comp=`cat tmp | xargs printf "%d"`
-expr ${semi_ncomp_no_short} '=' ${semi_ncomp_short_comp}
-set +x
-echo
-
-echo "=== Check peak memory usage is consistent ==="
-set -x
-${fitsdir}/lalapps_fits_header_getval "WeaveOutNoShort.fits[0]" 'PEAKMEM' > tmp
-peak_mem_no_short=`cat tmp | xargs printf "%g"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutShortComp.fits[0]" 'PEAKMEM' > tmp
-peak_mem_short_comp=`cat tmp | xargs printf "%g"`
-awk "BEGIN { print x = ${peak_mem_short_comp} / ${peak_mem_no_short}; exit ( ( 0.95 < x && x < 1.05 ) ? 0 : 1 ) }"
-set +x
-echo
-
-echo "=== Check miscellaneous per-segment information is consistent ==="
-set -x
-${fitsdir}/lalapps_fits_table_list "WeaveOutNoShort.fits[per_seg_info]" > per_seg_info_no_short
-${fitsdir}/lalapps_fits_table_list "WeaveOutShortComp.fits[per_seg_info]" > per_seg_info_short_comp
-diff per_seg_info_no_short per_seg_info_short_comp
-set +x
-echo
