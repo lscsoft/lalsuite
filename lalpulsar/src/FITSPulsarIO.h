0dee895d52476ad352372aeff5ac75add1f8a6c1
Karl Wette <karl.wette@ligo.org>
2017-03-09 16:55:50 +0100

    LALPulsar: add module for reading/writing pulsar-related FITS files Original: 91be2a7a853fa750222429bdf081a52d65426a1b

---
+++ lalpulsar/src/FITSPulsarIO.h
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2016, 2017 Karl Wette
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with with program; see the file COPYING. If not, write to the
+// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+// MA 02111-1307 USA
+//
+
+#ifndef _FITSPULSARIO_H
+#define _FITSPULSARIO_H
+
+#include <stdint.h>
+#include <gsl/gsl_matrix.h>
+#include <lal/LALStdlib.h>
+#include <lal/FITSFileIO.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+///
+/// \defgroup FITSPulsarIO_h Header FITSPulsarIO.h
+/// \ingroup lalpulsar_general
+/// \author Karl Wette
+/// \brief Routines for reading/writing pulsar-related data to/from FITS files
+///
+
+/// @{
+
+/// @}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _FITSPULSARIO_H
+
+// Local Variables:
+// c-file-style: "linux"
+// c-basic-offset: 2
+// End:
