1f2ead4c392f933517b3057f0bfe00a37d237cc4
Karl Wette <karl.wette@ligo.org>
2017-03-09 16:55:50 +0100

    LALPulsar: add module for reading/writing pulsar-related FITS files Original: 260e7d733b02e1c46adcaf51a7f8f9c084aa36f5

---
+++ lalpulsar/src/FITSPulsarIO.c
@@ -0,0 +1,25 @@
+//
+// Copyright (C) 2016, 2017 Karl Wette
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with with program; see the file COPYING. If not, write to the
+// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+// MA 02111-1307 USA
+//
+
+#include <lal/FITSPulsarIO.h>
+
+// Local Variables:
+// c-file-style: "linux"
+// c-basic-offset: 2
+// End:
