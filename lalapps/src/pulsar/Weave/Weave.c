019e71bd85c0077347e6aa28f93adf555ac7c68b
Karl Wette <karl.wette@ligo.org>
2017-03-09 14:37:43 +0100

    Weave: update copyright notices
    Original lalsuite-weave: 1122b8ded2a409fd18e6d0d7f04a421b9a690b00

--- lalapps/src/pulsar/Weave/Weave.c
+++ lalapps/src/pulsar/Weave/Weave.c
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2016 Karl Wette
+// Copyright (C) 2016, 2017 Karl Wette
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
