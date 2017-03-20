0a5a777fed761639f8aec2fe40ee4fc32972285d
Stephen Privitera <stephen.privitera@aei.mpg.de>
2017-03-20 20:11:01 +0100

    sbank: update copyrights Original: e8841f10e52820d3a8395c4b86a0d23e9977e23b

--- lalapps/src/inspiral/lalapps_cbc_sbank_pipe.py
+++ lalapps/src/inspiral/lalapps_cbc_sbank_pipe.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014  Stephen Privitera
+# Copyright (C) 2012-2017  Stephen Privitera
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
