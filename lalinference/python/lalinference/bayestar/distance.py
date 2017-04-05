b3a94429fea7c813bc2ae12257b9d38960e0c380
Leo Singer <leo.singer@ligo.org>
2017-04-05 15:46:28 -0400

    Remove lalinference.bayestar.distance.find_injection_distance It's a duplicate of lalinference.bayestar.distance.marginal_cdf. Original: 0f664ecfc2f8c43dc1ed2dbba04d4438f1738870

--- lalinference/python/lalinference/bayestar/distance.py
+++ lalinference/python/lalinference/bayestar/distance.py
@@ -39,7 +39,7 @@ from ._distance import *
 
 __all__ = tuple(_ for _ in _distance.__dict__ if not _.startswith('_')) + (
     'ud_grade', 'conditional_kde', 'cartesian_kde_to_moments', 'principal_axes',
-    'parameters_to_moments', 'find_injection_distance')
+    'parameters_to_moments')
 
 
 def _add_newdoc_ufunc(func, doc):
@@ -552,8 +552,3 @@ def parameters_to_marginal_moments(prob, distmu, distsigma):
     rbar = (prob * distmean).sum()
     r2bar = (prob * (np.square(diststd) + np.square(distmean))).sum()
     return rbar, np.sqrt(r2bar - np.square(rbar))
-
-
-def find_injection_distance(true_dist, prob, distmu, distsigma, distnorm):
-    return np.sum(prob * conditional_cdf(
-        true_dist, distmu, distsigma, distnorm))
