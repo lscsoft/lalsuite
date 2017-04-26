d0006ac91d65272397d4827be06aaf91b25ffda7
Leo Singer <leo.singer@ligo.org>
2017-04-26 13:45:15 -0400

    lalinference.io.hdf5: avoid mutating dictionary while iterating it Original: 8caa8472843f2df0f4e7a86ea06a38057e0932fe

--- lalinference/python/lalinference/io/hdf5.py
+++ lalinference/python/lalinference/io/hdf5.py
@@ -237,7 +237,7 @@ def write_samples(table, filename, metadata=None, **kwargs):
     table = table.copy()
 
     # Reconstruct table attributes.
-    for colname, column in table.columns.items():
+    for colname, column in tuple(table.columns.items()):
         if column.meta['vary'] == FIXED:
             np.testing.assert_array_equal(column[1:], column[0],
                                           'Column {0} is a fixed column, but '
