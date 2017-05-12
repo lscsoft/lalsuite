feb3ced08122e9a26680b81df5d7b5c6dd5c485d
Karl Wette <karl.wette@ligo.org>
2017-05-12 17:10:42 +0200

    SuperskyMetrics: update copyright notices Original: f9a75d780652390f17008273d551c310e11a9144

--- lalpulsar/src/SuperskyMetrics.h
+++ lalpulsar/src/SuperskyMetrics.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2014, 2015 Karl Wette
+// Copyright (C) 2014--2017 Karl Wette
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
