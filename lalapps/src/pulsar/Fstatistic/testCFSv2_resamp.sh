2c5c2a4fe82498efe08710d5df724c2203e09e52
Christoph Dreissigacker <christoph.dreissigacker@aei.mpg.de>
2017-05-17 15:34:12 +0200

    ComputeFstatistic_v2: add option --injectionSources to specify on-the-fly injections - allows runtime 'on the fly' injections - modified testCFSv2_resamp.sh to compare doing the injections during   runtime in CFSv2 versus prepared injections via Makefakedata. - refs #5548 Original: 0418f085408f9948eacb3fd90e49716fda5c3f85

--- lalapps/src/pulsar/Fstatistic/testCFSv2_resamp.sh
+++ lalapps/src/pulsar/Fstatistic/testCFSv2_resamp.sh
@@ -4,6 +4,11 @@
 echo "Setting LAL_DEBUG_LEVEL=${LAL_DEBUG_LEVEL:-msglvl1,memdbg}"
 export LAL_DEBUG_LEVEL
 
+## allow 'make test' to work from builddir != srcdir
+if [ -z "${srcdir}" ]; then
+    srcdir=`dirname $0`
+fi
+
 ## make sure we work in 'C' locale here to avoid awk sillyness
 LC_ALL_old=$LC_ALL
 export LC_ALL=C
@@ -72,9 +77,15 @@ haveNoise=true;
 
 ## ----- define output directory and files
 testDir=testCFSv2_resamp.d
+compDir=testCFSv2_resamp.d/injSourcesComp
 rm -rf $testDir
 mkdir -p $testDir
+mkdir -p $compDir
 SFTdir=${testDir}
+SFTdir2=${compDir}
+
+outfile_Comp=${testDir}/Fstat_Comp.dat
+loudest_Comp=${testDir}/Loudest_Comp.dat
 
 outfile_Demod=${testDir}/Fstat_Demod.dat
 loudest_Demod=${testDir}/Loudest_Demod.dat
@@ -82,6 +93,7 @@ loudest_Demod=${testDir}/Loudest_Demod.dat
 outfile_Resamp=${testDir}/Fstat_Resamp.dat
 loudest_Resamp=${testDir}/Loudest_Resamp.dat
 
+timefile_Comp=${testDir}/timing_Comp.dat
 timefile_Demod=${testDir}/timing_Demod.dat
 timefile_Resamp=${testDir}/timing_Resamp.dat
 
@@ -91,12 +103,21 @@ timefile_Resamp=${testDir}/timing_Resamp.dat
 
 echo
 echo "----------------------------------------------------------------------"
-echo " STEP 1: Generate Fake Signal"
+echo " STEP 1: Generate Fake Data"
 echo "----------------------------------------------------------------------"
 echo
 injectionSources="{refTime=${refTime}; Freq=$Freq; f1dot=$f1dot; Alpha=$Alpha; Delta=$Delta; h0=$h0; cosi=$cosi; psi=$psi; phi0=$phi0;}"
 dataSpec="--Tsft=$Tsft --startTime=$startTime --duration=$duration --sqrtSX=${sqrtSX} --fmin=$mfd_fmin --Band=$mfd_FreqBand"
-mfd_CL="--injectionSources='${injectionSources}' ${dataSpec} --outSingleSFT --outSFTdir=${SFTdir} --randSeed=1 --IFOs=H1,L1"
+mfd_CL_comp="${dataSpec} --injectionSources='${injectionSources}' --outSingleSFT --outSFTdir=${SFTdir2} --randSeed=1 --IFOs=H1,L1"
+cmdline="$mfd_code $mfd_CL_comp "
+echo $cmdline;
+if ! eval "$cmdline &> /dev/null"; then
+    echo "Error.. something failed when running '$mfd_code' ..."
+    exit 1
+fi
+
+echo
+mfd_CL="${dataSpec} --outSingleSFT --outSFTdir=${SFTdir} --randSeed=1 --IFOs=H1,L1"
 cmdline="$mfd_code $mfd_CL "
 echo $cmdline;
 if ! eval "$cmdline &> /dev/null"; then
@@ -104,16 +125,16 @@ if ! eval "$cmdline &> /dev/null"; then
     exit 1
 fi
 
-cfs_CL=" --refTime=${refTime} --Dterms=8 --Alpha=$Alpha --Delta=$Delta  --AlphaBand=$AlphaBand --DeltaBand=$DeltaBand --dAlpha=$dAlpha --dDelta=$dDelta --Freq=$cfs_Freq --FreqBand=$cfs_FreqBand --dFreq=$cfs_dFreq --f1dot=$cfs_f1dot --f1dotBand=$cfs_f1dotBand --df1dot=${cfs_df1dot} --DataFiles='$SFTdir/*.sft' --NumCandidatesToKeep=${cfs_nCands}"
+cfs_CL=" --refTime=${refTime} --Dterms=8 --Alpha=$Alpha --Delta=$Delta  --AlphaBand=$AlphaBand --DeltaBand=$DeltaBand --dAlpha=$dAlpha --dDelta=$dDelta --Freq=$cfs_Freq --FreqBand=$cfs_FreqBand --dFreq=$cfs_dFreq --f1dot=$cfs_f1dot --f1dotBand=$cfs_f1dotBand --df1dot=${cfs_df1dot} --NumCandidatesToKeep=${cfs_nCands}"
 if [ "$haveNoise" != "true" ]; then
     cfs_CL="$cfs_CL --SignalOnly"
 fi
 echo
 echo "----------------------------------------------------------------------"
-echo "STEP 2: run directed CFS_v2 with LALDemod method"
+echo "STEP 2: run directed CFS_v2 with LALDemod with injectionSources in MFDv5"
 echo "----------------------------------------------------------------------"
 echo
-cmdline="$cfs_code $cfs_CL --FstatMethod=DemodBest --outputFstat=$outfile_Demod --outputTiming=$timefile_Demod --outputLoudest=${loudest_Demod} "
+cmdline="$cfs_code $cfs_CL --FstatMethod=DemodBest --DataFiles='$SFTdir2/*.sft' --outputFstat=$outfile_Comp --outputTiming=$timefile_Comp --outputLoudest=${loudest_Comp} "
 echo $cmdline;
 if ! eval "$cmdline &> /dev/null"; then
     echo "Error.. something failed when running '$cfs_code' ..."
@@ -122,10 +143,22 @@ fi
 
 echo
 echo "----------------------------------------------------------------------"
-echo " STEP 3: run directed CFS_v2 with resampling"
+echo "STEP 3: run directed CFS_v2 with LALDemod method"
 echo "----------------------------------------------------------------------"
 echo
-cmdline="$cfs_code $cfs_CL --FstatMethod=ResampBest --outputFstat=$outfile_Resamp --outputTiming=$timefile_Resamp  --outputLoudest=${loudest_Resamp}"
+cmdline="$cfs_code $cfs_CL --injectionSources='${injectionSources}' --FstatMethod=DemodBest --DataFiles='$SFTdir/*.sft' --outputFstat=$outfile_Demod --outputTiming=$timefile_Demod --outputLoudest=${loudest_Demod} "
+echo $cmdline;
+if ! eval "$cmdline &> /dev/null"; then
+    echo "Error.. something failed when running '$cfs_code' ..."
+    exit 1
+fi
+
+echo
+echo "----------------------------------------------------------------------"
+echo " STEP 4: run directed CFS_v2 with resampling"
+echo "----------------------------------------------------------------------"
+echo
+cmdline="$cfs_code $cfs_CL --injectionSources='${injectionSources}' --FstatMethod=ResampBest --DataFiles='$SFTdir/*.sft' --outputFstat=$outfile_Resamp --outputTiming=$timefile_Resamp  --outputLoudest=${loudest_Resamp}"
 echo $cmdline;
 if ! eval "$cmdline 2> /dev/null"; then
     echo "Error.. something failed when running '$cfs_code' ..."
@@ -133,18 +166,29 @@ if ! eval "$cmdline 2> /dev/null"; then
 fi
 
 echo "----------------------------------------"
-echo " STEP 4: Comparing results: "
+echo " STEP 5: Comparing results: "
 echo "----------------------------------------"
 
+sort $outfile_Comp > __tmp_sorted && mv __tmp_sorted $outfile_Comp
 sort $outfile_Demod > __tmp_sorted && mv __tmp_sorted $outfile_Demod
 sort $outfile_Resamp > __tmp_sorted && mv __tmp_sorted $outfile_Resamp
 
 ## compare absolute differences instead of relative, allow deviations of up to sigma=sqrt(8)~2.8
 echo
+cmdline="$cmp_code -1 ./${outfile_Demod} -2 ./${outfile_Comp} --tol-L1=8e-2 --tol-L2=8e-2 --tol-angle=0.08 --tol-atMax=8e-2"
+echo -n $cmdline
+if ! eval $cmdline; then
+    echo "==> OUCH... files differ. Something might be wrong with injectionSources..."
+    exit 2
+else
+    echo "	==> OK."
+fi
+
+echo
 cmdline="$cmp_code -1 ./${outfile_Demod} -2 ./${outfile_Resamp} --tol-L1=8e-2 --tol-L2=8e-2 --tol-angle=0.08 --tol-atMax=8e-2"
 echo -n $cmdline
 if ! eval $cmdline; then
-    echo "==> OUCH... files differ. Something might be wrong..."
+    echo "==> OUCH... files differ. Something might be wrong with Resamp/Demod..."
     exit 2
 else
     echo "	==> OK."
@@ -152,7 +196,7 @@ fi
 
 echo
 echo "----------------------------------------------------------------------"
-echo " STEP 5: Sanity-check Resampling parameter estimation: "
+echo " STEP 6: Sanity-check Resampling parameter estimation: "
 echo "----------------------------------------------------------------------"
 echo
 
