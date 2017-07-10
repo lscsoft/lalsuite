3dbaac35afdf0226b626404e9a43e8c3b096ed61
Leo Singer <leo.singer@ligo.org>
2017-07-10 16:17:12 -0600

    StrictVersion -> LooseVersion Original: 39d88f89eaa594477bd23482c49a80667b6b4f04

--- lalinference/python/lalinference/plot/healpix.py
+++ lalinference/python/lalinference/plot/healpix.py
@@ -21,7 +21,7 @@ from __future__ import division
 
 import functools
 import matplotlib
-from distutils.version import StrictVersion
+from distutils.version import LooseVersion
 from matplotlib import text
 from matplotlib import ticker
 from matplotlib import patheffects
@@ -65,7 +65,7 @@ def heatmap(func, *args, **kwargs):
     zz[mask] = func(lons[mask], lats[mask])
 
     # Plot bitmap using imshow.
-    if StrictVersion(matplotlib.__version__) < StrictVersion('2.0'):
+    if LooseVersion(matplotlib.__version__) < LooseVersion('2.0'):
         # FIXME: workaround for old behavior of imshow().
         # Remove this once we require matplotlib >= 2.0.
         # See also:
