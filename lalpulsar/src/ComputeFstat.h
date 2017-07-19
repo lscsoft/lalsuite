lscsoft/lalsuite-archive

commit a086d950f6b638d407e77bf0154877d7e44f0aaa

Resampling F-stat: use whatToCompute=FSTATQ_NONE to shortcut F-stat calculation
- only computes resampled timeseries then returns
- refs #5206

lalpulsar/src/ComputeFstat.h
@@ -84,13 +84,13 @@ typedef struct tagFstatInputVector {
 /// Not all options are supported by all \f$\mathcal{F}\f$-statistic methods.
 ///
 typedef enum tagFstatQuantities {
-  FSTATQ_NONE           = 0x00,
+  FSTATQ_NONE           = 0x00,		///< Dont compute F-statistic, still compute buffered quantities
   FSTATQ_2F             = 0x01,         ///< Compute multi-detector \f$2\mathcal{F}\f$.
   FSTATQ_FAFB           = 0x02,         ///< Compute multi-detector \f$F_a\f$ and \f$F_b\f$.
   FSTATQ_2F_PER_DET     = 0x04,         ///< Compute \f$2\mathcal{F}\f$ for each detector.
   FSTATQ_FAFB_PER_DET   = 0x08,         ///< Compute \f$F_a\f$ and \f$F_b\f$ for each detector.
   FSTATQ_ATOMS_PER_DET  = 0x10,         ///< Compute per-SFT \f$\mathcal{F}\f$-statistic atoms for each detector (\a Demod only).
-  FSTATQ_LAST           = 0x20
+  FSTATQ_LAST           = 0x11
 } FstatQuantities;
 
 ///

lalpulsar/src/ComputeFstat.c
@@ -597,7 +597,7 @@ XLALComputeFstat ( FstatResults **Fstats,               ///< [in/out] Address of
   XLAL_CHECK ( doppler->asini >= 0, XLAL_EINVAL);
   XLAL_CHECK ( numFreqBins > 0, XLAL_EINVAL);
   XLAL_CHECK ( !input->singleFreqBin || numFreqBins == 1, XLAL_EINVAL, "numFreqBins must be 1 if XLALCreateFstatInput() was passed zero dFreq" );
-  XLAL_CHECK ( 0 < whatToCompute && whatToCompute < FSTATQ_LAST, XLAL_EINVAL);
+  XLAL_CHECK ( 0 <= whatToCompute && whatToCompute < FSTATQ_LAST, XLAL_EINVAL);
 
   // Allocate results struct, if needed
   if ( (*Fstats) == NULL ) {

lalpulsar/src/ComputeFstat_Resamp.c
@@ -461,6 +461,11 @@ XLALComputeFstatResamp ( FstatResults* Fstats,
     toc = XLALGetCPUTime();
     ti->Tau.bBary = (toc-tic);
   }
+
+  if ( whatToCompute == FSTATQ_NONE ) {
+    return XLAL_SUCCESS;
+  }
+
   MultiCOMPLEX8TimeSeries *multiTimeSeries_SRC_a = resamp->multiTimeSeries_SRC_a;
   MultiCOMPLEX8TimeSeries *multiTimeSeries_SRC_b = resamp->multiTimeSeries_SRC_b;
 

lscsoft/lalsuite

commit e8c2fb11b978a36ddae67e11ae8980e5151c4223

ComputeFstat.h: fix too-low FSTATQ_LAST setting - 'last' marker on bitflags needs to be exactly one higher than   maximal possible combined bitflag value - that bug was introduced in a086d950f6 as part of issue #5206 - fixes #5620 Original: 5947e32b90fe1ee05a942a412d59956742f0a69a

lalpulsar/src/ComputeFstat.h
@@ -91,7 +91,7 @@ typedef enum tagFstatQuantities {
   FSTATQ_2F_PER_DET     = 0x04,         ///< Compute \f$2\mathcal{F}\f$ for each detector.
   FSTATQ_FAFB_PER_DET   = 0x08,         ///< Compute \f$F_a\f$ and \f$F_b\f$ for each detector.
   FSTATQ_ATOMS_PER_DET  = 0x10,         ///< Compute per-SFT \f$\mathcal{F}\f$-statistic atoms for each detector (\a Demod only).
-  FSTATQ_LAST           = 0x11
+  FSTATQ_LAST           = 0x20
 } FstatQuantities;
 
 ///