aa41139c6509e120d4f745e10b00f6d0d6ea4c16
Karl Wette <karl.wette@ligo.org>
2017-09-03 05:11:21 +1000

    Weave/TestFreqPartition.sh: test with one frequency and with longer timespans
    Test suite:
    PASS: TestInterpolating.sh
    PASS: TestNonInterpolating.sh
    PASS: TestSingleSegment.sh
    PASS: TestSimulateSearch.sh
    PASS: TestCacheMaxSize.sh
    PASS: TestCheckpointing.sh
    PASS: TestFreqPartition.sh
    
    Original lalsuite-weave: 25dbcb8caf26a9f43afcc0bc9d814f0cf7f86fb6

--- lalapps/src/pulsar/Weave/TestFreqPartition.sh
+++ lalapps/src/pulsar/Weave/TestFreqPartition.sh
@@ -2,70 +2,105 @@
 
 export LAL_FSTAT_FFT_PLAN_MODE=ESTIMATE
 
-echo "=== Create search setup with 3 segments ==="
-set -x
-${builddir}/lalapps_WeaveSetup --first-segment=1122332211/90000 --segment-count=3 --detectors=H1,L1 --output-file=WeaveSetup.fits
-set +x
-echo
+for setup in onefreq short long; do
 
-echo "=== Restrict timestamps to segment list in WeaveSetup.fits ==="
-set -x
-${fitsdir}/lalapps_fits_table_list 'WeaveSetup.fits[segments][col c1=start_s; col2=end_s]' \
-    | awk 'BEGIN { print "/^#/ { print }" } /^#/ { next } { printf "%i <= $1 && $1 <= %i { print }\n", $1, $2 + 1 }' > timestamp-filter.awk
-awk -f timestamp-filter.awk ${srcdir}/timestamps-1.txt > timestamps-1.txt
-awk -f timestamp-filter.awk ${srcdir}/timestamps-2.txt > timestamps-2.txt
-set +x
-echo
+    case ${setup} in
 
-echo "=== Perform non-interpolating search without frequency partitions ==="
-set -x
-${builddir}/lalapps_Weave --freq-partitions=1 --output-file=WeaveOutNoPart.fits \
-    --toplists=all --toplist-limit=2321 --misc-info --setup-file=WeaveSetup.fits \
-    --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
-    --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
-    --alpha=0.9/1.4 --delta=-1.2/2.3 --freq=49.5/1e-3 --f1dot=-1e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3
-set +x
-echo
+        onefreq)
+            weave_setup_options="--segment-count=3"
+            weave_search_options="--alpha=0.2 --delta=-0.4 --freq=47.7 --f1dot=-1.1e-9,0 --semi-max-mismatch=5 --coh-max-mismatch=0.3"
+            ;;
 
-echo "=== Check for non-singular semicoherent dimensions ==="
-set -x
-semi_ntmpl_prev=1
-for dim in SSKYA SSKYB NU1DOT NU0DOT; do
-    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" "NSEMITMPL ${dim}" > tmp
-    semi_ntmpl=`cat tmp | xargs printf "%d"`
-    expr ${semi_ntmpl} '>' ${semi_ntmpl_prev}
-    semi_ntmpl_prev=${semi_ntmpl}
+        short)
+            weave_setup_options="--segment-count=3"
+            weave_search_options="--alpha=0.9/1.4 --delta=-1.2/2.3 --freq=49.5/1e-3 --f1dot=-1e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3"
+            ;;
+
+        long)
+            weave_setup_options="--segment-count=3 --segment-gap=11130000"
+            weave_search_options="--alpha=0.1/0.5 --delta=-0.2/0.4 --freq=41.5/1e-4 --f1dot=-3e-11,0 --semi-max-mismatch=12 --coh-max-mismatch=0.6"
+            ;;
+
+        *)
+            echo "$0: unknown setup '${setup}'"
+            exit 1
+
+    esac
+
+    echo "=== Setup '${setup}': Create search setup with ${weave_setup_options} ==="
+    set -x
+    ${builddir}/lalapps_WeaveSetup --first-segment=1122332211/90000 ${weave_setup_options} --detectors=H1,L1 --output-file=WeaveSetup.fits
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Restrict timestamps to segment list in WeaveSetup.fits ==="
+    set -x
+    ${fitsdir}/lalapps_fits_table_list 'WeaveSetup.fits[segments][col c1=start_s; col2=end_s]' \
+        | awk 'BEGIN { print "/^#/ { print }" } /^#/ { next } { printf "%i <= $1 && $1 <= %i { print }\n", $1, $2 + 1 }' > timestamp-filter.awk
+    awk -f timestamp-filter.awk ${srcdir}/timestamps-1.txt > timestamps-1.txt
+    awk -f timestamp-filter.awk ${srcdir}/timestamps-2.txt > timestamps-2.txt
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Perform interpolating search without frequency partitions ==="
+    set -x
+    ${builddir}/lalapps_Weave --freq-partitions=1 --output-file=WeaveOutNoPart.fits \
+        --toplists=all --toplist-limit=2321 --misc-info --setup-file=WeaveSetup.fits \
+        --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
+        --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
+        ${weave_search_options}
+    set +x
+    echo
+
+    case ${setup} in
+
+        onefreq)
+            ;;
+
+        *)
+            echo "=== Setup '${setup}': Check for non-singular semicoherent dimensions ==="
+            set -x
+            semi_ntmpl_prev=1
+            for dim in SSKYA SSKYB NU1DOT NU0DOT; do
+                ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" "NSEMITMPL ${dim}" > tmp
+                semi_ntmpl=`cat tmp | xargs printf "%d"`
+                expr ${semi_ntmpl} '>' ${semi_ntmpl_prev}
+                semi_ntmpl_prev=${semi_ntmpl}
+            done
+            set +x
+            echo
+
+    esac
+
+    echo "=== Setup '${setup}': Perform interpolating search with frequency partitions ==="
+    set -x
+    ${builddir}/lalapps_Weave --freq-partitions=5 --output-file=WeaveOutPart.fits \
+        --toplists=all --toplist-limit=2321 --misc-info --setup-file=WeaveSetup.fits \
+        --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
+        --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
+        ${weave_search_options}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Check that no results were recomputed ==="
+    set -x
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" 'NCOHRES' > tmp
+    coh_nres_no_part=`cat tmp | xargs printf "%d"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" 'NCOHTPL' > tmp
+    coh_ntmpl_no_part=`cat tmp | xargs printf "%d"`
+    expr ${coh_nres_no_part} '=' ${coh_ntmpl_no_part}
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutPart.fits[0]" 'NCOHRES' > tmp
+    coh_nres_part=`cat tmp | xargs printf "%d"`
+    ${fitsdir}/lalapps_fits_header_getval "WeaveOutPart.fits[0]" 'NCOHTPL' > tmp
+    coh_ntmpl_part=`cat tmp | xargs printf "%d"`
+    expr ${coh_nres_part} '=' ${coh_ntmpl_part}
+    set +x
+    echo
+
+    echo "=== Setup '${setup}': Compare F-statistics from lalapps_Weave without/with frequency partitions ==="
+    set -x
+    env LAL_DEBUG_LEVEL="${LAL_DEBUG_LEVEL},info" ${builddir}/lalapps_WeaveCompare --setup-file=WeaveSetup.fits --result-file-1=WeaveOutNoPart.fits --result-file-2=WeaveOutPart.fits
+    set +x
+    echo
+
 done
-set +x
-echo
-
-echo "=== Perform non-interpolating search with frequency partitions ==="
-set -x
-${builddir}/lalapps_Weave --freq-partitions=5 --output-file=WeaveOutPart.fits \
-    --toplists=all --toplist-limit=2321 --misc-info --setup-file=WeaveSetup.fits \
-    --rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 \
-    --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt \
-    --alpha=0.9/1.4 --delta=-1.2/2.3 --freq=49.5/1e-3 --f1dot=-1e-9,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3
-set +x
-echo
-
-echo "=== Check that no results were recomputed ==="
-set -x
-${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" 'NCOHRES' > tmp
-coh_nres_no_part=`cat tmp | xargs printf "%d"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutNoPart.fits[0]" 'NCOHTPL' > tmp
-coh_ntmpl_no_part=`cat tmp | xargs printf "%d"`
-expr ${coh_nres_no_part} '=' ${coh_ntmpl_no_part}
-${fitsdir}/lalapps_fits_header_getval "WeaveOutPart.fits[0]" 'NCOHRES' > tmp
-coh_nres_part=`cat tmp | xargs printf "%d"`
-${fitsdir}/lalapps_fits_header_getval "WeaveOutPart.fits[0]" 'NCOHTPL' > tmp
-coh_ntmpl_part=`cat tmp | xargs printf "%d"`
-expr ${coh_nres_part} '=' ${coh_ntmpl_part}
-set +x
-echo
-
-echo "=== Compare F-statistics from lalapps_Weave without/with frequency partitions ==="
-set -x
-env LAL_DEBUG_LEVEL="${LAL_DEBUG_LEVEL},info" ${builddir}/lalapps_WeaveCompare --setup-file=WeaveSetup.fits --result-file-1=WeaveOutNoPart.fits --result-file-2=WeaveOutPart.fits
-set +x
-echo
