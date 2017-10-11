12e9ffa2556fda930bfb4093e7408779a54c209e
Karl Wette <karl.wette@ligo.org>
2017-10-11 18:41:58 +1100

    Weave/TestCacheMaxSize: reduce tolerance on recomputed results back to zero
    - Possible after changes in commit 7ca2343afaa59dc6d0692e97c9170e07ade0648b
    
    Original lalsuite-weave: f2670dedd179a122ce4460b8ffe4c8e5451257a5

--- lalapps/src/pulsar/Weave/TestCacheMaxSize.sh
+++ lalapps/src/pulsar/Weave/TestCacheMaxSize.sh
@@ -12,7 +12,7 @@ for setup in short long; do
             weave_sft_options="--rand-seed=3456 --sft-timebase=1800 --sft-noise-psd=1,1 --sft-timestamps-files=timestamps-1.txt,timestamps-2.txt"
             weave_search_options="--alpha=0.9/1.4 --delta=-1.2/2.3 --freq=50.5/0.01 --f1dot=-1.5e-9,0 --semi-max-mismatch=5 --coh-max-mismatch=0.3"
             weave_cache_options="--cache-max-size=2"
-            weave_recomp_threshold=0.02
+            weave_recomp_threshold=0.0
             ;;
 
         long)
@@ -21,7 +21,7 @@ for setup in short long; do
             weave_sft_options=
             weave_search_options="--simulate-search --alpha=2.3/0.9 --delta=-1.2/2.3 --freq=50.5/0.01 --f1dot=-5e-11,0 --semi-max-mismatch=6 --coh-max-mismatch=0.3"
             weave_cache_options="--cache-max-size=25"
-            weave_recomp_threshold=0.02
+            weave_recomp_threshold=0.0
             ;;
 
         *)
