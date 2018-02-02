787ddd8b80d4f3ae7b7c20d53cca5253be34e1a0
Leo Singer <leo.singer@ligo.org>
2018-02-02 00:31:06 -0500

    Don't import pyplot at module level
    Importing pyplot has lots of side effects. It should be imported
    at the very last possible moment.

--- lalinference/python/lalinference/plot/spindisk.py
+++ lalinference/python/lalinference/plot/spindisk.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 import os
-from matplotlib import pyplot as plt
 from matplotlib.lines import Line2D
 
 __all__=('make_disk_plot',)
 
 def make_disk_plot(post,outpath=None):
+  from matplotlib import pyplot as plt
   from matplotlib import rc
   rc('text', usetex=True)
   rc('font', family='lmodern')
