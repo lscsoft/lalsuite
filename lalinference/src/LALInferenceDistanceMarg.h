00b7ddc976b742ef4db30e8d5c37552b772e92c7
John Veitch <john.veitch@ligo.org>
2018-03-20 14:55:09 -0700

    Add files for distance marg

---
+++ lalinference/src/LALInferenceDistanceMarg.h
@@ -0,0 +1,24 @@
+//
+//  LALInferenceDistanceMarg.h
+//  
+//
+//  Created by John Veitch on 20/03/2018.
+//
+
+#ifndef LALInferenceDistanceMarg_h
+#define LALInferenceDistanceMarg_h
+
+#include <stdio.h>
+
+/**
+ * Compute the integral
+ Int p(d | D, rho_opt, rho_mf) dD
+ * D is luminosity distance
+ * rho_opt is optimal SNR at 1 Mpc
+ * rho_mf is <d|h> at 1 Mpc
+ */
+double dist_integral(double rho_opt, double rho_match);
+
+double dist_snr_pdf(double rho_opt, double rho_match, double dL);
+
+#endif /* LALInferenceDistanceMarg_h */
