84f77eba91bb6386e5c8016631500f9a90b01a47
John Veitch <john.veitch@ligo.org>
2018-03-25 19:07:07 +0100

    Store injection sample in HDF5 file

--- lalinference/src/LALInferenceReadData.c
+++ lalinference/src/LALInferenceReadData.c
@@ -2189,7 +2189,7 @@ void LALInferenceInjectionToVariables(SimInspiralTable *theEventTable, LALInfere
 
 }
 
-void LALInferencePrintInjectionSample(LALInferenceRunState *runState) {
+LALInferenceVariables *LALInferencePrintInjectionSample(LALInferenceRunState *runState) {
     int errnum=0;
     char *fname=NULL;
     char defaultname[]="injection_params.dat";
@@ -2208,7 +2208,7 @@ void LALInferencePrintInjectionSample(LALInferenceRunState *runState) {
 
     ProcessParamsTable *ppt = LALInferenceGetProcParamVal(runState->commandLine,"--inj");
     if (!ppt)
-        return;
+        return(NULL);
 
     SimInspiralTableFromLIGOLw(&injTable, ppt->value, 0, 0);
 
@@ -2239,7 +2239,7 @@ void LALInferencePrintInjectionSample(LALInferenceRunState *runState) {
 
     if (!(approx && order)){
         fprintf(stdout,"Unable to print injection sample: No approximant/PN order set\n");
-        return;
+        return(NULL);
     }
     /* Fill named variables */
     LALInferenceInjectionToVariables(theEventTable, injparams);
@@ -2275,9 +2275,8 @@ void LALInferencePrintInjectionSample(LALInferenceRunState *runState) {
     LALInferencePrintSample(outfile, injparams);
 
     fclose(outfile);
-    LALInferenceClearVariables(injparams);
-    XLALFree(injparams);
-    return;
+    //LALInferenceClearVariables(injparams);
+    return(injparams);
 }
 
 int enforce_m1_larger_m2(SimInspiralTable* injEvent){
