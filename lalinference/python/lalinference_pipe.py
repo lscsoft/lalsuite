807ed93d10841bf498c28faf99cec55231b96ab4
John Veitch <john.veitch@ligo.org>
2018-04-06 05:17:41 -0700

    Fix check for roq flag

--- lalinference/python/lalinference_pipe.py
+++ lalinference/python/lalinference_pipe.py
@@ -201,12 +201,12 @@ variations.update(add_variations(cp, 'analysis', 'roq'))
 roq_paths=[]
 def setup_roq(cp):
     use_roq=False
-    if not cp.getboolean('analysis','roq'): return
     if cp.has_option('paths','roq_b_matrix_directory'):
         if not cp.getboolean('analysis','roq'):
             print("Warning: If you are attempting to enable ROQ by specifying paths.roq_b_matrix_directory,\
             please use analysis.roq in your config file in future")
             return
+    if not cp.getboolean('analysis','roq'): return
     from numpy import genfromtxt, array
     path=cp.get('paths','roq_b_matrix_directory')
     if not os.path.isdir(path):
