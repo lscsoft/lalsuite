bd681cab40c6ecad3e20c23b6dbf17a47a0ff038
Soichiro Morisaki <soichiro.morisaki@ligo.org>
2019-03-11 07:57:30 -0700

    lalinference_pipe_utils.py: force ifos to be user-specified ifos

--- lalinference/python/lalinference/lalinference_pipe_utils.py
+++ lalinference/python/lalinference/lalinference_pipe_utils.py
@@ -1357,6 +1357,8 @@ class LALInferencePipelineDAG(pipeline.CondorDAG):
         Will determine the data to be read and the output file.
         Will use all IFOs known to the DAG, unless otherwise specified as a list of strings
         """
+        if self.config.has_option('analysis','ifos'):
+            ifos = ast.literal_eval(self.config.get('analysis','ifos'))
         if ifos is None and len(event.ifos)>0:
             ifos=event.ifos
         if ifos is None:
