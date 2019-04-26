43606d56206e51a7ad747093767b64de5a98376c
Adam Mercer <adam.mercer@ligo.org>
2019-04-25 18:31:49 -0700

    WIP: switch to lalInferenceVCSInfo

--- lalinference/src/LALInferenceMCMCSampler.c
+++ lalinference/src/LALInferenceMCMCSampler.c
@@ -49,7 +49,7 @@
 #include <lal/LALInferenceHDF5.h>
 #include <sys/time.h>
 
-#include <LALAppsVCSInfo.h>
+#include <lal/LALInferenceVCSInfo.h>
 #include <lal/LALStdlib.h>
 
 #define PROGRAM_NAME "LALInferenceMCMCSampler.c"
@@ -1500,7 +1500,7 @@ void LALInferencePrintPTMCMCHeaderFile(LALInferenceRunState *runState, LALInfere
         benchmark = 1;
 
     /* Print version info */
-    fprintf(threadoutput, "  LALInference version:%s,%s,%s,%s,%s\n", lalAppsVCSInfo.vcsId, lalAppsVCSInfo.vcsDate, lalAppsVCSInfo.vcsBranch, lalAppsVCSInfo.vcsAuthor, lalAppsVCSInfo.vcsStatus);
+    fprintf(threadoutput, "  LALInference version:%s,%s,%s,%s,%s\n", lalInferenceVCSInfo.vcsId, lalInferenceVCSInfo.vcsDate, lalInferenceVCSInfo.vcsBranch, lalInferenceVCSInfo.vcsAuthor, lalInferenceVCSInfo.vcsStatus);
     fprintf(threadoutput,"  %s\n", arg_str);
 
     /* Print algorithm parameters */
