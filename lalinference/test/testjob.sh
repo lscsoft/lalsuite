1c5368620e81d3aca7607b8c762b6e2b23d567fc
Shrobana Ghosh <shrobana.ghosh@ligo.org>
2019-08-27 10:20:50 -0700

    edited testjob

--- lalinference/test/testjob.sh
+++ lalinference/test/testjob.sh
@@ -1,6 +1,7 @@
 HERE=$( cd "$(dirname $(readlink -f "${BASH_SOURCE[0]}" ) )"/.. && pwd)
+export OMP_NUM_THREADS=1
 sed \
-  -e 's|/home/albert.einstein/opt/lalsuite/||'\
+  -e 's|/home/albert.einstein/opt/lalsuite/| |'\
   -e "/ligo-skymap-from-samples=/c\ligo-skymap-from-samples=/bin/true" \
   -e "/ligo-skymap-plot=/c\ligo-skymap-plot=/bin/true" \
   -e "/email=/c\email="test" " \
@@ -11,7 +12,9 @@ sed \
   -e "/approx=SEOBNRv2_ROM_DoubleSpinpseudoFourPN,IMRPhenomPv2pseudoFourPN/c\approx=IMRPhenomPv2pseudoFourPN" \
   -e 's|roq=False|roq=True|' \
   -e "/nparallel=/c\nparallel=1" \
-  -e 's|nlive=512|nlive=256 adapt-tau=3|' \
+  -e 's|tolerance=0.1|tolerance=100|' \
+  -e 's|nlive=512|nlive=256\
+adapt-tau=3|' \
   -e 's|# randomseed=4321|randomseed=4321|' \
   -e "/mpirun=/c\mpirun=/bin/true " \
   -e 's|mpi_task_count=8|mpi_task_count=4|' \
@@ -20,6 +23,6 @@ sed \
   -e 's|#notification=Complete|notification=Complete|' \
   -e "/accounting_group=/c\accounting_group=ligo.dev.o3.cbc.pe.lalinference" \
 ${HERE}/src/lalinference_pipe_example.ini > example.ini
-#lalinference_pipe --run-path ./example -I injection.xml --daglog-path ./daglog ./example.ini
-#cd example/4s
-# time ./lalinference_441417463-441417627.sh
+lalinference_pipe --run-path ./example -I injection_standard.xml --daglog-path ./daglog ./example.ini
+cd example/4s
+time ./lalinference_441417463-441417627.sh
