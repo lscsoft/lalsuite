e142a72edb26fd4a51a6fcdf40fbc33b580e208b
Shrobana Ghosh <shrobana.ghosh@ligo.org>
2019-08-27 16:22:23 -0700

    edited injection_standard path

--- lalinference/test/testjob.sh
+++ lalinference/test/testjob.sh
@@ -23,6 +23,6 @@ adapt-tau=3|' \
   -e 's|#notification=Complete|notification=Complete|' \
   -e "/accounting_group=/c\accounting_group=ligo.dev.o3.cbc.pe.lalinference" \
 ${HERE}/src/lalinference_pipe_example.ini > example.ini
-lalinference_pipe --run-path ./example -I injection_standard.xml --daglog-path ./daglog ./example.ini
+lalinference_pipe --run-path ./example -I lalinference/test/injection_standard.xml --daglog-path ./daglog ./example.ini
 cd example/4s
 time ./lalinference_441417463-441417627.sh
