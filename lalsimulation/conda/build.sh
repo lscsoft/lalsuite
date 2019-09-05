98819346cd5d5a777f97240d78b796c320bcd39d
Duncan Macleod <duncan.macleod@ligo.org>
2019-09-05 16:05:20 +0200

    lalsimulation: try again

--- lalsimulation/conda/build.sh
+++ lalsimulation/conda/build.sh
@@ -5,6 +5,7 @@ set -e
 # when running on gitlab-ci, we are not using a production
 # build, so we don't want to use NDEBUG
 export CPPFLAGS=${CPPFLAGS//-DNDEBUG/}
+export DEBUG_CPPFLAGS=${DEBUG_CPPFLAGS//-DNDEBUG/}
 
 # only link libraries we actually use
 export GSL_LIBS="-L${PREFIX}/lib -lgsl"
