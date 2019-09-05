8004e9087d21d69ede35346be3ed35bf49eb6d8c
Duncan Macleod <duncan.macleod@ligo.org>
2019-09-05 15:05:23 +0200

    lalsimulation: don't use NDEBUG in conda CI

--- lalsimulation/conda/build.sh
+++ lalsimulation/conda/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# when running on gitlab-ci, we are not using a production
+# build, so we don't want to use NDEBUG
+export CPPFLAGS=${CPPFLAGS//-DNDEBUG/}
+
 # only link libraries we actually use
 export GSL_LIBS="-L${PREFIX}/lib -lgsl"
 
