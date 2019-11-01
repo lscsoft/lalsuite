faa4920ab501c5312566c393ab9c89efdc3d13d5
Duncan Macleod <duncan.macleod@ligo.org>
2019-11-01 11:38:38 -0700

    test: fix bug in testing python stuff from tarball

--- test/test_python_scripts.py
+++ test/test_python_scripts.py
@@ -21,7 +21,7 @@ import pytest
 
 HERE = Path(__file__).parent.absolute()
 TOPDIR = HERE.parent.parent
-SUBPACKAGE = TOPDIR.name
+SUBPACKAGE = TOPDIR.name.split("-", 1)[0]
 PYTHONDIR = TOPDIR / "python"
 
 
