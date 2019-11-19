a5ac30bbcc084ab6211954a4bd27d647f276fc56
Duncan Macleod <duncan.macleod@ligo.org>
2019-11-19 13:55:41 +0000

    lalinference: set randomseed for integration test

--- lalinference/test/lalinference_nestedSampling_integration_test.sh
+++ lalinference/test/lalinference_nestedSampling_integration_test.sh
@@ -11,9 +11,11 @@ sed \
   -e "/ligo-skymap-plot=/c\ligo-skymap-plot=/bin/true" \
   -e "/email=/c\email="test" " \
   -e 's|albert.einstein/public_html/|'$USER'/public_html/LVC/|' \
-  -e 's|#fake-cache|fake-cache|' \
-  -e 's|# dataseed|dataseed|' \
-  -e 's|#0noise=|0noise=|' \
+  -e 's|^#.*fake-cache=|fake-cache=|' \
+  -e 's|^#.*dataseed|dataseed|' \
+  -e 's|^#.*randomseed|randomseed|' \
+  -e 's|^#.*0noise=|0noise=|' \
+  -e 's|^#.*notification=Complete|notification=Complete|' \
   -e "/approx=SEOBNRv2_ROM_DoubleSpinpseudoFourPN,IMRPhenomPv2pseudoFourPN/c\approx=IMRPhenomPv2pseudoFourPN" \
   -e 's|roq=False|roq=True|' \
   -e 's|/home/cbc|'$PWD'|' \
@@ -21,12 +23,10 @@ sed \
   -e "/tolerance=/c\tolerance=20" \
   -e 's|nlive=512|nlive=256\
 adapt-tau=3|' \
-  -e 's|# randomseed=4321|randomseed=4321|' \
   -e "/mpirun=/c\mpirun=/bin/true " \
   -e 's|mpi_task_count=8|mpi_task_count=4|' \
   -e 's|machine-count=8|machine-count=4|' \
   -e 's|ntemps=8|ntemps=4|' \
-  -e 's|#notification=Complete|notification=Complete|' \
   -e "/accounting_group=/c\accounting_group=ligo.dev.o3.cbc.pe.lalinference" \
   -e 's|sharedfs=False|sharedfs=True|' \
   -e 's|^resume=|#resume=|' \
