c4f257a0b2be8223858b011e9db56dbb2dce0545
NV KRISHNENDU <nv.krishnendu@ldas-grid.ligo-wa.caltech.edu>
2019-11-22 08:25:52 -0800

    removing the modifications from cbcBayesPPAnalysis.py

--- lalinference/bin/cbcBayesPPAnalysis.py
+++ lalinference/bin/cbcBayesPPAnalysis.py
@@ -83,11 +83,7 @@ posterior_name_to_latex_name = {
     'dbeta1':r'$d\beta_1$',
     'dbeta2':r'$d\beta_2$',
     'dbeta3':r'$d\beta_3$',
-    'dbeta4':r'$d\beta_4$',
-    'dquadmons':r'$\delta\kappa_s$',
-    'dquadmona':r'$\delta\kappa_a$',
-    'dquadmon1':r'$\delta\kappa_1$',
-    'dquadmon2':r'$\delta\kappa_2$'
+    'dbeta4':r'$d\beta_4$'
 }
 
 def fractional_rank(x, xs):
