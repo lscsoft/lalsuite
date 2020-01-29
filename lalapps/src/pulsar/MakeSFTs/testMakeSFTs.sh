f73da511fc1fcd34d3c976fdb095f9c1493bd5cd
Karl Wette <karl.wette@ligo.org>
2020-01-29 21:53:09 +1100

    Use LALApps CW test compiler for tests under MakeSFTs/

--- lalapps/src/pulsar/MakeSFTs/testMakeSFTs.sh
+++ lalapps/src/pulsar/MakeSFTs/testMakeSFTs.sh
@@ -1,40 +1,16 @@
-#!/bin/bash
-
-## set LAL debug level
-echo "Setting LAL_DEBUG_LEVEL=${LAL_DEBUG_LEVEL:-msglvl1,memdbg}"
-export LAL_DEBUG_LEVEL
-
-## allow 'make test' to work from builddir != srcdir
-if [ -z "${srcdir}" ]; then
-    srcdir=`dirname $0`
-fi
-builddir=".";
-injectdir="../Injections"
-sftdir="../SFTTools"
-
-## make sure we work in 'C' locale here to avoid awk sillyness
-export LC_ALL=C
-
-## define output directory and files
-testdir=testMakeSFTs.dir
-rm -rf $testdir
-mkdir -p $testdir
-
-## ----- start test -----
-
 ## common variables
 tstart=1257741529
 Tsft=1800
 Band=256
 
 ## run MFDv5 to create a fake frame and equivalent SFT
-cmdline="$injectdir/lalapps_Makefakedata_v5 --IFOs H1 --sqrtSX 1e-24 --startTime ${tstart} --duration ${Tsft} --fmin 0 --Band ${Band} --injectionSources '{Alpha=0.1; Delta=0.4; Freq=50; f1dot=1e-10; h0=1e-24; cosi=0.7; refTime=${tstart}}' --outFrameDir $testdir/ --outSFTdir $testdir/"
+cmdline="lalapps_Makefakedata_v5 --IFOs H1 --sqrtSX 1e-24 --startTime ${tstart} --duration ${Tsft} --fmin 0 --Band ${Band} --injectionSources '{Alpha=0.1; Delta=0.4; Freq=50; f1dot=1e-10; h0=1e-24; cosi=0.7; refTime=${tstart}}' --outFrameDir . --outSFTdir ."
 if ! eval "$cmdline"; then
     echo "ERROR: something failed when running '$cmdline'"
     exit 1
 fi
-MFDv5gwf="$testdir/H-H1_mfdv5-${tstart}-${Tsft}.gwf"
-MFDv5sft="$testdir/H-1_H1_${Tsft}SFT_mfdv5-${tstart}-${Tsft}.sft"
+MFDv5gwf="./H-H1_mfdv5-${tstart}-${Tsft}.gwf"
+MFDv5sft="./H-1_H1_${Tsft}SFT_mfdv5-${tstart}-${Tsft}.sft"
 for file in $MFDv5gwf $MFDv5sft; do
     if ! test -f $file; then
         echo "ERROR: could not find file '$file'"
@@ -43,18 +19,18 @@ for file in $MFDv5gwf $MFDv5sft; do
 done
 
 ## make a cache file for the MFDv5 frame
-framecache="$testdir/framecache"
+framecache="./framecache"
 echo "H H1_mfdv5 ${tstart} ${Tsft} file://localhost$PWD/$MFDv5gwf" > $framecache
 
 ## run MakeSFTs to create an SFT from the fake frame
 tend=`echo "${tstart} + ${Tsft}" | bc`
 Band2=`echo "${Band} + 0.0006" | bc`   ## add 0.0006 to bandwidth to get same number of SFT bins as MDFv5
-cmdline="$builddir/lalapps_MakeSFTs -f 0 -t ${Tsft} -p $testdir -C $framecache -s ${tstart} -e ${tend} -N H1:mfdv5 -v 2 -i H1 -u PROC_REAL8 -w 0 -F 0 -B ${Band2} -D 4 -X MSFT"
+cmdline="lalapps_MakeSFTs -f 0 -t ${Tsft} -p . -C $framecache -s ${tstart} -e ${tend} -N H1:mfdv5 -v 2 -i H1 -u PROC_REAL8 -w 0 -F 0 -B ${Band2} -D 4 -X MSFT"
 if ! eval "$cmdline"; then
     echo "ERROR: something failed when running '$cmdline'"
     exit 1
 fi
-MSFTsft="$testdir/H-1_H1_${Tsft}SFT_MSFT-1257/H-1_H1_${Tsft}SFT_MSFT-${tstart}-${Tsft}.sft"
+MSFTsft="./H-1_H1_${Tsft}SFT_MSFT-1257/H-1_H1_${Tsft}SFT_MSFT-${tstart}-${Tsft}.sft"
 for file in $MSFTsft; do
     if ! test -f $file; then
         echo "ERROR: could not find file '$file'"
@@ -64,14 +40,9 @@ done
 
 ## compare SFTs produced by MFDv5 and MakeSFTs, should be very nearly identical
 tol=1e-10
-cmdline="$sftdir/lalapps_compareSFTs -V -e $tol -1 $MFDv5sft -2 $MSFTsft"
+cmdline="lalapps_compareSFTs -V -e $tol -1 $MFDv5sft -2 $MSFTsft"
 echo "Comparing SFTs produced by MFDv5 and MakeSFTs, allowed tolerance=$tol:"
 if ! eval "$cmdline"; then
     echo "ERROR: something failed when running '$cmdline'"
     exit 1
-fi
-
-## clean up files
-if [ -z "$NOCLEANUP" ]; then
-    rm -rf $testdir
 fi
