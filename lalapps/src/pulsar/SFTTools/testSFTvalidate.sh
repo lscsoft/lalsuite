bd145a44938ed5262183c6ac0908e0dd348e787b
Karl Wette <karl.wette@ligo.org>
2020-02-06 17:03:20 +1100

    testSFTvalidate: use $sft instead of $i

--- lalapps/src/pulsar/SFTTools/testSFTvalidate.sh
+++ lalapps/src/pulsar/SFTTools/testSFTvalidate.sh
@@ -1,25 +1,22 @@
 ## create good and bad SFTs
 SFTwrite
 
-goodSFTs="SFT-good SFT-test*"
-badSFTs="SFT-bad*"
-
 ## these SFTs have to pass
-for i in $goodSFTs; do
-    echo "lalapps_SFTvalidate ./$i"
-    if ! lalapps_SFTvalidate ./$i; then
-        echo "lalapps_SFTvalidate failed SFT $i; should have passed"
+for sft in SFT-good SFT-test*; do
+    echo "lalapps_SFTvalidate ./$sft"
+    if ! lalapps_SFTvalidate ./$sft; then
+        echo "lalapps_SFTvalidate failed SFT $sft; should have passed"
         exit 1
     fi
     echo
 done
 
 ## these SFTs have to fail
-for i in $badSFTs; do
-    echo "lalapps_SFTvalidate ./$i"
-    if lalapps_SFTvalidate ./$i; then
-        echo "lalapps_SFTvalidate passed SFT $i; should have failed"
-	exit 1
+for sft in SFT-bad*; do
+    echo "lalapps_SFTvalidate ./$sft"
+    if lalapps_SFTvalidate ./$sft; then
+        echo "lalapps_SFTvalidate passed SFT $sft; should have failed"
+        exit 1
     fi
     echo
 done
