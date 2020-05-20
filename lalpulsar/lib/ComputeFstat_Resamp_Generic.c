3525e4cffa62e0a83e2b229fbcbcd8b122690fe0
Karl Wette <karl.wette@ligo.org>
2020-05-20 14:32:44 +1000

    ComputeFstat: move common resampling structs to ComputeFstat_Resamp_internal.h

--- lalpulsar/lib/ComputeFstat_Resamp_Generic.c
+++ lalpulsar/lib/ComputeFstat_Resamp_Generic.c
@@ -26,6 +26,7 @@
 #include <fftw3.h>
 
 #include "ComputeFstat_internal.h"
+#include "ComputeFstat_Resamp_internal.h"
 
 #include <lal/FFTWMutex.h>
 #include <lal/Factorial.h>
@@ -46,74 +47,11 @@
 
 // ========== Resamp internals ==========
 
+// ----- local constants ----------
+
 // ----- local macros ----------
 
-// local macro versions of library functions to avoid calling external functions in GPU-ready code
-#define GPSDIFF(x,y) (1.0*((x).gpsSeconds - (y).gpsSeconds) + ((x).gpsNanoSeconds - (y).gpsNanoSeconds)*1e-9)
-#define GPSGETREAL8(x) ( (x)->gpsSeconds + ( (x)->gpsNanoSeconds / XLAL_BILLION_REAL8 ) );
-#define GPSSETREAL8(gps,r8) do {                                        \
-    (gps).gpsSeconds     = (UINT4)floor(r8);                            \
-    (gps).gpsNanoSeconds = (UINT4)round ( ((r8) - (gps).gpsSeconds) * XLAL_BILLION_REAL8 ); \
-    if ( (gps).gpsNanoSeconds == XLAL_BILLION_INT4 ) {                  \
-      (gps).gpsSeconds += 1;                                            \
-      (gps).gpsNanoSeconds = 0;                                         \
-    }                                                                   \
-  } while(0)
-
-
-// ----- local constants
-
 // ----- local types ----------
-
-// ---------- BEGIN: Resamp-specific timing model data ----------
-typedef struct tagTimings_t
-{
-  REAL4 Total;		// total time spent in XLALComputeFstatResamp()
-  REAL4 Bary;		// time spent (in this call) in barycentric resampling
-  REAL4 Spin;		// time spent in spindown+frequency correction
-  REAL4 FFT;		// time spent in FFT
-  REAL4 Copy;		// time spent copying results from FFT to FabX
-  REAL4 Norm;		// time spent normalizing the final Fa,Fb
-  REAL4 Fab2F;		// time to compute Fstat from {Fa,Fb}
-  REAL4 Mem;		// time to realloc and Memset-0 arrays
-  REAL4 SumFabX;	// time to sum_X Fab^X
-  BOOLEAN BufferRecomputed; // did we need to recompute the buffer this time?
-} Timings_t;
-
-// Resamp-specific timing model data
-typedef struct tagFstatTimingResamp
-{
-  UINT4 NsampFFT0;		// original number of FFT samples (not rounded to power-of-two)
-  UINT4 NsampFFT;		// actual number of FFT samples (rounded up to power-of-two if optArgs->resampFFTPowerOf2 == true)
-  REAL4 Resolution;	// (internal) frequency resolution 'R' in natural units: df_internal = R / T_FFT\n
-
-  REAL4 tau0_Fbin;      // timing coefficient for all contributions scaling with output frequency-bins
-  REAL4 tau0_spin;      // timing coefficient for spindown-correction
-  REAL4 tau0_FFT;       // timing coefficient for FFT-time
-  REAL4 tau0_bary;      // timing coefficient for barycentering
-
-  Timings_t Tau;
-
-} FstatTimingResamp;
-
-static const char FstatTimingResampHelp[] =
-  "%%%% ----- Resampling-specific timing model -----\n"
-  "%%%% NsampFFT0:      original number of FFT samples (not yet rounded up to power-of-two)\n"
-  "%%%% NsampFFT:       actual number of FFT samples (rounded to power-of-two if optArgs->resampFFTPowerOf2 == true)\n"
-  "%%%% R:              (internal) frequency resolution in natural units: df_internal = R / T_FFT\n"
-  "%%%%\n"
-  "%%%% tau0_Fbin:      timing coefficient for all contributions scaling with output frequency-bins\n"
-  "%%%% tau0_spin:      timing coefficient for spindown-correction\n"
-  "%%%% tau0_FFT:       timing coefficient for FFT-time\n"
-  "%%%% tau0_bary:      timing coefficient for barycentering\n"
-  "%%%%\n"
-  "%%%% Resampling F-statistic timing model:\n"
-  "%%%% tauF_core       = tau0_Fbin + (NsampFFT/NFbin) * ( R * tau0_spin + 5 * log2(NsampFFT) * tau0_FFT )\n"
-  "%%%% tauF_buffer     = R * NsampFFT * tau0_bary / NFbin\n"
-  "%%%%"
-  "";
-// ---------- END: Resamp-specific timing model data ----------
-
 
 // ----- workspace ----------
 typedef struct tagResampWorkspace
