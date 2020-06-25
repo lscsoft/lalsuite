47c4519c2bdd79f0c39e855f30daa25146dc2830
Pep Covas <pep.covas@ligo.org>
2020-06-25 06:04:42 -0500

    SFDB reading: various fixes

--- lalapps/src/pulsar/SFTTools/WriteSFTsfromSFDBs.c
+++ lalapps/src/pulsar/SFTTools/WriteSFTsfromSFDBs.c
@@ -35,13 +35,12 @@
 
 typedef struct
 {
-    UINT4 numifo;
     REAL8 fmin;
     REAL8 fmax;
     CHAR *file_pattern;
     CHAR *timeStampsStarting;
     CHAR *timeStampsFinishing;
-    CHAR *outputSingleSFT;
+    CHAR *outSFTnames;
 } UserInput_t;
 
 int initUserVars ( UserInput_t *uvar );
@@ -62,46 +61,25 @@ int main(int argc, char *argv[]) {
 
     MultiSFTVector *inputSFTs = NULL;
     inputSFTs =  XLALReadSFDB(uvar.fmin, uvar.fmax, uvar.file_pattern, uvar.timeStampsStarting, uvar.timeStampsFinishing);
+    XLAL_CHECK_MAIN(inputSFTs != NULL, XLAL_EFUNC);
+
+    LALStringVector *fnames;
+    XLAL_CHECK_NULL ( (fnames = XLALFindFiles (uvar.outSFTnames)) != NULL, XLAL_EFUNC, "Failed to find filelist matching pattern '%s'.\n\n", uvar.outSFTnames );
 
 
-    for ( UINT4 k = 0; k < uvar.numifo; k++) {
+    for ( UINT4 k = 0; k < inputSFTs->length; k++) {
 
         FILE *fpSingleSFT = NULL;
-        CHAR   fileSigma[ 500 ];
-        strcpy ( fileSigma, uvar.outputSingleSFT);
-        if (uvar.numifo>1) {
-            if (k==0) strcat ( fileSigma, "_H1");  // Assuming that the returned SFTs are alphabetically sorted in the first index
-            if (k==1) strcat ( fileSigma, "_L1");
-            if (k==2) strcat ( fileSigma, "_V1");
-        }
-        XLAL_CHECK ( ( fpSingleSFT = fopen ( fileSigma, "wb" )) != NULL, XLAL_EIO, "Failed to open singleSFT file '%s' for writing\n", uvar.outputSingleSFT );
+        const CHAR *filenameOut = fnames->data[k];
+        XLAL_CHECK ( ( fpSingleSFT = fopen ( filenameOut, "wb" )) != NULL, XLAL_EIO, "Failed to open singleSFT file '%s' for writing\n", filenameOut );
 
         UINT4 numSFTs = inputSFTs->data[k]->length;
         for ( UINT4 i=0; i < numSFTs; i++ )
         {   
             const SFTtype *thisSFT = inputSFTs->data[k]->data + i;
 
-            /*const CHAR *sft_comment;
-            CHAR *new_comment;
-            UINT4 comment_len = 0;
-            comment_len = strlen ( add_comment ) + 10;
-            sft_comment = oneSFTCatalog.data->comment;
-            if ( sft_comment ) comment_len += strlen ( sft_comment );
-            XLAL_CHECK_MAIN ( ( new_comment  = LALCalloc (1, comment_len )) != NULL, XLAL_ENOMEM );
-            if ( sft_comment ) {
-              strcpy ( new_comment, sft_comment );
-              strcat ( new_comment, ";\n");
-            }
-            strcat ( new_comment, add_comment );*/
-        
-
-        
-            // if user asked for single-SFT output, add this SFT to the open file
-            //if ( uvar.outputSingleSFT ) XLAL_CHECK ( XLAL_SUCCESS == XLALWriteSFT2fp( &(thisSFT->data[0]), fpSingleSFT, new_comment ), XLAL_EFUNC,  "XLALWriteSFT2fp() failed to write SFT to '%s'!\n", uvar.outputSingleSFT );
-            XLAL_CHECK ( XLAL_SUCCESS == XLALWriteSFT2fp( thisSFT, fpSingleSFT, NULL ), XLAL_EFUNC,  "XLALWriteSFT2fp() failed to write SFT to '%s'!\n", uvar.outputSingleSFT );
-                
-            //XLALFree ( new_comment );
- 
+            XLAL_CHECK ( XLAL_SUCCESS == XLALWriteSFT2fp( thisSFT, fpSingleSFT, NULL ), XLAL_EFUNC,  "XLALWriteSFT2fp() failed to write SFT to '%s'!\n", filenameOut );
+                 
         } /* for i < numSFTs */
 
         if ( fpSingleSFT ) fclose ( fpSingleSFT );
@@ -109,6 +87,7 @@ int main(int argc, char *argv[]) {
     }
 
     XLALDestroyMultiSFTVector (inputSFTs); 
+    XLALDestroyStringVector(fnames);
 
     return 0;
 
@@ -119,22 +98,20 @@ int initUserVars ( UserInput_t *uvar ) {
    
    XLAL_CHECK ( uvar != NULL, XLAL_EINVAL );
 
-   uvar->numifo = 0;
    uvar->fmin = 0;
    uvar->fmax = 0;
    uvar->file_pattern = NULL;
    uvar->timeStampsStarting = NULL;
    uvar->timeStampsFinishing = NULL;
-   uvar->outputSingleSFT = NULL;
+   uvar->outSFTnames = NULL;
 
    /* now register all our user-variable */
-   XLALRegisterUvarMember( file_pattern,            STRING, 'i', REQUIRED, "File-pattern for input SFDBs");
-   XLALRegisterUvarMember( timeStampsStarting,      STRING, 's', OPTIONAL, "File-pattern for timestamp file with starting times of SCIENCE segments");
-   XLALRegisterUvarMember( timeStampsFinishing,     STRING, 'f', OPTIONAL, "File-pattern for timestamp file with ending times of SCIENCE segments");
-   XLALRegisterUvarMember( outputSingleSFT,         STRING, 'd', REQUIRED, "File-pattern for output file");
+   XLALRegisterUvarMember( file_pattern,            STRING, 'i', REQUIRED, "String of SFDB files (possibly from more than one detector, separated by a ;)");
+   XLALRegisterUvarMember( timeStampsStarting,      STRING, 's', OPTIONAL, "File(s) containing the starting timestamps of science segments (possibly from more than one detector, separated by a ;)");
+   XLALRegisterUvarMember( timeStampsFinishing,     STRING, 'f', OPTIONAL, "File(s) containing the starting timestamps of science segments (possibly from more than one detector, separated by a ;)");
+   XLALRegisterUvarMember( outSFTnames,         STRING, 'd', REQUIRED, "Output file(s) (possibly from more than one detector, separated by a ;)");
    XLALRegisterUvarMember(   fmin,               REAL8, 0, REQUIRED, "Lowest frequency to extract from SFTs");
    XLALRegisterUvarMember(   fmax,               REAL8, 0, REQUIRED, "Highest frequency to extract from SFTs");
-   XLALRegisterUvarMember(   numifo,             UINT4, 0,  REQUIRED, "Number of detectors");
 
    return XLAL_SUCCESS;
  
