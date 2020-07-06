d90181297fdc35cee5214bc957230420bd3dec4c
Duncan Macleod <duncan.macleod@ligo.org>
2020-07-06 16:53:39 +0100

    conda: fix library installation

--- conda/install-lib.sh
+++ conda/install-lib.sh
@@ -5,7 +5,10 @@ set -ex
 cd _build
 
 # install library and headers
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C lib
+make -j ${CPU_COUNT} V=1 VERBOSE=1 -C lib install
+
+# install SWIG binding definitions and headers
+make -j ${CPU_COUNT} V=1 VERBOSE=1 -C swig install-data
 
 # install pkg-config
 make -j ${CPU_COUNT} V=1 VERBOSE=1 install-pkgconfigDATA
