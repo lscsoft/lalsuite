3ee7835404c82b3ff801138b12f66011a4c973e8
Duncan Macleod <duncan.macleod@ligo.org>
2020-08-28 13:42:30 +0100

    lalinference: restructure binary package builds

--- lalinference/conda/build.sh
+++ lalinference/conda/build.sh
@@ -6,12 +6,24 @@
 
 set -e
 
+# use out-of-tree build
+mkdir -pv _build
+cd _build
+
+# enable nightly mode for CI
+if [ "${CI_PIPELINE_SOURCE}" = "schedule" ] || [ "${CI_PIPELINE_SOURCE}" = "web" ]; then
+	EXTRA_CONFIG_FLAGS="--enable-nightly"
+fi
+
 # when running on gitlab-ci, we are not using a production
 # build, so we don't want to use NDEBUG
 export CPPFLAGS="${CPPFLAGS} -UNDEBUG"
 
+# only link libraries we actually use
+export GSL_LIBS="-L${PREFIX}/lib -lgsl"
+
 # configure
-./configure \
+${SRC_DIR}/configure \
 	--disable-doxygen \
 	--disable-python \
 	--disable-swig-octave \
@@ -21,6 +33,7 @@ export CPPFLAGS="${CPPFLAGS} -UNDEBUG"
 	--enable-openmp \
 	--enable-swig-iface \
 	--prefix="${PREFIX}" \
+	${EXTRA_CONFIG_FLAGS} \
 ;
 
 # build
@@ -28,30 +41,3 @@ make -j ${CPU_COUNT} V=1 VERBOSE=1
 
 # test
 make -j ${CPU_COUNT} V=1 VERBOSE=1 check
-
-# install
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install
-
-# -- create activate/deactivate scripts
-PKG_NAME_UPPER=$(echo ${PKG_NAME} | awk '{ print toupper($0) }')
-
-# activate.sh
-ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${ACTIVATE_SH})
-cat > ${ACTIVATE_SH} << EOF
-#!/bin/bash
-export CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR="\${${PKG_NAME_UPPER}_DATADIR:-empty}"
-export ${PKG_NAME_UPPER}_DATADIR="/opt/anaconda1anaconda2anaconda3/share/${PKG_NAME}"
-EOF
-# deactivate.sh
-DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${DEACTIVATE_SH})
-cat > ${DEACTIVATE_SH} << EOF
-#!/bin/bash
-if [ "\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}" == "empty" ]; then
-	unset ${PKG_NAME_UPPER}_DATADIR
-else
-	export ${PKG_NAME_UPPER}_DATADIR="\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}"
-fi
-unset CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR
-EOF
