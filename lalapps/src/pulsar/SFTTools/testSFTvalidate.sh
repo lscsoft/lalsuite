b80a5841b8ce512ee757489281d006897ae155d6
Karl Wette <karl.wette@ligo.org>
2021-01-14 00:23:49 +1100

    lalapps_SFTvalidate: read file names from standard input, check all file names
    - Read file names from stdin if no files names are given on command line.
    - Check all file names, then return non-zero exit code if any file failed.
      This is more convenient for checking a large number of files and finding
      all errors.

--- lalapps/src/pulsar/SFTTools/testSFTvalidate.sh
+++ lalapps/src/pulsar/SFTTools/testSFTvalidate.sh
@@ -3,20 +3,44 @@ SFTwrite
 
 ## these SFTs have to pass
 for sft in SFT-good SFT-test*; do
-    echo "lalapps_SFTvalidate ./$sft"
-    if ! lalapps_SFTvalidate ./$sft; then
+    echo "lalapps_SFTvalidate $sft"
+    if ! lalapps_SFTvalidate $sft; then
         echo "lalapps_SFTvalidate failed SFT $sft; should have passed"
         exit 1
     fi
     echo
 done
+echo "lalapps_SFTvalidate SFT-good SFT-test*"
+if ! lalapps_SFTvalidate SFT-good SFT-test*; then
+    echo "lalapps_SFTvalidate failed SFTs SFT-good SFT-test*; should have passed"
+    exit 1
+fi
+echo
+echo "printf '%s\n' SFT-good '' SFT-test* | lalapps_SFTvalidate"
+if ! printf '%s\n' SFT-good '' SFT-test* | lalapps_SFTvalidate; then
+    echo "lalapps_SFTvalidate failed SFTs SFT-good SFT-test*; should have passed"
+    exit 1
+fi
+echo
 
 ## these SFTs have to fail
 for sft in SFT-bad*; do
-    echo "lalapps_SFTvalidate ./$sft"
-    if lalapps_SFTvalidate ./$sft; then
+    echo "lalapps_SFTvalidate $sft"
+    if lalapps_SFTvalidate $sft; then
         echo "lalapps_SFTvalidate passed SFT $sft; should have failed"
         exit 1
     fi
     echo
 done
+echo "lalapps_SFTvalidate SFT-bad*"
+if lalapps_SFTvalidate SFT-bad*; then
+    echo "lalapps_SFTvalidate passed SFTs SFT-bad*; should have failed"
+    exit 1
+fi
+echo
+echo "printf '%s\n' SFT-bad* | lalapps_SFTvalidate"
+if printf '%s\n' SFT-bad* | lalapps_SFTvalidate; then
+    echo "lalapps_SFTvalidate passed SFTs SFT-bad*; should have failed"
+    exit 1
+fi
+echo
