2842220ba8d669214fada239fbaa42e64d7154a3
David Keitel <david.keitel@ligo.org>
2021-01-20 11:10:30 +0100

    testSFTclean.sh: extend to multi-SFT, multi-IFO case

--- lalapps/src/pulsar/SFTTools/testSFTclean.sh
+++ lalapps/src/pulsar/SFTTools/testSFTclean.sh
@@ -15,47 +15,50 @@ fBand=0.01
 fmax=$(echo "$fmin + $fBand" | bc)
 linefreq="50.005"
 TSFT=1800
-duration=$TSFT
+NSFTs=3
+duration=$(echo "$NSFTs * $TSFT" | bc)
 
 # ---------- define comparison functions
 awk_reim_abs='{printf "%.6e", sqrt($1*$1+$2*$2) }'
 awk_is_much_less='{if($1<0.1*$2) {print "1"}}'
 
 # ---------- set output file names
-original_SFT="H-1_H1_${TSFT}SFT_mfdv4-${startTime}-${TSFT}.sft"
-cleaned_SFT="H-1_H1_${TSFT}SFT_cleaned-${startTime}-${duration}.sft"
-dump_orig="${original_SFT}.dump"
-dump_clean="${cleaned_SFT}.dump"
+IFOs=("H1" "L1")
+IFObits=("H-1_H1" "L-1_L1")
+original_SFT_pattern="_${TSFT}SFT_mfdv4-*-${TSFT}.sft"
+cleaned_SFT_pattern="_${TSFT}SFT_cleaned-*-${TSFT}.sft"
 
 echo "----------------------------------------------------------------------"
-echo "STEP 1: creating test data with line feature..."
-echo "----------------------------------------------------------------------"
-echo
-
-cmdline="${mfd_code} --IFO=H1 --outSFTbname=. --startTime=$startTime --duration=$duration --fmin=$fmin --Band=$fBand --noiseSqrtSh=$sqrtSh --lineFeature=1 --h0=$h0 --cosi=$cosi --Freq=$linefreq --randSeed=1"
-
-echo $cmdline
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$mfd_code' ..."
-    exit 1
-fi
-
-## ----- dump original SFT
-cmdline="${dump_code} --dataOnly --SFTfiles=$original_SFT > $dump_orig"
-
-echo $cmdline
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$dump_code' ..."
-    exit 1
-fi
-
-echo
-echo "----------------------------------------------------------------------"
-echo "STEP 2: 'clean' the SFT without passing a linefile (should do nothing)..."
+echo "STEP 1: creating H1L1 test data with line features..."
 echo "----------------------------------------------------------------------"
 echo
 
-cmdline="${clean_code} --sftDir=$original_SFT --outDir=. --fMin $fmin --fMax $fmax"
+for ifo in 0 1; do
+    cmdline="${mfd_code} --IFO=${IFOs[$ifo]} --outSFTbname=. --startTime=$startTime --duration=$duration --fmin=$fmin --Band=$fBand --noiseSqrtSh=$sqrtSh --lineFeature=1 --h0=$h0 --cosi=$cosi --Freq=$linefreq --randSeed=1"
+    echo $cmdline
+    if ! eval $cmdline; then
+        echo "Error.. something failed when running '$mfd_code' ..."
+        exit 1
+    fi
+
+    ## ----- dump original SFTs
+    original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
+    dump_orig="dump_${original_SFTs}.txt"
+    cmdline="${dump_code} --dataOnly --SFTfiles=${original_SFTs} > $dump_orig"
+    echo $cmdline
+    if ! eval $cmdline; then
+        echo "Error.. something failed when running '$dump_code' ..."
+        exit 1
+    fi
+done
+
+echo
+echo "----------------------------------------------------------------------"
+echo "STEP 2: 'clean' the SFTs without passing a linefile (should do nothing)..."
+echo "----------------------------------------------------------------------"
+echo
+
+cmdline="${clean_code} --sftDir=*${original_SFT_pattern} --outDir=. --fMin $fmin --fMax $fmax"
 
 echo $cmdline
 if ! eval $cmdline; then
@@ -63,48 +66,13 @@ if ! eval $cmdline; then
     exit 1
 fi
 
-## ----- dump the "cleaned" SFT
-cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFT > $dump_clean"
-
-echo $cmdline
-if ! eval $cmdline; then
-    echo "Error.. something failed when running '$dump_code' ..."
-    exit 1
-fi
-
-diff_cmd="diff -s $dump_orig $dump_clean"
-echo $diff_cmd
-if ! eval $diff_cmd; then
-    echo "error: $dump_orig and $dump_clean should be equal as long as no --linefiles are given."
-    exit 1
-fi
-
-echo
-echo "----------------------------------------------------------------------"
-echo "STEP 3: actually clean the SFT with some simple linefiles..."
-echo "----------------------------------------------------------------------"
-echo
-
-# function to de-spaghettify multiple calls with different linefiles
-test_cleaning () {
-
-    linefile_content=$1
-    nexpected=$2
-
-    linefile="H1lines.txt"
-    echo "writing line definition '$linefile_content' to file $linefile ..."
-    echo -e $linefile_content > $linefile
-
-    cmdline="${clean_code} --sftDir=$original_SFT --outDir=. --fMin $fmin --fMax $fmax --linefiles=$linefile"
-
-    echo $cmdline
-    if ! eval $cmdline; then
-        echo "Error.. something failed when running '$clean_code' ..."
-        exit 1
-    fi
-
-    ## ----- dump the cleaned SFT
-    cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFT > $dump_clean"
+## ----- dump the "cleaned" SFTs
+for ifo in 0 1; do
+    original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
+    cleaned_SFTs="${IFObits[$ifo]}$cleaned_SFT_pattern"
+    dump_orig="dump_${original_SFTs}.txt"
+    dump_clean="dump_${cleaned_SFTs}.txt"
+    cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFTs > $dump_clean"
 
     echo $cmdline
     if ! eval $cmdline; then
@@ -112,26 +80,84 @@ test_cleaning () {
         exit 1
     fi
 
-    diff_cmd="diff --suppress-common-lines -y -s $dump_orig $dump_clean"
+    diff_cmd="diff -s $dump_orig $dump_clean"
     echo $diff_cmd
     if ! eval $diff_cmd; then
-        # This diff will returned a non-0 exit status, but that's ok, we just wanted to print it. So pass.
-        :
+        echo "error: $dump_orig and $dump_clean should be equal as long as no --linefiles are given."
+        exit 1
     fi
-    diff_cmd="diff --suppress-common-lines -y $dump_orig $dump_clean"
-    ndiff=$($diff_cmd | wc -l)
-    if [ $ndiff == $nexpected ]; then
-        echo "OK: $dump_orig and $dump_clean differ by exactly $nexpected line(s), as expected."
-    else
-        echo "error: $dump_orig and $dump_clean should differ by exactly $nexpected line(s)."
+done
+
+echo
+echo "----------------------------------------------------------------------"
+echo "STEP 3: actually clean the SFTs with some simple linefiles (L1 only)..."
+echo "----------------------------------------------------------------------"
+echo
+
+# function to de-spaghettify multiple calls with different linefiles
+test_cleaning () {
+
+    linefile_content=$1
+    nexpected=$(echo "$NSFTs * $2" | bc)
+
+    linefile="L1lines.txt"
+    echo "writing line definition '$linefile_content' to file $linefile ..."
+    echo -e $linefile_content > $linefile
+
+    cmdline="${clean_code} --sftDir=*${original_SFT_pattern} --outDir=. --fMin $fmin --fMax $fmax --linefiles=$linefile"
+
+    echo $cmdline
+    if ! eval $cmdline; then
+        echo "Error.. something failed when running '$clean_code' ..."
         exit 1
     fi
 
+    ## ----- dump the "cleaned" SFTs
+    for ifo in 0 1; do
+        original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
+        cleaned_SFTs="${IFObits[$ifo]}$cleaned_SFT_pattern"
+        dump_orig="dump_${original_SFTs}.txt"
+        dump_clean="dump_${cleaned_SFTs}.txt"
+        cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFTs > $dump_clean"
+
+        echo $cmdline
+        if ! eval $cmdline; then
+            echo "Error.. something failed when running '$dump_code' ..."
+            exit 1
+        fi
+
+        if [ "${IFOs[$ifo]}" == "L1" ]; then
+            diff_cmd="diff --suppress-common-lines -y -s $dump_orig $dump_clean"
+            echo $diff_cmd
+            if ! eval $diff_cmd; then
+                # This diff will returned a non-0 exit status, but that's ok, we just wanted to print it. So pass.
+                :
+            fi
+            diff_cmd="diff --suppress-common-lines -y $dump_orig $dump_clean"
+            ndiff=$($diff_cmd | wc -l)
+            if [ $ndiff == $nexpected ]; then
+                echo "OK: $dump_orig and $dump_clean differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s), as expected."
+            else
+                echo "error: $dump_orig and $dump_clean should differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s)."
+                exit 1
+            fi
+        else
+            diff_cmd="diff -s $dump_orig $dump_clean"
+            echo $diff_cmd
+            if ! eval $diff_cmd; then
+                echo "error: $dump_orig and $dump_clean should be equal as long as no matching --linefiles entry for ${IFOs[$ifo]} is given."
+                exit 1
+            fi
+        fi
+    done
+
 }
 
-# in this first case, we actually clean where there was an injected line,
-# so here we do the additional check if the absolute SFT value has really reduced
 test_cleaning "50.005 0.0 1 0.0 0.0 /*single_bin_line*/" "1"
+# In the first case, we actually clean L1 where there was an injected line,
+# so here we do the additional check if the absolute SFT value has really reduced.
+# But we only check the first SFT.
+# Note we rely on $diff_cmd still being set to the right L1 files from the function call.
 diff_tr=$($diff_cmd | tr -s ' ')
 freq1=$(echo $diff_tr | cut -d ' ' -f 1)
 re1=$(echo $diff_tr | cut -d ' ' -f 2)
@@ -148,9 +174,9 @@ abs1=$(echo $re1 $im1 | awk "$awk_reim_abs")
 abs2=$(echo $re2 $im2 | awk "$awk_reim_abs")
 checked=$(echo $abs2 $abs1 | awk "$awk_is_much_less")
 if [ "$checked" == "1" ]; then
-    echo "OK: Strong line at $freq1 Hz has indeed been cleaned, reducing SFT data from $abs1 to $abs2."
+    echo "OK: Strong line at $freq1 Hz has indeed been cleaned in ${IFOs[$ifo]}, reducing |data| in first SFT  from $abs1 to $abs2."
 else
-    echo "error: Strong line at $freq1 Hz should have been cleaned, but SFT data is $abs2 not at least 10 times smaller than $abs1."
+    echo "error: Strong line at $freq1 Hz should have been cleaned in ${IFOs[$ifo]}, but |data| in first SFT is $abs2 not at least 10 times smaller than $abs1."
     exit 1
 fi
 
