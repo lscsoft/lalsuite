049a700eb538fd0a628573a32b4b47e58556623b
David Keitel <david.keitel@ligo.org>
2021-01-24 13:08:17 +0100

    lalapps_SFTclean: implement same comment handling as in lalapps_splitSFTs

--- lalapps/src/pulsar/SFTTools/SFTclean.c
+++ lalapps/src/pulsar/SFTTools/SFTclean.c
@@ -51,6 +51,10 @@
 #define TRUE (1==1)
 #define FALSE (1==0)
 
+#define CMT_NONE 0
+#define CMT_OLD  1
+#define CMT_FULL 2
+
 int main(int argc, char *argv[]){ 
 
   static LALStatus       status;  /* LALStatus pointer */ 
@@ -72,7 +76,7 @@ int main(int argc, char *argv[]){
   CHAR *uvar_sftDir;    /* directory for unclean sfts */
   CHAR *uvar_outDir;   /* directory for cleaned sfts */
   REAL8 uvar_fMin, uvar_fMax;
-  INT4  uvar_window, uvar_maxBins;
+  INT4  uvar_window, uvar_maxBins, uvar_addComment;
 
   /* set defaults */
   uvar_sftDir = NULL;
@@ -81,6 +85,7 @@ int main(int argc, char *argv[]){
   uvar_fMax = -1;
   uvar_window = 100;
   uvar_maxBins = 20;
+  uvar_addComment = CMT_FULL; /* add VCS ID and full command-line to every SFT file */
 
   /* register user input variables */
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_sftDir,    "sftDir",    STRING,       'i', REQUIRED,  "Input SFT file pattern") == XLAL_SUCCESS, XLAL_EFUNC);
@@ -90,7 +95,7 @@ int main(int argc, char *argv[]){
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_window,    "window",    INT4,         'w',OPTIONAL,  "Window size for noise floor estimation in vicinity of a line") == XLAL_SUCCESS, XLAL_EFUNC);
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_maxBins,   "maxBins",   INT4,         'm',OPTIONAL,  "Max. bins to clean") == XLAL_SUCCESS, XLAL_EFUNC);
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_linefiles, "linefiles", STRINGVector, 0,   OPTIONAL, "List of per-detector files with list of lines (each full path must start with a canonical IFO name)") == XLAL_SUCCESS, XLAL_EFUNC);
-
+  XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_addComment, "addComment", INT4,       'c', OPTIONAL, "How to deal with comments - 0 means no comment is written at all, 1 means that the comment is taken unmodified from the input SFTs, 2 (default) means that the program appends its RCS id and command-line to the comment.") == XLAL_SUCCESS, XLAL_EFUNC);
 
 
   /* read all command line variables */
@@ -98,7 +103,25 @@ int main(int argc, char *argv[]){
   XLAL_CHECK_MAIN( XLALUserVarReadAllInput(&should_exit, argc, argv, lalAppsVCSInfoList) == XLAL_SUCCESS, XLAL_EFUNC);
   if (should_exit)
     exit(1);
-  
+
+  /* record VCS ID and command-line for the comment */
+  char *cmdline = NULL;
+  if ( uvar_addComment > CMT_OLD ) {
+    XLAL_CHECK_MAIN( ( cmdline = ( char * )XLALMalloc( strlen( lalAppsVCSIdentInfo.vcsId ) + strlen( lalAppsVCSIdentInfo.vcsStatus ) + 2 ) ) != NULL, XLAL_ENOMEM, "out of memory allocating cmdline" );
+    strcpy( cmdline, lalAppsVCSIdentInfo.vcsId );
+    strcat( cmdline, lalAppsVCSIdentInfo.vcsStatus );
+    strcat( cmdline, "\n" );
+    for ( int arg = 0; arg < argc; arg++ ) {
+        XLAL_CHECK_MAIN( ( cmdline = ( char * )XLALRealloc( ( void * )cmdline, strlen( cmdline ) + strlen( argv[arg] ) + 2 ) ) != NULL, XLAL_ENOMEM, "out of memory allocating cmdline" );
+        strcat( cmdline, argv[arg] );
+        if ( arg == argc - 1 ) {
+        strcat( cmdline, "\n" );
+        } else {
+        strcat( cmdline, " " );
+        }
+    }
+  }
+
   /* set detector constraint */
   constraints.detector = NULL;
 
@@ -134,7 +157,26 @@ int main(int argc, char *argv[]){
   
     /* read the sfts */
     XLAL_CHECK_MAIN( ( inputSFTs = XLALLoadMultiSFTs ( &thisCatalog, uvar_fMin, uvar_fMax) ) != NULL, XLAL_EFUNC);    
-    
+
+    /* construct comment for output SFTs */
+    char *comment = NULL;
+    char *oldcomment = thisCatalog.data[0].comment;
+    int comment_length = strlen(oldcomment) + 1;
+    if ( uvar_addComment > CMT_OLD ) {
+      /* allocate space for new comment */
+      XLAL_CHECK_MAIN( ( comment = ( char * )XLALMalloc( comment_length + strlen( cmdline ) + 1 ) ) != NULL, XLAL_ENOMEM, "out of memory allocating comment" );
+      /* append the commandline of this program to the old comment */
+      if ( oldcomment ) {
+        strcpy( comment, oldcomment );
+      } else {
+        *comment = '\0';
+      }
+      strcat( comment, cmdline );
+    } else if ( uvar_addComment == CMT_OLD ) {
+      /* only copied existing comment, no additional space needed */
+      comment = oldcomment;
+    } /* else (uvar_addComment == CMT_NONE) and (comment == NULL) i.e. no comment at all */
+
     /* clean  lines */
     if ( XLALUserVarWasSet( &uvar_linefiles ) ) {
       LAL_CALL( LALRemoveKnownLinesInMultiSFTVector ( &status, inputSFTs, uvar_maxBins, 
@@ -143,10 +185,13 @@ int main(int argc, char *argv[]){
     
     /* write output */
     for (k = 0; k < inputSFTs->length; k++) {
-      XLAL_CHECK_MAIN( XLALWriteSFTVector2Dir (inputSFTs->data[k], uvar_outDir, "cleaned", "cleaned") == XLAL_SUCCESS, XLAL_EFUNC);
+      XLAL_CHECK_MAIN( XLALWriteSFTVector2Dir (inputSFTs->data[k], uvar_outDir, comment, "cleaned") == XLAL_SUCCESS, XLAL_EFUNC);
     }
 
     XLALDestroyMultiSFTVector( inputSFTs);
+    if ( uvar_addComment > CMT_OLD ) {
+      XLALFree( comment );
+    }
 
   } /* end loop over sfts */
 
@@ -154,16 +199,11 @@ int main(int argc, char *argv[]){
   XLALDestroySFTCatalog(catalog );
   XLALDestroyUserVars();
   LAL_CALL( LALDestroyRandomParams (&status, &randPar), &status);
+  if ( uvar_addComment > CMT_OLD ) {
+    XLALFree( cmdline );
+  }
 
   LALCheckMemoryLeaks(); 
 
   return 0;
 }
-
-
-
-
-
-
-
-
