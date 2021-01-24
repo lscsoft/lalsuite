9c395546816f3c7ddbb526e493854453487cad4d
David Keitel <david.keitel@ligo.org>
2021-01-24 19:01:17 +0100

    lalapps_SFTclean: enable --outSingleSFT mode
     - SFTs will be written to a single file per IFO
     - now always using a MultiSFTCatalogView to loop over SFTs in per-IFO order
     - using canonical output file names from XLALOfficialSFTFilename

--- lalapps/src/pulsar/SFTTools/SFTclean.c
+++ lalapps/src/pulsar/SFTTools/SFTclean.c
@@ -27,6 +27,7 @@
 
 #include <lal/SFTClean.h>
 #include <lal/SFTutils.h>
+#include <lal/SFTReferenceLibrary.h>
 #include <glob.h> 
 #include <LALAppsVCSInfo.h>
 
@@ -63,13 +64,12 @@ int main(int argc, char *argv[]){
   static SFTCatalog *catalog = NULL;
   static SFTCatalog thisCatalog;
   static SFTConstraints constraints;
-
+  const char *misc = "cleaned"; /* used for setting output filenames */
 
   /* 09/09/05 gam; randPar now a parameter for LALCleanCOMPLEX8SFT */
   FILE *fp=NULL;   
   INT4 seed, ranCount;
   RandomParams *randPar=NULL; 
-  UINT4 k, j;  
 
   /* user input variables */
   LALStringVector *uvar_linefiles=NULL; /* files with harmonics info */
@@ -77,6 +77,7 @@ int main(int argc, char *argv[]){
   CHAR *uvar_outDir;   /* directory for cleaned sfts */
   REAL8 uvar_fMin, uvar_fMax;
   INT4  uvar_window, uvar_maxBins, uvar_addComment;
+  BOOLEAN uvar_outSingleSFT;
 
   /* set defaults */
   uvar_sftDir = NULL;
@@ -86,6 +87,7 @@ int main(int argc, char *argv[]){
   uvar_window = 100;
   uvar_maxBins = 20;
   uvar_addComment = CMT_FULL; /* add VCS ID and full command-line to every SFT file */
+  uvar_outSingleSFT = FALSE;
 
   /* register user input variables */
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_sftDir,    "sftDir",    STRING,       'i', REQUIRED,  "Input SFT file pattern") == XLAL_SUCCESS, XLAL_EFUNC);
@@ -96,7 +98,7 @@ int main(int argc, char *argv[]){
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_maxBins,   "maxBins",   INT4,         'm',OPTIONAL,  "Max. bins to clean") == XLAL_SUCCESS, XLAL_EFUNC);
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_linefiles, "linefiles", STRINGVector, 0,   OPTIONAL, "List of per-detector files with list of lines (each full path must start with a canonical IFO name)") == XLAL_SUCCESS, XLAL_EFUNC);
   XLAL_CHECK_MAIN( XLALRegisterNamedUvar( &uvar_addComment, "addComment", INT4,       'c', OPTIONAL, "How to deal with comments - 0 means no comment is written at all, 1 means that the comment is taken unmodified from the input SFTs, 2 (default) means that the program appends its RCS id and command-line to the comment.") == XLAL_SUCCESS, XLAL_EFUNC);
-
+  XLAL_CHECK_MAIN( XLALRegisterNamedUvar ( &uvar_outSingleSFT, "outSingleSFT", BOOLEAN, 's', OPTIONAL, "Write a single concatenated SFT file per IFO, instead of individual files") == XLAL_SUCCESS, XLAL_EFUNC );
 
   /* read all command line variables */
   BOOLEAN should_exit = 0;
@@ -125,7 +127,7 @@ int main(int argc, char *argv[]){
   /* set detector constraint */
   constraints.detector = NULL;
 
-  /* get sft catalog */
+  /* get SFT catalog - this can be for multiple detectors */
   XLAL_CHECK_MAIN( ( catalog = XLALSFTdataFind( uvar_sftDir, &constraints) ) != NULL, XLAL_EFUNC);
   if ( (catalog == NULL) || (catalog->length == 0) ) {
     fprintf (stderr,"Unable to match any SFTs with pattern '%s'\n", uvar_sftDir );
@@ -148,55 +150,124 @@ int main(int argc, char *argv[]){
       exit(1); 
     } 
   LAL_CALL ( LALCreateRandomParams (&status, &randPar, seed), &status );
-  
-  /* loop over sfts and clean them -- load one sft at a time */
-  thisCatalog.length = 1;
-  for (j=0; j<catalog->length; j++) {
 
-    thisCatalog.data = catalog->data + j;
+  /* loop over all SFTs in the full catalog, but sorted by detector */
+  MultiSFTCatalogView* multiCatalogView = NULL;
+  XLAL_CHECK ( (multiCatalogView = XLALGetMultiSFTCatalogView ( catalog )) != NULL, XLAL_EFUNC );
+  for (UINT4 X = 0; X < multiCatalogView->length; X++) {
   
-    /* read the sfts */
-    XLAL_CHECK_MAIN( ( inputSFTs = XLALLoadMultiSFTs ( &thisCatalog, uvar_fMin, uvar_fMax) ) != NULL, XLAL_EFUNC);    
-
-    /* construct comment for output SFTs */
-    char *comment = NULL;
-    char *oldcomment = thisCatalog.data[0].comment;
-    int comment_length = strlen(oldcomment) + 1;
-    if ( uvar_addComment > CMT_OLD ) {
-      /* allocate space for new comment */
-      XLAL_CHECK_MAIN( ( comment = ( char * )XLALMalloc( comment_length + strlen( cmdline ) + 1 ) ) != NULL, XLAL_ENOMEM, "out of memory allocating comment" );
-      /* append the commandline of this program to the old comment */
-      if ( oldcomment ) {
-        strcpy( comment, oldcomment );
-      } else {
-        *comment = '\0';
+    /* optionally prepare merged-file output:
+     * if uvar_outSingleSFT, a file pointer will be opened and all SFTs for X
+     * will be appended to it
+     */
+    char *outpath = NULL;
+    FILE *fpout = NULL;
+    UINT4 numSFTs = multiCatalogView->data[X].length;
+    if ( uvar_outSingleSFT ) {
+      /* grab the first and last entry from the single-IFO catalog,
+       * relying here on XLALSFTdataFind returning a catalogue with SFTs sorted by increasing GPS-epochs
+       */
+      SFTtype *sftStart = &(multiCatalogView->data[X].data[0].header);
+      SFTtype *sftEnd = &(multiCatalogView->data[X].data[numSFTs-1].header);
+      LIGOTimeGPS *epochStart = &(sftStart->epoch);
+      LIGOTimeGPS *epochEnd   = &(sftEnd->epoch);
+      const char *name = sftStart->name;
+      UINT4 Tsft = (UINT4) round ( 1.0 / sftStart->deltaF );
+      /* calculate time interval covered -- may be different from timebase if nanosecond of sft-epochs are non-zero */
+      UINT4 Tspan = epochEnd->gpsSeconds - epochStart->gpsSeconds + Tsft;
+      if ( epochStart->gpsNanoSeconds > 0) {
+        Tspan += 1;
       }
-      strcat( comment, cmdline );
-    } else if ( uvar_addComment == CMT_OLD ) {
-      /* only copied existing comment, no additional space needed */
-      comment = oldcomment;
-    } /* else (uvar_addComment == CMT_NONE) and (comment == NULL) i.e. no comment at all */
-
-    /* clean  lines */
-    if ( XLALUserVarWasSet( &uvar_linefiles ) ) {
-      LAL_CALL( LALRemoveKnownLinesInMultiSFTVector ( &status, inputSFTs, uvar_maxBins, 
-						      uvar_window, uvar_linefiles, randPar), &status);
-    }
-    
-    /* write output */
-    for (k = 0; k < inputSFTs->length; k++) {
-      XLAL_CHECK_MAIN( XLALWriteSFTVector2Dir (inputSFTs->data[k], uvar_outDir, comment, "cleaned") == XLAL_SUCCESS, XLAL_EFUNC);
-    }
-
-    XLALDestroyMultiSFTVector( inputSFTs);
-    if ( uvar_addComment > CMT_OLD ) {
-      XLALFree( comment );
+      if ( epochEnd->gpsNanoSeconds > 0) {
+        Tspan += 1;
+      }
+      /* get the official-format filename for the merged output file for this detector */
+      char *filename;
+      XLAL_CHECK_MAIN ( (filename = XLALOfficialSFTFilename ( name[0], name[1], numSFTs, Tsft, epochStart->gpsSeconds, Tspan, misc )) != NULL, XLAL_EFUNC );
+      /* append to output directory name */
+      int len = strlen ( uvar_outDir ) + 1 + strlen ( filename ) + 1;
+      XLAL_CHECK_MAIN ( (outpath = XLALCalloc ( 1, len )) != NULL, XLAL_ENOMEM );
+      sprintf ( outpath, "%s/%s", uvar_outDir, filename );
+      XLALFree ( filename );
+      printf ( "Writing %d cleaned SFTs for %s to merged output file %s ...\n", numSFTs, name, outpath );
+      XLAL_CHECK_MAIN ( (fpout = fopen ( outpath, "wb" )) != NULL, XLAL_EIO, "Failed to open '%s' for writing: %s\n\n", outpath, strerror(errno));
+    } else {
+      printf ( "Writing %d cleaned SFTs for %s to individual output files in %s ...\n", numSFTs, multiCatalogView->data[X].data[0].header.name, uvar_outDir );
     }
 
-  } /* end loop over sfts */
+    /* loop over SFTs for this IFO and clean them -- load one SFT at a time */
+    thisCatalog.length = 1;
+    for (UINT4 j=0; j<numSFTs; j++) {
+
+      thisCatalog.data = multiCatalogView->data[X].data + j;
+
+      /* read a multi-SFT vector, but it will only cover a single IFO and a single SFT */
+      XLAL_CHECK_MAIN( ( inputSFTs = XLALLoadMultiSFTs ( &thisCatalog, uvar_fMin, uvar_fMax) ) != NULL, XLAL_EFUNC);
+      XLAL_CHECK_MAIN( inputSFTs->length == 1, XLAL_EIO, "We should have data from a single IFO here, but have inputSFTs->length==%d", inputSFTs->length );
+
+      /* construct comment for output SFTs */
+      char *comment = NULL;
+      char *oldcomment = thisCatalog.data[0].comment;
+      int comment_length = strlen(oldcomment) + 1;
+      if ( uvar_addComment > CMT_OLD ) {
+        /* allocate space for new comment */
+        XLAL_CHECK_MAIN( ( comment = ( char * )XLALMalloc( comment_length + strlen( cmdline ) + 1 ) ) != NULL, XLAL_ENOMEM, "out of memory allocating comment" );
+        /* append the commandline of this program to the old comment */
+        if ( oldcomment ) {
+            strcpy( comment, oldcomment );
+        } else {
+            *comment = '\0';
+        }
+        strcat( comment, cmdline );
+      } else if ( uvar_addComment == CMT_OLD ) {
+        /* only copied existing comment, no additional space needed */
+        comment = oldcomment;
+      } /* else (uvar_addComment == CMT_NONE) and (comment == NULL) i.e. no comment at all */
+
+      /* clean lines
+       * Here we still pass the full, possible multi-IFO linesfiles,
+       * but LALRemoveKnownLinesInMultiSFTVector() has logic to properly apply
+       * only those files with canonic IFO names matching the single-IFO inputSFTs.
+       */
+      if ( XLALUserVarWasSet( &uvar_linefiles ) ) {
+        LAL_CALL( LALRemoveKnownLinesInMultiSFTVector ( &status, inputSFTs, uvar_maxBins,
+                      uvar_window, uvar_linefiles, randPar), &status);
+      }
+  
+      /* write output - technically looping over all SFTs for a single detector here,
+       * but actually the current implementation has it always be a single SFT too
+       */
+      if ( uvar_outSingleSFT ) {
+        for ( UINT4 k = 0; k < inputSFTs->data[0]->length; k++ ) {
+          SFTtype *this_sft = &( inputSFTs->data[0]->data[k] );
+          XLAL_CHECK ( XLALWriteSFT2fp ( this_sft, fpout, comment ) == XLAL_SUCCESS, XLAL_EFUNC );
+        }
+      } else {
+        XLAL_CHECK_MAIN( XLALWriteSFTVector2Dir ( inputSFTs->data[0], uvar_outDir, comment, misc ) == XLAL_SUCCESS, XLAL_EFUNC);
+      }
+
+      XLALDestroyMultiSFTVector( inputSFTs);
+      if ( uvar_addComment > CMT_OLD ) {
+        XLALFree( comment );
+      }
+
+    } /* end loop over sfts */
+
+    if (uvar_outSingleSFT ) {
+      fclose( fpout );
+      /* validate the merged output file now that we're finished with it:
+       * in principle we have made sure all SFT timestamps are ascending etc,
+       * but best to make sure
+       */
+      XLAL_CHECK_MAIN ( ValidateSFTFile(outpath) == 0, XLAL_EFUNC, "Output file %s failed validation.", outpath );
+      XLALFree( outpath );
+    }
+
+  } /* for (UINT4 X = 0; X < multiCatalogView->length; X++) */
 
   /* Free memory */
   XLALDestroySFTCatalog(catalog );
+  XLALDestroyMultiSFTCatalogView ( multiCatalogView );
   XLALDestroyUserVars();
   LAL_CALL( LALDestroyRandomParams (&status, &randPar), &status);
   if ( uvar_addComment > CMT_OLD ) {
--- lalapps/src/pulsar/SFTTools/testSFTclean.sh
+++ lalapps/src/pulsar/SFTTools/testSFTclean.sh
@@ -33,6 +33,8 @@ echo "STEP 1: creating H1L1 test data with line features..."
 echo "----------------------------------------------------------------------"
 echo
 
+original_SFTs=()
+dumps_orig=()
 for ifo in 0 1; do
     cmdline="${mfd_code} --IFO=${IFOs[$ifo]} --outSFTbname=. --startTime=$startTime --duration=$duration --fmin=$fmin --Band=$fBand --noiseSqrtSh=$sqrtSh --lineFeature=1 --h0=$h0 --cosi=$cosi --Freq=$linefreq --randSeed=1"
     echo $cmdline
@@ -42,9 +44,9 @@ for ifo in 0 1; do
     fi
 
     ## ----- dump original SFTs
-    original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
-    dump_orig="dump_${original_SFTs}.txt"
-    cmdline="${dump_code} --dataOnly --SFTfiles=${original_SFTs} > $dump_orig"
+    original_SFTs+=("${IFObits[$ifo]}$original_SFT_pattern")
+    dumps_orig+=("dump_${original_SFTs[$ifo]}.txt")
+    cmdline="${dump_code} --dataOnly --SFTfiles=${original_SFTs[$ifo]} > ${dumps_orig[$ifo]}"
     echo $cmdline
     if ! eval $cmdline; then
         echo "Error.. something failed when running '$dump_code' ..."
@@ -68,9 +70,7 @@ fi
 
 ## ----- dump the "cleaned" SFTs
 for ifo in 0 1; do
-    original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
     cleaned_SFTs="${IFObits[$ifo]}$cleaned_SFT_pattern"
-    dump_orig="dump_${original_SFTs}.txt"
     dump_clean="dump_${cleaned_SFTs}.txt"
     cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFTs > $dump_clean"
 
@@ -80,10 +80,10 @@ for ifo in 0 1; do
         exit 1
     fi
 
-    diff_cmd="diff -s $dump_orig $dump_clean"
+    diff_cmd="diff -s ${dumps_orig[$ifo]} $dump_clean"
     echo $diff_cmd
     if ! eval $diff_cmd; then
-        echo "error: $dump_orig and $dump_clean should be equal as long as no --linefiles are given."
+        echo "error: ${dumps_orig[$ifo]} and $dump_clean should be equal as long as no --linefiles are given."
         exit 1
     fi
 done
@@ -99,12 +99,13 @@ test_cleaning () {
 
     linefile_content=$1
     nexpected=$(echo "$NSFTs * $2" | bc)
+    extraflags=$3
 
     linefile="L1lines.txt"
     echo "writing line definition '$linefile_content' to file $linefile ..."
     echo -e $linefile_content > $linefile
 
-    cmdline="${clean_code} --sftDir=*${original_SFT_pattern} --outDir=. --fMin $fmin --fMax $fmax --linefiles=$linefile"
+    cmdline="${clean_code} --sftDir=*${original_SFT_pattern} --outDir=. --fMin $fmin --fMax $fmax --linefiles=$linefile $extraflags"
 
     echo $cmdline
     if ! eval $cmdline; then
@@ -114,9 +115,7 @@ test_cleaning () {
 
     ## ----- dump the "cleaned" SFTs
     for ifo in 0 1; do
-        original_SFTs="${IFObits[$ifo]}$original_SFT_pattern"
         cleaned_SFTs="${IFObits[$ifo]}$cleaned_SFT_pattern"
-        dump_orig="dump_${original_SFTs}.txt"
         dump_clean="dump_${cleaned_SFTs}.txt"
         cmdline="${dump_code} --dataOnly --SFTfiles=$cleaned_SFTs > $dump_clean"
 
@@ -127,25 +126,25 @@ test_cleaning () {
         fi
 
         if [ "${IFOs[$ifo]}" == "L1" ]; then
-            diff_cmd="diff --suppress-common-lines -y -s $dump_orig $dump_clean"
+            diff_cmd="diff --suppress-common-lines -y -s ${dumps_orig[$ifo]} $dump_clean"
             echo $diff_cmd
             if ! eval $diff_cmd; then
                 # This diff will returned a non-0 exit status, but that's ok, we just wanted to print it. So pass.
                 :
             fi
-            diff_cmd="diff --suppress-common-lines -y $dump_orig $dump_clean"
+            diff_cmd="diff --suppress-common-lines -y ${dumps_orig[$ifo]} $dump_clean"
             ndiff=$($diff_cmd | wc -l)
             if [ $ndiff == $nexpected ]; then
-                echo "OK: $dump_orig and $dump_clean differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s), as expected."
+                echo "OK: ${dumps_orig[$ifo]} and $dump_clean differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s), as expected."
             else
-                echo "error: $dump_orig and $dump_clean should differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s)."
+                echo "error: ${dumps_orig[$ifo]} and $dump_clean should differ by exactly NSFTs*$2=$NSFTs*$2=$nexpected line(s)."
                 exit 1
             fi
         else
-            diff_cmd="diff -s $dump_orig $dump_clean"
+            diff_cmd="diff -s ${dumps_orig[$ifo]} $dump_clean"
             echo $diff_cmd
             if ! eval $diff_cmd; then
-                echo "error: $dump_orig and $dump_clean should be equal as long as no matching --linefiles entry for ${IFOs[$ifo]} is given."
+                echo "error: ${dumps_orig[$ifo]} and $dump_clean should be equal as long as no matching --linefiles entry for ${IFOs[$ifo]} is given."
                 exit 1
             fi
         fi
@@ -191,5 +190,15 @@ test_cleaning "50.002222222 0.002222222 4 0.0 0.0  /*comb*/" "4"
 
 echo
 test_cleaning "50.005 0.0 1 0.0 0.0 /*line1*/\n50.01 0.0 1 0.0 0.0 /*line2*/" "2"
+
+echo
+echo "----------------------------------------------------------------------"
+echo "STEP 4: test --outSingleSFT mode"
+echo "----------------------------------------------------------------------"
+echo
+
+IFObits=("H-${NSFTs}_H1" "L-${NSFTs}_L1")
+cleaned_SFT_pattern="_${TSFT}SFT_cleaned-*-${duration}.sft"
+test_cleaning "50.005 0.0 1 0.0 0.0 /*single_bin_line*/" "1" "--outSingleSFT"
 
 exit $retstatus
