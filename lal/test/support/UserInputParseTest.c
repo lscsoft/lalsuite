967308fab431fcabd31e883620236033402819e5
Duncan Macleod <duncan.macleod@ligo.org>
2021-01-26 06:56:36 +0000

    lal: update FSF address

--- lal/test/support/UserInputParseTest.c
+++ lal/test/support/UserInputParseTest.c
@@ -14,8 +14,8 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with with program; see the file COPYING. If not, write to the
-// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-// MA 02111-1307 USA
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301 USA
 //
 
 // Tests of the unit conversion functions in the UserInputParser.[ch] module
