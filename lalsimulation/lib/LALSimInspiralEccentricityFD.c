53b3af8f721a9c0c16c00d1e29abd911a3e27837
Duncan Macleod <duncan.macleod@ligo.org>
2021-01-26 06:56:36 +0000

    lalsimulation: update FSF address

--- lalsimulation/lib/LALSimInspiralEccentricityFD.c
+++ lalsimulation/lib/LALSimInspiralEccentricityFD.c
@@ -12,8 +12,8 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with with program; see the file COPYING. If not, write to the
- *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA
  *
  * This code describes the gravitational radiation emitted by compact binaries with low to moderate values of eccentricity [0 < e < 0.5]. The waveform phase includes post-Newtonian corrections up to 3.5 order and eccentricity corrections up to order e^8 at each post-Newtonian order. The waveform amplitude is modeled within the restricted post-Newtonian approach.
  */
