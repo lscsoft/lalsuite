5cf97ad6f5f21ce3f3395fd9d5d1b9b95a1609be
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-11 10:39:08 +0000

    conda: move activate scripts into lib packages
    since either those hold the data files anyway, or are absolutely required by the packages that do
    [skip rhel][skip debian][skip integration][skip docs][skip wheels]

--- conda/install-bin.sh
+++ conda/install-bin.sh
@@ -11,27 +11,3 @@ make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
 
 # install system configuration files
 make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
-
-# -- create activate/deactivate scripts
-PKG_NAME_UPPER=$(echo ${PKG_NAME} | awk '{ print toupper($0) }')
-
-# activate.sh
-ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${ACTIVATE_SH})
-cat > ${ACTIVATE_SH} << EOF
-#!/bin/bash
-export CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR="\${${PKG_NAME_UPPER}_DATADIR:-empty}"
-export ${PKG_NAME_UPPER}_DATADIR="/opt/anaconda1anaconda2anaconda3/share/${PKG_NAME}"
-EOF
-# deactivate.sh
-DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${DEACTIVATE_SH})
-cat > ${DEACTIVATE_SH} << EOF
-#!/bin/bash
-if [ "\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}" = "empty" ]; then
-	unset ${PKG_NAME_UPPER}_DATADIR
-else
-	export ${PKG_NAME_UPPER}_DATADIR="\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}"
-fi
-unset CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR
-EOF
