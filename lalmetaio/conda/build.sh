7bd979f4ecacc4d2eb4103efe0e6e5df76690945
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-13 16:11:32 +0000

    lalmetaio: update variable name in conda build.sh

--- lalmetaio/conda/build.sh
+++ lalmetaio/conda/build.sh
@@ -8,7 +8,7 @@ cd _build
 
 # enable nightly mode for CI
 if [ "${CI_PIPELINE_SOURCE}" = "schedule" ] || [ "${CI_PIPELINE_SOURCE}" = "web" ]; then
-	EXTRA_CONFIG_FLAGS="--enable-nightly"
+	ENABLE_NIGHTLY="--enable-nightly"
 fi
 
 # when running on gitlab-ci, we are not using a production
@@ -24,7 +24,7 @@ ${SRC_DIR}/configure \
 	--enable-help2man \
 	--enable-swig-iface \
 	--prefix="${PREFIX}" \
-	${EXTRA_CONFIG_FLAGS} \
+	${ENABLE_NIGHTLY} \
 ;
 
 # build
