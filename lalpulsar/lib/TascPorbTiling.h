d7258246989f6d32e3eab4f5cfc90a90424a8e60
John T. Whelan <john.whelan@ligo.org>
2021-03-01 20:44:43 +0100

    Update street address for Free Software Foundation in license headers
    See merge request !1511

--- lalpulsar/lib/TascPorbTiling.h
+++ lalpulsar/lib/TascPorbTiling.h
@@ -6,15 +6,15 @@
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,x5
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with with program; see the file COPYING. If not, write to the
-// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-// MA 02111-1307 USA
+//  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//  MA  02110-1301  USA
 //
 
 #ifndef _TASCPORBTILING_H
