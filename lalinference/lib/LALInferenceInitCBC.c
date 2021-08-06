1f8ee76e676d714e76b34026fc522bbbb069f6ba
NV KRISHNENDU <nv.krishnendu@ldas-pcdev6.ligo.caltech.edu>
2021-08-06 06:56:06 -0700

    rebase conflict fixes

--- lalinference/lib/LALInferenceInitCBC.c
+++ lalinference/lib/LALInferenceInitCBC.c
@@ -2366,7 +2366,6 @@ static void LALInferenceInitNonGRParams(LALInferenceRunState *state, LALInferenc
           }
           else LALInferenceAddVariable(model->params,"nonGR_alpha", &nonGR_alpha, LALINFERENCE_REAL8_t, LALINFERENCE_PARAM_FIXED);
         }
-        UINT4 nParams_dQuadMon= 0;
 	/* Relative shifts for inspiral phase PN coefficients (absolute value for dchi1) */
         if (checkParamInList(ppt->value,"dchi0")) LALInferenceRegisterUniformVariableREAL8(state, model->params, "dchi0", tmpVal, dchi_min, dchi_max, LALINFERENCE_PARAM_LINEAR);
         if (checkParamInList(ppt->value,"dchi1")) LALInferenceRegisterUniformVariableREAL8(state, model->params, "dchi1", tmpVal, dchi_min, dchi_max, LALINFERENCE_PARAM_LINEAR);
