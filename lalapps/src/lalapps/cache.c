0cd34ed65bd84b02f0647a07eb1da730bc17eb62
Karl Wette <karl.wette@ligo.org>
2022-06-26 17:30:00 +1000

    cache.c: add missing copyright information

--- lalapps/src/lalapps/cache.c
+++ lalapps/src/lalapps/cache.c
@@ -1,3 +1,22 @@
+/*
+*  Copyright (C) 2007 Jolien Creighton
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with with program; see the file COPYING. If not, write to the
+*  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*  MA  02110-1301  USA
+*/
+
 #include <string.h>
 #include <lal/LALStdlib.h>
 #include <lal/LALCache.h>
