6d17357a54e5fd2886406f92185ff6e75c7934b6
Karl Wette <karl.wette@ligo.org>
2022-06-27 19:05:11 +1000

    LALPulsar: update Conda package dependencies
    - lalpulsar/conda/meta.yaml.in.in: add host/run dependencies to
      "lalpulsar" subpackage for building/testing binaries
    - lalpulsar/conda/install-python.sh: compile with --enable-cfitsio,
      needed for some lalpulsar/bin binaries
    - lalpulsar/conda/install-bin.sh: test lalpulsar/bin binaries

--- lalpulsar/conda/install-bin.sh
+++ lalpulsar/conda/install-bin.sh
@@ -1,1 +1,18 @@
-../../conda/install-bin.sh
+#!/bin/bash
+
+set -ex
+
+# install from python build directory
+_pybuilddir="_build${PY_VER}"
+cd ${_pybuilddir}
+
+# test binaries
+if [[ "${build_platform}" == "${target_platform}" ]]; then
+	make -j ${CPU_COUNT} V=1 VERBOSE=1 check -C bin
+fi
+
+# install binaries
+make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
+
+# install system configuration files
+make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
