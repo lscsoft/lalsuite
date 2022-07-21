297433dd426c9b63900aeba729af4507a33cede1
Karl Wette <karl.wette@ligo.org>
2022-07-21 22:18:30 +1000

    lal/lib/fft/Makefile.am: do not install CudaPlan.h

--- lal/lib/fft/CudaComplexFFT.c
+++ lal/lib/fft/CudaComplexFFT.c
@@ -23,12 +23,12 @@
 #include <lal/LALMalloc.h>
 #include <lal/XLALError.h>
 #include <lal/ComplexFFT.h>
-#include <lal/CudaPlan.h>
 #include <lal/FFTWMutex.h>
 #include <fftw3.h>
 
 #include "CudaFunctions.h"
 #include "CudaFFT.h"
+#include "CudaPlan.h"
 
 #ifdef __GNUC__
 #define UNUSED __attribute__ ((unused))
