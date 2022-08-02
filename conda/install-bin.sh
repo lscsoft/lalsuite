16b9e0c0bb8e979cecc1ca12045bf01128c1bfdd
Karl Wette <karl.wette@ligo.org>
2022-08-02 10:07:07 +1000

    conda/install-bin.sh: test binaries

--- conda/install-bin.sh
+++ conda/install-bin.sh
@@ -6,6 +6,11 @@ set -ex
 _pybuilddir="_build${PY_VER}"
 cd ${_pybuilddir}
 
+# test binaries
+if [[ "${build_platform}" == "${target_platform}" ]]; then
+	make -j ${CPU_COUNT} V=1 VERBOSE=1 check -C bin
+fi
+
 # install binaries
 make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
 
