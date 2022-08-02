f38ccdb79f152384f1786bc4d333f7324c9c8712
Karl Wette <karl.wette@ligo.org>
2022-08-02 10:07:28 +1000

    lalpulsar/conda/install-bin.sh: use conda/install-bin.sh

--- lalpulsar/conda/install-bin.sh
+++ lalpulsar/conda/install-bin.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-set -ex
-
-# install from python build directory
-_pybuilddir="_build${PY_VER}"
-cd ${_pybuilddir}
-
-# test binaries
-if [[ "${build_platform}" == "${target_platform}" ]]; then
-	make -j ${CPU_COUNT} V=1 VERBOSE=1 check -C bin
-fi
-
-# install binaries
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
-
-# install system configuration files
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
+../../conda/install-bin.sh
