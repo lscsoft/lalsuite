ed4ebd05067f0b7d1822d7908bf750ca5367af2a
Karl Wette <karl.wette@ligo.org>
2022-08-25 13:59:24 +1000

    Weave: use lower resolution parameter point comparison against reference results

--- lalpulsar/bin/Weave/testWeave_non_interpolating.sh
+++ lalpulsar/bin/Weave/testWeave_non_interpolating.sh
@@ -110,7 +110,7 @@ rm -rf newtarball/
 
 echo "=== Compare semicoherent F-statistics from lalpulsar_Weave to reference results ==="
 set -x
-env LAL_DEBUG_LEVEL="${LAL_DEBUG_LEVEL},info" lalpulsar_WeaveCompare --setup-file=WeaveSetup.fits --result-file-1=WeaveOut.fits --result-file-2=RefWeaveOut.fits
+env LAL_DEBUG_LEVEL="${LAL_DEBUG_LEVEL},info" lalpulsar_WeaveCompare --setup-file=WeaveSetup.fits --result-file-1=WeaveOut.fits --result-file-2=RefWeaveOut.fits --round-param-to-n-sf=4
 set +x
 echo
 
