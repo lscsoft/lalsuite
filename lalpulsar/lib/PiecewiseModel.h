988fc72bc87a8bf9d811e504afe0a0f35b7c874f
Karl Wette <karl.wette@ligo.org>
2022-10-05 13:23:48 +1100

    Add copyright headers

--- lalpulsar/lib/PiecewiseModel.h
+++ lalpulsar/lib/PiecewiseModel.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2021, Ben Grace
+// Copyright (C) 2019--2023 Benjamin Grace
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
