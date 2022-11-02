56aae42c6270ba283669c15a1e34436d8d6b3cc8
Karl Wette <karl.wette@ligo.org>
2022-11-02 23:03:31 +1100

    SWIG: use pytest for Python tests
    - For verbose assert() and other features
    - Use nasty (abort) handlers in Octave tests only when running under gdb

--- lalsimulation/swig/SWIGTestLALSimulationPython.py
+++ lalsimulation/swig/SWIGTestLALSimulationPython.py
@@ -1,33 +1,45 @@
 # Check SWIG Python bindings for LALSimulation
 # Author: Karl Wette, 2011--2014
 
+import os
+import sys
+import pytest
+
 # check module load
-print("checking module load ...")
+print("checking module load ...", file=sys.stderr)
 import lal
 import lalsimulation
 from lalsimulation import globalvar as lalsimulationglobalvar
 from lal import globalvar as lalglobalvar
-print("PASSED module load")
+print("PASSED module load", file=sys.stderr)
 
 # set error handlers
 def set_nice_error_handlers():
     lal.swig_set_nice_error_handlers()
 def set_default_error_handlers():
-    lal.swig_set_nasty_error_handlers()
+    if 'NASTY_ERROR_HANDLERS' in os.environ:
+        lal.swig_set_nasty_error_handlers()
+    else:
+        lal.swig_set_nice_error_handlers()
 set_default_error_handlers()
 
-# check object parent tracking
-print("checking object parent tracking ...")
-a = lalsimulation.swig_lalsimulation_test_parent_map_struct()
-for i in range(0, 7):
-    b = a.s
-    c = lalsimulationglobalvar.swig_lalsimulation_test_parent_map.s
-    lalsimulationglobalvar.swig_lalsimulation_test_parent_map.s = lalglobalvar.swig_lal_test_struct_const
-del c
-del b
-del a
-lal.CheckMemoryLeaks()
-print("PASSED object parent tracking")
+def test_object_parent_tracking():
+    """check object parent tracking
+    """
 
-# passed all tests!
-print("PASSED all tests")
+    print("checking object parent tracking ...", file=sys.stderr)
+    a = lalsimulation.swig_lalsimulation_test_parent_map_struct()
+    for i in range(0, 7):
+        b = a.s
+        c = lalsimulationglobalvar.swig_lalsimulation_test_parent_map.s
+        lalsimulationglobalvar.swig_lalsimulation_test_parent_map.s = lalglobalvar.swig_lal_test_struct_const
+    del c
+    del b
+    del a
+    locals()   # update locals() to remove lingering references
+    lal.CheckMemoryLeaks()
+    print("PASSED object parent tracking", file=sys.stderr)
+
+if __name__ == '__main__':
+    args = sys.argv[1:] or ["-v", "-rs", "--junit-xml=junit-SWIGTestLALSimulationPython.xml"]
+    sys.exit(pytest.main(args=[__file__] + args))
