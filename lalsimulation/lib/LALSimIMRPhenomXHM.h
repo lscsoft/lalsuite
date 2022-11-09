827f9dc6816d6ae5502b1ac30870379543077cf2
racheltennant <115003554+racheltennant@users.noreply.github.com>
2022-11-09 17:51:42 +0000

    Update LALSimIMRPhenomXHM.h

--- lalsimulation/lib/LALSimIMRPhenomXHM.h
+++ lalsimulation/lib/LALSimIMRPhenomXHM.h
@@ -17,13 +17,13 @@
 *  MA  02110-1301  USA
 */
 
-#ifndef _LALSIM_IMR_PHENOMXHM_H
-#define _LALSIM_IMR_PHENOMXHM_H
+#ifndef _LALSIM_IMR_PHENOMXHM_H       //ifndef is for conditional compilation - determines it doesnt exist before including in compilation
+#define _LALSIM_IMR_PHENOMXHM_H       //creates definition  
 
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifdef __cplusplus        //ifdef is for conditional compilation - determines it exists before including in compilation
+extern "C" {              //Ensures names arent mangled within C++ compiler so names arent changed 
+#endif                    //just closes ifdef statement 
 
 #include <math.h>
 #include <complex.h>
@@ -36,6 +36,8 @@ extern "C" {
 #include <lal/TimeFreqFFT.h>
 #include <lal/Units.h>
 #include <lal/LALSimInspiral.h>
+   
+//These are all other files contained within the lalsuite repository which are just being imported
 
 
 // /* Returns the Fourier domain strain of just one negative mode: h_l-m. This quantity is zero for negative frequencies.
@@ -43,13 +45,13 @@ extern "C" {
 // This is a wrapper function that use XLALSimIMRPhenomXASGenerateFD for the 22 mode and XLALSimIMRPhenomXHMOneMode for the higher modes. */
 
 
-#include "LALSimIMRPhenomXHM_structs.h"
+#include "LALSimIMRPhenomXHM_structs.h"      
 
-int IMRPhenomXHMGenerateFDOneMode(
-   COMPLEX16FrequencySeries **htildelm,  /**< [out] hlm for one mode **/
-   const REAL8Sequence *freqs_In,        /**< fmin, fmax [Hz] **/
-   IMRPhenomXWaveformStruct *pWF,        /**< structure of the 22 mode **/
-   UINT4 ell,                            /**< first index of the mode **/
+int IMRPhenomXHMGenerateFDOneMode(                                                //int gives the variable an integer type
+   COMPLEX16FrequencySeries **htildelm,  /**< [out] hlm for one mode **/          //** is pointer to pointer - so stores the address of another pointer variable
+   const REAL8Sequence *freqs_In,        /**< fmin, fmax [Hz] **/                 //const specifies that th variable is constant so tells compiler to prevent programmer from modifying 
+   IMRPhenomXWaveformStruct *pWF,        /**< structure of the 22 mode **/        //* is a pointer and will store addresses
+   UINT4 ell,                            /**< first index of the mode **/         //comma evaluates first thing, discards then evaluates second and gives result 
    UINT4 emm,                            /**< second index of the mode **/
    LALDict *lalParams                    /**< extra params **/
 );
