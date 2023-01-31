451948c1b48e5a4ac811f2fae31c006791783af2
duncan.macleod <duncan.macleod@ligo.org>
2023-01-31 15:41:40 +0000

    lalpulsar: refactor conda recipe
    to match upstream changes in lalpulsar-feedstock

--- lalpulsar/conda/build-python.sh
+++ lalpulsar/conda/build-python.sh
@@ -4,58 +4,37 @@
 # for a LALSuite subpackage.
 #
 
-set -ex
+set -e
+
+# load common options
+. ${RECIPE_DIR}/common.sh
 
 # build python in a sub-directory using a copy of the C build
 _builddir="_build${PY_VER}"
 cp -r _build ${_builddir}
 cd ${_builddir}
 
-# customisation for LALSuite development CI
-if [[ "${GITLAB_CI}" == "true" ]] && [[ "x${CI_COMMIT_TAG}" == x ]]; then
-	# declare nightly builds
-	if [ "${CI_PIPELINE_SOURCE}" = "schedule" ] || [ "${CI_PIPELINE_SOURCE}" = "web" ]; then
-		CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-nightly"
-	fi
-# production builds ignore GCC warnings
-else
-	CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-gcc-flags"
-fi
-
-# handle cross compiling
-if [[ "${build_platform}" != "${target_platform}" ]]; then
-	# help2man doesn't work when cross compiling
-	CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-help2man"
-fi
-
-# only link libraries we actually use
-export GSL_LIBS="-L${PREFIX}/lib -lgsl"
-
 # configure only python bindings and pure-python extras
 ${SRC_DIR}/configure \
-	--disable-doxygen \
-	--disable-swig-iface \
-	--enable-cfitsio \
-	--enable-python \
-	--enable-sistr \
-	--enable-swig-python \
-	--prefix="${PREFIX}" \
-	${CONFIGURE_ARGS} \
+  ${CONFIGURE_ARGS} \
+  --disable-swig-iface \
+  --enable-sistr \
+  --enable-python \
+  --enable-swig-python \
 ;
 
 # patch out dependency_libs from libtool archive to prevent overlinking
-# of the C library dependencies to the SWIG library
 sed -i.tmp '/^dependency_libs/d' lib/lib${PKG_NAME##*-}.la
 
-# build (use LIBS="" to doubly-prevent overlinking)
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C swig LIBS=""
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C python LIBS=""
+# build
+${_make} -C swig LIBS=""
+${_make} -C python LIBS=""
 
 # test
-if [[ "${build_platform}" == "${target_platform}" ]]; then
-	make -j ${CPU_COUNT} V=1 VERBOSE=1 check -C swig
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  ${_make} check -C swig
 fi
 
 # install
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C swig install-exec  # swig bindings
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C python install  # pure-python extras
+${_make} -C swig install-exec  # swig bindings
+${_make} -C python install  # pure-python extras
--- lalpulsar/conda/build.sh
+++ lalpulsar/conda/build.sh
@@ -2,43 +2,25 @@
 
 set -ex
 
+# load common options
+. ${RECIPE_DIR}/common.sh
+
 # use out-of-tree build
 mkdir -pv _build
 cd _build
 
-# customisation for LALSuite development CI
-if [[ "${GITLAB_CI}" == "true" ]] && [[ "x${CI_COMMIT_TAG}" == x ]]; then
-	# declare nightly builds
-	if [ "${CI_PIPELINE_SOURCE}" = "schedule" ] || [ "${CI_PIPELINE_SOURCE}" = "web" ]; then
-		CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-nightly"
-	fi
-# production builds ignore GCC warnings
-else
-	CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-gcc-flags"
-fi
-
-# only link libraries we actually use
-export GSL_LIBS="-L${PREFIX}/lib -lgsl"
-export CFITSIO_LIBS="-L${PREFIX}/lib -lcfitsio"
-
 # configure
 ${SRC_DIR}/configure \
-	--disable-doxygen \
-	--disable-help2man \
-	--disable-python \
-	--disable-swig-octave \
-	--disable-swig-python \
-	--enable-cfitsio \
-	--enable-openmp \
-	--enable-swig-iface \
-	--prefix="${PREFIX}" \
-	${CONFIGURE_ARGS} \
+  ${CONFIGURE_ARGS} \
+  --disable-python \
+  --disable-swig-python \
+  --enable-swig-iface \
 ;
 
 # build
-make -j ${CPU_COUNT} V=1 VERBOSE=1
+${_make}
 
 # test
-if [[ "${build_platform}" == "${target_platform}" ]]; then
-	make -j ${CPU_COUNT} V=1 VERBOSE=1 check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1"  ]]; then
+  ${_make} check
 fi
