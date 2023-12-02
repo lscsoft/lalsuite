909666df5adf0f9dc0dab40a568b4120812146f5
John T. Whelan <john.whelan@ligo.org>
2023-12-02 09:10:07 -0500

    PulsarCrossCorr_v2: SWIG directive to handle in/out argument

--- lalpulsar/lib/PulsarCrossCorr_v2.c
+++ lalpulsar/lib/PulsarCrossCorr_v2.c
@@ -2,6 +2,7 @@
  *  Copyright (C) 2012, 2013 John Whelan, Shane Larson and Badri Krishnan
  *  Copyright (C) 2013, 2014 Badri Krishnan, John Whelan, Yuanhao Zhang
  *  Copyright (C) 2016, 2017 Grant David Meadors
+ *  Copyright (C) 2023 John Whelan
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
