f2106785a296d304b78cca78ca31d21d840dde7f
Rossella Gamba <rossella.gamba@ligo.org>
2024-02-23 02:52:38 +0000

    Comment-out trimming, not working properly

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "LALSuite"
+#define PACKAGE_TARNAME "lalsuite"
+#define PACKAGE_VERSION "7.21.1"
+#define PACKAGE_STRING "LALSuite 7.21.1"
+#define PACKAGE_BUGREPORT "lal-discuss@ligo.org"
+#define PACKAGE_URL ""
+#define PACKAGE "lalsuite"
+#define VERSION "7.21.1"
