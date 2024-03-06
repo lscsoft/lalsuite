f8ee22adf4108da9578cb375470e7952e205945b
Karl Wette <karl.wette@ligo.org>
2024-03-06 14:06:17 +1100

    lal*/swig/: Pretty-format all C/Python code

--- lalframe/swig/SWIGTestLALFramePython.py
+++ lalframe/swig/SWIGTestLALFramePython.py
@@ -11,35 +11,43 @@ import lal
 import lalframe
 from lalframe import globalvar as lalframeglobalvar
 from lal import globalvar as lalglobalvar
+
 print("PASSED module load", file=sys.stderr)
 
 # set error handlers
 def set_nice_error_handlers():
     lal.swig_set_nice_error_handlers()
+
+
 def set_default_error_handlers():
-    if 'NASTY_ERROR_HANDLERS' in os.environ:
+    if "NASTY_ERROR_HANDLERS" in os.environ:
         lal.swig_set_nasty_error_handlers()
     else:
         lal.swig_set_nice_error_handlers()
+
+
 set_default_error_handlers()
 
+
 def test_object_parent_tracking():
-    """check object parent tracking
-    """
+    """check object parent tracking"""
 
     print("checking object parent tracking ...", file=sys.stderr)
     a = lalframe.swig_lalframe_test_parent_map_struct()
     for i in range(0, 7):
         b = a.s
         c = lalframeglobalvar.swig_lalframe_test_parent_map.s
-        lalframeglobalvar.swig_lalframe_test_parent_map.s = lalglobalvar.swig_lal_test_struct_const
+        lalframeglobalvar.swig_lalframe_test_parent_map.s = (
+            lalglobalvar.swig_lal_test_struct_const
+        )
     del c
     del b
     del a
-    locals()   # update locals() to remove lingering references
+    locals()  # update locals() to remove lingering references
     lal.CheckMemoryLeaks()
     print("PASSED object parent tracking", file=sys.stderr)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = sys.argv[1:] or ["-v", "-rs", "--junit-xml=junit-SWIGTestLALFramePython.xml"]
     sys.exit(pytest.main(args=[__file__] + args))
