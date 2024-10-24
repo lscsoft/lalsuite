7afd35327f1f4133584cd70819f5a5a49b5dc196
Karl Wette <karl.wette@ligo.org>
2024-10-24 23:09:51 +1100

    LAL: skip tests that require ligo.lw due to circular dependency
    - Ensures that "make check" can be run locally

--- lal/test/python/test_rate.py
+++ lal/test/python/test_rate.py
@@ -1,4 +1,13 @@
 import doctest
+
+try:
+	from ligo.lw import ligolw
+except ImportError as exc:
+	import warnings
+	import sys
+	warnings.warn(str(exc))
+	sys.exit(77)
+
 from lal import rate
 
 if __name__ == '__main__':
