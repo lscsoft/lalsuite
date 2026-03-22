b696ff965a6ed23232164e6dff189b9e28c2ebdb
Karl Wette <karl.wette@ligo.org>
2026-03-22 21:29:22 +1100

    Matrix CI: related Debian packaging changes

--- common/debian/lalsuite.mk
+++ common/debian/lalsuite.mk
@@ -11,37 +11,54 @@ ifneq (,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
 	NUMJOBS = $(patsubst parallel=%,%,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
 endif
 
+ifneq (,$(LALSUITE_DEB_WITH_CUDA))
+# build with CUDA support
+# - CFLAGS: -fno-lto is needed for CUDA linking
+CONFIGUREARGS_CUDA = --with-cuda=$(LALSUITE_DEB_WITH_CUDA) CFLAGS="-O3 -fno-lto"
+# - ignore extra library dependencies from CUDA
+DHSHLIBDEPSARGS_CUDA = --dpkg-shlibdeps-params=--ignore-missing-info
+endif
+
 %:
 	dh $@ \
 		--buildsystem=autoconf \
 		--with=python3 \
 	;
 
+# configure the build for the 'main' python version
 override_dh_auto_configure:
-	# configure the build for the 'main' python version
 	dh_auto_configure -- \
 		--disable-gcc-flags \
 		--disable-swig-octave \
 		$(CONFIGUREARGS) \
-		PYTHON=$(PYTHON)
+		$(CONFIGUREARGS_CUDA) \
+		PYTHON=$(PYTHON) \
+		;
 
+# build for the 'main' python version
 override_dh_auto_build:
-	# build for the 'main' python version
 	dh_auto_build --parallel
 
 override_dh_auto_install:
 	dh_auto_install --destdir=debian/tmp
 
+ifneq (,$(LALSUITE_DEB_WITH_CUDA))
+override_dh_auto_test:
+	echo Tests skipped for CUDA architecture
+else
 override_dh_auto_test:
 	dh_auto_test
+endif
+
+override_dh_missing:
+	find debian -name '*.la' -delete
+	dh_missing
 
 override_dh_shlibdeps:
-	dh_shlibdeps \
-	&& find debian -name '*.la' -delete \
-	&& dh_numpy3 \
-	;
+	dh_shlibdeps $(DHSHLIBDEPSARGS_CUDA)
+	dh_numpy3
 
+# apply debian/lintian-overrides to all binary packages
 override_dh_lintian:
-	# apply debian/lintian-overrides to all binary packages
 	perl -ne 'print "cp -f debian/lintian-overrides debian/$$1.lintian-overrides\n" if /^Package:\s*(\S+)/' debian/control | bash
 	dh_lintian
