cba375786d2a5231368b17948be02adc5595ef87
Karl Wette <karl.wette@ligo.org>
2026-04-25 18:36:48 +1000

    lalframe/swig/SWIGTestLALFramePython.py: sort imports, remove unused imports, whitespace

--- lalframe/swig/SWIGTestLALFramePython.py
+++ lalframe/swig/SWIGTestLALFramePython.py
@@ -1,10 +1,10 @@
 # Check SWIG Python bindings for LALFrame
 # Author: Karl Wette, 2011--2014
 
+import gc
+import inspect
 import os
 import sys
-import inspect
-import gc
 
 import pytest
 
@@ -12,8 +12,8 @@ import pytest
 print("checking module load ...", file=sys.stderr)
 import lal
 import lalframe
-from lalframe import globalvar as lalframeglobalvar
 from lal import globalvar as lalglobalvar
+from lalframe import globalvar as lalframeglobalvar
 
 print("PASSED module load", file=sys.stderr)
 
