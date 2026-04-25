276deb4a770610f05072e0ca66d735ece49f4998
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:37 +1000

    lalframe/python/lalframe/utils/__init__.py: prettify

--- lalframe/python/lalframe/utils/__init__.py
+++ lalframe/python/lalframe/utils/__init__.py
@@ -14,8 +14,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""Frame I/O utilities
-"""
+"""Frame I/O utilities"""
 
 from .. import git_version
 
