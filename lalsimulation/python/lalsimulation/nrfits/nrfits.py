0e35fb20055239184d7c49bf4a3cdc0d967d19fc
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:43 +1000

    lalsimulation/python/lalsimulation/nrfits/nrfits.py: prettify

--- lalsimulation/python/lalsimulation/nrfits/nrfits.py
+++ lalsimulation/python/lalsimulation/nrfits/nrfits.py
@@ -4,7 +4,8 @@ Base class for Numerical Relativity fits such as remnant BH mass, spin, etc.
 Vijay Varma, 2019.
 """
 
-#=============================================================================
+
+# =============================================================================
 class NRFits(object):
     """
     Base class to evaluate NR fits.
@@ -19,9 +20,8 @@ class NRFits(object):
     """
 
     # ------------------------------------------------------------------------
-    def _get_fit_params(self, m1, m2, chiA_vec, chiB_vec, f_ref,
-            extra_params_dict):
-        """ Not all fits will require all of these parameters, this function
+    def _get_fit_params(self, m1, m2, chiA_vec, chiB_vec, f_ref, extra_params_dict):
+        """Not all fits will require all of these parameters, this function
         should be used to get the required params, and if necessary, process
         them to get the parameters that are used to evaluate the actual fits.
 
@@ -37,42 +37,49 @@ class NRFits(object):
 
     # ------------------------------------------------------------------------
     def _eval_fit(self, fit_params, fit_type, extra_params_dict):
-        """ Evaluates a particular fit for a given model using the fit_params
+        """Evaluates a particular fit for a given model using the fit_params
         returned by _get_fit_params().
         """
         raise NotImplementedError("Please override me.")
 
     # ------------------------------------------------------------------------
-    def __call__(self, m1, m2, chiA_vec, chiB_vec, f_ref, fit_types_list,
-            extra_params_dict):
-        """ Evaluates all fits given in fit_types_list and returns them as
-            a dictionary.
+    def __call__(
+        self, m1, m2, chiA_vec, chiB_vec, f_ref, fit_types_list, extra_params_dict
+    ):
+        """Evaluates all fits given in fit_types_list and returns them as
+        a dictionary.
 
-            See eval_fits.eval_nrfit() for the definitions of the arguments of
-            this function.
+        See eval_fits.eval_nrfit() for the definitions of the arguments of
+        this function.
         """
 
         # Get the parameters used to evaluate the fits. This includes any
         # spin evolution that may be required
-        fit_params = self._get_fit_params(m1, m2, chiA_vec, chiB_vec,
-            f_ref, extra_params_dict)
+        fit_params = self._get_fit_params(
+            m1, m2, chiA_vec, chiB_vec, f_ref, extra_params_dict
+        )
 
         # Add more if needed and update the fit_types_list documentation in
         # eval_fits.py
         allowed_fit_types = [
-                "FinalMass",
-                "FinalSpin",
-                "RecoilKick",
-                "PeakLuminosity",
-                ]
+            "FinalMass",
+            "FinalSpin",
+            "RecoilKick",
+            "PeakLuminosity",
+        ]
 
         # Loop over fit_types_list and return a dictionary of values
         return_dict = {}
         for fit_type in fit_types_list:
             if fit_type not in allowed_fit_types:
-                raise ValueError("Invalid fit_type=%s. "%fit_type \
-                    + "Should be one of ["+ ", ".join(allowed_fit_types) + "].")
-            return_dict[fit_type] = self._eval_fit(fit_params, fit_type,
-                    extra_params_dict)
+                raise ValueError(
+                    "Invalid fit_type=%s. " % fit_type
+                    + "Should be one of ["
+                    + ", ".join(allowed_fit_types)
+                    + "]."
+                )
+            return_dict[fit_type] = self._eval_fit(
+                fit_params, fit_type, extra_params_dict
+            )
 
         return return_dict
