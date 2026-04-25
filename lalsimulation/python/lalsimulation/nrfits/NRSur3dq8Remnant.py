b845ddf872a0bb2727eb46a206c6bfa5c2bc6c90
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:43 +1000

    lalsimulation/python/lalsimulation/nrfits/NRSur3dq8Remnant.py: prettify

--- lalsimulation/python/lalsimulation/nrfits/NRSur3dq8Remnant.py
+++ lalsimulation/python/lalsimulation/nrfits/NRSur3dq8Remnant.py
@@ -39,9 +39,8 @@ class NRSur3dq8Remnant(NRFits):
     """
 
     # ------------------------------------------------------------------------
-    def _get_fit_params(self, m1, m2, chiA_vec, chiB_vec, f_ref,
-            extra_params_dict):
-        """ No processing or spin evolution is required here, just returns the
+    def _get_fit_params(self, m1, m2, chiA_vec, chiB_vec, f_ref, extra_params_dict):
+        """No processing or spin evolution is required here, just returns the
         mass ratio, total mass and spins along the z-direction.
 
         See eval_fits.eval_nrfit() for the definitions of the arguments of
@@ -61,13 +60,13 @@ class NRSur3dq8Remnant(NRFits):
         if f_ref != -1:
             raise ValueError("This model only works for f_ref=-1.")
 
-        q = m1/m2
+        q = m1 / m2
         fit_params = [q, chiA_vec[2], chiB_vec[2]]
         return fit_params
 
     # ------------------------------------------------------------------------
     def _eval_fit(self, fit_params, fit_type, extra_params_dict):
-        """ Evaluates a particular fit for NRSur3dq8Remnant using the fit_params
+        """Evaluates a particular fit for NRSur3dq8Remnant using the fit_params
         returned by _get_fit_params().
         """
         q, chiAz, chiBz = fit_params
@@ -81,14 +80,16 @@ class NRSur3dq8Remnant(NRFits):
         elif fit_type == "FinalSpin":
             # chifx and chify are zero for aligned-spin systems
             chifz = lalsim.NRSur3dq8Remnant(q, chiAz, chiBz, "chifz", LALParams)
-            val = [0,0,chifz]
+            val = [0, 0, chifz]
         elif fit_type == "RecoilKick":
             # vfz is zero for aligned-spin systems
             vfx = lalsim.NRSur3dq8Remnant(q, chiAz, chiBz, "vfx", LALParams)
             vfy = lalsim.NRSur3dq8Remnant(q, chiAz, chiBz, "vfy", LALParams)
             val = [vfx, vfy, 0]
         else:
-            raise ValueError("Invalid fit_type=%s. This model only allows "
-                "'FinalMass', 'FinalSpin' and 'RecoilKick'."%fit_type)
+            raise ValueError(
+                "Invalid fit_type=%s. This model only allows "
+                "'FinalMass', 'FinalSpin' and 'RecoilKick'." % fit_type
+            )
 
         return np.atleast_1d(val)
