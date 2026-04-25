d3f9410684952b5753eca2cb45a738eeeaa17b47
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:40 +1000

    lalinference/python/lalinference/io/__init__.py: prettify

--- lalinference/python/lalinference/io/__init__.py
+++ lalinference/python/lalinference/io/__init__.py
@@ -23,9 +23,11 @@ __all__ = ()
 
 # Import all symbols from all submodules of this module.
 for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]):
-    exec('from . import {0};'
-         '__all__ += getattr({0}, "__all__", ());'
-         'from .{0} import *'.format(module))
+    exec(
+        "from . import {0};"
+        '__all__ += getattr({0}, "__all__", ());'
+        "from .{0} import *".format(module)
+    )
     del module
 
 # Clean up
