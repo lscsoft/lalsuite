fdc6c28701fa0fcdce0f63034ff6c000d4d33585
Karl Wette <karl.wette@ligo.org>
2026-04-25 18:36:51 +1000

    lalinference/bin/lalinference_pipe.py: sort imports, remove unused imports, whitespace

--- lalinference/bin/lalinference_pipe.py
+++ lalinference/bin/lalinference_pipe.py
@@ -2,16 +2,18 @@
 # DAG generation code for running LALInference pipeline
 # (C) 2012 John Veitch, Vivien Raymond
 
-from lalinference import lalinference_pipe_utils as pipe_utils
-import numpy as np
 import configparser
-from optparse import OptionParser
-import sys
 import os
+import sys
 import uuid
-from lal import pipeline
+from optparse import OptionParser
+
+import numpy as np
 from igwn_ligolw import lsctables
 from igwn_ligolw import utils as ligolw_utils
+
+from lal import pipeline
+from lalinference import lalinference_pipe_utils as pipe_utils
 
 usage=""" %prog [options] config.ini
 Setup a Condor DAG file to run the LALInference pipeline based on
