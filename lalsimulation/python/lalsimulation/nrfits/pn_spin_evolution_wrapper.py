3b7940420d0df8f8f2227b8c6ba49dcd792d3c98
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:43 +1000

    lalsimulation/python/lalsimulation/nrfits/pn_spin_evolution_wrapper.py: prettify

--- lalsimulation/python/lalsimulation/nrfits/pn_spin_evolution_wrapper.py
+++ lalsimulation/python/lalsimulation/nrfits/pn_spin_evolution_wrapper.py
@@ -4,8 +4,9 @@ import lalsimulation as lalsim
 from lal import MSUN_SI, MTSUN_SI
 
 
-def spin_evolution(q, chiA0, chiB0, omega0, approximant='SpinTaylorT4',
-        dt=0.1, spinO=6, phaseO=7):
+def spin_evolution(
+    q, chiA0, chiB0, omega0, approximant="SpinTaylorT4", dt=0.1, spinO=6, phaseO=7
+):
     """
     Wrapper for PN spin and dynamics evolution in LAL.
     Inputs:
@@ -37,17 +38,17 @@ def spin_evolution(q, chiA0, chiB0, omega0, approximant='SpinTaylorT4',
     approxTag = lalsim.SimInspiralGetApproximantFromString(approximant)
 
     # Total mass in solar masses
-    M = 100     # This does not affect the returned values as they are
-                # dimensionless
+    M = 100  # This does not affect the returned values as they are
+    # dimensionless
 
     # time step and initial GW freq in SI units
-    MT = M*MTSUN_SI
-    deltaT = dt*MT
-    fStart = omega0/np.pi/MT
+    MT = M * MTSUN_SI
+    deltaT = dt * MT
+    fStart = omega0 / np.pi / MT
 
     # component masses of the binary
-    m1_SI =  M*MSUN_SI*q/(1.+q)
-    m2_SI =  M*MSUN_SI/(1.+q)
+    m1_SI = M * MSUN_SI * q / (1.0 + q)
+    m2_SI = M * MSUN_SI / (1.0 + q)
 
     # spins at fStart
     s1x, s1y, s1z = chiA0
@@ -57,7 +58,7 @@ def spin_evolution(q, chiA0, chiB0, omega0, approximant='SpinTaylorT4',
     fEnd = 0
 
     # initial value of orbital angular momentum unit vector, i.e at fStart
-    lnhatx, lnhaty, lnhatz = 0,0,1
+    lnhatx, lnhaty, lnhatz = 0, 0, 1
 
     # initial value of orbital plane basis vector, i.e at fStart
     e1x, e1y, e1z = 1, 0, 0
@@ -77,11 +78,36 @@ def spin_evolution(q, chiA0, chiB0, omega0, approximant='SpinTaylorT4',
     # evolve spins and collect data into a nice format. The input values start
     # with lower-case letters, while output values start with upper-case
     # letters.
-    V, Phi, S1x, S1y, S1z, S2x, S2y, S2z, LNhatx, LNhaty, LNhatz, \
-        E1x, E1y, E1z = lalsim.SimInspiralSpinTaylorPNEvolveOrbit(deltaT, \
-        m1_SI, m2_SI, fStart, fEnd, s1x, s1y, s1z, s2x, s2y, s2z, \
-        lnhatx, lnhaty, lnhatz, e1x, e1y, e1z, lambda1, lambda2, \
-        quadparam1, quadparam2, spinO, tideO, phaseO, lscorr, approxTag)
+    V, Phi, S1x, S1y, S1z, S2x, S2y, S2z, LNhatx, LNhaty, LNhatz, E1x, E1y, E1z = (
+        lalsim.SimInspiralSpinTaylorPNEvolveOrbit(
+            deltaT,
+            m1_SI,
+            m2_SI,
+            fStart,
+            fEnd,
+            s1x,
+            s1y,
+            s1z,
+            s2x,
+            s2y,
+            s2z,
+            lnhatx,
+            lnhaty,
+            lnhatz,
+            e1x,
+            e1y,
+            e1z,
+            lambda1,
+            lambda2,
+            quadparam1,
+            quadparam2,
+            spinO,
+            tideO,
+            phaseO,
+            lscorr,
+            approxTag,
+        )
+    )
     V = np.array(V.data.data)
     Phi = np.array(Phi.data.data)
     ChiA = np.array([S1x.data.data, S1y.data.data, S1z.data.data]).T
