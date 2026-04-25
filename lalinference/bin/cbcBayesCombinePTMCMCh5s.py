9e77950e61bcdda5f06ad3ed260db3f0852aa3bd
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:38 +1000

    lalinference/bin/cbcBayesCombinePTMCMCh5s.py: prettify

--- lalinference/bin/cbcBayesCombinePTMCMCh5s.py
+++ lalinference/bin/cbcBayesCombinePTMCMCh5s.py
@@ -29,32 +29,37 @@ import h5py
 
 from lalinference import git_version
 
-__author__="Ben Farr <benjamin.farr@ligo.org>"
-__version__= "git id %s"%git_version.id
-__date__= git_version.date
+__author__ = "Ben Farr <benjamin.farr@ligo.org>"
+__version__ = "git id %s" % git_version.id
+__date__ = git_version.date
 
-USAGE='''%prog [options] PTMCMC_datafile.hdf5 [PTMCMC_datafile2.hdf5 ...]
+USAGE = """%prog [options] PTMCMC_datafile.hdf5 [PTMCMC_datafile2.hdf5 ...]
 Combine chains from a parallel-tempered MCMC run spread across several HDF5 files.
-'''
+"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = OptionParser(USAGE)
     parser.add_option(
-        '-o', '--outfile', type='string', default=None,
-        help='Output file for posterior samples. If None, file containing T=1 chain will be used', metavar='combined_chains.hdf5')
+        "-o",
+        "--outfile",
+        type="string",
+        default=None,
+        help="Output file for posterior samples. If None, file containing T=1 chain will be used",
+        metavar="combined_chains.hdf5",
+    )
     opts, args = parser.parse_args()
 
     datafiles = args
 
-    group_id = '/lalinference/lalinference_mcmc'
+    group_id = "/lalinference/lalinference_mcmc"
 
     outfile = opts.outfile
 
     # find the file containing the coldest chain (i.e. a posterior_samples group)
     for datafile in datafiles:
         rootfile = datafile
-        possible_root = h5py.File(datafile, 'a')
-        if group_id + '/posterior_samples' in possible_root:
+        possible_root = h5py.File(datafile, "a")
+        if group_id + "/posterior_samples" in possible_root:
             possible_root.close()
             break
         else:
@@ -63,23 +68,24 @@ if __name__ == '__main__':
     if outfile:
         if outfile != rootfile:
             try:
-                outp = h5py.File(outfile, 'w-')
+                outp = h5py.File(outfile, "w-")
 
                 # Copy contents of the root hdf5 to preserve metadata
-                with h5py.File(rootfile, 'r') as inp:
+                with h5py.File(rootfile, "r") as inp:
                     for group in inp.keys():
-                       inp.copy(group, outp['/'])
+                        inp.copy(group, outp["/"])
 
             except IOError:
-                assert outfile in datafiles, \
-                    "Trying to write to an existing file that isn't being explicitly combined"
+                assert (
+                    outfile in datafiles
+                ), "Trying to write to an existing file that isn't being explicitly combined"
 
-                outp = h5py.File(outfile, 'a')
+                outp = h5py.File(outfile, "a")
 
                 # Copy root over now for consistent behavior
-                with h5py.File(rootfile, 'r') as inp:
+                with h5py.File(rootfile, "r") as inp:
                     for chain in inp[group_id].keys():
-                        chain_id = group_id + '/' + chain
+                        chain_id = group_id + "/" + chain
                         inp.copy(chain_id, outp[group_id])
 
             # make sure the target group exists
@@ -93,9 +99,9 @@ if __name__ == '__main__':
     for datafile in datafiles:
         if datafile == outfile:
             continue
-        with h5py.File(datafile, 'r') as inp:
+        with h5py.File(datafile, "r") as inp:
             for chain in inp[group_id].keys():
-                chain_id = group_id + '/' + chain
+                chain_id = group_id + "/" + chain
                 if chain_id not in outp:
-                   inp.copy(chain_id, outp[group_id])
+                    inp.copy(chain_id, outp[group_id])
     outp.close()
