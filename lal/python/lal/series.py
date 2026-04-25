3e5042d5d4515683f9e52c134ea7b5bee6a51ec3
Karl Wette <karl.wette@ligo.org>
2026-04-25 23:07:33 +1000

    lal/python/lal/series.py: prettify

--- lal/python/lal/series.py
+++ lal/python/lal/series.py
@@ -2,4 +2,7 @@ from warnings import warn
 
 from lalmetaio.series import *
 
-warn("The module lal.series is deprecated. Please import lalmetaio.series instead.", DeprecationWarning)
+warn(
+    "The module lal.series is deprecated. Please import lalmetaio.series instead.",
+    DeprecationWarning,
+)
